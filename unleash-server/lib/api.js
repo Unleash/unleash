f93b61485e43d217075508de76f300f4c1050cb1
svelovla <svein.erik.lovland@finn.no>
2014-10-20 15:12:30 +0200

    #8  mocked api respons

--- unleash-server/lib/api.js
+++ unleash-server/lib/api.js
@@ -1,5 +1,41 @@
-module.exports = function (app) {
-    app.get('/features', function (req, res) {
-        res.send('some nice json features here!');
+var featuresMock = require('./featuresMock');
+
+function getFeature(name) {
+    var featureFound;
+    featuresMock.forEach(function (feature) {
+        if (feature.name === name) {
+            featureFound = feature;
+        }
     });
+    return featureFound;
+}
+
+module.exports = function (app) {
+
+    app.get('/features', function (req, res) {
+        res.json(featuresMock);
+    });
+
+    app.get('/features/:id', function (req, res) {
+        var feature = getFeature(req.params.id);
+
+        if (feature) {
+            res.json(feature);
+        } else {
+            res.json(404, {error: 'Could not find feature'});
+        }
+    });
+
+    app.post('/features', function (req, res) {
+        var newFeature = req.body;
+
+        if (!getFeature(newFeature.name)) {
+            featuresMock.push(newFeature);
+            res.status(201).end();
+        } else {
+            res.status(500).end();
+        }
+    });
+
 };
+
---
+++ unleash-server/lib/featuresMock.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        "name": "featureX",
+        "status": "on",
+        "strategy": "default"
+    },
+    {
+        "name": "featureY",
+        "status": "off",
+        "strategy": "baz",
+        "parameters": {
+            "foo": "bar"
+        }
+    },
+    {
+        "name": "featureZ",
+        "status": "on",
+        "strategy": "baz",
+        "parameters": {
+            "foo": "rab"
+        }
+    }
+];

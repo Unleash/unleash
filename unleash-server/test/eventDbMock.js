c32b101093e0bdec5db0f6f604a306d3586c107c
Ivar Conradi Østhus <ivarconr@gmail.com>
2014-10-23 16:00:51 +0200

    Patch events should update features.

--- unleash-server/test/eventDbMock.js
+++ unleash-server/test/eventDbMock.js
@@ -1,7 +1,6 @@
 var Promise = require('bluebird');
 
 function storeEvent(event) {
-    console.log('using eventDbMock to store: ', event);
     return new Promise(function (resolve) {
         resolve();
     });

8e6ed4d73b35a82c994b3c67bd79f9484c660138
Jari Bakken <jari.bakken@gmail.com>
2014-10-23 14:39:50 +0200

    Fail fast if DB is not configured

--- unleash-server/lib/dbPool.js
+++ unleash-server/lib/dbPool.js
@@ -6,19 +6,18 @@ var anyDB = require('any-db'),
 
 
 function createDbPool() {
-    // Use postgres db from DATABASE_URL if set
     if (process.env.DATABASE_URL) {
         logger.info('unleash started with DATABASE_URL');
         return anyDB.createPool(process.env.DATABASE_URL);
-    }
-
-    // Finn specific way of delivering env variables
-    else if(nconf.argv().get('databaseini') !== undefined) {
+    } else if (nconf.argv().get('databaseini') !== undefined) {
+        // Finn specific way of delivering env variables
         var databaseini = nconf.argv().get('databaseini');
         logger.info('unleash started with databaseini: ' + databaseini);
         var config = ini.parse(fs.readFileSync(databaseini, 'utf-8'));
         return anyDB.createPool(config.DATABASE_URL, {min: 2, max: 20});
     }
+
+    throw new Error('please set DATABASE_URL or pass --databaseini');
 }
 
 module.exports = createDbPool();

6f81468cb7959404f86bc906fad5b60486f35851
Ivar Conradi Østhus <ivarconr@gmail.com>
2014-10-23 09:03:46 +0200

    issue #18 - updated the inital db

--- unleash-server/migrations/sql/001-initial-schema.up.sql
+++ unleash-server/migrations/sql/001-initial-schema.up.sql
@@ -7,12 +7,19 @@ CREATE TABLE strategies (
 CREATE TABLE features (
   created_at timestamp default now(),
   name varchar(255) PRIMARY KEY NOT NULL,
+  enabled integer default 0,
   strategy_name varchar(255) references strategies(name),
   parameters json
 );
 
 CREATE TABLE events (
+  id serial primary key,
   created_at timestamp default now(),
   type varchar(255) NOT NULL,
   data json
 );
+
+GRANT ALL ON TABLE events TO unleash_user;
+GRANT ALL ON TABLE features TO unleash_user;
+GRANT ALL ON TABLE strategies TO unleash_user;
+GRANT USAGE, SELECT ON SEQUENCE events_id_seq TO unleash_user;

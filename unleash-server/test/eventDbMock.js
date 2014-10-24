4f1ccc426d6e80254d9bcafea868fb9e07c6d61a
andsandv <anders.olsen.sandvik@finn.no>
2014-10-24 16:15:59 +0200

    Add events test

--- unleash-server/test/eventDbMock.js
+++ unleash-server/test/eventDbMock.js
@@ -1,4 +1,45 @@
-var Promise = require('bluebird');
+var Promise = require("bluebird");
+
+var events = [
+    {
+        "id": 1,
+        "created_at": 1414159948677,
+        "type": "feature-create",
+        "created_by": "me",
+        "data": {
+            
+        }
+    },
+    {
+        "id": 2,
+        "created_at": 1414159948677,
+        "type": "feature-create",
+        "created_by": "me",
+        "data": {
+            "foo": "bar"
+        }
+    },
+    {
+        "id": 3,
+        "created_at": 1414159948677,
+        "type": "feature-create",
+        "created_by": "me",
+        "data": {
+            "foo": "rab"
+        }
+    }
+];
+
+function getEvent(name) {
+    var eventFound;
+    events.forEach(function (event) {
+        if (event.name === name) {
+            eventFound = event;
+        }
+    });
+
+    return eventFound;
+}
 
 function storeEvent() {
     return new Promise(function (resolve) {
@@ -8,5 +49,20 @@ function storeEvent() {
 
 
 module.exports = {
-    store: storeEvent
+    store: storeEvent,
+
+    getEvents: function() {
+        return new Promise(function (resolve) {
+            resolve(events);
+        });
+    },
+
+    getEvent: function(name) {
+        var event = getEvent(name);
+        if(event) {
+            return Promise.resolve(event);
+        } else {
+            return Promise.reject("Event not found");
+        }
+    }
 };

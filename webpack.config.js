43ff5732aa31707a4d8099cba30450769b5bebac
Ivar Østhus <ivarconr@gmail.com>
2014-11-06 21:26:55 +0100

    Modified configuration to make it run on heroku.
    Please note that scripts and webpack is moved from
    devDependences to dependencies. This is due to heroku
    running the build process in production mode.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
 // docs: http://webpack.github.io/docs/configuration.html
 
+var path = require('path');
+var root = path.normalize(path.join(__dirname, '.'));
+var js_root = path.join(path.join(root, 'public'), 'js');
+
 module.exports = {
 
-    context: __dirname + '/public',
-    entry: './js/app',
+    context: js_root,
+    entry: 'app',
 
     output: {
-        path: __dirname + '/public/js',
+        path: js_root,
         filename: 'bundle.js',
         publicPath: '/js/'
     },
 
     resolve: {
-        extensions: ['', '.js', '.jsx']
+	root: [js_root],
+        extensions: ['', '.js', '.jsx'],
+	modulesDirectories: ["web_modules", "node_modules"]
     },
 
     module: {

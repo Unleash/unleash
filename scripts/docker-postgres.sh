3dfe27cbabc3f4fbc415558aa56f6d8dae6fc272
Gard Rimestad <gardleopard@gmail.com>
2014-12-15 09:25:31 +0100

    Start postgres on random port in docker #64

--- scripts/docker-postgres.sh
+++ scripts/docker-postgres.sh
@@ -3,23 +3,26 @@ export POSTGRES_PASSWORD="uleash"
 
 echo "starting postgres in docker "
 
-HASH=`docker run -p 0.0.0.0:15432:5432 --name unleash-postgres -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d postgres:9.3`
-
+HASH=`docker run -P --name unleash-postgres -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD -d postgres:9.3`
+export PGPORT=`docker ps| grep unleash-post| awk '{print $(NF-1)}'| awk -F "->" '{print $1}'| awk -F \: '{print $2}'`
+echo "PGPORT: $PGPORT"
+echo ""
 # ----------- Wait for postgres to start -----------
-for i in `seq 1 120`;
-do
-  echo -n "."
-  sleep 1
-  netcat -z experimental-docker.finntech.no 15432 && echo "postgres is up and running in docker in $i seconds!" && break
-done
-
 if [ -z "$DOCKER_HOST" ]
   then
     export database_host="127.0.0.1"
 else
   export database_host=$(echo $DOCKER_HOST |awk -F \/ '{print $NF}'| awk -F \: '{print $1}')
 fi
-export TEST_DATABASE_URL=postgres://postgres:unleash@$database_host:15432/postgres
+for i in `seq 1 120`;
+do
+  echo -n "."
+  sleep 1
+  netcat -z $database_host $PGPORT && echo "postgres is up and running in docker in $i seconds!" && break
+done
+
+
+export TEST_DATABASE_URL=postgres://postgres:unleash@$database_host:$PGPORT/postgres
 
 npm install
 DATABASE_URL=$TEST_DATABASE_URL ./node_modules/.bin/db-migrate up

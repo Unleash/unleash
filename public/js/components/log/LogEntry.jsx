b1f2bce2489b93b81ef654f94cd85a9451bc4bc3
Jari Bakken <jari@finn.no>
2015-01-26 16:54:50 +0100

    log views: show event diffs by default, toggle to show the full event.

--- public/js/components/log/LogEntry.jsx
+++ public/js/components/log/LogEntry.jsx
@@ -1,5 +1,19 @@
 var React = require('react');
 
+var DIFF_PREFIXES = {
+    A: ' ',
+    E: ' ',
+    D: '-',
+    N: '+'
+}
+
+var SPADEN_CLASS = {
+    A: 'blue', // array edited
+    E: 'blue', // edited
+    D: 'negative', // deleted
+    N: 'positive', // added
+}
+
 var LogEntry = React.createClass({
     propTypes: {
         event: React.PropTypes.object.isRequired
@@ -7,25 +21,64 @@ var LogEntry = React.createClass({
 
     render: function() {
         var d = new Date(this.props.event.createdAt);
+
+        return (
+            <tr>
+            <td>
+            {d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear()}<br />
+            kl. {d.getHours() + "." + d.getMinutes()}
+            </td>
+            <td>
+            <strong>{this.props.event.data.name}</strong><em>[{this.props.event.type}]</em>
+            </td>
+            <td style={{maxWidth: 300}}>
+              {this.renderEventDiff()}
+            </td>
+            <td>{this.props.event.createdBy}</td>
+            </tr>
+        );
+    },
+
+    renderFullEventData: function() {
         var localEventData = JSON.parse(JSON.stringify(this.props.event.data));
         delete localEventData.description;
         delete localEventData.name;
-        return (
-            <tr>
-                <td>
-                    {d.getDate() + "." + d.getMonth() + "." + d.getFullYear()}<br />
-                    kl. {d.getHours() + "." + d.getMinutes()}
-                </td>
-                <td>
-                    <strong>{this.props.event.data.name}</strong><em>[{this.props.event.type}]</em>
-                </td>
-                <td style={{maxWidth: 300}}>
-                    <code className='JSON smalltext man'>{JSON.stringify(localEventData, null, 2)}</code>
-                </td>
-                <td>{this.props.event.createdBy}</td>
-            </tr>
-        );
+
+        var prettyPrinted = JSON.stringify(localEventData, null, 2);
+
+        return (<code className='JSON smalltext man'>{prettyPrinted}</code>)
+    },
+
+    renderEventDiff: function() {
+        if (!this.props.showFullEvents && this.props.event.diffs) {
+            var changes = this.props.event.diffs.map(this.buildDiff);
+            return (<code className='smalltext man'>{changes.length === 0 ? '(no changes)' : changes}</code>)
+        } else {
+            return this.renderFullEventData();
+        }
+    },
+
+    buildDiff: function(diff, idx) {
+        var change;
+        var key = diff.path.join('.');
+
+        if (diff.lhs !== undefined && diff.rhs !== undefined) {
+            change = (
+                <div>
+                  <div className={SPADEN_CLASS.D}>- {key}: {JSON.stringify(diff.lhs)}</div>
+                  <div className={SPADEN_CLASS.N}>+ {key}: {JSON.stringify(diff.rhs)}</div>
+                </div>
+            );
+        } else {
+            var spadenClass = SPADEN_CLASS[diff.kind]
+            var prefix      = DIFF_PREFIXES[diff.kind];
+
+            change = (<div className={spadenClass}>{prefix} {key}: {JSON.stringify(diff.rhs)}</div>)
+        }
+
+        return (<div key={idx}>{change}</div>)
     }
+
 });
 
 module.exports = LogEntry;

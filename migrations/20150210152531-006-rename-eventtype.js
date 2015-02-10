90aa74a464a33d9a6bcf180591ef0c0a323a198a
andsandv <anders.olsen.sandvik@finn.no>
2015-02-10 16:27:59 +0100

    #80 Add schema change

---
+++ migrations/20150210152531-006-rename-eventtype.js
@@ -0,0 +1,1 @@
+module.exports = require('../lib/migrationRunner').create('006-rename-eventtype');
---
+++ migrations/sql/006-rename-eventtype.down.sql
@@ -0,0 +1,2 @@
+UPDATE events SET type=feature-revive WHERE type=feature-revived;
+UPDATE events SET type=feature-archive WHERE type=feature-archived;

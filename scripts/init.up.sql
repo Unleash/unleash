d7ece7825e99f94df3f1bc435c35548b050e9ba7
sveisvei <sveinung.rosaker@gmail.com>
2016-06-16 23:30:21 +0200

    remove finn stuff

--- scripts/init.up.sql
+++
@@ -1,2 +0,0 @@
-alter default privileges for user unleash_admin grant select,insert,update,delete on tables TO unleash_user;
-alter default privileges for user unleash_admin grant select,update on sequences TO unleash_user;

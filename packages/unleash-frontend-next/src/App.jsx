dcdd5ac31df7a1b79b929c962c54d2d0e9014969
Ivar <ivarconr@gmail.com>
2016-09-11 21:54:31 +0200

    Setup react, webpack, babel, hmr etc #153

---
+++ packages/unleash-frontend-next/src/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1>Hello, world</h1>
+    );
+  }
+}
---
+++ packages/unleash-frontend-next/src/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));

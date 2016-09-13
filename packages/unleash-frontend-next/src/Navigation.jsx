bb5168c80584df7cc1341e3e7ba078b212d49290
Ivar <ivarconr@gmail.com>
2016-09-13 23:01:38 +0200

    fix styling #153

--- packages/unleash-frontend-next/src/Navigation.jsx
+++ packages/unleash-frontend-next/src/Navigation.jsx
@@ -5,11 +5,10 @@ import { ListSubHeader, List, ListItem } from 'react-toolbox';
 export default class UnleashNav extends Component {
     render () {
         return (
-                <List>
-                    <ListSubHeader caption="Menu" />
-                    <ListItem><Link to="/features">Feature Toggles </Link></ListItem>
-                    <ListItem><Link to="/strategies">Strategies </Link></ListItem>
-                    <ListItem><a href="https://github.com/finn-no/unleash/" target="_blank">GitHub </a></ListItem>
+                <List selectable ripple>
+                    <Link to="/features"><ListItem selectable className="active" caption="Feature Toggles" /></Link>
+                    <Link to="/strategies"><ListItem selectable caption="Strategies" /></Link>
+                    <a href="https://github.com/finn-no/unleash/" target="_blank"><ListItem selectable caption="GitHub" /></a>
                 </List>
         );
     }

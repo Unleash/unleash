299fa9964bf839cf03b056ba14b024ee0f9e987d
Ivar <ivarconr@gmail.com>
2016-09-13 21:35:16 +0200

    hacking more of layout #153

--- packages/unleash-frontend-next/src/App.jsx
+++ packages/unleash-frontend-next/src/App.jsx
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
-import { AppBar } from 'react-toolbox';
+import { Layout, Panel, NavDrawer } from 'react-toolbox';
+import { AppBar, IconButton } from 'react-toolbox';
 
 import Navigation from './Navigation';
 
 export default class App extends Component {
+    state = {
+        drawerActive: false,
+    };
+
+    toggleDrawerActive = () => {
+        this.setState({ drawerActive: !this.state.drawerActive });
+    };
+
     render () {
         return (
             <div>
-                <AppBar flat leftIcon="(Unleash)">
-                    <Navigation />
+                <AppBar>
+                    <IconButton icon="menu" onClick={ this.toggleDrawerActive }/>
                 </AppBar>
-                {this.props.children}
+                <Layout>
+                    <NavDrawer
+                        active={this.state.drawerActive}
+                        permanentAt="sm" style={{ width: '200px' }}>
+                        <Navigation />
+                    </NavDrawer>
+                    <Panel scrollY={false}>
+                        <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem', minHeight: '100%' }}>
+                            {this.props.children}
+                        </div>
+
+                    </Panel>
+                </Layout>
             </div>
         );
     }

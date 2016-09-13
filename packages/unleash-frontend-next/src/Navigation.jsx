6b89e958df1b7de279fceba2a9195000c501411f
Ivar <ivarconr@gmail.com>
2016-09-13 21:35:16 +0200

    hacking more of layout #153

--- packages/unleash-frontend-next/src/Navigation.jsx
+++ packages/unleash-frontend-next/src/Navigation.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { Navigation } from 'react-toolbox';
+import { ListSubHeader, List, ListItem } from 'react-toolbox';
 
 export default class UnleashNav extends Component {
     render () {
         return (
-            <Navigation type="horizontal">
-                <Link to="/features">Feature Toggles </Link>
-                <Link to="/strategies">Strategies </Link>
-                <a href="https://github.com/finn-no/unleash/" target="_blank">GitHub </a>
-            </Navigation>
+                <List>
+                    <ListSubHeader caption="Menu" />
+                    <ListItem><Link to="/features">Feature Toggles </Link></ListItem>
+                    <ListItem><Link to="/strategies">Strategies </Link></ListItem>
+                    <ListItem><a href="https://github.com/finn-no/unleash/" target="_blank">GitHub </a></ListItem>
+                </List>
         );
     }
 };
--- packages/unleash-frontend-next/src/Strategies.jsx
+++ packages/unleash-frontend-next/src/Strategies.jsx
@@ -4,7 +4,9 @@ import React, { Component } from 'react';
 export default class Strategies extends Component {
     render () {
         return (
-            <h1>Strategies</h1>
+            <p>
+                <h1>Strategies</h1>
+            </p>
         );
     }
 };

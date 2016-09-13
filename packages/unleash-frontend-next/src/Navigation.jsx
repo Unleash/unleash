834a0a151db3ff8b0c2799126e0842dbf328c2d1
Ivar <ivarconr@gmail.com>
2016-09-13 23:10:58 +0200

    a few more menu-items #153

--- packages/unleash-frontend-next/src/Navigation.jsx
+++ packages/unleash-frontend-next/src/Navigation.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { ListSubHeader, List, ListItem } from 'react-toolbox';
+import { ListSubHeader, List, ListItem, ListDivider } from 'react-toolbox';
 
 export default class UnleashNav extends Component {
     render () {
@@ -8,8 +8,18 @@ export default class UnleashNav extends Component {
                 <List selectable ripple>
                     <Link to="/features"><ListItem selectable className="active" caption="Feature Toggles" /></Link>
                     <Link to="/strategies"><ListItem selectable caption="Strategies" /></Link>
+                    <Link to="/log"><ListItem selectable caption="Log" /></Link>
+                    <Link to="/archive"><ListItem selectable caption="Archive" /></Link>
+                    <ListDivider />
+                    <ListSubHeader Resources/>
+                    <Link to="/archive"><ListItem selectable caption="Documentation" /></Link>
                     <a href="https://github.com/finn-no/unleash/" target="_blank"><ListItem selectable caption="GitHub" /></a>
+                    <ListDivider />
+                    <ListItem selectable={false} ripple="false">
+                        <p>A product by <a href="https://finn.no" target="_blank">FINN.no</a></p>
+                    </ListItem>
                 </List>
+
         );
     }
 };

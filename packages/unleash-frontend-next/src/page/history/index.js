cfd604b36e22221cbbf4f44e8ec71c9adea8085e
Ivar <ivarconr@gmail.com>
2016-09-22 13:52:54 +0200

    rename Log-page to HistoryPage

---
+++ packages/unleash-frontend-next/src/page/history/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EventHisotry extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Event history</h1>
+            </div>
+        );
+    }
+};

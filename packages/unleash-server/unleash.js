e2d5153edb77dbebed012a83b6cc8da1b06c420e
ivaosthu <ivar.conradi.osthus@finn.no>
2016-09-29 18:10:23 +0200

    upgrade db-migrate to support programmatic API #150

--- packages/unleash-server/unleash.js
+++ packages/unleash-server/unleash.js
@@ -2,14 +2,21 @@
 
 'use strict';
 
+process.env.NODE_ENV = 'production';
+
 const program = require('commander');
 const unleash = require('./server.js');
 
 
 program
-    .command('start', 'start unleash server')
-    .command('migrate', 'migrate the unleash db')
+    .option('-p, --port <port>', 'The full port you want to start unleash on.')
     .option('-d, --databaseUri <databaseUri>', 'The full databaseUri to connect to, including username and password')
     .parse(process.argv);
 
-unleash.start({ databaseUri: program.databaseUri });
+unleash.start({
+    databaseUri: program.databaseUri || process.env.DATABASE_URL,
+    port: program.port || process.env.PORT || 4242,
+}).then(conf => {
+    console.log(`Unleash started on port:${conf.app.get('port')}`);
+});
+

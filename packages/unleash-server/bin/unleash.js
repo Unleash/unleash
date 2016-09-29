a5b54818c303d3174c65379800ca3cc7c43d6832
Ivar <ivarconr@gmail.com>
2016-09-29 22:50:55 +0200

    Minor fixes after feedback #150

--- packages/unleash-server/bin/unleash.js
+++ packages/unleash-server/bin/unleash.js
@@ -5,11 +5,11 @@
 process.env.NODE_ENV = 'production';
 
 const program = require('commander');
-const unleash = require('./server.js');
+const unleash = require('../server.js');
 
 
 program
-    .option('-p, --port <port>', 'The full port you want to start unleash on.')
+    .option('-p, --port <port>', 'The port you want to start unleash on')
     .option('-d, --databaseUri <databaseUri>', 'The full databaseUri to connect to, including username and password')
     .parse(process.argv);
 

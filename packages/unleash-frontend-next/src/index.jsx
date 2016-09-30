cd6fde6526598a4a493f92676e09be9e4a7ea2e7
ivaosthu <ivar.conradi.osthus@finn.no>
2016-09-30 14:29:51 +0200

    fixed more layout to new design #153

--- packages/unleash-frontend-next/src/index.jsx
+++ packages/unleash-frontend-next/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRedirect, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
@@ -9,6 +9,7 @@ import store from './store';
 import App from './App';
 
 import Features from './page/features';
+import CreateFeatureToggle from './page/features/create';
 import Strategies from './page/strategies';
 import HistoryPage from './page/history';
 import Archive from './page/archive';
@@ -24,7 +25,9 @@ ReactDOM.render(
     <Provider store={unleashStore}>
         <Router history={hashHistory}>
             <Route path="/" component={App}>
-                <IndexRoute component={Features} />
+                <IndexRedirect to="/features" />
+                <Route path="/features" component={Features} />
+                <Route path="/features/create" component={CreateFeatureToggle} />
                 <Route path="/strategies" component={Strategies} />
                 <Route path="/history" component={HistoryPage} />
                 <Route path="/archive" component={Archive} />

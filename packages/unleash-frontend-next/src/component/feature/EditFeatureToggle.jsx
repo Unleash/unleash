186fbc551d92bedb669ed30d25c3d224edef783c
ivaosthu <ivar.conradi.osthus@finn.no>
2016-10-22 16:35:49 +0200

    UI for editing feature toggles

--- packages/unleash-frontend-next/src/component/feature/EditFeatureToggle.jsx
+++ packages/unleash-frontend-next/src/component/feature/EditFeatureToggle.jsx
@@ -1,20 +1,21 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { updateFeatureToggle } from '../../store/feature-actions';
+import AddFeatureToggleUI from './AddFeatureToggleUI';
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state, ownProps) => ({
     strategies: state.strategies.toJS(),
+    featureToggle: state.features.toJS().find(toggle => toggle.name === ownProps.featureToggleName) || {},
 });
 
-class EditFeatureToggle extends React.Component {
-    constructor () {
-        super();
+class AddFeatureToggle extends React.Component {
+    constructor (props) {
+        super(props);
         this.state = {
-            featureToggle: {
-                name: '',
-                description: '',
-                enabled: false,
-                strategies: [],
-            },
+            name: props.featureToggle.name || '',
+            description: props.featureToggle.description || '',
+            enabled: props.featureToggle.enabled || false,
+            strategies: props.featureToggle.strategies || [],
         };
     }
 
@@ -22,6 +23,8 @@ class EditFeatureToggle extends React.Component {
         return {
             dispatch: PropTypes.func.isRequired,
             strategies: PropTypes.array,
+            featureToggle: PropTypes.featureToggle.isRequired,
+            fetchFeatureToggles: PropTypes.func.isRequired,
         };
     }
 
@@ -29,13 +32,51 @@ class EditFeatureToggle extends React.Component {
         router: React.PropTypes.object,
     }
 
+    onSubmit = (evt) => {
+        evt.preventDefault();
+        this.props.dispatch(updateFeatureToggle(this.state));
+        this.context.router.push('/features');
+    };
+
+    onCancel = (evt) => {
+        evt.preventDefault();
+        this.context.router.push('/features');
+    };
+
+    updateField = (key, value) => {
+        const change = {};
+        change[key] = value;
+        this.setState(change);
+    };
+
+    addStrategy = (strategy) => {
+        const strategies = this.state.strategies;
+        strategies.push(strategy);
+        this.setState({ strategies });
+    }
+
+    removeStrategy = (strategy) => {
+        const strategies = this.state.strategies.filter(s => s !== strategy);
+        this.setState({ strategies });
+    }
+
     render () {
         return (
             <div>
-                <p>edit input</p>
+                <AddFeatureToggleUI
+                    editmode="true"
+                    strategies={this.props.strategies}
+                    featureToggle={this.state}
+                    updateField={this.updateField}
+                    addStrategy={this.addStrategy}
+                    removeStrategy={this.removeStrategy}
+                    onSubmit={this.onSubmit}
+                    onCancel={this.onCancel}
+                    
+                />
             </div>
         );
     }
 }
 
-export default connect(mapStateToProps)(EditFeatureToggle);
+export default connect(mapStateToProps)(AddFeatureToggle);

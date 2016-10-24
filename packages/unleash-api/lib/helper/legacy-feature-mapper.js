0444503e868c6a145ef131de1457866bdc93c6c4
sveisvei <sveinung.rosaker@gmail.com>
2016-10-24 18:24:32 +0200

    avoid exception if missing strategy

--- packages/unleash-api/lib/helper/legacy-feature-mapper.js
+++ packages/unleash-api/lib/helper/legacy-feature-mapper.js
@@ -6,8 +6,10 @@ function addOldFields (feature) {
         modifiedFeature.strategies = [];
         return modifiedFeature;
     }
-    modifiedFeature.strategy = feature.strategies[0].name;
-    modifiedFeature.parameters = Object.assign({}, feature.strategies[0].parameters);
+    if (feature.strategies[0]) {
+        modifiedFeature.strategy = feature.strategies[0].name;
+        modifiedFeature.parameters = Object.assign({}, feature.strategies[0].parameters);
+    }
     return modifiedFeature;
 }
 

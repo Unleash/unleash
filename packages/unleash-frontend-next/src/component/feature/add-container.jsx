fa01ebe2afba1902a90985878d77bd9309470dae
ivaosthu <ivar.conradi.osthus@finn.no>
2016-10-25 22:33:23 +0200

    minor fix

--- packages/unleash-frontend-next/src/component/feature/add-container.jsx
+++ packages/unleash-frontend-next/src/component/feature/add-container.jsx
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { hashHistory } from 'react-router';
+import { browserHistory } from 'react-router';
 
 
 import { createFeatureToggles } from '../../store/feature-actions';
@@ -21,7 +21,7 @@ const prepare = (methods, dispatch) => {
 
     methods.onCancel = (evt) => {
         evt.preventDefault();
-        hashHistory.push('/features');
+        browserHistory.push('/features');
     };
 
     methods.addStrategy = (v) => {

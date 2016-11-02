6b6b4008478477e75c342edd915076e3066801a6
Ivar <ivarconr@gmail.com>
2016-11-02 23:17:28 +0100

    WIP client_strategies

---
+++ packages/unleash-api/lib/db/client-strategies.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const COLUMNS = ['app_name', 'strategies'];
+const TABLE = 'client_strategies';
+
+module.exports = function (db) {
+    function update (appName, strategies) {
+        return db(TABLE)
+            .where('app_name', appName)  // eslint-disable-line
+            .update({
+                strategies: JSON.stringify(strategies),
+                updated_at: 'now()', // eslint-disable-line
+            });
+    }
+
+    function insert (appName, strategies) {
+        return db(TABLE).insert({
+            app_name: appName,  // eslint-disable-line
+            strategies: JSON.stringify(strategies),
+        });
+    }
+
+    function insertOrUpdate (appName, strategies) {
+        return db(TABLE)
+            .count('*')
+            .where('app_name', appName)
+            .map(row => ({ count: row.count }))
+            .then(rows => {
+                return rows[0].count > 0 ? update(appName, strategies) : insert(appName, strategies);
+            });
+    }
+
+    function getAll () {
+        return db
+            .select(COLUMNS)
+            .from(TABLE)
+            .map(mapRow);
+    }
+
+    function mapRow (row) {
+        return {
+            appName: row.app_name,
+            strategies: row.strategies,
+        };
+    }
+
+    return { insertOrUpdate, getAll };
+};
---
+++ packages/unleash-api/migrations/20161102212415-create-client-strategies.js
@@ -0,0 +1,3 @@
+'use strict';
+
+module.exports = require('../scripts/migration-runner').create('009-create-client-strategies');

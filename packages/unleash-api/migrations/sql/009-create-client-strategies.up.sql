3ada178ce49973f57ca4dbcefbb19b0a2fe472a9
Ivar Conradi Østhus <ivarconr@gmail.com>
2016-11-10 12:45:39 +0100

    Update 009-create-client-strategies.up.sql

--- packages/unleash-api/migrations/sql/009-create-client-strategies.up.sql
+++ packages/unleash-api/migrations/sql/009-create-client-strategies.up.sql
@@ -1,6 +1,6 @@
 --create new client_strategies table
 CREATE TABLE client_strategies (
   app_name varchar(255) PRIMARY KEY NOT NULL,
-  r timestamp default now(),
+  updated_at timestamp default now(),
   strategies json
 );

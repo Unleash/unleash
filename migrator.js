ce5258e6a68e4e56a69681d4d3082ea53d8f6065
ivaosthu <ivar.conradi.osthus@finn.no>
2016-11-12 11:21:40 +0100

    options to specify different db-schema

--- migrator.js
+++ migrator.js
@@ -2,12 +2,14 @@
 
 const DBMigrate = require('db-migrate');
 const path = require('path');
+const parseDbUrl = require('parse-database-url');
 
-function migrateDb (dbUri) {
-    console.log(dbUri);
+function migrateDb (dbUrl, schema = "public") {
+    const custom = parseDbUrl(dbUrl);
+    custom.schema = schema;
     const dbmigrate = DBMigrate.getInstance(true, {
         cwd: __dirname,
-        config: { "custom": dbUri},
+        config: { custom },
         env: 'custom' }
     );
     return dbmigrate.up();
--- test/e2e/util/database-config.js
+++ test/e2e/util/database-config.js
@@ -4,7 +4,6 @@ function getDatabaseUri () {
     if (process.env.TEST_DATABASE_URL) {
         return process.env.TEST_DATABASE_URL;
     } else {
-        console.log('Using default unleash_test database');
         return 'postgres://unleash_user:passord@localhost:5432/unleash_test';
     }
 }

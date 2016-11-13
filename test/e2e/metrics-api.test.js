ce056df8b699da94e877764b8bae30fcd8580b82
sveisvei <sveinung.rosaker@gmail.com>
2016-11-13 15:41:35 +0100

    use ava as testrunner

--- test/e2e/metrics-api.test.js
+++ test/e2e/metrics-api.test.js
@@ -1,64 +1,65 @@
 'use strict';
+const test = require('ava');
+const { setupApp } = require('./util/test-helper');
+const logger = require('../../lib/logger');
 
-const specHelper = require('./util/test-helper');
-const assert     = require('assert');
-let request;
-
-describe('The metrics api', () => {
-    beforeEach(done => {
-        specHelper.setupApp().then((app) => {
-            request = app.request;
-            done();
-        });
-    });
-
-    it('should register client', (done) => {
-        request
-            .post('/api/client/register')
-            .send({ 
-                appName: 'demo',
-                instanceId: 'test',
-                strategies: ['default'],
-                started: Date.now(),
-                interval: 10 
-            })
-            .expect(202, done);
-    });
-
-    it('should accept client metrics', (done) => {
-        request
-            .post('/api/client/metrics')
-            .send({ 
-                appName: 'demo',
-                instanceId: '1',
-                bucket: {
-                    start: Date.now(),
-                    stop: Date.now(),
-                    toggles: {}
-                }
-            })
-            .expect(202, done)
-    });
-
-    it('should get client strategies', done => {
-        request
-            .get('/api/client/strategies')
-            .expect('Content-Type', /json/)
-            .end((err, res) => {
-                assert(res.status, 200);
-                assert(res.body.length === 1, `expected 1 registerd client, got ${res.body}`);
-                done();
-            });;
-    });
-
-    it('should get client instances', done => {
-        request
-            .get('/api/client/instances')
-            .expect('Content-Type', /json/)
-            .end((err, res) => {
-                assert(res.status, 200);
-                assert(res.body.length === 1, `expected 1 registerd client, got ${res.body}`);
-                done();
-            });;
-    });
+test.beforeEach(() =>  {
+    logger.setLevel('FATAL');
 });
+
+test.serial('should register client', async (t) => {
+    const { request, destroy  } = await setupApp('metrics_serial');
+    return request
+        .post('/api/client/register')
+        .send({
+            appName: 'demo',
+            instanceId: 'test',
+            strategies: ['default'],
+            started: Date.now(),
+            interval: 10
+        })
+        .expect(202)
+        .then(destroy);
+});
+
+test.serial('should accept client metrics', async t => {
+    const { request, destroy  } = await setupApp('metrics_serial');
+    return request
+        .post('/api/client/metrics')
+        .send({
+            appName: 'demo',
+            instanceId: '1',
+            bucket: {
+                start: Date.now(),
+                stop: Date.now(),
+                toggles: {}
+            }
+        })
+        .expect(202)
+        .then(destroy);
+});
+
+test.serial('should get client strategies', async t => {
+    const { request, destroy  } = await setupApp('metrics_serial');
+    return request
+        .get('/api/client/strategies')
+        .expect('Content-Type', /json/)
+        .expect((res) => {
+            t.true(res.status === 200);
+            t.true(res.body.length === 1);
+        })
+        .then(destroy);
+});
+
+test.serial('should get client instances', async t => {
+    const { request, destroy  } = await setupApp('metrics_serial');
+    return request
+        .get('/api/client/instances')
+        .expect('Content-Type', /json/)
+        .expect((res) => {
+            t.true(res.status === 200);
+            t.true(res.body.length === 1);
+        })
+        .then(destroy);
+});
+
--- test/unit/event-differ.test.js
+++ test/unit/event-differ.test.js
@@ -1,99 +1,103 @@
 'use strict';
 
+const test = require('ava');
 const eventDiffer = require('../../lib/event-differ');
-const eventType   = require('../../lib/event-type');
-const assert      = require('assert');
+const eventType = require('../../lib/event-type');
+const logger = require('../../lib/logger');
 
-describe('eventDiffer', () => {
-    it('fails if events include an unknown event type', () => {
-        const events = [
-            { type: eventType.featureCreated, data: {} },
-            { type: 'unknown-type', data: {} },
-        ];
+test.beforeEach(() =>  {
+    logger.setLevel('FATAL');
+});
 
-        assert.throws(() => {
-            eventDiffer.addDiffs(events);
-        });
-    });
+test('fails if events include an unknown event type', t => {
+    const events = [
+        { type: eventType.featureCreated, data: {} },
+        { type: 'unknown-type', data: {} },
+    ];
 
-    it('diffs a feature-update event', () => {
-        const feature = 'foo';
-        const desc = 'bar';
-
-        const events = [
-            {
-                type: eventType.featureUpdated,
-                data: { name: feature, description: desc, strategy: 'default', enabled: true, parameters: { value: 2 } },
-            },
-            {
-                type: eventType.featureCreated,
-                data: { name: feature, description: desc, strategy: 'default', enabled: false, parameters: { value: 1 } },
-            },
-        ];
-
+    t.throws(() => {
         eventDiffer.addDiffs(events);
-
-        assert.deepEqual(events[0].diffs, [
-            { kind: 'E', path: ['enabled'], lhs: false, rhs: true },
-            { kind: 'E', path: ['parameters', 'value'], lhs: 1, rhs: 2 },
-        ]);
-
-        assert.strictEqual(events[1].diffs, null);
-    });
-
-    it('diffs only against features with the same name', () => {
-        const events = [
-            {
-                type: eventType.featureUpdated,
-                data: { name: 'bar', description: 'desc', strategy: 'default', enabled: true, parameters: {} },
-            },
-            {
-                type: eventType.featureUpdated,
-                data: { name: 'foo', description: 'desc', strategy: 'default', enabled: false, parameters: {} },
-            },
-            {
-                type: eventType.featureCreated,
-                data: { name: 'bar', description: 'desc', strategy: 'default', enabled: false, parameters: {} },
-            },
-            {
-                type: eventType.featureCreated,
-                data: { name: 'foo', description: 'desc', strategy: 'default', enabled: true, parameters: {} },
-            },
-        ];
-
-        eventDiffer.addDiffs(events);
-
-        assert.strictEqual(events[0].diffs[0].rhs, true);
-        assert.strictEqual(events[1].diffs[0].rhs, false);
-        assert.strictEqual(events[2].diffs, null);
-        assert.strictEqual(events[3].diffs, null);
-    });
-
-    it('sets an empty array of diffs if nothing was changed', () => {
-        const events = [
-            {
-                type: eventType.featureUpdated,
-                data: { name: 'foo', description: 'desc', strategy: 'default', enabled: true, parameters: {} },
-            },
-            {
-                type: eventType.featureCreated,
-                data: { name: 'foo', description: 'desc', strategy: 'default', enabled: true, parameters: {} },
-            },
-        ];
-
-        eventDiffer.addDiffs(events);
-        assert.deepEqual(events[0].diffs, []);
-    });
-
-    it('sets diffs to null if there was nothing to diff against', () => {
-        const events = [
-            {
-                type: eventType.featureUpdated,
-                data: { name: 'foo', description: 'desc', strategy: 'default', enabled: true, parameters: {} },
-            },
-        ];
-
-        eventDiffer.addDiffs(events);
-        assert.strictEqual(events[0].diffs, null);
     });
 });
+
+test('diffs a feature-update event', t => {
+    const feature = 'foo';
+    const desc = 'bar';
+
+    const events = [
+        {
+            type: eventType.featureUpdated,
+            data: { name: feature, description: desc, strategy: 'default', enabled: true, parameters: { value: 2 } },
+        },
+        {
+            type: eventType.featureCreated,
+            data: { name: feature, description: desc, strategy: 'default', enabled: false, parameters: { value: 1 } },
+        },
+    ];
+
+    eventDiffer.addDiffs(events);
+
+    t.deepEqual(events[0].diffs, [
+        { kind: 'E', path: ['enabled'], lhs: false, rhs: true },
+        { kind: 'E', path: ['parameters', 'value'], lhs: 1, rhs: 2 },
+    ]);
+
+    t.true(events[1].diffs === null);
+});
+
+test('diffs only against features with the same name', t => {
+    const events = [
+        {
+            type: eventType.featureUpdated,
+            data: { name: 'bar', description: 'desc', strategy: 'default', enabled: true, parameters: {} },
+        },
+        {
+            type: eventType.featureUpdated,
+            data: { name: 'foo', description: 'desc', strategy: 'default', enabled: false, parameters: {} },
+        },
+        {
+            type: eventType.featureCreated,
+            data: { name: 'bar', description: 'desc', strategy: 'default', enabled: false, parameters: {} },
+        },
+        {
+            type: eventType.featureCreated,
+            data: { name: 'foo', description: 'desc', strategy: 'default', enabled: true, parameters: {} },
+        },
+    ];
+
+    eventDiffer.addDiffs(events);
+
+    t.true(events[0].diffs[0].rhs === true);
+    t.true(events[1].diffs[0].rhs === false);
+    t.true(events[2].diffs === null);
+    t.true(events[3].diffs === null);
+});
+
+test('sets an empty array of diffs if nothing was changed', t => {
+    const events = [
+        {
+            type: eventType.featureUpdated,
+            data: { name: 'foo', description: 'desc', strategy: 'default', enabled: true, parameters: {} },
+        },
+        {
+            type: eventType.featureCreated,
+            data: { name: 'foo', description: 'desc', strategy: 'default', enabled: true, parameters: {} },
+        },
+    ];
+
+    eventDiffer.addDiffs(events);
+    t.deepEqual(events[0].diffs, []);
+});
+
+test('sets diffs to null if there was nothing to diff against', t => {
+    const events = [
+        {
+            type: eventType.featureUpdated,
+            data: { name: 'foo', description: 'desc', strategy: 'default', enabled: true, parameters: {} },
+        },
+    ];
+
+    eventDiffer.addDiffs(events);
+    t.true(events[0].diffs === null);
+});
+

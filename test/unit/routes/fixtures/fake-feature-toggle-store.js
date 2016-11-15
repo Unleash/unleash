2d8947c30f51bb3440ec8176379bd0b67b324736
Ivar <ivarconr@gmail.com>
2016-11-15 21:27:03 +0100

    Added strategies validation when creating/updating toggles

--- test/unit/routes/fixtures/fake-feature-toggle-store.js
+++ test/unit/routes/fixtures/fake-feature-toggle-store.js
@@ -4,6 +4,14 @@
 module.exports =  () => {
     const _features = [];
     return {
+        getFeature: (name) => {
+            const toggle = _features.find(f => f.name === name);
+            if (toggle) {
+                return Promise.resolve(toggle);
+            } else {
+                return Promise.reject();
+            }
+        },
         getFeatures: () => Promise.resolve(_features),
         addFeature: (feature) => _features.push(feature),
     };

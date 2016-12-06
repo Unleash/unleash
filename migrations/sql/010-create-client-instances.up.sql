c80a46e2c3de134e696f22f31736837048d2cd3c
ivaosthu <ivar.conradi.osthus@finn.no>
2016-12-06 11:44:58 +0100

    Cleanup client_instances
    related to #176

--- migrations/sql/010-create-client-instances.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE client_instances (
-  app_name varchar(255),
-  instance_id varchar(255),
-  client_ip varchar(255),
-  last_seen timestamp default now(),
-  created_at timestamp default now()
-);

b67f9a64874f8be556a7c0d7aabd4616a1ef486b
ivaosthu <ivar.conradi.osthus@finn.no>
2016-12-06 11:44:58 +0100

    Cleanup client_instances
    related to #176

--- migrations/20161104074441-create-client-instances.js
+++ migrations/20161104074441-create-client-instances.js
@@ -1,3 +1,17 @@
 'use strict';
 
-module.exports = require('../scripts/migration-runner').create('010-create-client-instances');
+exports.up = function (db, callback) {
+    db.runSql(`
+        CREATE TABLE client_instances (
+            app_name varchar(255),
+            instance_id varchar(255),
+            client_ip varchar(255),
+            last_seen timestamp default now(),
+            created_at timestamp default now()
+        );`, callback);
+};
+
+
+exports.down = function (db, callback) {
+    db.runSql('DROP TABLE client_instances;', callback);
+};

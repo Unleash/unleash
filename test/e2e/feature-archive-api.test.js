066fe5fdd2367d0abb22a2448b279c4dd13200a9
sveisvei <sveinung.rosaker@gmail.com>
2016-12-09 14:54:18 +0100

    recieve togglename to archive or revive via path

--- test/e2e/feature-archive-api.test.js
+++ test/e2e/feature-archive-api.test.js
@@ -23,8 +23,7 @@ test.serial('returns three archived toggles', async t => {
 test.serial('revives a feature by name', async t => {
     const { request, destroy  } = await setupApp('archive_serial');
     return request
-        .post('/api/archive/revive')
-        .send({ name: 'featureArchivedX' })
+        .post('/api/archive/revive/featureArchivedX')
         .set('Content-Type', 'application/json')
         .expect(200)
         .then(destroy);
@@ -33,8 +32,7 @@ test.serial('revives a feature by name', async t => {
 test.serial('must set name when reviving toggle', async t => {
     const { request, destroy  } = await setupApp('archive_serial');
     return request
-        .post('/api/archive/revive')
-        .send({ name: '' })
-        .expect(400)
+        .post('/api/archive/revive/')
+        .expect(404)
         .then(destroy);
 });

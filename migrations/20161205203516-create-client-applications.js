ac846b9879198b75152aa789e0c87b8a82b3a7d3
ivaosthu <ivar.conradi.osthus@finn.no>
2016-12-09 16:02:06 +0100

    Add client_applications table

--- migrations/20161205203516-create-client-applications.js
+++ migrations/20161205203516-create-client-applications.js
@@ -1,3 +1,20 @@
+/* eslint camelcase: "off" */
 'use strict';
 
-module.exports = require('../scripts/migration-runner').create('011-create-client-applications');
+exports.up = function (db, cb) {
+    db.createTable('client_applications', {
+        app_name: { type: 'varchar', length: 255, primaryKey: true, notNull: true },
+        created_at: { type: 'timestamp', defaultValue: 'now()' },
+        updated_at: { type: 'timestamp', defaultValue: 'now()' },
+        seen_at: { type: 'timestamp' },
+        strategies: { type: 'json' },
+        description: { type: 'varchar', length: 255 },
+        icon: { type: 'varchar', length: 255 },
+        url: { type: 'varchar', length: 255 },
+        color: { type: 'varchar', length: 255 },
+    }, cb);
+};
+
+exports.down = function (db, cb) {
+    return db.dropTable('client_applications', cb);
+};
--- migrations/sql/011-create-client-applications.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE client_applications (
-  app_name varchar(255) PRIMARY KEY NOT NULL,
-  created_at timestamp default now(),
-  updated_at timestamp default now(),
-  description varchar(255),
-  icon varchar(255),
-  url varchar(255),
-  color varchar(255)
-);

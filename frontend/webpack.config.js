2a931cb812751b3a54018dba2f65c3044c0ef393
Simen Bekkhus <sbekkhus91@gmail.com>
2017-01-06 11:52:44 +0100

    Add redux dev tools in development

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,20 +6,20 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const entry = ['whatwg-fetch', './src/index'];
-const plugins = [new ExtractTextPlugin('bundle.css',  { allChunks: true })];
+const plugins = [
+    new ExtractTextPlugin('bundle.css', { allChunks: true }),
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+        },
+    }),
+];
 
 if (process.env.NODE_ENV === 'development') {
     entry.push('webpack-dev-server/client?http://localhost:3000');
     entry.push('webpack/hot/only-dev-server');
     plugins.push(new webpack.HotModuleReplacementPlugin());
-} else {
-    plugins.push(new webpack.DefinePlugin({
-        'process.env': {
-            NODE_ENV: JSON.stringify('production'),
-        },
-    }));
 }
-
 
 module.exports = {
     entry,
@@ -38,15 +38,15 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.(jsx|js)$/,
+                test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loaders: ['babel'],
+                loader: 'babel',
                 include: path.join(__dirname, 'src'),
             },
             {
                 test: /(\.scss)$/,
                 loader: ExtractTextPlugin.extract('style',
-                'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass'),
+                    'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass'),
             },
             {
                 test: /\.css$/,

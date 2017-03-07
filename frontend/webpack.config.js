367df8ba9660a929d458254699a47cf72f4e9e2c
ivaosthu <ivar.conradi.osthus@finn.no>
2017-03-07 18:30:45 +0100

    Fix static assets inside public folder on release

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
     },
 
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'dist/public'),
         filename: 'bundle.js',
         publicPath: '/static/',
     },

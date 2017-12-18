98f5e43f68b2affd91626ac1441e47a44163a2f5
ivaosthu <ivar.conradi.osthus@finn.no>
2017-12-18 14:21:11 +0100

    Add e2e test for client metrics

--- test/e2e/api/client/metrics.e2e.test.js
+++ test/e2e/api/client/metrics.e2e.test.js
@@ -2,4 +2,27 @@
 
 const { test } = require('ava');
 
-test.todo('e2e client metrics');
+const { setupApp } = require('./../../helpers/test-helper');
+const metricsExample = require('../../../examples/client-metrics.json');
+
+test.serial('should be possble to send metrics', async t => {
+    t.plan(0);
+    const { request, destroy } = await setupApp('metrics_api_client');
+    return request
+        .post('/api/client/metrics')
+        .send(metricsExample)
+        .expect(202)
+        .then(destroy);
+});
+
+test.serial('should require valid send metrics', async t => {
+    t.plan(0);
+    const { request, destroy } = await setupApp('metrics_api_client');
+    return request
+        .post('/api/client/metrics')
+        .send({
+            appName: 'test',
+        })
+        .expect(400)
+        .then(destroy);
+});

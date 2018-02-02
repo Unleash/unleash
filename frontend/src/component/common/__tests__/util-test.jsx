6cf9c689db8d85010c78e7083c949a02bfed8741
Ivar Conradi Østhus <ivarconr@gmail.com>
2018-02-02 12:52:31 +0100

    chore(travis) build with node 8

--- frontend/src/component/common/__tests__/util-test.jsx
+++ frontend/src/component/common/__tests__/util-test.jsx
@@ -1,7 +1,7 @@
 import { formatFullDateTime } from '../util';
 
 test('formats dates correctly', () => {
-    expect(formatFullDateTime(1487861809466)).toEqual('02/23/2017, 2:56:49 PM');
-    expect(formatFullDateTime(1487232809466)).toEqual('02/16/2017, 8:13:29 AM');
-    expect(formatFullDateTime(1477232809466)).toEqual('10/23/2016, 2:26:49 PM');
+    expect(formatFullDateTime(1487861809466)).toEqual('2017-02-23 15:56:49');
+    expect(formatFullDateTime(1487232809466)).toEqual('2017-02-16 09:13:29');
+    expect(formatFullDateTime(1477232809466)).toEqual('2016-10-23 16:26:49');
 });

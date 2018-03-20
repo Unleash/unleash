a7a7201590140a2504e2446bf182f443df98ff17
Corinne Krych <corinnekrych@gmail.com>
2018-03-20 19:07:17 +0100

    clean(feature-list): remove ternary if in jsx

--- frontend/src/component/feature/feature-list-item-component.jsx
+++ frontend/src/component/feature/feature-list-item-component.jsx
@@ -16,10 +16,8 @@ const Feature = ({
     revive,
 }) => {
     const { name, description, enabled, strategies } = feature;
-
     const { showLastHour = false } = settings;
     const isStale = showLastHour ? metricsLastHour.isFallback : metricsLastMinute.isFallback;
-
     const percent =
         1 *
         (showLastHour
@@ -28,7 +26,6 @@ const Feature = ({
 
     const strategiesToShow = Math.min(strategies.length, 3);
     const remainingStrategies = strategies.length - strategiesToShow;
-
     const strategyChips =
         strategies &&
         strategies.slice(0, strategiesToShow).map((s, i) => (
@@ -37,55 +34,26 @@ const Feature = ({
             </Chip>
         ));
     const summaryChip = remainingStrategies > 0 && <Chip className={styles.strategyChip}>+{remainingStrategies}</Chip>;
-
+    const featureUrl = toggleFeature === undefined ? `/archive/strategies/${name}` : `/features/strategies/${name}`;
     return (
         <ListItem twoLine>
             <span className={styles.listItemMetric}>
                 <Progress strokeWidth={15} percentage={percent} isFallback={isStale} />
             </span>
-            {toggleFeature ? ( // display feature list, toggleFeature is available
-                <span className={styles.listItemToggle}>
-                    <Switch
-                        title={`Toggle ${name}`}
-                        key="left-actions"
-                        onChange={() => toggleFeature(name)}
-                        checked={enabled}
-                    />
-                </span>
-            ) : (
-                // display archive, toggleFeature is undefined
-                <span className={styles.listItemToggle}>
-                    <Switch
-                        disabled
-                        title={`Toggle ${name}`}
-                        key="left-actions"
-                        onChange={() => toggleFeature(name)}
-                        checked={enabled}
-                    />
-                </span>
-            )}
+            <span className={styles.listItemToggle}>
+                <Switch
+                    disabled={toggleFeature !== undefined}
+                    title={`Toggle ${name}`}
+                    key="left-actions"
+                    onChange={() => toggleFeature(name)}
+                    checked={enabled}
+                />
+            </span>
             <span className={['mdl-list__item-primary-content', styles.listItemLink].join(' ')}>
-                {toggleFeature ? ( // display feature list, toggleFeature is available
-                    <Link
-                        to={`/features/strategies/${name}`}
-                        className={[commonStyles.listLink, commonStyles.truncate].join(' ')}
-                    >
-                        {name}
-                        <span className={['mdl-list__item-sub-title', commonStyles.truncate].join(' ')}>
-                            {description}
-                        </span>
-                    </Link>
-                ) : (
-                    <Link
-                        to={`/archive/strategies/${name}`}
-                        className={[commonStyles.listLink, commonStyles.truncate].join(' ')}
-                    >
-                        {name}
-                        <span className={['mdl-list__item-sub-title', commonStyles.truncate].join(' ')}>
-                            {description}
-                        </span>
-                    </Link>
-                )}
+                <Link to={featureUrl} className={[commonStyles.listLink, commonStyles.truncate].join(' ')}>
+                    {name}
+                    <span className={['mdl-list__item-sub-title', commonStyles.truncate].join(' ')}>{description}</span>
+                </Link>
             </span>
             <span className={[styles.listItemStrategies, commonStyles.hideLt920].join(' ')}>
                 {strategyChips}

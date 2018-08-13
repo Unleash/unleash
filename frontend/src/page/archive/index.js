28600218226a9efc2d8099b16949c8d585e08aa6
ivaosthu <ivar.conradi.osthus@finn.no>
2018-08-13 09:21:23 +0200

    fix(router): Add 'history' prop to the archive-list.

--- frontend/src/page/archive/index.js
+++ frontend/src/page/archive/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Archive from '../../component/archive/archive-list-container';
 import PropTypes from 'prop-types';
 
-const render = ({ match }) => <Archive name={match.params.name} />;
+const render = ({ match: { params }, history }) => <Archive name={params.name} history={history} />;
 render.propTypes = {
     match: PropTypes.object,
+    history: PropTypes.object,
 };
 
 export default render;

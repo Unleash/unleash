7bd18b514b02750c18c0cafea8444729e3be25b4
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2019-05-19 08:38:07 +0200

    Fix keycloak sample code to use keycloak instead of google

--- examples/keycloak-auth-unleash.js
+++ examples/keycloak-auth-unleash.js
@@ -3,14 +3,14 @@
 // const unleash = require('unleash-server');
 const unleash = require('../lib/server-impl.js');
 
-const enableGoogleOauth = require('./google-auth-hook');
+const enableKeycloak = require('./keycloak-auth-hook');
 
 unleash
     .start({
         databaseUrl: 'postgres://unleash_user:passord@localhost:5432/unleash',
         secret: 'super-duper-secret',
         adminAuthentication: 'custom',
-        preRouterHook: enableGoogleOauth,
+        preRouterHook: enableKeycloak,
     })
     .then(server => {
         console.log(

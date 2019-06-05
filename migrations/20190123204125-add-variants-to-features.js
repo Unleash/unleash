10bc85bfdf85c1b0275509526edd9d6b12d46a10
Dustin Barnes <dustin.barnes@gmail.com>
2019-06-05 10:59:51 -0700

    More fixes, attempting to run as sqlite3

--- migrations/20190123204125-add-variants-to-features.js
+++ migrations/20190123204125-add-variants-to-features.js
@@ -1,15 +1,11 @@
 'use strict';
 
-exports.up = function(db, callback) {
-    db.runSql(
-        `
-        ALTER TABLE features ADD "variants" json;
-        ALTER TABLE features ALTER COLUMN "variants" SET DEFAULT '[]';
-    `,
-        callback
+exports.up = function(knex) {
+    return knex.schema.table('features', table =>
+        table.json('variants').defaultTo('[]')
     );
 };
 
-exports.down = function(db, callback) {
-    db.runSql(`ALTER TABLE features DROP COLUMN "variants";`, callback);
+exports.down = function(knex) {
+    return knex.schema.table('features', table => table.dropColumn('variants'));
 };

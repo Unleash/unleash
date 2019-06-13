0785f52f01a2429cd5769cbb15d264b7eeb78053
Ran Magen <ran@apollographql.com>
2019-06-13 09:09:47 -0700

    add databaseUrlFile option to bin/unleash.js

--- bin/unleash.js
+++ bin/unleash.js
@@ -24,10 +24,18 @@ const argv = require('yargs')
     .option('databaseUrl', {
         alias: 'd',
         describe:
-            'The full databaseUrl to connect to, including username and password',
-        demand: true,
+            'The full databaseUrl to connect to, including username and password. Either databaseUrl or databaseUrlFile is required.',
+        demand: false,
         type: 'string',
     })
+    .option('databaseUrlFile', {
+        alias: 'f',
+        describe:
+            'The full path to a file containing the full database url to connect to, including username and password. When this option is supplied, it takes precedence over databaseUrl.',
+        demand: false,
+        type: 'string',
+    })
+    .check(args => args.databaseUrl || args.databaseUrlFile)
     .option('databaseSchema', {
         alias: 's',
         describe: 'The database schema to use',
@@ -35,6 +43,12 @@ const argv = require('yargs')
         demand: false,
         type: 'string',
     }).argv;
+
+if (argv.databaseUrlFile) {
+    // Where options are processed, the DATABASE_URL_FILE env variable takes precedence over databaseUrl.
+    process.env.DATABASE_URL_FILE = argv.databaseUrlFile;
+    delete argv.databaseUrlFile;
+}
 
 serverImpl
     .start(argv)

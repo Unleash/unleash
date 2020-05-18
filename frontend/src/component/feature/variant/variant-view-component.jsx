61cd47a0e75af9ac06b252f719e56e2dc31dc3e6
Ivar Conradi Østhus <ivarconr@gmail.com>
2020-05-18 20:18:48 +0200

    fix: convert variant-view-component to function

--- frontend/src/component/feature/variant/variant-view-component.jsx
+++ frontend/src/component/feature/variant/variant-view-component.jsx
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { IconButton } from 'react-mdl';
 import styles from './variant.scss';
 import { UPDATE_FEATURE } from '../../../permissions';
 
-class VariantViewComponent extends Component {
-    render() {
-        const { variant, editVariant, removeVariant, hasPermission } = this.props;
-        return (
-            <tr>
-                <td onClick={editVariant}>{variant.name}</td>
-                <td>{variant.weight}</td>
-                {hasPermission(UPDATE_FEATURE) ? (
-                    <td className={styles.actions}>
-                        <IconButton name="expand_more" onClick={editVariant} />
-                        <IconButton name="delete" onClick={removeVariant} />
-                    </td>
-                ) : (
-                    <td className={styles.actions} />
-                )}
-            </tr>
-        );
-    }
+function VariantViewComponent({ variant, editVariant, removeVariant, hasPermission }) {
+    return (
+        <tr>
+            <td onClick={editVariant}>{variant.name}</td>
+            <td>{variant.weight}</td>
+            {hasPermission(UPDATE_FEATURE) ? (
+                <td className={styles.actions}>
+                    <IconButton name="expand_more" onClick={editVariant} />
+                    <IconButton name="delete" onClick={removeVariant} />
+                </td>
+            ) : (
+                <td className={styles.actions} />
+            )}
+        </tr>
+    );
 }
 
 VariantViewComponent.propTypes = {

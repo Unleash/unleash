d2a3e72bd8417ed611f13f1060f48bdd49e51639
Jakob Heuser <jakob@aibex-corp.com>
2020-06-16 23:03:02 -0700

    Feat: Adds server.create() (#606)

---
+++ examples/express-parent-app.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-unresolved */
+
+'use strict';
+
+const express = require('express');
+const unleash = require('../lib/server-impl.js');
+
+const app = express();
+unleash
+    .create({
+        databaseUrl: 'postgres://unleash_user:passord@localhost:5432/unleash',
+        baseUriPath: '/unleash',
+    })
+    .then(unl => {
+        app.use(unl.app); // automatically mounts to baseUriPath
+        app.listen(process.env.PORT);
+    });

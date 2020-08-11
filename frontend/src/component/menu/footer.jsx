d059706299689641d6a7efa24da8ad99acacd3ce
Ivar Conradi Østhus <ivarconr@gmail.com>
2020-08-11 11:02:29 +0200

    fix: add link to client SDKs

--- frontend/src/component/menu/footer.jsx
+++ frontend/src/component/menu/footer.jsx
@@ -18,7 +18,7 @@ export const FooterMenu = () => (
                 </a>
             </FooterLinkList>
         </FooterDropDownSection>
-        <FooterDropDownSection title="Clients">
+        <FooterDropDownSection title="Client SDKs">
             <FooterLinkList>
                 <a href="https://github.com/Unleash/unleash-client-node/">Node.js</a>
                 <a href="https://github.com/Unleash/unleash-client-java/">Java</a>
@@ -26,6 +26,7 @@ export const FooterMenu = () => (
                 <a href="https://github.com/unleash/unleash-client-ruby">Ruby</a>
                 <a href="https://github.com/Unleash/unleash-client-python">Python</a>
                 <a href="https://github.com/Unleash/unleash-client-core">.Net Core</a>
+                <a href="https://unleash.github.io/docs/client_sdk">All client SDKs</a>
             </FooterLinkList>
         </FooterDropDownSection>
     </FooterSection>

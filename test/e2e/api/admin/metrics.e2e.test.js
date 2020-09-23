b2f0e7a7051515711081ec160199a16c8128fe1f
Ivar Conradi Østhus <ivarconr@gmail.com>
2020-09-23 23:19:54 +0200

    feat: Remove applications
    closes #634

--- test/e2e/api/admin/metrics.e2e.test.js
+++ test/e2e/api/admin/metrics.e2e.test.js
@@ -22,61 +22,6 @@ test.afterEach(async () => {
     await reset();
 });
 
-test.serial('should register client', async t => {
-    t.plan(0);
-    const request = await setupApp(stores);
-    return request
-        .post('/api/client/register')
-        .send({
-            appName: 'demo',
-            instanceId: 'test',
-            strategies: ['default'],
-            started: Date.now(),
-            interval: 10,
-        })
-        .expect(202);
-});
-
-test.serial('should allow client to register multiple times', async t => {
-    t.plan(0);
-    const request = await setupApp(stores);
-    const clientRegistration = {
-        appName: 'multipleRegistration',
-        instanceId: 'test',
-        strategies: ['default', 'another'],
-        started: Date.now(),
-        interval: 10,
-    };
-
-    return request
-        .post('/api/client/register')
-        .send(clientRegistration)
-        .expect(202)
-        .then(() =>
-            request
-                .post('/api/client/register')
-                .send(clientRegistration)
-                .expect(202),
-        );
-});
-
-test.serial('should accept client metrics', async t => {
-    t.plan(0);
-    const request = await setupApp(stores);
-    return request
-        .post('/api/client/metrics')
-        .send({
-            appName: 'demo',
-            instanceId: '1',
-            bucket: {
-                start: Date.now(),
-                stop: Date.now(),
-                toggles: {},
-            },
-        })
-        .expect(202);
-});
-
 test.serial('should get application details', async t => {
     t.plan(3);
     const request = await setupApp(stores);
@@ -98,6 +43,22 @@ test.serial('should get list of applications', async t => {
         .expect('Content-Type', /json/)
         .expect(res => {
             t.true(res.status === 200);
+            t.is(res.body.applications.length, 3);
+        });
+});
+
+test.serial('should delee application', async t => {
+    t.plan(2);
+    const request = await setupApp(stores);
+    await request
+        .delete('/api/admin/metrics/applications/deletable-app')
+        .expect(res => {
+            t.true(res.status === 200);
+        });
+    return request
+        .get('/api/admin/metrics/applications')
+        .expect('Content-Type', /json/)
+        .expect(res => {
             t.is(res.body.applications.length, 2);
         });
 });
--- test/e2e/api/client/metrics.e2e.test.js
+++ test/e2e/api/client/metrics.e2e.test.js
@@ -37,3 +37,58 @@ test.serial('should require valid send metrics', async t => {
         })
         .expect(400);
 });
+
+test.serial('should register client', async t => {
+    t.plan(0);
+    const request = await setupApp(stores);
+    return request
+        .post('/api/client/register')
+        .send({
+            appName: 'demo',
+            instanceId: 'test',
+            strategies: ['default'],
+            started: Date.now(),
+            interval: 10,
+        })
+        .expect(202);
+});
+
+test.serial('should allow client to register multiple times', async t => {
+    t.plan(0);
+    const request = await setupApp(stores);
+    const clientRegistration = {
+        appName: 'multipleRegistration',
+        instanceId: 'test',
+        strategies: ['default', 'another'],
+        started: Date.now(),
+        interval: 10,
+    };
+
+    return request
+        .post('/api/client/register')
+        .send(clientRegistration)
+        .expect(202)
+        .then(() =>
+            request
+                .post('/api/client/register')
+                .send(clientRegistration)
+                .expect(202),
+        );
+});
+
+test.serial('should accept client metrics', async t => {
+    t.plan(0);
+    const request = await setupApp(stores);
+    return request
+        .post('/api/client/metrics')
+        .send({
+            appName: 'demo',
+            instanceId: '1',
+            bucket: {
+                start: Date.now(),
+                stop: Date.now(),
+                toggles: {},
+            },
+        })
+        .expect(202);
+});

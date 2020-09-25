4a3c136167c8b61e68ddd427e6ef103db9559628
Ivar Conradi Østhus <ivarconr@gmail.com>
2020-09-25 09:39:12 +0200

    feat: Remove applications (#635)

--- test/e2e/api/admin/metrics.e2e.test.js
+++ test/e2e/api/admin/metrics.e2e.test.js
@@ -22,61 +22,6 @@ test.afterEach(async () => {
     await reset();
 });
 
-test.serial('should register client', async t => {
-    t.plan(0);
-    const request = await setupApp(stores);
-    return request
-        .post('/api/client/register')
-        .send({
-            appName: 'demo',
-            instanceId: 'test',
-            strategies: ['default'],
-            started: Date.now(),
-            interval: 10,
-        })
-        .expect(202);
-});
-
-test.serial('should allow client to register multiple times', async t => {
-    t.plan(0);
-    const request = await setupApp(stores);
-    const clientRegistration = {
-        appName: 'multipleRegistration',
-        instanceId: 'test',
-        strategies: ['default', 'another'],
-        started: Date.now(),
-        interval: 10,
-    };
-
-    return request
-        .post('/api/client/register')
-        .send(clientRegistration)
-        .expect(202)
-        .then(() =>
-            request
-                .post('/api/client/register')
-                .send(clientRegistration)
-                .expect(202),
-        );
-});
-
-test.serial('should accept client metrics', async t => {
-    t.plan(0);
-    const request = await setupApp(stores);
-    return request
-        .post('/api/client/metrics')
-        .send({
-            appName: 'demo',
-            instanceId: '1',
-            bucket: {
-                start: Date.now(),
-                stop: Date.now(),
-                toggles: {},
-            },
-        })
-        .expect(202);
-});
-
 test.serial('should get application details', async t => {
     t.plan(3);
     const request = await setupApp(stores);
@@ -98,6 +43,32 @@ test.serial('should get list of applications', async t => {
         .expect('Content-Type', /json/)
         .expect(res => {
             t.true(res.status === 200);
+            t.is(res.body.applications.length, 3);
+        });
+});
+
+test.serial('should delete application', async t => {
+    t.plan(2);
+    const request = await setupApp(stores);
+    await request
+        .delete('/api/admin/metrics/applications/deletable-app')
+        .expect(res => {
+            t.is(res.status, 200);
+        });
+    return request
+        .get('/api/admin/metrics/applications')
+        .expect('Content-Type', /json/)
+        .expect(res => {
             t.is(res.body.applications.length, 2);
+        });
+});
+
+test.serial('should get 409 when deleting unknwn application', async t => {
+    t.plan(1);
+    const request = await setupApp(stores);
+    return request
+        .delete('/api/admin/metrics/applications/unkown')
+        .expect(res => {
+            t.is(res.status, 409);
         });
 });
--- test/fixtures/fake-client-instance-store.js
+++ test/fixtures/fake-client-instance-store.js
@@ -3,4 +3,5 @@
 module.exports = () => ({
     insert: () => Promise.resolve(),
     getApplications: () => Promise.resolve([]),
+    deleteForApplication: () => Promise.resolve(),
 });

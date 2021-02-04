02ab9169da965d862bf45f3808078205a0d4e956
Simen Bekkhus <sbekkhus91@gmail.com>
2021-02-04 16:03:32 +0100

    fix: add missing space (#239)

--- frontend/src/component/feature/view/metric-component.jsx
+++ frontend/src/component/feature/view/metric-component.jsx
@@ -108,8 +108,8 @@ export default class MetricComponent extends React.Component {
                                 />
                                 <div>
                                     <small>
-                                        <strong>Not used in an app in the last hour.</strong>
-                                        This might be due to your client implementation is not reporting usage.
+                                        <strong>Not used in an app in the last hour. </strong>
+                                        This might be due to your client implementation not reporting usage.
                                     </small>
                                 </div>
                             </div>

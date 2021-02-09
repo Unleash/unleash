0d44e23c0c3d083845bfd065236270c9f582e456
Clint Checketts <clintchecketts@churchofjesuschrist.org>
2021-02-09 10:30:02 -0700

    Move JS files under src directory in preparation for TS conversion

--- server-dev.js
+++ server-dev.js
@@ -4,11 +4,11 @@ const unleash = require('./lib/server-impl');
 
 unleash.start({
     db: {
-        user: 'unleash_user',
-        password: 'passord',
+        user: 'postgres',
+        password: 'password',
         host: 'localhost',
-        port: 5432,
-        database: 'unleash',
+        port: 54320,
+        database: 'postgres',
         ssl: false,
     },
     enableRequestLogger: true,

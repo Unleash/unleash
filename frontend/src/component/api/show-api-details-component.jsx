a9cb68705ab8b0b14d492989b284124851cc3548
Christopher Kolstad <chriswk@getunleash.ai>
2021-02-23 12:59:38 +0100

    feat: add oss/enterprise version to footer (#245)

--- frontend/src/component/api/show-api-details-component.jsx
+++ frontend/src/component/api/show-api-details-component.jsx
@@ -8,13 +8,35 @@ class ShowApiDetailsComponent extends Component {
     };
 
     render() {
-        const { slogan, environment, version, name } = this.props.uiConfig;
-
+        const { slogan, environment, version, versionInfo, name } = this.props.uiConfig;
+        let versionStr;
+        let updateNotification;
+        let instanceId;
+        if (versionInfo) {
+            if (versionInfo.current.enterprise) {
+                versionStr = `${name} ${versionInfo.current.enterprise}`;
+                if (versionInfo.latest && !versionInfo.isLatest) {
+                    updateNotification = `Upgrade available - Latest Enterprise release: ${versionInfo.latest.enterprise}`;
+                }
+            } else {
+                versionStr = `${name} ${versionInfo.current.oss}`;
+                if (versionInfo.latest && !versionInfo.isLatest) {
+                    updateNotification = `Upgrade available - Latest OSS release: ${versionInfo.latest.oss}`;
+                }
+            }
+            instanceId = versionInfo.instanceId;
+        } else {
+            versionStr = `${name} ${version}`;
+        }
         return (
-            <FooterSection type="bottom" logo={`${name} ${version}`}>
+            <FooterSection type="bottom" logo={`${versionStr}`}>
                 <small>{environment ? `(${environment})` : ''}</small>
                 <br />
+                <small>{updateNotification ? `${updateNotification}` : ''}</small>
+                <br />
                 <small>{slogan}</small>
+                <br />
+                <small>{instanceId ? `${instanceId}` : ''}</small>
             </FooterSection>
         );
     }

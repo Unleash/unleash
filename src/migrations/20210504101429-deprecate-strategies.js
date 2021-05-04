48cad24bc75bb10e0fad31b4124614550aadb0cd
Fredrik Strand Oseberg <fredrik.no@gmail.com>
2021-05-04 21:42:58 +0200

    feat: add migration (#832)

---
+++ src/migrations/20210504101429-deprecate-strategies.js
@@ -0,0 +1,23 @@
+exports.up = function(db, cb) {
+    db.runSql(
+        `
+      UPDATE strategies
+      SET deprecated = true, built_in = 0
+      WHERE name = 'gradualRolloutSessionId' OR name = 'gradualRolloutUserId'
+      OR name = 'gradualRolloutRandom'
+    `,
+        cb,
+    );
+};
+
+exports.down = function(db, cb) {
+    db.runSql(
+        `
+      UPDATE strategies
+      SET deprecated = false, built_in = 1
+      WHERE name = 'gradualRolloutSessionId' OR name = 'gradualRolloutUserId'
+      OR name = 'gradualRolloutRandom'
+    `,
+        cb,
+    );
+};

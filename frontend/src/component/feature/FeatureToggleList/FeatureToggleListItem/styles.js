6db15076e317a1ee09f1226589c4689a8db3876d
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-05-05 22:11:39 +0200

    fix: tiny margin for feature toggle list item

--- frontend/src/component/feature/FeatureToggleList/FeatureToggleListItem/styles.js
+++ frontend/src/component/feature/FeatureToggleList/FeatureToggleListItem/styles.js
@@ -3,11 +3,11 @@ import { makeStyles } from '@material-ui/styles';
 export const useStyles = makeStyles(theme => ({
     listItem: {
         padding: '0',
-        margin: '0.25rem 0',
+        margin: '1rem 0',
     },
     listItemMetric: {
         width: '40px',
-        marginRight: '1rem',
+        marginRight: '0.25rem',
         flexShrink: '0',
     },
     listItemSvg: {

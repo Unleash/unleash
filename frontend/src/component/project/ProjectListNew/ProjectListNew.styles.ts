f1a714e7e1a0f90551b7ff3f681a93f2638e2bc0
Fredrik Oseberg <fredrik.no@gmail.com>
2021-07-07 17:05:56 +0200

    fix: add flex wrap

--- frontend/src/component/project/ProjectListNew/ProjectListNew.styles.ts
+++ frontend/src/component/project/ProjectListNew/ProjectListNew.styles.ts
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles(theme => ({
     container: {
         display: 'flex',
+        flexWrap: 'wrap',
     },
     apiError: {
         maxWidth: '400px',

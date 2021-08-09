de69622064ae9865e02f16de9e06713d43a69038
Fredrik Oseberg <fredrik.no@gmail.com>
2021-08-09 14:25:24 +0200

    Add project-health.ts

---
+++ src/lib/routes/admin-api/project/project-health.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from 'express';
+import Controller from '../../controller';
+import { IUnleashServices } from '../../../types/services';
+import { IUnleashConfig } from '../../../types/option';
+import ProjectHealthService from '../../../services/project-health-service';
+import { Logger } from '../../../logger';
+import { IProjectParam } from '../../../types/model';
+import { handleErrors } from '../util';
+
+export default class ProjectHealthReport extends Controller {
+    private projectHealthService: ProjectHealthService;
+
+    private logger: Logger;
+
+    constructor(
+        config: IUnleashConfig,
+        {
+            projectHealthService,
+        }: Pick<IUnleashServices, 'projectHealthService'>,
+    ) {
+        super(config);
+        this.logger = config.getLogger('/admin-api/project/health-report');
+        this.projectHealthService = projectHealthService;
+        this.get('/:projectId/health-report', this.getProjectHealthReport);
+    }
+
+    async getProjectHealthReport(
+        req: Request<IProjectParam, any, any, any>,
+        res: Response,
+    ): Promise<void> {
+        const { projectId } = req.params;
+        try {
+            const overview = await this.projectHealthService.getProjectHealthReport(
+                projectId,
+            );
+            res.json({
+                version: 2,
+                ...overview,
+            });
+        } catch (e) {
+            handleErrors(res, this.logger, e);
+        }
+    }
+}

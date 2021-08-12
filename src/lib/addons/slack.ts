5760d10e70534dc5e1ed30fccfb5423928ae7c5c
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-08-12 23:00:47 +0200

    Revert "fix: upgrade js-yaml to 4.1.0"
    This reverts commit 4852d6704953e844f31c678131b7b8abead9777a.

--- src/lib/addons/slack.ts
+++ src/lib/addons/slack.ts
@@ -123,7 +123,7 @@ This was changed by ${createdBy}.`;
         const stale = data.stale ? '("stale")' : '';
         const typeStr = `*Type*: ${data.type}`;
         const project = `*Project*: ${data.project}`;
-        const strategies = `*Activation strategies*: \`\`\`${YAML.dump(
+        const strategies = `*Activation strategies*: \`\`\`${YAML.safeDump(
             data.strategies,
             { skipInvalid: true },
         )}\`\`\``;

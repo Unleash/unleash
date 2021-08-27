9b521de34e210efc09e5750271f154b5bc769ce0
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-08-27 19:30:51 +0200

    fix: correct data format for FEATURE_CREATED event

--- src/lib/services/feature-toggle-service-v2.ts
+++ src/lib/services/feature-toggle-service-v2.ts
@@ -244,7 +244,7 @@ class FeatureToggleServiceV2 {
                 projectId,
             );
 
-            const data = { featureData, projectId };
+            const data = { ...featureData, projectId };
 
             await this.eventStore.store({
                 type: FEATURE_CREATED,

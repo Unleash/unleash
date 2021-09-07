9fe15fec8850d91e756426451e3f561d53496eef
Christopher Kolstad <chriswk@getunleash.ai>
2021-09-07 15:01:52 +0200

    fix: lint fixing

--- src/migrations/20210907124058-add-dbcritic-indices.js
+++ src/migrations/20210907124058-add-dbcritic-indices.js
@@ -1,5 +1,6 @@
-exports.up = function(db, cb) {
-    db.runSql(`
+exports.up = function (db, cb) {
+    db.runSql(
+        `
         CREATE INDEX feature_environments_feature_name_idx ON feature_environments(feature_name);
         CREATE INDEX feature_strategies_environment_idx ON feature_strategies(environment);
         CREATE INDEX feature_strategies_feature_name_idx ON feature_strategies(feature_name);
@@ -9,11 +10,14 @@ exports.up = function(db, cb) {
         CREATE INDEX role_permission_role_id_idx ON role_permission(role_id);
         CREATE INDEX role_user_user_id_idx ON role_user(user_id);
 
-    `, cb);
+    `,
+        cb,
+    );
 };
 
-exports.down = function(db, cb) {
-  db.runSql(`
+exports.down = function (db, cb) {
+    db.runSql(
+        `
     DROP INDEX feature_environments_feature_name_idx;
     DROP INDEX feature_strategies_environment_idx;
     DROP INDEX feature_strategies_feature_name_idx;
@@ -23,9 +27,11 @@ exports.down = function(db, cb) {
     DROP INDEX role_permission_role_id_idx;
     DROP INDEX role_user_user_id_idx;
 
-    `, cb);
+    `,
+        cb,
+    );
 };
 
 exports._meta = {
-  "version": 1
+    version: 1,
 };

cf74075f5f3ab96508ff0e004e21129d3b7e611c
Christopher Kolstad <chriswk@getunleash.ai>
2021-09-22 12:57:15 +0200

    fix: lints

--- src/migrations/20210922084509-add-non-null-constraint-to-environment-type.js
+++ src/migrations/20210922084509-add-non-null-constraint-to-environment-type.js
@@ -1,11 +1,14 @@
 'use strict';
 
-exports.up = function(db, cb) {
-  db.runSql(`
+exports.up = function (db, cb) {
+    db.runSql(
+        `
     UPDATE environments SET type = 'production' WHERE type IS null;
-    ALTER TABLE environments ALTER COLUMN type SET NOT NULL`, cb);
+    ALTER TABLE environments ALTER COLUMN type SET NOT NULL`,
+        cb,
+    );
 };
 
-exports.down = function(db) {
+exports.down = function (db, cb) {
     db.runSql(`ALTER TABLE environments ALTER COLUMN type DROP NOT NULL`, cb);
 };

f230d1fbdbed80a7cc130c61353b433a268d6090
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-10-13 15:15:14 +0200

    fix: setup flags for envrionments

--- src/lib/create-config.test.ts
+++ src/lib/create-config.test.ts
@@ -1,7 +1,17 @@
 import { createConfig } from './create-config';
 
 test('should create default config', async () => {
-    const config = createConfig({});
+    const config = createConfig({
+        db: {
+            host: 'localhost',
+            port: 4242,
+            user: 'unleash',
+            database: 'unleash_db',
+        },
+        server: {
+            port: 4242,
+        },
+    });
 
     expect(config).toMatchSnapshot();
 });

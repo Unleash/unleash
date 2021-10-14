9ddd8590375a94a8d71bd49353614186da7aaab8
Youssef <khedher.youssef@hotmail.fr>
2021-10-14 22:57:20 +0100

    make Overview tab the default tab when open a feature

--- frontend/src/utils/route-path-helpers.ts
+++ frontend/src/utils/route-path-helpers.ts
@@ -1,5 +1,5 @@
 export const getTogglePath = (projectId: string, featureToggleName: string, newPath: boolean) => {
-    return `/projects/${projectId}/features${newPath ? '2' : ''}/${featureToggleName}/strategies`;
+    return `/projects/${projectId}/features${newPath ? '2' : ''}/${featureToggleName}`;
 };
 
 export const getToggleCopyPath = (

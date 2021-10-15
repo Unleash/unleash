11740e718bef1df0021acdac05df325689bea3ea
Youssef <khedher.youssef@hotmail.fr>
2021-10-15 08:39:37 +0100

    fix route rendering

--- frontend/src/utils/route-path-helpers.ts
+++ frontend/src/utils/route-path-helpers.ts
@@ -1,5 +1,5 @@
 export const getTogglePath = (projectId: string, featureToggleName: string, newPath: boolean) => {
-    return `/projects/${projectId}/features${newPath ? '2' : ''}/${featureToggleName}`;
+    return `/projects/${projectId}/features${newPath ? `2/${featureToggleName}` : `/${featureToggleName}/strategies`}`;
 };
 
 export const getToggleCopyPath = (

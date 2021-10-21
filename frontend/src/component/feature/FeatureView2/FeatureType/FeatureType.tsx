0ddd52a8ba70007bc5e467db15538b156679ea7c
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-10-21 10:41:46 +0200

    fix: adjust tooltip for toggle type

--- frontend/src/component/feature/FeatureView2/FeatureType/FeatureType.tsx
+++ frontend/src/component/feature/FeatureView2/FeatureType/FeatureType.tsx
@@ -1,6 +1,7 @@
 import { useStyles } from './FeatureType.styles';
 import { Tooltip } from '@material-ui/core';
 import { getFeatureTypeIcons } from '../../../../utils/get-feature-type-icons';
+import useFeatureTypes from '../../../../hooks/api/getters/useFeatureTypes/useFeatureTypes';
 
 
 interface FeatureTypeProps {
@@ -9,10 +10,15 @@ interface FeatureTypeProps {
 
 const FeatureStatus = ({ type }: FeatureTypeProps) => {
     const styles = useStyles();
+    const { featureTypes } = useFeatureTypes()
     const IconComponent = getFeatureTypeIcons(type);
 
+    const typeName = featureTypes.filter(t => t.id === type).map(t => t.name);
+
+    const title = `This is a "${typeName || type}" toggle`;
+
     return (
-        <Tooltip arrow placement="right" title={type}>
+        <Tooltip arrow placement="right" title={title}>
             <IconComponent
                 data-loading
                 className={styles.icon}

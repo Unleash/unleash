937a84adfe5008ad34f5805327c8c7d124cb67d8
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-11-11 14:10:39 +0100

    fix: use jsonb instead of json

--- src/migrations/20211105105509-add-predata-column-to-events.js
+++ src/migrations/20211105105509-add-predata-column-to-events.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.up = function (db, cb) {
-    db.runSql(`ALTER TABLE events ADD COLUMN pre_data json;`, cb);
+    db.runSql(`ALTER TABLE events ADD COLUMN pre_data jsonb;`, cb);
 };
 
 exports.down = function (db, cb) {

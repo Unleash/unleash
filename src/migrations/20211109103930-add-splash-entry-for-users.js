fc275a6183f6fa88da1b73f0d0c8eea29c69138b
Youssef <khedher.youssef@hotmail.fr>
2021-11-12 11:02:12 +0100

    fix: update sql query in add-splash-entry

--- src/migrations/20211109103930-add-splash-entry-for-users.js
+++ src/migrations/20211109103930-add-splash-entry-for-users.js
@@ -1,12 +1,8 @@
 exports.up = function (db, cb) {
-    db.runSql(`SELECT * FROM users`, (err, results) => {
-        results.rows.forEach((user) => {
-            db.runSql(
-                `INSERT INTO user_splash(splash_id, user_id, seen) SELECT 'environment', u.id, false FROM users u`,
-            );
-        });
-        cb();
-    });
+    db.runSql(
+        `INSERT INTO user_splash(splash_id, user_id, seen) SELECT 'environment', u.id, false FROM users u`,
+        cb,
+    );
 };
 
 exports.down = function (db, cb) {

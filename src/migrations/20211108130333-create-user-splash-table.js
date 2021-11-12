16429fcf9a91f18291b3839fb838faf91856fb50
Youssef <khedher.youssef@hotmail.fr>
2021-11-12 13:01:56 +0100

    fix: add cascade query inside create table for splash

--- src/migrations/20211108130333-create-user-splash-table.js
+++ src/migrations/20211108130333-create-user-splash-table.js
@@ -4,11 +4,12 @@ exports.up = function (db, cb) {
     db.runSql(
         `
         CREATE TABLE IF NOT EXISTS user_splash 
-        (user_id INTEGER NOT NULL references users (id), 
+        (user_id INTEGER NOT NULL references users (id) ON DELETE CASCADE, 
         splash_id TEXT, 
         seen BOOLEAN NOT NULL DEFAULT false, 
         PRIMARY KEY (user_id, splash_id));
-        CREATE INDEX user_splash_user_id_idx ON user_splash (user_id);`,
+        CREATE INDEX user_splash_user_id_idx ON user_splash (user_id);
+        `,
         cb,
     );
 };

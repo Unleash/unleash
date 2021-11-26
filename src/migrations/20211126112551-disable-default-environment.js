7e7732864a69924bbd2d4d65b0c7a49afa868c27
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-11-26 13:19:13 +0100

    fix: remove default environments for fresh installations

---
+++ src/migrations/20211126112551-disable-default-environment.js
@@ -0,0 +1,26 @@
+exports.up = function (db, cb) {
+    db.runSql(
+        `
+        DELETE FROM project_environments 
+        WHERE environment_name = 'default'
+        AND NOT EXISTS (select  * from features);
+
+        DELETE FROM environments 
+        WHERE name = 'default'
+        AND NOT EXISTS (select  * from features);
+
+        INSERT INTO project_environments(project_id, environment_name) 
+        SELECT 'default', 'development'
+        WHERE NOT EXISTS (select  * from features);
+        
+        INSERT INTO project_environments(project_id, environment_name) 
+        SELECT 'default', 'production'
+        WHERE NOT EXISTS (select  * from features);
+      `,
+        cb,
+    );
+};
+
+exports.down = function (db, cb) {
+    db.runSql('', cb);
+};

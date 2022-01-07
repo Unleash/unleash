0d6c440c6934d6fa70cea01c0bf0216d63ee9aaa
Youssef <khedher.youssef@hotmail.fr>
2022-01-07 11:25:10 +0100

    feat: update feature types description

--- src/migrations/20200805091409-add-feature-toggle-type.js
+++ src/migrations/20200805091409-add-feature-toggle-type.js
@@ -21,11 +21,11 @@ exports.up = function (db, cb) {
             db.runSql.bind(
                 db,
                 `
-              INSERT INTO feature_types(id, name, description, lifetime_days) VALUES('release', 'Release', 'Used to enable trunk-based development for teams practicing Continuous Delivery.', 40);
-              INSERT INTO feature_types(id, name, description, lifetime_days) VALUES('experiment', 'Experiment', 'Used to perform multivariate or A/B testing.', 40);
-              INSERT INTO feature_types(id, name, description, lifetime_days) VALUES('operational', 'Operational', 'Used to control operational aspects of the system behavior.', 7);
-              INSERT INTO feature_types(id, name, description, lifetime_days) VALUES('kill-switch', 'Kill switch', 'Used to to gracefully degrade system functionality.', null);
-              INSERT INTO feature_types(id, name, description, lifetime_days) VALUES('permission', 'Permission', 'Used to change the features or product experience that certain users receive.', null);
+                INSERT INTO feature_types(id, name, description, lifetime_days) VALUES('release', 'Release', 'Release feature toggles are used to release new features.', 40);
+                INSERT INTO feature_types(id, name, description, lifetime_days) VALUES('experiment', 'Experiment', 'Experiment feature toggles are used to test and verify multiple different versions of a feature.', 40);
+                INSERT INTO feature_types(id, name, description, lifetime_days) VALUES('operational', 'Operational', 'Operational feature toggles are used to control aspects of a rollout.', 7);
+                INSERT INTO feature_types(id, name, description, lifetime_days) VALUES('kill-switch', 'Kill switch', 'Kill switch feature toggles are used to quickly turn on or off critical functionality in your system.', null);
+                INSERT INTO feature_types(id, name, description, lifetime_days) VALUES('permission', 'Permission', 'Permission feature toggles are used to control permissions in your system.', null);
               `,
             ),
         ],

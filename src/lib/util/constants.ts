0a35ce5a18d7723f0898e4076f6402582f210cb2
sighphyre <liquidwicked64@gmail.com>
2022-01-07 11:10:48 +0200

    feat: Schema validation for roles

--- src/lib/util/constants.ts
+++ src/lib/util/constants.ts
@@ -3,3 +3,5 @@ export const DEFAULT_ENV = 'default';
 export const ROOT_PERMISSION_TYPE = 'root';
 export const ENVIRONMENT_PERMISSION_TYPE = 'environment';
 export const PROJECT_PERMISSION_TYPE = 'project';
+
+export const CUSTOM_ROLE_TYPE = 'custom';

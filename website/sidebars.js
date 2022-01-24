738643eb242ce37e333b03ed397b843be6b999d3
Thomas Heartman <thomas@getunleash.ai>
2022-01-24 13:41:29 +0100

    docs: Add landing pages for all first and second-level categories.

--- website/sidebars.js
+++ website/sidebars.js
@@ -13,7 +13,15 @@ module.exports = {
     documentation: [
         'user_guide/index',
         {
-            'First steps': [
+            label: 'First steps and tutorials',
+            type: 'category',
+            link: {
+                type: 'generated-index',
+                title: 'First steps',
+                description: 'Learn how and where to get started with Unleash.',
+                slug: '/tutorials',
+            },
+            items: [
                 'user_guide/unleash_overview',
                 'user_guide/important-concepts',
                 'user_guide/quickstart',
@@ -26,38 +34,94 @@ module.exports = {
             link: {
                 type: 'generated-index',
                 title: 'How-to guides',
-                description: 'Step-by-step recipes for you to follow',
+                description: 'Step-by-step recipes for you to follow.',
                 slug: '/how-to',
             },
             items: [
                 {
-                    API: ['user_guide/api-token', 'advanced/api_access'],
-                    'Feature toggles, strategies, context': [
+                    type: 'category',
+                    link: {
+                        type: 'generated-index',
+                        title: 'How-to: Unleash API',
+                        description: 'Learn how to work with the Unleash API',
+                        slug: '/how-to/api',
+                    },
+                    label: 'API',
+                    items: ['user_guide/api-token', 'advanced/api_access'],
+                },
+                {
+                    label: 'Feature toggles, strategies, context',
+                    items: [
                         'how-to/how-to-add-strategy-constraints',
                         'user_guide/create_feature_toggle',
                         'how-to/how-to-define-custom-context-fields',
                         'how-to/how-to-use-custom-strategies',
                     ],
-                    'Users and permissions': [
+                    type: 'category',
+                    link: {
+                        type: 'generated-index',
+                        title: 'How-to: general Unleash tasks',
+                        description:
+                            'Guides for how to perform general Unleash tasks.',
+                        slug: '/how-to/misc',
+                    },
+                },
+                {
+                    label: 'Users and permissions',
+                    items: [
                         'user_guide/user-management',
                         'how-to/how-to-create-and-assign-custom-project-roles',
                     ],
-                    SSO: [
+                    type: 'category',
+                    link: {
+                        type: 'generated-index',
+                        title: 'How-to: users and permissions',
+                        description: 'Users and permission how-to guides.',
+                        slug: '/how-to/users-and-permissions',
+                    },
+                },
+                {
+                    label: 'Single Sign-On (SSO)',
+                    items: [
                         'advanced/sso-open-id-connect',
                         'advanced/sso-saml',
                         'advanced/sso-saml-keycloak',
                         'advanced/sso-google',
                     ],
+                    type: 'category',
+                    link: {
+                        type: 'generated-index',
+                        title: 'How-to: Single Sign-On',
+                        description: 'Single Sign-On how-to guides.',
+                        slug: '/how-to/sso',
+                    },
                 },
+                ,
             ],
         },
         {
             label: 'Reference documentation',
             type: 'category',
+            link: {
+                type: 'generated-index',
+                title: 'Reference documentation',
+                description:
+                    'Technical reference documentation relating to Unleash, including APIs, SDKs, Unleash concepts and deployment.',
+                slug: '/reference',
+            },
             collapsible: true,
             items: [
                 {
-                    'Unleash concepts': [
+                    type: 'category',
+                    link: {
+                        type: 'generated-index',
+                        title: 'Unleash concepts',
+                        description:
+                            'Documents describing the inner parts of Unleash.',
+                        slug: '/reference/concepts',
+                    },
+                    label: 'Unleash concepts',
+                    items: [
                         'user_guide/activation_strategy',
                         'advanced/archived_toggles',
                         'advanced/audit_log',
@@ -94,6 +158,17 @@ module.exports = {
                                 'api/admin/state',
                                 'api/admin/strategies',
                                 'api/admin/user-admin',
+                                {
+                                    type: 'category',
+                                    label: 'subcat',
+                                    link: {
+                                        type: 'generated-index',
+                                        title: 'Subcat',
+                                        description: 'Lorem ipsum',
+                                        slug: '/api/subcat',
+                                    },
+                                    items: ['api/admin/user-admin'],
+                                },
                             ],
                             'Client API': [
                                 'api/client/features',
@@ -109,8 +184,10 @@ module.exports = {
                     ],
                 },
                 {
-                    'Unleash SDKs': [
-                        'sdks/index',
+                    type: 'category',
+                    label: 'Unleash SDKs',
+                    link: { type: 'doc', id: 'sdks/index' },
+                    items: [
                         'sdks/unleash-proxy',
                         {
                             type: 'category',
@@ -146,7 +223,18 @@ module.exports = {
                             href: '/sdks#community-sdks',
                         },
                     ],
-                    'Deploy and manage Unleash': [
+                },
+                {
+                    type: 'category',
+                    link: {
+                        type: 'generated-index',
+                        title: 'Deployment and management of Unleash',
+                        description:
+                            'All you need to learn how to deploy and manage your own Unleash instance.',
+                        slug: '/deploy',
+                    },
+                    label: 'Deploy and manage Unleash',
+                    items: [
                         'deploy/getting_started',
                         'deploy/configuring_unleash',
                         'deploy/database-setup',
@@ -186,7 +274,16 @@ module.exports = {
             ],
         },
         {
-            'Topic guides': ['topics/a-b-testing'],
+            label: 'Topic guides',
+            type: 'category',
+            link: {
+                type: 'generated-index',
+                title: 'Topic guides',
+                description:
+                    'Discussions, explanations, and explorations regarding topics related to Unleash.',
+                slug: '/topics',
+            },
+            items: ['topics/a-b-testing'],
         },
     ],
 };

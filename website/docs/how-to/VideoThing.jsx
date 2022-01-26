2c318546b63820529047d559bc1009ede49d53f7
Thomas Heartman <thomas@getunleash.ai>
2022-01-26 13:12:22 +0100

    docs: remove unnecessary wrapping divs.

--- website/docs/how-to/VideoThing.jsx
+++ website/docs/how-to/VideoThing.jsx
@@ -3,37 +3,33 @@ import Admonition from '@theme/Admonition';
 
 const Component = ({ videoUrls, children }) => {
     return (
-        <article className="unleash-video-wrapper">
-            <div className="unleash-video-container">
-                <Admonition type="tip" title="Video content">
-                    {children}
-                    Hey! Did you know that the content in this guide is also
-                    available in video format? If that's more your speed, feel
-                    free to check out one of these related videos 🍿📽
-                </Admonition>
+        <article className="unleash-video-container">
+            <Admonition type="tip" title="Video content">
+                {children}
+                Hey! Did you know that the content in this guide is also
+                available in video format? If that's more your speed, feel free
+                to check out one of these related videos 🍿📽
+            </Admonition>
 
-                <div className="videos">
-                    {videoUrls ? (
-                        videoUrls.map((url) => (
-                            <div className="video-wrapper">
-                                <iframe
-                                    key={url}
-                                    width="100%"
-                                    height="auto"
-                                    src={url}
-                                    title="YouTube video player"
-                                    frameBorder="0"
-                                    allowFullScreen
-                                ></iframe>
-                            </div>
-                        ))
-                    ) : (
-                        <Admonition type="danger">
-                            You need to provide YouTube video URLs for this
-                            component to work properly.
-                        </Admonition>
-                    )}
-                </div>
+            <div className="videos">
+                {videoUrls ? (
+                    videoUrls.map((url) => (
+                        <iframe
+                            key={url}
+                            width="100%"
+                            height="auto"
+                            src={url}
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allowFullScreen
+                        ></iframe>
+                    ))
+                ) : (
+                    <Admonition type="danger">
+                        You need to provide YouTube video URLs for this
+                        component to work properly.
+                    </Admonition>
+                )}
             </div>
         </article>
     );

1b8a15aa126eb50957f05a10fb005167a4fa55ea
Thomas Heartman <thomas@getunleash.ai>
2022-02-17 12:30:49 +0100

    docs: Add storybook

---
+++ website/.storybook/preview.js
@@ -0,0 +1,11 @@
+import '../src/css/custom.css';
+
+export const parameters = {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+};

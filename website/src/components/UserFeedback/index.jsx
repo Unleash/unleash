39f4d73d59edc87fc8c1c10a0588dfcd9f0b5584
Thomas Heartman <thomas@getunleash.ai>
2022-02-24 17:42:53 +0100

    feat: add basic (sorta styled) first feedback screen

--- website/src/components/UserFeedback/index.jsx
+++ website/src/components/UserFeedback/index.jsx
@@ -3,8 +3,42 @@ import './styles.css';
 
 const Component = ({ text }) => (
     <article className="user-feedback">
-        <form></form>
-        <p className="scream">{text}</p>
+        <form>
+            <p>
+                <span className="visually-hidden">
+                    On a scale from 1 to 5 where 1 is very unsatisfied and 5 is
+                    very satisfied,
+                </span>{' '}
+                How would you rate your overall satisfaction with the Unleash
+                documentation?
+            </p>
+
+            <div className="satisfaction-input-container">
+                <span aria-hidden="true">Very unsatisfied</span>
+                {[1, 2, 3, 4, 5].map((n) => (
+                    <span key={`input-group-${n}`}>
+                        <input
+                            className="visually-hidden user-satisfaction-score-input"
+                            required
+                            id={`user-satisfaction-score-${n}`}
+                            name="satisfaction-level"
+                            type="radio"
+                            value={n}
+                        />
+                        <label
+                            className="user-satisfaction-score-label"
+                            htmlFor={`user-satisfaction-score-${n}`}
+                        >
+                            {n}
+                        </label>
+                    </span>
+                ))}
+                <span aria-hidden="true">Very satisfied</span>
+            </div>
+            <div className="button-container">
+                <button type="submit">Next</button>
+            </div>
+        </form>
     </article>
 );
 

2fd633c0ecd50c5df261460cff62cca979672163
Ivar Conradi Østhus <ivarconr@gmail.com>
2022-02-24 10:46:22 +0100

    fix: remove project column from roles if exists

--- src/migrations/20220224081422-remove-project-column-from-roles.js
+++ src/migrations/20220224081422-remove-project-column-from-roles.js
@@ -3,6 +3,5 @@ exports.up = function (db, cb) {
 };
 
 exports.down = function (db, cb) {
-    // We can't just remove roles for users as we don't know if there has been any manual additions.
     db.runSql('ALTER TABLE roles ADD COLUMN IF NOT EXISTS project text', cb);
 };

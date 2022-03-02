e106943e4b29a307cd590f01de00f21ba2ba9183
Thomas Heartman <thomas@getunleash.ai>
2022-03-02 13:50:08 +0100

    fix: remove node-fetch and SSR to fix the build

--- website/src/theme/Root.tsx
+++ website/src/theme/Root.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import UserFeedback from '@site/src/components/UserFeedback';
 import { UnleashClient } from 'unleash-proxy-client';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import nodeFetch from 'node-fetch'
 
 // Default implementation, that you can customize
 function Root({ children }) {
@@ -11,17 +10,11 @@ function Root({ children }) {
     } = useDocusaurusContext();
 
 
-    const unleashSsrOptions = {
-        disableMetrics: true,
-        fetch: nodeFetch
-    }
-
     const unleashConfig = {
         clientKey: customFields.unleashProxyClientKey as string,
         url: customFields.unleashProxyUrl as string,
         disableRefresh: true,
         appName: `docs.getunleash.io-${customFields.environment}`,
-        ...(typeof fetch === "undefined" ? unleashSsrOptions : {})
     };
 
     const [showFeedback, setShowFeedback] = React.useState(false);

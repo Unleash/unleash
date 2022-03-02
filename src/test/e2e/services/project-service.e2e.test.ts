535d575de6229a5ba1710f782bd5b812fa6a867d
Ivar Conradi Østhus <ivarconr@gmail.com>
2022-03-02 23:53:46 +0100

    fix: lint

--- src/test/e2e/services/project-service.e2e.test.ts
+++ src/test/e2e/services/project-service.e2e.test.ts
@@ -678,8 +678,8 @@ test('should update role for user on project', async () => {
     );
 
     const { users } = await projectService.getUsersWithAccess(project.id, user);
-    const memberUsers = users.filter((user) => user.roleId === memberRole.id);
-    const ownerUsers = users.filter((user) => user.roleId === ownerRole.id);
+    const memberUsers = users.filter((u) => u.roleId === memberRole.id);
+    const ownerUsers = users.filter((u) => u.roleId === ownerRole.id);
 
     expect(memberUsers).toHaveLength(0);
     expect(ownerUsers).toHaveLength(2);

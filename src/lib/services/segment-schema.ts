1446f0f231508060c4e563494aa5e50fcbb1cb0b
olav <mail@olav.io>
2022-03-17 12:00:33 +0100

    refactor: allow empty description

--- src/lib/services/segment-schema.ts
+++ src/lib/services/segment-schema.ts
@@ -5,7 +5,7 @@ export const unsavedSegmentSchema = joi
     .object()
     .keys({
         name: joi.string().required(),
-        description: joi.string().allow(null).optional(),
+        description: joi.string().allow(null).allow('').optional(),
         constraints: joi.array().items(constraintSchema).required(),
     })
     .options({ allowUnknown: true });

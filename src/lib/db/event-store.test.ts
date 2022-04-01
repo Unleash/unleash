9f7d8784228aae9d82eb14d2ad90982f3a051103
olav <mail@olav.io>
2022-04-01 11:10:21 +0200

    fix: add missing awaits (#1472)
    * refactor: silence expected console.error call
    
    * fix: add missing access service awaits
    
    * fix: add missing constraint validation await

--- src/lib/db/event-store.test.ts
+++ src/lib/db/event-store.test.ts
@@ -2,6 +2,14 @@ import knex from 'knex';
 import EventStore from './event-store';
 import getLogger from '../../test/fixtures/no-logger';
 
+beforeAll(() => {
+    getLogger.setMuteError(true);
+});
+
+afterAll(() => {
+    getLogger.setMuteError(false);
+});
+
 test('Trying to get events if db fails should yield empty list', async () => {
     const db = knex({
         client: 'pg',

032176576b1748b83eebfabc8cb351844099de42
Thomas Heartman <thomas@getunleash.ai>
2022-04-01 13:44:27 +0200

    chore: add more docusaurus alias resolutions

--- website/.storybook/main.js
+++ website/.storybook/main.js
@@ -1,3 +1,5 @@
+const AliasPlugin = require('enhanced-resolve/lib/AliasPlugin');
+
 module.exports = {
     stories: [
         '../src/**/*.stories.mdx',
@@ -19,9 +21,72 @@ module.exports = {
     webpackFinal: async (config) => {
         const path = require('path');
 
+        const docusaurusPath = (...paths) =>
+            path.resolve(
+                __dirname,
+                '../',
+                'node_modules',
+                '@docusaurus',
+                ...paths,
+            );
+
+        config.resolve.plugins = [
+            new AliasPlugin(
+                'described-resolve',
+                [
+                    {
+                        name: '@theme',
+                        alias: [
+                            path.resolve(__dirname, '../', 'src', 'theme'),
+                            docusaurusPath(
+                                'theme-classic',
+                                'lib-next',
+                                'theme',
+                            ),
+                            docusaurusPath(
+                                'core',
+                                'lib',
+                                'client',
+                                'theme-fallback',
+                            ),
+                        ],
+                    },
+                ],
+                'resolve',
+            ),
+        ];
+
+        // const docusaurusAliases = await loadDocusaurusAliases();
+        // console.log(docusaurusAliases);
+
         config.resolve.alias = {
             ...config.resolve.alias,
             '@site': path.resolve(__dirname, '../'),
+            '@docusaurus/theme-common': path.resolve(
+                __dirname,
+                '../',
+                'node_modules',
+                '@docusaurus',
+                'theme-common',
+                'src',
+                'index.ts',
+            ),
+            '@docusaurus/utils-common': path.resolve(
+                __dirname,
+                '../',
+                'node_modules',
+                '@docusaurus',
+                'utils-common',
+                'lib',
+            ),
+            '@docusaurus/plugin-content-docs': path.resolve(
+                __dirname,
+                '../',
+                'node_modules',
+                '@docusaurus',
+                'plugin-content-docs',
+                'src',
+            ),
             '@docusaurus': path.resolve(
                 __dirname,
                 '../',
@@ -43,6 +108,12 @@ module.exports = {
                 return {
                     ...rule,
                     exclude: /\.module\.css$/,
+                };
+            } else if (rule.test.toString() === '/\\.(mjs|tsx?|jsx?)$/') {
+                return {
+                    ...rule,
+                    // don't exclude docusaurus files
+                    exclude: /node_modules\/(?!@docusaurus)/,
                 };
             } else return rule;
         });

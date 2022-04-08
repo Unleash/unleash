881728bb5ee00feb179254ef93848019f0cce977
Ivar Conradi Østhus <ivarconr@gmail.com>
2022-04-08 11:45:23 +0200

    fix: expose unleash-frontend version in index.js (#661)

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,9 @@
-
-
+require('pkginfo')(module, 'version');
 const path = require('path');
+
+const { version } = module.exports;
 
 module.exports = {
     publicFolder: path.join(__dirname, 'build'),
+    version
 };

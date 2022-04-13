358ef640ca92b1a13ab1e18d615572ae0b80db04
olav <mail@olav.io>
2022-04-13 09:57:57 +0200

    refactor: upcase SQL keywords

--- src/migrations/20220411103724-add-legal-value-description.js
+++ src/migrations/20220411103724-add-legal-value-description.js
@@ -4,23 +4,23 @@ exports.up = function (db, cb) {
     db.runSql(
         `
         -- 1. Convert '1,2,3'::text to "1,2,3"::json
-        alter table context_fields
-        alter column legal_values set data type json using to_json(legal_values);
+        ALTER TABLE context_fields
+        ALTER COLUMN legal_values SET DATA TYPE json USING to_json(legal_values);
 
         -- 2. Convert "1,2,3"::json to [{"value":"1"}, ...]::json.
-        with sub as (
-            select a.name, b.json as legal_values
-            from context_fields as a
-             left join (
-                select name, json_agg(json_build_object('value', value)) as json
-                from context_fields
-                cross join unnest(string_to_array(legal_values #>> '{}', ',')) as value
-                group by name
-            ) as b on a.name = b.name
+        WITH sub AS (
+            SELECT a.name, b.json AS legal_values
+            FROM context_fields AS a
+            LEFT JOIN (
+                SELECT name, json_agg(json_build_object('value', value)) AS json
+                FROM context_fields
+                CROSS JOIN unnest(string_to_array(legal_values #>> '{}', ',')) AS value
+                GROUP BY name
+            ) AS b ON a.name = b.name
         )
-        update context_fields set legal_values = sub.legal_values
-        from sub
-        where context_fields.name = sub.name;
+        UPDATE context_fields SET legal_values = sub.legal_values
+        FROM sub
+        WHERE context_fields.name = sub.name;
         `,
         cb,
     );
@@ -30,26 +30,26 @@ exports.down = function (db, cb) {
     db.runSql(
         `
         -- 1. Revert []::json to null.
-        update context_fields set legal_values = null
-        where legal_values::jsonb = '[]'::jsonb;
+        UPDATE context_fields SET legal_values = null
+        WHERE legal_values::jsonb = '[]'::jsonb;
 
         -- 2. Revert [{"value":"1"}, ...]::json to "1,2,3"::json.
-        with sub as (
-            select name, string_agg(json->>'value', ',') as legal_values
-            from context_fields
-            cross join json_array_elements(legal_values) as json
-            group by name
+        WITH sub AS (
+            SELECT name, string_agg(json->>'value', ',') AS legal_values
+            FROM context_fields
+            CROSS JOIN json_array_elements(legal_values) AS json
+            GROUP BY name
         )
-        update context_fields set legal_values = to_json(sub.legal_values)
-        from sub
-        where context_fields.name = sub.name;
+        UPDATE context_fields SET legal_values = to_json(sub.legal_values)
+        FROM sub
+        WHERE context_fields.name = sub.name;
 
         -- 3. Revert "1,2,3"::json to '"1,2,3"'::text
-        alter table context_fields
-        alter column legal_values set data type text;
+        ALTER TABLE context_fields
+        ALTER COLUMN legal_values SET DATA TYPE text;
 
         -- 4. Revert '"1,2,3"'::text to '1,2,3'::text
-        update context_fields set legal_values = legal_values::json #>> '{}';
+        UPDATE context_fields SET legal_values = legal_values::json #>> '{}';
         `,
         cb,
     );

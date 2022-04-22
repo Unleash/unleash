14694fdf04c4d00a351809e7cf7f67a9022bfe62
Ivar Conradi Østhus <ivarconr@gmail.com>
2022-04-22 13:52:27 +0200

    fix: allow missing instanceId in client metrics (#1522)
    * fix: allow missing instanceId in client metrics
    
    * fix: remove ts-ignore

--- src/lib/routes/client-api/register.test.ts
+++ src/lib/routes/client-api/register.test.ts
@@ -84,12 +84,26 @@ test('should require strategies field', () => {
         .expect(400);
 });
 
+test('should allow an no instanceId field', () => {
+    expect.assertions(0);
+    return request
+        .post('/api/client/register')
+        .send({
+            appName: 'demo',
+            strategies: ['default'],
+            started: Date.now(),
+            interval: 10,
+        })
+        .expect(202);
+});
+
 test('should allow an empty instanceId field', () => {
     expect.assertions(0);
     return request
         .post('/api/client/register')
         .send({
             appName: 'demo',
+            instanceId: '',
             strategies: ['default'],
             started: Date.now(),
             interval: 10,

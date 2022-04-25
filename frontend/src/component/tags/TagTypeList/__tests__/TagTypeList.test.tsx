76951f5d98ea6440fe004bf5d20f22dfbea87a30
olav <mail@olav.io>
2022-04-25 09:04:30 +0200

    refactor: remove unused props in tests (#903)

--- frontend/src/component/tags/TagTypeList/__tests__/TagTypeList.test.tsx
+++ frontend/src/component/tags/TagTypeList/__tests__/TagTypeList.test.tsx
@@ -5,11 +5,7 @@ import { MemoryRouter } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles';
 import theme from 'themes/mainTheme';
 import AccessProvider from 'component/providers/AccessProvider/AccessProvider';
-import {
-    ADMIN,
-    DELETE_TAG_TYPE,
-    UPDATE_TAG_TYPE,
-} from 'component/providers/AccessProvider/permissions';
+import { ADMIN } from 'component/providers/AccessProvider/permissions';
 import UIProvider from 'component/providers/UIProvider/UIProvider';
 
 test('renders an empty list correctly', () => {
@@ -18,45 +14,11 @@ test('renders an empty list correctly', () => {
             <ThemeProvider theme={theme}>
                 <UIProvider>
                     <AccessProvider permissions={[{ permission: ADMIN }]}>
-                        <TagTypeList
-                            tagTypes={[]}
-                            fetchTagTypes={jest.fn()}
-                            removeTagType={jest.fn()}
-                        />
+                        <TagTypeList />
                     </AccessProvider>
                 </UIProvider>
             </ThemeProvider>
         </MemoryRouter>
-    );
-    expect(tree).toMatchSnapshot();
-});
-
-test('renders a list with elements correctly', () => {
-    const tree = renderer.create(
-        <ThemeProvider theme={theme}>
-            <MemoryRouter>
-                <UIProvider>
-                    <AccessProvider
-                        permissions={[
-                            { permission: UPDATE_TAG_TYPE },
-                            { permission: DELETE_TAG_TYPE },
-                        ]}
-                    >
-                        <TagTypeList
-                            tagTypes={[
-                                {
-                                    name: 'simple',
-                                    description: 'Some simple description',
-                                    icon: '#',
-                                },
-                            ]}
-                            fetchTagTypes={jest.fn()}
-                            removeTagType={jest.fn()}
-                        />
-                    </AccessProvider>
-                </UIProvider>
-            </MemoryRouter>
-        </ThemeProvider>
     );
     expect(tree).toMatchSnapshot();
 });

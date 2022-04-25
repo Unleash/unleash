d006640401ef70d184558108884a3d2c8ac8a33d
Ivar Conradi Østhus <ivarconr@gmail.com>
2022-04-25 13:23:35 +0200

    Update src/lib/types/openapi.d.ts

--- src/lib/types/openapi.d.ts
+++ src/lib/types/openapi.d.ts
@@ -1,4 +1,4 @@
-// Partial types for "unleash-express-openapi".
+// Partial types for "@unleash/express-openapi".
 declare module '@unleash/express-openapi' {
     import { RequestHandler } from 'express';
     import { OpenAPIV3 } from 'openapi-types';

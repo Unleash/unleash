adad7763c3acb6fb22a27dce8f67cc533e4f238f
andreas-unleash <andreas@getunleash.ai>
2022-05-06 11:19:28 +0300

    fix pr comment

--- src/lib/openapi/spec/patch-request.ts
+++ src/lib/openapi/spec/patch-request.ts
@@ -1,11 +1,11 @@
 import { OpenAPIV3 } from 'openapi-types';
 
-export const patchFeatureRequest: OpenAPIV3.RequestBodyObject = {
+export const patchRequest: OpenAPIV3.RequestBodyObject = {
     required: true,
     content: {
         'application/json': {
             schema: {
-                $ref: '#/components/schemas/patchFeatureSchema',
+                $ref: '#/components/schemas/patchSchema',
             },
         },
     },

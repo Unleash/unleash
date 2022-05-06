36efd81f3b772b3371015bad0635f58cc29f70f1
andreas-unleash <andreas@getunleash.ai>
2022-05-06 11:12:43 +0300

    fix pr comment

--- src/lib/openapi/spec/tag-response.ts
+++ src/lib/openapi/spec/tag-response.ts
@@ -1,11 +1,12 @@
 import { OpenAPIV3 } from 'openapi-types';
-import { tagSchema } from './tag-schema';
 
 export const tagResponse: OpenAPIV3.ResponseObject = {
     description: 'tagResponse',
     content: {
         'application/json': {
-            schema: tagSchema,
+            schema: {
+                $ref: '#/components/schemas/tagSchema',
+            },
         },
     },
 };

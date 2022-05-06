347fbc154e0e9c1b43ae69d0b52acf0dcb69c9e9
andreas-unleash <andreas@getunleash.ai>
2022-05-06 11:14:51 +0300

    fix pr comment

--- src/lib/openapi/spec/update-feature-request.ts
+++ src/lib/openapi/spec/update-feature-request.ts
@@ -1,11 +1,12 @@
 import { OpenAPIV3 } from 'openapi-types';
-import { updateFeatureSchema } from './updateFeatureSchema';
 
 export const updateFeatureRequest: OpenAPIV3.RequestBodyObject = {
     required: true,
     content: {
         'application/json': {
-            schema: updateFeatureSchema,
+            schema: {
+                $ref: '#/components/schemas/updateFeatureSchema',
+            },
         },
     },
 };

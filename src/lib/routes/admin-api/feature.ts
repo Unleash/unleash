2007580c8879b12e7468f2d1166de8a08529155b
andreas-unleash <andreas@getunleash.ai>
2022-05-09 11:18:35 +0300

    Update addTag response status to 201

--- src/lib/routes/admin-api/feature.ts
+++ src/lib/routes/admin-api/feature.ts
@@ -117,7 +117,7 @@ class FeatureController extends Controller {
                     tags: ['admin'],
                     operationId: 'addTag',
                     requestBody: createTagRequest,
-                    responses: { 200: tagResponse },
+                    responses: { 201: tagResponse },
                 }),
             ],
         });

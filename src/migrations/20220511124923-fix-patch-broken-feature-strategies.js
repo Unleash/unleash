7250a0574cd3fb80e0d465a8f8dff3f061f19efd
Ivar Conradi Østhus <ivarconr@gmail.com>
2022-05-11 14:50:13 +0200

    fix: correct patch for feature project id

---
+++ src/migrations/20220511124923-fix-patch-broken-feature-strategies.js
@@ -0,0 +1,19 @@
+'use strict';
+
+exports.up = function (db, cb) {
+    db.runSql(
+        `
+          UPDATE feature_strategies
+          SET project_name = project
+          FROM features
+          WHERE features.project != feature_strategies.project_name
+          AND features.name = feature_strategies.feature_name;
+        `,
+        cb,
+    );
+};
+
+// This is a fix for a broken state, we don't want this to be rolled back
+exports.down = function (db, cb) {
+    cb();
+};

d88ff77f91caa602ca87c0384481cbdc6d522364
olav <mail@olav.io>
2022-05-20 15:21:19 +0200

    refactor: remove ref order note

--- src/lib/openapi/types.ts
+++ src/lib/openapi/types.ts
@@ -28,7 +28,6 @@ export type CreateSchemaType<T> = FromSchema<
 // Create an OpenAPIV3.SchemaObject from a const schema object.
 // Make sure the schema contains an object of refs for type generation.
 // Pass an empty 'components/schemas' object if there are no refs in the schema.
-// Note: The order of the refs must match the order they are present in the object
 export const createSchemaObject = <
     T extends { 'components/schemas': { [key: string]: object } },
 >(

7b7f0b84e8cddd5880dcf29c231672113224b9a7
Ivar Conradi Østhus <ivarconr@gmail.com>
2022-05-20 12:53:36 +0200

    fix: coverage action need username

--- scripts/commit-coverage.sh
+++ scripts/commit-coverage.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+git config --global user.name "${GH_NAME}"
+git config --global user.email "${GH_EMAIL}"
+echo "machine github.com login ${GH_NAME} password ${GH_TOKEN}" > ~/.netrc
+
 git add coverage
 git diff-index --quiet HEAD || git commit -m 'chore: update coverage reports'
 git push origin

c4e96a0dabf28e250638a481277cd551f36212eb
olav <mail@olav.io>
2022-06-02 15:18:54 +0200

    refactor: fix emptySchema definition

--- src/lib/openapi/spec/empty-schema.ts
+++ src/lib/openapi/spec/empty-schema.ts
@@ -2,7 +2,6 @@ import { FromSchema } from 'json-schema-to-ts';
 import { DeepMutable } from '../../types/mutable';
 
 const schema = {
-    type: 'object',
     description: 'emptySchema',
 } as const;
 

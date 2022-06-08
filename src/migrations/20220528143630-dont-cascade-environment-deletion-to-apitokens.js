829611eae406ebedb143048a3116676c7ddd2796
szczepad <dennis.szczepanski@web.de>
2022-06-08 06:47:37 +0200

    fix: Adds correct down-script for migration 20220528143630

--- src/migrations/20220528143630-dont-cascade-environment-deletion-to-apitokens.js
+++ src/migrations/20220528143630-dont-cascade-environment-deletion-to-apitokens.js
@@ -12,7 +12,7 @@ exports.up = function (db, cb) {
 exports.down = function (db, cb) {
     db.runSql(
         `
-          ALTER TABLE api_tokens DROP CONSTRAINT api_tokens_environment_fkey;
+          ALTER TABLE api_tokens ADD CONSTRAINT api_tokens_environment_fkey FOREIGN KEY(environment) REFERENCES environments(name) ON DELETE CASCADE;
       `,
         cb,
     );

6c84d9465173e68f938e91b137ad65e7e364265a
olav <mail@olav.io>
2022-06-13 10:24:34 +0200

    refactor: add tests for the schemas list

--- src/lib/openapi/index.test.ts
+++ src/lib/openapi/index.test.ts
@@ -3,7 +3,34 @@ import {
     createRequestSchema,
     createResponseSchema,
     removeJsonSchemaProps,
+    schemas,
 } from './index';
+import fs from 'fs';
+import path from 'path';
+
+test('all schema files should be added to the schemas object', () => {
+    const schemaFileNames = fs
+        .readdirSync(path.join(__dirname, 'spec'))
+        .filter((fileName) => fileName.endsWith('-schema.ts'));
+
+    const expectedSchemaNames = schemaFileNames.map((fileName) => {
+        return fileName
+            .replace(/\.ts$/, '')
+            .replace(/-./g, (x) => x[1].toUpperCase());
+    });
+
+    const addedSchemaNames = Object.keys(schemas);
+    expect(expectedSchemaNames.sort()).toEqual(addedSchemaNames.sort());
+});
+
+test('all schema $id attributes should have the expected format', () => {
+    const schemaIds = Object.values(schemas).map((schema) => schema.$id);
+    const schemaIdRegExp = new RegExp(`^#/components/schemas/[a-z][a-zA-Z]+$`);
+
+    schemaIds.forEach((schemaId) => {
+        expect(schemaId).toMatch(schemaIdRegExp);
+    });
+});
 
 test('createRequestSchema', () => {
     expect(createRequestSchema('schemaName')).toMatchInlineSnapshot(`

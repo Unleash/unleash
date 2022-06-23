9ca2b6fc46a4ccd7c8187cff46d6dbad1f06684f
Nuno Góis <github@nunogois.com>
2022-06-23 07:54:08 +0100

    feat: add stripe integration (#1109)
    * feat: add stripe integration
    
    * add start:demo yarn script

--- frontend/src/component/admin/billing/BillingDashboard/BillingInformation/BillingInformationButton/BillingInformationButton.tsx
+++ frontend/src/component/admin/billing/BillingDashboard/BillingInformation/BillingInformationButton/BillingInformationButton.tsx
@@ -1,20 +1,8 @@
 import { Button, styled } from '@mui/material';
 import { VFC } from 'react';
+import { formatApiPath } from 'utils/formatPath';
 
-const href = `mailto:elise@getunleash.ai?subject=Continue with Unleash&body=Hi Unleash,%0D%0A%0D%0A
-I would like to continue with Unleash.%0D%0A%0D%0A%0D%0A%0D%0A
-
-Billing information:%0D%0A%0D%0A
-
-1. Company name (legal name): [add your information here]%0D%0A%0D%0A
-2. Email address (where we will send the invoice): [add your information here]%0D%0A%0D%0A
-3. Address: [add your information here]%0D%0A%0D%0A
-4. Country: [add your information here]%0D%0A%0D%0A
-5. VAT ID (optional - only European countries): [add your information here]%0D%0A%0D%0A%0D%0A%0D%0A%0D%0A
-
-
-
--- Thank you for signing up. We will upgrade your trial as quick as possible and we will grant you access to the application again. --`;
+const PORTAL_URL = formatApiPath('api/admin/invoices');
 
 const StyledButton = styled(Button)(({ theme }) => ({
     width: '100%',
@@ -27,10 +15,11 @@ interface IBillingInformationButtonProps {
 
 export const BillingInformationButton: VFC<IBillingInformationButtonProps> = ({
     update,
-}) => {
-    return (
-        <StyledButton href={href} variant={update ? 'outlined' : 'contained'}>
-            {update ? 'Update billing information' : 'Add billing information'}
-        </StyledButton>
-    );
-};
+}) => (
+    <StyledButton
+        href={`${PORTAL_URL}/${update ? 'portal' : 'checkout'}`}
+        variant={update ? 'outlined' : 'contained'}
+    >
+        {update ? 'Update billing information' : 'Add billing information'}
+    </StyledButton>
+);

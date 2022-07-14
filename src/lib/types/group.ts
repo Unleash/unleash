262bed069a39e8e373670038b3337c17b4396117
sjaanus <sellinjaanus@gmail.com>
2022-07-14 07:46:04 +0000

    Fix tests

--- src/lib/types/group.ts
+++ src/lib/types/group.ts
@@ -2,7 +2,7 @@ import Joi from 'joi';
 import { IUser } from './user';
 
 export interface IGroup {
-    id: number;
+    id?: number;
     name: string;
     description: string;
     createdAt?: Date;

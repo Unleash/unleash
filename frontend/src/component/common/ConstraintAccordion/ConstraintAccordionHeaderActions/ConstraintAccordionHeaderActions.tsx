47c33e62e1e48db80b430bf6864e136f42e0bd1d
andreas-unleash <104830839+andreas-unleash@users.noreply.github.com>
2022-07-22 13:56:27 +0300

    bug fix

--- frontend/src/component/common/ConstraintAccordion/ConstraintAccordionHeaderActions/ConstraintAccordionHeaderActions.tsx
+++ frontend/src/component/common/ConstraintAccordion/ConstraintAccordionHeaderActions/ConstraintAccordionHeaderActions.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ConditionallyRender } from '../../ConditionallyRender/ConditionallyRender';
 import { IconButton, Tooltip } from '@mui/material';
 import { Delete, Edit } from '@mui/icons-material';
 import { useStyles } from '../ConstraintAccordion.styles';
@@ -7,13 +6,11 @@ import { useStyles } from '../ConstraintAccordion.styles';
 interface ConstraintAccordionHeaderActionsProps {
     onDelete?: () => void;
     onEdit?: () => void;
-    disableEdit?: boolean;
 }
 
 export const ConstraintAccordionHeaderActions = ({
     onEdit,
     onDelete,
-    disableEdit = false,
 }: ConstraintAccordionHeaderActionsProps) => {
     const { classes: styles } = useStyles();
     const onEditClick =
@@ -32,34 +29,24 @@ export const ConstraintAccordionHeaderActions = ({
 
     return (
         <div className={styles.headerActions}>
-            <ConditionallyRender
-                condition={Boolean(onEditClick) && !disableEdit}
-                show={() => (
-                    <Tooltip title="Edit constraint" arrow>
-                        <IconButton
-                            type="button"
-                            onClick={onEditClick}
-                            disabled={!onEdit}
-                        >
-                            <Edit />
-                        </IconButton>
-                    </Tooltip>
-                )}
-            />
-            <ConditionallyRender
-                condition={Boolean(onDeleteClick)}
-                show={() => (
-                    <Tooltip title="Delete constraint" arrow>
-                        <IconButton
-                            type="button"
-                            onClick={onDeleteClick}
-                            disabled={!onDelete}
-                        >
-                            <Delete />
-                        </IconButton>
-                    </Tooltip>
-                )}
-            />
+            <Tooltip title="Edit constraint" arrow>
+                <IconButton
+                    type="button"
+                    onClick={onEditClick}
+                    disabled={!Boolean(onEditClick)}
+                >
+                    <Edit />
+                </IconButton>
+            </Tooltip>
+            <Tooltip title="Delete constraint" arrow>
+                <IconButton
+                    type="button"
+                    onClick={onDeleteClick}
+                    disabled={!Boolean(onDeleteClick)}
+                >
+                    <Delete />
+                </IconButton>
+            </Tooltip>
         </div>
     );
 };

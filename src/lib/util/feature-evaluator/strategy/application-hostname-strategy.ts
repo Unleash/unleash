558094b11d8352dea3b9068a326c9d943a74c519
Thomas Heartman <thomas@getunleash.ai>
2022-08-03 14:37:38 +0200

    Chore: remove redundant comment

--- src/lib/util/feature-evaluator/strategy/application-hostname-strategy.ts
+++ src/lib/util/feature-evaluator/strategy/application-hostname-strategy.ts
@@ -13,7 +13,6 @@ export default class ApplicationHostnameStrategy extends Strategy {
         ).toLowerCase();
     }
 
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
     isEnabled(parameters: { hostNames: string }): boolean {
         if (!parameters.hostNames) {
             return false;

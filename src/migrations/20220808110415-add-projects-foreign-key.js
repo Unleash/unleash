96799f8ea19df5d0fdf95371ec552f4baf1d551e
sjaanus <sellinjaanus@gmail.com>
2022-08-08 12:17:59 +0000

    Add constraint for project

--- src/migrations/20220808110415-add-projects-foreign-key.js
+++ src/migrations/20220808110415-add-projects-foreign-key.js
@@ -13,6 +13,7 @@ exports.up = function (db, cb) {
 exports.down = function (db, cb) {
     db.runSql(
         `
+
             ALTER TABLE group_role DROP CONSTRAINT fk_group_role_project;
 `,
         cb,

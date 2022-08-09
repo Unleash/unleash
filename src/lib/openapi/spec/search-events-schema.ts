04229e76853b340be61b1ba8a2cf57de2f9c31d9
olav <mail@olav.io>
2022-08-09 12:14:54 +0200

    refactor: describe searchEventsSchema fields

--- src/lib/openapi/spec/search-events-schema.ts
+++ src/lib/openapi/spec/search-events-schema.ts
@@ -3,18 +3,31 @@ import { FromSchema } from 'json-schema-to-ts';
 export const searchEventsSchema = {
     $id: '#/components/schemas/searchEventsSchema',
     type: 'object',
+    description: `
+        Search for events by type, project, feature, free-text query,
+        or a combination thereof. Pass an empty object to fetch all events.
+    `,
     properties: {
         type: {
             type: 'string',
+            description: 'Find events by event type (case-sensitive).',
         },
         project: {
             type: 'string',
+            description: 'Find events by project ID (case-sensitive).',
         },
         feature: {
             type: 'string',
+            description: 'Find events by feature toggle name (case-sensitive).',
         },
         query: {
             type: 'string',
+            description: `
+                Find events by a free-text search query.
+                The query will be matched against the event type,
+                the username or email that created the event (if any),
+                and the event data payload (if any).
+            `,
         },
         limit: {
             type: 'integer',

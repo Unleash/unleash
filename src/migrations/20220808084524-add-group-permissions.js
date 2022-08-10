ee437db5c384ef4b332dc1edee111722cb994aa0
Simon Hornby <liquidwicked64@gmail.com>
2022-08-10 11:57:07 +0200

    Update src/migrations/20220808084524-add-group-permissions.js

--- src/migrations/20220808084524-add-group-permissions.js
+++ src/migrations/20220808084524-add-group-permissions.js
@@ -12,7 +12,7 @@ exports.up = function (db, cb) {
 exports.down = function (db, cb) {
     db.runSql(
         `
-            ALTER TABLE group_user ADD COLUMN role text check(role in ('Owner', 'Member'));
+            ALTER TABLE group_user ADD COLUMN role text check(role in ('Owner', 'Member')) default 'Member';
     `,
         cb,
     );

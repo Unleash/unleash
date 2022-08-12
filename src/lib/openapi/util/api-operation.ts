2f6669c2df28437455ccf0c26d6e6ad65f6006e5
Thomas Heartman <thomas@getunleash.ai>
2022-08-12 14:00:10 +0200

    Docs: add explanatory comment for why the type exists.

--- src/lib/openapi/util/api-operation.ts
+++ src/lib/openapi/util/api-operation.ts
@@ -1,7 +1,14 @@
 import { OpenAPIV3 } from 'openapi-types';
 import { OpenApiTag } from './openapi-tags';
 
-type DeprecatedOpenAPITag = 'client' | 'other' | 'auth' | 'admin';
+type DeprecatedOpenAPITag =
+    // Deprecated tag names. Please use a tag from the OpenAPITag type instead.
+    //
+    // These tag names were the original ones we used for OpenAPI, but they
+    // turned out to be too generic and/or didn't match the new tag naming
+    // schema. Because we require our operations to have one of a predefined set
+    // of values, it would be breaking change to remove them completely.
+    'client' | 'other' | 'auth' | 'admin';
 
 export interface ApiOperation<Tag = OpenApiTag | DeprecatedOpenAPITag>
     extends Omit<OpenAPIV3.OperationObject, 'tags'> {

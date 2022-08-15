934b0cdf889496cecccc6f26c1a4e4c20af1b8aa
olav <mail@olav.io>
2022-08-15 16:01:57 +0200

    feat: support listening for any event

--- src/lib/types/stores/event-store.ts
+++ src/lib/types/stores/event-store.ts
@@ -1,9 +1,9 @@
-import EventEmitter from 'events';
 import { IBaseEvent, IEvent } from '../events';
 import { Store } from './store';
 import { SearchEventsSchema } from '../../openapi/spec/search-events-schema';
+import { AnyEventEmitter } from 'lib/util/anyEventEmitter';
 
-export interface IEventStore extends Store<IEvent, number>, EventEmitter {
+export interface IEventStore extends Store<IEvent, number>, AnyEventEmitter {
     store(event: IBaseEvent): Promise<void>;
     batchStore(events: IBaseEvent[]): Promise<void>;
     getEvents(): Promise<IEvent[]>;

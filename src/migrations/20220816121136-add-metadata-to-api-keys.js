083686fe95a5cb34e07c5882aec91ae365ef32ff
Fredrik Oseberg <fredrik.no@gmail.com>
2022-08-16 15:32:29 +0200

    fix: default to empty object for metadata

--- src/migrations/20220816121136-add-metadata-to-api-keys.js
+++ src/migrations/20220816121136-add-metadata-to-api-keys.js
@@ -1,7 +1,10 @@
 'use strict';
 
 exports.up = function (db, cb) {
-    db.runSql(`ALTER TABLE api_tokens ADD COLUMN metadata JSONB;`, cb);
+    db.runSql(
+        `ALTER TABLE api_tokens ADD COLUMN metadata JSONB NOT NULL DEFAULT '{}'::jsonb;`,
+        cb,
+    );
 };
 
 exports.down = function (db, cb) {

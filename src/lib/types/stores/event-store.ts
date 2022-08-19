8f8245aa7bc80ee446f00d6ae9cf8bfe33dabaf4
olav <mail@olav.io>
2022-08-19 16:03:29 +0200

    refactor: fix missing IEventStore event emitter methods

--- src/lib/types/stores/event-store.ts
+++ src/lib/types/stores/event-store.ts
@@ -1,9 +1,9 @@
 import { IBaseEvent, IEvent } from '../events';
 import { Store } from './store';
 import { SearchEventsSchema } from '../../openapi/spec/search-events-schema';
-import { AnyEventEmitter } from 'lib/util/anyEventEmitter';
+import EventEmitter from 'events';
 
-export interface IEventStore extends Store<IEvent, number>, AnyEventEmitter {
+export interface IEventStore extends Store<IEvent, number>, EventEmitter {
     store(event: IBaseEvent): Promise<void>;
     batchStore(events: IBaseEvent[]): Promise<void>;
     getEvents(): Promise<IEvent[]>;

00e0e705782d88ee4440456fa9471db0d75d6e89
olav <mail@olav.io>
2022-08-24 08:10:15 +0200

    refactor: simplify login redirect logic

--- frontend/src/hooks/api/getters/httpErrorResponseHandler.ts
+++ frontend/src/hooks/api/getters/httpErrorResponseHandler.ts
@@ -1,24 +1,23 @@
+import { ResponseError } from 'utils/apiUtils';
+
 const handleErrorResponses = (target: string) => async (res: Response) => {
     if (!res.ok) {
-        const error = new Error(
-            `An error occurred while trying to get ${target}`
+        throw new ResponseError(
+            target,
+            res.status,
+            await parseErrorResponse(res)
         );
-        // Try to resolve body, but don't rethrow res.json is not a function
-        try {
-            // @ts-expect-error
-            error.info = await res.json();
-        } catch (e) {
-            // @ts-expect-error
-            error.info = {};
-        }
-        // @ts-expect-error
-        error.status = res.status;
-        // @ts-expect-error
-        error.statusText = res.statusText;
-        throw error;
     }
 
     return res;
+};
+
+const parseErrorResponse = async (res: Response): Promise<unknown> => {
+    try {
+        return await res.json();
+    } catch {
+        return res.statusText;
+    }
 };
 
 export default handleErrorResponses;
--- frontend/src/utils/apiUtils.ts
+++ frontend/src/utils/apiUtils.ts
@@ -57,6 +57,18 @@ export class NotFoundError extends Error {
     }
 }
 
+export class ResponseError extends Error {
+    status: number;
+    body: unknown;
+
+    constructor(target: string, status: number, body: unknown) {
+        super(`An error occurred while trying to get ${target}.`);
+        this.name = 'ResponseError';
+        this.status = status;
+        this.body = body;
+    }
+}
+
 export const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json',

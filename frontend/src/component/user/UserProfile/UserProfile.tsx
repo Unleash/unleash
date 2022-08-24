e5f0c39dbd4c9af2d8b8c1e1c6b812132d67bbfe
Fredrik Oseberg <fredrik.no@gmail.com>
2022-08-24 10:48:46 +0200

    fix: avatar padding

--- frontend/src/component/user/UserProfile/UserProfile.tsx
+++ frontend/src/component/user/UserProfile/UserProfile.tsx
@@ -75,7 +75,7 @@ const UserProfile = ({
                     disableRipple
                 >
                     <Avatar
-                        style={{ backgroundColor: '#fff', padding: '0.15rem' }}
+                        style={{ backgroundColor: '#fff' }}
                         alt="Your Gravatar"
                         src={imageUrl}
                         data-testid={HEADER_USER_AVATAR}

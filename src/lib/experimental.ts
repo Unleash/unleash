74afb6881001870aed68ac59650709328d37e41b
Ivar Conradi Østhus <ivarconr@gmail.com>
2022-08-24 10:53:31 +0200

    fix: remove unused exp flag

--- src/lib/experimental.ts
+++ src/lib/experimental.ts
@@ -1,6 +1,5 @@
 export interface IExperimentalOptions {
     metricsV2?: IExperimentalToggle;
-    clientFeatureMemoize?: IExperimentalToggle;
     userGroups?: boolean;
     anonymiseEventLog?: boolean;
     embedProxy?: boolean;

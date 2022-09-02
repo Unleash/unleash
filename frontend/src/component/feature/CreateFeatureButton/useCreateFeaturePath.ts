3331e2aa85948bf26fa82eb7fd28774ea794d7be
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2022-09-02 13:35:39 +0200

    refactor: use permission button for new toggle (#2009)

--- frontend/src/component/feature/CreateFeatureButton/useCreateFeaturePath.ts
+++ frontend/src/component/feature/CreateFeatureButton/useCreateFeaturePath.ts
@@ -2,33 +2,29 @@ import { useDefaultProjectId } from 'hooks/api/getters/useDefaultProject/useDefa
 import { IFeaturesFilter } from 'hooks/useFeaturesFilter';
 import { getCreateTogglePath } from 'utils/routePathHelpers';
 import useUiConfig from 'hooks/api/getters/useUiConfig/useUiConfig';
-import { CREATE_FEATURE } from 'component/providers/AccessProvider/permissions';
-import AccessContext from 'contexts/AccessContext';
-import { useContext } from 'react';
 
 interface IUseCreateFeaturePathOutput {
     path: string;
-    access: boolean;
+    projectId: string;
 }
 
 export const useCreateFeaturePath = (
     filter: IFeaturesFilter
 ): IUseCreateFeaturePathOutput | undefined => {
-    const { hasAccess } = useContext(AccessContext);
     const defaultProjectId = useDefaultProjectId();
     const { uiConfig } = useUiConfig();
 
-    const selectedProjectId =
+    const projectId =
         filter.project === '*' || !filter.project
             ? defaultProjectId
             : filter.project;
 
-    if (!selectedProjectId) {
+    if (!projectId) {
         return;
     }
 
     return {
-        path: getCreateTogglePath(selectedProjectId, uiConfig.flags.E),
-        access: hasAccess(CREATE_FEATURE, selectedProjectId),
+        path: getCreateTogglePath(projectId, uiConfig.flags.E),
+        projectId,
     };
 };

ef80ba6493455c896cceecec5cba7f2e126acde2
andreas-unleash <104830839+andreas-unleash@users.noreply.github.com>
2022-09-06 14:11:15 +0300

    FMT

--- frontend/src/component/feature/FeatureStrategy/FeatureStrategyEmpty/FeatureStrategyEmpty.tsx
+++ frontend/src/component/feature/FeatureStrategy/FeatureStrategyEmpty/FeatureStrategyEmpty.tsx
@@ -101,7 +101,7 @@ export const FeatureStrategyEmpty = ({
                 parameters: {
                     rollout: '50',
                     stickiness: 'default',
-                    groupId: feature.name
+                    groupId: feature.name,
                 },
                 constraints: [],
             });

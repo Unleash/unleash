2001d098de1ccbc7615ff6a0f2cd0a46b300d540
Thomas Heartman <thomas@getunleash.ai>
2022-09-15 12:33:59 +0200

    Chore(#1391): update comment explaining api token schema TODO

--- src/lib/openapi/spec/create-api-token-schema.ts
+++ src/lib/openapi/spec/create-api-token-schema.ts
@@ -1,17 +1,19 @@
 import { FromSchema } from 'json-schema-to-ts';
 import { ApiTokenType } from '../../types/models/api-token';
 
-// TODO: (openapi) this schema isn't entirely correct: `project` and `projects` are mutually exclusive.
+// TODO: (openapi) this schema isn't entirely correct: `project` and `projects`
+// are mutually exclusive.
 //
-// That is, when creating a token, you can provide either `project` _or_ `projects`, but *not* both.
+// That is, when creating a token, you can provide either `project` _or_
+// `projects`, but *not* both.
 //
-// We should be able to annotate this using `oneOf` and `allOf`, but I'm having some issues with the inbound validation at the moment, and it _may_ be a bug with the package we're using.
+// We should be able to annotate this using `oneOf` and `allOf`, but making
+// `oneOf` only valid for _either_ `project` _or_ `projects` is tricky.
 //
-// I've opened an issue to keep track of it:
+// I've opened an issue to get some help (thought it was a bug initially).
+// There's more info available at:
 //
 // https://github.com/ajv-validator/ajv/issues/2096
-//
-// Until it's resolved: feel free to take a stab at it: it might just be _me_ messing up 😅
 //
 // This also applies to apiTokenSchema and potentially other related schemas.
 

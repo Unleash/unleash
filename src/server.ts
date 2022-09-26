c4f20d0321bf0b1c73530bfb251be70febb6f9fb
Gastón Fournier <gastonfournier@gmail.com>
2022-09-26 12:55:51 +0200

    Trying to reproduce an error

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import * as unleash from './lib/server-impl';
 
 try {
     unleash.start();
+    console.log('Unleash started', 'Do not merge this change, it is just to test https://github.com/Unleash/unleash/pull/2087');
 } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);

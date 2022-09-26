31a0ab5f13889bb99a9a29556058c83149cf4890
Gastón Fournier <gastonfournier@gmail.com>
2022-09-26 13:06:52 +0200

    lint

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,10 @@ import * as unleash from './lib/server-impl';
 
 try {
     unleash.start();
-    console.log('Unleash started', 'Do not merge this change, it is just to test https://github.com/Unleash/unleash/pull/2087');
+    console.log(
+        'Unleash started',
+        'Do not merge this change, it is just to test https://github.com/Unleash/unleash/pull/2087',
+    );
 } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);

2a54ace005ae47fd9a17d5df15495024336570ea
sjaanus <sellinjaanus@gmail.com>
2022-09-26 14:42:39 +0200

    Add description to PAT (#2090)
    * Add description to PAT
    
    * Add tests

--- src/migrations/20220912165344-pat-tokens.js
+++ src/migrations/20220912165344-pat-tokens.js
@@ -5,6 +5,7 @@ exports.up = function (db, cb) {
         `
         CREATE TABLE personal_access_tokens (
             secret text not null primary key,
+            description text,
             user_id integer not null references users (id) ON DELETE CASCADE,
             expires_at  timestamp with time zone NOT NULL,
             seen_at  timestamp with time zone,

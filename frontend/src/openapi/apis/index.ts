b3e00000466d871bbca8d25aa4e542e4262284f8
Tymoteusz Czech <tymek+gpg@getunleash.ai>
2022-09-30 10:24:48 +0200

    revert openapi changes

--- frontend/src/openapi/apis/index.ts
+++ frontend/src/openapi/apis/index.ts
@@ -1,24 +1,3 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './APITokensApi';
-export * from './AddonsApi';
 export * from './AdminApi';
-export * from './AdminUIApi';
-export * from './ArchiveApi';
-export * from './AuthApi';
-export * from './ClientApi';
-export * from './ContextApi';
-export * from './EdgeApi';
-export * from './EnvironmentsApi';
-export * from './EventsApi';
-export * from './FeaturesApi';
-export * from './ImportExportApi';
-export * from './MetricsApi';
-export * from './OperationalApi';
-export * from './PlaygroundApi';
-export * from './ProjectsApi';
-export * from './PublicSignupTokensApi';
-export * from './StrategiesApi';
-export * from './TagsApi';
-export * from './UnstableApi';
-export * from './UsersApi';

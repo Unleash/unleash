5787017dd4695c251f8508ad0c897aa378d99529
sjaanus <sellinjaanus@gmail.com>
2022-10-04 13:08:39 +0300

    Remove dependencies

--- frontend/src/component/project/ProjectCard/ProjectCard.tsx
+++ frontend/src/component/project/ProjectCard/ProjectCard.tsx
@@ -3,7 +3,7 @@ import { useStyles } from './ProjectCard.styles';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { ReactComponent as ProjectIcon } from 'assets/icons/projectIcon.svg';
 import React, { useState, SyntheticEvent, useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Delete, Edit } from '@mui/icons-material';
 import { getProjectEditPath } from 'utils/routePathHelpers';
 import PermissionIconButton from 'component/common/PermissionIconButton/PermissionIconButton';
@@ -15,8 +15,6 @@ import AccessContext from 'contexts/AccessContext';
 import { DEFAULT_PROJECT_ID } from 'hooks/api/getters/useDefaultProject/useDefaultProjectId';
 import useUiConfig from 'hooks/api/getters/useUiConfig/useUiConfig';
 import { DeleteProjectDialogue } from '../Project/DeleteProject/DeleteProjectDialogue';
-import classnames from 'classnames';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { ConditionallyRender } from '../../common/ConditionallyRender/ConditionallyRender';
 
 interface IProjectCardProps {

c950e06f927155e58e17ea289623e3f192407444
Aneesh Relan <aneesh.relan@gmail.com>
2022-10-11 23:18:56 +0530

    add tooltip for larger numbers

--- frontend/src/component/common/PrettifyLargeNumber/PrettifyLargeNumber.tsx
+++ frontend/src/component/common/PrettifyLargeNumber/PrettifyLargeNumber.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import millify from 'millify';
+import { Tooltip } from '@mui/material';
 
 interface IPrettifyLargeNumberProps {
     /**
@@ -8,17 +9,36 @@ interface IPrettifyLargeNumberProps {
     value: number;
     /**
      * Threshold above which the number will be prettified. Values lower than this will just have comma separators added
+     * @default 1_000_000
      */
     threshold?: number;
+    /**
+     * The number of significant figures
+     * @default 2
+     */
+    precision?: number;
 }
 
 export const PrettifyLargeNumber: FC<IPrettifyLargeNumberProps> = ({
     value,
     threshold = 1_000_000,
+    precision = 2,
 }) => {
+    let prettyValue: string;
+    let showTooltip = false;
+
     if (value < threshold) {
-        return <>{value.toLocaleString()}</>;
+        prettyValue = value.toLocaleString();
+    } else {
+        prettyValue = millify(value, { precision });
+        showTooltip = true;
     }
 
-    return <>{millify(value)}</>;
+    return showTooltip ? (
+        <Tooltip title={value.toLocaleString()} arrow>
+            <span>{prettyValue}</span>
+        </Tooltip>
+    ) : (
+        <>{prettyValue}</>
+    );
 };

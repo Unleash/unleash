9ca9d29808bcb660c9290aefd735027987052767
andreas-unleash <104830839+andreas-unleash@users.noreply.github.com>
2022-10-26 16:18:12 +0300

    fix alignment

--- frontend/src/component/suggest-changes/ProjectSuggestions/ProjectSuggestions.styles.ts
+++ frontend/src/component/suggest-changes/ProjectSuggestions/ProjectSuggestions.styles.ts
@@ -2,7 +2,7 @@ import { makeStyles } from 'tss-react/mui';
 
 export const useStyles = makeStyles()(theme => ({
     tabContainer: {
-        padding: '0 2rem',
+        paddingLeft: 0,
     },
     tabButton: {
         textTransform: 'none',

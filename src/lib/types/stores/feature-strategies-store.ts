35cdd840832912c540ff10eed25b9f2c32f7a731
Fredrik Oseberg <fredrik.no@gmail.com>
2022-10-28 09:21:48 +0200

    fix: remove method from interface

--- src/lib/types/stores/feature-strategies-store.ts
+++ src/lib/types/stores/feature-strategies-store.ts
@@ -30,10 +30,6 @@ export interface IFeatureStrategiesStore
         featureName: string,
         environment: string,
     ): Promise<IFeatureStrategy[]>;
-    getFeatureToggleForEnvironment(
-        featureName: string,
-        environment: string,
-    ): Promise<FeatureToggleWithEnvironment>;
     getFeatureToggleWithEnvs(
         featureName: string,
         archived?: boolean,

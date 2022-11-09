5e14e80ae33dcda511c43ad910fbb0014034346f
sjaanus <sellinjaanus@gmail.com>
2022-11-09 09:40:47 +0100

    Change request approvals table (#2347)
    1. Added database table for change request approvals
    2. Removed separate endpoint for applying. **Now all state changes will
    go through same endpoint.**

--- src/lib/util/extract-user.ts
+++ src/lib/util/extract-user.ts
@@ -1,5 +1,9 @@
-import { IAuthRequest } from '../server-impl';
+import { IAuthRequest, User } from '../server-impl';
+
+export function extractUsernameFromUser(user: User): string {
+    return user ? user.email || user.username : 'unknown';
+}
 
 export function extractUsername(req: IAuthRequest): string {
-    return req.user ? req.user.email || req.user.username : 'unknown';
+    return extractUsernameFromUser(req.user);
 }

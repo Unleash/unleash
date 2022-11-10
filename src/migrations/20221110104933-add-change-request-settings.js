275e853e4aaafaf9e9a20702e9df68b8ac475b57
andreas-unleash <104830839+andreas-unleash@users.noreply.github.com>
2022-11-10 16:35:31 +0200

    create change_request_settings table

--- src/migrations/20221110104933-add-change-request-settings.js
+++ src/migrations/20221110104933-add-change-request-settings.js
@@ -3,11 +3,11 @@
 exports.up = function (db, callback) {
     db.runSql(
         `
-    CREATE TABLE IF NOT EXISTS change_request_settings (
-       project varchar(255) REFERENCES projects(id) ON DELETE CASCADE,
-       environment varchar(100) REFERENCES environments(name) ON DELETE CASCADE,
-       PRIMARY KEY(project, environment)
-    );
+            CREATE TABLE IF NOT EXISTS change_request_settings (
+               project varchar(255) REFERENCES projects(id) ON DELETE CASCADE,
+               environment varchar(100) REFERENCES environments(name) ON DELETE CASCADE,
+               PRIMARY KEY(project, environment)
+            );
         `,
         callback,
     );
@@ -16,7 +16,7 @@ exports.up = function (db, callback) {
 exports.down = function (db, callback) {
     db.runSql(
         `
-DROP TABLE IF EXISTS change_request_settings;
+            DROP TABLE IF EXISTS change_request_settings;
         `,
         callback,
     );

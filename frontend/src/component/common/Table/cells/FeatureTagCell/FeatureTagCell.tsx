4b781163da01bafad2199848c7a067309e69a240
Nuno Góis <github@nunogois.com>
2022-11-10 16:19:47 +0000

    add FeatureTagCell to toggles overview

---
+++ frontend/src/component/common/Table/cells/FeatureTagCell/FeatureTagCell.tsx
@@ -0,0 +1,59 @@
+import { VFC } from 'react';
+import { FeatureSchema } from 'openapi';
+import { Link, styled, Typography } from '@mui/material';
+import { TextCell } from '../TextCell/TextCell';
+import { HtmlTooltip } from 'component/common/HtmlTooltip/HtmlTooltip';
+import { Highlighter } from 'component/common/Highlighter/Highlighter';
+import { useSearchHighlightContext } from 'component/common/Table/SearchHighlightContext/SearchHighlightContext';
+
+const StyledTag = styled(Typography)(({ theme }) => ({
+    fontSize: theme.fontSizes.smallerBody,
+}));
+
+const StyledLink = styled(Link, {
+    shouldForwardProp: prop => prop !== 'highlighted',
+})<{ highlighted?: boolean }>(({ theme, highlighted }) => ({
+    backgroundColor: highlighted ? theme.palette.highlight : 'transparent',
+}));
+
+interface IFeatureTagCellProps {
+    row: {
+        original: FeatureSchema;
+    };
+    value: string;
+}
+
+export const FeatureTagCell: VFC<IFeatureTagCellProps> = ({ row, value }) => {
+    const { searchQuery } = useSearchHighlightContext();
+
+    if (row.original.tags?.length === 0) return <TextCell />;
+
+    return (
+        <TextCell>
+            <HtmlTooltip
+                title={
+                    <>
+                        {row.original.tags?.map(tag => (
+                            <StyledTag key={tag.type + tag.value}>
+                                <Highlighter search={searchQuery}>
+                                    {`${tag.type}:${tag.value}`}
+                                </Highlighter>
+                            </StyledTag>
+                        ))}
+                    </>
+                }
+            >
+                <StyledLink
+                    underline="always"
+                    highlighted={
+                        searchQuery.length > 0 && value.includes(searchQuery)
+                    }
+                >
+                    {row.original.tags?.length === 1
+                        ? '1 tag'
+                        : `${row.original.tags?.length} tags`}
+                </StyledLink>
+            </HtmlTooltip>
+        </TextCell>
+    );
+};
--- frontend/src/component/feature/FeatureToggleList/FeatureToggleListTable.tsx
+++ frontend/src/component/feature/FeatureToggleList/FeatureToggleListTable.tsx
@@ -20,6 +20,7 @@ import { CreateFeatureButton } from '../CreateFeatureButton/CreateFeatureButton'
 import { FeatureStaleCell } from './FeatureStaleCell/FeatureStaleCell';
 import { useSearch } from 'hooks/useSearch';
 import { Search } from 'component/common/Search/Search';
+import { FeatureTagCell } from 'component/common/Table/cells/FeatureTagCell/FeatureTagCell';
 
 export const featuresPlaceholder: FeatureSchema[] = Array(15).fill({
     name: 'Name of the feature',
@@ -73,6 +74,15 @@ const columns = [
         sortType: 'alphanumeric',
         maxWidth: 150,
         filterName: 'project',
+        searchable: true,
+    },
+    {
+        id: 'tags',
+        Header: 'Tags',
+        accessor: (row: FeatureSchema) =>
+            row.tags?.map(({ type, value }) => `${type}:${value}`).join(', ') ||
+            '',
+        Cell: FeatureTagCell,
         searchable: true,
     },
     {
@@ -139,7 +149,7 @@ export const FeatureToggleListTable: VFC = () => {
         setHiddenColumns,
     } = useTable(
         {
-            columns,
+            columns: columns as any[],
             data,
             initialState,
             sortTypes,
@@ -153,14 +163,17 @@ export const FeatureToggleListTable: VFC = () => {
 
     useEffect(() => {
         const hiddenColumns = ['description'];
+        if (!features.some(({ tags }) => tags?.length)) {
+            hiddenColumns.push('tags');
+        }
         if (isMediumScreen) {
             hiddenColumns.push('lastSeenAt', 'stale');
         }
         if (isSmallScreen) {
-            hiddenColumns.push('type', 'createdAt');
+            hiddenColumns.push('type', 'createdAt', 'tags');
         }
         setHiddenColumns(hiddenColumns);
-    }, [setHiddenColumns, isSmallScreen, isMediumScreen]);
+    }, [setHiddenColumns, isSmallScreen, isMediumScreen, features]);
 
     useEffect(() => {
         const tableState: PageQueryType = {};

af64618b5983e9a58eb994f8ab9717057a32b2e9
andreas-unleash <andreas@getunleash.ai>
2022-11-25 15:42:32 +0200

    test fix - state loop
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- frontend/src/component/App.tsx
+++ frontend/src/component/App.tsx
@@ -1,5 +1,5 @@
-import { Suspense, useEffect } from 'react';
-import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { Suspense, useEffect, useState } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Error } from 'component/layout/Error/Error';
 import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
@@ -20,29 +20,21 @@ import useUiConfig from 'hooks/api/getters/useUiConfig/useUiConfig';
 import useProjects from '../hooks/api/getters/useProjects/useProjects';
 import { useLastViewedProject } from '../hooks/useLastViewedProject';
 
-const MainApp = () => {
-    const { classes: styles } = useStyles();
-    const { isOss } = useUiConfig();
-
-    const availableRoutes = isOss()
-        ? routes.filter(route => !route.enterprise)
-        : routes;
-
+const InitialRedirect = () => {
     const { lastViewed } = useLastViewedProject();
-    const navigate = useNavigate();
     const { projects, loading } = useProjects();
+
+    const [redirectTo, setRedirectTo] = useState<string>('/projects');
 
     useEffect(() => {
         const redirect = () => {
-            let to = '/';
             if (projects && lastViewed) {
-                to = `/projects/${lastViewed}`;
+                setRedirectTo(`/projects/${lastViewed}`);
             }
 
             if (projects && !lastViewed && projects.length === 1) {
-                to = `/projects/${projects[0].id}`;
+                setRedirectTo(`/projects/${projects[0].id}`);
             }
-            navigate(to);
         };
 
         if (!loading) {
@@ -51,36 +43,20 @@ const MainApp = () => {
         //eslint-disable-next-line
     }, [loading]);
 
-    return (
-        <div className={styles.container}>
-            <ToastRenderer />
-            <Routes>
-                {availableRoutes.map(route => (
-                    <Route
-                        key={route.path}
-                        path={route.path}
-                        element={
-                            <LayoutPicker
-                                isStandalone={route.isStandalone === true}
-                            >
-                                <ProtectedRoute route={route} />
-                            </LayoutPicker>
-                        }
-                    />
-                ))}
-                <Route path="/" element={<Navigate to="/projects" replace />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-            <FeedbackNPS openUrl="http://feedback.unleash.run" />
-            <SplashPageRedirect />
-        </div>
-    );
+    return <Navigate to={redirectTo || '/'} />;
 };
 
 export const App = () => {
     const { authDetails } = useAuthDetails();
     const { user } = useAuthUser();
     const hasFetchedAuth = Boolean(authDetails || user);
+
+    const { classes: styles } = useStyles();
+    const { isOss } = useUiConfig();
+
+    const availableRoutes = isOss()
+        ? routes.filter(route => !route.enterprise)
+        : routes;
 
     return (
         <ErrorBoundary FallbackComponent={Error}>
@@ -90,7 +66,43 @@ export const App = () => {
                         <ConditionallyRender
                             condition={!hasFetchedAuth}
                             show={<Loader />}
-                            elseShow={<MainApp />}
+                            elseShow={
+                                <>
+                                    <div className={styles.container}>
+                                        <ToastRenderer />
+                                        <Routes>
+                                            {availableRoutes.map(route => (
+                                                <Route
+                                                    key={route.path}
+                                                    path={route.path}
+                                                    element={
+                                                        <LayoutPicker
+                                                            isStandalone={
+                                                                route.isStandalone ===
+                                                                true
+                                                            }
+                                                        >
+                                                            <ProtectedRoute
+                                                                route={route}
+                                                            />
+                                                        </LayoutPicker>
+                                                    }
+                                                />
+                                            ))}
+                                            <Route
+                                                path="/"
+                                                element={<InitialRedirect />}
+                                            />
+                                            <Route
+                                                path="*"
+                                                element={<NotFound />}
+                                            />
+                                        </Routes>
+                                        <FeedbackNPS openUrl="http://feedback.unleash.run" />
+                                        <SplashPageRedirect />
+                                    </div>
+                                </>
+                            }
                         />
                     </PlausibleProvider>
                 </Suspense>

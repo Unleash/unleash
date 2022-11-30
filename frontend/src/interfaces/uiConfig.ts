f661baa243297da5f6463c6ddc724223d0803246
Tymoteusz Czech <tymek+gpg@getunleash.ai>
2022-11-30 13:38:39 +0100

    fix: duplicate uiconfig favorites

--- frontend/src/interfaces/uiConfig.ts
+++ frontend/src/interfaces/uiConfig.ts
@@ -41,7 +41,6 @@ export interface IFlags {
     ENABLE_DARK_MODE_SUPPORT?: boolean;
     embedProxyFrontend?: boolean;
     syncSSOGroups?: boolean;
-    favorites?: boolean;
     changeRequests?: boolean;
     cloneEnvironment?: boolean;
     variantsPerEnvironment?: boolean;

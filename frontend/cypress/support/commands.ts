bf1aaf0461a420ea90e4920d8e699d0bfd495533
andreas-unleash <andreas@getunleash.ai>
2022-12-05 14:57:43 +0200

    revert
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -29,8 +29,8 @@ const AUTH_PASSWORD = Cypress.env('AUTH_PASSWORD');
 
 Cypress.Commands.add('login', (user = AUTH_USER, password = AUTH_PASSWORD) =>
     cy.session(user, () => {
-        cy.visit('/login');
-        cy.wait(1200);
+        cy.visit('/');
+        cy.wait(1500);
         cy.get("[data-testid='LOGIN_EMAIL_ID']").type(user);
 
         if (AUTH_PASSWORD) {

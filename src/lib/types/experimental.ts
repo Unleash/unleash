0186683bf19e1369c8f62855bfda3e9d76e6e2e8
Nuno Góis <github@nunogois.com>
2022-12-12 15:26:29 +0000

    refactor: DRYer approach

--- src/lib/types/experimental.ts
+++ src/lib/types/experimental.ts
@@ -1,73 +1,61 @@
 import { parseEnvVarBoolean } from '../util';
 
-export type IFlags = IExperimentalOptions['flags'];
+export type IFlags = Partial<typeof flags>;
 export type IFlagKey = keyof IFlags;
 
+const flags = {
+    E: false,
+    ENABLE_DARK_MODE_SUPPORT: false,
+    anonymiseEventLog: false,
+    embedProxy: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_EMBED_PROXY,
+        true,
+    ),
+    changeRequests: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_CHANGE_REQUESTS,
+        false,
+    ),
+    embedProxyFrontend: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_EMBED_PROXY_FRONTEND,
+        true,
+    ),
+    batchMetrics: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_BATCH_METRICS,
+        false,
+    ),
+    responseTimeWithAppName: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_RESPONSE_TIME_WITH_APP_NAME,
+        false,
+    ),
+    proxyReturnAllToggles: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_PROXY_RETURN_ALL_TOGGLES,
+        false,
+    ),
+    variantsPerEnvironment: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_VARIANTS_PER_ENVIRONMENT,
+        false,
+    ),
+    favorites: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_FAVORITES,
+        false,
+    ),
+    networkView: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_NETWORK_VIEW,
+        false,
+    ),
+    maintenance: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_MAINTENANCE,
+        false,
+    ),
+};
+
 export const defaultExperimentalOptions: IExperimentalOptions = {
-    flags: {
-        ENABLE_DARK_MODE_SUPPORT: false,
-        anonymiseEventLog: false,
-        embedProxy: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_EMBED_PROXY,
-            true,
-        ),
-        changeRequests: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_CHANGE_REQUESTS,
-            false,
-        ),
-        embedProxyFrontend: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_EMBED_PROXY_FRONTEND,
-            true,
-        ),
-        batchMetrics: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_BATCH_METRICS,
-            false,
-        ),
-        responseTimeWithAppName: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_RESPONSE_TIME_WITH_APP_NAME,
-            false,
-        ),
-        proxyReturnAllToggles: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_PROXY_RETURN_ALL_TOGGLES,
-            false,
-        ),
-        variantsPerEnvironment: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_VARIANTS_PER_ENVIRONMENT,
-            false,
-        ),
-        favorites: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_FAVORITES,
-            false,
-        ),
-        maintenance: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_MAINTENANCE,
-            false,
-        ),
-        networkView: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_NETWORK_VIEW,
-            false,
-        ),
-    },
+    flags,
     externalResolver: { isEnabled: (): boolean => false },
 };
 
 export interface IExperimentalOptions {
-    flags: {
-        E?: boolean;
-        ENABLE_DARK_MODE_SUPPORT?: boolean;
-        embedProxy?: boolean;
-        embedProxyFrontend?: boolean;
-        batchMetrics?: boolean;
-        anonymiseEventLog?: boolean;
-        changeRequests?: boolean;
-        responseTimeWithAppName?: boolean;
-        toggleTagFiltering?: boolean;
-        proxyReturnAllToggles?: boolean;
-        variantsPerEnvironment?: boolean;
-        favorites?: boolean;
-        networkView?: boolean;
-        maintenance?: boolean;
-    };
+    flags: IFlags;
     externalResolver: IExternalFlagResolver;
 }
 

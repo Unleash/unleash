883679d60f5f755d9c569627a02c8af7a7612b55
Ivar Conradi Østhus <ivar@getunleash.ai>
2022-12-14 17:35:22 +0100

    Fix/remove settings cache (#2694)
    In this PR we remove the general SettingService cache, as it will not
    work across multiple horizontal unleash instances, events are not
    published across.
    
    We also fix the CORS origin to: 
    - Access-Control-Allow-Origin set to "*" if no Origin is configured
    - Access-Control-Allow-Origin set to "*" if any Origin is configured to
    "*"
    - - Access-Control-Allow-Origin set to array and have the "cors"
    middleware to return an exact match on the user provided Origin.
    
    Co-authored-by: Fredrik Oseberg <fredrik.no@gmail.com>

--- frontend/src/component/admin/cors/CorsHelpAlert.tsx
+++ frontend/src/component/admin/cors/CorsHelpAlert.tsx
@@ -17,6 +17,15 @@ export const CorsHelpAlert = () => {
                 An asterisk (<code>*</code>) may be used to allow API calls from
                 any origin.
             </p>
+            <br />
+            <p>
+                Be aware that changes here will take up to two minutes to be
+                updated. In addition, there is a maxAge on the
+                Access-Control-Allow-Origin header that will instruct browsers
+                to cache this header for some time. The cache period is set to
+                the maxium that the browser allows (2h for Chrome, 24h for
+                Firefox).
+            </p>
         </Alert>
     );
 };

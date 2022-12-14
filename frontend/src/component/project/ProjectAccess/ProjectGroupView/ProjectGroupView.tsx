db7b39af2dfdd33c8aef62c3b44fb7c8ec4c7ff0
Nuno Góis <github@nunogois.com>
2022-12-14 15:22:38 +0000

    fix: upgrades useHiddenColumns hook to fix related issues (#2691)
    https://linear.app/unleash/issue/2-514/fix-issues-with-conditionally-hidden-table-columns
    
    This upgrades the old `useHiddenColumns` to a new
    `useConditionallyHiddenColumns`. This implementation covers some issues
    and edge cases, and should hopefully be the standard way of achieving
    responsive visibility for table columns from now on.
    
    Some of these issues included incorrectly showing/hiding table columns,
    whether when resizing the window or at page load, even when the proper
    conditions were met to toggle their visibility.
    
    This PR adapts the tables that were already using `useHiddenColumns` to
    use the new approach.
    I'll create a new PR after this one to adapt our other existing tables
    to use this new approach as well.

--- frontend/src/component/project/ProjectAccess/ProjectGroupView/ProjectGroupView.tsx
+++ frontend/src/component/project/ProjectAccess/ProjectGroupView/ProjectGroupView.tsx
@@ -19,7 +19,7 @@ import { IGroup, IGroupUser } from 'interfaces/group';
 import { VFC, useState } from 'react';
 import { SortingRule, useFlexLayout, useSortBy, useTable } from 'react-table';
 import { sortTypes } from 'utils/sortTypes';
-import useHiddenColumns from 'hooks/useHiddenColumns';
+import { useConditionallyHiddenColumns } from 'hooks/useConditionallyHiddenColumns';
 
 const StyledPageContent = styled(PageContent)(({ theme }) => ({
     height: '100vh',
@@ -154,7 +154,16 @@ export const ProjectGroupView: VFC<IProjectGroupViewProps> = ({
         useFlexLayout
     );
 
-    useHiddenColumns(setHiddenColumns, hiddenColumnsSmall, isSmallScreen);
+    useConditionallyHiddenColumns(
+        [
+            {
+                condition: isSmallScreen,
+                columns: hiddenColumnsSmall,
+            },
+        ],
+        setHiddenColumns,
+        columns
+    );
 
     return (
         <SidebarModal

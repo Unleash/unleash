7ce5b3de640c10ac69755d430e4de92ac8ee3192
Nuno Góis <github@nunogois.com>
2022-12-20 15:10:06 +0000

    refactor: flag resolver should use stricter types (#2571)
    Adding stricter types to `FlagResolver` can possibly help improve our DX
    - Help us prevent errors like typos, guide us to correctly add a flag
    when needed, and warn us of stray checks whenever we do a clean up at a
    later stage.

--- src/lib/types/experimental.ts
+++ src/lib/types/experimental.ts
@@ -1,70 +1,61 @@
 import { parseEnvVarBoolean } from '../util';
 
-export type IFlags = Partial<Record<string, boolean>>;
+export type IFlags = Partial<typeof flags>;
+export type IFlagKey = keyof IFlags;
 
-export const defaultExperimentalOptions = {
-    flags: {
-        ENABLE_DARK_MODE_SUPPORT: false,
-        anonymiseEventLog: false,
-        embedProxy: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_EMBED_PROXY,
-            true,
-        ),
-        changeRequests: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_CHANGE_REQUESTS,
-            false,
-        ),
-        embedProxyFrontend: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_EMBED_PROXY_FRONTEND,
-            true,
-        ),
-        batchMetrics: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_BATCH_METRICS,
-            false,
-        ),
-        responseTimeWithAppName: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_RESPONSE_TIME_WITH_APP_NAME,
-            false,
-        ),
-        proxyReturnAllToggles: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_PROXY_RETURN_ALL_TOGGLES,
-            false,
-        ),
-        variantsPerEnvironment: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_VARIANTS_PER_ENVIRONMENT,
-            false,
-        ),
-        favorites: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_FAVORITES,
-            false,
-        ),
-        maintenance: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_MAINTENANCE,
-            false,
-        ),
-        networkView: parseEnvVarBoolean(
-            process.env.UNLEASH_EXPERIMENTAL_NETWORK_VIEW,
-            false,
-        ),
-    },
+const flags = {
+    E: false,
+    ENABLE_DARK_MODE_SUPPORT: false,
+    anonymiseEventLog: false,
+    embedProxy: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_EMBED_PROXY,
+        true,
+    ),
+    changeRequests: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_CHANGE_REQUESTS,
+        false,
+    ),
+    embedProxyFrontend: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_EMBED_PROXY_FRONTEND,
+        true,
+    ),
+    batchMetrics: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_BATCH_METRICS,
+        false,
+    ),
+    responseTimeWithAppName: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_RESPONSE_TIME_WITH_APP_NAME,
+        false,
+    ),
+    proxyReturnAllToggles: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_PROXY_RETURN_ALL_TOGGLES,
+        false,
+    ),
+    variantsPerEnvironment: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_VARIANTS_PER_ENVIRONMENT,
+        false,
+    ),
+    favorites: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_FAVORITES,
+        false,
+    ),
+    networkView: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_NETWORK_VIEW,
+        false,
+    ),
+    maintenance: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_MAINTENANCE,
+        false,
+    ),
+};
+
+export const defaultExperimentalOptions: IExperimentalOptions = {
+    flags,
     externalResolver: { isEnabled: (): boolean => false },
 };
 
 export interface IExperimentalOptions {
-    flags: {
-        [key: string]: boolean;
-        ENABLE_DARK_MODE_SUPPORT?: boolean;
-        embedProxy?: boolean;
-        embedProxyFrontend?: boolean;
-        batchMetrics?: boolean;
-        anonymiseEventLog?: boolean;
-        changeRequests?: boolean;
-        proxyReturnAllToggles?: boolean;
-        variantsPerEnvironment?: boolean;
-        favorites?: boolean;
-        networkView?: boolean;
-        maintenance?: boolean;
-    };
+    flags: IFlags;
     externalResolver: IExternalFlagResolver;
 }
 
@@ -74,9 +65,9 @@ export interface IFlagContext {
 
 export interface IFlagResolver {
     getAll: (context?: IFlagContext) => IFlags;
-    isEnabled: (expName: string, context?: IFlagContext) => boolean;
+    isEnabled: (expName: IFlagKey, context?: IFlagContext) => boolean;
 }
 
 export interface IExternalFlagResolver {
-    isEnabled: (flagName: string, context?: IFlagContext) => boolean;
+    isEnabled: (flagName: IFlagKey, context?: IFlagContext) => boolean;
 }

cbe62d5777f318e4d8bc3cf3f5f35e5cd9010d93
Nuno Góis <github@nunogois.com>
2022-12-20 16:44:57 +0000

    feat: allows creation of PATs for other users

---
+++ src/migrations/20221220160345-user-pat-permissions.js
@@ -0,0 +1,33 @@
+exports.up = function (db, cb) {
+    db.runSql(
+        `
+        INSERT INTO permissions (permission, display_name, type) VALUES ('READ_USER_PAT', 'Read PATs for a specific user', 'root');
+        INSERT INTO permissions (permission, display_name, type) VALUES ('CREATE_USER_PAT', 'Create a PAT for a specific user', 'root');
+        INSERT INTO permissions (permission, display_name, type) VALUES ('DELETE_USER_PAT', 'Delete a PAT for a specific user', 'root');
+
+        INSERT INTO role_permission (role_id, permission_id)
+        SELECT
+            r.id AS role_id,
+            p.id AS permission_id
+        FROM roles r
+        JOIN permissions p ON p.permission = 'READ_USER_PAT' OR p.permission = 'CREATE_USER_PAT' OR p.permission = 'DELETE_USER_PAT'
+        WHERE r.name IN ('Admin');
+        `,
+        cb,
+    );
+};
+
+exports.down = function (db, cb) {
+    db.runSql(
+        `
+        DELETE FROM role_permission WHERE permission_id IN (
+            SELECT id FROM permissions WHERE permission = 'READ_USER_PAT' OR permission = 'CREATE_USER_PAT' OR permission = 'DELETE_USER_PAT'
+        );
+
+        DELETE FROM permissions WHERE permission = 'READ_USER_PAT';
+        DELETE FROM permissions WHERE permission = 'CREATE_USER_PAT';
+        DELETE FROM permissions WHERE permission = 'DELETE_USER_PAT';
+  `,
+        cb,
+    );
+};

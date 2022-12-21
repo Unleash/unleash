7bda724501993dfd770a88689493b5ff70d13eee
Gastón Fournier <gaston@getunleash.ai>
2022-12-21 17:14:22 +0100

    Draft idea

--- src/lib/middleware/response-time-metrics.ts
+++ src/lib/middleware/response-time-metrics.ts
@@ -2,21 +2,36 @@ import * as responseTime from 'response-time';
 import EventEmitter from 'events';
 import { REQUEST_TIME } from '../metric-events';
 import { IFlagResolver } from '../types/experimental';
+import { InstanceStatsService } from 'lib/services';
+import memoizee from 'memoizee';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const _responseTime = responseTime.default;
 
+const countAppNames = async (instanceStatsService: InstanceStatsService) => {
+    const response = await instanceStatsService.getLabeledAppCounts();
+    return response.find((c) => c.range === '7d').count;
+};
 export function responseTimeMetrics(
     eventBus: EventEmitter,
     flagResolver: IFlagResolver,
+    instanceStatsService: InstanceStatsService,
 ): any {
-    return _responseTime((req, res, time) => {
+    const appNameReportingThreshold = 100;
+    let appNameCount = memoizee(() => countAppNames(instanceStatsService), {
+        promise: true,
+        maxAge: 30000, // milliseconds
+    });
+    return _responseTime(async (req, res, time) => {
         const { statusCode } = res;
 
         const pathname = req.route ? req.baseUrl + req.route.path : '(hidden)';
 
         let appName;
-        if (flagResolver.isEnabled('responseTimeWithAppName')) {
+        if (
+            flagResolver.isEnabled('responseTimeWithAppName') &&
+            (await appNameCount()) < appNameReportingThreshold
+        ) {
             appName = req.headers['unleash-appname'] ?? req.query.appName;
         }
 

a92e20760d07e1803b00dc2ec2710308ba230e61
Nuno Góis <github@nunogois.com>
2022-12-23 08:51:56 +0000

    feat: service accounts (BE)

--- src/lib/db/user-store.ts
+++ src/lib/db/user-store.ts
@@ -50,6 +50,14 @@ const mapUserToColumns = (user: ICreateUser) => ({
     image_url: user.imageUrl,
 });
 
+const mapServiceAccountToColumns = (user: ICreateUser) => ({
+    name: user.name,
+    username: user.username,
+    email: safeToLower(user.email),
+    image_url: user.imageUrl,
+    is_service: true,
+});
+
 const rowToUser = (row) => {
     if (!row) {
         throw new NotFoundError('No user found');
@@ -86,6 +94,13 @@ class UserStore implements IUserStore {
     async insert(user: ICreateUser): Promise<User> {
         const rows = await this.db(TABLE)
             .insert(mapUserToColumns(user))
+            .returning(USER_COLUMNS);
+        return rowToUser(rows[0]);
+    }
+
+    async insertServiceAccount(serviceAccount: ICreateUser): Promise<User> {
+        const rows = await this.db(TABLE)
+            .insert(mapServiceAccountToColumns(serviceAccount))
             .returning(USER_COLUMNS);
         return rowToUser(rows[0]);
     }
@@ -135,6 +150,20 @@ class UserStore implements IUserStore {
     async getAll(): Promise<User[]> {
         const users = await this.activeUsers().select(USER_COLUMNS);
         return users.map(rowToUser);
+    }
+
+    async getAllUsers(): Promise<User[]> {
+        const users = await this.activeUsers()
+            .where('is_service', false)
+            .select(USER_COLUMNS);
+        return users.map(rowToUser);
+    }
+
+    async getAllServiceAccounts(): Promise<User[]> {
+        const serviceAccounts = await this.activeUsers()
+            .where('is_service', true)
+            .select(USER_COLUMNS);
+        return serviceAccounts.map(rowToUser);
     }
 
     async search(query: string): Promise<User[]> {

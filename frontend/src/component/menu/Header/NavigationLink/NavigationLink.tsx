db5b73aea8c9df65866fa17c0b59aff267c70dfd
Fredrik Oseberg <fredrik.no@gmail.com>
2023-01-02 12:37:37 +0100

    refactor: initial styled componentes for NavigationLink

--- frontend/src/component/menu/Header/NavigationLink/NavigationLink.tsx
+++ frontend/src/component/menu/Header/NavigationLink/NavigationLink.tsx
@@ -1,5 +1,6 @@
 import { ListItem, Link } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
+import { styled } from '@mui/system';
 
 import { useStyles } from './NavigationLink.styles';
 
@@ -9,27 +10,56 @@ interface INavigationLinkProps {
     handleClose: () => void;
 }
 
+const StyledListItem = styled(ListItem)({
+    minWidth: '150px',
+    height: '100%',
+    width: '100%',
+    margin: '0',
+    padding: '0',
+});
+
+const StyledLink = styled(RouterLink)(({ theme }) => ({
+    textDecoration: 'none',
+    alignItems: 'center',
+    display: 'flex',
+    color: 'inherit',
+    height: '100%',
+    width: '100%',
+    '&&': {
+        // Override MenuItem's built-in padding.
+        padding: '0.5rem 1rem',
+    },
+}));
+
+const StyledSpan = styled('span')(({ theme }) => ({
+    width: '12.5px',
+    height: '12.5px',
+    display: 'block',
+    backgroundColor: theme.palette.primary.main,
+    marginRight: '1rem',
+    borderRadius: '2px',
+}));
+
 const NavigationLink = ({ path, text, handleClose }: INavigationLinkProps) => {
     const { classes: styles } = useStyles();
 
     return (
-        <ListItem
-            className={styles.menuItem}
+        <StyledListItem
             onClick={() => {
                 handleClose();
             }}
         >
             <Link
                 style={{ textDecoration: 'none' }}
-                component={RouterLink}
+                component={StyledLink}
                 className={styles.navMenuLink}
                 to={path}
                 underline="hover"
             >
-                <span className={styles.menuItemBox} />
+                <StyledSpan />
                 {text}
             </Link>
-        </ListItem>
+        </StyledListItem>
     );
 };
 

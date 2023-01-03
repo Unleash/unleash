cc1512cd4410a08312700b9088687438e06797ad
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2023-01-03 14:52:10 +0100

    refactor: styled component in header (#2808)

--- frontend/src/themes/themeStyles.ts
+++ frontend/src/themes/themeStyles.ts
@@ -1,5 +1,25 @@
 import { makeStyles } from 'tss-react/mui';
+import { Theme } from '@mui/material';
 
+export const focusable = (theme: Theme) => ({
+    '&:focus-visible': {
+        outline: 0,
+        outlineStyle: 'solid',
+        outlineWidth: 2,
+        outlineOffset: 2,
+        outlineColor: theme.palette.primary.main,
+    },
+});
+
+export const flexRow = {
+    display: 'flex',
+    alignItems: 'center',
+};
+
+/**
+ * Please extract styles below into MUI fragments as shown above
+ * @deprecated
+ */
 export const useThemeStyles = makeStyles()(theme => ({
     focusable: {
         '&:focus-visible': {

863a5db970c59005bc4119815d986606743d5134
Tymoteusz Czech <tymek+gpg@getunleash.ai>
2023-01-05 14:43:03 +0100

    refactor: sortable table header styles

--- frontend/src/component/common/Table/SortableTableHeader/SortableTableHeader.tsx
+++ frontend/src/component/common/Table/SortableTableHeader/SortableTableHeader.tsx
@@ -1,7 +1,6 @@
 import { VFC } from 'react';
-import { TableHead, TableRow } from '@mui/material';
+import { styled, TableHead, TableRow } from '@mui/material';
 import { HeaderGroup } from 'react-table';
-import { useStyles } from './SortableTableHeader.styles';
 import { CellSortable } from './CellSortable/CellSortable';
 
 interface ISortableTableHeaderProps {
@@ -10,52 +9,62 @@ interface ISortableTableHeaderProps {
     flex?: boolean;
 }
 
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '& > th': {
+        height: theme.shape.tableRowHeightCompact,
+        border: 0,
+        backgroundColor: theme.palette.tableHeaderBackground,
+        color: theme.palette.tableHeaderColor,
+        '&:first-of-type': {
+            borderTopLeftRadius: theme.shape.borderRadiusMedium,
+            borderBottomLeftRadius: theme.shape.borderRadiusMedium,
+        },
+        '&:last-of-type': {
+            borderTopRightRadius: theme.shape.borderRadiusMedium,
+            borderBottomRightRadius: theme.shape.borderRadiusMedium,
+        },
+    },
+}));
+
 export const SortableTableHeader: VFC<ISortableTableHeaderProps> = ({
     headerGroups,
     className,
     flex,
-}) => {
-    const { classes: styles } = useStyles();
+}) => (
+    <TableHead className={className}>
+        {headerGroups.map(headerGroup => (
+            <StyledTableRow {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column: HeaderGroup) => {
+                    const content = column.render('Header');
 
-    return (
-        <TableHead className={className}>
-            {headerGroups.map(headerGroup => (
-                <TableRow
-                    {...headerGroup.getHeaderGroupProps()}
-                    className={styles.tableHeader}
-                >
-                    {headerGroup.headers.map((column: HeaderGroup) => {
-                        const content = column.render('Header');
-
-                        return (
-                            <CellSortable
-                                {...column.getHeaderProps(
-                                    column.canSort
-                                        ? column.getSortByToggleProps()
-                                        : undefined
-                                )}
-                                ariaTitle={
-                                    typeof content === 'string'
-                                        ? content
-                                        : undefined
-                                }
-                                isSortable={Boolean(column.canSort)}
-                                isSorted={column.isSorted}
-                                isDescending={column.isSortedDesc}
-                                maxWidth={column.maxWidth}
-                                minWidth={column.minWidth}
-                                width={column.width}
-                                isFlex={flex}
-                                isFlexGrow={Boolean(column.minWidth)}
-                                // @ts-expect-error -- check after `react-table` v8
-                                align={column.align}
-                            >
-                                {content}
-                            </CellSortable>
-                        );
-                    })}
-                </TableRow>
-            ))}
-        </TableHead>
-    );
-};
+                    return (
+                        <CellSortable
+                            {...column.getHeaderProps(
+                                column.canSort
+                                    ? column.getSortByToggleProps()
+                                    : undefined
+                            )}
+                            ariaTitle={
+                                typeof content === 'string'
+                                    ? content
+                                    : undefined
+                            }
+                            isSortable={Boolean(column.canSort)}
+                            isSorted={column.isSorted}
+                            isDescending={column.isSortedDesc}
+                            maxWidth={column.maxWidth}
+                            minWidth={column.minWidth}
+                            width={column.width}
+                            isFlex={flex}
+                            isFlexGrow={Boolean(column.minWidth)}
+                            // @ts-expect-error -- check after `react-table` v8
+                            align={column.align}
+                        >
+                            {content}
+                        </CellSortable>
+                    );
+                })}
+            </StyledTableRow>
+        ))}
+    </TableHead>
+);

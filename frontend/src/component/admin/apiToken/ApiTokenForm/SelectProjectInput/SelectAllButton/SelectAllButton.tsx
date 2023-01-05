3759a8a0476739ce6ed864f1695764c1de69f885
andreas-unleash <andreas@getunleash.ai>
2023-01-05 12:30:46 +0200

    MakeStyles refactor 1-2
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- frontend/src/component/admin/apiToken/ApiTokenForm/SelectProjectInput/SelectAllButton/SelectAllButton.tsx
+++ frontend/src/component/admin/apiToken/ApiTokenForm/SelectProjectInput/SelectAllButton/SelectAllButton.tsx
@@ -1,28 +1,25 @@
 import React, { FC } from 'react';
-import { Box, Link } from '@mui/material';
-import { useStyles } from './SelectAllButton.styles';
+import { Box, Link, styled } from '@mui/material';
 
 type SelectAllButtonProps = {
     isAllSelected: boolean;
     onClick: () => void;
 };
 
+const StyledLink = styled(Link)(({ theme }) => ({
+    cursor: 'pointer',
+    fontSize: theme.fontSizes.bodySize,
+})) as typeof Link;
+
 export const SelectAllButton: FC<SelectAllButtonProps> = ({
     isAllSelected,
     onClick,
 }) => {
-    const { classes: styles } = useStyles();
-
     return (
         <Box sx={{ ml: 3.5, my: 0.5 }}>
-            <Link
-                onClick={onClick}
-                className={styles.selectOptionsLink}
-                component="button"
-                underline="hover"
-            >
+            <StyledLink onClick={onClick} component="button" underline="hover">
                 {isAllSelected ? 'Deselect all' : 'Select all'}
-            </Link>
+            </StyledLink>
         </Box>
     );
 };

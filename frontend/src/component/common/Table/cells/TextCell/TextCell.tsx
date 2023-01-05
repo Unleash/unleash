6f051227c5bd8386146bb13b2c0b3d21c98aa8be
Tymoteusz Czech <tymek+gpg@getunleash.ai>
2023-01-05 14:21:42 +0100

    refactor: text cell styles

--- frontend/src/component/common/Table/cells/TextCell/TextCell.tsx
+++ frontend/src/component/common/Table/cells/TextCell/TextCell.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
-import { Box, SxProps, Theme } from '@mui/material';
-import { useStyles } from './TextCell.styles';
+import { Box, styled, SxProps, Theme } from '@mui/material';
 
 interface ITextCellProps {
     value?: string | null;
@@ -9,20 +8,30 @@ interface ITextCellProps {
     sx?: SxProps<Theme>;
 }
 
+const StyledWrapper = styled(Box, {
+    shouldForwardProp: prop => prop !== 'lineClamp',
+})<{ lineClamp?: number }>(({ theme, lineClamp }) => ({
+    padding: theme.spacing(1, 2),
+    display: '-webkit-box',
+    overflow: lineClamp ? 'hidden' : 'auto',
+    WebkitLineClamp: lineClamp ? lineClamp : 'none',
+    WebkitBoxOrient: 'vertical',
+    wordBreak: 'break-all',
+    [theme.breakpoints.down('sm')]: {
+        wordBreak: 'normal',
+    },
+}));
+
 export const TextCell: FC<ITextCellProps> = ({
     value,
     children,
     lineClamp,
     sx,
     'data-testid': testid,
-}) => {
-    const { classes } = useStyles({ lineClamp });
-
-    return (
-        <Box className={classes.wrapper} sx={sx}>
-            <span data-loading="true" data-testid={testid}>
-                {children ?? value}
-            </span>
-        </Box>
-    );
-};
+}) => (
+    <StyledWrapper lineClamp={lineClamp} sx={sx}>
+        <span data-loading="true" data-testid={testid}>
+            {children ?? value}
+        </span>
+    </StyledWrapper>
+);

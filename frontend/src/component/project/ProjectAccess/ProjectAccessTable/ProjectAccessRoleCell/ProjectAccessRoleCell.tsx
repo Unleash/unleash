7619255785b04cdf9c89a62591702c170de3dff1
Nuno Góis <github@nunogois.com>
2023-01-12 17:40:20 +0000

    feat: adapt project access role, improve role description

--- frontend/src/component/project/ProjectAccess/ProjectAccessTable/ProjectAccessRoleCell/ProjectAccessRoleCell.tsx
+++ frontend/src/component/project/ProjectAccess/ProjectAccessTable/ProjectAccessRoleCell/ProjectAccessRoleCell.tsx
@@ -1,19 +1,7 @@
-import { Link, Popover, styled } from '@mui/material';
 import { TextCell } from 'component/common/Table/cells/TextCell/TextCell';
-import React from 'react';
 import { VFC } from 'react';
 import { ProjectRoleDescription } from 'component/project/ProjectAccess/ProjectAccessAssign/ProjectRoleDescription/ProjectRoleDescription';
-
-const StyledLink = styled(Link)(() => ({
-    textDecoration: 'none',
-    '&:hover, &:focus': {
-        textDecoration: 'underline',
-    },
-}));
-
-const StyledPopover = styled(Popover)(() => ({
-    pointerEvents: 'none',
-}));
+import { TooltipLink } from 'component/common/TooltipLink/TooltipLink';
 
 interface IProjectAccessRoleCellProps {
     roleId: number;
@@ -28,49 +16,25 @@ export const ProjectAccessRoleCell: VFC<IProjectAccessRoleCellProps> = ({
     value,
     emptyText,
 }) => {
-    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
-
-    const onPopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const onPopoverClose = () => {
-        setAnchorEl(null);
-    };
-
     if (!value) return <TextCell>{emptyText}</TextCell>;
 
     return (
-        <>
-            <TextCell>
-                <StyledLink
-                    onMouseEnter={event => {
-                        onPopoverOpen(event);
-                    }}
-                    onMouseLeave={onPopoverClose}
-                >
-                    {value}
-                </StyledLink>
-            </TextCell>
-            <StyledPopover
-                open={Boolean(anchorEl)}
-                anchorEl={anchorEl}
-                onClose={onPopoverClose}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
+        <TextCell>
+            <TooltipLink
+                tooltip={
+                    <ProjectRoleDescription
+                        roleId={roleId}
+                        projectId={projectId}
+                        popover
+                    />
+                }
+                tooltipProps={{
+                    maxWidth: 500,
+                    maxHeight: 600,
                 }}
             >
-                <ProjectRoleDescription
-                    roleId={roleId}
-                    projectId={projectId}
-                    popover
-                />
-            </StyledPopover>
-        </>
+                {value}
+            </TooltipLink>
+        </TextCell>
     );
 };

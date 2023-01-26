20c0facd49a4d1841c7f192f3965d9dc45fb974a
Tymoteusz Czech <tymek+gpg@getunleash.ai>
2023-01-26 18:04:55 +0100

    feat: new project health widget
    Test Linear integration:
    
    Close 1-605
    Close ID-1-605
    Close ID1-605

--- frontend/src/component/project/Project/ProjectInfo/HealthWidget.tsx
+++ frontend/src/component/project/Project/ProjectInfo/HealthWidget.tsx
@@ -1,5 +1,6 @@
 import {
     StyledArrowIcon,
+    StyledCount,
     StyledDivInfoContainer,
     StyledDivPercentageContainer,
     StyledLink,
@@ -8,12 +9,64 @@ import {
     StyledSpanLinkText,
 } from './ProjectInfo.styles';
 import PercentageCircle from 'component/common/PercentageCircle/PercentageCircle';
+import useUiConfig from 'hooks/api/getters/useUiConfig/useUiConfig';
+import { Box, styled, Typography } from '@mui/material';
 
 interface IHealthWidgetProps {
     projectId: string;
     health: number;
+    total?: number;
+    stale?: number;
 }
-export const HealthWidget = ({ projectId, health }: IHealthWidgetProps) => {
+
+const StyledWarning = styled('span')<{ active?: boolean }>(
+    ({ theme, active }) => ({
+        color: active ? theme.palette.warning.dark : 'inherit',
+    })
+);
+
+export const HealthWidget = ({
+    projectId,
+    health,
+    total,
+    stale,
+}: IHealthWidgetProps) => {
+    const { uiConfig } = useUiConfig();
+
+    if (uiConfig?.flags?.newProjectOverview) {
+        return (
+            <StyledDivInfoContainer>
+                <StyledParagraphSubtitle data-loading>
+                    Project health
+                </StyledParagraphSubtitle>
+                <StyledDivPercentageContainer>
+                    <PercentageCircle percentage={health} />
+                </StyledDivPercentageContainer>
+                <StyledParagraphEmphasizedText data-loading>
+                    {health}%
+                </StyledParagraphEmphasizedText>
+                <Typography data-loading>
+                    <StyledCount>{total}</StyledCount> toggles in total
+                </Typography>
+                <Typography data-loading sx={{ marginBottom: 2 }}>
+                    <StyledCount>
+                        <StyledWarning active={Boolean(stale)}>
+                            {stale}
+                        </StyledWarning>
+                    </StyledCount>{' '}
+                    <StyledWarning active={Boolean(stale)}>
+                        potentially stale
+                    </StyledWarning>
+                </Typography>
+                <StyledLink data-loading to={`/projects/${projectId}/health`}>
+                    <StyledSpanLinkText data-loading>
+                        View project health
+                    </StyledSpanLinkText>
+                </StyledLink>
+            </StyledDivInfoContainer>
+        );
+    }
+
     return (
         <StyledDivInfoContainer>
             <StyledDivPercentageContainer>

1fcfeda11c9282b8466bc142009c92d75acf262c
andreas-unleash <andreas@getunleash.ai>
2023-02-08 11:43:28 +0200

    add project api token permissions
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

---
+++ src/migrations/20230208091810-assign-project-api-token-permissions_admin.js
@@ -0,0 +1,26 @@
+exports.up = function (db, cb) {
+    db.runSql(
+        `
+            INSERT INTO role_permission (role_id, permission_id, environment)
+            SELECT (SELECT id as role_id from roles WHERE name = 'Admin' LIMIT 1),
+                   p.id   as permission_id,
+                   e.name as environment
+            FROM permissions p
+                     CROSS JOIN environments e
+            WHERE p.permission IN
+                  ('READ_PROJECT_API_TOKEN',
+                   'CREATE_PROJECT_API_TOKEN',
+                   'DELETE_PROJECT_API_TOKEN');
+            \`,
+        `,
+        cb,
+    );
+};
+
+exports.down = function (db, cb) {
+    db.runSql(
+        `
+  `,
+        cb,
+    );
+};

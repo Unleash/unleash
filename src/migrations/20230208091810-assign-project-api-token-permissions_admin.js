fd1fb8e208aff716bdfdacd76f99a7cb4027b210
andreas-unleash <andreas@getunleash.ai>
2023-02-08 12:42:48 +0200

    bug fix
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- src/migrations/20230208091810-assign-project-api-token-permissions_admin.js
+++ src/migrations/20230208091810-assign-project-api-token-permissions_admin.js
@@ -3,7 +3,7 @@ exports.up = function (db, cb) {
         `
             INSERT INTO role_permission (role_id, permission_id)
             SELECT (SELECT id as role_id from roles WHERE name = 'Admin' LIMIT 1),
-                   p.id   as permission_id,
+                   p.id  as permission_id
             FROM permissions p
             WHERE p.permission IN
                   ('READ_PROJECT_API_TOKEN',

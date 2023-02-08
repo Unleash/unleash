07e8a351bb4379886af403aa4048a68d838afe3a
Ivar Conradi Østhus <ivar@getunleash.ai>
2023-02-08 19:57:21 +0100

    Fix/show tag events (#3064)
    This PR fixes two issues with events today:
    
    1. Feature toggles "Event log" must include all events, regardless of
    the project. This is important as feature toggles may move between
    2. Add/remove tags on a feature toggle events should include project id
    in order to show up in the project specific event log.

--- frontend/src/component/feature/FeatureView/FeatureLog/FeatureLog.tsx
+++ frontend/src/component/feature/FeatureView/FeatureLog/FeatureLog.tsx
@@ -20,12 +20,7 @@ const FeatureLog = () => {
 
     return (
         <StyledContainer>
-            <EventLog
-                title="Event log"
-                project={projectId}
-                feature={featureId}
-                displayInline
-            />
+            <EventLog title="Event log" feature={featureId} displayInline />
         </StyledContainer>
     );
 };

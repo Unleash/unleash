d1572fc9b7697cbc90469ab2a6e4b03e74a5bb7b
kwasniew <kwasniewski.mateusz@gmail.com>
2023-02-10 10:40:35 +0100

    added examples and descriptions

--- src/lib/openapi/spec/feature-schema.ts
+++ src/lib/openapi/spec/feature-schema.ts
@@ -15,58 +15,87 @@ export const featureSchema = {
     properties: {
         name: {
             type: 'string',
+            example: 'disable-comments',
+            description: 'Unique feature name',
         },
         type: {
             type: 'string',
+            example: 'kill-switch',
+            description:
+                'Type of the toggle e.g. experiment, kill-switch, release, operational, permission',
         },
         description: {
             type: 'string',
             nullable: true,
+            example:
+                'Controls disabling of the comments section in case of an incident',
+            description: 'Detailed description of the feature',
         },
         archived: {
             type: 'boolean',
+            example: true,
+            description: 'Is this feature archived',
         },
         project: {
             type: 'string',
+            example: 'dx-squad',
+            description: 'Name of the project the feature belongs to',
         },
         enabled: {
             type: 'boolean',
+            example: true,
         },
         stale: {
             type: 'boolean',
+            example: false,
+            description:
+                'Is the feature considered stale depending on its age and feature type',
         },
         favorite: {
             type: 'boolean',
+            example: true,
+            description: 'Is this feature added to your favourite list',
         },
         impressionData: {
             type: 'boolean',
+            example: false,
+            description:
+                'Is impression data collection enabled for this feature',
         },
         createdAt: {
             type: 'string',
             format: 'date-time',
             nullable: true,
+            example: '2023-01-28T15:21:39.975Z',
+            description: 'When was the feature created',
         },
         archivedAt: {
             type: 'string',
             format: 'date-time',
             nullable: true,
+            example: '2023-01-29T15:21:39.975Z',
+            description: 'When was the feature archived',
         },
         lastSeenAt: {
             type: 'string',
             format: 'date-time',
             nullable: true,
+            example: '2023-01-28T16:21:39.975Z',
+            description: 'When was the feature last seen',
         },
         environments: {
             type: 'array',
             items: {
                 $ref: '#/components/schemas/featureEnvironmentSchema',
             },
+            description: 'List of environments where the feature can be used',
         },
         variants: {
             type: 'array',
             items: {
                 $ref: '#/components/schemas/variantSchema',
             },
+            description: 'List of feature variants',
         },
         tags: {
             type: 'array',
@@ -74,6 +103,7 @@ export const featureSchema = {
                 $ref: '#/components/schemas/tagSchema',
             },
             nullable: true,
+            description: 'List of feature tags',
         },
     },
     components: {

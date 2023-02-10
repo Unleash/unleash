465ca9a26a6bf7ac124fb0637d8f97abeb8a9af0
kwasniew <kwasniewski.mateusz@gmail.com>
2023-02-10 10:28:03 +0100

    added examples and descriptions

--- src/lib/openapi/spec/project-overview-schema.ts
+++ src/lib/openapi/spec/project-overview-schema.ts
@@ -17,42 +17,60 @@ export const projectOverviewSchema = {
     properties: {
         stats: {
             $ref: '#/components/schemas/projectStatsSchema',
+            description: 'Project statistics',
         },
         version: {
             type: 'number',
+            example: 1,
+            description: '',
         },
         name: {
             type: 'string',
+            example: 'dx-squad',
+            description: 'Name of this project used to identify it',
         },
         description: {
             type: 'string',
             nullable: true,
+            example: 'DX squad feature release',
+            description: 'Additional information about the project',
         },
         members: {
             type: 'number',
+            example: 4,
+            description: 'Count of the project members',
         },
         health: {
             type: 'number',
+            example: 50,
+            description: 'Indicator of the project health in the 0-100 scale',
         },
         environments: {
             type: 'array',
             items: {
                 type: 'string',
             },
+            example: ['development', 'production'],
+            description: 'Enabled environments for this project',
         },
         features: {
             type: 'array',
             items: {
                 $ref: '#/components/schemas/featureSchema',
             },
+            description: 'List of features in this project',
         },
         updatedAt: {
             type: 'string',
             format: 'date-time',
             nullable: true,
+            example: '2023-02-10T08:36:35.262Z',
+            description: 'Last modification data of this project',
         },
         favorite: {
             type: 'boolean',
+            example: true,
+            description: 'Is this project added to your favourite list',
         },
     },
     components: {
--- src/lib/openapi/spec/project-stats-schema.ts
+++ src/lib/openapi/spec/project-stats-schema.ts
@@ -18,30 +18,57 @@ export const projectStatsSchema = {
     properties: {
         avgTimeToProdCurrentWindow: {
             type: 'number',
+            example: 10,
+            description:
+                'Average time to production for features in the current window (last 30 days)',
         },
         avgTimeToProdPastWindow: {
             type: 'number',
+            example: 10,
+            description:
+                'Average time to production for features in the past window (previous 30 days before current window)',
         },
         createdCurrentWindow: {
             type: 'number',
+            example: 15,
+            description:
+                'Number of feature toggles created in the current window (last 30 days)',
         },
         createdPastWindow: {
             type: 'number',
+            example: 15,
+            description:
+                'Number of feature toggles created in the past window (previous 30 days before current window)',
         },
         archivedCurrentWindow: {
             type: 'number',
+            example: 5,
+            description:
+                'Number of feature toggles archived in the current window (last 30 days)',
         },
         archivedPastWindow: {
             type: 'number',
+            example: 5,
+            description:
+                'Number of feature toggles archived in the past window (previous 30 days before current window)',
         },
         projectActivityCurrentWindow: {
             type: 'number',
+            example: 100,
+            description:
+                'Number of event from the event log in the current window (last 30 days)',
         },
         projectActivityPastWindow: {
             type: 'number',
+            example: 100,
+            description:
+                'Number of event from the event log in the past window (previous 30 days before current window)',
         },
         projectMembersAddedCurrentWindow: {
             type: 'number',
+            example: 1,
+            description:
+                'Number of members added to the project in the current window (last 30 days)',
         },
     },
     components: {},

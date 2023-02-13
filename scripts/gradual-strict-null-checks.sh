d45a5a146134490d48e27c0fb2e688808c5a2d1c
Gastón Fournier <gaston@getunleash.io>
2023-02-13 12:28:11 +0100

    Test using different checkout paths

--- scripts/gradual-strict-null-checks.sh
+++ scripts/gradual-strict-null-checks.sh
@@ -1,28 +1,21 @@
 #!/usr/bin/env bash
-SCRIPT_DIR=$( dirname -- $0 )
+SCRIPT_DIR=$( dirname -- "$0" )
 CURRENT_BRANCH=$(git branch --show-current)
-MAIN_BRANCH=${1:-main}
+MAIN_BRANCH_DIR=${1:-../main}
 
-git stash
-git checkout "${MAIN_BRANCH}"
-git stash pop
-# update strictNullChecks
+# update strictNullChecks in main and current branch
+sed -i 's/\/\/\s*"strictNullChecks":\s*true,/"strictNullChecks": true,/' "${MAIN_BRANCH_DIR}/tsconfig.json"
 sed -i 's/\/\/\s*"strictNullChecks":\s*true,/"strictNullChecks": true,/' "${SCRIPT_DIR}/../tsconfig.json"
-# count errors
-MAIN_ERRORS=$(yarn 2> /dev/null | grep "Found [0-9]* errors" | sed 's/Found \(.*\) errors in .* files./\1/')
-
-# reset tsconfig changes
-git stash # including changes to tsconfig.json
-git checkout "${CURRENT_BRANCH}"
-git stash pop
 
 # count errors
+MAIN_ERRORS=$(cd "${MAIN_BRANCH}"; yarn 2> /dev/null | grep "Found [0-9]* errors" | sed 's/Found \(.*\) errors in .* files./\1/')
 BRANCH_ERRORS=$(yarn 2> /dev/null | grep "Found [0-9]* errors" | sed 's/Found \(.*\) errors in .* files./\1/')
 
 # reset tsconfig changes
 git checkout -- "${SCRIPT_DIR}/../tsconfig.json"
+$(cd "${MAIN_BRANCH}"; git checkout -- tsconfig.json)
 
-if [ ${BRANCH_ERRORS} -gt ${MAIN_ERRORS} ]; then
+if [ $BRANCH_ERRORS -gt $MAIN_ERRORS ]; then
     echo "Null check errors in ${CURRENT_BRANCH} are higher than in ${MAIN_BRANCH}: ${BRANCH_ERRORS} vs ${MAIN_ERRORS}. Please fix them before merging."
     exit 1
 else

e0635ee65606ae1536b475c1bb2d459a493dba59
Gastón Fournier <gaston@getunleash.io>
2023-02-13 12:01:35 +0100

    Stash changes to preserve changes while testing

--- scripts/gradual-strict-null-checks.sh
+++ scripts/gradual-strict-null-checks.sh
@@ -1,27 +1,35 @@
 #!/usr/bin/env bash
+set -x
 SCRIPT_DIR=$( dirname -- $0 )
 CURRENT_BRANCH=$(git branch --show-current)
 MAIN_BRANCH=${0:-main}
 
-git checkout ${MAIN_BRANCH}
+git stash
+git checkout "${MAIN_BRANCH}"
+git stash pop
 # update strictNullChecks
-sed -i 's/\/\/\s*"strictNullChecks":\s*true,/"strictNullChecks": true,/' ${SCRIPT_DIR}/../tsconfig.json
+sed -i 's/\/\/\s*"strictNullChecks":\s*true,/"strictNullChecks": true,/' "${SCRIPT_DIR}/../tsconfig.json"
 # count errors
 MAIN_ERRORS=$(yarn 2> /dev/null | grep "Found [0-9]* errors" | sed 's/Found \(.*\) errors in .* files./\1/')
 
 # reset tsconfig changes
-git checkout -- ${SCRIPT_DIR}/../tsconfig.json
-git checkout ${CURRENT_BRANCH}
+git checkout -- "${SCRIPT_DIR}/../tsconfig.json"
+git stash
+git checkout "${CURRENT_BRANCH}"
+git stash pop
 
 # update strictNullChecks
-sed -i 's/\/\/\s*"strictNullChecks":\s*true,/"strictNullChecks": true,/' ${SCRIPT_DIR}/../tsconfig.json
+sed -i 's/\/\/\s*"strictNullChecks":\s*true,/"strictNullChecks": true,/' "${SCRIPT_DIR}/../tsconfig.json"
 # count errors
 BRANCH_ERRORS=$(yarn 2> /dev/null | grep "Found [0-9]* errors" | sed 's/Found \(.*\) errors in .* files./\1/')
 
 # reset tsconfig changes
-git checkout -- ${SCRIPT_DIR}/../tsconfig.json
+git checkout -- "${SCRIPT_DIR}/../tsconfig.json"
 
 if [ ${MAIN_ERRORS} -gt ${BRANCH_ERRORS} ]; then
-    echo "You have introduced ${MAIN_ERRORS} null check errors in your branch. Please fix them before merging."
+    echo "You have ${BRANCH_ERRORS} from ${MAIN_ERRORS} null check errors in your branch. Please fix them before merging."
     exit 1
+else
+    echo "Branch errors: ${BRANCH_ERRORS} vs main errors: ${MAIN_ERRORS}"
+    exit 0
 fi

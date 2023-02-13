280d7513e9ccf4c478deb3e4dcbb27ca79445f1e
Gastón Fournier <gaston@getunleash.io>
2023-02-13 12:08:23 +0100

    Fix parameter

--- scripts/gradual-strict-null-checks.sh
+++ scripts/gradual-strict-null-checks.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-set -x
 SCRIPT_DIR=$( dirname -- $0 )
 CURRENT_BRANCH=$(git branch --show-current)
-MAIN_BRANCH=${0:-main}
+MAIN_BRANCH=${1:-main}
 
 git stash
 git checkout "${MAIN_BRANCH}"
@@ -13,23 +12,20 @@ sed -i 's/\/\/\s*"strictNullChecks":\s*true,/"strictNullChecks": true,/' "${SCRI
 MAIN_ERRORS=$(yarn 2> /dev/null | grep "Found [0-9]* errors" | sed 's/Found \(.*\) errors in .* files./\1/')
 
 # reset tsconfig changes
-git checkout -- "${SCRIPT_DIR}/../tsconfig.json"
-git stash
+git stash # including changes to tsconfig.json
 git checkout "${CURRENT_BRANCH}"
 git stash pop
 
-# update strictNullChecks
-sed -i 's/\/\/\s*"strictNullChecks":\s*true,/"strictNullChecks": true,/' "${SCRIPT_DIR}/../tsconfig.json"
 # count errors
 BRANCH_ERRORS=$(yarn 2> /dev/null | grep "Found [0-9]* errors" | sed 's/Found \(.*\) errors in .* files./\1/')
 
 # reset tsconfig changes
 git checkout -- "${SCRIPT_DIR}/../tsconfig.json"
 
-if [ ${MAIN_ERRORS} -gt ${BRANCH_ERRORS} ]; then
-    echo "You have ${BRANCH_ERRORS} from ${MAIN_ERRORS} null check errors in your branch. Please fix them before merging."
+if [ ${BRANCH_ERRORS} -gt ${MAIN_ERRORS} ]; then
+    echo "Null check errors in ${CURRENT_BRANCH} are higher than in ${MAIN_BRANCH}: ${BRANCH_ERRORS} vs ${MAIN_ERRORS}. Please fix them before merging."
     exit 1
 else
-    echo "Branch errors: ${BRANCH_ERRORS} vs main errors: ${MAIN_ERRORS}"
+    echo "Branch null check errors: ${BRANCH_ERRORS} vs ${MAIN_BRANCH}: ${MAIN_ERRORS} - You're good to go!"
     exit 0
 fi

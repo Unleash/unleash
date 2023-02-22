894b2d6918ca707df725c5d286001e7f4ca0946c
Christopher Kolstad <chriswk@getunleash.ai>
2023-02-22 11:05:56 +0100

    feat: add migration for login history

--- src/migrations/20230222084211-add-login-events-table.js
+++ src/migrations/20230222084211-add-login-events-table.js
@@ -2,12 +2,12 @@ exports.up = function (db, cb) {
     db.runSql(
         `
         CREATE TABLE login_events (
-            id TEXT PRIMARY KEY NOT NULL,
-            ip INET,
-            username TEXT,
-            auth_type TEXT,
+            id SERIAL PRIMARY KEY NOT NULL,
+            username TEXT NOT NULL,
+            auth_type TEXT NOT NULL,
             created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
             successful BOOLEAN NOT NULL,
+            ip INET,
             failure_reason TEXT
         );
         CREATE INDEX login_events_ip_idx ON login_events(ip);

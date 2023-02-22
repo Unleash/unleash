0273b6e4d58520929bde79101c4ae09c0870a5dd
andreas-unleash <andreas@getunleash.ai>
2023-02-22 18:25:50 +0200

    create notifications table and user_notifications join table
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- src/migrations/20230222154915-create-notiications-table.js
+++ src/migrations/20230222154915-create-notiications-table.js
@@ -6,10 +6,16 @@ exports.up = function (db, cb) {
             create table if not exists notifications (
                  id serial primary key,
                  created_at timestamp with time zone not null default now(),
-                 read_at timestamp with time zone,
-                 user_id integer not null references users (id),
                  event_id integer not null references events (id)
-            );`,
+            );
+
+            create table if not exists user_notifications (
+                id serial primary key,
+                notification_id integer not null references notifications (id),
+                read_at timestamp with time zone,
+                user_id integer not null references users (id)
+            )
+        `,
         cb,
     );
 };

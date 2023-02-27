ac9f054461854d1eb2a4395ee69732993906f9bc
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2023-02-27 11:14:21 +0100

    fix: limited script for now

--- .github/update_own_version.sh
+++ .github/update_own_version.sh
@@ -2,12 +2,4 @@
 
 set -e
 
-CURRENT_VERSION=$(npm pkg get version)
-
-if [[ $CURRENT_VERSION == *beta* ]]; then
-  echo "Current beta update"
-  npm version prerelease --preid=beta --ignore-scripts
-else
-  echo "Next minor beta update"
-  npm version preminor --preid=beta --ignore-scripts
-fi
+npm version prerelease --preid=beta --ignore-scripts

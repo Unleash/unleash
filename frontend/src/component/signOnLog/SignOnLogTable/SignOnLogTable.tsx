c0124371cae108246e5eeef8dd637088a660f054
Nuno Góis <github@nunogois.com>
2023-02-28 08:32:21 +0000

    feat: add authentication as a table filter (#3206)
    Adds `auth` as a table filter.
    Also fixes the hook to correctly fetch the events based on the new
    return type.
    
    <img width="1046" alt="image"
    src="https://user-images.githubusercontent.com/14320932/221603312-a40055a8-4c23-4f1f-9a87-7fbd03e7b49e.png">

--- frontend/src/component/signOnLog/SignOnLogTable/SignOnLogTable.tsx
+++ frontend/src/component/signOnLog/SignOnLogTable/SignOnLogTable.tsx
@@ -79,6 +79,7 @@ export const SignOnLogTable = () => {
                 maxWidth: 150,
                 Cell: HighlightCell,
                 searchable: true,
+                filterName: 'auth',
             },
             {
                 Header: 'IP address',
@@ -87,11 +88,11 @@ export const SignOnLogTable = () => {
                 searchable: true,
             },
             {
-                Header: 'Successful',
+                Header: 'Success',
                 accessor: 'successful',
                 align: 'center',
                 Cell: SignOnLogSuccessfulCell,
-                filterName: 'successful',
+                filterName: 'success',
                 filterParsing: (value: boolean) => value.toString(),
             },
             {

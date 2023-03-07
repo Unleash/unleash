07661b3f49cdbca748077be0e28ae278c3fd2469
andreas-unleash <andreas@getunleash.ai>
2023-03-07 13:04:55 +0200

    add notifications_retention setting
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

---
+++ src/migrations/20230307110038-add-setting-for-notification-retention.js
@@ -0,0 +1,13 @@
+exports.up = function (db, cb) {
+    db.runSql(
+        `INSERT INTO settings(name, content) VALUES ('notifications_retention', '{"hours": 720}')`,
+        cb,
+    );
+};
+
+exports.down = function (db, cb) {
+    db.runSql(
+        `DELETE FROM settings WHERE name = 'notifications_retention'`,
+        cb,
+    );
+};

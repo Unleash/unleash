920e452d7ad1d2210ca259232706199e783bc5a8
Gastón Fournier <gaston@getunleash.io>
2023-03-07 14:21:40 +0100

    Delegate call to callback

--- src/migrations/20230306103400-add-project-column-to-segments.js
+++ src/migrations/20230306103400-add-project-column-to-segments.js
@@ -3,10 +3,11 @@ exports.up = function (db, cb) {
         `ALTER TABLE segments ADD COLUMN segment_project_id varchar(255) REFERENCES projects(id) ON DELETE CASCADE;`,
         cb,
     );
-    cb();
 };
 
 exports.down = function (db, cb) {
-    db.runSql(`ALTER TABLE segments DROP COLUMN IF EXISTS segment_project_id;`);
-    cb();
+    db.runSql(
+        `ALTER TABLE segments DROP COLUMN IF EXISTS segment_project_id;`,
+        cb,
+    );
 };

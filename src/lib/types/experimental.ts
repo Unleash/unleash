24077d094352e260dbb828a851a98787799d2e21
sjaanus <sellinjaanus@gmail.com>
2023-03-09 11:43:58 +0200

    Change flag name

--- src/lib/types/experimental.ts
+++ src/lib/types/experimental.ts
@@ -64,7 +64,10 @@ const flags = {
     ),
     notifications: parseEnvVarBoolean(process.env.NOTIFICATIONS, false),
     loginHistory: parseEnvVarBoolean(process.env.UNLEASH_LOGIN_HISTORY, false),
-    bulkOperations: parseEnvVarBoolean(process.env.BULK_OPERATIONS, false),
+    bulkOperations: parseEnvVarBoolean(
+        process.env.UNLEASH_BULK_OPERATIONS,
+        false,
+    ),
     projectScopedSegments: parseEnvVarBoolean(
         process.env.PROJECT_SCOPED_SEGMENTS,
         false,

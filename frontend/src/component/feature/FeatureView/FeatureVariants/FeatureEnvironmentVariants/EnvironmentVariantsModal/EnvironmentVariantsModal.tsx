c1df4e2d918866f6d6afc63fbbb562918cf57712
andreas-unleash <andreas@getunleash.ai>
2023-03-10 11:20:48 +0200

    improvement
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- frontend/src/component/feature/FeatureView/FeatureVariants/FeatureEnvironmentVariants/EnvironmentVariantsModal/EnvironmentVariantsModal.tsx
+++ frontend/src/component/feature/FeatureView/FeatureVariants/FeatureEnvironmentVariants/EnvironmentVariantsModal/EnvironmentVariantsModal.tsx
@@ -19,7 +19,7 @@ import { v4 as uuidv4 } from 'uuid';
 import useUnleashContext from 'hooks/api/getters/useUnleashContext/useUnleashContext';
 import { updateWeightEdit } from 'component/common/util';
 import { StickinessSelect } from 'component/feature/StrategyTypes/FlexibleStrategy/StickinessSelect/StickinessSelect';
-import { useDefaultProjectStickiness } from '../../../../../../hooks/useDefaultProjectStickiness';
+import { useDefaultProjectStickiness } from 'hooks/useDefaultProjectStickiness';
 
 const StyledFormSubtitle = styled('div')(({ theme }) => ({
     display: 'flex',

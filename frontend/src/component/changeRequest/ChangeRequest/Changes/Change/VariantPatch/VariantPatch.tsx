5871133a999939ce17552fa9c5bad365a6331046
Nuno Góis <github@nunogois.com>
2023-03-15 10:58:19 +0000

    Update frontend/src/component/changeRequest/ChangeRequest/Changes/Change/VariantPatch/VariantPatch.tsx
    Co-authored-by: Gastón Fournier <gaston@getunleash.io>

--- frontend/src/component/changeRequest/ChangeRequest/Changes/Change/VariantPatch/VariantPatch.tsx
+++ frontend/src/component/changeRequest/ChangeRequest/Changes/Change/VariantPatch/VariantPatch.tsx
@@ -66,7 +66,7 @@ export const VariantPatch = ({
                         maxHeight: 600,
                     }}
                 >
-                    Updating variants:
+                    Updating variants to:
                 </TooltipLink>
                 {discard}
             </StyledChangeHeader>

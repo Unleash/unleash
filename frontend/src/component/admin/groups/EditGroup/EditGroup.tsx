09afd7bba389e508f4eea7475d5f4458c7daa57c
Fredrik Oseberg <fredrik.no@gmail.com>
2023-03-15 11:25:57 +0100

    fix: add container for EditGroups

--- frontend/src/component/admin/groups/EditGroup/EditGroup.tsx
+++ frontend/src/component/admin/groups/EditGroup/EditGroup.tsx
@@ -13,10 +13,24 @@ import { useGroup } from 'hooks/api/getters/useGroup/useGroup';
 import { UG_SAVE_BTN_ID } from 'utils/testIds';
 import { GO_BACK } from 'constants/navigate';
 import { useGroups } from 'hooks/api/getters/useGroups/useGroups';
+import { IGroup } from 'interfaces/group';
 
-export const EditGroup = () => {
+export const EditGroupContainer = () => {
     const groupId = Number(useRequiredPathParam('groupId'));
-    const { group, refetchGroup } = useGroup(groupId);
+    const { group } = useGroup(groupId);
+
+    if (!group) return null;
+
+    return <EditGroup group={group} groupId={groupId} />;
+};
+
+interface IEditGroupProps {
+    group: IGroup;
+    groupId: number;
+}
+
+export const EditGroup = ({ group, groupId }: IEditGroupProps) => {
+    const { refetchGroup } = useGroup(groupId);
     const { refetchGroups } = useGroups();
     const { setToastData, setToastApiError } = useToast();
     const { uiConfig } = useUiConfig();

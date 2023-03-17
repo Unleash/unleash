ffdbc2bd0117643de241d5ae7d63c5aa16c1fb4e
Nuno Góis <github@nunogois.com>
2023-03-17 16:01:43 +0000

    feat: implement permission checks on respective buttons

--- frontend/src/component/segments/EditSegmentButton/EditSegmentButton.tsx
+++ frontend/src/component/segments/EditSegmentButton/EditSegmentButton.tsx
@@ -1,7 +1,10 @@
 import { ISegment } from 'interfaces/segment';
 import { Edit } from '@mui/icons-material';
 import PermissionIconButton from 'component/common/PermissionIconButton/PermissionIconButton';
-import { UPDATE_SEGMENT } from 'component/providers/AccessProvider/permissions';
+import {
+    UPDATE_SEGMENT,
+    UPDATE_PROJECT_SEGMENT,
+} from 'component/providers/AccessProvider/permissions';
 import { useNavigate } from 'react-router-dom';
 import { useOptionalPathParam } from 'hooks/useOptionalPathParam';
 
@@ -24,7 +27,8 @@ export const EditSegmentButton = ({ segment }: IEditSegmentButtonProps) => {
                     navigate(`/segments/edit/${segment.id}`);
                 }
             }}
-            permission={UPDATE_SEGMENT}
+            permission={[UPDATE_SEGMENT, UPDATE_PROJECT_SEGMENT]}
+            projectId={projectId}
             tooltipProps={{ title: 'Edit segment' }}
         >
             <Edit data-loading />

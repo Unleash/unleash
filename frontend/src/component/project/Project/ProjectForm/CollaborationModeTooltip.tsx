420d0af6c8db0cc105f4a38d203b736fddf85a10
kwasniew <kwasniewski.mateusz@gmail.com>
2023-03-21 13:33:51 +0100

    fix: message

--- frontend/src/component/project/Project/ProjectForm/CollaborationModeTooltip.tsx
+++ frontend/src/component/project/Project/ProjectForm/CollaborationModeTooltip.tsx
@@ -25,7 +25,7 @@ export const CollaborationModeTooltip: FC = () => (
                 <Box sx={{ mt: 2 }}>
                     <StyledTitle>protected: </StyledTitle>
                     <StyledDescription>
-                        viewers who are not project members cannot submit change
+                        Only admins and project members can submit change
                         requests
                     </StyledDescription>
                 </Box>

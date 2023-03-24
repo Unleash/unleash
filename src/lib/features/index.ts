f4b39a1767f0eb600a49ce13d407c078be491fb2
kwasniew <kwasniewski.mateusz@gmail.com>
2023-03-24 10:49:11 +0100

    add export

--- src/lib/features/index.ts
+++ src/lib/features/index.ts
@@ -2,3 +2,5 @@ export * from './access/createAccessService';
 export * from './export-import-toggles/createExportImportService';
 export * from './feature-toggle/createFeatureToggleService';
 export * from './project/createProjectService';
+export * from './change-request-access-service/createChangeRequestAccessReadModel';
+export * from './change-request-access-service/sql-change-request-access-read-model';

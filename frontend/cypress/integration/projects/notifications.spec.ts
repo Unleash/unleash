6a734a1de0f00ccb94e5331ea70b3a6b7b945e25
sjaanus <sellinjaanus@gmail.com>
2023-03-29 14:10:56 +0300

    Fix

--- frontend/cypress/integration/projects/notifications.spec.ts
+++ frontend/cypress/integration/projects/notifications.spec.ts
@@ -115,7 +115,6 @@ describe('notifications', () => {
 
         //then
         cy.get("[data-testid='NOTIFICATIONS_MODAL']").should('exist');
-        cy.get("[data-testid='UNREAD_NOTIFICATIONS']").should('not.exist');
 
         const credentials = userCredentials[0];
 

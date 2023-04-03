45f4f5affd6e285f0dc2a36389eeb3f9ecfa5325
Thomas Heartman <thomas@getunleash.io>
2023-04-03 14:00:45 +0200

    docs: add large segments explainer

--- website/sidebars.js
+++ website/sidebars.js
@@ -42,6 +42,7 @@ module.exports = {
             items: [
                 'topics/the-anatomy-of-unleash',
                 'topics/a-b-testing',
+                'topics/large-segments',
                 'topics/proxy-hosting',
             ],
         },

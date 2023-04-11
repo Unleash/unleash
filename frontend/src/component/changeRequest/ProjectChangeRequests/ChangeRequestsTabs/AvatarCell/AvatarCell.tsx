21ee1578af273b801bf31b9cf261d6e808261a13
sjaanus <sellinjaanus@gmail.com>
2023-04-11 16:54:03 +0300

    feat: add username to CR overview and list

--- frontend/src/component/changeRequest/ProjectChangeRequests/ChangeRequestsTabs/AvatarCell/AvatarCell.tsx
+++ frontend/src/component/changeRequest/ProjectChangeRequests/ChangeRequestsTabs/AvatarCell/AvatarCell.tsx
@@ -1,13 +1,27 @@
 import { UserAvatar } from '../../../../common/UserAvatar/UserAvatar';
 import { TextCell } from '../../../../common/Table/cells/TextCell/TextCell';
+import { Box, styled, Typography } from '@mui/material';
+
+const StyledContainer = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 0,
+}));
 
 export const AvatarCell = ({ value }: any) => {
     return (
         <TextCell>
-            <UserAvatar
-                user={value}
-                sx={{ maxWidth: '30px', maxHeight: '30px', alignSelf: 'left' }}
-            />
+            <StyledContainer>
+                <UserAvatar
+                    user={value}
+                    sx={theme => ({ marginRight: theme.spacing(0.5) })}
+                />
+                <Typography component={'span'} variant={'body2'}>
+                    {' '}
+                    {value?.username}
+                </Typography>
+            </StyledContainer>
         </TextCell>
     );
 };

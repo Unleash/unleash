3442bfd8cf22b8ab5defef1b30b25c6aadda0ce6
Christopher Kolstad <chriswk@getunleash.ai>
2023-04-13 14:20:27 +0200

    Update src/lib/openapi/spec/edge-token-schema.ts
    Co-authored-by: Thomas Heartman <thomas@getunleash.ai>

--- src/lib/openapi/spec/edge-token-schema.ts
+++ src/lib/openapi/spec/edge-token-schema.ts
@@ -26,7 +26,7 @@ export const edgeTokenSchema = {
         },
         token: {
             description:
-                '[Unleash API tokens](https://docs.getunleash.io/reference/api-tokens-and-client-keys) are comprised of three parts. <project(s)>:<environment>.randomcharacters',
+                'The actual token value. [Unleash API tokens](https://docs.getunleash.io/reference/api-tokens-and-client-keys) are comprised of three parts. <project(s)>:<environment>.randomcharacters',
             type: 'string',
             example:
                 '*:development.5c806b5320c88cf27e81f3e9b97dab298a77d5879316e3c2d806206b',

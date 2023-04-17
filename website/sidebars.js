bbfb88122b8de680d542402311d1183658544d72
David Leek <david@getunleash.io>
2023-04-17 15:16:01 +0200

    docs: jira cloud documentation (#3494)
    <!-- Thanks for creating a PR! To make it easier for reviewers and
    everyone else to understand what your changes relate to, please add some
    relevant content to the headings below. Feel free to ignore or delete
    sections that you don't think are relevant. Thank you! ❤️ -->
    
    New PR using correct prefixes
    
    ## About the changes
    <!-- Describe the changes introduced. What are they and why are they
    being introduced? Feel free to also add screenshots or steps to view the
    changes if they're visual. -->
    
    Adds documentation to the docs website for the new Jira Cloud plugin.
    Couple of pieces on install page are missing due to the nature of having
    to have the docs before we can start the review process after which
    we'll get the final details on how/where to install/from etc.
    
    Also a couple of pending fixes that might impact screenshots/warrant new
    ones made
    
    ![Skjermbilde 2023-04-06 kl 12 34
    40](https://user-images.githubusercontent.com/707867/230353148-98c5915f-0d97-4cba-bec5-33d46503448d.png)
    ![Skjermbilde 2023-04-06 kl 12 34
    49](https://user-images.githubusercontent.com/707867/230353157-e9dd3f57-e6a3-4e9b-a966-ebdfcdaf2581.png)
    
    <!-- (For internal contributors): Does it relate to an issue on public
    roadmap? -->
    Relates to [roadmap](https://github.com/orgs/Unleash/projects/10) item:
    #2952
    
    ### Important files
    <!-- PRs can contain a lot of changes, but not all changes are equally
    important. Where should a reviewer start looking to get an overview of
    the changes? Are any files particularly important? -->
    
    - website/docs/reference/integrations/jira-cloud-plugin-installation.md
    - website/docs/reference/integrations/jira-cloud-plugin-usage.md
    
    ## Discussion points
    <!-- Anything about the PR you'd like to discuss before it gets merged?
    Got any questions or doubts? -->
    Very open to improvements on wording and tips on standards used in
    screenshots
    
    ---------
    
    Co-authored-by: Simon Hornby <liquidwicked64@gmail.com>
    Co-authored-by: Thomas Heartman <thomas@getunleash.ai>

--- website/sidebars.js
+++ website/sidebars.js
@@ -293,6 +293,10 @@ module.exports = {
                                 'reference/integrations/jira-server-plugin-installation',
                                 'reference/integrations/jira-server-plugin-usage',
                             ],
+                            'Jira Cloud': [
+                                'reference/integrations/jira-cloud-plugin-installation',
+                                'reference/integrations/jira-cloud-plugin-usage',
+                            ],
                         },
                         ,
                     ],

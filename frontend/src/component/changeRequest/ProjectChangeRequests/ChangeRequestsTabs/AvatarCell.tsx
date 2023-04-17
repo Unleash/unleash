56f09ce3929165a14cd49166e511f0bc2cd7512d
Jaanus Sellin <sellinjaanus@gmail.com>
2023-04-17 16:03:59 +0300

    feat: list affected features (#3534)

--- frontend/src/component/changeRequest/ProjectChangeRequests/ChangeRequestsTabs/AvatarCell.tsx
+++ frontend/src/component/changeRequest/ProjectChangeRequests/ChangeRequestsTabs/AvatarCell.tsx
@@ -1,4 +1,3 @@
-import { UserAvatar } from 'component/common/UserAvatar/UserAvatar';
 import { TextCell } from 'component/common/Table/cells/TextCell/TextCell';
 import { styled, Typography } from '@mui/material';
 
@@ -13,10 +12,6 @@ export const AvatarCell = ({ value }: any) => {
     return (
         <TextCell>
             <StyledContainer>
-                <UserAvatar
-                    user={value}
-                    sx={theme => ({ marginRight: theme.spacing(0.5) })}
-                />
                 <Typography component={'span'} variant={'body2'}>
                     {' '}
                     {value?.username}

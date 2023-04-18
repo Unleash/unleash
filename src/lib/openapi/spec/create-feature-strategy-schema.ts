2da279b7fbda59f8d8199e4f4fdc2e86d0c1cea8
andreas-unleash <andreas@getunleash.ai>
2023-04-18 09:59:02 +0300

    feat: add title to strategy (#3510)
    <!-- Thanks for creating a PR! To make it easier for reviewers and
    everyone else to understand what your changes relate to, please add some
    relevant content to the headings below. Feel free to ignore or delete
    sections that you don't think are relevant. Thank you! ❤️ -->
    Adds title column to strategies, feature_strategies and features_view in
    the db
    Updates model/schemas
    ## About the changes
    <!-- Describe the changes introduced. What are they and why are they
    being introduced? Feel free to also add screenshots or steps to view the
    changes if they're visual. -->
    
    <!-- Does it close an issue? Multiple? -->
    Closes #
    [1-855](https://linear.app/unleash/issue/1-855/allow-for-title-on-strategy-backend)
    
    <!-- (For internal contributors): Does it relate to an issue on public
    roadmap? -->
    <!--
    Relates to [roadmap](https://github.com/orgs/Unleash/projects/10) item:
    #
    -->
    
    ### Important files
    <!-- PRs can contain a lot of changes, but not all changes are equally
    important. Where should a reviewer start looking to get an overview of
    the changes? Are any files particularly important? -->
    
    
    ## Discussion points
    <!-- Anything about the PR you'd like to discuss before it gets merged?
    Got any questions or doubts? -->
    
    ---------
    
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- src/lib/openapi/spec/create-feature-strategy-schema.ts
+++ src/lib/openapi/spec/create-feature-strategy-schema.ts
@@ -9,22 +9,49 @@ export const createFeatureStrategySchema = {
     properties: {
         name: {
             type: 'string',
+            description: 'The name or type of strategy',
+            example: 'flexibleRollout',
+        },
+        title: {
+            type: 'string',
+            nullable: true,
+            description: 'A descriptive title for the strategy',
+            example: 'Gradual Rollout 25-Prod',
         },
         sortOrder: {
             type: 'number',
+            description: 'The order of the strategy in the list',
+            example: 9999,
         },
         constraints: {
             type: 'array',
+            description: 'A list of the constraints attached to the strategy',
+            example: [
+                {
+                    values: ['1', '2'],
+                    inverted: false,
+                    operator: 'IN',
+                    contextName: 'appName',
+                    caseInsensitive: false,
+                },
+            ],
             items: {
                 $ref: '#/components/schemas/constraintSchema',
             },
         },
         parameters: {
+            description: 'An object containing the parameters for the strategy',
+            example: {
+                groupId: 'some_new',
+                rollout: '25',
+                stickiness: 'sessionId',
+            },
             $ref: '#/components/schemas/parametersSchema',
         },
         segments: {
             type: 'array',
             description: 'Ids of segments to use for this strategy',
+            example: [1, 2],
             items: {
                 type: 'number',
             },
--- src/lib/openapi/spec/feature-strategy-schema.ts
+++ src/lib/openapi/spec/feature-strategy-schema.ts
@@ -4,30 +4,49 @@ import { parametersSchema } from './parameters-schema';
 
 export const featureStrategySchema = {
     $id: '#/components/schemas/featureStrategySchema',
+    description:
+        'A singles activation strategy configuration schema for a feature',
     type: 'object',
     additionalProperties: false,
     required: ['name'],
     properties: {
         id: {
             type: 'string',
+            description: 'A uuid for the feature strategy',
+            example: '6b5157cb-343a-41e7-bfa3-7b4ec3044840',
         },
         name: {
             type: 'string',
+            description: 'The name or type of strategy',
+            example: 'flexibleRollout',
+        },
+        title: {
+            type: 'string',
+            description: 'A descriptive title for the strategy',
+            example: 'Gradual Rollout 25-Prod',
+            nullable: true,
         },
         featureName: {
             type: 'string',
+            description: 'The name or feature the strategy is attached to',
+            example: 'myAwesomeFeature',
         },
         sortOrder: {
             type: 'number',
+            description: 'The order of the strategy in the list',
+            example: 9999,
         },
         segments: {
             type: 'array',
+            description: 'A list of segment ids attached to the strategy',
+            example: [1, 2],
             items: {
                 type: 'number',
             },
         },
         constraints: {
             type: 'array',
+            description: 'A list of the constraints attached to the strategy',
             items: {
                 $ref: '#/components/schemas/constraintSchema',
             },

7403c3d9a4f4e590a6153961d0cab96a4df0c300
andreas-unleash <andreas@getunleash.ai>
2023-04-19 13:08:09 +0300

    fix: override built in stickiness options
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- frontend/src/component/feature/StrategyTypes/FlexibleStrategy/StickinessSelect/StickinessSelect.tsx
+++ frontend/src/component/feature/StrategyTypes/FlexibleStrategy/StickinessSelect/StickinessSelect.tsx
@@ -1,12 +1,16 @@
 import Select from 'component/common/select';
 import { SelectChangeEvent, useTheme } from '@mui/material';
 import useUnleashContext from 'hooks/api/getters/useUnleashContext/useUnleashContext';
+import { IUnleashContextDefinition } from '../../../../../interfaces/context';
+
 const builtInStickinessOptions = [
     { key: 'default', label: 'default' },
     { key: 'userId', label: 'userId' },
     { key: 'sessionId', label: 'sessionId' },
     { key: 'random', label: 'random' },
 ];
+type SimpleContextDefinition = Partial<IUnleashContextDefinition>;
+type OptionType = { key: string; label: string };
 
 interface IStickinessSelectProps {
     label: string;
@@ -25,20 +29,29 @@ export const StickinessSelect = ({
     const { context } = useUnleashContext();
     const theme = useTheme();
 
-    const resolveStickinessOptions = () =>
-        builtInStickinessOptions.concat(
-            context
-                .filter(contextDefinition => contextDefinition.stickiness)
-                .filter(
-                    contextDefinition =>
-                        !builtInStickinessOptions.find(
-                            builtInStickinessOption =>
-                                builtInStickinessOption.key ===
-                                contextDefinition.name
-                        )
-                )
-                .map(c => ({ key: c.name, label: c.name }))
+    const resolveStickinessOptions = () => {
+        const contextFieldNames = context.map(field => field.name);
+
+        const mappedBuiltInOptions: SimpleContextDefinition[] =
+            builtInStickinessOptions
+                .filter(({ key }) => !contextFieldNames.includes(key))
+                .map(({ key }) => ({
+                    name: key,
+                    stickiness: true,
+                }));
+
+        const mappedContextFields: SimpleContextDefinition[] = context.map(
+            ({ name, stickiness }) => ({
+                name,
+                stickiness,
+            })
         );
+
+        const options = [...mappedBuiltInOptions, ...mappedContextFields];
+        return options
+            .filter(contextDefinition => contextDefinition.stickiness)
+            .map(c => ({ key: c.name, label: c.name })) as OptionType[];
+    };
 
     const stickinessOptions = resolveStickinessOptions();
     return (

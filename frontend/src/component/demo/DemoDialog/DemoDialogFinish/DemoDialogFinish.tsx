89f2865f12e3b1ca601e04444128bc289e4a7fcb
Nuno Góis <github@nunogois.com>
2023-04-20 16:54:27 +0100

    feat: add confetti

--- frontend/src/component/demo/DemoDialog/DemoDialogFinish/DemoDialogFinish.tsx
+++ frontend/src/component/demo/DemoDialog/DemoDialogFinish/DemoDialogFinish.tsx
@@ -1,5 +1,7 @@
 import { Button, Typography, styled } from '@mui/material';
 import { DemoDialog } from '../DemoDialog';
+import Confetti from 'react-confetti';
+import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
 
 const StyledActions = styled('div')(({ theme }) => ({
     display: 'grid',
@@ -23,23 +25,42 @@ export const DemoDialogFinish = ({
     onClose,
     onRestart,
 }: IDemoDialogFinishProps) => (
-    <DemoDialog open={open} onClose={onClose}>
-        <DemoDialog.Header>You finished the tutorial</DemoDialog.Header>
-        <Typography color="textSecondary" sx={{ mt: 4 }}>
-            Great job! Keep exploring Unleash, as this was just a small example
-            of its full potential. You can do the tutorial again at any moment.
-        </Typography>
-        <StyledActions>
-            <StyledButton
-                variant="outlined"
-                color="primary"
-                onClick={onRestart}
-            >
-                Restart tutorial
-            </StyledButton>
-            <StyledButton variant="contained" color="primary" onClick={onClose}>
-                Close
-            </StyledButton>
-        </StyledActions>
-    </DemoDialog>
+    <>
+        <ConditionallyRender
+            condition={open}
+            show={
+                <Confetti
+                    recycle={false}
+                    numberOfPieces={1000}
+                    initialVelocityY={50}
+                    gravity={0.3}
+                    style={{ zIndex: 3000 }}
+                />
+            }
+        />
+        <DemoDialog open={open} onClose={onClose}>
+            <DemoDialog.Header>You finished the tutorial</DemoDialog.Header>
+            <Typography color="textSecondary" sx={{ mt: 4 }}>
+                Great job! Keep exploring Unleash, as this was just a small
+                example of its full potential. You can do the tutorial again at
+                any moment.
+            </Typography>
+            <StyledActions>
+                <StyledButton
+                    variant="outlined"
+                    color="primary"
+                    onClick={onRestart}
+                >
+                    Restart tutorial
+                </StyledButton>
+                <StyledButton
+                    variant="contained"
+                    color="primary"
+                    onClick={onClose}
+                >
+                    Close
+                </StyledButton>
+            </StyledActions>
+        </DemoDialog>
+    </>
 );

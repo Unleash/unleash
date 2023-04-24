90a9e63e800c3f8883e2b46724f453ed2ce68926
David Leek <david@getunleash.io>
2023-04-24 11:03:34 +0200

    docs: openapi schema for PATs

--- src/lib/openapi/spec/pat-schema.ts
+++ src/lib/openapi/spec/pat-schema.ts
@@ -3,27 +3,42 @@ import { FromSchema } from 'json-schema-to-ts';
 export const patSchema = {
     $id: '#/components/schemas/patSchema',
     type: 'object',
+    description:
+        'An overview of a [Personal Access Token](https://docs.getunleash.io/how-to/how-to-create-personal-access-tokens).',
     properties: {
         id: {
-            type: 'number',
+            type: 'integer',
+            description:
+                'The unique identification number for this Personal Access Token.',
+            example: 1,
+            minimum: 1,
         },
         secret: {
             type: 'string',
+            description: 'The token used for authentication.',
+            example: 'user.xyzrandomstring',
         },
         expiresAt: {
             type: 'string',
             format: 'date-time',
             nullable: true,
+            description: `The token's expiration date.`,
+            example: '2023-04-19T08:15:14.000Z',
         },
         createdAt: {
             type: 'string',
             format: 'date-time',
             nullable: true,
+            example: '2023-04-19T08:15:14.000Z',
+            description: 'When the token was created.',
         },
         seenAt: {
             type: 'string',
             format: 'date-time',
             nullable: true,
+            example: '2023-04-19T08:15:14.000Z',
+            description:
+                'When the token was last seen/used to authenticate with.',
         },
     },
     components: {
--- src/lib/openapi/spec/pats-schema.ts
+++ src/lib/openapi/spec/pats-schema.ts
@@ -4,12 +4,15 @@ import { patSchema } from './pat-schema';
 export const patsSchema = {
     $id: '#/components/schemas/patsSchema',
     type: 'object',
+    description:
+        'Contains a collection of [Personal Access Tokens](https://docs.getunleash.io/how-to/how-to-create-personal-access-tokens).',
     properties: {
         pats: {
             type: 'array',
             items: {
                 $ref: '#/components/schemas/patSchema',
             },
+            description: 'A collection of Personal Access Tokens',
         },
     },
     components: {

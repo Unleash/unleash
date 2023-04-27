3599e7478c7e374257093f4bdad8aa87ab83edb2
Nuno Góis <github@nunogois.com>
2023-04-27 08:15:17 +0100

    feat: demo ui-ux improvements (#3634)
    https://linear.app/unleash/issue/2-914/improve-demo-guide-uiux
    
    Includes a big batch of UI/UX improvements, including but not limited
    to:
    
    - Updating steps text;
    - Improve behavior of intro step dialogs (use normal dialogs);
    - Improve overall design;
    - Improve escape key and backdrop click behaviors;
    - Add plans dialog;
    - Add sticky demo banner;
    - Assume `demo-app` project and `dev` environment to better fit our demo
    instance;
    - Misc fixes and refactors;
    
    Relates to [roadmap](https://github.com/orgs/Unleash/projects/10) item:
    #3537
    
    
    ![image](https://user-images.githubusercontent.com/14320932/234637210-1936fd48-ce40-4980-81ae-f1fe64e65545.png)

---
+++ frontend/src/component/demo/DemoDialog/DemoDialogPlans/DemoDialogPlans.tsx
@@ -0,0 +1,130 @@
+import { Button, Typography, styled } from '@mui/material';
+import { DemoDialog } from '../DemoDialog';
+import { GitHub } from '@mui/icons-material';
+import { Launch } from '@mui/icons-material';
+
+const StyledDemoDialog = styled(DemoDialog)(({ theme }) => ({
+    '& .MuiDialog-paper': {
+        maxWidth: theme.spacing(120),
+    },
+}));
+
+const StyledPlans = styled('div')(({ theme }) => ({
+    display: 'grid',
+    gridTemplateColumns: 'auto auto auto',
+    gap: theme.spacing(1),
+    marginTop: theme.spacing(6),
+}));
+
+const StyledPlan = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    backgroundColor: theme.palette.background.elevation1,
+    borderRadius: theme.shape.borderRadiusLarge,
+    padding: theme.spacing(4, 3),
+    '& > a': {
+        whiteSpace: 'nowrap',
+    },
+    height: theme.spacing(34),
+    width: theme.spacing(34),
+}));
+
+const StyledCompareLink = styled('a')(({ theme }) => ({
+    fontSize: theme.fontSizes.mainHeader,
+    textDecoration: 'none',
+    '&:hover': {
+        textDecoration: 'underline',
+    },
+    margin: 'auto',
+    marginTop: theme.spacing(4),
+    display: 'inline-flex',
+    alignItems: 'center',
+    gap: theme.spacing(1),
+    '& > svg': {
+        fontSize: theme.fontSizes.mainHeader,
+    },
+}));
+
+interface IDemoDialogPlansProps {
+    open: boolean;
+    onClose: () => void;
+}
+
+export const DemoDialogPlans = ({ open, onClose }: IDemoDialogPlansProps) => (
+    <StyledDemoDialog open={open} onClose={onClose}>
+        <DemoDialog.Header>Want to keep going with Unleash?</DemoDialog.Header>
+        <StyledPlans>
+            <StyledPlan>
+                <Typography variant="h5" fontWeight="bold">
+                    Open Source
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                    Self-hosted basic feature management solution
+                </Typography>
+                <Typography variant="h6" fontWeight="normal">
+                    Free
+                </Typography>
+                <Button
+                    variant="outlined"
+                    color="primary"
+                    startIcon={<GitHub />}
+                    href="https://github.com/unleash/unleash"
+                    target="_blank"
+                >
+                    View project on GitHub
+                </Button>
+            </StyledPlan>
+            <StyledPlan>
+                <Typography variant="h5" fontWeight="bold">
+                    Pro
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                    Free your team to collaborate. We'll do the heavy lifting.
+                </Typography>
+                <div>
+                    <Typography variant="h6" fontWeight="normal">
+                        $80/month
+                    </Typography>
+                    <Typography variant="body2">includes 5 seats</Typography>
+                </div>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    href="https://www.getunleash.io/plans/pro"
+                    target="_blank"
+                >
+                    Start 14-day free trial
+                </Button>
+            </StyledPlan>
+            <StyledPlan>
+                <Typography variant="h5" fontWeight="bold">
+                    Enterprise
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                    Security, compliance, and development controls for scale.
+                </Typography>
+                <div>
+                    <Typography variant="h6" fontWeight="normal">
+                        Custom
+                    </Typography>
+                    <Typography variant="body2">unlimited seats</Typography>
+                </div>
+                <Button
+                    variant="contained"
+                    color="web"
+                    href="https://www.getunleash.io/plans/enterprise"
+                    target="_blank"
+                >
+                    Contact sales
+                </Button>
+            </StyledPlan>
+        </StyledPlans>
+        <StyledCompareLink
+            href="https://www.getunleash.io/plans"
+            target="_blank"
+        >
+            Compare plans <Launch />
+        </StyledCompareLink>
+    </StyledDemoDialog>
+);
--- frontend/src/component/demo/DemoSteps/DemoSteps.tsx
+++ frontend/src/component/demo/DemoSteps/DemoSteps.tsx
@@ -3,45 +3,11 @@ import Joyride, {
     CallBackProps,
     TooltipRenderProps,
 } from 'react-joyride';
-import { Button, Typography, styled, useTheme } from '@mui/material';
+import { useTheme } from '@mui/material';
 import { ITutorialTopic, ITutorialTopicStep } from '../demo-topics';
 import { useEffect, useState } from 'react';
-import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
 import { useLocation, useNavigate } from 'react-router-dom';
-
-const StyledTooltip = styled('div')(({ theme }) => ({
-    backgroundColor: theme.palette.background.paper,
-    color: theme.palette.text.primary,
-    borderRadius: theme.shape.borderRadiusMedium,
-    width: '100%',
-    maxWidth: theme.spacing(45),
-    padding: theme.spacing(3),
-}));
-
-const StyledTooltipTitle = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    gap: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-    flexWrap: 'wrap',
-}));
-
-const StyledTooltipActions = styled('div')(({ theme }) => ({
-    display: 'flex',
-    justifyContent: 'space-between',
-    marginTop: theme.spacing(3),
-    '&&& button': {
-        '&:first-of-type': {
-            marginLeft: theme.spacing(-2),
-        },
-        fontSize: theme.fontSizes.smallBody,
-    },
-}));
-
-const StyledTooltipPrimaryActions = styled('div')(({ theme }) => ({
-    display: 'flex',
-    gap: theme.spacing(1),
-}));
+import { DemoStepTooltip } from './DemoStepTooltip/DemoStepTooltip';
 
 interface IDemoStepsProps {
     setExpanded: React.Dispatch<React.SetStateAction<boolean>>;
@@ -82,10 +48,9 @@ export const DemoSteps = ({
         }
     };
 
-    const skip = () => {
+    const close = () => {
         abortController.abort();
         setTopicStep(-1);
-        setExpanded(false);
     };
 
     const back = () => {
@@ -123,6 +88,10 @@ export const DemoSteps = ({
         }
     ) => {
         const { action, index, step } = data;
+
+        if (action === ACTIONS.CLOSE) {
+            close();
+        }
 
         if (action === ACTIONS.UPDATE) {
             const el = document.querySelector(step.target as string);
@@ -237,78 +206,27 @@ export const DemoSteps = ({
                     border: `2px solid ${theme.palette.primary.main}`,
                     outline: `2px solid ${theme.palette.secondary.border}`,
                     backgroundColor: 'transparent',
+                    animation: 'pulse 2s infinite',
                 },
                 overlay: {
                     backgroundColor: 'transparent',
                     mixBlendMode: 'unset',
                 },
             }}
-            tooltipComponent={({
-                step,
-                tooltipProps,
-            }: TooltipRenderProps & {
-                step: ITutorialTopicStep;
-            }) => (
-                <StyledTooltip {...tooltipProps}>
-                    <StyledTooltipTitle>
-                        <ConditionallyRender
-                            condition={Boolean(step.title)}
-                            show={step.title}
-                            elseShow={
-                                <Typography fontWeight="bold">
-                                    {topics[topic].title}
-                                </Typography>
-                            }
-                        />
-                        <ConditionallyRender
-                            condition={topics[topic].steps.length > 1}
-                            show={
-                                <Typography
-                                    variant="body2"
-                                    color="text.secondary"
-                                    flexShrink={0}
-                                >
-                                    (step {steps[topic] + 1} of{' '}
-                                    {topics[topic].steps.length})
-                                </Typography>
-                            }
-                        />
-                    </StyledTooltipTitle>
-                    {step.content}
-                    <StyledTooltipActions>
-                        <Button variant="text" onClick={skip}>
-                            Skip
-                        </Button>
-                        <StyledTooltipPrimaryActions>
-                            <ConditionallyRender
-                                condition={topic > 0 || steps[topic] > 0}
-                                show={
-                                    <Button
-                                        variant="outlined"
-                                        onClick={() => onBack(step)}
-                                    >
-                                        Back
-                                    </Button>
-                                }
-                            />
-                            <ConditionallyRender
-                                condition={Boolean(step.nextButton)}
-                                show={
-                                    <Button
-                                        onClick={() => next(steps[topic])}
-                                        variant="contained"
-                                    >
-                                        {topic === topics.length - 1 &&
-                                        steps[topic] ===
-                                            topics[topic].steps.length - 1
-                                            ? 'Finish'
-                                            : 'Next'}
-                                    </Button>
-                                }
-                            />
-                        </StyledTooltipPrimaryActions>
-                    </StyledTooltipActions>
-                </StyledTooltip>
+            tooltipComponent={(
+                props: TooltipRenderProps & {
+                    step: ITutorialTopicStep;
+                }
+            ) => (
+                <DemoStepTooltip
+                    {...props}
+                    topic={topic}
+                    topics={topics}
+                    steps={steps}
+                    onClose={close}
+                    onBack={onBack}
+                    onNext={next}
+                />
             )}
         />
     );

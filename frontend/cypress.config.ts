df5a428efe1a1f0a249250c23e29520495804d1b
Nuno Góis <github@nunogois.com>
2023-04-28 15:20:44 +0100

    bump cypress to 12.11.0

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import { defineConfig } from 'cypress';
+import vitePreprocessor from 'cypress-vite';
+
+export default defineConfig({
+    projectId: 'tc2qff',
+    defaultCommandTimeout: 12000,
+    screenshotOnRunFailure: false,
+    video: false,
+    e2e: {
+        specPattern: '**/*.spec.ts',
+        setupNodeEvents(on, config) {
+            on(
+                'file:preprocessor',
+                vitePreprocessor({
+                    configFile: path.resolve(__dirname, './vite.config.ts'),
+                    mode: 'development',
+                })
+            );
+        },
+    },
+});

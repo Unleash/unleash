0a2a620fd5555b70532562486fcbf042d9bf5242
andreas-unleash <andreas@getunleash.ai>
2023-05-09 11:51:46 +0300

    fix: project api token type to lowercase
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- src/lib/db/api-token-store.ts
+++ src/lib/db/api-token-store.ts
@@ -40,7 +40,7 @@ const tokenRowReducer = (acc, tokenRow) => {
         acc[tokenRow.secret] = {
             secret: token.secret,
             tokenName: token.token_name,
-            type: token.type,
+            type: token.type.toLowerCase(),
             project: ALL,
             projects: [ALL],
             environment: token.environment ? token.environment : ALL,

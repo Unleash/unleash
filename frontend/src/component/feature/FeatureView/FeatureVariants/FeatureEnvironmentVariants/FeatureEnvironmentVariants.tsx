eaacb979d63323121935e38f5f8e28232fbc40de
Christopher Kolstad <chriswk@getunleash.ai>
2023-05-10 15:46:41 +0200

    fix: Removed CR on variants flag (it's GA) (#3738)
    This PR removes the usage of crOnVariants flag, but keeps the behaviour,
    so CR are now enabled on variants.
    
    ---------
    
    Co-authored-by: Nuno Góis <github@nunogois.com>

--- frontend/src/component/feature/FeatureView/FeatureVariants/FeatureEnvironmentVariants/FeatureEnvironmentVariants.tsx
+++ frontend/src/component/feature/FeatureView/FeatureVariants/FeatureEnvironmentVariants/FeatureEnvironmentVariants.tsx
@@ -69,11 +69,9 @@ export const FeatureEnvironmentVariants = () => {
         () =>
             feature?.environments?.map(environment => ({
                 ...environment,
-                crEnabled:
-                    uiConfig.flags.crOnVariants &&
-                    isChangeRequestConfigured(environment.name),
+                crEnabled: isChangeRequestConfigured(environment.name),
             })) || [],
-        [feature.environments, uiConfig.flags.crOnVariants]
+        [feature.environments]
     );
 
     const createPatch = (

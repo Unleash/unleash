aeb5e1ddc38cf9414bff2c50e3b6a77e24daf62d
Thomas Heartman <thomas@getunleash.io>
2023-05-23 09:22:59 +0200

    chore: lower log level for unexpected error kinds
    This change lowers the log level from warning to debug for when we see
    unexpected error types.
    
    Right now this triggers for Joi errors, which we still rely on pretty
    heavily. Lowering this should clear up logs for most users.

--- src/lib/routes/util.ts
+++ src/lib/routes/util.ts
@@ -30,7 +30,7 @@ export const handleErrors: (
         error instanceof UnleashError ? error : fromLegacyError(error);
 
     if (!(error instanceof UnleashError)) {
-        logger.warn(
+        logger.debug(
             `I encountered an error that wasn't an instance of the \`UnleashError\` type. This probably means that we had an unexpected crash. The original error and what it was mapped to are:`,
             error,
             finalError,

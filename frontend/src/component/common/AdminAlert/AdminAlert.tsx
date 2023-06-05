6b79945eae2874332f4c68a0ac17a2e356b00e46
talhatahir <trinitro619@gmail.com>
2023-06-05 17:15:45 +0500

    Add Memo to AdminAlert component

--- frontend/src/component/common/AdminAlert/AdminAlert.tsx
+++ frontend/src/component/common/AdminAlert/AdminAlert.tsx
@@ -1,9 +1,10 @@
-import { Alert } from '@mui/material';
+import React from 'react';
+import Alert from '@mui/material/Alert';
 
-export const AdminAlert = () => {
+export const AdminAlert = React.memo(() => {
     return (
         <Alert severity="error">
             You need instance admin to access this section.
         </Alert>
     );
-};
+});

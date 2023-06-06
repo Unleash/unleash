44f752e7147cb2a70ca275a39276924ea9a0acba
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2023-06-06 15:04:55 +0200

    feat: display strategy title and type (#3908)

---
+++ frontend/src/component/common/StrategyItemContainer/StrategyItemContainer.test.tsx
@@ -0,0 +1,34 @@
+import { screen } from '@testing-library/react';
+import { render } from 'utils/testRenderer';
+import { StrategyItemContainer } from './StrategyItemContainer';
+import { IFeatureStrategy } from 'interfaces/strategy';
+import { testServerRoute, testServerSetup } from 'utils/testServer';
+import { UIProviderContainer } from '../../providers/UIProvider/UIProviderContainer';
+
+const server = testServerSetup();
+testServerRoute(server, '/api/admin/ui-config', {
+    flags: { strategyImprovements: true },
+});
+
+test('should render strategy name, custom title and description', async () => {
+    const strategy: IFeatureStrategy = {
+        id: 'irrelevant',
+        name: 'strategy name',
+        title: 'custom title',
+        constraints: [],
+        parameters: {},
+    };
+
+    render(
+        <UIProviderContainer>
+            <StrategyItemContainer
+                strategy={strategy}
+                description={'description'}
+            />
+        </UIProviderContainer>
+    );
+
+    expect(screen.getByText('strategy name')).toBeInTheDocument();
+    expect(screen.getByText('description')).toBeInTheDocument();
+    await screen.findByText('custom title'); // behind async flag
+});

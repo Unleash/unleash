ff183d45481be316518de094a83bb6089ab1b75d
sjaanus <sellinjaanus@gmail.com>
2023-06-09 12:16:12 +0300

    Fix

--- frontend/src/component/context/ContextFieldUsage/ContextFieldUsage.test.tsx
+++ frontend/src/component/context/ContextFieldUsage/ContextFieldUsage.test.tsx
@@ -1,7 +1,6 @@
 import { render } from '../../../utils/testRenderer';
 import { screen } from '@testing-library/react';
 import React from 'react';
-import { SegmentTable } from './SegmentTable';
 import { testServerRoute, testServerSetup } from '../../../utils/testServer';
 import { UIProviderContainer } from '../../providers/UIProvider/UIProviderContainer';
 import { ContextFieldUsage } from './ContextFieldUsage';

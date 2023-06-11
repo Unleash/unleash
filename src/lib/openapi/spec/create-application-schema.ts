4192ee9c2fe968c925969bfe8eadfdad2055570d
Gastón Fournier <gaston@getunleash.io>
2023-06-11 17:27:00 +0200

    fix open api validation

--- src/lib/openapi/spec/create-application-schema.ts
+++ src/lib/openapi/spec/create-application-schema.ts
@@ -5,7 +5,6 @@ export const createApplicationSchema = {
     type: 'object',
     additionalProperties: true,
     description: 'Reported application information from Unleash SDKs',
-    required: [],
     properties: {
         appName: {
             description: 'Name of the application',

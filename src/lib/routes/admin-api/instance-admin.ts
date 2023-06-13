5f3e5729b941982e3ddfb0f3a2f1da8117386a66
Christopher Kolstad <chriswk@getunleash.ai>
2023-06-13 14:10:21 +0200

    docs: Instance admin  (#3961)
    ### What
    Adds the documentation for the instance admin endpoints.
    
    ---------
    
    Co-authored-by: Thomas Heartman <thomas@getunleash.ai>

--- src/lib/routes/admin-api/instance-admin.ts
+++ src/lib/routes/admin-api/instance-admin.ts
@@ -9,14 +9,20 @@ import { UiConfigSchema } from '../../openapi/spec/ui-config-schema';
 import {
     InstanceStats,
     InstanceStatsService,
+    InstanceStatsSigned,
 } from '../../services/instance-stats-service';
 import { OpenApiService } from '../../services/openapi-service';
-import { createResponseSchema } from '../../openapi/util/create-response-schema';
+import {
+    createCsvResponseSchema,
+    createResponseSchema,
+} from '../../openapi/util/create-response-schema';
 
 class InstanceAdminController extends Controller {
     private instanceStatsService: InstanceStatsService;
 
     private openApiService: OpenApiService;
+
+    private jsonCsvParser: Parser;
 
     constructor(
         config: IUnleashConfig,
@@ -26,7 +32,7 @@ class InstanceAdminController extends Controller {
         }: Pick<IUnleashServices, 'instanceStatsService' | 'openApiService'>,
     ) {
         super(config);
-
+        this.jsonCsvParser = new Parser();
         this.openApiService = openApiService;
         this.instanceStatsService = instanceStatsService;
 
@@ -35,6 +41,23 @@ class InstanceAdminController extends Controller {
             path: '/statistics/csv',
             handler: this.getStatisticsCSV,
             permission: NONE,
+            middleware: [
+                openApiService.validPath({
+                    tags: ['Instance Admin'],
+                    summary: 'Instance usage statistics',
+                    description:
+                        'Provides statistics about various features of Unleash to allow for reporting of usage for self-hosted customers. The response contains data such as the number of users, groups, features, strategies, versions, etc.',
+                    operationId: 'getInstanceAdminStatsCsv',
+                    responses: {
+                        200: createCsvResponseSchema(
+                            'instanceAdminStatsSchemaCsv',
+                            this.jsonCsvParser.parse(
+                                this.instanceStatsExample(),
+                            ),
+                        ),
+                    },
+                }),
+            ],
         });
 
         this.route({
@@ -46,6 +69,9 @@ class InstanceAdminController extends Controller {
                 openApiService.validPath({
                     tags: ['Instance Admin'],
                     operationId: 'getInstanceAdminStats',
+                    summary: 'Instance usage statistics',
+                    description:
+                        'Provides statistics about various features of Unleash to allow for reporting of usage for self-hosted customers. The response contains data such as the number of users, groups, features, strategies, versions, etc.',
                     responses: {
                         200: createResponseSchema('instanceAdminStatsSchema'),
                     },
@@ -53,6 +79,34 @@ class InstanceAdminController extends Controller {
                 }),
             ],
         });
+    }
+
+    instanceStatsExample(): InstanceStatsSigned {
+        return {
+            OIDCenabled: true,
+            SAMLenabled: false,
+            clientApps: [
+                { range: 'allTime', count: 15 },
+                { range: '30d', count: 9 },
+                { range: '7d', count: 5 },
+            ],
+            contextFields: 6,
+            environments: 2,
+            featureExports: 0,
+            featureImports: 0,
+            featureToggles: 29,
+            groups: 3,
+            instanceId: 'ed3861ae-78f9-4e8c-8e57-b57efc15f82b',
+            projects: 1,
+            roles: 5,
+            segments: 2,
+            strategies: 8,
+            sum: 'some-sha256-hash',
+            timestamp: new Date(2023, 6, 12, 10, 0, 0, 0),
+            users: 10,
+            versionEnterprise: '5.1.7',
+            versionOSS: '5.1.7',
+        };
     }
 
     async getStatistics(

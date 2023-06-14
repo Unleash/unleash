d53b2c9571c70b165b48798a40c6246d38d1614d
Christopher Kolstad <chriswk@getunleash.ai>
2023-06-14 13:20:46 +0200

    Update src/lib/routes/client-api/feature.ts
    Co-authored-by: Thomas Heartman <thomas@getunleash.ai>

--- src/lib/routes/client-api/feature.ts
+++ src/lib/routes/client-api/feature.ts
@@ -112,8 +112,8 @@ export default class FeatureController extends Controller {
             permission: NONE,
             middleware: [
                 openApiService.validPath({
-                    summary: 'Get all toggles the API key has access to',
-                    description: 'Used by SDKs to configure local evaluation',
+                    summary: 'Get all toggles (SDK)', 
+                    description: 'Returns the SDK configuration for all feature toggles that are available to the provided API key. Used by SDKs to configure local evaluation',
                     operationId: 'getAllClientFeatures',
                     tags: ['Client'],
                     responses: {

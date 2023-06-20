fa081e901414b4b062594120c9539b9e025dc1a5
Christopher Kolstad <chriswk@getunleash.ai>
2023-06-20 12:10:05 +0200

    task: Make keepalive configurable via an environment variable (#4015)
    As requested in
    [Linear](https://linear.app/unleash/issue/2-1147/unleash-cloud-make-keepalive-configurable)
    this PR makes the serverKeepAliveTimeout configurable via the
    SERVER_KEEPALIVE_TIMEOUT environment variable. This was already
    configurable when starting Unleash programmatically, but it's nice to
    have as an env variable as well

--- src/lib/create-config.ts
+++ src/lib/create-config.ts
@@ -22,11 +22,7 @@ import {
 import { getDefaultLogProvider, LogLevel, validateLogProvider } from './logger';
 import { defaultCustomAuthDenyAll } from './default-custom-auth-deny-all';
 import { formatBaseUri } from './util/format-base-uri';
-import {
-    hoursToMilliseconds,
-    minutesToMilliseconds,
-    secondsToMilliseconds,
-} from 'date-fns';
+import { hoursToMilliseconds, secondsToMilliseconds } from 'date-fns';
 import EventEmitter from 'events';
 import {
     ApiTokenType,
@@ -170,7 +166,10 @@ const defaultServerOption: IServerOption = {
         process.env.ENABLE_HEAP_SNAPSHOT_ENPOINT,
         false,
     ),
-    keepAliveTimeout: minutesToMilliseconds(1),
+    keepAliveTimeout: parseEnvVarNumber(
+        process.env.SERVER_KEEPALIVE_TIMEOUT,
+        secondsToMilliseconds(15),
+    ),
     headersTimeout: secondsToMilliseconds(61),
     enableRequestLogger: false,
     gracefulShutdownEnable: parseEnvVarBoolean(

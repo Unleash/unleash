95517a0bd2c97c1c9db6c0020785d9cef295117d
kwasniew <kwasniewski.mateusz@gmail.com>
2023-06-20 10:27:30 +0200

    test: initial data loading

--- frontend/src/component/playground/Playground/AdvancedPlayground.test.tsx
+++ frontend/src/component/playground/Playground/AdvancedPlayground.test.tsx
@@ -4,7 +4,30 @@ import { UIProviderContainer } from '../../providers/UIProvider/UIProviderContai
 import AdvancedPlayground from './AdvancedPlayground';
 import { createLocalStorage } from '../../../utils/createLocalStorage';
 
-test('should fetch form data from local storage', async () => {
+const testComponent = (
+    <UIProviderContainer>
+        <AdvancedPlayground
+            FormComponent={props => (
+                <div>
+                    <div data-id="projects">
+                        {JSON.stringify(props.projects)}
+                    </div>
+                    <div data-id="environments">
+                        {JSON.stringify(props.environments)}
+                    </div>
+                    <div data-id="context">{JSON.stringify(props.context)}</div>
+                </div>
+            )}
+        />
+    </UIProviderContainer>
+);
+
+afterEach(() => {
+    const { setValue } = createLocalStorage('AdvancedPlayground:v1', {});
+    setValue({});
+});
+
+test('should fetch initial form data from local storage', async () => {
     const { setValue } = createLocalStorage('AdvancedPlayground:v1', {});
     setValue({
         projects: ['projectA', 'projectB'],
@@ -12,25 +35,7 @@ test('should fetch form data from local storage', async () => {
         context: { userId: '1' },
     });
 
-    render(
-        <UIProviderContainer>
-            <AdvancedPlayground
-                FormComponent={props => (
-                    <div>
-                        <div data-id="projects">
-                            {JSON.stringify(props.projects)}
-                        </div>
-                        <div data-id="environments">
-                            {JSON.stringify(props.environments)}
-                        </div>
-                        <div data-id="context">
-                            {JSON.stringify(props.context)}
-                        </div>
-                    </div>
-                )}
-            />
-        </UIProviderContainer>
-    );
+    render(testComponent);
 
     expect(screen.getByText('Unleash playground')).toBeInTheDocument();
     expect(screen.getByText('["projectA","projectB"]')).toBeInTheDocument();
@@ -38,4 +43,22 @@ test('should fetch form data from local storage', async () => {
         screen.getByText('["development","production"]')
     ).toBeInTheDocument();
     expect(screen.getByText('{"userId":"1"}')).toBeInTheDocument();
+});
+
+test('should fetch initial form data from url', async () => {
+    const { setValue } = createLocalStorage('AdvancedPlayground:v1', {});
+    setValue({
+        projects: ['projectA', 'projectB'],
+        environments: ['development', 'production'],
+        context: { userId: '1' },
+    });
+
+    render(testComponent, {
+        route: '/playground?context=customContext&environments=customEnv&projects=urlProject&sort=name',
+    });
+
+    expect(screen.getByText('Unleash playground')).toBeInTheDocument();
+    expect(screen.getByText('["urlProject"]')).toBeInTheDocument();
+    expect(screen.getByText('["customEnv"]')).toBeInTheDocument();
+    expect(screen.getByText('"customContext"')).toBeInTheDocument();
 });

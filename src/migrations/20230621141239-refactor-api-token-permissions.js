dd2ac482360943bf66a80a79b400492dc60df046
Nuno Góis <github@nunogois.com>
2023-06-21 18:10:32 +0100

    fix: remove DB deletes for now

--- src/migrations/20230621141239-refactor-api-token-permissions.js
+++ src/migrations/20230621141239-refactor-api-token-permissions.js
@@ -1,10 +1,6 @@
 exports.up = function (db, cb) {
     db.runSql(
         `
-    DELETE FROM permissions WHERE permission IN ('CREATE_API_TOKEN', 'UPDATE_API_TOKEN', 'DELETE_API_TOKEN', 'READ_API_TOKEN');
-    DELETE FROM permissions WHERE permission = 'UPDATE_ROLE';
-    DELETE FROM permissions WHERE permission IN ('CREATE_ADMIN_API_TOKEN', 'UPDATE_ADMIN_API_TOKEN', 'DELETE_ADMIN_API_TOKEN', 'READ_ADMIN_API_TOKEN');
-
     UPDATE permissions SET display_name = 'Create CLIENT API tokens' WHERE permission = 'CREATE_CLIENT_API_TOKEN';
     UPDATE permissions SET display_name = 'Update CLIENT API tokens' WHERE permission = 'UPDATE_CLIENT_API_TOKEN';
     UPDATE permissions SET display_name = 'Delete CLIENT API tokens' WHERE permission = 'DELETE_CLIENT_API_TOKEN';

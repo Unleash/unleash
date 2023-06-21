1bc2498c57110a869a5f47463bd9b063b2895a1e
David Leek <david@getunleash.io>
2023-06-21 13:37:59 +0200

    feat: telemetry settings endpoint and hook

---
+++ src/lib/routes/admin-api/telemetry.ts
@@ -0,0 +1,64 @@
+import { Response } from 'express';
+import { OpenApiService } from 'lib/services';
+import { IAuthRequest } from '../unleash-types';
+import { IUnleashConfig } from '../../types/option';
+import Controller from '../controller';
+import { NONE } from '../../types/permissions';
+import { IUnleashServices } from 'lib/types';
+import { createResponseSchema } from '../../openapi/util/create-response-schema';
+import {
+    telemetrySettingsSchema,
+    TelemetrySettingsSchema,
+} from '../../openapi/spec/telemetry-settings-schema';
+
+class TelemetryController extends Controller {
+    config: IUnleashConfig;
+
+    openApiService: OpenApiService;
+
+    constructor(
+        config: IUnleashConfig,
+        { openApiService }: Pick<IUnleashServices, 'openApiService'>,
+    ) {
+        super(config);
+        this.config = config;
+        this.openApiService = openApiService;
+
+        this.route({
+            method: 'get',
+            path: '/settings',
+            handler: this.getTelemetrySettings,
+            permission: NONE,
+            middleware: [
+                openApiService.validPath({
+                    tags: ['Telemetry'],
+                    summary:
+                        'Get settings for telemetry information collection',
+                    description:
+                        'Provides the configured settings for telemetry information collection',
+                    operationId: 'getTelemetrySettings',
+                    responses: {
+                        200: createResponseSchema('telemetrySettingsSchema'),
+                    },
+                }),
+            ],
+        });
+    }
+
+    async getTelemetrySettings(
+        req: IAuthRequest,
+        res: Response<TelemetrySettingsSchema>,
+    ): Promise<void> {
+        this.openApiService.respondWithValidation(
+            200,
+            res,
+            telemetrySettingsSchema.$id,
+            {
+                versionInfoCollectionEnabled: false,
+                featureInfoCollectionEnabled: true,
+            },
+        );
+    }
+}
+
+export default TelemetryController;

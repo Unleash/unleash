5ff164ffe2fc9b5ac96f24bb7977e72d90515125
David Leek <david@getunleash.io>
2023-06-21 13:37:15 +0200

    feat: telemetry configuration openapi schema

---
+++ src/lib/openapi/spec/telemetry-settings-schema.ts
@@ -0,0 +1,27 @@
+import { FromSchema } from 'json-schema-to-ts';
+
+export const telemetrySettingsSchema = {
+    $id: '#/components/schemas/telemetrySettingsSchema',
+    type: 'object',
+    additionalProperties: false,
+    required: ['versionInfoCollectionEnabled', 'featureInfoCollectionEnabled'],
+    description:
+        'Contains information about which settings are configured for version info collection and feature usage collection.',
+    properties: {
+        versionInfoCollectionEnabled: {
+            type: 'boolean',
+            description:
+                'Whether collection of version info is enabled/active.',
+        },
+        featureInfoCollectionEnabled: {
+            type: 'boolean',
+            description:
+                'Whether collection of feature usage metrics is enabled/active.',
+        },
+    },
+    components: {},
+} as const;
+
+export type TelemetrySettingsSchema = FromSchema<
+    typeof telemetrySettingsSchema
+>;

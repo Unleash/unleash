40a4451818d7abf476bc3bdc6a4ff0835da6da7a
Nuno Góis <github@nunogois.com>
2023-06-22 15:37:02 +0100

    fix: add admin guard to groups (#4069)
    Adds an admin guard to groups: It is an admin feature and should be
    guarded on the UI the same way other admin features are.

--- frontend/src/component/admin/groups/GroupsAdmin.tsx
+++ frontend/src/component/admin/groups/GroupsAdmin.tsx
@@ -1,9 +1,20 @@
+import { AdminAlert } from 'component/common/AdminAlert/AdminAlert';
 import { GroupsList } from './GroupsList/GroupsList';
+import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
+import { useContext } from 'react';
+import AccessContext from 'contexts/AccessContext';
+import { ADMIN } from '@server/types/permissions';
 
 export const GroupsAdmin = () => {
+    const { hasAccess } = useContext(AccessContext);
+
     return (
         <div>
-            <GroupsList />
+            <ConditionallyRender
+                condition={hasAccess(ADMIN)}
+                show={<GroupsList />}
+                elseShow={<AdminAlert />}
+            />
         </div>
     );
 };

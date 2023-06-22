ffed4e78b0f9be9d037efad34f7a8015b23289bb
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2023-06-22 08:46:13 +0200

    feat: configurable playground limit (#4047)

--- src/lib/features/playground/validateQueryComplexity.ts
+++ src/lib/features/playground/validateQueryComplexity.ts
@@ -6,6 +6,7 @@ export const validateQueryComplexity = (
     environmentsCount: number,
     featuresCount: number,
     contextCombinationsCount: number,
+    limit = MAX_COMPLEXITY,
 ): void => {
     const totalCount =
         environmentsCount * featuresCount * contextCombinationsCount;
@@ -13,7 +14,7 @@ export const validateQueryComplexity = (
     const reason = `Rejecting evaluation as it would generate ${totalCount} combinations exceeding ${MAX_COMPLEXITY} limit. `;
     const action = `Please reduce the number of selected environments (${environmentsCount}), features (${featuresCount}), context field combinations (${contextCombinationsCount}).`;
 
-    if (totalCount > MAX_COMPLEXITY) {
+    if (totalCount > limit) {
         throw new BadDataError(reason + action);
     }
 };

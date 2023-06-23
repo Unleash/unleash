684d71a8f488cd209332a07f8a3afbe537889dac
Thomas Heartman <thomas@getunleash.io>
2023-06-23 12:33:04 +0200

    bug(#3545): add tests to verify that title is only added when asked

--- src/test/e2e/stores/feature-toggle-client-store.e2e.test.ts
+++ src/test/e2e/stores/feature-toggle-client-store.e2e.test.ts
@@ -20,13 +20,34 @@ afterAll(async () => {
     await db.destroy();
 });
 
-test('should be able to fetch client toggles', async () => {
+beforeAll(async () => {
     const response = await app.request
         .post('/api/admin/state/import?drop=true')
         .attach('file', 'src/test/examples/exported-segments.json');
 
     expect(response.status).toBe(202);
+});
 
+test('should be able to fetch client toggles', async () => {
     const clientToggles = await featureToggleClientStore.getClient();
     expect(clientToggles).toHaveLength(1);
+});
+
+describe('strategy title queries', () => {
+    test('should not add `title` by default', async () => {
+        const clientToggles = await featureToggleClientStore.getClient();
+
+        expect(clientToggles[0].strategies[0].title).toBeUndefined();
+    });
+
+    test('should add `title` to strategies when asked', async () => {
+        const clientToggles = await featureToggleClientStore.getClient(
+            undefined,
+            undefined,
+            undefined,
+            true,
+        );
+
+        expect(clientToggles[0].strategies[0].title).not.toBeUndefined();
+    });
 });

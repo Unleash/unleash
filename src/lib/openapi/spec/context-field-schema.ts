ac5d3e5d74a33425d347422f2f01805704738209
Christopher Kolstad <chriswk@getunleash.io>
2023-06-29 10:56:48 +0200

    docs: Context api tag

--- src/lib/openapi/spec/context-field-schema.ts
+++ src/lib/openapi/spec/context-field-schema.ts
@@ -5,25 +5,39 @@ export const contextFieldSchema = {
     $id: '#/components/schemas/contextFieldSchema',
     type: 'object',
     additionalProperties: false,
+    description:
+        'A [context field](https://docs.getunleash.io/reference/unleash-context) used to configure strategies with data used to evaluate isEnabled calls',
     required: ['name'],
     properties: {
         name: {
+            description: 'The name of the context field',
             type: 'string',
+            example: 'userId',
         },
         description: {
+            description: 'The description of the context field.',
             type: 'string',
             nullable: true,
+            example: 'Used to uniquely identify users',
         },
         stickiness: {
+            description:
+                'Does this context field support being used for [stickiness](https://docs.getunleash.io/reference/stickiness) calculations',
             type: 'boolean',
+            example: true,
         },
         sortOrder: {
+            description:
+                'Used to have deterministic sorting of a list of context fields. Can also be used as a draw-breaker if list of context fields is sorted alphabetically.',
             type: 'number',
+            example: 900,
         },
         createdAt: {
+            description: 'When this context field was created',
             type: 'string',
             format: 'date-time',
             nullable: true,
+            example: '2023-06-29T10:19:00.000Z',
         },
         usedInFeatures: {
             type: 'number',
@@ -31,6 +45,7 @@ export const contextFieldSchema = {
                 'Number of projects where this context field is used in',
             example: 3,
             nullable: true,
+            minimum: 0,
         },
         usedInProjects: {
             type: 'number',
@@ -38,8 +53,11 @@ export const contextFieldSchema = {
                 'Number of projects where this context field is used in',
             example: 2,
             nullable: true,
+            minimum: 0,
         },
         legalValues: {
+            description:
+                'Allowed values for this context field schema. Can be used to narrow down accepted input',
             type: 'array',
             items: {
                 $ref: '#/components/schemas/legalValueSchema',
--- src/lib/openapi/spec/legal-value-schema.ts
+++ src/lib/openapi/spec/legal-value-schema.ts
@@ -4,13 +4,19 @@ export const legalValueSchema = {
     $id: '#/components/schemas/legalValueSchema',
     type: 'object',
     additionalProperties: false,
+    description:
+        'Describes a legal value. Typically used to limit possible values for contextFields or strategy properties',
     required: ['value'],
     properties: {
         value: {
+            description: 'The valid value',
             type: 'string',
+            example: 'red',
         },
         description: {
+            description: 'Describes the intended usage of this value',
             type: 'string',
+            example: 'We only carry these colors in our shop',
         },
     },
     components: {},

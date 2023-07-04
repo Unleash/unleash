214c738f9e52820ff850d4344c9e3617b85f7e88
sjaanus <sellinjaanus@gmail.com>
2023-07-04 15:28:55 +0300

    Fix

--- frontend/src/component/common/Table/cells/LinkCell/LinkCell.tsx
+++ frontend/src/component/common/Table/cells/LinkCell/LinkCell.tsx
@@ -1,3 +1,4 @@
+import { FC } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
 import { Highlighter } from 'component/common/Highlighter/Highlighter';

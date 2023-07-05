d9963ebb46160a64ba0b45d767a113fbe81ecdee
Thomas Heartman <thomas@getunleash.io>
2023-07-05 10:35:31 +0200

    openapi: extract shared schemas

--- src/lib/openapi/spec/create-api-token-schema.ts
+++ src/lib/openapi/spec/create-api-token-schema.ts
@@ -1,28 +1,79 @@
 import { FromSchema } from 'json-schema-to-ts';
-// const tokenNameOptions = [
-//     {
-//         required: ['tokenName'],
-//         properties: {
-//             tokenName: {
-//                 type: 'string',
-//                 description: 'The name of the token.',
-//                 example: 'client-token-64522',
-//             },
-//         },
-//     },
-//     {
-//         required: ['username'],
-//         properties: {
-//             username: {
-//                 deprecated: true,
-//                 type: 'string',
-//                 description:
-//                     'The name of the token. This property is deprecated. Use `tokenName` instead.',
-//                 example: 'client-token-64523',
-//             },
-//         },
-//     },
-// ] as const;
+
+const adminSchema = {
+    required: ['type'],
+    type: 'object',
+    properties: {
+        type: {
+            type: 'string',
+            pattern: '^[Aa][Dd][Mm][Ii][Nn]$',
+            description: `An admin token. Must be the string "admin" (not case sensitive).`,
+            example: 'admin',
+        },
+    },
+} as const;
+
+const tokenNameSchema = {
+    type: 'object',
+    required: ['tokenName'],
+    properties: {
+        tokenName: {
+            type: 'string',
+            description: 'The name of the token.',
+            example: 'client-token-64522',
+        },
+    },
+} as const;
+
+const usernameSchema = {
+    type: 'object',
+    required: ['username'],
+    properties: {
+        username: {
+            deprecated: true,
+            type: 'string',
+            description:
+                'The name of the token. This property is deprecated. Use `tokenName` instead.',
+            example: 'client-token-64523',
+        },
+    },
+} as const;
+
+const clientFrontendSchema = {
+    required: ['type'],
+    type: 'object',
+    properties: {
+        type: {
+            type: 'string',
+            pattern:
+                '^([Cc][Ll][Ii][Ee][Nn][Tt]|[Ff][Rr][Oo][Nn][Tt][Ee][Nn][Dd])$',
+            description: `A client or frontend token. Must be one of the strings "client" or "frontend" (not case sensitive).`,
+            example: 'frontend',
+        },
+        environment: {
+            type: 'string',
+            description:
+                'The environment that the token should be valid for. Defaults to "default"',
+            example: 'development',
+        },
+        project: {
+            type: 'string',
+            description:
+                'The project that the token should be valid for. Defaults to "*" meaning every project. This property is mutually incompatible with the `projects` property. If you specify one, you cannot specify the other.',
+            example: 'project-851',
+        },
+        projects: {
+            type: 'array',
+            description:
+                'A list of projects that the token should be valid for. This property is mutually incompatible with the `project` property. If you specify one, you cannot specify the other.',
+            example: ['project-851', 'project-852'],
+            items: {
+                type: 'string',
+            },
+        },
+    },
+} as const;
+
 // TODO: (openapi) this schema isn't entirely correct: `project` and `projects`
 // are mutually exclusive.
 //
@@ -46,255 +97,17 @@ export const createApiTokenSchema = {
         },
     },
     oneOf: [
-        // ...(tokenNameOptions.map((option) => ({
-        //     required: [...option.required, 'type'],
-        //     properties: {
-        //         ...option.properties,
-        //         type: {
-        //             type: 'string',
-        //             pattern: '^[Aa][Dd][Mm][Ii][Nn]$',
-        //             description: `An admin token. Must be the string "admin" (not case sensitive).`,
-        //             example: 'admin',
-        //         },
-        //     },
-        // })) as const),
-        // ...(tokenNameOptions.map((option) => ({
-        //     required: [...option.required, 'type'],
-        //     properties: {
-        //         ...option.properties,
-        //         type: {
-        //             type: 'string',
-        //             pattern:
-        //                 '^([Cc][Ll][Ii][Ee][Nn][Tt]|[Ff][Rr][Oo][Nn][Tt][Ee][Nn][Dd])$',
-        //             description: `A client or frontend token. Must be one of the strings "client" or "frontend" (not case sensitive).`,
-        //             example: 'frontend',
-        //         },
-        //         environment: {
-        //             type: 'string',
-        //             description:
-        //                 'The environment that the token should be valid for. Defaults to "default"',
-        //             example: 'development',
-        //         },
-        //         project: {
-        //             type: 'string',
-        //             description:
-        //                 'The project that the token should be valid for. Defaults to "*" meaning every project. This property is mutually incompatible with the `projects` property. If you specify one, you cannot specify the other.',
-        //             example: 'project-851',
-        //         },
-        //         projects: {
-        //             type: 'array',
-        //             description:
-        //                 'A list of projects that the token should be valid for. This property is mutually incompatible with the `project` property. If you specify one, you cannot specify the other.',
-        //             example: ['project-851', 'project-852'],
-        //             items: {
-        //                 type: 'string',
-        //             },
-        //         },
-        //     },
-        // })) as const),
-
-        // {
-        //     required: ['type'],
-        //     properties: {
-        //         type: {
-        //             type: 'string',
-        //             pattern: '^[Aa][Dd][Mm][Ii][Nn]$',
-        //             description: `An admin token. Must be the string "admin" (not case sensitive).`,
-        //             example: 'admin',
-        //         },
-        //     },
-        //     // anyOf: tokenNameOptions,
-        // },
-
-        // {
-        //     required: ['type'],
-        //     properties: {
-        //         type: {
-        //             type: 'string',
-        //             pattern:
-        //                 '^([Cc][Ll][Ii][Ee][Nn][Tt]|[Ff][Rr][Oo][Nn][Tt][Ee][Nn][Dd])$',
-        //             description: `A client or frontend token. Must be one of the strings "client" or "frontend" (not case sensitive).`,
-        //             example: 'frontend',
-        //         },
-        //         environment: {
-        //             type: 'string',
-        //             description:
-        //                 'The environment that the token should be valid for. Defaults to "default"',
-        //             example: 'development',
-        //         },
-        //         project: {
-        //             type: 'string',
-        //             description:
-        //                 'The project that the token should be valid for. Defaults to "*" meaning every project. This property is mutually incompatible with the `projects` property. If you specify one, you cannot specify the other.',
-        //             example: 'project-851',
-        //         },
-        //         projects: {
-        //             type: 'array',
-        //             description:
-        //                 'A list of projects that the token should be valid for. This property is mutually incompatible with the `project` property. If you specify one, you cannot specify the other.',
-        //             example: ['project-851', 'project-852'],
-        //             items: {
-        //                 type: 'string',
-        //             },
-        //         },
-        //     },
-        //     anyOf: tokenNameOptions,
-        // },
         {
-            allOf: [
-                {
-                    required: ['type'],
-                    type: 'object',
-                    properties: {
-                        type: {
-                            type: 'string',
-                            pattern: '^[Aa][Dd][Mm][Ii][Nn]$',
-                            description: `An admin token. Must be the string "admin" (not case sensitive).`,
-                            example: 'admin',
-                        },
-                    },
-                },
-                {
-                    type: 'object',
-                    required: ['tokenName'],
-                    properties: {
-                        tokenName: {
-                            type: 'string',
-                            description: 'The name of the token.',
-                            example: 'client-token-64522',
-                        },
-                    },
-                },
-            ],
+            allOf: [adminSchema, tokenNameSchema],
         },
         {
-            allOf: [
-                {
-                    required: ['type'],
-                    type: 'object',
-                    properties: {
-                        type: {
-                            type: 'string',
-                            pattern: '^[Aa][Dd][Mm][Ii][Nn]$',
-                            description: `An admin token. Must be the string "admin" (not case sensitive).`,
-                            example: 'admin',
-                        },
-                    },
-                },
-                {
-                    type: 'object',
-                    required: ['username'],
-                    properties: {
-                        username: {
-                            deprecated: true,
-                            type: 'string',
-                            description:
-                                'The name of the token. This property is deprecated. Use `tokenName` instead.',
-                            example: 'client-token-64523',
-                        },
-                    },
-                },
-            ],
+            allOf: [adminSchema, usernameSchema],
         },
         {
-            allOf: [
-                {
-                    required: ['type'],
-                    type: 'object',
-                    properties: {
-                        type: {
-                            type: 'string',
-                            pattern:
-                                '^([Cc][Ll][Ii][Ee][Nn][Tt]|[Ff][Rr][Oo][Nn][Tt][Ee][Nn][Dd])$',
-                            description: `A client or frontend token. Must be one of the strings "client" or "frontend" (not case sensitive).`,
-                            example: 'frontend',
-                        },
-                        environment: {
-                            type: 'string',
-                            description:
-                                'The environment that the token should be valid for. Defaults to "default"',
-                            example: 'development',
-                        },
-                        project: {
-                            type: 'string',
-                            description:
-                                'The project that the token should be valid for. Defaults to "*" meaning every project. This property is mutually incompatible with the `projects` property. If you specify one, you cannot specify the other.',
-                            example: 'project-851',
-                        },
-                        projects: {
-                            type: 'array',
-                            description:
-                                'A list of projects that the token should be valid for. This property is mutually incompatible with the `project` property. If you specify one, you cannot specify the other.',
-                            example: ['project-851', 'project-852'],
-                            items: {
-                                type: 'string',
-                            },
-                        },
-                    },
-                },
-                {
-                    type: 'object',
-                    required: ['tokenName'],
-                    properties: {
-                        tokenName: {
-                            type: 'string',
-                            description: 'The name of the token.',
-                            example: 'client-token-64522',
-                        },
-                    },
-                },
-            ],
+            allOf: [clientFrontendSchema, tokenNameSchema],
         },
         {
-            allOf: [
-                {
-                    required: ['type'],
-                    type: 'object',
-                    properties: {
-                        type: {
-                            type: 'string',
-                            pattern:
-                                '^([Cc][Ll][Ii][Ee][Nn][Tt]|[Ff][Rr][Oo][Nn][Tt][Ee][Nn][Dd])$',
-                            description: `A client or frontend token. Must be one of the strings "client" or "frontend" (not case sensitive).`,
-                            example: 'frontend',
-                        },
-                        environment: {
-                            type: 'string',
-                            description:
-                                'The environment that the token should be valid for. Defaults to "default"',
-                            example: 'development',
-                        },
-                        project: {
-                            type: 'string',
-                            description:
-                                'The project that the token should be valid for. Defaults to "*" meaning every project. This property is mutually incompatible with the `projects` property. If you specify one, you cannot specify the other.',
-                            example: 'project-851',
-                        },
-                        projects: {
-                            type: 'array',
-                            description:
-                                'A list of projects that the token should be valid for. This property is mutually incompatible with the `project` property. If you specify one, you cannot specify the other.',
-                            example: ['project-851', 'project-852'],
-                            items: {
-                                type: 'string',
-                            },
-                        },
-                    },
-                },
-                {
-                    type: 'object',
-                    required: ['username'],
-                    properties: {
-                        username: {
-                            deprecated: true,
-                            type: 'string',
-                            description:
-                                'The name of the token. This property is deprecated. Use `tokenName` instead.',
-                            example: 'client-token-64523',
-                        },
-                    },
-                },
-            ],
+            allOf: [clientFrontendSchema, usernameSchema],
         },
     ],
     components: {},

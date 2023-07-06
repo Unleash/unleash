02fea44956b0156676c1adf502f999e5fdda1aae
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2023-07-06 16:04:33 +0200

    fix: initial playground env (#4167)

--- frontend/src/component/playground/Playground/PlaygroundForm/PlaygroundForm.tsx
+++ frontend/src/component/playground/Playground/PlaygroundForm/PlaygroundForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, useTheme } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { IEnvironment } from 'interfaces/environments';
 import { FormEvent, VFC } from 'react';
 import { PlaygroundCodeFieldset } from './PlaygroundCodeFieldset/PlaygroundCodeFieldset';
@@ -25,8 +25,6 @@ export const PlaygroundForm: VFC<IPlaygroundFormProps> = ({
     context,
     setContext,
 }) => {
-    const theme = useTheme();
-
     return (
         <Box
             component="form"

f5f04e24d323603a580bdabeef712898eba0b5c4
Nuno Góis <github@nunogois.com>
2023-07-07 13:43:07 +0100

    refactor: simplify flow

--- frontend/src/component/project/Project/DeleteProject/DeleteProjectDialogue.tsx
+++ frontend/src/component/project/Project/DeleteProject/DeleteProjectDialogue.tsx
@@ -8,7 +8,7 @@ import useToast from 'hooks/useToast';
 interface IDeleteProjectDialogueProps {
     project: string;
     open: boolean;
-    onClose?: (e: React.SyntheticEvent) => void;
+    onClose: (e: React.SyntheticEvent) => void;
     onSuccess?: () => void;
 }
 
@@ -36,14 +36,14 @@ export const DeleteProjectDialogue = ({
         } catch (ex: unknown) {
             setToastApiError(formatUnknownError(ex));
         }
-        onClose?.(e);
+        onClose(e);
     };
 
     return (
         <Dialogue
             open={open}
             onClick={onClick}
-            onClose={e => onClose?.(e)}
+            onClose={onClose}
             title="Really delete project"
         />
     );

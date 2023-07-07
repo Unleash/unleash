42df3100b0c0a8820faf5bd750089392ee9f5543
Christopher Kolstad <chriswk@getunleash.io>
2023-07-07 12:42:26 +0200

    docs: add description to requests per seconds schemas

--- src/lib/openapi/spec/requests-per-second-schema.ts
+++ src/lib/openapi/spec/requests-per-second-schema.ts
@@ -3,15 +3,25 @@ import { FromSchema } from 'json-schema-to-ts';
 export const requestsPerSecondSchema = {
     $id: '#/components/schemas/requestsPerSecondSchema',
     type: 'object',
+    description:
+        'Statistics for usage of Unleash, formatted so it can easily be used in a graph',
     properties: {
         status: {
             type: 'string',
+            description:
+                'Whether the query against prometheus succeeded or failed',
+            enum: ['success', 'failure'],
+            example: 'success',
         },
         data: {
             type: 'object',
+            description: 'The query result from prometheus',
             properties: {
                 resultType: {
                     type: 'string',
+                    description: 'Prometheus compatible result type.',
+                    enum: ['matrix', 'vector', 'scalar', 'string'],
+                    example: 'vector',
                 },
                 result: {
                     description:
@@ -19,6 +29,8 @@ export const requestsPerSecondSchema = {
                     type: 'array',
                     items: {
                         type: 'object',
+                        description:
+                            'A representation of a single metric to build a line in a graph',
                         properties: {
                             metric: {
                                 description:
@@ -26,10 +38,16 @@ export const requestsPerSecondSchema = {
                                 type: 'object',
                                 properties: {
                                     appName: {
+                                        description:
+                                            'Name of the application this metric relate to',
                                         type: 'string',
+                                        example: 'mySdk',
                                     },
                                     endpoint: {
+                                        description:
+                                            'Which endpoint has been accessed',
                                         type: 'string',
+                                        example: '/api/frontend',
                                     },
                                 },
                             },
@@ -39,10 +57,20 @@ export const requestsPerSecondSchema = {
                                 type: 'array',
                                 items: {
                                     type: 'array',
+                                    description:
+                                        'Either the x axis represented as a number or the y axis represented as a string',
                                     items: {
                                         anyOf: [
-                                            { type: 'string' },
-                                            { type: 'number' },
+                                            {
+                                                type: 'string',
+                                                description:
+                                                    'An identifier for the line in the graph',
+                                            },
+                                            {
+                                                type: 'number',
+                                                description:
+                                                    'The number of requests at this point in time',
+                                            },
                                         ],
                                     },
                                 },

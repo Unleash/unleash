d5bd3bd08889072a0635d686f3cecb61a00f9c58
Thomas Heartman <thomas@getunleash.io>
2023-07-10 15:35:47 +0200

    openapi: update stale endpoint

--- src/lib/openapi/spec/batch-stale-schema.ts
+++ src/lib/openapi/spec/batch-stale-schema.ts
@@ -3,16 +3,25 @@ import { FromSchema } from 'json-schema-to-ts';
 export const batchStaleSchema = {
     $id: '#/components/schemas/batchStaleSchema',
     type: 'object',
+    description:
+        'A list of features to operate on and whether they should be marked as stale or as not stale.',
     required: ['features', 'stale'],
     properties: {
         features: {
             type: 'array',
+            description: 'A list of features to mark as (not) stale',
+            example: ['my-feature-1', 'my-feature-2', 'my-feature-3'],
             items: {
                 type: 'string',
+                description: 'A feature name',
+                example: 'my-feature-5',
             },
         },
         stale: {
             type: 'boolean',
+            example: 'true',
+            description:
+                'Whether the list of features should be marked as stale or not stale. If `true`, the features will be marked as stale. If `false`, the features will be marked as not stale.',
         },
     },
     components: {

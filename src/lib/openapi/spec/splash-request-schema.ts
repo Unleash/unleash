a785465943a040533a21aedad14513f2854addd4
Thomas Heartman <thomas@getunleash.ai>
2023-07-13 10:29:54 +0200

    openapi: update the splash endpoints and schemas for splash (#4227)
    This change updates the splash endpoints and related schemas for the
    admin ui tag.

---
+++ src/lib/openapi/spec/splash-request-schema.ts
@@ -0,0 +1,23 @@
+import { FromSchema } from 'json-schema-to-ts';
+
+export const splashRequestSchema = {
+    $id: '#/components/schemas/splashRequestSchema',
+    type: 'object',
+    description: 'Data related to a user having seen a splash screen.',
+    required: ['userId', 'splashId'],
+    properties: {
+        userId: {
+            type: 'integer',
+            description: 'The ID of the user that was shown the splash screen.',
+            example: 1,
+        },
+        splashId: {
+            type: 'string',
+            description: 'The ID of the splash screen that was shown.',
+            example: 'new-splash-screen',
+        },
+    },
+    components: {},
+} as const;
+
+export type SplashRequestSchema = FromSchema<typeof splashRequestSchema>;

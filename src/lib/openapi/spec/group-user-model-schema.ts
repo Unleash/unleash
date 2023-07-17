c06e01ab6bd44d3a26405e93182caccbd5c885d3
kwasniew <kwasniewski.mateusz@gmail.com>
2023-07-17 17:01:50 +0200

    feat: group schema updates

--- src/lib/openapi/spec/group-user-model-schema.ts
+++ src/lib/openapi/spec/group-user-model-schema.ts
@@ -6,14 +6,19 @@ export const groupUserModelSchema = {
     type: 'object',
     additionalProperties: false,
     required: ['user'],
+    description: 'Details for the single user belonging to a group',
     properties: {
         joinedAt: {
+            description: 'A date when the user join the group',
             type: 'string',
             format: 'date-time',
+            example: '2023-06-30T11:41:00.123Z',
         },
         createdBy: {
+            description: 'A user who added the group user',
             type: 'string',
             nullable: true,
+            example: 'admin',
         },
         user: {
             $ref: '#/components/schemas/userSchema',

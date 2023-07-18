90827615b61fc90b41481ac5f20114121a1eee52
Thomas Heartman <thomas@getunleash.io>
2023-07-18 10:11:38 +0200

    Chore: use `lifetimeDays` in request schema to stay consistent
    This is what we already use everywhere else, so it's better to stay
    consistent here.

--- src/lib/openapi/spec/update-feature-type-lifetime-schema.ts
+++ src/lib/openapi/spec/update-feature-type-lifetime-schema.ts
@@ -3,11 +3,11 @@ import { FromSchema } from 'json-schema-to-ts';
 export const updateFeatureTypeLifetimeSchema = {
     $id: '#/components/schemas/updateFeatureTypeLifetimeSchema',
     type: 'object',
-    required: ['lifetime'],
+    required: ['lifetimeDays'],
     description:
         'Data used when updating the lifetime of a [feature toggle type](https://docs.getunleash.io/reference/feature-toggle-types).',
     properties: {
-        lifetime: {
+        lifetimeDays: {
             description:
                 'The new lifetime (in days) that you want to assign to the feature toggle type. If the value is `null`, then the feature toggles of that type will never be marked as potentially stale. If the value is `0`, then Unleash will consider them potentially stale immediately. Otherwise, they will be considered potentially stale after the number of days indicated by this property.',
             example: 7,

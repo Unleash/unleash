4ee5fe9efcd707fa472b56d247fa35d98bc03f76
Gastón Fournier <gaston@getunleash.io>
2023-07-25 12:49:09 +0000

    fix: Client metrics name validation (#4339)
    1. Add a test for the failing use case (we can see it
    [here](https://github.com/Unleash/unleash/actions/runs/5656229196/job/15322845002?pr=4339#step:5:783)):
    ```
    FAIL src/lib/services/client-metrics/metrics-service-v2.test.ts
      ● process metrics properly even when some names are not url friendly
    
        ValidationError: "name" must be URL friendly
    ```
    2. Fix and handle this gracefully
    3. Added a new toggle to silently ignore bad names: filterInvalidClientMetrics
    
    Fixes: https://github.com/Unleash/unleash/pull/4193

--- src/lib/services/client-metrics/metrics-service-v2.ts
+++ src/lib/services/client-metrics/metrics-service-v2.ts
@@ -1,5 +1,5 @@
 import { Logger } from '../../logger';
-import { IUnleashConfig } from '../../types';
+import { IFlagResolver, IUnleashConfig } from '../../types';
 import { IUnleashStores } from '../../types';
 import { ToggleMetricsSummary } from '../../types/models/metrics';
 import {
@@ -21,7 +21,6 @@ import { LastSeenService } from './last-seen-service';
 import { generateHourBuckets } from '../../util/time-utils';
 import { ClientMetricsSchema } from 'lib/openapi';
 import { nameSchema } from '../../schema/feature-schema';
-import { BadDataError } from '../../error';
 
 export default class ClientMetricsServiceV2 {
     private config: IUnleashConfig;
@@ -33,6 +32,8 @@ export default class ClientMetricsServiceV2 {
     private clientMetricsStoreV2: IClientMetricsStoreV2;
 
     private lastSeenService: LastSeenService;
+
+    private flagResolver: Pick<IFlagResolver, 'isEnabled'>;
 
     private logger: Logger;
 
@@ -48,6 +49,7 @@ export default class ClientMetricsServiceV2 {
         this.logger = config.getLogger(
             '/services/client-metrics/client-metrics-service-v2.ts',
         );
+        this.flagResolver = config.flagResolver;
 
         this.timers.push(
             setInterval(() => {
@@ -60,6 +62,32 @@ export default class ClientMetricsServiceV2 {
                 this.clientMetricsStoreV2.clearMetrics(48).catch(console.error);
             }, hoursToMilliseconds(12)).unref(),
         );
+    }
+
+    async filterValidToggleNames(toggleNames: string[]): Promise<string[]> {
+        const nameValidations: Promise<
+            PromiseFulfilledResult<{ name: string }> | PromiseRejectedResult
+        >[] = toggleNames.map((toggleName) =>
+            nameSchema.validateAsync({ name: toggleName }),
+        );
+        const badNames = (await Promise.allSettled(nameValidations)).filter(
+            (r) => r.status === 'rejected',
+        );
+        if (badNames.length > 0) {
+            this.logger.warn(
+                `Got a few toggles with invalid names: ${JSON.stringify(
+                    badNames,
+                )}`,
+            );
+
+            if (this.flagResolver.isEnabled('filterInvalidClientMetrics')) {
+                const justNames = badNames.map(
+                    (r: PromiseRejectedResult) => r.reason._original.name,
+                );
+                return toggleNames.filter((name) => !justNames.includes(name));
+            }
+        }
+        return toggleNames;
     }
 
     async registerBulkMetrics(metrics: IClientMetricsEnv[]): Promise<void> {
@@ -83,28 +111,30 @@ export default class ClientMetricsServiceV2 {
                 ),
         );
 
-        for (const toggle of toggleNames) {
-            if (!(await nameSchema.validateAsync({ name: toggle }))) {
-                throw new BadDataError(
-                    `Invalid feature toggle name "${toggle}"`,
-                );
-            }
+        const validatedToggleNames = await this.filterValidToggleNames(
+            toggleNames,
+        );
+
+        this.logger.debug(
+            `Got ${toggleNames.length} (${validatedToggleNames.length} valid) metrics from ${clientIp}`,
+        );
+
+        if (validatedToggleNames.length > 0) {
+            const clientMetrics: IClientMetricsEnv[] = validatedToggleNames.map(
+                (name) => ({
+                    featureName: name,
+                    appName: value.appName,
+                    environment: value.environment ?? 'default',
+                    timestamp: value.bucket.start, //we might need to approximate between start/stop...
+                    yes: value.bucket.toggles[name].yes ?? 0,
+                    no: value.bucket.toggles[name].no ?? 0,
+                    variants: value.bucket.toggles[name].variants,
+                }),
+            );
+            await this.registerBulkMetrics(clientMetrics);
+
+            this.config.eventBus.emit(CLIENT_METRICS, value);
         }
-
-        this.logger.debug(`got metrics from ${clientIp}`);
-
-        const clientMetrics: IClientMetricsEnv[] = toggleNames.map((name) => ({
-            featureName: name,
-            appName: value.appName,
-            environment: value.environment,
-            timestamp: value.bucket.start, //we might need to approximate between start/stop...
-            yes: value.bucket.toggles[name].yes,
-            no: value.bucket.toggles[name].no,
-            variants: value.bucket.toggles[name].variants,
-        }));
-        await this.registerBulkMetrics(clientMetrics);
-
-        this.config.eventBus.emit(CLIENT_METRICS, value);
     }
 
     async bulkAdd(): Promise<void> {

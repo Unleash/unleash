e876807f30d53cece0ef5b31ac029e736589826f
Gastón Fournier <gaston@getunleash.io>
2023-07-25 13:33:21 +0000

    fix: Client metrics name validation (#4339) (#4342)
    1. Add a test for the failing use case (we can see it
    [here](https://github.com/Unleash/unleash/actions/runs/5656229196/job/15322845002?pr=4339#step:5:783)):
    ```
    FAIL src/lib/services/client-metrics/metrics-service-v2.test.ts
      ● process metrics properly even when some names are not url friendly
    
        ValidationError: "name" must be URL friendly
    ```
    2. Fix and handle this gracefully
    3. Added a new toggle to silently ignore bad names:
    filterInvalidClientMetrics
    
    Fixes: https://github.com/Unleash/unleash/pull/4193

---
+++ src/lib/services/client-metrics/metrics-service-v2.test.ts
@@ -0,0 +1,116 @@
+import ClientMetricsServiceV2 from './metrics-service-v2';
+
+import getLogger from '../../../test/fixtures/no-logger';
+
+import createStores from '../../../test/fixtures/store';
+import EventEmitter from 'events';
+import { LastSeenService } from './last-seen-service';
+import { IUnleashConfig } from 'lib/types';
+
+function initClientMetrics(flagEnabled = true) {
+    const stores = createStores();
+
+    const eventBus = new EventEmitter();
+    eventBus.emit = jest.fn();
+
+    const config = {
+        eventBus,
+        getLogger,
+        flagResolver: {
+            isEnabled: () => {
+                return flagEnabled;
+            },
+        },
+    } as unknown as IUnleashConfig;
+
+    const lastSeenService = new LastSeenService(stores, config);
+    lastSeenService.updateLastSeen = jest.fn();
+
+    const service = new ClientMetricsServiceV2(stores, config, lastSeenService);
+    return { clientMetricsService: service, eventBus, lastSeenService };
+}
+
+test('process metrics properly', async () => {
+    const { clientMetricsService, eventBus, lastSeenService } =
+        initClientMetrics();
+    await clientMetricsService.registerClientMetrics(
+        {
+            appName: 'test',
+            bucket: {
+                start: '1982-07-25T12:00:00.000Z',
+                stop: '2023-07-25T12:00:00.000Z',
+                toggles: {
+                    myCoolToggle: {
+                        yes: 25,
+                        no: 42,
+                        variants: {
+                            blue: 6,
+                            green: 15,
+                            red: 46,
+                        },
+                    },
+                    myOtherToggle: {
+                        yes: 0,
+                        no: 100,
+                    },
+                },
+            },
+            environment: 'test',
+        },
+        '127.0.0.1',
+    );
+
+    expect(eventBus.emit).toHaveBeenCalledTimes(1);
+    expect(lastSeenService.updateLastSeen).toHaveBeenCalledTimes(1);
+});
+
+test('process metrics properly even when some names are not url friendly, filtering out invalid names when flag is on', async () => {
+    const { clientMetricsService, eventBus, lastSeenService } =
+        initClientMetrics();
+    await clientMetricsService.registerClientMetrics(
+        {
+            appName: 'test',
+            bucket: {
+                start: '1982-07-25T12:00:00.000Z',
+                stop: '2023-07-25T12:00:00.000Z',
+                toggles: {
+                    'not url friendly ☹': {
+                        yes: 0,
+                        no: 100,
+                    },
+                },
+            },
+            environment: 'test',
+        },
+        '127.0.0.1',
+    );
+
+    // only toggle with a bad name gets filtered out
+    expect(eventBus.emit).not.toHaveBeenCalled();
+    expect(lastSeenService.updateLastSeen).not.toHaveBeenCalled();
+});
+
+test('process metrics properly even when some names are not url friendly, with default behavior when flag is off', async () => {
+    const { clientMetricsService, eventBus, lastSeenService } =
+        initClientMetrics(false);
+    await clientMetricsService.registerClientMetrics(
+        {
+            appName: 'test',
+            bucket: {
+                start: '1982-07-25T12:00:00.000Z',
+                stop: '2023-07-25T12:00:00.000Z',
+                toggles: {
+                    'not url friendly ☹': {
+                        yes: 0,
+                        no: 100,
+                    },
+                },
+            },
+            environment: 'test',
+        },
+        '127.0.0.1',
+    );
+
+    expect(eventBus.emit).toHaveBeenCalledTimes(1);
+    expect(lastSeenService.updateLastSeen).toHaveBeenCalledTimes(1);
+});

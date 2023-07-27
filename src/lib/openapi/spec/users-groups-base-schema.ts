0032c2b012a620c8c2a0024a369459d11fdecc1d
Thomas Heartman <thomas@getunleash.io>
2023-07-27 14:48:22 +0200

    1-1167: users groups base schema

--- src/lib/openapi/spec/users-groups-base-schema.ts
+++ src/lib/openapi/spec/users-groups-base-schema.ts
@@ -7,18 +7,42 @@ export const usersGroupsBaseSchema = {
     $id: '#/components/schemas/usersGroupsBaseSchema',
     type: 'object',
     additionalProperties: false,
+    description: 'An overview of user groups and users.',
     properties: {
         groups: {
             type: 'array',
+            description: 'A list of user groups and their configuration.',
             items: {
                 $ref: '#/components/schemas/groupSchema',
             },
+            example: [
+                {
+                    id: 1,
+                    name: 'unleash-e2e-7095756699593281',
+                    userCount: 1,
+                    rootRole: null,
+                },
+            ],
         },
         users: {
             type: 'array',
             items: {
                 $ref: '#/components/schemas/userSchema',
             },
+            description: 'A list of users.',
+            example: [
+                {
+                    id: 4,
+                    name: 'unleash-e2e-user2-7095756699593281',
+                    email: 'unleash-e2e-user2-7095756699593281@test.com',
+                    accountType: 'User',
+                },
+                {
+                    id: 1,
+                    username: 'admin',
+                    accountType: 'User',
+                },
+            ],
         },
     },
     components: {

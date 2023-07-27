a02d6f3a15bbe6655d4bbe2453a8279b1cd29317
Thomas Heartman <thomas@getunleash.io>
2023-07-27 12:05:05 +0200

    1-1167: maintenance schema

--- src/lib/openapi/spec/maintenance-schema.ts
+++ src/lib/openapi/spec/maintenance-schema.ts
@@ -4,9 +4,11 @@ export const maintenanceSchema = {
     $id: '#/components/schemas/maintenanceSchema',
     type: 'object',
     additionalProperties: false,
+    description: "The current state of Unleash's maintenance mode feature.",
     required: ['enabled'],
     properties: {
         enabled: {
+            description: 'Whether maintenance mode is enabled or not.',
             type: 'boolean',
             example: true,
         },

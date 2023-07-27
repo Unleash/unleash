b628fe5a89b3f9f2310a702d04a6cd5abe569e27
Thomas Heartman <thomas@getunleash.io>
2023-07-27 16:06:46 +0200

    1-1167: add missing files 😳

---
+++ src/lib/openapi/spec/create-context-field-schema.ts
@@ -0,0 +1,21 @@
+import { FromSchema } from 'json-schema-to-ts';
+import { updateContextFieldSchema } from './update-context-field-schema';
+
+export const createContextFieldSchema = {
+    ...updateContextFieldSchema,
+    $id: '#/components/schemas/createContextFieldSchema',
+    description: 'Data used to create a context field configuration.',
+    required: ['name'],
+    properties: {
+        ...updateContextFieldSchema.properties,
+        name: {
+            description: 'The name of the context field.',
+            type: 'string',
+            example: 'subscriptionTier',
+        },
+    },
+} as const;
+
+export type CreateContextFieldSchema = FromSchema<
+    typeof createContextFieldSchema
+>;
---
+++ src/lib/openapi/spec/update-context-field-schema.ts
@@ -0,0 +1,48 @@
+import { FromSchema } from 'json-schema-to-ts';
+import { legalValueSchema } from './legal-value-schema';
+
+export const updateContextFieldSchema = {
+    $id: '#/components/schemas/updateContextFieldSchema',
+    type: 'object',
+    description: 'Data to update an existing context field configuration.',
+    properties: {
+        description: {
+            type: 'string',
+            description: 'A description of the context field',
+            example: "The user's subscription tier",
+        },
+        stickiness: {
+            type: 'boolean',
+            description:
+                '`true` if this field should be available for use with [custom stickiness](https://docs.getunleash.io/reference/stickiness#custom-stickiness), otherwise `false`',
+            example: false,
+        },
+        sortOrder: {
+            type: 'integer',
+            description:
+                'How this context field should be sorted if no other sort order is selected',
+            example: 2,
+        },
+        legalValues: {
+            type: 'array',
+            description: 'A list of allowed values for this context field',
+            example: [
+                { value: 'gold' },
+                { value: 'silver' },
+                { value: 'crystal' },
+            ],
+            items: {
+                $ref: '#/components/schemas/legalValueSchema',
+            },
+        },
+    },
+    components: {
+        schemas: {
+            legalValueSchema,
+        },
+    },
+} as const;
+
+export type UpdateContextFieldSchema = FromSchema<
+    typeof updateContextFieldSchema
+>;

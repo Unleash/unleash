1cf3a0f25b5511307cfef5c6b730a1e5c3917779
Thomas Heartman <thomas@getunleash.io>
2023-07-27 11:33:54 +0200

    1-1167: create-feature-schema

--- src/lib/openapi/meta-schema-rules.test.ts
+++ src/lib/openapi/meta-schema-rules.test.ts
@@ -117,7 +117,6 @@ const metaRules: Rule[] = [
             required: ['description'],
         },
         knownExceptions: [
-            'createFeatureSchema',
             'featureStrategySegmentSchema',
             'maintenanceSchema',
             'toggleMaintenanceSchema',

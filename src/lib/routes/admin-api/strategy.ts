a5167a8dfa54f809b155a06a482e1d617c6aaacd
Gastón Fournier <gaston@getunleash.io>
2023-08-09 13:59:41 +0200

    Rename parameter access

--- src/lib/routes/admin-api/strategy.ts
+++ src/lib/routes/admin-api/strategy.ts
@@ -257,13 +257,13 @@ class StrategyController extends Controller {
     }
 
     async updateStrategy(
-        req: IAuthRequest<{ strategyName: string }, UpdateStrategySchema>,
+        req: IAuthRequest<{ name: string }, UpdateStrategySchema>,
         res: Response<void>,
     ): Promise<void> {
         const userName = extractUsername(req);
 
         await this.strategyService.updateStrategy(
-            { ...req.body, name: req.params.strategyName },
+            { ...req.body, name: req.params.name },
             userName,
         );
         res.status(200).end();

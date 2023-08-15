1d5281708bf841f474e50ab62d64ee5009ecc40f
kwasniew <kwasniewski.mateusz@gmail.com>
2023-08-15 12:17:31 +0200

    feat: reject change request dialog

---
+++ frontend/src/component/changeRequest/ChangeRequestOverview/ChangeRequestRejectDialog/ChangeRequestRejectDialog.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { vi } from 'vitest';
+import { fireEvent, screen } from '@testing-library/react';
+import { render } from 'utils/testRenderer';
+import { ChangeRequestRejectDialogue } from './ChangeRequestRejectDialog'; // Make sure you update the path accordingly
+
+describe('<ChangeRequestRejectDialogue />', () => {
+    test('submits the typed comment to onConfirm', () => {
+        const handleConfirm = vi.fn();
+        const handleClose = vi.fn();
+
+        render(
+            <ChangeRequestRejectDialogue
+                open={true}
+                onConfirm={handleConfirm}
+                onClose={handleClose}
+            />
+        );
+
+        const commentInput = screen.getByPlaceholderText(
+            'Add your comment here'
+        );
+        fireEvent.change(commentInput, { target: { value: 'Test Comment' } });
+
+        const rejectButton = screen.getByRole('button', {
+            name: /Reject changes/i,
+        });
+        fireEvent.click(rejectButton);
+
+        expect(handleConfirm).toHaveBeenCalledWith('Test Comment');
+    });
+});

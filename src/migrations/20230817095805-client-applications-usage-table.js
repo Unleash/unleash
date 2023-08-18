e228bb105893012c145e19016500f19bf52ad3ac
andreas-unleash <andreas@getunleash.ai>
2023-08-18 14:07:36 +0300

    add fks
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- src/migrations/20230817095805-client-applications-usage-table.js
+++ src/migrations/20230817095805-client-applications-usage-table.js
@@ -5,8 +5,8 @@ exports.up = function (db, callback) {
         `
           CREATE TABLE IF NOT EXISTS client_applications_usage (
                app_name VARCHAR(255) REFERENCES client_applications(app_name) ON DELETE CASCADE,
-               project VARCHAR(255) NULL REFERENCES projects(name) ON DELETE CASCADE,
-               environment VARCHAR(100) NULL REFERENCES environments(name) ON DELETE CASCADE,
+               project VARCHAR(255) REFERENCES projects(id) ON DELETE CASCADE,
+               environment VARCHAR(100) REFERENCES environments(name) ON DELETE CASCADE,
                PRIMARY KEY(app_name, project, environment)
           ) ;
         `,

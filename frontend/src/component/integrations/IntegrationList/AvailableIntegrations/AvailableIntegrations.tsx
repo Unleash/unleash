7b0fbb22d66af3ce5b5887154038b8878428c993
Tymoteusz Czech <tymek+gpg@getunleash.ai>
2023-08-18 13:54:08 +0200

    load cards into grid

--- frontend/src/component/integrations/IntegrationList/AvailableIntegrations/AvailableIntegrations.tsx
+++ frontend/src/component/integrations/IntegrationList/AvailableIntegrations/AvailableIntegrations.tsx
@@ -1,8 +1,34 @@
+import { type VFC } from 'react';
+import type { AddonTypeSchema } from 'openapi';
 import { PageContent } from 'component/common/PageContent/PageContent';
-import { VFC } from 'react';
+import { PageHeader } from 'component/common/PageHeader/PageHeader';
+import { IntegrationCard } from '../IntegrationCard/IntegrationCard';
+import { styled } from '@mui/material';
 
-interface IAvailableIntegrationsProps {}
+interface IAvailableIntegrationsProps {
+    providers: AddonTypeSchema[];
+}
 
-export const AvailableIntegrations: VFC<IAvailableIntegrationsProps> = () => {
-    return <PageContent>Available integrations</PageContent>;
+const StyledGrid = styled('div')(({ theme }) => ({
+    gridTemplateColumns: 'repeat(auto-fit, minmax(350px, 1fr))',
+    gap: theme.spacing(2),
+    display: 'grid',
+}));
+
+export const AvailableIntegrations: VFC<IAvailableIntegrationsProps> = ({
+    providers,
+}) => {
+    return (
+        <PageContent header={<PageHeader title="Available integrations" />}>
+            <StyledGrid>
+                {providers?.map(({ name, displayName, description }) => (
+                    <IntegrationCard
+                        key={name}
+                        title={displayName || name}
+                        description={description}
+                    />
+                ))}
+            </StyledGrid>
+        </PageContent>
+    );
 };

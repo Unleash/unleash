fb01ea840aeb68bfd64576fcfb57f23645afdfb7
Thomas Heartman <thomas@getunleash.io>
2023-09-05 11:58:37 +0200

    1-1319: add migration

---
+++ src/migrations/20230905095500-update-feature-naming-description.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.up = function (db, cb) {
+    db.runSql(
+        `
+        ALTER TABLE project_settings
+            ADD COLUMN IF NOT EXISTS "feature_naming_example" text;
+        `,
+        cb(),
+    );
+};
+
+exports.down = function (db, cb) {
+    db.runSql(
+        `
+        ALTER TABLE project_settings DROP COLUMN IF EXISTS "feature_naming_example";
+        `,
+        cb,
+    );
+};

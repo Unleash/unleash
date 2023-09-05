3e9e0a12538f18b313af0b2e58537e2cc65c2d3f
Thomas Heartman <thomas@getunleash.io>
2023-09-05 14:26:24 +0200

    1-1319-change-prompt-to-description: add new migration

--- src/migrations/20230905122605-add-feature-naming-description.js
+++ src/migrations/20230905122605-add-feature-naming-description.js
@@ -4,7 +4,7 @@ exports.up = function (db, cb) {
     db.runSql(
         `
         ALTER TABLE project_settings
-            ADD COLUMN IF NOT EXISTS "feature_naming_example" text;
+            ADD COLUMN IF NOT EXISTS "feature_naming_description" text;
         `,
         cb(),
     );
@@ -13,7 +13,7 @@ exports.up = function (db, cb) {
 exports.down = function (db, cb) {
     db.runSql(
         `
-        ALTER TABLE project_settings DROP COLUMN IF EXISTS "feature_naming_example";
+        ALTER TABLE project_settings DROP COLUMN IF EXISTS "feature_naming_description";
         `,
         cb,
     );

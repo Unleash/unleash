41858a4952226eb2f5a2e7d2448296cc7d55e5a9
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2023-09-05 15:31:31 +0200

    feat: Search suggestion selectable (#4610)

--- frontend/src/component/common/Search/Search.tsx
+++ frontend/src/component/common/Search/Search.tsx
@@ -7,6 +7,7 @@ import { SearchSuggestions } from './SearchSuggestions/SearchSuggestions';
 import { IGetSearchContextOutput } from 'hooks/useSearch';
 import { useKeyboardShortcut } from 'hooks/useKeyboardShortcut';
 import { SEARCH_INPUT } from 'utils/testIds';
+import { useOnClickOutside } from 'hooks/useOnClickOutside';
 
 interface ISearchProps {
     initialValue?: string;
@@ -69,8 +70,10 @@ export const Search = ({
     containerStyles,
     debounceTime = 200,
 }: ISearchProps) => {
-    const ref = useRef<HTMLInputElement>();
+    const searchInputRef = useRef<HTMLInputElement>(null);
+    const suggestionsRef = useRef<HTMLInputElement>(null);
     const [showSuggestions, setShowSuggestions] = useState(false);
+    const hideSuggestions = () => setShowSuggestions(false);
 
     const [value, setValue] = useState(initialValue);
     const debouncedOnChange = useAsyncDebounce(onChange, debounceTime);
@@ -83,19 +86,22 @@ export const Search = ({
     const hotkey = useKeyboardShortcut(
         { modifiers: ['ctrl'], key: 'k', preventDefault: true },
         () => {
-            if (document.activeElement === ref.current) {
-                ref.current?.blur();
+            if (document.activeElement === searchInputRef.current) {
+                searchInputRef.current?.blur();
             } else {
-                ref.current?.focus();
+                searchInputRef.current?.focus();
             }
         }
     );
     useKeyboardShortcut({ key: 'Escape' }, () => {
-        if (document.activeElement === ref.current) {
-            ref.current?.blur();
+        if (document.activeElement === searchInputRef.current) {
+            searchInputRef.current?.blur();
+            hideSuggestions();
         }
     });
     const placeholder = `${customPlaceholder ?? 'Search'} (${hotkey})`;
+
+    useOnClickOutside([searchInputRef, suggestionsRef], hideSuggestions);
 
     return (
         <StyledContainer style={containerStyles}>
@@ -107,7 +113,7 @@ export const Search = ({
                     }}
                 />
                 <StyledInputBase
-                    inputRef={ref}
+                    inputRef={searchInputRef}
                     placeholder={placeholder}
                     inputProps={{
                         'aria-label': placeholder,
@@ -116,7 +122,6 @@ export const Search = ({
                     value={value}
                     onChange={e => onSearchChange(e.target.value)}
                     onFocus={() => setShowSuggestions(true)}
-                    onBlur={() => setShowSuggestions(false)}
                     disabled={disabled}
                 />
                 <Box sx={{ width: theme => theme.spacing(4) }}>
@@ -128,7 +133,7 @@ export const Search = ({
                                     size="small"
                                     onClick={() => {
                                         onSearchChange('');
-                                        ref.current?.focus();
+                                        searchInputRef.current?.focus();
                                     }}
                                     sx={{ padding: theme => theme.spacing(1) }}
                                 >
@@ -142,7 +147,11 @@ export const Search = ({
             <ConditionallyRender
                 condition={Boolean(hasFilters) && showSuggestions}
                 show={
-                    <SearchSuggestions getSearchContext={getSearchContext!} />
+                    <div ref={suggestionsRef}>
+                        <SearchSuggestions
+                            getSearchContext={getSearchContext!}
+                        />
+                    </div>
                 }
             />
         </StyledContainer>

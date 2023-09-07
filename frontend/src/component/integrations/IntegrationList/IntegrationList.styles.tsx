777d53b025569cc59269df8001de5e20d89d36e3
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-09-07 11:12:25 +0200

    sections in available integrations
    Co-authored-by: Jaanus Sellin <sellinjaanus@gmail.com>

--- frontend/src/component/integrations/IntegrationList/IntegrationList.styles.tsx
+++ frontend/src/component/integrations/IntegrationList/IntegrationList.styles.tsx
@@ -1,8 +1,12 @@
 import { styled } from '@mui/material';
 
-export const StyledCardsGrid = styled('div')(({ theme }) => ({
-    gridTemplateColumns: 'repeat(auto-fit, minmax(350px, 1fr))',
-    gridAutoRows: '1fr',
-    gap: theme.spacing(2),
-    display: 'grid',
-}));
+export const StyledCardsGrid = styled('div')<{ small?: boolean }>(
+    ({ theme, small = false }) => ({
+        gridTemplateColumns: `repeat(auto-fit, minmax(${
+            small ? '250px' : '350px'
+        }, 1fr))`,
+        gridAutoRows: '1fr',
+        gap: theme.spacing(2),
+        display: 'grid',
+    })
+);

de796627399f54ade55bad098bda5081265eebfe
Thomas Heartman <thomas@getunleash.io>
2023-09-08 11:29:07 +0200

    1-1346: add getFetaureNamingPattern method to project service

--- src/lib/services/project-service.ts
+++ src/lib/services/project-service.ts
@@ -1026,4 +1026,10 @@ export default class ProjectService {
             version: 1,
         };
     }
+
+    async getFeatureNamingPattern(projectId: string): Promise<string | null> {
+        return (
+            (await this.store.get(projectId))?.featureNaming?.pattern ?? null
+        );
+    }
 }

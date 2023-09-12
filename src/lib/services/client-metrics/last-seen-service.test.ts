eae1a9710cfdb857c41e335da123e1b07ede58b3
sjaanus <sellinjaanus@gmail.com>
2023-09-12 11:26:40 +0300

    fix: last seen environment remove duplicates

---
+++ src/lib/services/client-metrics/last-seen-service.test.ts
@@ -0,0 +1,61 @@
+import createStores from '../../../test/fixtures/store';
+import EventEmitter from 'events';
+import getLogger from '../../../test/fixtures/no-logger';
+import { IUnleashConfig } from '../../types';
+import { LastSeenService } from './last-seen-service';
+
+function initLastSeenService(flagEnabled = true) {
+    const stores = createStores();
+
+    const eventBus = new EventEmitter();
+    eventBus.emit = jest.fn();
+
+    const config = {
+        eventBus,
+        getLogger,
+        flagResolver: {
+            isEnabled: () => {
+                return flagEnabled;
+            },
+        },
+    } as unknown as IUnleashConfig;
+
+    const lastSeenService = new LastSeenService(stores, config);
+
+    return { lastSeenService, featureToggleStore: stores.featureToggleStore };
+}
+
+test('process metrics properly', async () => {
+    const { lastSeenService, featureToggleStore } = initLastSeenService();
+
+    lastSeenService.updateLastSeen([
+        {
+            featureName: 'myFeature',
+            environment: 'development',
+            yes: 1,
+            no: 0,
+            appName: 'test',
+            timestamp: new Date(),
+        },
+    ]);
+
+    lastSeenService.updateLastSeen([
+        {
+            featureName: 'myFeature',
+            environment: 'development',
+            yes: 1,
+            no: 0,
+            appName: 'test',
+            timestamp: new Date(),
+        },
+    ]);
+    featureToggleStore.setLastSeen = jest.fn();
+    await lastSeenService.store();
+
+    expect(featureToggleStore.setLastSeen).toHaveBeenCalledWith([
+        {
+            environment: 'development',
+            featureName: 'myFeature',
+        },
+    ]);
+});

c55c01ed277d18620679008bf1f8df8719de2377
Thomas Heartman <thomas@getunleash.io>
2023-09-15 11:18:40 +0200

    fix: point to `useUiFlag` instead of `useUiFlags`
    I was a little confused because I couldn't find `useUiFlags` in the
    codebase.

--- frontend/src/interfaces/uiConfig.ts
+++ frontend/src/interfaces/uiConfig.ts
@@ -8,7 +8,7 @@ export interface IUiConfig {
      * @deprecated `useUiFlags` can be used instead
      * @example
      * ```ts
-     *   const yourFlag = useUiFlags("yourFlag")
+     *   const yourFlag = useUiFlag("yourFlag")
      * ```
      */
     flags: UiFlags;

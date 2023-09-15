6ad536fac2806f2f0b729dbf3968649d98c0351d
Thomas Heartman <thomas@getunleash.io>
2023-09-15 11:23:36 +0200

    refactor: make feature naming non-optional

--- src/lib/services/project-service.ts
+++ src/lib/services/project-service.ts
@@ -203,7 +203,9 @@ export default class ProjectService {
         const data = await projectSchema.validateAsync(newProject);
         await this.validateUniqueId(data.id);
 
-        this.validateAndProcessFeatureNamingPattern(data.featureNaming);
+        if (data.featureNaming) {
+            this.validateAndProcessFeatureNamingPattern(data.featureNaming);
+        }
 
         await this.store.create(data);
 
@@ -236,9 +238,11 @@ export default class ProjectService {
     async updateProject(updatedProject: IProject, user: User): Promise<void> {
         const preData = await this.store.get(updatedProject.id);
 
-        this.validateAndProcessFeatureNamingPattern(
-            updatedProject.featureNaming,
-        );
+        if (updatedProject.featureNaming) {
+            this.validateAndProcessFeatureNamingPattern(
+                updatedProject.featureNaming,
+            );
+        }
 
         await this.store.update(updatedProject);
 

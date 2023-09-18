40cf98c1b6d12d6ba9ea5b32799d7a1f76df9e8f
kwasniew <kwasniewski.mateusz@gmail.com>
2023-09-18 13:50:44 +0200

    fix: prevent blur when selecting text

--- frontend/src/hooks/useOnBlur.ts
+++ frontend/src/hooks/useOnBlur.ts
@@ -5,17 +5,27 @@ export const useOnBlur = (
     callback: () => void
 ): void => {
     useEffect(() => {
+        let mouseDownInside = false;
+
+        const handleMouseDown = (event: MouseEvent) => {
+            mouseDownInside = containerRef.current?.contains(event.target as Node) || false;
+        };
+
         const handleBlur = (event: FocusEvent) => {
-            // setTimeout is used because activeElement might not immediately be the new focused element after a blur event
             setTimeout(() => {
                 if (
+                    !mouseDownInside &&
                     containerRef.current &&
                     !containerRef.current.contains(document.activeElement)
                 ) {
                     callback();
                 }
+                // Reset the flag for the next sequence of events.
+                mouseDownInside = false;
             }, 0);
         };
+
+        document.addEventListener('mousedown', handleMouseDown);
 
         const containerElement = containerRef.current;
         if (containerElement) {
@@ -23,6 +33,7 @@ export const useOnBlur = (
         }
 
         return () => {
+            document.removeEventListener('mousedown', handleMouseDown);
             if (containerElement) {
                 containerElement.removeEventListener('blur', handleBlur, true);
             }

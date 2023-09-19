ecce3898b6fe27df3dc1a2178630c0a91fe80f1e
David Leek <david@getunleash.io>
2023-09-19 08:54:04 +0200

    feat: simplify ui

--- frontend/src/component/integrations/IntegrationForm/IntegrationParameters/IntegrationParameter/IntegrationParameter.tsx
+++ frontend/src/component/integrations/IntegrationForm/IntegrationParameters/IntegrationParameter/IntegrationParameter.tsx
@@ -1,7 +1,6 @@
 import { ChangeEventHandler } from 'react';
 import { StyledAddonParameterContainer } from '../../IntegrationForm.styles';
 import type { AddonParameterSchema, AddonSchema } from 'openapi';
-import { IntegrationParameterEnableWithDropdown } from './IntegrationParameterEnableWithDropdown';
 import { IntegrationParameterTextField } from './IntegrationParameterTextField';
 import { useUiFlag } from 'hooks/useUiFlag';
 
@@ -23,18 +22,6 @@ export const IntegrationParameter = ({
         if (!datadogJson) {
             return null;
         }
-
-        return (
-            <IntegrationParameterEnableWithDropdown
-                config={config}
-                definition={definition}
-                parametersErrors={parametersErrors}
-                setParameterValue={setParameterValue}
-                enableTitle="Data format"
-                disableOptionText="Standard"
-                enableOptionText="Custom template (JSON)"
-            />
-        );
     }
 
     return (

afed94f69b2220d3e2101c2865441f9b5dbcf31f
Nuno Góis <github@nunogois.com>
2023-09-26 10:51:43 +0100

    docs: update placeholder / example to be more generic

--- src/lib/addons/datadog-definition.ts
+++ src/lib/addons/datadog-definition.ts
@@ -55,8 +55,8 @@ const dataDogDefinition: IAddonDefinition = {
             name: 'customHeaders',
             displayName: 'Extra HTTP Headers',
             placeholder: `{
-  "ISTIO_USER_KEY": "hunter2",
-  "SOME_OTHER_CUSTOM_HTTP_HEADER": "SOMEVALUE"
+  "SOME_CUSTOM_HTTP_HEADER": "SOME_VALUE",
+  "SOME_OTHER_CUSTOM_HTTP_HEADER": "SOME_OTHER_VALUE"
 }`,
             description:
                 '(Optional) Used to add extra HTTP Headers to the request the plugin fires off. This must be a valid json object of key-value pairs where both the key and the value are strings',

ecc42ea9e8a5c313eb7e37b9433700c74dfb6046
Fredrik Oseberg <fredrik.no@gmail.com>
2023-09-27 10:20:01 +0200

    fix: alter migration to only delete given feedback older than 3 months

--- src/migrations/20230927071830-reset-pnps-feedback.js
+++ src/migrations/20230927071830-reset-pnps-feedback.js
@@ -3,7 +3,7 @@
 exports.up = function (db, callback) {
     db.runSql(
         `
-       DELETE FROM user_feedback WHERE feedback_id = 'pnps';
+       DELETE FROM user_feedback WHERE feedback_id = 'pnps' AND given < NOW() - INTERVAL '3 months';
         `,
         callback(),
     );

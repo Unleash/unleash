6953bf29da18e3a99fbf7664a8e89f42398f0f20
Fredrik Oseberg <fredrik.no@gmail.com>
2023-09-27 09:20:21 +0200

    feat: reset pnps feedback

---
+++ src/migrations/20230927071830-reset-pnps-feedback.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports.up = function (db, callback) {
+    db.runSql(
+        `
+       DELETE FROM user_feedback WHERE feedback_id = 'pnps'
+        `,
+        callback(),
+    );
+};
+
+exports.down = function (db, callback) {
+    callback();
+};

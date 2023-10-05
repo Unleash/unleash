e7ddc918849eec1e46fb5df994bf39ffc7f214c0
kwasniew <kwasniewski.mateusz@gmail.com>
2023-10-05 14:16:20 +0200

    test: playground with dependencies

--- frontend/src/component/playground/Playground/PlaygroundResultsTable/FeatureResultInfoPopoverCell/FeatureDetails/FeatureDetails.test.tsx
+++ frontend/src/component/playground/Playground/PlaygroundResultsTable/FeatureResultInfoPopoverCell/FeatureDetails/FeatureDetails.test.tsx
@@ -2,156 +2,94 @@ import { screen } from '@testing-library/react';
 import { render } from 'utils/testRenderer';
 import React from 'react';
 import { FeatureDetails } from './FeatureDetails';
-import {
-    PlaygroundFeatureSchema,
-    PlaygroundRequestSchema,
-} from 'openapi';
+import { PlaygroundFeatureSchema, PlaygroundRequestSchema } from 'openapi';
 
-test('Feature has unsatisfied parent dependency and feature environment is disabled', async () => {
-    render(
-        <FeatureDetails
-            feature={
-                {
-                    hasUnsatisfiedDependency: true,
-                    isEnabledInCurrentEnvironment: false,
-                } as PlaygroundFeatureSchema
-            }
-            input={{ environment: 'development' } as PlaygroundRequestSchema}
-            onClose={() => {}}
-        />,
-    );
+const testCases = [
+    {
+        name: 'Feature has unsatisfied parent dependency and feature environment is disabled',
+        feature: {
+            hasUnsatisfiedDependency: true,
+            isEnabledInCurrentEnvironment: false,
+        } as PlaygroundFeatureSchema,
+        expectedText1: 'This feature toggle is False in development because',
+        expectedText2:
+            'parent dependency is not satisfied and the environment is disabled',
+    },
+    {
+        name: 'Feature has unsatisfied parent dependency',
+        feature: {
+            hasUnsatisfiedDependency: true,
+            isEnabledInCurrentEnvironment: true,
+        } as PlaygroundFeatureSchema,
+        expectedText1: 'This feature toggle is False in development because',
+        expectedText2: 'parent dependency is not satisfied',
+    },
+    {
+        name: 'Feature environment is disabled',
+        feature: {
+            hasUnsatisfiedDependency: false,
+            isEnabledInCurrentEnvironment: false,
+        } as PlaygroundFeatureSchema,
+        expectedText1: 'This feature toggle is False in development because',
+        expectedText2: 'the environment is disabled',
+    },
+    {
+        name: 'Feature environment is enabled',
+        feature: {
+            isEnabled: true,
+        } as PlaygroundFeatureSchema,
+        expectedText1: 'This feature toggle is True in development because',
+        expectedText2: 'at least one strategy is True',
+    },
+    {
+        name: 'Feature has only custom strategies',
+        feature: {
+            isEnabledInCurrentEnvironment: true,
+            strategies: {
+                data: [{ name: 'custom' }],
+            },
+        } as PlaygroundFeatureSchema,
+        expectedText1: 'This feature toggle is Unknown in development because',
+        expectedText2: 'no strategies could be fully evaluated',
+    },
+    {
+        name: 'Feature has some custom strategies',
+        feature: {
+            isEnabledInCurrentEnvironment: true,
+            strategies: {
+                data: [{ name: 'custom' }, { name: 'default' }],
+            },
+        } as PlaygroundFeatureSchema,
+        expectedText1: 'This feature toggle is Unknown in development because',
+        expectedText2: 'not all strategies could be fully evaluated',
+    },
+    {
+        name: 'Feature has all strategies evaluating to false',
+        feature: {
+            isEnabledInCurrentEnvironment: true,
+            strategies: {
+                data: [{ name: 'default' }],
+            },
+        } as PlaygroundFeatureSchema,
+        expectedText1: 'This feature toggle is False in development because',
+        expectedText2:
+            'all strategies are either False or could not be fully evaluated',
+    },
+];
 
-    await screen.findByText(
-        'This feature toggle is False in development because',
-    );
-    await screen.findByText(
-        'parent dependency is not satisfied and the environment is disabled',
-    );
-});
+testCases.forEach(({ name, feature, expectedText1, expectedText2 }) => {
+    test(name, async () => {
+        render(
+            <FeatureDetails
+                feature={feature}
+                input={
+                    { environment: 'development' } as PlaygroundRequestSchema
+                }
+                onClose={() => {}}
+            />,
+        );
 
-test('Feature has unsatisfied parent dependency', async () => {
-    render(
-        <FeatureDetails
-            feature={
-                {
-                    hasUnsatisfiedDependency: true,
-                    isEnabledInCurrentEnvironment: true,
-                } as PlaygroundFeatureSchema
-            }
-            input={{ environment: 'development' } as PlaygroundRequestSchema}
-            onClose={() => {}}
-        />,
-    );
-
-    await screen.findByText(
-        'This feature toggle is False in development because',
-    );
-    await screen.findByText('parent dependency is not satisfied');
-});
-
-test('Feature environment is disabled', async () => {
-    render(
-        <FeatureDetails
-            feature={
-                {
-                    hasUnsatisfiedDependency: false,
-                    isEnabledInCurrentEnvironment: false,
-                } as PlaygroundFeatureSchema
-            }
-            input={{ environment: 'development' } as PlaygroundRequestSchema}
-            onClose={() => {}}
-        />,
-    );
-
-    await screen.findByText(
-        'This feature toggle is False in development because',
-    );
-    await screen.findByText('the environment is disabled');
-});
-
-test('Feature environment is enabled', async () => {
-    render(
-        <FeatureDetails
-            feature={
-                {
-                    isEnabled: true,
-                } as PlaygroundFeatureSchema
-            }
-            input={{ environment: 'development' } as PlaygroundRequestSchema}
-            onClose={() => {}}
-        />,
-    );
-
-    await screen.findByText(
-        'This feature toggle is True in development because',
-    );
-    await screen.findByText('at least one strategy is True');
-});
-
-test('Feature has only custom strategies', async () => {
-    render(
-        <FeatureDetails
-            feature={
-                {
-                    isEnabledInCurrentEnvironment: true,
-                    strategies: {
-                        data: [{ name: 'custom' }],
-                    },
-                } as PlaygroundFeatureSchema
-            }
-            input={{ environment: 'development' } as PlaygroundRequestSchema}
-            onClose={() => {}}
-        />,
-    );
-
-    await screen.findByText(
-        'This feature toggle is Unknown in development because',
-    );
-    await screen.findByText('no strategies could be fully evaluated');
-});
-
-test('Feature has some custom strategies', async () => {
-    render(
-        <FeatureDetails
-            feature={
-                {
-                    isEnabledInCurrentEnvironment: true,
-                    strategies: {
-                        data: [{ name: 'custom' }, { name: 'default' }],
-                    },
-                } as PlaygroundFeatureSchema
-            }
-            input={{ environment: 'development' } as PlaygroundRequestSchema}
-            onClose={() => {}}
-        />,
-    );
-
-    await screen.findByText(
-        'This feature toggle is Unknown in development because',
-    );
-    await screen.findByText('not all strategies could be fully evaluated');
-});
-
-test('Feature has all strategies evaluating to false', async () => {
-    render(
-        <FeatureDetails
-            feature={
-                {
-                    isEnabledInCurrentEnvironment: true,
-                    strategies: {
-                        data: [{ name: 'default' }],
-                    },
-                } as PlaygroundFeatureSchema
-            }
-            input={{ environment: 'development' } as PlaygroundRequestSchema}
-            onClose={() => {}}
-        />,
-    );
-
-    await screen.findByText(
-        'This feature toggle is False in development because',
-    );
-    await screen.findByText(
-        'all strategies are either False or could not be fully evaluated',
-    );
+        await screen.findByText(expectedText1);
+        await screen.findByText(expectedText2);
+    });
 });
--- frontend/src/component/playground/Playground/PlaygroundResultsTable/FeatureResultInfoPopoverCell/FeatureStrategyList/PlaygroundResultFeatureStrategyList.test.tsx
+++ frontend/src/component/playground/Playground/PlaygroundResultsTable/FeatureResultInfoPopoverCell/FeatureStrategyList/PlaygroundResultFeatureStrategyList.test.tsx
@@ -1,71 +1,80 @@
 import { screen } from '@testing-library/react';
 import { render } from 'utils/testRenderer';
 import React from 'react';
-import { PlaygroundFeatureSchema, PlaygroundRequestSchema } from '../../../../../../openapi';
+import { PlaygroundFeatureSchema, PlaygroundRequestSchema } from 'openapi';
 import { PlaygroundResultFeatureStrategyList } from './PlaygroundResultFeatureStrategyList';
 
-test('Environment not enabled and parent dependency not satisfied', async () => {
-    render(
-        <PlaygroundResultFeatureStrategyList
-            feature={
-                {
-                    strategies: {
-                        result: true,
-                        data: [{name: 'default', parameters: {}, result: {enabled: true, evaluationStatus: 'complete'}}]
+const testCases = [
+    {
+        name: 'Environment not enabled and parent dependency not satisfied',
+        feature: {
+            strategies: {
+                result: true,
+                data: [
+                    {
+                        name: 'default',
+                        parameters: {},
+                        result: { enabled: true, evaluationStatus: 'complete' },
                     },
-                    isEnabledInCurrentEnvironment: false,
-                    hasUnsatisfiedDependency: true
-                } as PlaygroundFeatureSchema
-            }
-            input={{ environment: 'development' } as PlaygroundRequestSchema}
-        />,
-    );
-
-    await screen.findByText(
-        'If environment was enabled and parent dependencies were satisfied, then this feature toggle would be TRUE with strategies evaluated like so:',
-    );
-});
-
-test('Environment enabled and parent dependency not satisfied', async () => {
-    render(
-        <PlaygroundResultFeatureStrategyList
-            feature={
-                {
-                    strategies: {
-                        result: true,
-                        data: [{name: 'default', parameters: {}, result: {enabled: true, evaluationStatus: 'complete'}}]
+                ],
+            },
+            isEnabledInCurrentEnvironment: false,
+            hasUnsatisfiedDependency: true,
+        } as PlaygroundFeatureSchema,
+        expectedText:
+            'If environment was enabled and parent dependencies were satisfied, then this feature toggle would be TRUE with strategies evaluated like so:',
+    },
+    {
+        name: 'Environment enabled and parent dependency not satisfied',
+        feature: {
+            strategies: {
+                result: true,
+                data: [
+                    {
+                        name: 'default',
+                        parameters: {},
+                        result: { enabled: true, evaluationStatus: 'complete' },
                     },
-                    isEnabledInCurrentEnvironment: true,
-                    hasUnsatisfiedDependency: true
-                } as PlaygroundFeatureSchema
-            }
-            input={{ environment: 'development' } as PlaygroundRequestSchema}
-        />,
-    );
-
-    await screen.findByText(
-        'If parent dependencies were satisfied, then this feature toggle would be TRUE with strategies evaluated like so:',
-    );
-});
-
-test('Environment not enabled and parent dependency satisfied', async () => {
-    render(
-        <PlaygroundResultFeatureStrategyList
-            feature={
-                {
-                    strategies: {
-                        result: true,
-                        data: [{name: 'default', parameters: {}, result: {enabled: true, evaluationStatus: 'complete'}}]
+                ],
+            },
+            isEnabledInCurrentEnvironment: true,
+            hasUnsatisfiedDependency: true,
+        } as PlaygroundFeatureSchema,
+        expectedText:
+            'If parent dependencies were satisfied, then this feature toggle would be TRUE with strategies evaluated like so:',
+    },
+    {
+        name: 'Environment not enabled and parent dependency satisfied',
+        feature: {
+            strategies: {
+                result: true,
+                data: [
+                    {
+                        name: 'default',
+                        parameters: {},
+                        result: { enabled: true, evaluationStatus: 'complete' },
                     },
-                    isEnabledInCurrentEnvironment: false,
-                    hasUnsatisfiedDependency: false
-                } as PlaygroundFeatureSchema
-            }
-            input={{ environment: 'development' } as PlaygroundRequestSchema}
-        />,
-    );
+                ],
+            },
+            isEnabledInCurrentEnvironment: false,
+            hasUnsatisfiedDependency: false,
+        } as PlaygroundFeatureSchema,
+        expectedText:
+            'If environment was enabled, then this feature toggle would be TRUE with strategies evaluated like so:',
+    },
+];
 
-    await screen.findByText(
-        'If environment was enabled, then this feature toggle would be TRUE with strategies evaluated like so:',
-    );
+testCases.forEach(({ name, feature, expectedText }) => {
+    test(name, async () => {
+        render(
+            <PlaygroundResultFeatureStrategyList
+                feature={feature}
+                input={
+                    { environment: 'development' } as PlaygroundRequestSchema
+                }
+            />,
+        );
+
+        await screen.findByText(expectedText);
+    });
 });

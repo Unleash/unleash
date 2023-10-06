132c87cb4fd045f18c9dbe6b053d1eeae7decdb6
kwasniew <kwasniewski.mateusz@gmail.com>
2023-10-06 09:54:13 +0200

    feat: allow to delete dependencies when no orphans

--- src/lib/features/dependent-features/dependent-features-read-model-type.ts
+++ src/lib/features/dependent-features/dependent-features-read-model-type.ts
@@ -2,6 +2,9 @@ import { IDependency } from '../../types';
 
 export interface IDependentFeaturesReadModel {
     getChildren(parents: string[]): Promise<string[]>;
+    // given a list of parents and children verifies if some children would be orphaned after deletion
+    // we're interested in the list of parents, not orphans
+    getOrphanParents(parentsAndChildren: string[]): Promise<string[]>;
     getParents(child: string): Promise<IDependency[]>;
     getParentOptions(child: string): Promise<string[]>;
     hasDependencies(feature: string): Promise<boolean>;

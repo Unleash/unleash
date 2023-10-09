79fb64deec76da9734da13d4bb20e2ccf00056e5
kwasniew <kwasniewski.mateusz@gmail.com>
2023-10-09 14:10:32 +0200

    feat: do not allow to manage dependencies directly with cr enabled

--- src/lib/features/dependent-features/dependent-features-service.ts
+++ src/lib/features/dependent-features/dependent-features-service.ts
@@ -1,24 +1,32 @@
-import { InvalidOperationError } from '../../error';
+import { InvalidOperationError, PermissionError } from '../../error';
 import { CreateDependentFeatureSchema } from '../../openapi';
 import { IDependentFeaturesStore } from './dependent-features-store-type';
 import { FeatureDependency, FeatureDependencyId } from './dependent-features';
 import { IDependentFeaturesReadModel } from './dependent-features-read-model-type';
 import { EventService } from '../../services';
+import { User } from '../../server-impl';
+import { SKIP_CHANGE_REQUEST } from '../../types';
+import { IChangeRequestAccessReadModel } from '../change-request-access-service/change-request-access-read-model';
+import { extractUsernameFromUser } from '../../util';
 
 export class DependentFeaturesService {
     private dependentFeaturesStore: IDependentFeaturesStore;
 
     private dependentFeaturesReadModel: IDependentFeaturesReadModel;
 
+    private changeRequestAccessReadModel: IChangeRequestAccessReadModel;
+
     private eventService: EventService;
 
     constructor(
         dependentFeaturesStore: IDependentFeaturesStore,
         dependentFeaturesReadModel: IDependentFeaturesReadModel,
+        changeRequestAccessReadModel: IChangeRequestAccessReadModel,
         eventService: EventService,
     ) {
         this.dependentFeaturesStore = dependentFeaturesStore;
         this.dependentFeaturesReadModel = dependentFeaturesReadModel;
+        this.changeRequestAccessReadModel = changeRequestAccessReadModel;
         this.eventService = eventService;
     }
 
@@ -35,7 +43,7 @@ export class DependentFeaturesService {
         );
         await Promise.all(
             parents.map((parent) =>
-                this.upsertFeatureDependency(
+                this.unprotectedUpsertFeatureDependency(
                     { child: newFeatureName, projectId },
                     {
                         feature: parent.feature,
@@ -49,6 +57,20 @@ export class DependentFeaturesService {
     }
 
     async upsertFeatureDependency(
+        { child, projectId }: { child: string; projectId: string },
+        dependentFeature: CreateDependentFeatureSchema,
+        user: User,
+    ): Promise<void> {
+        await this.stopWhenChangeRequestsEnabled(projectId, user);
+
+        return this.unprotectedUpsertFeatureDependency(
+            { child, projectId },
+            dependentFeature,
+            extractUsernameFromUser(user),
+        );
+    }
+
+    async unprotectedUpsertFeatureDependency(
         { child, projectId }: { child: string; projectId: string },
         dependentFeature: CreateDependentFeatureSchema,
         user: string,
@@ -94,6 +116,20 @@ export class DependentFeaturesService {
     async deleteFeatureDependency(
         dependency: FeatureDependencyId,
         projectId: string,
+        user: User,
+    ): Promise<void> {
+        await this.stopWhenChangeRequestsEnabled(projectId, user);
+
+        return this.unprotectedDeleteFeatureDependency(
+            dependency,
+            projectId,
+            extractUsernameFromUser(user),
+        );
+    }
+
+    async unprotectedDeleteFeatureDependency(
+        dependency: FeatureDependencyId,
+        projectId: string,
         user: string,
     ): Promise<void> {
         await this.dependentFeaturesStore.delete(dependency);
@@ -109,6 +145,20 @@ export class DependentFeaturesService {
     async deleteFeatureDependencies(
         feature: string,
         projectId: string,
+        user: User,
+    ): Promise<void> {
+        await this.stopWhenChangeRequestsEnabled(projectId, user);
+
+        return this.unprotectedDeleteFeatureDependencies(
+            feature,
+            projectId,
+            extractUsernameFromUser(user),
+        );
+    }
+
+    async unprotectedDeleteFeatureDependencies(
+        feature: string,
+        projectId: string,
         user: string,
     ): Promise<void> {
         await this.dependentFeaturesStore.deleteAll(feature);
@@ -122,5 +172,16 @@ export class DependentFeaturesService {
 
     async getParentOptions(feature: string): Promise<string[]> {
         return this.dependentFeaturesReadModel.getParentOptions(feature);
+    }
+
+    private async stopWhenChangeRequestsEnabled(project: string, user?: User) {
+        const canBypass =
+            await this.changeRequestAccessReadModel.canBypassChangeRequestForProject(
+                project,
+                user,
+            );
+        if (!canBypass) {
+            throw new PermissionError(SKIP_CHANGE_REQUEST);
+        }
     }
 }

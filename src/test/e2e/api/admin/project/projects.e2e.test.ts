df62131c2cf39e9b8fa68dbdb76437556c62e04f
Fredrik Oseberg <fredrik.no@gmail.com>
2023-10-10 08:31:42 +0200

    feat: add test for project overview last seen at

--- src/test/e2e/api/admin/project/projects.e2e.test.ts
+++ src/test/e2e/api/admin/project/projects.e2e.test.ts
@@ -1,12 +1,17 @@
-import dbInit from '../../../helpers/database-init';
-import { setupAppWithCustomConfig } from '../../../helpers/test-helper';
+import dbInit, { ITestDb } from '../../../helpers/database-init';
+import {
+    IUnleashTest,
+    setupAppWithCustomConfig,
+} from '../../../helpers/test-helper';
 import getLogger from '../../../../fixtures/no-logger';
 import ProjectStore from '../../../../../lib/db/project-store';
+import { Knex } from 'knex';
+import { IProjectStore } from 'lib/types';
 
-let app;
-let db;
+let app: IUnleashTest;
+let db: ITestDb;
 
-let projectStore: ProjectStore;
+let projectStore: IProjectStore;
 
 beforeAll(async () => {
     db = await dbInit('projects_api_serial', getLogger);
@@ -35,7 +40,6 @@ test('Should ONLY return default project', async () => {
         name: 'test',
         description: '',
         mode: 'open',
-        defaultStickiness: 'default',
     });
 
     const { body } = await app.request
@@ -61,4 +65,49 @@ test('response for default project should include created_at', async () => {
         .expect('Content-Type', /json/)
         .expect(200);
     expect(body.createdAt).toBeDefined();
+});
+
+test('response should include last seen at per environment', async () => {
+    await app.createFeature('my-new-feature-toggle');
+
+    await db.rawDatabase.raw(`
+        INSERT INTO last_seen_at_metrics (feature_name, environment, last_seen_at)
+        VALUES ('my-new-feature-toggle', 'default', '2023-10-01 12:34:56');
+    `);
+
+    await db.rawDatabase.raw(`
+        INSERT INTO feature_environments (feature_name, environment, last_seen_at, enabled)
+        VALUES ('my-new-feature-toggle', 'default', '2023-05-01 12:34:56', true)
+        ON CONFLICT (feature_name, environment) DO UPDATE SET last_seen_at = '2022-05-01 12:34:56', enabled = true;
+    `);
+
+    const { body } = await app.request
+        .get('/api/admin/projects/default')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+    expect(body.features[0].environments[0].lastSeenAt).toEqual(
+        '2022-05-01T12:34:56.000Z',
+    );
+
+    const appWithLastSeenRefactor = await setupAppWithCustomConfig(
+        db.stores,
+        {
+            experimental: {
+                flags: {
+                    useLastSeenRefactor: true,
+                },
+            },
+        },
+        db.rawDatabase,
+    );
+
+    const response = await appWithLastSeenRefactor.request
+        .get('/api/admin/projects/default')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+    expect(response.body.features[0].environments[0].lastSeenAt).toEqual(
+        '2023-10-01T12:34:56.000Z',
+    );
 });

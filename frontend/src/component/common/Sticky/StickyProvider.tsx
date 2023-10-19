aa1be17536be8cf40a8f3e86b3a65a9a6908aa9b
Nuno Góis <github@nunogois.com>
2023-10-19 10:47:38 +0100

    refactor: slight refactor to StickyProvider for readability

--- frontend/src/component/common/Sticky/StickyProvider.tsx
+++ frontend/src/component/common/Sticky/StickyProvider.tsx
@@ -13,8 +13,10 @@ export const StickyProvider = ({ children }: IStickyProviderProps) => {
     const registerStickyItem = useCallback(
         (item: RefObject<HTMLDivElement>) => {
             setStickyItems((prevItems) => {
-                if (item && !prevItems.includes(item)) {
+                // We should only register a new item if it is not already registered
+                if (!prevItems.includes(item)) {
                     const newItems = [...prevItems, item];
+                    // We should try to sort the items by their top on the viewport, so that their order in the DOM is the same as their order in the array
                     return newItems.sort((a, b) => {
                         const elementA = a.current;
                         const elementB = b.current;
@@ -43,20 +45,26 @@ export const StickyProvider = ({ children }: IStickyProviderProps) => {
 
     const getTopOffset = useCallback(
         (ref: RefObject<HTMLDivElement>) => {
-            return stickyItems.some((item) => item === ref)
-                ? stickyItems
-                      .slice(
-                          0,
-                          stickyItems.findIndex((item) => item === ref),
-                      )
-                      .reduce((acc, item) => {
-                          return item === ref
-                              ? acc
-                              : acc +
-                                      (item.current?.getBoundingClientRect()
-                                          .height || 0);
-                      }, 0)
-                : 0;
+            if (!stickyItems.some((item) => item === ref)) {
+                // Return 0 in case the item is not registered yet
+                return 0;
+            }
+            const stickyItemsUpToOurItem = stickyItems.slice(
+                0,
+                stickyItems.findIndex((item) => item === ref),
+            );
+            return stickyItemsUpToOurItem.reduce((acc, item) => {
+                if (item === ref) {
+                    // We should not include the current item in the calculation
+                    return acc;
+                }
+
+                // Accumulate the height of all sticky items above our item
+                const itemHeight =
+                    item.current?.getBoundingClientRect().height || 0;
+
+                return acc + itemHeight;
+            }, 0);
         },
         [stickyItems],
     );

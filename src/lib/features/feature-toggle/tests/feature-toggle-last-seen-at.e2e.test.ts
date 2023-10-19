d04cffee85caaf6d85001263b657bd866e0539d4
Fredrik Oseberg <fredrik.no@gmail.com>
2023-10-19 08:15:21 +0200

    fix: add tests for multiple environments

--- src/lib/features/feature-toggle/tests/feature-toggle-last-seen-at.e2e.test.ts
+++ src/lib/features/feature-toggle/tests/feature-toggle-last-seen-at.e2e.test.ts
@@ -52,3 +52,64 @@ test('should return last seen at per env for /api/admin/features', async () => {
         '2023-10-01T12:34:56.000Z',
     );
 });
+
+test('response should include last seen at per environment for multiple environments', async () => {
+    await db.stores.environmentStore.create({
+        name: 'development',
+        type: 'development',
+        sortOrder: 1,
+        enabled: true,
+    });
+
+    await db.stores.environmentStore.create({
+        name: 'production',
+        type: 'production',
+        sortOrder: 2,
+        enabled: true,
+    });
+
+    await app.services.projectService.addEnvironmentToProject(
+        'default',
+        'development',
+    );
+    await app.services.projectService.addEnvironmentToProject(
+        'default',
+        'production',
+    );
+
+    await app.createFeature('multiple-environment-last-seen-at');
+
+    await insertLastSeenAt(
+        'multiple-environment-last-seen-at',
+        db.rawDatabase,
+        'default',
+    );
+    await insertLastSeenAt(
+        'multiple-environment-last-seen-at',
+        db.rawDatabase,
+        'development',
+    );
+    await insertLastSeenAt(
+        'multiple-environment-last-seen-at',
+        db.rawDatabase,
+        'production',
+    );
+
+    const { body } = await app.request
+        .get('/api/admin/features')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+    const featureEnvironments = body.features[1].environments;
+
+    const [def, development, production] = featureEnvironments;
+
+    expect(def.name).toBe('default');
+    expect(def.lastSeenAt).toEqual('2023-10-01T12:34:56.000Z');
+
+    expect(development.name).toBe('development');
+    expect(development.lastSeenAt).toEqual('2023-10-01T12:34:56.000Z');
+
+    expect(production.name).toBe('production');
+    expect(production.lastSeenAt).toEqual('2023-10-01T12:34:56.000Z');
+});

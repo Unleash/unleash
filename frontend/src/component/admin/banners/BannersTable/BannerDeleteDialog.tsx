667aed828be813a9308e0afb626a627eb2f60c9f
Nuno Góis <github@nunogois.com>
2023-10-20 11:14:48 +0100

    feat: banners admin page (#5111)
    https://linear.app/unleash/issue/2-1484/ui-create-an-admin-banners-configuration-page
    
    Adds a new "Banners" page to the admin UI.
    This first iteration allows admins to list and preview all configured
    message banners, toggle them (whether they are currently visible to all
    users or not), and remove them.
    
    Next step will be creating the modal for "new" and "edit" operations.
    
    ### Admin menu
    
    ![image](https://github.com/Unleash/unleash/assets/14320932/39bcf575-b03a-481b-b19e-fc87697ed51c)
    
    ### Banners page
    
    ![image](https://github.com/Unleash/unleash/assets/14320932/39df6bc2-6949-4956-9dd0-0e5b1d2959f6)

---
+++ frontend/src/component/admin/banners/BannersTable/BannerDeleteDialog.tsx
@@ -0,0 +1,31 @@
+import { Dialogue } from 'component/common/Dialogue/Dialogue';
+import { IInternalBanner } from 'interfaces/banner';
+
+interface IBannerDeleteDialogProps {
+    banner?: IInternalBanner;
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    onConfirm: (banner: IInternalBanner) => void;
+}
+
+export const BannerDeleteDialog = ({
+    banner,
+    open,
+    setOpen,
+    onConfirm,
+}: IBannerDeleteDialogProps) => (
+    <Dialogue
+        title='Delete banner?'
+        open={open}
+        primaryButtonText='Delete banner'
+        secondaryButtonText='Cancel'
+        onClick={() => onConfirm(banner!)}
+        onClose={() => {
+            setOpen(false);
+        }}
+    >
+        <p>
+            You are about to delete banner: <strong>{banner?.message}</strong>
+        </p>
+    </Dialogue>
+);

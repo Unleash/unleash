cc422f73e3577d0054e6fa89ecbae25a933b7f49
Christopher Kolstad <chriswk@getunleash.io>
2023-10-23 15:08:54 +0200

    feat: replace gravatar-url with inline function
    fixes: Linear https://linear.app/unleash/issue/SR-112/gh-support-swap-out-gravatar-url-lib
    closes: #4475

---
+++ src/lib/util/generateImageUrl.test.ts
@@ -0,0 +1,21 @@
+import { generateImageUrl } from './generateImageUrl';
+
+describe('Gravatar image url', () => {
+    it('generates the correct sha-256 hash for gravatars test idents', () => {
+        expect(generateImageUrl({ email: 'MyEmailAddress@example.com' })).toBe(
+            'https://gravatar.com/avatar/84059b07d4be67b806386c0aad8070a23f18836bbaae342275dc0a83414c32ee?s=42&d=retro&r=g',
+        );
+    });
+    it('lowercases and trims all emails', () => {
+        const upperCaseAndLeadingSpace = ' helloWorld@example.com';
+        const upperCaseAndTrailingSpace = 'helloWorld@exAMPLE.com ';
+        const lowerCaseAndNoSpaces = 'helloworld@example.com';
+        const uCALSHash = generateImageUrl({ email: upperCaseAndLeadingSpace });
+        const uCATSHash = generateImageUrl({
+            email: upperCaseAndTrailingSpace,
+        });
+        const lCANSHash = generateImageUrl({ email: lowerCaseAndNoSpaces });
+        expect(uCALSHash).toBe(uCATSHash);
+        expect(uCATSHash).toBe(lCANSHash);
+    });
+});

Unleash/unleash

commit 347c1cabbc2e91cce7606482d554871597cf5326

feat: add new sticky component to handle stacked stickies (#5088)
https://linear.app/unleash/issue/2-1509/discovery-stacked-sticky-elements

Adds a new `Sticky` element that will attempt to stack sticky elements
in the DOM in a smart way.
This needs a wrapping `StickyProvider` that will keep track of sticky
elements.

This PR adapts a few components to use this new element:
 - `DemoBanner`
 - `FeatureOverviewSidePanel`
 - `DraftBanner`
 - `MaintenanceBanner`
 - `MessageBanner`

Pre-existing `top` properties are taken into consideration for the top
offset, so we can have nice margins like in the feature overview side
panel.

### Before - Sticky elements overlap 😞

![image](https://github.com/Unleash/unleash/assets/14320932/dd6fa188-6774-4afb-86fd-0eefb9aba93e)

### After - Sticky elements stack 😄 

![image](https://github.com/Unleash/unleash/assets/14320932/c73a84ab-7133-448f-9df6-69bd4c5330c2)

frontend/src/component/layout/MainLayout/DraftBanner/DraftBanner.tsx
@@ -5,6 +5,7 @@ import { ChangeRequestSidebar } from 'component/changeRequest/ChangeRequestSideb
 import { usePendingChangeRequests } from 'hooks/api/getters/usePendingChangeRequests/usePendingChangeRequests';
 import { IChangeRequest } from 'component/changeRequest/changeRequest.types';
 import { changesCount } from 'component/changeRequest/changesCount';
+import { Sticky } from 'component/common/Sticky/Sticky';
 
 interface IDraftBannerProps {
     project: string;
@@ -98,10 +99,7 @@ const DraftBannerContent: FC<{
     );
 };
 
-const StickyBanner = styled(Box)(({ theme }) => ({
-    position: 'sticky',
-    top: -1,
-    zIndex: 250 /* has to lower than header.zIndex and higher than body.zIndex */,
+const StickyBanner = styled(Sticky)(({ theme }) => ({
     borderTop: `1px solid ${theme.palette.warning.border}`,
     borderBottom: `1px solid ${theme.palette.warning.border}`,
     color: theme.palette.warning.contrastText,

frontend/src/component/banners/Banner/Banner.tsx
@@ -11,29 +11,22 @@ import { BannerDialog } from './BannerDialog/BannerDialog';
 import { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { BannerVariant, IBanner } from 'interfaces/banner';
+import { Sticky } from 'component/common/Sticky/Sticky';
 
 const StyledBar = styled('aside', {
-    shouldForwardProp: (prop) => prop !== 'variant' && prop !== 'sticky',
-})<{ variant: BannerVariant; sticky?: boolean }>(
-    ({ theme, variant, sticky }) => ({
-        position: sticky ? 'sticky' : 'relative',
-        zIndex: 1,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: theme.spacing(1),
-        gap: theme.spacing(1),
-        borderBottom: '1px solid',
-        borderColor: theme.palette[variant].border,
-        background: theme.palette[variant].light,
-        color: theme.palette[variant].dark,
-        fontSize: theme.fontSizes.smallBody,
-        ...(sticky && {
-            top: 0,
-            zIndex: theme.zIndex.sticky - 100,
-        }),
-    }),
-);
+    shouldForwardProp: (prop) => prop !== 'variant',
+})<{ variant: BannerVariant }>(({ theme, variant }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: theme.spacing(1),
+    gap: theme.spacing(1),
+    borderBottom: '1px solid',
+    borderColor: theme.palette[variant].border,
+    background: theme.palette[variant].light,
+    color: theme.palette[variant].dark,
+    fontSize: theme.fontSizes.smallBody,
+}));
 
 const StyledIcon = styled('div', {
     shouldForwardProp: (prop) => prop !== 'variant',
@@ -62,8 +55,8 @@ export const Banner = ({ banner }: IBannerProps) => {
         dialog,
     } = banner;
 
-    return (
-        <StyledBar variant={variant} sticky={sticky}>
+    const bannerBar = (
+        <StyledBar variant={variant}>
             <StyledIcon variant={variant}>
                 <BannerIcon icon={icon} variant={variant} />
             </StyledIcon>
@@ -84,6 +77,12 @@ export const Banner = ({ banner }: IBannerProps) => {
             </BannerDialog>
         </StyledBar>
     );
+
+    if (sticky) {
+        return <Sticky>{bannerBar}</Sticky>;
+    }
+
+    return bannerBar;
 };
 
 const VariantIcons = {

frontend/src/component/changeRequest/ChangeRequest.test.tsx
@@ -8,6 +8,7 @@ import { AccessProvider } from '../providers/AccessProvider/AccessProvider';
 import { AnnouncerProvider } from '../common/Announcer/AnnouncerProvider/AnnouncerProvider';
 import { testServerRoute, testServerSetup } from '../../utils/testServer';
 import { UIProviderContainer } from '../providers/UIProvider/UIProviderContainer';
+import { StickyProvider } from 'component/common/Sticky/StickyProvider';
 
 const server = testServerSetup();
 
@@ -227,12 +228,16 @@ const UnleashUiSetup: FC<{ path: string; pathTemplate: string }> = ({
             <MemoryRouter initialEntries={[path]}>
                 <ThemeProvider>
                     <AnnouncerProvider>
-                        <Routes>
-                            <Route
-                                path={pathTemplate}
-                                element={<MainLayout>{children}</MainLayout>}
-                            />
-                        </Routes>
+                        <StickyProvider>
+                            <Routes>
+                                <Route
+                                    path={pathTemplate}
+                                    element={
+                                        <MainLayout>{children}</MainLayout>
+                                    }
+                                />
+                            </Routes>
+                        </StickyProvider>
                     </AnnouncerProvider>
                 </ThemeProvider>
             </MemoryRouter>

frontend/src/component/changeRequest/ChangeRequestPermissions.test.tsx
@@ -10,6 +10,7 @@ import { FC } from 'react';
 import { IPermission } from '../../interfaces/user';
 import { SWRConfig } from 'swr';
 import { ProjectMode } from '../project/Project/hooks/useProjectEnterpriseSettingsForm';
+import { StickyProvider } from 'component/common/Sticky/StickyProvider';
 
 const server = testServerSetup();
 
@@ -186,9 +187,14 @@ const UnleashUiSetup: FC<{ path: string; pathTemplate: string }> = ({
                 <MemoryRouter initialEntries={[path]}>
                     <ThemeProvider>
                         <AnnouncerProvider>
-                            <Routes>
-                                <Route path={pathTemplate} element={children} />
-                            </Routes>
+                            <StickyProvider>
+                                <Routes>
+                                    <Route
+                                        path={pathTemplate}
+                                        element={children}
+                                    />
+                                </Routes>
+                            </StickyProvider>
                         </AnnouncerProvider>
                     </ThemeProvider>
                 </MemoryRouter>

frontend/src/component/common/Sticky/Sticky.test.tsx
@@ -1,0 +1,112 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import { Sticky } from './Sticky';
+import { IStickyContext, StickyContext } from './StickyContext';
+import { vi, expect } from 'vitest';
+
+describe('Sticky component', () => {
+    let originalConsoleError: () => void;
+    let mockRegisterStickyItem: () => void;
+    let mockUnregisterStickyItem: () => void;
+    let mockGetTopOffset: () => number;
+    let mockContextValue: IStickyContext;
+
+    beforeEach(() => {
+        originalConsoleError = console.error;
+        console.error = vi.fn();
+
+        mockRegisterStickyItem = vi.fn();
+        mockUnregisterStickyItem = vi.fn();
+        mockGetTopOffset = vi.fn(() => 10);
+
+        mockContextValue = {
+            registerStickyItem: mockRegisterStickyItem,
+            unregisterStickyItem: mockUnregisterStickyItem,
+            getTopOffset: mockGetTopOffset,
+            stickyItems: [],
+        };
+    });
+
+    afterEach(() => {
+        cleanup();
+        console.error = originalConsoleError;
+    });
+
+    it('renders correctly within StickyContext', () => {
+        render(
+            <StickyContext.Provider value={mockContextValue}>
+                <Sticky>Content</Sticky>
+            </StickyContext.Provider>,
+        );
+
+        expect(screen.getByText('Content')).toBeInTheDocument();
+    });
+
+    it('throws error when not wrapped in StickyContext', () => {
+        console.error = vi.fn();
+
+        expect(() => render(<Sticky>Content</Sticky>)).toThrow(
+            'Sticky component must be used within a StickyProvider',
+        );
+    });
+
+    it('applies sticky positioning', () => {
+        render(
+            <StickyContext.Provider value={mockContextValue}>
+                <Sticky>Content</Sticky>
+            </StickyContext.Provider>,
+        );
+
+        const stickyElement = screen.getByText('Content');
+        expect(stickyElement).toHaveStyle({ position: 'sticky' });
+    });
+
+    it('registers and unregisters sticky item on mount/unmount', () => {
+        const { unmount } = render(
+            <StickyContext.Provider value={mockContextValue}>
+                <Sticky>Content</Sticky>
+            </StickyContext.Provider>,
+        );
+
+        expect(mockRegisterStickyItem).toHaveBeenCalledTimes(1);
+
+        unmount();
+
+        expect(mockUnregisterStickyItem).toHaveBeenCalledTimes(1);
+    });
+
+    it('correctly sets the top value when mounted', async () => {
+        render(
+            <StickyContext.Provider value={mockContextValue}>
+                <Sticky>Content</Sticky>
+            </StickyContext.Provider>,
+        );
+
+        const stickyElement = await screen.findByText('Content');
+        expect(stickyElement).toHaveStyle({ top: '10px' });
+    });
+
+    it('updates top offset when stickyItems changes', async () => {
+        const { rerender } = render(
+            <StickyContext.Provider value={mockContextValue}>
+                <Sticky>Content</Sticky>
+            </StickyContext.Provider>,
+        );
+
+        let stickyElement = await screen.findByText('Content');
+        expect(stickyElement).toHaveStyle({ top: '10px' });
+
+        const updatedMockContextValue = {
+            ...mockContextValue,
+            getTopOffset: vi.fn(() => 20),
+        };
+
+        rerender(
+            <StickyContext.Provider value={updatedMockContextValue}>
+                <Sticky>Content</Sticky>
+            </StickyContext.Provider>,
+        );
+
+        stickyElement = await screen.findByText('Content');
+        expect(stickyElement).toHaveStyle({ top: '20px' });
+    });
+});

frontend/src/component/common/Sticky/Sticky.tsx
@@ -1,0 +1,80 @@
+import {
+    HTMLAttributes,
+    ReactNode,
+    useContext,
+    useEffect,
+    useRef,
+    useState,
+} from 'react';
+import { StickyContext } from './StickyContext';
+import { styled } from '@mui/material';
+
+const StyledSticky = styled('div', {
+    shouldForwardProp: (prop) => prop !== 'top',
+})<{ top?: number }>(({ theme, top }) => ({
+    position: 'sticky',
+    zIndex: theme.zIndex.sticky - 100,
+    ...(top !== undefined
+        ? {
+              '&': {
+                  top,
+              },
+          }
+        : {}),
+}));
+
+interface IStickyProps extends HTMLAttributes<HTMLDivElement> {
+    children: ReactNode;
+}
+
+export const Sticky = ({ children, ...props }: IStickyProps) => {
+    const context = useContext(StickyContext);
+    const ref = useRef<HTMLDivElement>(null);
+    const [initialTopOffset, setInitialTopOffset] = useState<number | null>(
+        null,
+    );
+    const [top, setTop] = useState<number>();
+
+    if (!context) {
+        throw new Error(
+            'Sticky component must be used within a StickyProvider',
+        );
+    }
+
+    const { registerStickyItem, unregisterStickyItem, getTopOffset } = context;
+
+    useEffect(() => {
+        // We should only set the initial top offset once - when the component is mounted
+        // This value will be set based on the initial top that was set for this component
+        // After that, the top will be calculated based on the height of the previous sticky items + this initial top offset
+        if (ref.current && initialTopOffset === null) {
+            setInitialTopOffset(
+                parseInt(getComputedStyle(ref.current).getPropertyValue('top')),
+            );
+        }
+    }, []);
+
+    useEffect(() => {
+        // (Re)calculate the top offset based on the sticky items
+        setTop(getTopOffset(ref) + (initialTopOffset || 0));
+    }, [getTopOffset, initialTopOffset]);
+
+    useEffect(() => {
+        // We should register the sticky item when it is mounted and unregister it when it is unmounted
+        if (!ref.current) {
+            return;
+        }
+
+        registerStickyItem(ref);
+
+        return () => {
+            unregisterStickyItem(ref);
+        };
+    }, [ref, registerStickyItem, unregisterStickyItem]);
+
+    return (
+        <StyledSticky ref={ref} top={top} {...props}>
+            {children}
+        </StyledSticky>
+    );
+};

frontend/src/component/common/Sticky/StickyContext.tsx
@@ -1,0 +1,12 @@
+import { RefObject, createContext } from 'react';
+
+export interface IStickyContext {
+    stickyItems: RefObject<HTMLDivElement>[];
+    registerStickyItem: (ref: RefObject<HTMLDivElement>) => void;
+    unregisterStickyItem: (ref: RefObject<HTMLDivElement>) => void;
+    getTopOffset: (ref: RefObject<HTMLDivElement>) => number;
+}
+
+export const StickyContext = createContext<IStickyContext | undefined>(
+    undefined,
+);

frontend/src/component/common/Sticky/StickyProvider.test.tsx
@@ -1,0 +1,160 @@
+import { render, cleanup } from '@testing-library/react';
+import { StickyProvider } from './StickyProvider';
+import { IStickyContext, StickyContext } from './StickyContext';
+import { expect } from 'vitest';
+
+const defaultGetBoundingClientRect = {
+    width: 0,
+    height: 0,
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    x: 0,
+    y: 0,
+    toJSON() {},
+};
+
+describe('StickyProvider component', () => {
+    afterEach(cleanup);
+
+    it('provides the sticky context with expected functions', () => {
+        let receivedContext = null;
+        render(
+            <StickyProvider>
+                <StickyContext.Consumer>
+                    {(context) => {
+                        receivedContext = context;
+                        return null;
+                    }}
+                </StickyContext.Consumer>
+            </StickyProvider>,
+        );
+
+        expect(receivedContext).not.toBeNull();
+        expect(receivedContext).toHaveProperty('stickyItems');
+        expect(receivedContext).toHaveProperty('registerStickyItem');
+        expect(receivedContext).toHaveProperty('unregisterStickyItem');
+        expect(receivedContext).toHaveProperty('getTopOffset');
+    });
+
+    it('registers and unregisters sticky items', () => {
+        let contextValues: IStickyContext | undefined;
+        const refMock = { current: document.createElement('div') };
+
+        const { rerender } = render(
+            <StickyProvider>
+                <StickyContext.Consumer>
+                    {(context) => {
+                        contextValues = context;
+                        return null;
+                    }}
+                </StickyContext.Consumer>
+            </StickyProvider>,
+        );
+
+        contextValues?.registerStickyItem(refMock);
+        rerender(
+            <StickyProvider>
+                <StickyContext.Consumer>
+                    {(context) => {
+                        contextValues = context;
+                        return null;
+                    }}
+                </StickyContext.Consumer>
+            </StickyProvider>,
+        );
+
+        expect(contextValues?.stickyItems).toContain(refMock);
+
+        contextValues?.unregisterStickyItem(refMock);
+        rerender(
+            <StickyProvider>
+                <StickyContext.Consumer>
+                    {(context) => {
+                        contextValues = context;
+                        return null;
+                    }}
+                </StickyContext.Consumer>
+            </StickyProvider>,
+        );
+
+        expect(contextValues?.stickyItems).not.toContain(refMock);
+    });
+
+    it('sorts sticky items based on their DOM position', () => {
+        let contextValues: IStickyContext | undefined;
+
+        const refMockA = { current: document.createElement('div') };
+        const refMockB = { current: document.createElement('div') };
+
+        refMockA.current.getBoundingClientRect = () => ({
+            ...defaultGetBoundingClientRect,
+            top: 200,
+        });
+        refMockB.current.getBoundingClientRect = () => ({
+            ...defaultGetBoundingClientRect,
+            top: 100,
+        });
+
+        render(
+            <StickyProvider>
+                <StickyContext.Consumer>
+                    {(context) => {
+                        contextValues = context;
+                        return null;
+                    }}
+                </StickyContext.Consumer>
+            </StickyProvider>,
+        );
+
+        contextValues?.registerStickyItem(refMockA);
+        contextValues?.registerStickyItem(refMockB);
+
+        expect(contextValues?.stickyItems[0]).toBe(refMockB);
+        expect(contextValues?.stickyItems[1]).toBe(refMockA);
+    });
+
+    it('calculates top offset correctly', () => {
+        let contextValues: IStickyContext | undefined;
+        const refMockA = { current: document.createElement('div') };
+        const refMockB = { current: document.createElement('div') };
+
+        refMockA.current.getBoundingClientRect = () => ({
+            ...defaultGetBoundingClientRect,
+            height: 100,
+        });
+
+        refMockB.current.getBoundingClientRect = () => ({
+            ...defaultGetBoundingClientRect,
+            height: 200,
+        });
+
+        const { rerender } = render(
+            <StickyProvider>
+                <StickyContext.Consumer>
+                    {(context) => {
+                        contextValues = context;
+                        return null;
+                    }}
+                </StickyContext.Consumer>
+            </StickyProvider>,
+        );
+
+        contextValues?.registerStickyItem(refMockA);
+        contextValues?.registerStickyItem(refMockB);
+        rerender(
+            <StickyProvider>
+                <StickyContext.Consumer>
+                    {(context) => {
+                        contextValues = context;
+                        return null;
+                    }}
+                </StickyContext.Consumer>
+            </StickyProvider>,
+        );
+
+        const topOffset = contextValues?.getTopOffset(refMockB);
+        expect(topOffset).toBe(100);
+    });
+});

commit 93fb6158410523bec6584a0fc29a72442d95ebb4

fix: draft banner zIndex (#5124)
Tiny zIndex fix for the draft banner for a regression introduced in
https://github.com/Unleash/unleash/pull/5088

### Before - Draft banner is displayed on top of the profile popup:

![image](https://github.com/Unleash/unleash/assets/14320932/63865f01-9bbe-42f1-9cc4-85c3c985334c)

### After - Profile popup displays on top of the draft banner:

![image](https://github.com/Unleash/unleash/assets/14320932/565e1017-5163-445d-bc0c-ee957023241b)

frontend/src/component/layout/MainLayout/DraftBanner/DraftBanner.tsx
@@ -104,6 +104,7 @@ const StickyBanner = styled(Sticky)(({ theme }) => ({
     borderBottom: `1px solid ${theme.palette.warning.border}`,
     color: theme.palette.warning.contrastText,
     backgroundColor: theme.palette.warning.light,
+    zIndex: 250,
 }));
 
 export const DraftBanner: VFC<IDraftBannerProps> = ({ project }) => {
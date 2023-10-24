939a32ad0aae21232b9b0da29e24a6c353927dbd
sjaanus <sellinjaanus@gmail.com>
2023-10-24 11:58:50 +0300

    bug: remove strategies from copy breadcrumbs

--- frontend/src/component/feature/FeatureView/FeatureView.tsx
+++ frontend/src/component/feature/FeatureView/FeatureView.tsx
@@ -248,7 +248,7 @@ export const FeatureView = () => {
                             projectId={projectId}
                             data-loading
                             component={Link}
-                            to={`/projects/${projectId}/features/${featureId}/strategies/copy`}
+                            to={`/projects/${projectId}/features/${featureId}/copy`}
                             tooltipProps={{
                                 title: 'Copy feature toggle',
                             }}

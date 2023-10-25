25cfc4160e713847b7768ef07c77362801535225
Fredrik Oseberg <fredrik.no@gmail.com>
2023-10-25 15:01:50 +0200

    fix: lint

--- frontend/src/component/project/Project/ProjectOverview.tsx
+++ frontend/src/component/project/Project/ProjectOverview.tsx
@@ -1,42 +1,42 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 import useProject, {
     useProjectNameOrId,
-} from "hooks/api/getters/useProject/useProject";
-import { Box, styled } from "@mui/material";
-import { ProjectFeatureToggles as LegacyProjectFeatureToggles } from "./ProjectFeatureToggles/LegacyProjectFeatureToggles";
-import { ProjectFeatureToggles } from "./ProjectFeatureToggles/ProjectFeatureToggles";
-import ProjectInfo from "./ProjectInfo/ProjectInfo";
-import { usePageTitle } from "hooks/usePageTitle";
-import { useRequiredPathParam } from "hooks/useRequiredPathParam";
-import { useLastViewedProject } from "hooks/useLastViewedProject";
-import { ProjectStats } from "./ProjectStats/ProjectStats";
-import { ConditionallyRender } from "component/common/ConditionallyRender/ConditionallyRender";
-import { useUiFlag } from "hooks/useUiFlag";
-import { useFeatureSearch } from "hooks/api/getters/useFeatureSearch/useFeatureSearch";
+} from 'hooks/api/getters/useProject/useProject';
+import { Box, styled } from '@mui/material';
+import { ProjectFeatureToggles as LegacyProjectFeatureToggles } from './ProjectFeatureToggles/LegacyProjectFeatureToggles';
+import { ProjectFeatureToggles } from './ProjectFeatureToggles/ProjectFeatureToggles';
+import ProjectInfo from './ProjectInfo/ProjectInfo';
+import { usePageTitle } from 'hooks/usePageTitle';
+import { useRequiredPathParam } from 'hooks/useRequiredPathParam';
+import { useLastViewedProject } from 'hooks/useLastViewedProject';
+import { ProjectStats } from './ProjectStats/ProjectStats';
+import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
+import { useUiFlag } from 'hooks/useUiFlag';
+import { useFeatureSearch } from 'hooks/api/getters/useFeatureSearch/useFeatureSearch';
 
 const refreshInterval = 15 * 1000;
 
-const StyledContainer = styled("div")(({ theme }) => ({
-    display: "flex",
-    [theme.breakpoints.down("md")]: {
-        flexDirection: "column",
+const StyledContainer = styled('div')(({ theme }) => ({
+    display: 'flex',
+    [theme.breakpoints.down('md')]: {
+        flexDirection: 'column',
     },
 }));
 
-const StyledProjectToggles = styled("div")(() => ({
-    width: "100%",
+const StyledProjectToggles = styled('div')(() => ({
+    width: '100%',
     minWidth: 0,
 }));
 
 const StyledContentContainer = styled(Box)(() => ({
-    display: "flex",
-    flexDirection: "column",
-    width: "100%",
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
     minWidth: 0,
 }));
 
 const ProjectOverview = () => {
-    const projectId = useRequiredPathParam("projectId");
+    const projectId = useRequiredPathParam('projectId');
     const projectName = useProjectNameOrId(projectId);
     const { project, loading } = useProject(projectId, {
         refreshInterval,
@@ -45,8 +45,7 @@ const ProjectOverview = () => {
         project;
     usePageTitle(`Project overview – ${projectName}`);
     const { setLastViewed } = useLastViewedProject();
-    const featureSwitchRefactor = useUiFlag("featureSwitchRefactor");
-
+    const featureSwitchRefactor = useUiFlag('featureSwitchRefactor');
 
     useEffect(() => {
         setLastViewed(projectId);
@@ -69,7 +68,7 @@ const ProjectOverview = () => {
                         condition={Boolean(featureSwitchRefactor)}
                         show={() => (
                             <ProjectFeatureToggles
-                                key={loading ? "loading" : "ready"}
+                                key={loading ? 'loading' : 'ready'}
                                 features={features}
                                 environments={environments}
                                 loading={loading}
@@ -77,7 +76,7 @@ const ProjectOverview = () => {
                         )}
                         elseShow={() => (
                             <LegacyProjectFeatureToggles
-                                key={loading ? "loading" : "ready"}
+                                key={loading ? 'loading' : 'ready'}
                                 features={features}
                                 environments={environments}
                                 loading={loading}

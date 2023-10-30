6e242ee0acfa977e3f467261b08a33f863f1e761
andreas-unleash <andreas@getunleash.ai>
2023-10-30 11:53:18 +0200

    feat: add created_by and status columns to change_request_schedule
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- src/migrations/20231030091931-add-created-by-and-status-change-request-schedule.js
+++ src/migrations/20231030091931-add-created-by-and-status-change-request-schedule.js
@@ -2,11 +2,13 @@
 
 exports.up = function (db, cb) {
   db.runSql(`
+        CREATE TYPE change_request_schedule_status AS ENUM ('pending', 'failed');
+
         ALTER TABLE change_request_schedule
             ADD COLUMN IF NOT EXISTS created_by integer REFERENCES users(id);
 
         ALTER TABLE change_request_schedule
-            ADD COLUMN IF NOT EXISTS status text default 'pending';
+            ADD COLUMN IF NOT EXISTS status change_request_schedule_status default 'pending';
 
     `, cb);
 };
@@ -18,5 +20,7 @@ exports.down = function (db, cb) {
 
       ALTER TABLE change_request_schedule
           DROP COLUMN IF EXISTS status;
+
+      DROP TYPE change_request_schedule_status
     `, cb);
 };

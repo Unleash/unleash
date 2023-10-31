b958b711846a71c400f5beecbd908dca0f7cc95a
kwasniew <kwasniewski.mateusz@gmail.com>
2023-10-31 08:42:35 +0100

    feat: infinite scroll

--- frontend/src/component/project/Project/ProjectFeatureToggles/ProjectFeatureFlags.tsx
+++ frontend/src/component/project/Project/ProjectFeatureToggles/ProjectFeatureFlags.tsx
@@ -1,20 +1,43 @@
+import React, { useState, useEffect, useCallback } from 'react';
 import { useFeatureSearch } from 'hooks/api/getters/useFeatureSearch/useFeatureSearch';
-import { Table, TableCell } from '../../../common/Table';
-import { TableBody, TableHead, TableRow } from '@mui/material';
-import { useEffect, useState } from 'react';
-import InfiniteScroll from 'react-infinite-scroller';
+import { Paper, TableCell } from '@mui/material';
+import { FixedSizeList as List, areEqual } from 'react-window';
+import InfiniteLoader from 'react-window-infinite-loader';
 import { IFeatureToggleListItem } from '../../../../interfaces/featureToggle';
-import { useFeatureToggleSwitch } from './FeatureToggleSwitch/useFeatureToggleSwitch';
-import { useRequiredPathParam } from '../../../../hooks/useRequiredPathParam';
-import useProject from '../../../../hooks/api/getters/useProject/useProject';
-import { useChangeRequestsEnabled } from '../../../../hooks/useChangeRequestsEnabled';
 import { FeatureToggleSwitch } from './FeatureToggleSwitch/FeatureToggleSwitch';
 
-interface DataItem {
-    name: string;
-    type: string;
+// Additional imports
+import { useRequiredPathParam } from '../../../../hooks/useRequiredPathParam';
+import { useFeatureToggleSwitch } from './FeatureToggleSwitch/useFeatureToggleSwitch';
+
+interface RowProps {
+    index: number;
+    style: React.CSSProperties;
+    data: IFeatureToggleListItem[];
+    projectId: string;
 }
 
+const Row: React.FC<RowProps> = React.memo(({ index, style, data, projectId }) => {
+    const row = data[index];
+    if (!row) {
+        return <div style={style}>Loading...</div>;
+    }
+    return (
+        <div style={{ ...style, display: 'flex' }} key={row.name}>
+            <TableCell component="div" style={{ flexGrow: 1 }}>{row.name}</TableCell>
+            <TableCell component="div" style={{ flexGrow: 1 }}>{row.type}</TableCell>
+            <TableCell component="div" style={{ flexGrow: 1 }}>
+                <FeatureToggleSwitch
+                    projectId={projectId}
+                    value={false} // Assuming a default value
+                    featureId={row.name}
+                    environmentName={'development'}
+                    onToggle={() => {}} // Replace with your actual toggle function
+                />
+            </TableCell>
+        </div>
+    );
+}, areEqual);
 
 export const ProjectFeatureFlags = () => {
     const [dataList, setDataList] = useState<IFeatureToggleListItem[]>([]);
@@ -22,67 +45,57 @@ export const ProjectFeatureFlags = () => {
     const [hasMore, setHasMore] = useState(true);
     const { features, loading } = useFeatureSearch(nextCursor);
     const projectId = useRequiredPathParam('projectId');
-    const { onToggle: onFeatureToggle, modals: featureToggleModals } =
-        useFeatureToggleSwitch(projectId);
-    const { refetch } = useProject(projectId);
-    const { isChangeRequestConfigured } = useChangeRequestsEnabled(projectId);
+    const { onToggle: onFeatureToggle, modals: featureToggleModals } = useFeatureToggleSwitch(projectId);
 
-    const loadMore = () => {
-        console.log('loadMore', hasMore);
-        if (hasMore && features.length > 0) {
-            console.log('next cursor', features[features.length - 1].createdAt);
-            setNextCursor(features[features.length - 1].createdAt);
+    const isItemLoaded = useCallback((index) => !!dataList[index], [dataList]);
+    const itemCount = hasMore ? dataList.length + 1 : dataList.length;
+
+    const loadMoreItems = useCallback(async () => {
+        if (features.length > 0) {
+            const newCursor = features[features.length - 1].createdAt;
+            setNextCursor(newCursor);
         }
-    };
+    }, [features]);
 
     useEffect(() => {
-        if(features && features.length === 0) {
+        if (features && features.length === 0) {
             setHasMore(false);
         }
-        if(features && features.length > 0) {
-            setDataList(prev => [...prev, ...features]);
+        if (features && features.length > 0) {
+            setDataList((prev) => [...prev, ...features]);
             setHasMore(true);
         }
-    }, [JSON.stringify(features)]);
+    }, [features]);
 
-    return <InfiniteScroll
-        pageStart={0}
-        loadMore={loadMore}
-        hasMore={hasMore}
-        loader={<h4>Loading...</h4>}
-        useWindow={true}
-    >
-        <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell>Name</TableCell>
-                    <TableCell>Type</TableCell>
-                    <TableCell>Development</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-
-                {dataList.map((row) => {
-                    return <TableRow
-                        key={row.name}
-                    >
-                        <TableCell sx={{pt: 10}}>
-                            {row.name}
-                        </TableCell>
-                        <TableCell sx={{pt: 10}}>{row.type}</TableCell>
-                        <TableCell>
-                            <FeatureToggleSwitch
-                                projectId={projectId}
-                                value={false}
-                                featureId={row.name}
-                                environmentName={'development'}
-                                onToggle={() => {}}
-                            />
-                        </TableCell>
-                    </TableRow>;
-                })}
-
-            </TableBody>
-        </Table>
-    </InfiniteScroll>;
+    return (
+        <Paper style={{ height: '100vh', width: '100%' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+                <div style={{ display: 'flex' }}>
+                    <TableCell component="div" style={{ flexGrow: 1, fontWeight: 'bold' }}>Name</TableCell>
+                    <TableCell component="div" style={{ flexGrow: 1, fontWeight: 'bold' }}>Type</TableCell>
+                    <TableCell component="div" style={{ flexGrow: 1, fontWeight: 'bold' }}>Development</TableCell>
+                </div>
+                <InfiniteLoader
+                    isItemLoaded={isItemLoaded}
+                    itemCount={itemCount}
+                    loadMoreItems={loadMoreItems}
+                >
+                    {({ onItemsRendered, ref }) => (
+                        <List
+                            height={Math.min(itemCount * 53, window.innerHeight - 53)} // Adjust the height dynamically
+                            itemCount={itemCount}
+                            itemSize={53} // Adjust the item size to fit your design
+                            onItemsRendered={onItemsRendered}
+                            ref={ref}
+                            width={'100%'} // Adjust the width to fit your design
+                            itemData={dataList} // Pass the data as itemData
+                            project={projectId} // Pass the projectId as project
+                        >
+                            {Row}
+                        </List>
+                    )}
+                </InfiniteLoader>
+            </div>
+        </Paper>
+    );
 };

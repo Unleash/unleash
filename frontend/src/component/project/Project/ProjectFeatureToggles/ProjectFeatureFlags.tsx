866a2ffb6c8c5cf4f1a6457bf2f5023f002cbadc
kwasniew <kwasniewski.mateusz@gmail.com>
2023-10-31 08:12:35 +0100

    feat: infinite scroll

--- frontend/src/component/project/Project/ProjectFeatureToggles/ProjectFeatureFlags.tsx
+++ frontend/src/component/project/Project/ProjectFeatureToggles/ProjectFeatureFlags.tsx
@@ -1,54 +1,88 @@
 import { useFeatureSearch } from 'hooks/api/getters/useFeatureSearch/useFeatureSearch';
 import { Table, TableCell } from '../../../common/Table';
 import { TableBody, TableHead, TableRow } from '@mui/material';
-import { useState } from 'react';
-import { FixedSizeList as List, ListChildComponentProps } from 'react-window';
+import { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
+import { IFeatureToggleListItem } from '../../../../interfaces/featureToggle';
+import { useFeatureToggleSwitch } from './FeatureToggleSwitch/useFeatureToggleSwitch';
+import { useRequiredPathParam } from '../../../../hooks/useRequiredPathParam';
+import useProject from '../../../../hooks/api/getters/useProject/useProject';
+import { useChangeRequestsEnabled } from '../../../../hooks/useChangeRequestsEnabled';
+import { FeatureToggleSwitch } from './FeatureToggleSwitch/FeatureToggleSwitch';
 
 interface DataItem {
     name: string;
     type: string;
 }
 
-const RowRenderer: React.FC<ListChildComponentProps> = ({ index, style, data }) => {
-    const row = (data as DataItem[])[index];
-    return (
-        <TableRow
-            key={row.name}
-        >
-            <TableCell>
-                {row.name}
-            </TableCell>
-            <TableCell>{row.type}</TableCell>
-        </TableRow>
-    );
-};
 
 export const ProjectFeatureFlags = () => {
-    const [cursor, setCursor] = useState('');
-    const { features, loading } = useFeatureSearch(cursor);
+    const [dataList, setDataList] = useState<IFeatureToggleListItem[]>([]);
+    const [nextCursor, setNextCursor] = useState('');
+    const [hasMore, setHasMore] = useState(true);
+    const { features, loading } = useFeatureSearch(nextCursor);
+    const projectId = useRequiredPathParam('projectId');
+    const { onToggle: onFeatureToggle, modals: featureToggleModals } =
+        useFeatureToggleSwitch(projectId);
+    const { refetch } = useProject(projectId);
+    const { isChangeRequestConfigured } = useChangeRequestsEnabled(projectId);
 
-    return <Table>
-        <TableHead onClick={() => {
-            if (features.length) {
-                setCursor(features[features.length - 1].createdAt);
-            }
-        }}>
-            <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell>Type</TableCell>
-                {/* ... other columns with sorting ... */}
-            </TableRow>
-        </TableHead>
-        <TableBody>
-            <List
-                height={400} // Adjust as needed
-                itemCount={features.length}
-                itemSize={50} // Height of each row. Adjust as needed.
-                width="100%"
-                itemData={features}
-            >
-                {RowRenderer}
-            </List>
-        </TableBody>
-    </Table>;
+    const loadMore = () => {
+        console.log('loadMore', hasMore);
+        if (hasMore && features.length > 0) {
+            console.log('next cursor', features[features.length - 1].createdAt);
+            setNextCursor(features[features.length - 1].createdAt);
+        }
+    };
+
+    useEffect(() => {
+        if(features && features.length === 0) {
+            setHasMore(false);
+        }
+        if(features && features.length > 0) {
+            setDataList(prev => [...prev, ...features]);
+            setHasMore(true);
+        }
+    }, [JSON.stringify(features)]);
+
+    return <InfiniteScroll
+        pageStart={0}
+        loadMore={loadMore}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        useWindow={true}
+    >
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Type</TableCell>
+                    <TableCell>Development</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+
+                {dataList.map((row) => {
+                    return <TableRow
+                        key={row.name}
+                    >
+                        <TableCell sx={{pt: 10}}>
+                            {row.name}
+                        </TableCell>
+                        <TableCell sx={{pt: 10}}>{row.type}</TableCell>
+                        <TableCell>
+                            <FeatureToggleSwitch
+                                projectId={projectId}
+                                value={false}
+                                featureId={row.name}
+                                environmentName={'development'}
+                                onToggle={() => {}}
+                            />
+                        </TableCell>
+                    </TableRow>;
+                })}
+
+            </TableBody>
+        </Table>
+    </InfiniteScroll>;
 };

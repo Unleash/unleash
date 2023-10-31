cfa5c766381120489dfdc467ed479f0a3058472b
Fredrik Oseberg <fredrik.no@gmail.com>
2023-10-31 11:29:34 +0100

    refactor: table component for react window

--- frontend/src/component/project/Project/ProjectFeatureToggles/ProjectFeatureFlags.tsx
+++ frontend/src/component/project/Project/ProjectFeatureToggles/ProjectFeatureFlags.tsx
@@ -1,6 +1,14 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useFeatureSearch } from 'hooks/api/getters/useFeatureSearch/useFeatureSearch';
-import { TableCell } from '@mui/material';
+import {
+    Table,
+    TableCell,
+    useTheme,
+    TableHead,
+    TableRow,
+    Checkbox,
+    TableBody,
+} from '@mui/material';
 import { FixedSizeList as VirtualList, areEqual } from 'react-window';
 import InfiniteLoader from 'react-window-infinite-loader';
 import { IFeatureToggleListItem } from '../../../../interfaces/featureToggle';
@@ -9,6 +17,11 @@ import { FeatureToggleSwitch } from './FeatureToggleSwitch/FeatureToggleSwitch';
 // Additional imports
 import { useRequiredPathParam } from '../../../../hooks/useRequiredPathParam';
 import { useFeatureToggleSwitch } from './FeatureToggleSwitch/useFeatureToggleSwitch';
+import { PageContent } from 'component/common/PageContent/PageContent';
+import { CellSortable } from 'component/common/Table/SortableTableHeader/CellSortable/CellSortable';
+import { FavoriteIconHeader } from 'component/common/Table/FavoriteIconHeader/FavoriteIconHeader';
+import { FavoriteIconCell } from 'component/common/Table/cells/FavoriteIconCell/FavoriteIconCell';
+import AutoSizer from 'react-virtualized-auto-sizer';
 
 interface RowProps {
     index: number;
@@ -17,48 +30,112 @@ interface RowProps {
     projectId: string;
 }
 
-const Row: React.FC<RowProps> = React.memo(({ index, style, data, projectId }) => {
-    const row = data[index];
-    if (!row) {
-        return <div style={style}>Loading...</div>;
-    }
-    return (
-        <div style={{ ...style, display: 'flex' }} key={row.name}>
-            <TableCell component="div" style={{ flexGrow: 1 }}>{row.name}</TableCell>
-            <TableCell component="div" style={{ flexGrow: 1 }}>{row.type}</TableCell>
-            <TableCell component="div" style={{ flexGrow: 1 }}>
-                <FeatureToggleSwitch
-                    projectId={projectId}
-                    value={false} // Assuming a default value
-                    featureId={row.name}
-                    environmentName={'development'}
-                    onToggle={() => {}} // Replace with your actual toggle function
-                />
-            </TableCell>
-        </div>
-    );
-}, areEqual);
+const Row: React.FC<RowProps> = React.memo(
+    ({ index, style, data, projectId }) => {
+        const row = data[index];
+        if (!row) {
+            return <TableRow style={style}>Loading...</TableRow>;
+        }
+        return (
+            <TableRow
+                style={{
+                    ...style,
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+                key={row.name}
+            >
+                <TableCell
+                    style={{
+                        width: 65,
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <Checkbox />
+                </TableCell>
+                <TableCell
+                    style={{
+                        width: 50,
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <FavoriteIconCell value={true} onClick={() => {}} />
+                </TableCell>
+                <TableCell
+                    component='td'
+                    style={{
+                        flexGrow: 1,
+                        width: 150,
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    {row.name}
+                </TableCell>
+                <TableCell
+                    component='td'
+                    style={{
+                        flexGrow: 1,
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    {row.type}
+                </TableCell>
+                <TableCell
+                    component='td'
+                    style={{
+                        flexGrow: 1,
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}
+                >
+                    <FeatureToggleSwitch
+                        projectId={projectId}
+                        value={false} // Assuming a default value
+                        featureId={row.name}
+                        environmentName={'development'}
+                        onToggle={() => {}} // Replace with your actual toggle function
+                    />
+                </TableCell>
+            </TableRow>
+        );
+    },
+    areEqual,
+);
 
 export const ProjectFeatureFlags = () => {
+    const theme = useTheme();
     const [dataList, setDataList] = useState<IFeatureToggleListItem[]>([]);
     const [nextCursor, setNextCursor] = useState('');
     const [hasMore, setHasMore] = useState(true);
     const { features, loading } = useFeatureSearch(nextCursor);
     const projectId = useRequiredPathParam('projectId');
-    const { onToggle: onFeatureToggle, modals: featureToggleModals } = useFeatureToggleSwitch(projectId);
+    const { onToggle: onFeatureToggle, modals: featureToggleModals } =
+        useFeatureToggleSwitch(projectId);
 
     const isItemLoaded = useCallback((index) => !!dataList[index], [dataList]);
     const itemCount = hasMore ? dataList.length + 1 : dataList.length;
 
-    const loadMoreItems = useCallback(async (startIndex, stopIndex) => {
-        // Only load more items if the last item is within the range of what's visible
-        if (stopIndex >= dataList.length && !loading && hasMore) {
-            if (features.length > 0) {
-                const newCursor = features[features.length - 1].createdAt;
-                setNextCursor(newCursor);
+    const loadMoreItems = useCallback(
+        async (startIndex, stopIndex) => {
+            // Only load more items if the last item is within the range of what's visible
+            if (stopIndex >= dataList.length && !loading && hasMore) {
+                if (features.length > 0) {
+                    const newCursor = features[features.length - 1].createdAt;
+                    setNextCursor(newCursor);
+                }
             }
-        }
-    }, [features, dataList, loading, hasMore]);
+        },
+        [features, dataList, loading, hasMore],
+    );
 
     useEffect(() => {
         if (features && features.length === 0) {
@@ -71,12 +148,20 @@ export const ProjectFeatureFlags = () => {
     }, [features]);
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <div style={{ display: 'flex' }}>
-                <TableCell component="div" style={{ flexGrow: 1, fontWeight: 'bold' }}>Name</TableCell>
-                <TableCell component="div" style={{ flexGrow: 1, fontWeight: 'bold' }}>Type</TableCell>
-                <TableCell component="div" style={{ flexGrow: 1, fontWeight: 'bold' }}>Development</TableCell>
-            </div>
+        <PageContent
+            style={{
+                boxShadow: 'none',
+                marginLeft: '1rem',
+                minHeight: '100%',
+                width: 'calc(100% - 1rem)',
+                position: 'relative',
+                [theme.breakpoints.down('md')]: {
+                    marginLeft: '0',
+                    paddingBottom: '4rem',
+                    width: 'inherit',
+                },
+            }}
+        >
             <InfiniteLoader
                 isItemLoaded={isItemLoaded}
                 itemCount={itemCount}
@@ -86,6 +171,7 @@ export const ProjectFeatureFlags = () => {
                     <VirtualList
                         height={window.innerHeight} // Height of all rows
                         itemCount={itemCount}
+                        innerElementType={Inner}
                         itemSize={53} // Adjust the item size to fit your design
                         onItemsRendered={onItemsRendered}
                         ref={ref}
@@ -97,6 +183,49 @@ export const ProjectFeatureFlags = () => {
                     </VirtualList>
                 )}
             </InfiniteLoader>
-        </div>
+        </PageContent>
     );
 };
+
+const Inner = React.forwardRef<HTMLDivElement, React.HTMLProps<HTMLDivElement>>(
+    function Inner({ children, ...rest }, ref) {
+        //const { header, footer, top } = useContext(VirtualTableContext)
+        return (
+            <div ref={ref}>
+                <Table>
+                    <TableHead>
+                        <TableRow style={{ display: 'flex' }}>
+                            {/* todo make cell sortable flex align center*/}
+                            <CellSortable isFlex isSortable={false} width={65}>
+                                <Checkbox />
+                            </CellSortable>
+                            <CellSortable isFlex isSortable={false} width={50}>
+                                <FavoriteIconHeader
+                                    isActive={false}
+                                    onClick={() => {}}
+                                />
+                            </CellSortable>
+                            <CellSortable
+                                isFlex
+                                isSortable
+                                isFlexGrow
+                                width={150}
+                            >
+                                Name
+                            </CellSortable>
+                            <CellSortable isFlex isSortable isFlexGrow>
+                                Type
+                            </CellSortable>
+                            <CellSortable isFlex isSortable isFlexGrow>
+                                Development
+                            </CellSortable>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody style={{ position: 'absolute', width: '100%' }}>
+                        {children}
+                    </TableBody>
+                </Table>
+            </div>
+        );
+    },
+);

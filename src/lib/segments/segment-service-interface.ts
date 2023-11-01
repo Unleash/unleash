d022ef7bd5099170f9bd73894deb348e5495f405
Jaanus Sellin <sellinjaanus@gmail.com>
2023-11-01 16:46:45 +0200

    5.6.2 (#5243)

--- src/lib/segments/segment-service-interface.ts
+++ src/lib/segments/segment-service-interface.ts
@@ -13,7 +13,17 @@ export interface ISegmentService {
 
     get(id: number): Promise<ISegment>;
 
-    getStrategies(id: number, userId: number): Promise<IFeatureStrategy[]>;
+    /**
+     * Gets all strategies for a segment
+     * This is NOT considering the private projects
+     * For most use cases, use `getVisibleStrategies`
+     */
+    getAllStrategies(id: number): Promise<IFeatureStrategy[]>;
+
+    getVisibleStrategies(
+        id: number,
+        userId: number,
+    ): Promise<IFeatureStrategy[]>;
 
     validateName(name: string): Promise<void>;
 

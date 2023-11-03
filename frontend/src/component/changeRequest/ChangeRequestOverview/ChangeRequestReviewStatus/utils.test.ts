df7ccff28b6c8ff85273bb059db9110f3ea95224
andreas-unleash <andreas@getunleash.ai>
2023-11-03 15:24:39 +0200

    feat: scheduled change requests dialogs
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

---
+++ frontend/src/component/changeRequest/ChangeRequestOverview/ChangeRequestReviewStatus/utils.test.ts
@@ -0,0 +1,58 @@
+import { getBrowserTimezoneInHumanReadableUTCOffset } from './utils';
+
+describe('getBrowserTimezoneInHumanReadableUTCOffset', () => {
+    // Test for the current timezone offset
+    test('should return the correct UTC offset for the current timezone', () => {
+        const date = new Date('2023-01-01T00:00:00Z'); // fixed date in UTC
+        const expectedOffset = new Date(
+            '2023-01-01T00:00:00Z',
+        ).getTimezoneOffset();
+        const sign = expectedOffset > 0 ? '-' : '+';
+        const expectedHours = Math.floor(Math.abs(expectedOffset) / 60)
+            .toString()
+            .padStart(2, '0');
+        const expectedMinutes = (Math.abs(expectedOffset) % 60)
+            .toString()
+            .padStart(2, '0');
+        const expected = `UTC${sign}${expectedHours}:${expectedMinutes}`;
+
+        const result = getBrowserTimezoneInHumanReadableUTCOffset(date);
+        expect(result).toBe(expected);
+    });
+
+    // Test for known timezones
+    // Assuming that we can mock Date.prototype.getTimezoneOffset
+    const timezones = [
+        { offset: 0, expected: 'UTC+00:00' },
+        { offset: -330, expected: 'UTC+05:30' },
+        { offset: -120, expected: 'UTC+02:00' },
+        { offset: 420, expected: 'UTC-07:00' },
+    ];
+
+    timezones.forEach(({ offset, expected }) => {
+        test(`should return '${expected}' for offset ${offset} minutes`, () => {
+            // Mock the getTimezoneOffset function to return a fixed offset
+            Date.prototype.getTimezoneOffset = jest.fn(() => offset);
+            const result = getBrowserTimezoneInHumanReadableUTCOffset();
+            expect(result).toBe(expected);
+        });
+    });
+
+    // Edge cases
+    test('should handle the edge case for zero offset', () => {
+        Date.prototype.getTimezoneOffset = jest.fn(() => 0);
+        const result = getBrowserTimezoneInHumanReadableUTCOffset();
+        expect(result).toBe('UTC+00:00');
+    });
+
+    test('should handle offsets that are not on the hour', () => {
+        Date.prototype.getTimezoneOffset = jest.fn(() => -45);
+        const result = getBrowserTimezoneInHumanReadableUTCOffset();
+        expect(result).toBe('UTC+00:45');
+    });
+
+    // Reset mock after all tests are done
+    afterAll(() => {
+        jest.restoreAllMocks();
+    });
+});
---
+++ frontend/src/component/changeRequest/ChangeRequestOverview/ChangeRequestReviewStatus/utils.ts
@@ -0,0 +1,17 @@
+export const getBrowserTimezoneInHumanReadableUTCOffset = (
+    date = new Date(),
+): string => {
+    const offset = -date.getTimezoneOffset();
+    const hours = Math.floor(Math.abs(offset) / 60);
+    const minutes = Math.abs(offset) % 60;
+    let sign = '+';
+    if (offset < 0) {
+        sign = '-';
+    }
+
+    // Ensure that hours and minutes are two digits
+    const zeroPaddedHours = hours.toString().padStart(2, '0');
+    const zeroPaddedMinutes = minutes.toString().padStart(2, '0');
+
+    return `UTC${sign}${zeroPaddedHours}:${zeroPaddedMinutes}`;
+};

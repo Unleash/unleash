f453a97b4347e10bfd8c3498ff99cf1911454834
Thomas Heartman <thomas@getunleash.io>
2023-11-20 10:46:57 +0100

    feat: add some new types

--- src/lib/features/change-request-segment-usage-service/change-request-segment-usage-read-model.ts
+++ src/lib/features/change-request-segment-usage-service/change-request-segment-usage-read-model.ts
@@ -1,4 +1,15 @@
+type NewStrategy = {
+    projectId: string;
+    featureName: string;
+    strategyName: string;
+    environment: string;
+};
+
+type ExistingStrategy = NewStrategy & { id?: string };
+
+type ChangeRequestStrategy = NewStrategy | ExistingStrategy;
+
 export interface IChangeRequestSegmentUsageReadModel {
     isSegmentUsedInActiveChangeRequests(segmentId: number): Promise<boolean>;
-    getSegmentsUsedInActiveChangeRequests(): Promise<{}[]>;
+    getSegmentsUsedInActiveChangeRequests(): Promise<ChangeRequestStrategy[]>;
 }

3ea30ad6f5e25032d88bb3819b671e2827bb00ab
sjaanus <sellinjaanus@gmail.com>
2023-11-21 14:44:31 +0200

    Fix

--- src/lib/features/feature-toggle/feature-toggle-strategies-store.ts
+++ src/lib/features/feature-toggle/feature-toggle-strategies-store.ts
@@ -747,7 +747,6 @@ class FeatureStrategiesStore implements IFeatureStrategiesStore {
             .joinRaw('CROSS JOIN total_features')
             .whereBetween('final_rank', [offset + 1, offset + limit]);
 
-        console.log(finalQuery.toQuery());
         const rows = await finalQuery;
 
         if (rows.length > 0) {

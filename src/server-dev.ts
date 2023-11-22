5414fa6663b4ebb0d27eac959c6bb9b413b30001
Fredrik Strand Oseberg <fredrik.no@gmail.com>
2023-11-22 13:32:38 +0100

    Refactor/remove last seen at flag (#5394)
    This PR removes the feature flag for displaying the new last seen by
    environments component.

--- src/server-dev.ts
+++ src/server-dev.ts
@@ -37,7 +37,6 @@ process.nextTick(async () => {
                         embedProxyFrontend: true,
                         anonymiseEventLog: false,
                         responseTimeWithAppNameKillSwitch: false,
-                        lastSeenByEnvironment: true,
                         variantTypeNumber: true,
                         privateProjects: true,
                         dependentFeatures: true,

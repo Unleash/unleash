86d27c0a415810c7092bcfc51e22806d95541266
Gastón Fournier <gaston@getunleash.io>
2023-11-29 17:28:09 +0100

    Fixes

--- src/migrate-db.ts
+++ src/migrate-db.ts
@@ -2,7 +2,7 @@ import { applyDbMigration } from './lib/server-impl';
 
 try {
     const useLock = true;
-    applyDbMigration(useLock);
+    applyDbMigration(useLock).then(() => process.exit());
 } catch (error) {
     // eslint-disable-next-line no-console
     console.error(error);

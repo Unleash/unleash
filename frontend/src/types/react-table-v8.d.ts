d815bc1edabb5e6e3c8132e288accb63cedb65a9
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2023-12-01 14:35:52 +0100

    fix: more restrictive meta on react-table v8 types

--- frontend/src/types/react-table-v8.d.ts
+++ frontend/src/types/react-table-v8.d.ts
@@ -3,6 +3,5 @@ import '@tanstack/react-table';
 declare module '@tanstack/table-core' {
     interface ColumnMeta<TData extends RowData, TValue> {
         align: 'left' | 'center' | 'right';
-        [string]: string;
     }
 }

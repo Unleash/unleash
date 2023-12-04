081c0df77de7142b57c461ce1e3f6ef399b6718a
kwasniew <kwasniewski.mateusz@gmail.com>
2023-12-04 14:41:29 +0100

    test: persistent table state

---
+++ frontend/src/hooks/usePersistentTableState.test.tsx
@@ -0,0 +1,56 @@
+import { render } from 'utils/testRenderer';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { usePersistentTableState } from './usePersistentTableState';
+import { Route, Routes } from 'react-router-dom';
+import { createLocalStorage } from '../utils/createLocalStorage';
+import { StringParam } from 'use-query-params';
+
+type TestComponentProps = {
+    keyName: string;
+    queryParamsDefinition: Record<string, any>;
+};
+
+function TestComponent({ keyName, queryParamsDefinition }: TestComponentProps) {
+    const [tableState, setTableState] = usePersistentTableState(keyName, queryParamsDefinition);
+
+    return (
+        <Routes>
+            <Route path={'/my-url'} element={
+                <div>
+                    <span data-testid='state-value'>{tableState.query}</span>
+                    <button type='button' onClick={() => setTableState({ query: 'after' })}>Update State</button>
+                </div>}>
+
+            </Route>
+        </Routes>
+    );
+}
+
+describe('TestComponent with usePersistentTableState', () => {
+    it('initializes correctly from localStorage and URL', async () => {
+        createLocalStorage('testKey', {}).setValue({ query: 'initial' });
+
+        render(<TestComponent keyName='testKey'
+                              queryParamsDefinition={{ query: StringParam }} />, { route: '/my-url' });
+
+        expect(screen.getByTestId('state-value').textContent).toBe('initial');
+        expect(window.location.href).toContain('my-url?query=initial');
+    });
+
+    it('updates the state on button click', async () => {
+        createLocalStorage('testKey', {}).setValue({ query: 'before' });
+
+        render(<TestComponent keyName='testKey'
+                              queryParamsDefinition={{ query: StringParam }} />, { route: '/my-url' });
+
+        expect(screen.getByTestId('state-value').textContent).toBe('before');
+
+        screen.getByText('Update State').click();
+
+        expect(screen.getByTestId('state-value').textContent).toBe('after');
+        expect(window.location.href).toContain('my-url?query=after');
+    });
+});
+
+

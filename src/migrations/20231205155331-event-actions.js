96ed6f92483b1b44d10bab754a0c49d6257c6383
Nuno Góis <github@nunogois.com>
2023-12-06 17:18:28 +0000

    feat: add action sort_order col

--- src/migrations/20231205155331-event-actions.js
+++ src/migrations/20231205155331-event-actions.js
@@ -6,11 +6,12 @@ exports.up = function (db, cb) {
         CREATE TABLE IF NOT EXISTS event_actions
             (
                 id SERIAL PRIMARY KEY NOT NULL,
-                enabled BOOLEAN DEFAULT true NOT NULL,
-                name TEXT NOT NULL,
                 event TEXT NOT NULL,
+                enabled BOOLEAN DEFAULT true NOT NULL,
+                name TEXT,
                 action TEXT NOT NULL,
                 parameters JSONB NOT NULL DEFAULT '{}'::jsonb,
+                sort_order INTEGER NOT NULL DEFAULT 9999,
                 created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
             );
         `,

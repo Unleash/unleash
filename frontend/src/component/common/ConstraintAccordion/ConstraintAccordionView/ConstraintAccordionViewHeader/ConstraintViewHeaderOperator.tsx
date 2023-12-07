02c45a893faf06981482ff9967eb64fb5385bbd6
sighphyre <liquidwicked64@gmail.com>
2023-12-07 09:57:42 +0200

    wip: apply biome format

--- frontend/src/component/common/ConstraintAccordion/ConstraintAccordionView/ConstraintAccordionViewHeader/ConstraintViewHeaderOperator.tsx
+++ frontend/src/component/common/ConstraintAccordion/ConstraintAccordionView/ConstraintAccordionViewHeader/ConstraintViewHeaderOperator.tsx
@@ -1,29 +1,29 @@
-import { IConstraint } from "interfaces/strategy";
-import { ConditionallyRender } from "../../../ConditionallyRender/ConditionallyRender";
-import { Tooltip, Box, styled } from "@mui/material";
-import { stringOperators } from "constants/operators";
-import { ReactComponent as NegatedOnIcon } from "assets/icons/not_operator_selected.svg";
-import { ConstraintOperator } from "../../ConstraintOperator/ConstraintOperator";
-import { StyledIconWrapper } from "./StyledIconWrapper";
-import { ReactComponent as CaseSensitive } from "assets/icons/24_Text format.svg";
-import { oneOf } from "utils/oneOf";
-import { useTheme } from "@mui/material";
+import { IConstraint } from 'interfaces/strategy';
+import { ConditionallyRender } from '../../../ConditionallyRender/ConditionallyRender';
+import { Tooltip, Box, styled } from '@mui/material';
+import { stringOperators } from 'constants/operators';
+import { ReactComponent as NegatedOnIcon } from 'assets/icons/not_operator_selected.svg';
+import { ConstraintOperator } from '../../ConstraintOperator/ConstraintOperator';
+import { StyledIconWrapper } from './StyledIconWrapper';
+import { ReactComponent as CaseSensitive } from 'assets/icons/24_Text format.svg';
+import { oneOf } from 'utils/oneOf';
+import { useTheme } from '@mui/material';
 
 interface ConstraintViewHeaderOperatorProps {
     constraint: IConstraint;
     disabled?: boolean;
 }
 
-const StyledHeaderValuesContainerWrapper = styled("div")(({ theme }) => ({
-    display: "flex",
-    alignItems: "stretch",
-    margin: "auto 0",
+const StyledHeaderValuesContainerWrapper = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'stretch',
+    margin: 'auto 0',
 }));
 
-const StyledHeaderConstraintContainer = styled("div")(({ theme }) => ({
-    minWidth: "152px",
-    position: "relative",
-    [theme.breakpoints.down("sm")]: {
+const StyledHeaderConstraintContainer = styled('div')(({ theme }) => ({
+    minWidth: '152px',
+    position: 'relative',
+    [theme.breakpoints.down('sm')]: {
         paddingRight: 0,
     },
 }));
@@ -38,8 +38,13 @@ export const ConstraintViewHeaderOperator = ({
             <ConditionallyRender
                 condition={Boolean(constraint.inverted)}
                 show={
-                    <Tooltip title={"Operator is negated"} arrow>
-                        <Box sx={{ display: "flex", paddingRight: theme.spacing(1) }}>
+                    <Tooltip title={'Operator is negated'} arrow>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                paddingRight: theme.spacing(1),
+                            }}
+                        >
                             <StyledIconWrapper>
                                 <NegatedOnIcon
                                     color={theme.palette.primary.main}
@@ -62,7 +67,7 @@ export const ConstraintViewHeaderOperator = ({
                     oneOf(stringOperators, constraint.operator)
                 }
                 show={
-                    <Tooltip title="Case sensitive is active" arrow>
+                    <Tooltip title='Case sensitive is active' arrow>
                         <StyledIconWrapper>
                             <CaseSensitive />
                         </StyledIconWrapper>

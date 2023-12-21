a6194e5102b8516afb3cdcb353887725cf6962f4
sjaanus <sellinjaanus@gmail.com>
2023-12-21 10:43:33 +0200

    Fix

--- frontend/src/component/feature/FeatureToggleList/FeatureToggleListActions/FeatureToggleActions.test.tsx
+++ frontend/src/component/feature/FeatureToggleList/FeatureToggleListActions/FeatureToggleActions.test.tsx
@@ -14,7 +14,7 @@ test('all options are drawn', async () => {
 
     render(<FeatureToggleListActions onExportClick={() => {}} />);
 
-    const batchReviveButton = await screen.findByTitle('Options');
+    const batchReviveButton = await screen.findByTitle('Group actions');
 
     await userEvent.click(batchReviveButton!);
 

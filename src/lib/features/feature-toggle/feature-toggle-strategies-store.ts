784368e6f9eaeafd5dd3208aca69ed12d0246cdf
kwasniew <kwasniewski.mateusz@gmail.com>
2023-12-21 08:40:49 +0100

    fix: copy last seen at from env

--- src/lib/features/feature-toggle/feature-toggle-strategies-store.ts
+++ src/lib/features/feature-toggle/feature-toggle-strategies-store.ts
@@ -395,7 +395,7 @@ class FeatureStrategiesStore implements IFeatureStrategiesStore {
                 }
 
                 if (
-                    acc.lastSeenAt === undefined ||
+                    acc.lastSeenAt == null ||
                     new Date(r.env_last_seen_at) > new Date(acc.lastSeenAt)
                 ) {
                     acc.lastSeenAt = r.env_last_seen_at;
@@ -671,7 +671,7 @@ class FeatureStrategiesStore implements IFeatureStrategiesStore {
             }
             const featureRow = acc[row.feature_name];
             if (
-                featureRow.lastSeenAt === undefined ||
+                featureRow.lastSeenAt == null ||
                 new Date(row.env_last_seen_at) >
                     new Date(featureRow.last_seen_at)
             ) {

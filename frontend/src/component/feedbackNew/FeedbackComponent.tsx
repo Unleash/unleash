1defacf693f106bf572399b45f565873ac6133f2
sjaanus <sellinjaanus@gmail.com>
2023-12-29 12:41:11 +0200

    Configurable feedback uri

--- frontend/src/component/feedbackNew/FeedbackComponent.tsx
+++ frontend/src/component/feedbackNew/FeedbackComponent.tsx
@@ -161,7 +161,7 @@ export const FeedbackComponent = () => {
     if (!feedbackData) return null;
 
     const { setToastData } = useToast();
-    const { addFeedback, addDirectFeedback } = useUserFeedbackApi();
+    const { addFeedback } = useUserFeedbackApi();
     const { setHasSubmittedFeedback } = useUserSubmittedFeedback(
         feedbackData.category,
     );
@@ -185,7 +185,7 @@ export const FeedbackComponent = () => {
         const data = Object.fromEntries(formData);
 
         if (isProvideFeedbackSchema(data)) {
-            await addDirectFeedback(data as ProvideFeedbackSchema);
+            await addFeedback(data as ProvideFeedbackSchema);
             setToastData({
                 title: 'Feedback sent',
                 type: 'success',

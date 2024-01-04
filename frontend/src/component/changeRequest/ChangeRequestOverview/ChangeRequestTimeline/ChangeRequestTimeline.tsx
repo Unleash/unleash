ff96563742706b5914757832c00918c04584a1ac
Thomas Heartman <thomas@getunleash.io>
2024-01-04 16:04:50 +0530

    fix: align timeline icon for schedule failures (#5761)
    Aligns the icon to the end of the end of the box to make it match the
    text.
    
    Also removes a redundant br element.

--- frontend/src/component/changeRequest/ChangeRequestOverview/ChangeRequestTimeline/ChangeRequestTimeline.tsx
+++ frontend/src/component/changeRequest/ChangeRequestOverview/ChangeRequestTimeline/ChangeRequestTimeline.tsx
@@ -32,6 +32,7 @@ const StyledBox = styled(Box)(({ theme }) => ({
 const StyledSubtitle = styled(Box)(({ theme }) => ({
     display: 'flex',
     flexDirection: 'row',
+    alignItems: 'flex-end',
 }));
 
 const StyledTimeline = styled(Timeline)(() => ({
@@ -168,7 +169,6 @@ const createTimelineItem = (
         </TimelineSeparator>
         <TimelineContent>
             {title}
-            <br />
             <ConditionallyRender
                 condition={Boolean(subtitle)}
                 show={

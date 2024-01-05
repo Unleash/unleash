bf84478bafdbbfb027e03e1b1f4fe556bb69d0cb
Christopher Kolstad <chriswk@getunleash.io>
2024-01-05 15:35:05 +0100

    fix: updated operationId for client-bulk-metrics

--- src/lib/routes/client-api/metrics.ts
+++ src/lib/routes/client-api/metrics.ts
@@ -89,7 +89,7 @@ export default class ClientMetricsController extends Controller {
                     tags: ['Edge'],
                     summary: 'Send metrics in bulk',
                     description: `This operation accepts batched metrics from any client. Metrics will be inserted into Unleash's metrics storage`,
-                    operationId: 'bulkMetrics',
+                    operationId: 'clientBulkMetrics',
                     requestBody: createRequestSchema('bulkMetricsSchema'),
                     responses: {
                         202: emptyResponse,

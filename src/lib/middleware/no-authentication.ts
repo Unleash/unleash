dd1bf93b32a5811df1396dc15e5150ff2532594a
David Leek <david@getunleash.io>
2024-01-09 11:56:21 +0100

    chore: add a fix for the error message in frontend when authtype none

--- src/lib/middleware/no-authentication.ts
+++ src/lib/middleware/no-authentication.ts
@@ -1,5 +1,6 @@
 import { Application } from 'express';
 import NoAuthUser from '../types/no-auth-user';
+import NoAuthFrontendAPIUser from '../types/no-auth-frontend-api-user';
 
 // eslint-disable-next-line
 function noneAuthentication(basePath: string, app: Application): void {
@@ -8,6 +9,14 @@ function noneAuthentication(basePath: string, app: Application): void {
         if (!req.user) {
             // @ts-expect-error
             req.user = new NoAuthUser();
+        }
+        next();
+    });
+    app.use(`${basePath || ''}/api/frontend/`, (req, res, next) => {
+        // @ts-expect-error
+        if (!req.user) {
+            // @ts-expect-error
+            req.user = new NoAuthFrontendAPIUser();
         }
         next();
     });

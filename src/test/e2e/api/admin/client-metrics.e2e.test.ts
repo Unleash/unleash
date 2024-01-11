ca3b4c5057bdc7902e77d145ac28b1707b81e169
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2024-01-11 10:39:41 +0100

    feat: Metrics api returning daily data (#5830)

--- src/test/e2e/api/admin/client-metrics.e2e.test.ts
+++ src/test/e2e/api/admin/client-metrics.e2e.test.ts
@@ -25,6 +25,7 @@ beforeAll(async () => {
             experimental: {
                 flags: {
                     strictSchemaValidation: true,
+                    extendedUsageMetrics: true,
                 },
             },
         },
@@ -143,18 +144,21 @@ test('should support the hoursBack query param for raw metrics', async () => {
     ];
 
     await db.stores.clientMetricsStoreV2.batchInsertMetrics(metrics);
+    await db.stores.clientMetricsStoreV2.aggregateDailyMetrics();
 
     const hours1 = await fetchHoursBack(1);
     const hours24 = await fetchHoursBack(24);
     const hours48 = await fetchHoursBack(48);
     const hoursTooFew = await fetchHoursBack(-999);
-    const hoursTooMany = await fetchHoursBack(999);
+    const hoursTooMany = await fetchHoursBack(24 * 31 * 3 + 1); // 3 months + 1 hour
+    const days = await fetchHoursBack(48 + 1); // switch to days after 48 hours
 
     expect(hours1.data).toHaveLength(1);
     expect(hours24.data).toHaveLength(24);
     expect(hours48.data).toHaveLength(48);
     expect(hoursTooFew.data).toHaveLength(24);
     expect(hoursTooMany.data).toHaveLength(24);
+    expect(days.data).toHaveLength(2); // two days of data
 });
 
 test('should return toggle summary', async () => {

5b99a6e4e4168e8768a8b4d5fc01c43fb2707d01
Gastón Fournier <gaston@getunleash.io>
2024-01-11 14:15:07 +0100

    fix: migrations

--- src/migrations/20240111125100-automated-actions.js
+++ src/migrations/20240111125100-automated-actions.js
@@ -7,11 +7,12 @@ exports.up = function (db, cb) {
             created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
             created_by_user_id INTEGER,
             name varchar(255),
+            project VARCHAR(255) NOT NULL,
             actor_id INTEGER,
             source varchar(255),
             source_id INTEGER,
             payload JSONB DEFAULT '{}'::jsonb NOT NULL,
-            FOREIGN KEY (project) references projects(id) ON DELETE CASCADE,
+            FOREIGN KEY (project) references projects(id) ON DELETE CASCADE
         );
 
         CREATE TABLE IF NOT EXISTS actions
@@ -22,7 +23,7 @@ exports.up = function (db, cb) {
             action_set_id INTEGER references action_sets (id) ON DELETE CASCADE,
             sort_order INTEGER,
             action varchar(255) NOT NULL,
-            execution_params JSONB DEFAULT '{}'::jsonb NOT NULL,
+            execution_params JSONB DEFAULT '{}'::jsonb NOT NULL
         );
 
         CREATE INDEX idx_action_sets_project ON action_sets (project);

f937aa939ffcba28901bb044c55d77a8adf3e532
Thomas Heartman <thomas@getunleash.io>
2024-01-15 13:13:17 +0530

    chore: rewrite tests to use db testing directly

---
+++ src/lib/types/core.test.ts
@@ -0,0 +1,69 @@
+import { getDbConfig } from '../../test/e2e/helpers/database-config';
+import { createTestConfig } from '../../test/config/test-config';
+import { log } from 'db-migrate-shared';
+import { Client } from 'pg';
+import { IDBOption } from 'lib/types';
+import { migrateDb } from '../../migrator';
+import { SYSTEM_USER } from './core';
+
+log.setLogLevel('error');
+
+async function initSchema(db: IDBOption): Promise<void> {
+    const client = new Client(db);
+    await client.connect();
+    await client.query(`DROP SCHEMA IF EXISTS ${db.schema} CASCADE`);
+    await client.query(`CREATE SCHEMA IF NOT EXISTS ${db.schema}`);
+    await client.end();
+}
+
+describe('System user definitions in code and db', () => {
+    let dbDefinition: {
+        email: string | null;
+        username: string;
+        name: string;
+        id: number;
+    };
+    beforeAll(async () => {
+        jest.setTimeout(15000);
+        const config = createTestConfig({
+            db: {
+                ...getDbConfig(),
+                pool: { min: 1, max: 4 },
+                schema: 'system_user_alignment_test',
+                ssl: false,
+            },
+        });
+
+        await initSchema(config.db);
+
+        const e2e = {
+            ...config.db,
+            connectionTimeoutMillis: 2000,
+        };
+
+        await migrateDb(config);
+
+        // Set up the test data
+        const client = new Client(config.db);
+        await client.connect();
+
+        const query = await client.query(
+            `select * from system_user_alignment_test.users where id = -1337;`,
+        );
+
+        dbDefinition = query.rows[0];
+    });
+
+    test('usernames match', () => {
+        expect(SYSTEM_USER.username).toBe(dbDefinition.username);
+    });
+    test('ids match', () => {
+        expect(SYSTEM_USER.id).toBe(dbDefinition.id);
+    });
+    test('names match', () => {
+        expect(SYSTEM_USER.name).toBe(dbDefinition.name);
+    });
+    test('emails match', () => {
+        expect(SYSTEM_USER.email).toBe(dbDefinition.email);
+    });
+});

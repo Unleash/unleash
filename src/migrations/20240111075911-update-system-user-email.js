eaabe5f22e37ad11b8a7326f8e121a2f908546f4
Thomas Heartman <thomas@getunleash.io>
2024-01-15 11:53:43 +0530

    chore: set email to null instead of an empty string

--- src/migrations/20240111075911-update-system-user-email.js
+++ src/migrations/20240111075911-update-system-user-email.js
@@ -2,7 +2,7 @@
 
 exports.up = function (db, callback) {
     db.runSql(
-        `UPDATE users SET email = '' WHERE id = -1337;`,
+        `UPDATE users SET email = NULL WHERE id = -1337;`,
         callback,
     );
 };

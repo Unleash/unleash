feb49bfd46278e399f45e2a85101bf5ade43649a
Gastón Fournier <gaston@getunleash.io>
2024-01-17 13:01:24 +0100

    Improve extract user

--- src/lib/util/extract-user.ts
+++ src/lib/util/extract-user.ts
@@ -1,15 +1,21 @@
+import { SYSTEM_USER } from 'lib/types';
 import { IApiRequest, IApiUser, IAuthRequest, IUser } from '../server-impl';
 
 export function extractUsernameFromUser(user: IUser | IApiUser): string {
-    return (user as IUser)?.email || user?.username || 'unknown';
+    return (user as IUser)?.email || user?.username || SYSTEM_USER.username;
 }
 
 export function extractUsername(req: IAuthRequest | IApiRequest): string {
     return extractUsernameFromUser(req.user);
 }
 
+export const extractUserIdFromUser = (user: IUser | IApiUser) =>
+    (user as IUser).id ||
+    (user as IApiUser).internalAdminTokenUserId ||
+    SYSTEM_USER.id;
+
 export const extractUserId = (req: IAuthRequest | IApiRequest) =>
-    (req.user as IUser).id || (req.user as IApiUser).internalAdminTokenUserId;
+    extractUserIdFromUser(req.user);
 
 export const extractUserInfo = (req: IAuthRequest | IApiRequest) => ({
     id: extractUserId(req),

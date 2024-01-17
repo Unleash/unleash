a203c1be7cf510e7fd5294444bccbf030ed8a9cd
Gastón Fournier <gaston@getunleash.io>
2024-01-17 13:07:02 +0100

    Fix import

--- src/lib/util/extract-user.ts
+++ src/lib/util/extract-user.ts
@@ -1,4 +1,4 @@
-import { SYSTEM_USER } from 'lib/types';
+import { SYSTEM_USER } from '../../lib/types';
 import { IApiRequest, IApiUser, IAuthRequest, IUser } from '../server-impl';
 
 export function extractUsernameFromUser(user: IUser | IApiUser): string {

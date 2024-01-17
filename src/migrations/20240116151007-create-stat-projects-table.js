d1d57c4a31f9433633fdb3fd1b21f8cf067604c9
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2024-01-17 19:17:11 +0100

    stat_projects project reference

--- src/migrations/20240116151007-create-stat-projects-table.js
+++ src/migrations/20240116151007-create-stat-projects-table.js
@@ -2,11 +2,12 @@ exports.up = function(db, cb) {
     db.runSql(`
         CREATE TABLE stat_projects(
                 created_at TIMESTAMP NOT NULL,
-                projectId INT NOT NULL,
+                project VARCHAR(255) NOT NULL,
                 flags_active_total INT,
                 flags_stale INT,
                 flags_archived INT,
-                PRIMARY KEY (created_at, projectId)
+                PRIMARY KEY (created_at, project)
+                FOREIGN KEY (project) references projects(id) ON DELETE NO ACTION,
         );
     `, cb);
 };

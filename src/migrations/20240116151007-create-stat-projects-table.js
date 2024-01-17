dd49c5dd410688eff3836e17f1a2b9b891258156
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2024-01-17 23:36:15 +0100

    fix syntax issue

--- src/migrations/20240116151007-create-stat-projects-table.js
+++ src/migrations/20240116151007-create-stat-projects-table.js
@@ -1,13 +1,13 @@
 exports.up = function(db, cb) {
     db.runSql(`
         CREATE TABLE stat_projects(
-                created_at TIMESTAMP NOT NULL,
-                project VARCHAR(255) NOT NULL,
-                flags_active_total INT,
-                flags_stale INT,
-                flags_archived INT,
-                PRIMARY KEY (created_at, project)
-                FOREIGN KEY (project) references projects(id) ON DELETE NO ACTION,
+            created_at TIMESTAMP NOT NULL,
+            project VARCHAR(255) NOT NULL,
+            flags_active_total INT,
+            flags_stale INT,
+            flags_archived INT,
+            PRIMARY KEY (created_at, project),
+            FOREIGN KEY (project) REFERENCES projects(id) ON DELETE NO ACTION
         );
     `, cb);
 };

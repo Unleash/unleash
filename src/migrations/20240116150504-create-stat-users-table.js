c50289f8d8e475518c7e964c3e67e3e19557f9d7
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2024-01-17 19:16:27 +0100

    change stat_users - simplify columns

--- src/migrations/20240116150504-create-stat-users-table.js
+++ src/migrations/20240116150504-create-stat-users-table.js
@@ -2,11 +2,11 @@ exports.up = function(db, cb) {
     db.runSql(`
         CREATE TABLE stat_users(
                 created_at TIMESTAMP NOT NULL PRIMARY KEY,
-                users_total INT,
-                users_active_7 INT,
-                users_active_30 INT,
-                users_active_60 INT,
-                users_active_90 INT
+                total INT,
+                active_7 INT,
+                active_30 INT,
+                active_60 INT,
+                active_90 INT
         );
     `, cb);
 };

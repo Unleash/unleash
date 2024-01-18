de75c263e5525a524f116bc264d3e4130391b18d
Thomas Heartman <thomas@getunleash.io>
2024-01-18 14:40:20 +0400

    feat: update tests

--- frontend/src/component/changeRequest/ChangeRequest/Changes/Change/StrategyChange.test.ts
+++ frontend/src/component/changeRequest/ChangeRequest/Changes/Change/StrategyChange.test.ts
@@ -19,6 +19,22 @@ describe('Strategy change conflict detection', () => {
         segments: [],
     };
 
+    const snapshot: IFeatureStrategy = {
+        id: '31572930-2db7-461f-813b-3eedc200cb33',
+        name: 'flexibleRollout',
+        title: '',
+        disabled: false,
+        segments: [],
+        variants: [],
+        sortOrder: 0,
+        parameters: {
+            groupId: 'aaa',
+            rollout: '71',
+            stickiness: 'default',
+        },
+        constraints: [],
+    };
+
     const change: IChangeRequestUpdateStrategy = {
         id: 39,
         action: 'updateStrategy' as const,
@@ -28,21 +44,7 @@ describe('Strategy change conflict detection', () => {
             title: '',
             disabled: false,
             segments: [],
-            snapshot: {
-                id: '31572930-2db7-461f-813b-3eedc200cb33',
-                name: 'flexibleRollout',
-                title: '',
-                disabled: false,
-                segments: [],
-                variants: [],
-                sortOrder: 0,
-                parameters: {
-                    groupId: 'aaa',
-                    rollout: '71',
-                    stickiness: 'default',
-                },
-                constraints: [],
-            },
+            snapshot,
             variants: [],
             parameters: {
                 groupId: 'aaa',
@@ -132,60 +134,61 @@ describe('Strategy change conflict detection', () => {
 
         const { id, name, ...changedProperties } = withChanges;
 
-        // const expectedOutput = Object.entries(changedProperties).map(
-        //     ([property, oldValue]) => ({
-        //         property,
-        //         oldValue,
-        //         newValue: change.payload.snapshot[property],
-        //     }),
-        // );
+        const expectedOutput = Object.entries(changedProperties).map(
+            ([property, oldValue]) => ({
+                property,
+                oldValue,
+                newValue:
+                    change.payload.snapshot![
+                        property as keyof IFeatureStrategy
+                    ],
+            }),
+        );
 
-        expect(result).toStrictEqual([
-            {
-                property: 'constraints',
-                oldValue: [
-                    {
-                        values: ['blah'],
-                        inverted: false,
-                        operator: 'IN' as const,
-                        contextName: 'appName',
-                        caseInsensitive: false,
-                    },
-                ],
-                newValue: [],
-            },
-            { property: 'disabled', oldValue: true, newValue: false },
-            { property: 'segments', oldValue: [3], newValue: [] },
-            { property: 'sortOrder', oldValue: 1, newValue: 0 },
-            { property: 'title', oldValue: 'custom title', newValue: '' },
-            {
-                property: 'parameters',
-                oldValue: {
-                    groupId: 'aab',
-                    rollout: '39',
-                    stickiness: 'userId',
-                },
-                newValue: {
-                    groupId: 'aaa',
-                    rollout: '71',
-                    stickiness: 'default',
-                },
-            },
-            {
-                property: 'variants',
-                oldValue: [
-                    {
-                        name: 'variant1',
-                        weight: 1000,
-                        payload: { type: 'string', value: 'beaty' },
-                        stickiness: 'userId',
-                        weightType: 'variable' as const,
-                    },
-                ],
-                newValue: [],
-            },
-        ]);
+        expectedOutput.sort((a, b) => a.property.localeCompare(b.property));
+        expect(result).toStrictEqual(expectedOutput);
     });
 
-    test('it ignores object order on nested objects', () => {});
+    test('it ignores object order on nested objects', () => {
+        const existingStrategyMod: IFeatureStrategy = {
+            ...existingStrategy,
+            constraints: [
+                {
+                    values: ['blah'],
+                    inverted: false,
+                    operator: 'IN' as const,
+                    contextName: 'appName',
+                    caseInsensitive: false,
+                },
+            ],
+        };
+
+        const constraintChange: IChangeRequestUpdateStrategy = {
+            ...change,
+            payload: {
+                ...change.payload,
+                // @ts-expect-error Some of the properties that may be
+                // undefined, we know exist in the value
+                snapshot: {
+                    ...change.payload.snapshot,
+                    constraints: [
+                        {
+                            caseInsensitive: false,
+                            contextName: 'appName',
+                            inverted: false,
+                            operator: 'IN' as const,
+                            values: ['blah'],
+                        },
+                    ],
+                },
+            },
+        };
+
+        expect(
+            getChangesThatWouldBeOverwritten({
+                currentStrategyConfig: existingStrategyMod,
+                change: constraintChange,
+            }),
+        ).toBeNull();
+    });
 });

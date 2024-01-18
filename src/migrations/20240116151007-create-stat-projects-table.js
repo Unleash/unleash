02caddca7234a80c46a870b9144d212ef36134e3
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2024-01-18 11:20:14 +0100

    add users count to projects stats

--- src/migrations/20240116151007-create-stat-projects-table.js
+++ src/migrations/20240116151007-create-stat-projects-table.js
@@ -3,9 +3,10 @@ exports.up = function(db, cb) {
         CREATE TABLE stat_projects(
             created_at TIMESTAMP NOT NULL,
             project VARCHAR(255) NOT NULL,
-            flags_active_total INT,
+            flags_active INT,
             flags_stale INT,
             flags_archived INT,
+            users INT,
             PRIMARY KEY (created_at, project),
             FOREIGN KEY (project) REFERENCES projects(id) ON DELETE NO ACTION
         );

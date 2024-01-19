00bfa90b13b023c7352812e78a8fadf45402cb16
Gastón Fournier <gaston@getunleash.io>
2024-01-19 10:06:16 +0100

    fix: create tokens createByUserId

--- src/lib/util/extract-user.ts
+++ src/lib/util/extract-user.ts
@@ -1,14 +1,19 @@
+import { SYSTEM_USER } from '../../lib/types';
 import { IAuthRequest, IUser } from '../server-impl';
 
 export function extractUsernameFromUser(user: IUser): string {
-    return user?.email || user?.username || 'unknown';
+    return user?.email || user?.username || SYSTEM_USER.username;
 }
 
 export function extractUsername(req: IAuthRequest): string {
     return extractUsernameFromUser(req.user);
 }
 
-export const extractUserId = (req: IAuthRequest) => req.user.id;
+export const extractUserIdFromUser = (user: IUser) =>
+    user?.id || SYSTEM_USER.id;
+
+export const extractUserId = (req: IAuthRequest) =>
+    extractUserIdFromUser(req.user);
 
 export const extractUserInfo = (req: IAuthRequest) => ({
     id: extractUserId(req),

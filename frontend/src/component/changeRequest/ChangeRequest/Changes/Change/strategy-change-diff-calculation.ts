cc6d2638323ce1988028fc00fe923d682c572403
Thomas Heartman <thomas@getunleash.io>
2024-01-22 15:14:18 +0400

    wip: add quick impl update for strategy change diff calculation

--- frontend/src/component/changeRequest/ChangeRequest/Changes/Change/strategy-change-diff-calculation.ts
+++ frontend/src/component/changeRequest/ChangeRequest/Changes/Change/strategy-change-diff-calculation.ts
@@ -1,9 +1,29 @@
-import { IChangeRequestUpdateStrategy } from 'component/changeRequest/changeRequest.types';
+import {
+    ChangeRequestEditStrategy,
+    IChangeRequestUpdateStrategy,
+} from 'component/changeRequest/changeRequest.types';
 import { IFeatureStrategy } from 'interfaces/strategy';
 import isEqual from 'lodash.isequal';
+import omit from 'lodash.omit';
 
-const hasJsonDiff = (object: unknown, objectToCompare: unknown) =>
-    JSON.stringify(object) !== JSON.stringify(objectToCompare);
+type JsonDiffProps = {
+    snapshotValue: unknown;
+    liveValue: unknown;
+    changeValue: unknown;
+    fallback?: unknown;
+};
+const hasJsonDiff = ({
+    snapshotValue,
+    liveValue,
+    changeValue,
+    fallback,
+}: JsonDiffProps) => {
+    const liveJson = JSON.stringify(liveValue ?? fallback);
+    return (
+        JSON.stringify(snapshotValue ?? fallback) !== liveJson &&
+        JSON.stringify(changeValue ?? fallback) !== liveJson
+    );
+};
 
 type DataToOverwrite<Prop extends keyof IFeatureStrategy> = {
     property: Prop;
@@ -18,58 +38,90 @@ export const getChangesThatWouldBeOverwritten = (
 ): ChangesThatWouldBeOverwritten | null => {
     const { snapshot } = change.payload;
     if (snapshot && currentStrategyConfig) {
-        const hasChanged = (a: unknown, b: unknown) => {
-            if (typeof a === 'object') {
-                return !isEqual(a, b);
+        const hasChanged = (
+            snapshotValue: unknown,
+            liveValue: unknown,
+            changeValue: unknown,
+        ) => {
+            if (typeof snapshotValue === 'object') {
+                return (
+                    !isEqual(snapshotValue, liveValue) &&
+                    !isEqual(snapshotValue, changeValue)
+                );
             }
-            return hasJsonDiff(a, b);
+            return hasJsonDiff({ snapshotValue, liveValue, changeValue });
         };
 
         // compare each property in the snapshot. The property order
         // might differ, so using JSON.stringify to compare them
         // doesn't work.
         const changes: ChangesThatWouldBeOverwritten = Object.entries(
-            currentStrategyConfig,
+            omit(currentStrategyConfig, 'strategyName'),
         )
             .map(([key, currentValue]: [string, unknown]) => {
                 const snapshotValue = snapshot[key as keyof IFeatureStrategy];
+                const changeValue =
+                    change.payload[key as keyof ChangeRequestEditStrategy];
 
                 // compare, assuming that order never changes
                 if (key === 'segments') {
                     // segments can be undefined on the original
                     // object, but that doesn't mean it has changed
-                    if (hasJsonDiff(snapshotValue, currentValue ?? [])) {
+                    if (
+                        hasJsonDiff({
+                            snapshotValue,
+                            liveValue: currentValue,
+                            changeValue,
+                            fallback: [],
+                        })
+                    ) {
                         return {
                             property: key as keyof IFeatureStrategy,
                             oldValue: currentValue,
-                            newValue: snapshotValue,
+                            newValue: changeValue,
                         };
                     }
                 } else if (key === 'variants') {
                     // strategy variants might not be defined, so use
                     // fallback values
-                    if (hasJsonDiff(snapshotValue ?? [], currentValue ?? [])) {
+                    if (
+                        hasJsonDiff({
+                            snapshotValue: snapshotValue,
+                            liveValue: currentValue,
+                            changeValue,
+                            fallback: [],
+                        })
+                    ) {
                         return {
                             property: key as keyof IFeatureStrategy,
                             oldValue: currentValue,
-                            newValue: snapshotValue,
+                            newValue: changeValue,
                         };
                     }
                 } else if (key === 'title') {
                     // the title can be defined as `null` or
                     // `undefined`, so we fallback to an empty string
-                    if (hasJsonDiff(snapshotValue ?? '', currentValue ?? '')) {
+                    if (
+                        hasJsonDiff({
+                            snapshotValue: snapshotValue,
+                            liveValue: currentValue,
+                            changeValue,
+                            fallback: '',
+                        })
+                    ) {
                         return {
                             property: key as keyof IFeatureStrategy,
                             oldValue: currentValue,
-                            newValue: snapshotValue,
+                            newValue: changeValue,
                         };
                     }
-                } else if (hasChanged(snapshotValue, currentValue)) {
+                } else if (
+                    hasChanged(snapshotValue, currentValue, changeValue)
+                ) {
                     return {
                         property: key as keyof IFeatureStrategy,
                         oldValue: currentValue,
-                        newValue: snapshotValue,
+                        newValue: changeValue,
                     };
                 }
             })

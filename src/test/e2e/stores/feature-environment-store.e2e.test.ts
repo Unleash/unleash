8a97dcc875717e33bce39d6409e25cc7a1112573
andreas-unleash <andreas@getunleash.ai>
2024-01-25 09:45:39 +0200

    chore: add tests
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- src/test/e2e/stores/feature-environment-store.e2e.test.ts
+++ src/test/e2e/stores/feature-environment-store.e2e.test.ts
@@ -125,8 +125,8 @@ test('Copying features also copies variants', async () => {
 });
 
 test('Copying strategies also copies strategy variants', async () => {
-    const envName = 'copy-env';
-    const featureName = 'copy-env-toggle-feature';
+    const envName = 'copy-strategy';
+    const featureName = 'copy-env-strategy-feature';
     await environmentStore.create({
         name: envName,
         enabled: true,

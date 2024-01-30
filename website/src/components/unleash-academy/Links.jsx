8da2ae35df3f746772b4b42fac50cec2d38be8ea
Thomas Heartman <thomas@getunleash.io>
2024-01-30 14:07:03 +0900

    docs: work on styling links

--- website/src/components/unleash-academy/Links.jsx
+++ website/src/components/unleash-academy/Links.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const LinkBox = ({ level, header, description }) => {
+const LinkBox = ({ level, header, description, link }) => {
     return (
-        <article>
-            <div>
+        <article className={level.toLowerCase()}>
+            <div className='header'>
                 <span className='level'>{level}</span>
                 <h3>{header}</h3>
-                <p>{description}</p>
             </div>
-            <button>Start learning</button>
+            <p>{description}</p>
+            <a href={link}>Start learning</a>
         </article>
     );
 };
@@ -19,18 +19,21 @@ const links = [
         header: 'Foundational',
         description:
             'For all roles working with Unleash - Developers, Product owners, Leaders',
+        link: 'https://docs.google.com/forms/d/1iPUk2I0k5xMzicn9aLMcPF3b9ub3ZwdVjRxCxWxV7js/viewform',
     },
     {
         level: 'Advanced',
         header: 'Advanced for Developers',
         description:
             'For Developers only, after Foundational content has been reviewed',
+        link: 'https://docs.google.com/forms/d/1NUL9hyO8Ys916TB6fPV3-jkvD97OmPXZ8_TO84Wjqgc/viewform',
     },
     {
         level: 'Advanced',
         header: 'Managing Unleash for DevOps/Admins',
         description:
             'For DevOps, Platform leads and Admins only after Foundational content has been reviewed',
+        link: 'https://docs.google.com/forms/d/1JlIqmXI3P7dj0n-OiUs2IYsYXgmqw23BChaemlSgHJA/viewform',
     },
 ];
 
@@ -38,12 +41,13 @@ const Component = () => {
     return (
         <div className='links-container'>
             <ol className='links-wrapper'>
-                {links.map(({ level, header, description }) => (
+                {links.map(({ level, header, description, link }) => (
                     <li>
                         <LinkBox
                             level={level}
                             header={header}
                             description={description}
+                            link={link}
                         />
                     </li>
                 ))}

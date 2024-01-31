bc95ed654fad6287117d21bfb9c2707a03ae74b8
Thomas Heartman <thomas@getunleash.io>
2024-01-31 18:18:21 +0900

    docs: add academy links and banners (#6083)
    This PR adds academy links and banners. Supersedes #6077

---
+++ website/src/components/unleash-academy/Banner.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Component = () => {
+    return (
+        <div className='banner-container'>
+            <div className='banner'>
+                <h3>
+                    Gain new skills, earn certifications, train your team, and
+                    advance your career.
+                </h3>
+                <ul className="unleash-academy-banner-list">
+                    <li>100% Free & online</li>
+                    <li>Valuable certification</li>
+                </ul>
+                <div className='small-logo'></div>
+                <span className='unleash-action-button'>Get certified</span>
+            </div>
+            <div className='big-logo'></div>
+        </div>
+    );
+};
+
+export default Component;

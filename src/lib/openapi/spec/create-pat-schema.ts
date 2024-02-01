db0a0d7097ac01dd8e8e4ea3c751c6518e3fc585
Nuno Góis <github@nunogois.com>
2024-02-01 14:28:46 +0000

    refactor: PATs (#6101)
    https://linear.app/unleash/issue/SR-379/refactor-pats
    
    This PR refactors PATs.
    
    - Adds a new `createPatSchema`, which better aligns with
    https://docs.getunleash.io/contributing/ADRs/overarching/separation-request-response-schemas
    - Drops the model type and class in favor of using the schema types
    directly, which is more consistent with the rest of the codebase and
    easier to maintain
     - Misc scouting, improvement and fixes
    
    This breaks Enterprise temporarily, but it's faster to move forward this
    way.

---
+++ src/lib/openapi/spec/create-pat-schema.ts
@@ -0,0 +1,25 @@
+import { FromSchema } from 'json-schema-to-ts';
+
+export const createPatSchema = {
+    $id: '#/components/schemas/createPatSchema',
+    description:
+        'Describes the properties required to create a [personal access token](https://docs.getunleash.io/how-to/how-to-create-personal-access-tokens), or PAT. PATs are automatically scoped to the authenticated user.',
+    type: 'object',
+    required: ['description', 'expiresAt'],
+    properties: {
+        description: {
+            type: 'string',
+            description: `The PAT's description.`,
+            example: 'user:xyzrandomstring',
+        },
+        expiresAt: {
+            type: 'string',
+            format: 'date-time',
+            description: `The PAT's expiration date.`,
+            example: '2023-04-19T08:15:14.000Z',
+        },
+    },
+    components: {},
+} as const;
+
+export type CreatePatSchema = FromSchema<typeof createPatSchema>;

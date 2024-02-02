87b28da551cdff5b25f0f2737444efc6c92a6226
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2024-02-02 12:00:20 +0100

    fix: show all selected application names (#6110) (#6111)

---
+++ frontend/src/component/feature/FeatureView/FeatureMetrics/FeatureMetricsTable/ApplicationsCell.tsx
@@ -0,0 +1,45 @@
+import { VFC } from 'react';
+import { styled, Typography } from '@mui/material';
+import { TooltipLink } from 'component/common/TooltipLink/TooltipLink';
+import { TextCell } from 'component/common/Table/cells/TextCell/TextCell';
+
+const StyledTag = styled(Typography)(({ theme }) => ({
+    fontSize: theme.fontSizes.smallerBody,
+}));
+
+interface IFeatureTagCellProps {
+    row: {
+        original: {
+            appName: string;
+            selectedApplications: string[];
+        };
+    };
+}
+
+export const ApplicationsCell: VFC<IFeatureTagCellProps> = ({ row }) => {
+    if (
+        row.original.selectedApplications &&
+        row.original.selectedApplications.length > 1
+    ) {
+        return (
+            <TextCell>
+                <TooltipLink
+                    tooltip={
+                        <>
+                            {row.original.selectedApplications.map(
+                                (appName) => (
+                                    <StyledTag key={appName}>
+                                        {appName}
+                                    </StyledTag>
+                                ),
+                            )}
+                        </>
+                    }
+                >
+                    {row.original.appName}
+                </TooltipLink>
+            </TextCell>
+        );
+    }
+    return <TextCell>{row.original.appName}</TextCell>;
+};

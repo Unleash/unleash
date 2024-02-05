0ec898377db2146ce76aea35a44fb38c354863d5
sjaanus <sellinjaanus@gmail.com>
2024-02-05 12:34:50 +0200

    Update

--- frontend/src/component/executiveDashboard/ProjectHealthChart/ProjectHealthChart.tsx
+++ frontend/src/component/executiveDashboard/ProjectHealthChart/ProjectHealthChart.tsx
@@ -1,8 +1,12 @@
-import { type VFC } from 'react';
+import { useMemo, type VFC } from 'react';
 import 'chartjs-adapter-date-fns';
-import { ExecutiveSummarySchema } from 'openapi';
+import { useTheme } from '@mui/material';
+import {
+    ExecutiveSummarySchema,
+    ExecutiveSummarySchemaProjectFlagTrendsItem,
+} from 'openapi';
 import { LineChart } from '../LineChart/LineChart';
-import { useProjectChartData } from '../useProjectChartData';
+import { getRandomColor } from '../executive-dashboard-utils';
 
 interface IFlagsProjectChartProps {
     projectFlagTrends: ExecutiveSummarySchema['projectFlagTrends'];
@@ -11,7 +15,37 @@ interface IFlagsProjectChartProps {
 export const ProjectHealthChart: VFC<IFlagsProjectChartProps> = ({
     projectFlagTrends,
 }) => {
-    const data = useProjectChartData(projectFlagTrends, 'health');
+    const theme = useTheme();
+
+    const data = useMemo(() => {
+        const groupedFlagTrends = projectFlagTrends.reduce<
+            Record<string, ExecutiveSummarySchemaProjectFlagTrendsItem[]>
+        >((groups, item) => {
+            if (!groups[item.project]) {
+                groups[item.project] = [];
+            }
+            groups[item.project].push(item);
+            return groups;
+        }, {});
+
+        const datasets = Object.entries(groupedFlagTrends).map(
+            ([project, trends]) => {
+                const color = getRandomColor();
+                return {
+                    label: project,
+                    data: trends.map((item) => item.health || 0),
+                    borderColor: color,
+                    backgroundColor: color,
+                    fill: true,
+                };
+            },
+        );
+
+        return {
+            labels: projectFlagTrends.map((item) => item.date),
+            datasets,
+        };
+    }, [theme, projectFlagTrends]);
 
     return <LineChart data={data} />;
 };

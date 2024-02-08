cc2191fda2912ba5738cc38dddcd8d7314d8e024
David Leek <david@getunleash.io>
2024-02-08 15:41:48 +0100

    chore: add indexes for traffic_group and status_code_series  to migration

--- src/migrations/20240208123212-create-stat-traffic-usage-table.js
+++ src/migrations/20240208123212-create-stat-traffic-usage-table.js
@@ -9,6 +9,8 @@ exports.up = (db, callback) => {
             count BIGINT NOT NULL DEFAULT 0,
             PRIMARY KEY(day, traffic_group, status_code_series)
             );
+            CREATE INDEX IF NOT EXISTS stat_traffic_usage_traffic_group_idx ON stat_traffic_usage (traffic_group);
+            CREATE INDEX IF NOT EXISTS  stat_traffic_usage_status_code_series_idx ON stat_traffic_usage (status_code_series);
         `,
         callback,
     );
@@ -17,6 +19,8 @@ exports.up = (db, callback) => {
 exports.down = (db, callback) => {
     db.runSql(
         `
+        DROP INDEX IF EXISTS stat_traffic_usage_traffic_group_idx;
+        DROP INDEX IF EXISTS stat_traffic_usage_status_code_series_idx;
         DROP TABLE IF EXISTS stat_traffic_usage;
         `,
         callback,

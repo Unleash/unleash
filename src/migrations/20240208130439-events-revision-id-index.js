c4e3917c838d6e8b1090377049d0937e859743ad
sjaanus <sellinjaanus@gmail.com>
2024-02-08 13:59:32 +0200

    Fix

--- src/migrations/20240208130439-events-revision-id-index.js
+++ src/migrations/20240208130439-events-revision-id-index.js
@@ -5,7 +5,7 @@ exports.up = function(db, cb) {
         `
             CREATE INDEX IF NOT EXISTS idx_events_feature_type_id ON events (id)
                 WHERE feature_name IS NOT NULL
-                    AND type IN ('segment-updated', 'feature_import', 'features-imported');
+                    OR type IN ('segment-updated', 'feature_import', 'features-imported');
 
         `,
         cb,

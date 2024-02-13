a7878900a068f868560da10ac352149560e21775
Gastón Fournier <gaston@getunleash.io>
2024-02-13 10:59:51 +0100

    test: log all query times

--- src/lib/db/db-pool.ts
+++ src/lib/db/db-pool.ts
@@ -1,12 +1,13 @@
 import { knex, Knex } from 'knex';
 import { IUnleashConfig } from '../types/option';
+import timer from '../util/timer';
 
 export function createDb({
     db,
     getLogger,
 }: Pick<IUnleashConfig, 'db' | 'getLogger'>): Knex {
     const logger = getLogger('db-pool.js');
-    return knex({
+    const conn = knex({
         client: 'pg',
         version: db.version,
         connection: {
@@ -22,6 +23,23 @@ export function createDb({
             error: (msg) => logger.error(msg),
         },
     });
+
+    // Object to store the start times of queries
+    const queryTimes = {};
+
+    conn.on('query', (query) => {
+        const { __knexQueryUid } = query;
+        queryTimes[__knexQueryUid] = timer.new();
+    });
+
+    conn.on('query-response', (response, query) => {
+        const { __knexQueryUid } = query;
+        const durationInSeconds = queryTimes[__knexQueryUid]();
+        console.log(`Query ${query.sql} took ${durationInSeconds * 1000}ms`);
+        // Clean up the start time record
+        delete queryTimes[__knexQueryUid];
+    });
+    return conn;
 }
 
 // for backward compatibility

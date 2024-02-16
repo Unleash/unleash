b3b290aaef1303e140a8ad16cf289c0e8c7b1fc3
kwasniew <kwasniewski.mateusz@gmail.com>
2024-02-16 10:04:11 +0100

    fix: backport import all strategy fields

--- src/lib/features/export-import-toggles/export-import-service.ts
+++ src/lib/features/export-import-toggles/export-import-service.ts
@@ -345,17 +345,11 @@ export default class ExportImportService
         await Promise.all(
             dto.data.featureStrategies
                 ?.filter(hasFeatureName)
-                .map((featureStrategy) =>
+                .map(({ featureName, ...restOfFeatureStrategy }) =>
                     this.featureToggleService.createStrategy(
+                        restOfFeatureStrategy,
                         {
-                            name: featureStrategy.name,
-                            constraints: featureStrategy.constraints,
-                            parameters: featureStrategy.parameters,
-                            segments: featureStrategy.segments,
-                            sortOrder: featureStrategy.sortOrder,
-                        },
-                        {
-                            featureName: featureStrategy.featureName,
+                            featureName,
                             environment: dto.environment,
                             projectId: dto.project,
                         },

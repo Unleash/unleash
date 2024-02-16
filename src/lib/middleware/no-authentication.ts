62d75eec212562c41be1694585da572bb2f7a049
Ivar Conradi Østhus <ivar@getunleash.ai>
2024-02-16 09:18:21 +0100

    fix: types and such

--- src/lib/middleware/no-authentication.ts
+++ src/lib/middleware/no-authentication.ts
@@ -1,26 +1,30 @@
 import { Application } from 'express';
 import NoAuthUser from '../types/no-auth-user';
 import { ApiTokenType } from '../types/models/api-token';
-import { ApiUser, permissions } from '../server-impl';
+import {
+    ApiUser,
+    IApiRequest,
+    IAuthRequest,
+    permissions,
+} from '../server-impl';
 import { DEFAULT_ENV } from '../util';
 
 // eslint-disable-next-line
 function noneAuthentication(baseUriPath: string, app: Application): void {
-    app.use(`${baseUriPath || ''}/api/admin/`, (req, res, next) => {
-        // @ts-expect-error
-        if (!req.user) {
-            // @ts-expect-error
-            req.user = new NoAuthUser();
-        }
-        next();
-    });
+    app.use(
+        `${baseUriPath || ''}/api/admin/`,
+        (req: IAuthRequest, res, next) => {
+            if (!req.user) {
+                req.user = new NoAuthUser();
+            }
+            next();
+        },
+    );
 }
 
 export function noApiToken(baseUriPath: string, app: Application) {
-    app.use(`${baseUriPath}/api/frontend`, (req, res, next) => {
-        //@ts-ignore
+    app.use(`${baseUriPath}/api/frontend`, (req: IApiRequest, res, next) => {
         if (!req.headers.authorization && !req.user) {
-            //@ts-ignore
             req.user = new ApiUser({
                 tokenName: 'unknown',
                 permissions: [permissions.FRONTEND],
@@ -32,10 +36,8 @@ export function noApiToken(baseUriPath: string, app: Application) {
         }
         next();
     });
-    app.use(`${baseUriPath}/api/client`, (req, res, next) => {
-        //@ts-ignore
+    app.use(`${baseUriPath}/api/client`, (req: IApiRequest, res, next) => {
         if (!req.headers.authorization && !req.user) {
-            //@ts-ignore
             req.user = new ApiUser({
                 tokenName: 'unknown',
                 permissions: [permissions.CLIENT],

e5fe4a7766fc146a09282e8db94d8f7f95c6a445
Thomas Heartman <thomas@getunleash.io>
2024-02-16 15:43:27 +0800

    chore: add sdkReporting flag to front end (#6253)
    It's already in the back end, but wasn't declared in the front end
    flags.

--- frontend/src/interfaces/uiConfig.ts
+++ frontend/src/interfaces/uiConfig.ts
@@ -79,6 +79,7 @@ export type UiFlags = {
     displayUpgradeEdgeBanner?: boolean;
     showInactiveUsers?: boolean;
     featureSearchFeedbackPosting?: boolean;
+    sdkReporting?: boolean;
 };
 
 export interface IVersionInfo {

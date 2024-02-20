ede4080ac2ddaab582676e42392b0178692b2762
Thomas Heartman <thomas@getunleash.io>
2024-02-20 12:46:22 +0800

    feat: add file for new layout

--- frontend/src/component/application/Application.tsx
+++ frontend/src/component/application/Application.tsx
@@ -15,7 +15,7 @@ import {
 import { Link as LinkIcon } from '@mui/icons-material';
 import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
 import { UPDATE_APPLICATION } from 'component/providers/AccessProvider/permissions';
-import { ConnectedInstances } from '../ConnectedInstances/ConnectedInstances';
+import { ConnectedInstances } from './ConnectedInstances/ConnectedInstances';
 import { Dialogue } from 'component/common/Dialogue/Dialogue';
 import { PageContent } from 'component/common/PageContent/PageContent';
 import { PageHeader } from 'component/common/PageHeader/PageHeader';

e80078aebe5a2fb5ab4a4215bc580be2d8314589
David Leek <david@getunleash.io>
2024-02-22 13:15:09 +0100

    chore: metrics for time taken loading features in proxy-repository

--- src/lib/metric-events.ts
+++ src/lib/metric-events.ts
@@ -4,6 +4,7 @@ const SCHEDULER_JOB_TIME = 'scheduler_job_time';
 const FEATURES_CREATED_BY_PROCESSED = 'features_created_by_processed';
 const EVENTS_CREATED_BY_PROCESSED = 'events_created_by_processed';
 const PROXY_REPOSITORY_CREATED = 'proxy_repository_created';
+const PROXY_FEATURES_FOR_TOKEN_TIME = 'proxy_features_for_token_time';
 
 export {
     REQUEST_TIME,
@@ -12,4 +13,5 @@ export {
     FEATURES_CREATED_BY_PROCESSED,
     EVENTS_CREATED_BY_PROCESSED,
     PROXY_REPOSITORY_CREATED,
+    PROXY_FEATURES_FOR_TOKEN_TIME,
 };

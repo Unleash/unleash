03f46e870140e0107a9e388e4add65d297d6301c
kwasniew <kwasniewski.mateusz@gmail.com>
2024-02-26 10:49:30 +0100

    refactor: paginated data hook

--- frontend/src/hooks/api/getters/useProjectApplications/useProjectApplications.ts
+++ frontend/src/hooks/api/getters/useProjectApplications/useProjectApplications.ts
@@ -1,84 +1,18 @@
-import useSWR, { SWRConfiguration } from 'swr';
-import { useCallback } from 'react';
-import { formatApiPath } from 'utils/formatPath';
-import handleErrorResponses from '../httpErrorResponseHandler';
-import {
-    GetProjectApplicationsParams,
-    ProjectApplicationsSchema,
-} from 'openapi';
-import { useClearSWRCache } from 'hooks/useClearSWRCache';
+import { ProjectApplicationsSchema } from 'openapi';
+import { createPaginatedHook } from '../usePaginatedData/usePaginatedData';
 
-type UseProjectApplicationsOutput = {
-    loading: boolean;
-    error: string;
-    refetch: () => void;
-} & ProjectApplicationsSchema;
-
-const fallbackData: ProjectApplicationsSchema = {
-    applications: [],
-    total: 0,
-};
+export const DEFAULT_PAGE_LIMIT = 25;
 
 const getPrefixKey = (projectId: string) => {
     return `api/admin/projects/${projectId}/applications?`;
 };
+const useParameterizedProjectApplications =
+    createPaginatedHook<ProjectApplicationsSchema>({
+        applications: [],
+        total: 0,
+    });
 
-const createProjectApplications = () => {
-    return (
-        projectId: string,
-        params: GetProjectApplicationsParams,
-        options: SWRConfiguration = {},
-    ): UseProjectApplicationsOutput => {
-        const { KEY, fetcher } = getProjectApplicationsFetcher(
-            projectId,
-            params,
-        );
-
-        const { data, error, mutate, isLoading } =
-            useSWR<ProjectApplicationsSchema>(KEY, fetcher, options);
-
-        const refetch = useCallback(() => {
-            mutate();
-        }, [mutate]);
-
-        const returnData = data || fallbackData;
-        return {
-            ...returnData,
-            loading: isLoading,
-            error,
-            refetch,
-        };
-    };
-};
-
-export const DEFAULT_PAGE_LIMIT = 25;
-
-export const useProjectApplications = createProjectApplications();
-
-const getProjectApplicationsFetcher = (
+export const useProjectApplications = (
+    params: Record<string, any>,
     projectId: string,
-    params: GetProjectApplicationsParams,
-) => {
-    const urlSearchParams = new URLSearchParams(
-        Array.from(
-            Object.entries(params)
-                .filter(([_, value]) => !!value)
-                .map(([key, value]) => [key, value.toString()]), // TODO: parsing non-string parameters
-        ),
-    ).toString();
-    const KEY = `${getPrefixKey(projectId)}${urlSearchParams}`;
-    useClearSWRCache(KEY, getPrefixKey(projectId));
-    const fetcher = () => {
-        const path = formatApiPath(KEY);
-        return fetch(path, {
-            method: 'GET',
-        })
-            .then(handleErrorResponses('Feature search'))
-            .then((res) => res.json());
-    };
-
-    return {
-        fetcher,
-        KEY,
-    };
-};
+) => useParameterizedProjectApplications(params, getPrefixKey(projectId));

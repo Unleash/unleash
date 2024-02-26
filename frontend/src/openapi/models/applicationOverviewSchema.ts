5c5d37279d07e9d9155f3e9cfe5b60f2a49b39ef
sjaanus <sellinjaanus@gmail.com>
2024-02-26 13:05:33 +0200

    chore: generate orval types

--- frontend/src/openapi/models/applicationOverviewSchema.ts
+++ frontend/src/openapi/models/applicationOverviewSchema.ts
@@ -4,6 +4,7 @@
  * See `gen:api` script in package.json
  */
 import type { ApplicationOverviewEnvironmentSchema } from './applicationOverviewEnvironmentSchema';
+import type { ApplicationOverviewIssuesSchema } from './applicationOverviewIssuesSchema';
 
 /**
  * Data about an application that's connected to Unleash via an SDK.
@@ -13,6 +14,8 @@ export interface ApplicationOverviewSchema {
     environments: ApplicationOverviewEnvironmentSchema[];
     /** The number of features the application has been using. */
     featureCount: number;
+    /** This list of issues that might be wrong with the application */
+    issues: ApplicationOverviewIssuesSchema[];
     /** The list of projects the application has been using. */
     projects: string[];
 }

43b013ff2f12493b2c84943a304ee8df240ba57f
Simon Hornby <liquidwicked64@gmail.com>
2024-02-27 14:33:57 +0200

    chore: add a flag for scim (#6361)

--- src/lib/types/experimental.ts
+++ src/lib/types/experimental.ts
@@ -51,7 +51,8 @@ export type IFlagKey =
     | 'userAccessUIEnabled'
     | 'disableUpdateMaxRevisionId'
     | 'disablePublishUnannouncedEvents'
-    | 'sdkReporting';
+    | 'sdkReporting'
+    | 'scimApi';
 
 export type IFlags = Partial<{ [key in IFlagKey]: boolean | Variant }>;
 
@@ -250,6 +251,10 @@ const flags: IFlags = {
     ),
     disablePublishUnannouncedEvents: parseEnvVarBoolean(
         process.env.UNLEASH_EXPERIMENTAL_DISABLE_SCHEDULED_CACHES,
+        false,
+    ),
+    scimApi: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_SCIM_API,
         false,
     ),
 };

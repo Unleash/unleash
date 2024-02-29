d3735166388b7757dbda5cbbefafc5537d9ab94e
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2024-02-29 18:55:22 +0100

    feat: project filtering hooks with tests

---
+++ frontend/src/component/executiveDashboard/hooks/useFilteredFlagsSummary.ts
@@ -0,0 +1,46 @@
+import { useMemo } from 'react';
+import { ExecutiveSummarySchemaProjectFlagTrendsItem } from 'openapi';
+
+export const useFilteredFlagsSummary = (
+    filteredProjectFlagTrends: ExecutiveSummarySchemaProjectFlagTrendsItem[],
+) =>
+    useMemo(() => {
+        const lastWeekId = filteredProjectFlagTrends.reduce((prev, current) => {
+            if (current.week > prev) return current.week;
+            return prev;
+        }, '');
+
+        const lastWeekSummary = filteredProjectFlagTrends.filter(
+            (summary) => summary.week === lastWeekId,
+        );
+
+        const averageUsers = (
+            lastWeekSummary.reduce(
+                (acc, current) => acc + (current.users || 0),
+                0,
+            ) / lastWeekSummary.length
+        ).toFixed(2);
+
+        const sum = lastWeekSummary.reduce(
+            (acc, current) => ({
+                total: acc.total + current.total,
+                active: acc.active + current.active,
+                stale: acc.stale + current.stale,
+                potentiallyStale:
+                    acc.potentiallyStale + current.potentiallyStale,
+                averageUsers,
+            }),
+            {
+                total: 0,
+                active: 0,
+                stale: 0,
+                potentiallyStale: 0,
+            },
+        );
+
+        return {
+            ...sum,
+            averageUsers,
+            averageHealth: ((sum.active / (sum.total || 1)) * 100).toFixed(0),
+        };
+    }, [filteredProjectFlagTrends]);

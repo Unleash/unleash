a958797a8a7e5f0424720749aee5a448581a178c
Jaanus Sellin <sellinjaanus@gmail.com>
2024-02-29 12:25:42 +0200

    feat: add creation buttons to application features and strategies (#6387)
    ![image](https://github.com/Unleash/unleash/assets/964450/2c8215af-3ab3-43e6-bdeb-1ae36aad3aa3)

--- frontend/src/component/application/ApplicationOverview.test.tsx
+++ frontend/src/component/application/ApplicationOverview.test.tsx
@@ -72,3 +72,48 @@ test('Display application overview without environments', async () => {
     await screen.findByText('my-app');
     await screen.findByText('No data available.');
 });
+
+test('Display application with issues', async () => {
+    setupApi({
+        environments: [
+            {
+                name: 'development',
+                instanceCount: 999,
+                lastSeen: new Date().toISOString(),
+                sdks: ['unleash-client-node:5.5.0-beta.0'],
+            },
+        ],
+        issues: [
+            {
+                type: 'missingFeatures',
+                items: ['feature1'],
+            },
+            {
+                type: 'missingStrategies',
+                items: ['strategy1'],
+            },
+        ],
+        featureCount: 1,
+        projects: ['default'],
+    });
+    render(
+        <Routes>
+            <Route
+                path={'/applications/:name'}
+                element={<ApplicationOverview />}
+            />
+        </Routes>,
+        {
+            route: '/applications/my-app',
+        },
+    );
+
+    await screen.findByText(
+        'We detected 1 feature flag defined in the SDK that does not exist in Unleash',
+    );
+    await screen.findByText(
+        'We detected 1 strategy type defined in the SDK that does not exist in Unleash',
+    );
+    await screen.findByText('feature1');
+    await screen.findByText('strategy1');
+});

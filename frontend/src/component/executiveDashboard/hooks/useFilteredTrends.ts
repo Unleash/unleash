7b67f218eb66652193f687945293cb15ff5afbd6
andreas-unleash <andreas@getunleash.ai>
2024-03-01 14:28:57 +0200

    fix: Project select bug with duplicate values (#6405)
    Project select fix for Executive Dashboard and Playground
    
    Extract the select to it's own component and move to `common`
    Re-use for both Dashboard and Playground
    Adds the id in parenthesis when there are duplicate names
    
    <img width="1406" alt="Screenshot 2024-03-01 at 12 04 22"
    src="https://github.com/Unleash/unleash/assets/104830839/379ea11f-d627-493e-8088-a739d58fba61">
    <img width="1434" alt="Screenshot 2024-03-01 at 12 36 46"
    src="https://github.com/Unleash/unleash/assets/104830839/9c5cf863-002c-4630-ac3a-4a869303a308">
    
    ---------
    
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- frontend/src/component/executiveDashboard/hooks/useFilteredTrends.ts
+++ frontend/src/component/executiveDashboard/hooks/useFilteredTrends.ts
@@ -1,5 +1,5 @@
 import { useMemo } from 'react';
-import { allOption } from '../ProjectSelect/ProjectSelect';
+import { allOption } from 'component/common/ProjectSelect/ProjectSelect';
 
 export const useFilteredTrends = <
     T extends {

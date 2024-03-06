752030d73cd1cb196b349754d874371b356cda5a
Gastón Fournier <gaston@getunleash.io>
2024-03-06 19:11:23 +0100

    Update src/lib/error/exceeds-limit-error.ts

--- src/lib/error/exceeds-limit-error.ts
+++ src/lib/error/exceeds-limit-error.ts
@@ -3,7 +3,7 @@ import { GenericUnleashError } from './unleash-error';
 export class ExceedsLimitError extends GenericUnleashError {
     constructor(resource: string, limit: number) {
         super({
-            name: 'ExceedsLimitError', // it can also be 'OperationDeniedError' or we can create a new one
+            name: 'ExceedsLimitError',
             message: `Failed to create ${resource}. You can't create more than the established limit of ${limit}.`,
             statusCode: 400,
         });

d882cd5fa4715dd7f51ec402fd1993e07fa499fd
sjaanus <sellinjaanus@gmail.com>
2024-03-07 14:00:30 +0200

    Implement getAll for frontend api

--- src/lib/proxy/client-feature-toggle-read-model.ts
+++ src/lib/proxy/client-feature-toggle-read-model.ts
@@ -8,12 +8,7 @@ import {
     ITag,
     PartialDeep,
 } from '../types';
-import {
-    ALL_PROJECTS,
-    DEFAULT_ENV,
-    ensureStringValue,
-    mapValues,
-} from '../util';
+import { ensureStringValue, mapValues } from '../util';
 import EventEmitter from 'events';
 import { Db } from '../db/db';
 import FeatureToggleStore from '../features/feature-toggle/feature-toggle-store';
@@ -43,11 +38,7 @@ export default class ClientFeatureToggleReadModel {
         this.flagResolver = flagResolver;
     }
 
-    private async getAll({
-        featureQuery,
-        archived,
-    }: IGetAllFeatures): Promise<IFeatureToggleClient[]> {
-        const environment = featureQuery?.environment || DEFAULT_ENV;
+    private async getAll(): Promise<Record<string, IFeatureToggleClient[]>> {
         const selectColumns = [
             'features.name as name',
             'features.description as description',
@@ -77,164 +68,7 @@ export default class ClientFeatureToggleReadModel {
         ] as (string | Raw<any>)[];
 
         let query = this.db('features')
-            .modify(FeatureToggleStore.filterByArchived, archived)
-            .leftJoin(
-                this.db('feature_strategies')
-                    .select('*')
-                    .where({ environment })
-                    .as('fs'),
-                'fs.feature_name',
-                'features.name',
-            )
-            .leftJoin(
-                this.db('feature_environments')
-                    .select(
-                        'feature_name',
-                        'enabled',
-                        'environment',
-                        'variants',
-                        'last_seen_at',
-                    )
-                    .where({ environment })
-                    .as('fe'),
-                'fe.feature_name',
-                'features.name',
-            )
-            .leftJoin(
-                'feature_strategy_segment as fss',
-                `fss.feature_strategy_id`,
-                `fs.id`,
-            )
-            .leftJoin('segments', `segments.id`, `fss.segment_id`)
-            .leftJoin('dependent_features as df', 'df.child', 'features.name');
-
-        query = query.select(selectColumns);
-
-        if (featureQuery) {
-            if (featureQuery.tag) {
-                const tagQuery = this.db
-                    .from('feature_tag')
-                    .select('feature_name')
-                    .whereIn(['tag_type', 'tag_value'], featureQuery.tag);
-                query = query.whereIn('features.name', tagQuery);
-            }
-            if (
-                featureQuery.project &&
-                !featureQuery.project.includes(ALL_PROJECTS)
-            ) {
-                query = query.whereIn('project', featureQuery.project);
-            }
-            if (featureQuery.namePrefix) {
-                query = query.where(
-                    'features.name',
-                    'like',
-                    `${featureQuery.namePrefix}%`,
-                );
-            }
-        }
-
-        const rows = await query;
-
-        const featureToggles = rows.reduce((acc, r) => {
-            const feature: PartialDeep<IFeatureToggleClient> = acc[r.name] ?? {
-                strategies: [],
-            };
-            if (this.isUnseenStrategyRow(feature, r) && !r.strategy_disabled) {
-                feature.strategies?.push(this.rowToStrategy(r));
-            }
-            if (this.isNewTag(feature, r)) {
-                this.addTag(feature, r);
-            }
-            if (featureQuery?.inlineSegmentConstraints && r.segment_id) {
-                this.addSegmentToStrategy(feature, r);
-            } else if (
-                !featureQuery?.inlineSegmentConstraints &&
-                r.segment_id
-            ) {
-                this.addSegmentIdsToStrategy(feature, r);
-            }
-            if (r.parent) {
-                feature.dependencies = feature.dependencies || [];
-                feature.dependencies.push({
-                    feature: r.parent,
-                    enabled: r.parent_enabled,
-                    ...(r.parent_enabled
-                        ? { variants: r.parent_variants }
-                        : {}),
-                });
-            }
-            feature.impressionData = r.impression_data;
-            feature.enabled = !!r.enabled;
-            feature.name = r.name;
-            feature.description = r.description;
-            feature.project = r.project;
-            feature.stale = r.stale;
-            feature.type = r.type;
-            feature.lastSeenAt = r.last_seen_at;
-            feature.variants = r.variants || [];
-            feature.project = r.project;
-
-            acc[r.name] = feature;
-            return acc;
-        }, {});
-
-        const features: IFeatureToggleClient[] = Object.values(featureToggles);
-
-        // strip away unwanted properties
-        const cleanedFeatures = features.map(({ strategies, ...rest }) => ({
-            ...rest,
-            strategies: strategies
-                ?.sort((strategy1, strategy2) => {
-                    if (
-                        typeof strategy1.sortOrder === 'number' &&
-                        typeof strategy2.sortOrder === 'number'
-                    ) {
-                        return strategy1.sortOrder - strategy2.sortOrder;
-                    }
-                    return 0;
-                })
-                .map(({ id, title, sortOrder, ...strategy }) => ({
-                    ...strategy,
-                })),
-        }));
-
-        return cleanedFeatures;
-    }
-
-    private async getAllByEnvironment({
-        featureQuery,
-        archived,
-    }: IGetAllFeatures): Promise<IFeatureToggleClient[]> {
-        const selectColumns = [
-            'features.name as name',
-            'features.description as description',
-            'features.type as type',
-            'features.project as project',
-            'features.stale as stale',
-            'features.impression_data as impression_data',
-            'features.last_seen_at as last_seen_at',
-            'features.created_at as created_at',
-            'fe.variants as variants',
-            'fe.last_seen_at as env_last_seen_at',
-            'fe.enabled as enabled',
-            'fe.environment as environment',
-            'fs.id as strategy_id',
-            'fs.strategy_name as strategy_name',
-            'fs.title as strategy_title',
-            'fs.disabled as strategy_disabled',
-            'fs.parameters as parameters',
-            'fs.constraints as constraints',
-            'fs.sort_order as sort_order',
-            'fs.variants as strategy_variants',
-            'segments.id as segment_id',
-            'segments.constraints as segment_constraints',
-            'df.parent as parent',
-            'df.variants as parent_variants',
-            'df.enabled as parent_enabled',
-        ] as (string | Raw<any>)[];
-
-        let query = this.db('features')
-            .modify(FeatureToggleStore.filterByArchived, archived)
+            .modify(FeatureToggleStore.filterByArchived, false)
             .leftJoin(
                 this.db('feature_strategies').select('*').as('fs'),
                 'fs.feature_name',
@@ -263,95 +97,64 @@ export default class ClientFeatureToggleReadModel {
 
         query = query.select(selectColumns);
 
-        if (featureQuery) {
-            if (featureQuery.tag) {
-                const tagQuery = this.db
-                    .from('feature_tag')
-                    .select('feature_name')
-                    .whereIn(['tag_type', 'tag_value'], featureQuery.tag);
-                query = query.whereIn('features.name', tagQuery);
-            }
-            if (
-                featureQuery.project &&
-                !featureQuery.project.includes(ALL_PROJECTS)
-            ) {
-                query = query.whereIn('project', featureQuery.project);
-            }
-            if (featureQuery.namePrefix) {
-                query = query.where(
-                    'features.name',
-                    'like',
-                    `${featureQuery.namePrefix}%`,
-                );
-            }
-        }
-
         const rows = await query;
 
-        const featureToggles = rows.reduce((acc, r) => {
-            const feature: PartialDeep<IFeatureToggleClient> = acc[r.name] ?? {
-                strategies: [],
-            };
-            if (this.isUnseenStrategyRow(feature, r) && !r.strategy_disabled) {
-                feature.strategies?.push(this.rowToStrategy(r));
-            }
-            if (this.isNewTag(feature, r)) {
-                this.addTag(feature, r);
-            }
-            if (featureQuery?.inlineSegmentConstraints && r.segment_id) {
-                this.addSegmentToStrategy(feature, r);
-            } else if (
-                !featureQuery?.inlineSegmentConstraints &&
-                r.segment_id
-            ) {
-                this.addSegmentIdsToStrategy(feature, r);
-            }
-            if (r.parent) {
-                feature.dependencies = feature.dependencies || [];
-                feature.dependencies.push({
-                    feature: r.parent,
-                    enabled: r.parent_enabled,
-                    ...(r.parent_enabled
-                        ? { variants: r.parent_variants }
-                        : {}),
-                });
-            }
-            feature.impressionData = r.impression_data;
-            feature.enabled = !!r.enabled;
-            feature.name = r.name;
-            feature.description = r.description;
-            feature.project = r.project;
-            feature.stale = r.stale;
-            feature.type = r.type;
-            feature.lastSeenAt = r.last_seen_at;
-            feature.variants = r.variants || [];
-            feature.project = r.project;
+        const data = this.getAggregatedData(rows);
+        return data;
+    }
 
-            acc[r.name] = feature;
-            return acc;
-        }, {});
+    getAggregatedData(rows): Record<string, IFeatureToggleClient[]> {
+        const featureTogglesByEnv: Record<string, IFeatureToggleClient[]> = {};
 
-        const features: IFeatureToggleClient[] = Object.values(featureToggles);
+        rows.forEach((row) => {
+            const environment = row.environment;
 
-        // strip away unwanted properties
-        const cleanedFeatures = features.map(({ strategies, ...rest }) => ({
-            ...rest,
-            strategies: strategies
-                ?.sort((strategy1, strategy2) => {
-                    if (
-                        typeof strategy1.sortOrder === 'number' &&
-                        typeof strategy2.sortOrder === 'number'
-                    ) {
-                        return strategy1.sortOrder - strategy2.sortOrder;
-                    }
-                    return 0;
-                })
-                .map(({ id, title, sortOrder, ...strategy }) => ({
-                    ...strategy,
-                })),
-        }));
+            if (!featureTogglesByEnv[environment]) {
+                featureTogglesByEnv[environment] = [];
+            }
 
-        return cleanedFeatures;
+            let feature = featureTogglesByEnv[environment].find(
+                (f) => f.name === row.name,
+            );
+
+            if (!feature) {
+                feature = {
+                    name: row.name,
+                    strategies: [],
+                    variants: row.variants || [],
+                    dependencies: [],
+                    impressionData: row.impression_data,
+                    enabled: !!row.enabled,
+                    description: row.description,
+                    project: row.project,
+                    stale: row.stale,
+                    type: row.type,
+                    lastSeenAt: row.last_seen_at,
+                };
+                featureTogglesByEnv[environment].push(feature);
+            } else {
+                if (
+                    this.isUnseenStrategyRow(feature, row) &&
+                    !row.strategy_disabled
+                ) {
+                    feature.strategies?.push(this.rowToStrategy(row));
+                }
+                if (this.isNewTag(feature, row)) {
+                    this.addTag(feature, row);
+                }
+                if (row.parent) {
+                    feature.dependencies = feature.dependencies || [];
+                    feature.dependencies.push({
+                        feature: row.parent,
+                        enabled: row.parent_enabled,
+                        ...(row.parent_enabled
+                            ? { variants: row.parent_variants }
+                            : {}),
+                    });
+                }
+            }
+        });
+        return featureTogglesByEnv;
     }
 
     private rowToStrategy(row: Record<string, any>): IStrategyConfig {
@@ -433,23 +236,6 @@ export default class ClientFeatureToggleReadModel {
     }
 
     async getClient(): Promise<Record<string, IFeatureToggleClient[]>> {
-        const envNames = await this.db('environments').select('name');
-        const environments: string[] = envNames.map(({ name }) => name);
-        const results = await Promise.all(
-            environments.map((environment) =>
-                this.getAll({
-                    featureQuery: { project: [ALL_PROJECTS], environment },
-                    archived: false,
-                }),
-            ),
-        );
-
-        const featuresByEnv: Record<string, IFeatureToggleClient[]> = {};
-
-        environments.forEach((key, index) => {
-            featuresByEnv[key] = results[index];
-        });
-
-        return featuresByEnv;
+        return this.getAll();
     }
 }

26d7065dc34039c131692ea19d5ae46cc3d19d7a
Nuno Góis <github@nunogois.com>
2024-03-07 17:04:36 +0000

    chore: add actions warning to delete project (#6467)
    https://linear.app/unleash/issue/2-1831/what-happens-with-automated-actions-if-the-project-is-archived
    
    Adds an actions-related warning to the project deletion.
    
    <img width="1008" alt="image"
    src="https://github.com/Unleash/unleash/assets/14320932/06fcaab0-c4a1-4ba3-8879-0c188f3bc01a">

--- frontend/src/component/project/Project/ProjectSettings/Settings/DeleteProject.tsx
+++ frontend/src/component/project/Project/ProjectSettings/Settings/DeleteProject.tsx
@@ -4,18 +4,17 @@ import PermissionButton from 'component/common/PermissionButton/PermissionButton
 import { DeleteProjectDialogue } from '../../DeleteProject/DeleteProjectDialogue';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
+import { useUiFlag } from 'hooks/useUiFlag';
+import { useActions } from 'hooks/api/getters/useActions/useActions';
+import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
+import useUiConfig from 'hooks/api/getters/useUiConfig/useUiConfig';
 
 const StyledContainer = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
     borderTop: `1px solid ${theme.palette.divider}`,
-}));
-
-const StyledTitle = styled('div')(({ theme }) => ({
     paddingTop: theme.spacing(4),
-    lineHeight: 2,
-}));
-
-const StyledCounter = styled('div')(({ theme }) => ({
-    paddingTop: theme.spacing(3),
+    gap: theme.spacing(2),
 }));
 
 const StyledButtonContainer = styled('div')(({ theme }) => ({
@@ -33,21 +32,45 @@ export const DeleteProject = ({
     projectId,
     featureCount,
 }: IDeleteProjectProps) => {
+    const { isEnterprise } = useUiConfig();
+    const automatedActionsEnabled = useUiFlag('automatedActions');
+    const { actions } = useActions(projectId);
     const [showDelDialog, setShowDelDialog] = useState(false);
     const navigate = useNavigate();
     return (
         <StyledContainer>
-            <StyledTitle>Delete project</StyledTitle>
-            <div>
+            <p>
                 Before you can delete a project, you must first archive all the
                 feature toggles associated with it. Keep in mind that deleting a
                 project will permanently remove all the archived feature
                 toggles, and they cannot be recovered once deleted.
-            </div>
-            <StyledCounter>
+            </p>
+            <ConditionallyRender
+                condition={isEnterprise() && automatedActionsEnabled}
+                show={
+                    <p>
+                        Additionally, all configured actions for this project
+                        will no longer be executed as they will be permanently
+                        deleted.
+                    </p>
+                }
+            />
+            <p>
                 Currently there are{' '}
                 <strong>{featureCount} feature toggles active</strong>
-            </StyledCounter>
+            </p>
+            <ConditionallyRender
+                condition={isEnterprise() && automatedActionsEnabled}
+                show={
+                    <p>
+                        Currently there are{' '}
+                        <strong>
+                            {actions.filter(({ enabled }) => enabled).length}{' '}
+                            enabled actions
+                        </strong>
+                    </p>
+                }
+            />
             <StyledButtonContainer>
                 <PermissionButton
                     permission={DELETE_PROJECT}

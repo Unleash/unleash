17ea8b37348fa5f7250bd366f051084db65b26e1
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2024-03-11 12:05:17 +0100

    feat: compare feture definitions not evaluations (#6486)

--- src/lib/proxy/proxy-service.ts
+++ src/lib/proxy/proxy-service.ts
@@ -19,6 +19,7 @@ import { PROXY_REPOSITORY_CREATED } from '../metric-events';
 import { ProxyRepository } from './index';
 import { FrontendApiRepository } from './frontend-api-repository';
 import { GlobalFrontendApiCache } from './global-frontend-api-cache';
+import isEqual from 'lodash.isequal';
 
 export type Config = Pick<
     IUnleashConfig,
@@ -92,6 +93,25 @@ export class ProxyService {
                 }),
                 impressionData: Boolean(feature.impressionData),
             }));
+    }
+
+    async compareToggleDefinitions(token: IApiUser) {
+        const oldClient = await this.clientForProxyToken(token);
+        const oldDefinitions = oldClient.getFeatureToggleDefinitions() || [];
+
+        const newClient = await this.newClientForProxyToken(token);
+        const newDefinitions = newClient.getFeatureToggleDefinitions() || [];
+
+        if (
+            !isEqual(
+                oldDefinitions.sort((a, b) => a.name.localeCompare(b.name)),
+                newDefinitions.sort((a, b) => a.name.localeCompare(b.name)),
+            )
+        ) {
+            this.logger.warn(
+                `old features definitions and new features definitions are different. Old definitions count ${oldDefinitions.length}, new count ${newDefinitions.length}`,
+            );
+        }
     }
 
     async getNewProxyFeatures(

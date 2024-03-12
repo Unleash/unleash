34a7c6db15faff102bec9ef7e36b5aa1195f0449
Christopher Kolstad <chriswk@getunleash.io>
2024-03-12 10:02:09 +0100

    fix: lints

--- frontend/src/component/admin/users/UsersAdmin.tsx
+++ frontend/src/component/admin/users/UsersAdmin.tsx
@@ -1,14 +1,14 @@
 import UsersList from './UsersList/UsersList';
-import {ADMIN} from 'component/providers/AccessProvider/permissions';
-import {PermissionGuard} from 'component/common/PermissionGuard/PermissionGuard';
-import {InviteLinkBar} from './InviteLinkBar/InviteLinkBar';
-import {Route, Routes} from 'react-router-dom';
+import { ADMIN } from 'component/providers/AccessProvider/permissions';
+import { PermissionGuard } from 'component/common/PermissionGuard/PermissionGuard';
+import { InviteLinkBar } from './InviteLinkBar/InviteLinkBar';
+import { Route, Routes } from 'react-router-dom';
 import EditUser from './EditUser/EditUser';
 import NotFound from 'component/common/NotFound/NotFound';
-import {InactiveUsersList} from './InactiveUsersList/InactiveUsersList';
-import {AccessMatrix} from './AccessMatrix/AccessMatrix';
-import {PremiumFeature} from '../../common/PremiumFeature/PremiumFeature';
-import {ConditionallyRender} from '../../common/ConditionallyRender/ConditionallyRender';
+import { InactiveUsersList } from './InactiveUsersList/InactiveUsersList';
+import { AccessMatrix } from './AccessMatrix/AccessMatrix';
+import { PremiumFeature } from '../../common/PremiumFeature/PremiumFeature';
+import { ConditionallyRender } from '../../common/ConditionallyRender/ConditionallyRender';
 import useUiConfig from '../../../hooks/api/getters/useUiConfig/useUiConfig';
 
 export const UsersAdmin = () => {

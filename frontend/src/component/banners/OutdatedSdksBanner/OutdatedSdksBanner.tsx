8eb0233a96b9f1c7d0f621622a24a691c3a24d2e
kwasniew <kwasniewski.mateusz@gmail.com>
2024-03-13 17:00:56 +0100

    feat: show outdated sdks banner

--- frontend/src/component/banners/OutdatedSdksBanner/OutdatedSdksBanner.tsx
+++ frontend/src/component/banners/OutdatedSdksBanner/OutdatedSdksBanner.tsx
@@ -1,21 +1,48 @@
 import { ConditionallyRender } from '../../common/ConditionallyRender/ConditionallyRender';
 import { Banner } from '../Banner/Banner';
 import { IBanner } from '../../../interfaces/banner';
+import { useOutdatedSdks } from '../../../hooks/api/getters/useOutdatedSdks/useOutdatedSdks';
+import { useUiFlag } from '../../../hooks/useUiFlag';
+import { Link } from 'react-router-dom';
+import { styled } from '@mui/material';
+
+const StyledList = styled('ul')({ margin: 0 });
 
 export const OutdatedSdksBanner = () => {
-    const displayOutdatedSdksBanner = false;
+    const {
+        data: { sdks },
+    } = useOutdatedSdks();
+    const flagEnabled = useUiFlag('sdkReporting');
+
     const outdatedSdksBanner: IBanner = {
         message: `We noticed that you're using outdated SDKs. `,
         variant: 'warning',
         link: 'dialog',
         linkText: 'Please update those versions',
         dialogTitle: 'Outdated SDKs',
-        dialog: <h1>Outdated SDKs</h1>,
+        dialog: (
+            <>
+                {sdks.map((item) => (
+                    <div key={item.sdkVersion}>
+                        <span>{item.sdkVersion}</span>
+                        <StyledList>
+                            {item.applications.map((application) => (
+                                <li key={application}>
+                                    <Link to={`/applications/${application}`}>
+                                        {application}
+                                    </Link>
+                                </li>
+                            ))}
+                        </StyledList>
+                    </div>
+                ))}
+            </>
+        ),
     };
     return (
         <>
             <ConditionallyRender
-                condition={displayOutdatedSdksBanner}
+                condition={flagEnabled && sdks.length > 0}
                 show={<Banner banner={outdatedSdksBanner} />}
             />
         </>

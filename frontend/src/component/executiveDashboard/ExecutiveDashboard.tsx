9f597bc1e968d36366c1966f6ec3082e2f701d8c
andreas-unleash <andreas@getunleash.ai>
2024-03-13 15:48:06 +0200

    fix: make insights header sticky
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- frontend/src/component/executiveDashboard/ExecutiveDashboard.tsx
+++ frontend/src/component/executiveDashboard/ExecutiveDashboard.tsx
@@ -1,4 +1,4 @@
-import { VFC } from 'react';
+import { useState, VFC } from 'react';
 import { Box, styled } from '@mui/material';
 import { ArrayParam, withDefault } from 'use-query-params';
 import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
@@ -45,15 +45,19 @@ const ChartWidget = styled(Widget)(({ theme }) => ({
     },
 }));
 
-const StickyWrapper = styled(Box)(({ theme }) => ({
-    position: 'sticky',
-    top: 0,
-    zIndex: 1000,
-    padding: theme.spacing(2,0,4),
-    background: theme.palette.background.application,
-}));
+const StickyWrapper = styled(Box)<{ scrolled?: boolean }>(
+    ({ theme, scrolled }) => ({
+        position: 'sticky',
+        top: 0,
+        zIndex: 1000,
+        padding: scrolled ? theme.spacing(2, 0) : theme.spacing(0, 0, 2),
+        background: theme.palette.background.application,
+        transition: 'padding 0.5s ease',
+    }),
+);
 
 export const ExecutiveDashboard: VFC = () => {
+    const [scrolled, setScrolled] = useState(false);
     const { executiveDashboardData, loading, error } = useExecutiveDashboard();
     const stateConfig = {
         projects: withDefault(ArrayParam, [allOption.id]),
@@ -80,9 +84,21 @@ export const ExecutiveDashboard: VFC = () => {
     const summary = useFilteredFlagsSummary(projectsData);
     const isOneProjectSelected = projects.length === 1;
 
+    const handleScroll = () => {
+        if (!scrolled && window.scrollY > 0) {
+            setScrolled(true);
+        } else if (scrolled && window.scrollY === 0) {
+            setScrolled(false);
+        }
+    };
+
+    if (typeof window !== 'undefined') {
+        window.addEventListener('scroll', handleScroll);
+    }
+
     return (
         <>
-            <StickyWrapper>
+            <StickyWrapper scrolled={scrolled}>
                 <DashboardHeader
                     actions={
                         <ProjectSelect

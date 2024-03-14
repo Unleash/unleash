05c014cde78b5e415b53eb86c5ce07481a16eaf0
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2024-03-14 15:59:44 +0100

    fix: uri decode breadcrumbs (#6557)

---
+++ frontend/src/component/common/BreadcrumbNav/BreadcrumbNav.test.tsx
@@ -0,0 +1,22 @@
+import { render } from 'utils/testRenderer';
+import { Route, Routes } from 'react-router-dom';
+import BreadcrumbNav from './BreadcrumbNav';
+import { screen } from '@testing-library/react';
+
+test('decode URI encoded path in breadcrumbs nav', async () => {
+    render(
+        <Routes>
+            <Route
+                path={'/applications/:name/:instance'}
+                element={<BreadcrumbNav />}
+            />
+        </Routes>,
+        {
+            route: '/applications/my%20app/my%20instance',
+        },
+    );
+
+    await screen.findByText('applications');
+    await screen.findByText('my app');
+    await screen.findByText('my instance');
+});

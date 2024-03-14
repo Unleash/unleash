7c69500cd01065bcdd008f936c4d809878bf919e
Gastón Fournier <gaston@getunleash.io>
2024-03-14 13:59:20 +0100

    chore: mark resource limits config type fields mandatory (#6550)
    ## About the changes
    All fields should be defined in the configuration to either a
    user-provided value or a sensible default

--- src/lib/openapi/spec/resource-limits-schema.ts
+++ src/lib/openapi/spec/resource-limits-schema.ts
@@ -4,49 +4,59 @@ export const resourceLimitsSchema = {
     $id: '#/components/schemas/resourceLimitsSchema',
     type: 'object',
     description: 'A map of resource names and their limits.',
+    required: [
+        'segmentValues',
+        'strategySegments',
+        'actionSetActions',
+        'actionSetsPerProject',
+        'actionSetFilters',
+        'actionSetFilterValues',
+        'signalEndpoints',
+        'signalTokensPerEndpoint',
+    ],
     additionalProperties: false,
     properties: {
         segmentValues: {
-            type: 'number',
+            type: 'integer',
             example: 10,
             description: 'The maximum number of values per segment allowed.',
         },
         strategySegments: {
-            type: 'number',
+            type: 'integer',
             example: 10,
             description: 'The maximum number of strategy segments allowed.',
         },
         actionSetActions: {
-            type: 'number',
+            type: 'integer',
             example: 10,
             description:
                 'The maximum number of actions per action set allowed.',
         },
         actionSetsPerProject: {
-            type: 'number',
+            type: 'integer',
             example: 10,
             description:
                 'The maximum number of action set definitions per project allowed.',
         },
         actionSetFilters: {
-            type: 'number',
+            type: 'integer',
             example: 10,
             description:
                 'The maximum number of filters per action set allowed.',
         },
         actionSetFilterValues: {
-            type: 'number',
+            type: 'integer',
             example: 10,
             description:
                 'The maximum number of filter values inside an action set allowed.',
         },
         signalEndpoints: {
-            type: 'number',
+            type: 'integer',
             example: 10,
             description: 'The maximum number of signal endpoints allowed.',
         },
         signalTokensPerEndpoint: {
-            type: 'number',
+            type: 'integer',
             example: 10,
             description:
                 'The maximum number of signal tokens per endpoint allowed.',

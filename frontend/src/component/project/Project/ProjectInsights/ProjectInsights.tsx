f2c57f0fbd479b53e89e81c51d321b690d4c63f8
Jaanus Sellin <sellinjaanus@gmail.com>
2024-03-18 14:50:25 +0200

    feat: insights grid initial layout (#6591)

---
+++ frontend/src/component/project/Project/ProjectInsights/ProjectInsights.tsx
@@ -0,0 +1,47 @@
+import { Box, styled } from '@mui/material';
+
+const Grid = styled(Box)(({ theme }) => ({
+    display: 'grid',
+    gap: theme.spacing(2),
+    gridTemplateColumns: 'repeat(10, 1fr)',
+}));
+
+const Overview = styled(Box)(({ theme }) => ({
+    gridColumn: '1 / -1',
+}));
+
+const Health = styled(Box)(({ theme }) => ({
+    gridColumn: 'span 5',
+}));
+
+const LeadTime = styled(Box)(({ theme }) => ({
+    gridColumn: 'span 5',
+}));
+
+const ToggleTypesUsed = styled(Box)(({ theme }) => ({
+    gridColumn: 'span 2',
+}));
+
+const ProjectMembers = styled(Box)(({ theme }) => ({
+    gridColumn: 'span 2',
+}));
+
+const ChangeRequests = styled(Box)(({ theme }) => ({
+    gridColumn: 'span 5',
+}));
+
+export const ProjectInsights = () => {
+    return (
+        <Grid>
+            <Overview>
+                Total changes / avg time to production / feature flags /stale
+                flags
+            </Overview>
+            <Health>Project Health</Health>
+            <LeadTime>Lead time</LeadTime>
+            <ToggleTypesUsed>Toggle types used</ToggleTypesUsed>
+            <ProjectMembers>Project members</ProjectMembers>
+            <ChangeRequests>Change Requests</ChangeRequests>
+        </Grid>
+    );
+};

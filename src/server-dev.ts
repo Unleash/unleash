89f963cec19a0e084d84d96ea6fd82f83082f6f0
Fredrik Oseberg <fredrik.no@gmail.com>
2024-03-18 10:11:22 +0100

    chore: update server-dev

--- src/server-dev.ts
+++ src/server-dev.ts
@@ -52,12 +52,8 @@ process.nextTick(async () => {
                         sdkReporting: true,
                         outdatedSdksBanner: true,
                         globalFrontendApiCache: true,
-<<<<<<< HEAD
-                        returnGlobalFrontendApiCache: false,
-=======
                         returnGlobalFrontendApiCache: true,
                         projectOverviewRefactor: true,
->>>>>>> 14b7e4c1a (feat: setup ff)
                     },
                 },
                 authentication: {

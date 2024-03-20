c0e13ca9eb61c6d8bb88a9e4238dfba20f2bf731
andreas-unleash <andreas@getunleash.ai>
2024-03-20 16:20:56 +0200

    chore: fix react errors
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- frontend/src/component/executiveDashboard/hooks/useDashboardData.ts
+++ frontend/src/component/executiveDashboard/hooks/useDashboardData.ts
@@ -8,36 +8,45 @@ import { useAvgTimeToProduction } from './useAvgTimeToProduction';
 export const useDashboardData = (
     executiveDashboardData: ExecutiveSummarySchema,
     projects: string[],
-) =>
-    useMemo(() => {
-        const projectsData = useFilteredTrends(
-            executiveDashboardData.projectFlagTrends,
-            projects,
-        );
+) => {
+    const projectsData = useFilteredTrends(
+        executiveDashboardData.projectFlagTrends,
+        projects,
+    );
 
-        const groupedProjectsData = useGroupedProjectTrends(projectsData);
+    const groupedProjectsData = useGroupedProjectTrends(projectsData);
 
-        const metricsData = useFilteredTrends(
-            executiveDashboardData.metricsSummaryTrends,
-            projects,
-        );
-        const groupedMetricsData = useGroupedProjectTrends(metricsData);
+    const metricsData = useFilteredTrends(
+        executiveDashboardData.metricsSummaryTrends,
+        projects,
+    );
+    const groupedMetricsData = useGroupedProjectTrends(metricsData);
 
-        const { users, environmentTypeTrends } = executiveDashboardData;
+    const summary = useFilteredFlagsSummary(projectsData);
 
-        const summary = useFilteredFlagsSummary(projectsData);
+    const avgDaysToProduction = useAvgTimeToProduction(groupedProjectsData);
 
-        const avgDaysToProduction = useAvgTimeToProduction(groupedProjectsData);
-
-        return {
+    return useMemo(
+        () => ({
             ...executiveDashboardData,
             projectsData,
             groupedProjectsData,
             metricsData,
             groupedMetricsData,
-            users,
-            environmentTypeTrends,
+            users: executiveDashboardData.users,
+            environmentTypeTrends: executiveDashboardData.environmentTypeTrends,
             summary,
             avgDaysToProduction,
-        };
-    }, [executiveDashboardData, projects]);
+        }),
+        [
+            executiveDashboardData,
+            projects,
+            projectsData,
+            groupedProjectsData,
+            metricsData,
+            groupedMetricsData,
+            summary,
+            avgDaysToProduction,
+        ],
+    );
+};

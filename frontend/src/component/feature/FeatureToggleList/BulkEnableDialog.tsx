20c41ca1482f7484f9517aab1e3351690e6cc9ae
Jaanus Sellin <sellinjaanus@gmail.com>
2024-03-26 22:16:23 +0200

    refactor: remove quering old features endpoint from BulkEnableDialog  (#6698)
    This was meant to repull the features, but we are using search, so it
    was doing nothing.
    I tested and features are still updated after removal.

--- frontend/src/component/feature/FeatureToggleList/BulkEnableDialog.tsx
+++ frontend/src/component/feature/FeatureToggleList/BulkEnableDialog.tsx
@@ -7,7 +7,6 @@ import type { FeatureSchema } from 'openapi';
 
 import { formatUnknownError } from 'utils/formatUnknownError';
 import useFeatureApi from 'hooks/api/actions/useFeatureApi/useFeatureApi';
-import useProject from 'hooks/api/getters/useProject/useProject';
 import { useChangeRequestsEnabled } from 'hooks/useChangeRequestsEnabled';
 import { useChangeRequestApi } from 'hooks/api/actions/useChangeRequestApi/useChangeRequestApi';
 import { usePendingChangeRequests } from 'hooks/api/getters/usePendingChangeRequests/usePendingChangeRequests';
@@ -43,7 +42,6 @@ export const BulkEnableDialog = ({
     const [selected, setSelected] = useState(environments[0]);
     const { bulkToggleFeaturesEnvironmentOn } = useFeatureApi();
     const { addChange } = useChangeRequestApi();
-    const { refetch: refetchProject } = useProject(projectId);
     const { setToastApiError, setToastData } = useToast();
     const { isChangeRequestConfigured } = useChangeRequestsEnabled(projectId);
     const { refetch: refetchChangeRequests } =
@@ -85,7 +83,6 @@ export const BulkEnableDialog = ({
                     data.map((feature) => feature.name),
                     selected,
                 );
-                refetchProject();
                 setToastData({
                     text: 'Your feature toggles have been enabled',
                     type: 'success',

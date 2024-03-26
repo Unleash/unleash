7f5481834b16761c916e5972466634780cde4c62
sjaanus <sellinjaanus@gmail.com>
2024-03-26 16:30:25 +0200

    Fix

--- frontend/src/openapi/models/sdkContextSchema.ts
+++ frontend/src/openapi/models/sdkContextSchema.ts
@@ -26,5 +26,5 @@ export interface SdkContextSchema {
     sessionId?: string;
     /** An identifier for the current user */
     userId?: string;
-    [key: string]: string;
+    [key: string]: any;
 }

5b325568d14ba24ac50e078d7d7a4020b3f01acc
andreas-unleash <andreas@getunleash.ai>
2024-03-28 13:07:04 +0200

    Chore: context fields UI improvements when creating context field (#6714)
    Styling adjustments based on
    https://linear.app/unleash/issue/UNL-371/ui-improvement-createedit-context-field
    
    - Remove maxWidth from container
    - In ul - add maxHeight, padding and border and hide when :empty
    
    Closes #
    [UNL-371](https://linear.app/unleash/issue/UNL-371/ui-improvement-createedit-context-field)
    
    Before: 
    ![Screenshot 2024-03-28 at 11 46
    12](https://github.com/Unleash/unleash/assets/104830839/8150fb96-0a5a-4fd8-988a-82a42da51faf)
    
    After:
    ![Screenshot 2024-03-28 at 11 47
    03](https://github.com/Unleash/unleash/assets/104830839/186b806c-4557-42a8-b119-22d1b781cc57)
    
    ---------
    
    Signed-off-by: andreas-unleash <andreas@getunleash.ai>

--- frontend/src/component/context/ContectFormChip/ContextFormChipList.tsx
+++ frontend/src/component/context/ContectFormChip/ContextFormChipList.tsx
@@ -6,9 +6,16 @@ const StyledContainer = styled('ul')(({ theme }) => ({
     display: 'flex',
     flexWrap: 'wrap',
     gap: theme.spacing(1),
-    padding: 0,
+    padding: theme.spacing(2),
     margin: 0,
     marginBottom: '1rem !important',
+    maxHeight: '412px',
+    overflow: 'auto',
+    borderRadius: theme.shape.borderRadiusMedium,
+    border: `1px solid ${theme.palette.divider}`,
+    '&:empty': {
+        display: 'none',
+    },
 }));
 
 export const ContextFormChipList: React.FC = StyledContainer;

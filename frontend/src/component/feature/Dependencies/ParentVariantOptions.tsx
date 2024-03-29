bc2ea055fa7cee95005cbcaa0c9e590479ae25b8
kwasniew <kwasniewski.mateusz@gmail.com>
2024-03-29 14:49:02 +0100

    feat: variant dependencies ui

---
+++ frontend/src/component/feature/Dependencies/ParentVariantOptions.tsx
@@ -0,0 +1,48 @@
+import { Autocomplete, Checkbox, styled, TextField } from '@mui/material';
+import type { FC } from 'react';
+import { useParentVariantOptions } from 'hooks/api/getters/useFeatureDependencyOptions/useFeatureDependencyOptions';
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+
+const StyledAutocomplete = styled(Autocomplete)(({ theme }) => ({
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(1.5),
+}));
+
+export const ParentVariantOptions: FC<{
+    project: string;
+    parent: string;
+    selectedValues: string[];
+    onSelect: (values: string[]) => void;
+}> = ({ project, parent, onSelect, selectedValues }) => {
+    const { parentVariantOptions } = useParentVariantOptions(project, parent);
+    const icon = <CheckBoxOutlineBlankIcon fontSize='small' />;
+    const checkedIcon = <CheckBoxIcon fontSize='small' />;
+    return (
+        <StyledAutocomplete
+            multiple
+            id='parent-variant-options'
+            options={parentVariantOptions}
+            disableCloseOnSelect
+            renderOption={(props, option, { selected }) => (
+                <li {...props}>
+                    <Checkbox
+                        icon={icon}
+                        checkedIcon={checkedIcon}
+                        style={{ marginRight: 8 }}
+                        checked={selected}
+                    />
+                    {option}
+                </li>
+            )}
+            renderInput={(params) => (
+                <TextField {...params} placeholder='Select values' />
+            )}
+            fullWidth
+            value={selectedValues}
+            onChange={(_, selectedValues) => {
+                onSelect(selectedValues as string[]);
+            }}
+        />
+    );
+};

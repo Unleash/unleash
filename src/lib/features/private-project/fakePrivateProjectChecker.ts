0422e5b5b05bf4ac1687a99f43c29563f8107810
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2024-04-04 14:56:21 +0200

    feat: private projects in application overview (#6775)

--- src/lib/features/private-project/fakePrivateProjectChecker.ts
+++ src/lib/features/private-project/fakePrivateProjectChecker.ts
@@ -2,6 +2,12 @@ import type { IPrivateProjectChecker } from './privateProjectCheckerType';
 import { ALL_PROJECT_ACCESS, type ProjectAccess } from './privateProjectStore';
 
 export class FakePrivateProjectChecker implements IPrivateProjectChecker {
+    async filterUserAccessibleProjects(
+        userId: number,
+        projects: string[],
+    ): Promise<string[]> {
+        return projects;
+    }
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async getUserAccessibleProjects(userId: number): Promise<ProjectAccess> {
         return ALL_PROJECT_ACCESS;
--- src/lib/features/private-project/privateProjectChecker.ts
+++ src/lib/features/private-project/privateProjectChecker.ts
@@ -22,6 +22,21 @@ export class PrivateProjectChecker implements IPrivateProjectChecker {
             : Promise.resolve(ALL_PROJECT_ACCESS);
     }
 
+    async filterUserAccessibleProjects(
+        userId: number,
+        projects: string[],
+    ): Promise<string[]> {
+        if (!this.isEnterprise) {
+            return projects;
+        }
+        const accessibleProjects =
+            await this.privateProjectStore.getUserAccessibleProjects(userId);
+        if (accessibleProjects.mode === 'all') return projects;
+        return projects.filter((project) =>
+            accessibleProjects.projects.includes(project),
+        );
+    }
+
     async hasAccessToProject(
         userId: number,
         projectId: string,

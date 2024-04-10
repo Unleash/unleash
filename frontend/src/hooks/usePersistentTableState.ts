e351ecb2044ee3681af6b8d34f75b8eb8938a112
Thomas Heartman <thomas@getunleash.io>
2024-04-10 14:06:44 +0200

    fix: make it a one-line change instead

--- frontend/src/hooks/usePersistentTableState.ts
+++ frontend/src/hooks/usePersistentTableState.ts
@@ -42,9 +42,7 @@ export const usePersistentTableState = <T extends QueryParamConfigMap>(
 
     const [tableState, setTableStateInternal] = useQueryParams(
         queryParamsDefinition,
-        {
-            updateType: 'replace',
-        },
+        { updateType: 'replace' },
     );
 
     const [searchParams] = useSearchParams();

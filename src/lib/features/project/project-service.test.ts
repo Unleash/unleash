de098deb70da56e8556eea6fed686937f6255abf
Thomas Heartman <thomas@getunleash.io>
2024-04-17 11:06:22 +0200

    chore: add test to make sure the fn is called

---
+++ src/lib/features/project/project-service.test.ts
@@ -0,0 +1,21 @@
+import { createTestConfig } from '../../../test/config/test-config';
+import { createFakeProjectService } from './createProjectService';
+
+describe('enterprise extension: enable change requests', () => {
+    test('it calls the change request enablement function', async () => {
+        const enableChangeRequests = jest.fn();
+
+        const config = createTestConfig();
+        const service = createFakeProjectService(config);
+        await service.createProject(
+            {
+                id: 'fake-project-id',
+                name: 'fake-project-name',
+            },
+            { id: 1, permissions: [], isAPI: false },
+            enableChangeRequests,
+        );
+
+        expect(enableChangeRequests).toHaveBeenCalled();
+    });
+});

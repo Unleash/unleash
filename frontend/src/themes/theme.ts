18c35bc489cb54c5e9a0c6b85783a79b2a107b10
sjaanus <sellinjaanus@gmail.com>
2024-04-17 10:50:42 +0300

    feat: restyle the project meta box

--- frontend/src/themes/theme.ts
+++ frontend/src/themes/theme.ts
@@ -50,6 +50,7 @@ export const theme = {
     fontSizes: {
         extraLargeHeader: '2.5rem',
         largeHeader: '2rem',
+        mediumHeader: '1.5rem',
         mainHeader: '1.25rem',
         bodySize: '1rem',
         smallBody: `${14 / 16}rem`,

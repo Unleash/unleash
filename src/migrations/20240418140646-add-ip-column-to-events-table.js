bfed1fae11cf70780a77e4951c9a803e3ce56053
Christopher Kolstad <chriswk@getunleash.io>
2024-04-18 16:27:28 +0200

    Remember to add cb as parameter to down migration as well

--- src/migrations/20240418140646-add-ip-column-to-events-table.js
+++ src/migrations/20240418140646-add-ip-column-to-events-table.js
@@ -2,6 +2,6 @@ exports.up = function (db, cb) {
     db.runSql(`ALTER TABLE events ADD COLUMN ip TEXT`, cb);
 };
 
-exports.down = function (db) {
+exports.down = function (db, cb) {
     db.runSql(`ALTER TABLE events DROP COLUMN ip`, cb);
 };

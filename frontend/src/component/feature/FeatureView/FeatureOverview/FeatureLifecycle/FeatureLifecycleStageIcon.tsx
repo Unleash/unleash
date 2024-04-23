93627a0f0b3877f64f7fdfae487356af7396fe04
kwasniew <kwasniewski.mateusz@gmail.com>
2024-04-23 16:29:03 +0200

    feat: live and pre-live stages UI

--- frontend/src/component/feature/FeatureView/FeatureOverview/FeatureLifecycle/FeatureLifecycleStageIcon.tsx
+++ frontend/src/component/feature/FeatureView/FeatureOverview/FeatureLifecycle/FeatureLifecycleStageIcon.tsx
@@ -8,8 +8,14 @@ import { ReactComponent as ArchivedStageIcon } from 'assets/icons/stage-archived
 
 export type LifecycleStage =
     | { name: 'initial' }
-    | { name: 'pre-live' }
-    | { name: 'live' }
+    | {
+          name: 'pre-live';
+          environments: Array<{ name: string; lastSeenAt: string }>;
+      }
+    | {
+          name: 'live';
+          environments: Array<{ name: string; lastSeenAt: string }>;
+      }
     | {
           name: 'completed';
           status: 'kept' | 'discarded';

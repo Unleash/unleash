4697b70cc139ca80b58e1c9dcf713e1a1535cc9c
Thomas Heartman <thomas@getunleash.io>
2024-04-24 11:02:34 +0200

    feat: stub out some tests

--- src/lib/features/project/project-owners-read-model.test.ts
+++ src/lib/features/project/project-owners-read-model.test.ts
@@ -1,9 +1,37 @@
+import dbInit, { type ITestDb } from '../../../test/e2e/helpers/database-init';
+import getLogger from '../../../test/fixtures/no-logger';
+import { ProjectOwnersReadModel } from './project-owners-read-model';
+
 describe('unit tests', () => {
     test('maps owners to projects', () => {});
+});
+
+let db: ITestDb;
+let readModel: ProjectOwnersReadModel;
+
+beforeAll(async () => {
+    db = await dbInit('project_owners_read_model_serial', getLogger);
+    readModel = new ProjectOwnersReadModel(db.rawDatabase);
+});
+
+afterAll(async () => {
+    if (db) {
+        await db.destroy();
+    }
 });
 
 describe('integration tests', () => {
     test('gets project owners', () => {});
     test('returns the system owner for the default project', () => {});
-    test('returns an empty list if there are no projects', () => {});
+    test('returns an empty list if there are no projects', async () => {
+        const owners = await readModel.getAllProjectOwners();
+
+        expect(owners).toStrictEqual({});
+    });
+
+    test('enriches fully', async () => {
+        const owners = await readModel.enrichWithOwners([]);
+
+        expect(owners).toStrictEqual([]);
+    });
 });

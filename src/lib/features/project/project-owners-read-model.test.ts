cbecd6c5454621b434d444c436cc32af13c78839
Thomas Heartman <thomas@getunleash.io>
2024-04-24 10:57:17 +0200

    feat: rewrite some tests

--- src/lib/features/project/project-owners-read-model.test.ts
+++ src/lib/features/project/project-owners-read-model.test.ts
@@ -1,25 +1,9 @@
-// describe.only('get projects owners', () => {
-//     test('returns empty list if there is no project with an owner', async () => {
-//         const owners = await projectOwnersReadModel();
-//         expect(owners).toHaveLength(0);
-//     })
+describe('unit tests', () => {
+    test('maps owners to projects', () => {});
+});
 
-//     test('returns an user who created a project as an owner', async () => {
-//         const project = {
-//             id: 'project-owners',
-//             name: 'Project Owners',
-//             mode: 'open' as const,
-//             defaultStickiness: 'clientId',
-//         };
-
-//         const owner = await stores.userStore.insert({
-//             name: 'Owner',
-//             email: 'owner-test@example.com'
-//         });
-
-//         await projectService.createProject(project, owner, auditUser);
-
-//         const owners = await projectService.getProjectsOwners();
-//         expect(owners).toHaveLength(1);
-//     })
-// });
+describe('integration tests', () => {
+    test('gets project owners', () => {});
+    test('returns the system owner for the default project', () => {});
+    test('returns an empty list if there are no projects', () => {});
+});

5053d0ca24348ddc04724a1ee67821694d297fe8
Thomas Heartman <thomas@getunleash.io>
2024-04-25 12:48:35 +0200

    chore: more test updates

--- src/lib/features/project/project-service.test.ts
+++ src/lib/features/project/project-service.test.ts
@@ -1,4 +1,5 @@
 import { createTestConfig } from '../../../test/config/test-config';
+import { BadDataError } from '../../error';
 import { type IBaseEvent, RoleName, TEST_AUDIT_USER } from '../../types';
 import { createFakeProjectService } from './createProjectService';
 
@@ -26,7 +27,7 @@ describe('enterprise extension: enable change requests', () => {
         return service;
     };
 
-    test('it calls the change request enablement function on enterprise', async () => {
+    test('it calls the change request enablement function on enterprise after creating the project', async () => {
         expect.assertions(1);
         const service = createService();
 
@@ -35,6 +36,7 @@ describe('enterprise extension: enable change requests', () => {
             {
                 id: projectId,
                 name: 'fake-project-name',
+                changeRequestEnvironments: [],
             },
             {
                 id: 5,
@@ -77,12 +79,12 @@ describe('enterprise extension: enable change requests', () => {
         expect(fn).not.toHaveBeenCalled();
     });
 
-    test('the emitted event contains no changeRequestEnvironments, if the input had none', async () => {
+    test('the emitted event contains an empty list of changeRequestEnvironments if the input had none', async () => {
         expect.assertions(1);
         const service = createService();
 
         const storeEvent = async (e: IBaseEvent) => {
-            expect('changeRequestEnvironments' in e.data).toBeFalsy();
+            expect(e.data.changeRequestEnvironments).toStrictEqual([]);
         };
 
         // @ts-expect-error: for testing purposes
@@ -138,7 +140,11 @@ describe('enterprise extension: enable change requests', () => {
         expect.assertions(1);
         const service = createService();
 
+        // @ts-expect-error: we want this to pass to test the functionality
+        service.environmentStore.exists = () => true;
+
         const crEnvs = [{ name: 'prod', requiredApprovals: 10 }];
+
         const storeEvent = async (e: IBaseEvent) => {
             expect(e.data.changeRequestEnvironments).toMatchObject(crEnvs);
         };
@@ -172,6 +178,9 @@ describe('enterprise extension: enable change requests', () => {
 
         const crEnvs = [{ name: 'prod', requiredApprovals: 10 }];
 
+        // @ts-expect-error: we want this to pass to test the functionality
+        service.environmentStore.exists = () => true;
+
         const projectId = 'fake-project-id';
         const result = await service.createProject(
             {
@@ -193,6 +202,29 @@ describe('enterprise extension: enable change requests', () => {
         );
 
         expect(result.changeRequestEnvironments).toStrictEqual(crEnvs);
+    });
+
+    test('the create project function defaults to returning an empty list if the input had no cr envs', async () => {
+        const service = createService();
+
+        const projectId = 'fake-project-id';
+        const result = await service.createProject(
+            {
+                id: projectId,
+                name: 'fake-project-name',
+            },
+            {
+                id: 5,
+                permissions: [],
+                isAPI: false,
+            },
+            TEST_AUDIT_USER,
+            async () => {
+                return;
+            },
+        );
+
+        expect(result.changeRequestEnvironments).toStrictEqual([]);
     });
 
     test('the create project function does not return a list of change request envs if we are not on enterprise', async () => {
@@ -223,5 +255,30 @@ describe('enterprise extension: enable change requests', () => {
         );
 
         expect('changeRequestEnvironments' in result).toBeFalsy();
+    });
+
+    test("it throws an error if you provide it with environments that don't exist", async () => {
+        const service = createService();
+        // @ts-expect-error
+        service.environmentStore.exists = () => false;
+
+        const projectId = 'fake-project-id';
+        expect(
+            service.createProject(
+                {
+                    id: projectId,
+                    name: 'fake-project-name',
+                    changeRequestEnvironments: [
+                        { name: 'dev', requiredApprovals: 1 },
+                    ],
+                },
+                {
+                    id: 5,
+                    permissions: [],
+                    isAPI: false,
+                },
+                TEST_AUDIT_USER,
+            ),
+        ).rejects.toThrow(BadDataError);
     });
 });

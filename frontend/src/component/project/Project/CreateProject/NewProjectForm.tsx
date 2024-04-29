74eea8dd8a1431671d00bf3f9c264279e1463933
Thomas Heartman <thomas@getunleash.io>
2024-04-29 13:19:27 +0200

    feat: remove spacing

--- frontend/src/component/project/Project/CreateProject/NewProjectForm.tsx
+++ frontend/src/component/project/Project/CreateProject/NewProjectForm.tsx
@@ -93,7 +93,6 @@ export const NewProjectForm = () => {
             documentationLinkLabel='docs label'
             disablePadding
         >
-            {' '}
             <StyledContainer>
                 <TopGrid>
                     <span className='icon'>icon</span>

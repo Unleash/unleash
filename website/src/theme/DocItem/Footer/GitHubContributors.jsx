4ad56e8afced01c8a0f9eb7c8b8ceb05e9c1291c
Alvin Bryan <107407814+alvinometric@users.noreply.github.com>
2024-04-30 15:19:38 +0100

    Adds contributors to docs (#6900)
    Hello!
    
    We wanted to make the docs less impersonal, so we decided to add
    contributors. Now each doc page that has an `editUrl` (i.e, isn't
    generated) shows a list of everyone that contributed to it.
    
    This list is generated by:
    1. Running `swizzle` on the `DocItem/Footer` in Docusaurus.
    2. Grabbing metadata for the current file using an internal docusaurus
    API (Thank you to @homotechsual for the help there)
    3. Getting the commits to the file in question with the GitHub API
    
    
    ![image](https://github.com/Unleash/unleash/assets/107407814/fd9c92ef-36ab-4d9e-ac11-6d724fd55d11)
    
    <details>
     <summary> Here's the command I ran, for posterity </summary>
    <code>npm run swizzle @docusaurus/theme-classic DocItem/Footer --
    --wrap</code>
    </details>
    
    
    ## Discussion points
    
    1. Design. What do you think of the layout?
    2. Right now I'm hardcoding the info of Unleash team members. This
    creates a small maintenance burden, but it's something we wanted to add.

---
+++ website/src/theme/DocItem/Footer/GitHubContributors.jsx
@@ -0,0 +1,92 @@
+// biome-ignore lint/correctness/noUnusedImports: Needs this for React to work
+import React, { useState, useEffect } from 'react';
+import { getContributors } from './contributors';
+import styles from './contributors.module.scss';
+
+const unleashTeam = new Map([
+    ['alvinometric', 'developer advocate, Unleash'],
+    ['andreas-unleash', 'developer, Unleash'],
+    ['chriswk', 'principal developer, Unleash'],
+    ['daveleek', 'developer, Unleash'],
+    ['FredrikOseberg', 'principal developer, Unleash'],
+    ['gardleopard', 'platform lead, Unleash'],
+    ['gastonfournier', 'senior developer, Unleash'],
+    ['ivarconr', 'co-founder, Unleash'],
+    ['kwasniew', 'senior developer, Unleash'],
+    ['nnennandukwe', 'developer advocate, Unleash'],
+    ['nunogois', 'senior developer, Unleash'],
+    ['sighphyre', 'senior developer, Unleash'],
+    ['sjaanus', 'senior developer, Unleash'],
+    ['thomasheartman', 'developer, Unleash'],
+    ['Tymek', 'developer, Unleash'],
+]);
+
+const GitHubContributors = ({ owner, repo, filePath }) => {
+    const [contributors, setContributors] = useState([]);
+    const url = `https://api.github.com/repos/${owner}/${repo}/commits?path=${filePath}`;
+
+    useEffect(() => {
+        const fetchFileContributors = () => {
+            fetch(url)
+                .then((response) => response.json())
+                .then((commits) => {
+                    const contributors = getContributors(commits);
+
+                    contributors.sort((a, b) => {
+                        if (unleashTeam.has(a.login)) {
+                            return -1;
+                        }
+                        return 1;
+                    });
+                    setContributors(contributors);
+                })
+                .catch((error) => {
+                    console.error(error);
+                    setContributors([]);
+                });
+        };
+
+        fetchFileContributors();
+    }, []);
+
+    if (!contributors.length) {
+        return null;
+    }
+
+    return (
+        <div className={styles.contributors}>
+            <h3>Contributors</h3>
+
+            <ul className={styles.wrapper}>
+                {contributors?.map((contributor) => {
+                    const isUnleashTeam = unleashTeam.has(contributor.login);
+                    const name = isUnleashTeam
+                        ? `${contributor.login}, ${unleashTeam.get(
+                              contributor.login,
+                          )}`
+                        : contributor.login;
+                    return (
+                        <li
+                            key={contributor.login}
+                            className={styles.contributor}
+                        >
+                            <a
+                                href={contributor.html_url}
+                                className={isUnleashTeam ? styles.unleash : ''}
+                                title={`@${name}`}
+                            >
+                                <img
+                                    src={contributor.avatar_url}
+                                    alt={contributor.login}
+                                    width={70}
+                                />
+                            </a>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+};
+
+export default GitHubContributors;

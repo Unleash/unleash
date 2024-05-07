a45be8d10ce1364a5b61307107811f401df6a1e6
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2024-05-07 09:03:40 +0200

    test: move tests from cypress to rtl (#6987)

---
+++ frontend/src/component/project/Project/ProjectFeatureToggles/ProjectFeaturesBatchActions/ProjectFeaturesBatchActions.test.tsx
@@ -0,0 +1,71 @@
+import { render } from 'utils/testRenderer';
+import { screen, waitFor } from '@testing-library/react';
+import { ProjectFeaturesBatchActions } from './ProjectFeaturesBatchActions';
+import { DELETE_FEATURE } from 'component/providers/AccessProvider/permissions';
+import { testServerRoute, testServerSetup } from 'utils/testServer';
+
+const server = testServerSetup();
+
+const setupApi = () => {
+    testServerRoute(server, '/api/admin/projects/default/stale', {}, 'post');
+    testServerRoute(server, '/api/admin/ui-config', {});
+    testServerRoute(
+        server,
+        '/api/admin/projects/default/archive/validate',
+        { hasDeletedDependencies: false, parentsWithChildFeatures: [] },
+        'post',
+    );
+};
+
+test('batch archive', async () => {
+    setupApi();
+    render(
+        <ProjectFeaturesBatchActions
+            projectId='default'
+            onChange={() => {}}
+            onResetSelection={() => {}}
+            selectedIds={['featureA', 'featureB']}
+            data={[]}
+        />,
+        { permissions: [{ permission: DELETE_FEATURE }] },
+    );
+
+    const archiveButton = screen.getByText('Archive');
+    expect(archiveButton).toBeEnabled();
+
+    archiveButton.click();
+
+    screen.getByText('Archive feature toggles');
+    screen.getByText('featureA');
+    screen.getByText('featureB');
+});
+
+test('batch mark as stale', async () => {
+    setupApi();
+    let onChangeCalled = false;
+    render(
+        <ProjectFeaturesBatchActions
+            projectId='default'
+            onChange={() => {
+                onChangeCalled = true;
+            }}
+            onResetSelection={() => {}}
+            selectedIds={['featureA', 'featureB']}
+            data={[
+                { name: 'featureA', stale: false },
+                { name: 'featureB', stale: false },
+            ]}
+        />,
+    );
+
+    const moreActions = screen.getByTitle('More bulk actions');
+    moreActions.click();
+
+    const markAsStale = await screen.findByText('Mark as stale');
+    expect(markAsStale).toBeEnabled();
+    markAsStale.click();
+
+    await waitFor(() => {
+        expect(onChangeCalled).toBe(true);
+    });
+});

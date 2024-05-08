b043bcdd4a2a0d2e41877eee70229ea50ec8c40f
Thomas Heartman <thomas@getunleash.io>
2024-05-08 14:57:07 +0200

    feat: front end can create projects without ids (#7009)
    This PR updates the new project creation form to not include the project
    ID in the payload (because it's generated by the back end now).

--- frontend/src/component/project/Project/CreateProject/CreateProject.tsx
+++ frontend/src/component/project/Project/CreateProject/CreateProject.tsx
@@ -68,14 +68,16 @@ const CreateProject = () => {
         e.preventDefault();
         clearErrors();
         const validName = validateName();
-        const validId = await validateProjectId();
+        const validId = useNewProjectForm || (await validateProjectId());
 
         if (validName && validId) {
-            const payload = getCreateProjectPayload();
+            const payload = getCreateProjectPayload({
+                omitId: useNewProjectForm,
+            });
             try {
-                await createProject(payload);
+                const createdProject = await createProject(payload);
                 refetchUser();
-                navigate(`/projects/${projectId}`, { replace: true });
+                navigate(`/projects/${createdProject.id}`, { replace: true });
                 setToastData({
                     title: 'Project created',
                     text: 'Now you can add toggles to this project',
@@ -99,7 +101,11 @@ const CreateProject = () => {
         return `curl --location --request POST '${uiConfig.unleashUrl}/api/admin/projects' \\
 --header 'Authorization: INSERT_API_KEY' \\
 --header 'Content-Type: application/json' \\
---data-raw '${JSON.stringify(getCreateProjectPayload(), undefined, 2)}'`;
+--data-raw '${JSON.stringify(
+            getCreateProjectPayload({ omitId: useNewProjectForm }),
+            undefined,
+            2,
+        )}'`;
     };
 
     const handleCancel = () => {

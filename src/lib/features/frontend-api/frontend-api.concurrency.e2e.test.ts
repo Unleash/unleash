e8077356ff25a0de26908c9e4e0a6d16055a292b
Christopher Kolstad <chriswk@getunleash.io>
2024-05-21 20:42:03 +0200

    fix: ditch supertest and expose http.Server instance

--- src/lib/features/frontend-api/frontend-api.concurrency.e2e.test.ts
+++ src/lib/features/frontend-api/frontend-api.concurrency.e2e.test.ts
@@ -1,13 +1,13 @@
 import {
-    type IUnleashTest,
-    setupAppWithAuth,
+    setupAppWithoutSupertest,
+    type IUnleashNoSupertest,
 } from '../../../test/e2e/helpers/test-helper';
 import dbInit, { type ITestDb } from '../../../test/e2e/helpers/database-init';
 import getLogger from '../../../test/fixtures/no-logger';
 import { randomId } from '../../util';
 import { ApiTokenType } from '../../types/models/api-token';
 
-let app: IUnleashTest;
+let app: IUnleashNoSupertest;
 let db: ITestDb;
 let appErrorLogs: string[] = [];
 
@@ -21,7 +21,7 @@ beforeAll(async () => {
             baseLogger.error(msg, ...args);
         },
     };
-    app = await setupAppWithAuth(db.stores, {
+    app = await setupAppWithoutSupertest(db.stores, {
         frontendApiOrigins: ['https://example.com'],
         getLogger: () => appLogger,
     });
@@ -37,7 +37,7 @@ afterAll(async () => {
     await db.destroy();
 });
 
-beforeEach(() => {
+beforeEach(async () => {
     appErrorLogs = [];
 });
 
@@ -53,15 +53,19 @@ test('multiple parallel calls to api/frontend should not create multiple instanc
             environment: 'default',
             tokenName: `test-token-${randomId()}`,
         });
-
-    const promises = Array(10).fill(
-        app.request
-            .get('/api/frontend')
-            .set('Authorization', frontendTokenDefault.secret)
-            .expect('Content-Type', /json/)
-            .expect(200),
+    const address = app.server.address();
+    expect(address).not.toBeNull();
+    expect(address).toHaveProperty('port');
+    const serverUrl = `http://localhost:${address.port}/api/frontend`;
+    await Promise.all(
+        Array.from(Array(10).keys()).map(() =>
+            fetch(serverUrl, {
+                method: 'GET',
+                headers: {
+                    Authorization: frontendTokenDefault.secret,
+                },
+            }).then((res) => expect(res.status).toBe(200)),
+        ),
     );
-
-    await Promise.all(promises);
     expect(appErrorLogs).toHaveLength(0);
 });

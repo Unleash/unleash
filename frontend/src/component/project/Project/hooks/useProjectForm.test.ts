ab718ab8ec51677bb6f1b3d1d998804b88e1cfc1
Thomas Heartman <thomas@getunleash.io>
2024-05-22 08:38:23 +0200

    chore: remove mocking test that doesn't work

--- frontend/src/component/project/Project/hooks/useProjectForm.test.ts
+++ frontend/src/component/project/Project/hooks/useProjectForm.test.ts
@@ -1,6 +1,5 @@
 import { renderHook } from '@testing-library/react-hooks';
 import useProjectForm from './useProjectForm';
-import { vi } from 'vitest';
 
 test('setting project environments removes any change request envs that are not in the new project env list', () => {
     const { result } = renderHook(() => useProjectForm());
@@ -38,32 +37,6 @@ test(`adding a change request config for an env not in the project envs doesn't 
 });
 
 describe('payload generation', () => {
-    test(`change request env info is not included unless asked for specifically`, () => {
-        vi.mock(
-            'hooks/api/getters/useUiConfig/useUiConfig',
-            async (importOriginal) => {
-                const actual = await importOriginal();
-                return {
-                    //@ts-ignore
-                    ...actual,
-                    useUiConfig: () => ({
-                        isEnterprise: () => true,
-                    }),
-                };
-            },
-        );
-
-        const { result } = renderHook(() => useProjectForm());
-
-        const payloadWithoutCrs = result.current.getCreateProjectPayload();
-        expect('changeRequestEnvironments' in payloadWithoutCrs).toBeFalsy();
-
-        const payloadWithCrs = result.current.getCreateProjectPayload({
-            includeChangeRequestConfig: true,
-        });
-        expect('changeRequestEnvironments' in payloadWithCrs).toBeTruthy();
-    });
-
     test(`id is omitted only when explicitly asked to be`, () => {
         const { result } = renderHook(() => useProjectForm());
 

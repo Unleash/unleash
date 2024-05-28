62abe144fc61fcac72f080367aab245517aaf23a
kwasniew <kwasniewski.mateusz@gmail.com>
2024-05-28 15:58:54 +0200

    feat: expand admin settings

--- frontend/src/component/layout/MainLayout/NavigationSidebar/NavigationSidebar.tsx
+++ frontend/src/component/layout/MainLayout/NavigationSidebar/NavigationSidebar.tsx
@@ -1,7 +1,7 @@
 import { Box, styled } from '@mui/material';
 import { type FC, useState } from 'react';
 import { useNavigationMode } from './useNavigationMode';
-import { ShowHide } from './ShowHide';
+import { ShowAdmin, ShowHide } from './ShowHide';
 import { useRoutes } from './useRoutes';
 import { useExpanded } from './useExpanded';
 import {
@@ -37,13 +37,18 @@ export const MobileNavigationSidebar: FC<{ onClick: () => void }> = ({
     );
 };
 
-export const StyledBox = styled(Box)(({ theme }) => ({
+export const StretchContainer = styled(Box)(({ theme }) => ({
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(2, 2, 2, 2),
+    zIndex: theme.zIndex.tooltip,
+    alignSelf: 'stretch',
+}));
+
+export const ScreenHeightBox = styled(Box)(({ theme }) => ({
     display: 'flex',
     flexDirection: 'column',
     gap: theme.spacing(3),
-    zIndex: theme.zIndex.tooltip,
+    minHeight: '93vh',
 }));
 
 export const NavigationSidebar = () => {
@@ -59,59 +64,70 @@ export const NavigationSidebar = () => {
     const showRecentProject = mode === 'full' && lastViewed;
 
     return (
-        <StyledBox>
-            <PrimaryNavigationList
-                mode={mode}
-                onClick={setActiveItem}
-                activeItem={activeItem}
-            />
-            <SecondaryNavigation
-                expanded={expanded.includes('configure')}
-                onExpandChange={(expand) => {
-                    changeExpanded('configure', expand);
-                }}
-                mode={mode}
-                title='Configure'
-            >
-                <SecondaryNavigationList
-                    routes={routes.mainNavRoutes}
+        <StretchContainer>
+            <ScreenHeightBox>
+                <PrimaryNavigationList
                     mode={mode}
                     onClick={setActiveItem}
                     activeItem={activeItem}
                 />
-            </SecondaryNavigation>
-            {mode === 'full' && (
                 <SecondaryNavigation
-                    expanded={expanded.includes('admin')}
+                    expanded={expanded.includes('configure')}
                     onExpandChange={(expand) => {
-                        changeExpanded('admin', expand);
+                        changeExpanded('configure', expand);
                     }}
                     mode={mode}
-                    title='Admin'
+                    title='Configure'
                 >
                     <SecondaryNavigationList
-                        routes={routes.adminRoutes}
+                        routes={routes.mainNavRoutes}
                         mode={mode}
                         onClick={setActiveItem}
                         activeItem={activeItem}
                     />
                 </SecondaryNavigation>
-            )}
+                {mode === 'full' && (
+                    <SecondaryNavigation
+                        expanded={expanded.includes('admin')}
+                        onExpandChange={(expand) => {
+                            changeExpanded('admin', expand);
+                        }}
+                        mode={mode}
+                        title='Admin'
+                    >
+                        <SecondaryNavigationList
+                            routes={routes.adminRoutes}
+                            mode={mode}
+                            onClick={setActiveItem}
+                            activeItem={activeItem}
+                        />
+                    </SecondaryNavigation>
+                )}
 
-            {showRecentProject && (
-                <RecentProjectsNavigation
+                {mode === 'mini' && (
+                    <ShowAdmin
+                        onChange={() => {
+                            changeExpanded('admin', true);
+                            setMode('full');
+                        }}
+                    />
+                )}
+
+                {showRecentProject && (
+                    <RecentProjectsNavigation
+                        mode={mode}
+                        projectId={lastViewed}
+                        onClick={() => setActiveItem('/projects')}
+                    />
+                )}
+
+                <ShowHide
                     mode={mode}
-                    projectId={lastViewed}
-                    onClick={() => setActiveItem('/projects')}
+                    onChange={() => {
+                        setMode(mode === 'full' ? 'mini' : 'full');
+                    }}
                 />
-            )}
-
-            <ShowHide
-                mode={mode}
-                onChange={() => {
-                    setMode(mode === 'full' ? 'mini' : 'full');
-                }}
-            />
-        </StyledBox>
+            </ScreenHeightBox>
+        </StretchContainer>
     );
 };

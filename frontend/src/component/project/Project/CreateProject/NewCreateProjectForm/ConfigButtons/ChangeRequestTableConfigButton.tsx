9a1d2102a1e32bb5c52645ec583f72933f8bef6c
Thomas Heartman <thomas@getunleash.io>
2024-05-29 08:28:14 +0200

    fix: add missing scroll container

--- frontend/src/component/project/Project/CreateProject/NewCreateProjectForm/ConfigButtons/ChangeRequestTableConfigButton.tsx
+++ frontend/src/component/project/Project/CreateProject/NewCreateProjectForm/ConfigButtons/ChangeRequestTableConfigButton.tsx
@@ -3,7 +3,10 @@ import { ConfigButton, type ConfigButtonProps } from './ConfigButton';
 import { InputAdornment } from '@mui/material';
 import Search from '@mui/icons-material/Search';
 import { ChangeRequestTable } from './ChangeRequestTable';
-import { TableSearchInput } from './ChangeRequestTableConfigButton.styles';
+import {
+    ScrollContainer,
+    TableSearchInput,
+} from './ChangeRequestTableConfigButton.styles';
 
 type ChangeRequestTableConfigButtonProps = Pick<
     ConfigButtonProps,
@@ -116,11 +119,13 @@ export const ChangeRequestTableConfigButton: FC<
                 }}
                 onKeyDown={toggleTopItem}
             />
-            <ChangeRequestTable
-                environments={filteredEnvs}
-                enableEnvironment={onEnable}
-                disableEnvironment={onDisable}
-            />
+            <ScrollContainer>
+                <ChangeRequestTable
+                    environments={filteredEnvs}
+                    enableEnvironment={onEnable}
+                    disableEnvironment={onDisable}
+                />
+            </ScrollContainer>
         </ConfigButton>
     );
 };

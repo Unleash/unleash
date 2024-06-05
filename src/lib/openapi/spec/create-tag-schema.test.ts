2bad98a121069b3e09c347dbd177c3f3c5a506b8
Thomas Heartman <thomas@getunleash.io>
2024-06-05 08:31:30 +0200

    fix: disallow invalid tag values (#7268)
    This PR fixes how Unleash handles tag values. Specifically, it does
    these things:
    1. Trims leading and trailing whitespace from tag values before
    inserting them into the database
    2. Updates OpenAPI validation to not allow whitespace-only and to ignore
    leading and trailing whitespace
    
    Additionally, it moves the tag length constants into the constants file
    from the Joi tag schema file. This is because importing the values
    previously rendered them as undefined (probably due to a circular
    dependency somewhere in the system). This means that the previous values
    were also ignored by OpenAPI.
    
    UI updates reflecting this wil follow.
    
    ## Background
    When you tag a flag, there's nothing stopping you from using an entirely
    empty tag or a tag with leading/trailing whitespace.
    
    Empty tags make little sense and leading trailing whitespace differences
    are incredibly subtle:
    
    ![image](https://github.com/Unleash/unleash/assets/17786332/ec8fe193-8837-4c6a-b7bf-8766eff34eed)
    
    
    Additionally, leading and trailing whitespace is not shown in the
    dropdown list, so you'd have to guess at which is the right one.
    
    ![image](https://github.com/Unleash/unleash/assets/17786332/a14698ab-2bfd-4ec3-8814-b8e876d0aadb)

---
+++ src/lib/openapi/spec/create-tag-schema.test.ts
@@ -0,0 +1,101 @@
+import { TAG_MAX_LENGTH, TAG_MIN_LENGTH } from '../../util';
+import { validateSchema } from '../validate';
+
+describe('tag value validation', () => {
+    test.each([
+        ['minimum', TAG_MIN_LENGTH],
+        ['maximum', TAG_MAX_LENGTH],
+    ])(`names with the %s length are valid`, (_, length) => {
+        const data = {
+            value: 'a'.repeat(length),
+            type: 'simple',
+        };
+
+        const validationResult = validateSchema(
+            '#/components/schemas/createTagSchema',
+            data,
+        );
+
+        expect(validationResult).toBeUndefined();
+    });
+
+    test(`names can not be only whitespace`, () => {
+        const space = ' '.repeat(TAG_MIN_LENGTH);
+        const data = {
+            value: space,
+            type: 'simple',
+        };
+
+        const validationResult = validateSchema(
+            '#/components/schemas/createTagSchema',
+            data,
+        );
+
+        expect(validationResult).toMatchObject({
+            errors: [{ keyword: 'pattern', instancePath: '/value' }],
+        });
+    });
+
+    test(`names must be at least ${TAG_MIN_LENGTH} characters long, not counting leading and trailing whitespace`, () => {
+        const space = ' '.repeat(TAG_MIN_LENGTH);
+        const data = {
+            value: space + 'a'.repeat(TAG_MIN_LENGTH - 1) + space,
+            type: 'simple',
+        };
+
+        const validationResult = validateSchema(
+            '#/components/schemas/createTagSchema',
+            data,
+        );
+
+        expect(validationResult).toMatchObject({
+            errors: [{ keyword: 'pattern', instancePath: '/value' }],
+        });
+    });
+
+    test(`spaces within a tag value counts towards its maximum length`, () => {
+        const space = ' '.repeat(TAG_MAX_LENGTH);
+        const data = {
+            value: `a${space}z`,
+            type: 'simple',
+        };
+
+        const validationResult = validateSchema(
+            '#/components/schemas/createTagSchema',
+            data,
+        );
+
+        expect(validationResult).toMatchObject({
+            errors: [{ keyword: 'pattern', instancePath: '/value' }],
+        });
+    });
+
+    test(`leading and trailing whitespace does not count towards a name's maximum length`, () => {
+        const space = ' '.repeat(TAG_MAX_LENGTH);
+        const data = {
+            value: space + 'a'.repeat(TAG_MAX_LENGTH) + space,
+            type: 'simple',
+        };
+
+        const validationResult = validateSchema(
+            '#/components/schemas/createTagSchema',
+            data,
+        );
+
+        expect(validationResult).toBeUndefined();
+    });
+
+    test(`tag names can contain spaces`, () => {
+        const data = {
+            value: 'tag name with spaces',
+            type: 'simple',
+        };
+
+        const validationResult = validateSchema(
+            '#/components/schemas/createTagSchema',
+            data,
+        );
+
+        expect(validationResult).toBeUndefined();
+    });
+});

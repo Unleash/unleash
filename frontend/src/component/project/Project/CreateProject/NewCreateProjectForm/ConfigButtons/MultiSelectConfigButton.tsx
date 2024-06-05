c129541df6e874899bc7d3797a5c3c1f18ddc655
Thomas Heartman <thomas@getunleash.io>
2024-06-05 09:20:18 +0200

    feat: adds information about project modes to the project creation form (#7250)
    This change adds information about the project modes to the new
    project creation form, using the tooltip for project creation modes.
    
    In doing so, it updates the config button tooltip to accept extra
    elements and adds styling for them.
    
    What it looks like: 
    
    
    ![image](https://github.com/Unleash/unleash/assets/17786332/809fb48e-2404-416b-a867-6fa04978ccc1)
    
    ## a11y issues
    
    This solution does present one problem: the popover doesn't get focus,
    so it's impossible for you to scroll with only a keyboard. However, this
    is something that's present in Unleash already, and not something that I
    think would be easily solvable, so I don't think this is when we should
    solve it.

--- frontend/src/component/project/Project/CreateProject/NewCreateProjectForm/ConfigButtons/MultiSelectConfigButton.tsx
+++ frontend/src/component/project/Project/CreateProject/NewCreateProjectForm/ConfigButtons/MultiSelectConfigButton.tsx
@@ -4,7 +4,7 @@ import { DropdownList, type DropdownListProps } from './DropdownList';
 
 type MultiSelectConfigButtonProps = Pick<
     ConfigButtonProps,
-    'button' | 'onOpen' | 'onClose' | 'description' | 'tooltipHeader'
+    'button' | 'onOpen' | 'onClose' | 'description' | 'tooltip'
 > &
     Pick<DropdownListProps, 'search' | 'options'> & {
         selectedOptions: Set<string>;

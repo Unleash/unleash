ad747830b0c5f048714dd3664b75790a5011e329
Thomas Heartman <thomas@getunleash.io>
2024-06-05 12:04:22 +0200

    chore: remove superfluous error properties

--- frontend/src/component/feature/CreateFeature/CreateFeature.test.tsx
+++ frontend/src/component/feature/CreateFeature/CreateFeature.test.tsx
@@ -26,10 +26,6 @@ describe('flag name validation', () => {
         const user = userEvent.setup();
         const unfriendlyName = 'unfriendlyName####$#//';
         const error = {
-            id: 'de15e702-e7c7-4083-9f31-384dea4da2e9',
-            name: 'BadDataError',
-            message:
-                'Request validation failed: your request body or params contain invalid data. Refer to the `details` list for more information.',
             details: [
                 {
                     message: `"name" must be URL friendly. You provided "${unfriendlyName}".`,
@@ -60,9 +56,6 @@ describe('flag name validation', () => {
     test('it gives an error if a flag with that name already exists', async () => {
         const user = userEvent.setup();
         const error = {
-            id: '07716b38-076c-4ab0-94c9-1a73663fec94',
-            name: 'NameExistsError',
-            message: 'A flag with that name already exists',
             details: [
                 {
                     message: 'A flag with that name already exists',

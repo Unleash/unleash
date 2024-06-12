74bbf0831d6f777f2a86da4f13d29522e186ba1c
David Leek <david@getunleash.io>
2024-06-12 21:18:42 +0200

    chore: update test with routing chain

--- frontend/src/hooks/useRecentlyVisited.test.tsx
+++ frontend/src/hooks/useRecentlyVisited.test.tsx
@@ -1,28 +1,53 @@
 import type { FC } from 'react';
-import { render } from '@testing-library/react';
-import { screen } from '@testing-library/react';
-import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import {
+    MemoryRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from 'react-router-dom';
 import { useRecentlyVisited } from './useRecentlyVisited';
 
 const RouteNameRender: FC<{}> = () => {
     const { lastVisited } = useRecentlyVisited();
-    return <div>{lastVisited[0]?.pathName}</div>;
+    return (
+        <div>
+            {lastVisited.map((visited) => (
+                <div>{visited.pathName}</div>
+            ))}
+        </div>
+    );
 };
 
 beforeEach(() => {
     window.localStorage.clear();
 });
 
-test('visiting gets added to the list', async () => {
-    const router = createMemoryRouter([
-        { path: '/search', element: <RouteNameRender /> },
-        { path: '/projects', element: <RouteNameRender /> },
-        { path: '/', element: <RouteNameRender /> },
-    ]);
-    render(<RouterProvider router={router} />);
-    router.navigate('/');
-    router.navigate('/search');
+test('checks that routes that exist in routes.ts gets added to lastVisited', async () => {
+    render(
+        <Router initialEntries={['/']}>
+            <RouteNameRender />
+            <Routes>
+                <Route path='/' element={<Navigate to={'/search'} />} />
+                <Route path='/search' element={<Navigate to={'/unknown1'} />} />
+                <Route
+                    path='/unknown1'
+                    element={<Navigate to={'/integrations'} />}
+                />
+                <Route
+                    path='/integrations'
+                    element={<Navigate to={'/unknown2'} />}
+                />
+                <Route
+                    path='/unknown2'
+                    element={<Navigate to={'/segments'} />}
+                />
+                <Route path='/segments' element={<div>segment div</div>} />
+            </Routes>
+        </Router>,
+    );
     await screen.findByText('/search');
-    router.navigate('/projects');
-    await screen.findByText('/projects');
+    await screen.findByText('/integrations');
+    await screen.findByText('/segments');
+    await screen.findByText('segment div');
 });

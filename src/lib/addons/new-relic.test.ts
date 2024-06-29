4e5453998c0b1081c4c3c058633bc44094b79c2d
Alexandru <alexandrusavin@gmail.com>
2024-06-29 19:52:30 +0200

    feat: New Relic integration #878

---
+++ src/lib/addons/new-relic.test.ts
@@ -0,0 +1,157 @@
+import {FEATURE_ARCHIVED, FEATURE_CREATED, FEATURE_ENVIRONMENT_DISABLED, type IEvent,} from '../types/events';
+import type {Logger} from '../logger';
+
+import NewRelicAddon, {INewRelicParameters} from './new-relic';
+
+import noLogger from '../../test/fixtures/no-logger';
+import {gunzip} from "node:zlib";
+import {promisify} from "util";
+
+const asyncGunzip = promisify(gunzip);
+
+let fetchRetryCalls: any[] = [];
+
+jest.mock(
+    './addon',
+    () =>
+        class Addon {
+            logger: Logger;
+
+            constructor(definition, {getLogger}) {
+                this.logger = getLogger('addon/test');
+                fetchRetryCalls = [];
+            }
+
+            async fetchRetry(url, options, retries, backoff) {
+                fetchRetryCalls.push({
+                    url,
+                    options,
+                    retries,
+                    backoff,
+                });
+                return Promise.resolve({status: 200});
+            }
+        },
+);
+
+const defaultParameters = {
+    url: 'fakeUrl',
+    licenseKey: 'fakeLicenseKey',
+} as INewRelicParameters;
+
+const defaultEvent = {
+    id: 1,
+    createdAt: new Date(),
+    type: FEATURE_CREATED,
+    createdBy: 'some@user.com',
+    createdByUserId: -1337,
+    featureName: 'some-toggle',
+    data: {
+        name: 'some-toggle',
+        enabled: false,
+        strategies: [{name: 'default'}],
+    },
+} as IEvent;
+
+const makeAddHandleEvent = (event: IEvent, parameters: INewRelicParameters,) => {
+    const addon = new NewRelicAddon({
+        getLogger: noLogger,
+        unleashUrl: 'http://some-url.com',
+    });
+
+    return () => addon.handleEvent(event, parameters);
+}
+
+
+test.each([
+    {
+        partialEvent: {type: FEATURE_CREATED},
+        test: '$type toggle',
+    },
+    {
+        partialEvent: {
+            type: FEATURE_ARCHIVED,
+            data: {
+                name: 'some-toggle',
+            },
+        },
+        test: 'FEATURE_ARCHIVED toggle with project info'
+    },
+    {
+        partialEvent: {
+            type: FEATURE_ARCHIVED,
+            project: 'some-project',
+            data: {
+                name: 'some-toggle',
+            },
+        },
+        test: 'FEATURE_ARCHIVED with project info'
+    },
+    {
+        partialEvent: {
+            type: FEATURE_ENVIRONMENT_DISABLED,
+            environment: 'development',
+        },
+        test: 'toggled environment'
+    },
+    {
+        partialEvent: {
+            type: FEATURE_ENVIRONMENT_DISABLED,
+            environment: 'development',
+        },
+        partialParameters: {
+            customHeaders: `{ "MY_CUSTOM_HEADER": "MY_CUSTOM_VALUE" }`,
+        },
+        test: 'customHeaders in headers when calling service'
+    },
+    {
+        partialEvent: {
+            type: FEATURE_ENVIRONMENT_DISABLED,
+            environment: 'development',
+        },
+        partialParameters: {
+            bodyTemplate:
+                '{\n  "eventType": "{{event.type}}",\n  "createdBy": "{{event.createdBy}}"\n}',
+        },
+        test: 'custom body template'
+    }
+] as Array<{
+    partialEvent: Partial<IEvent>,
+    partialParameters?: Partial<INewRelicParameters>,
+    test: String
+}>)('Should call New Relic Event API for $test', async ({partialEvent, partialParameters}) => {
+    const event = {
+        ...defaultEvent,
+        ...partialEvent,
+    }
+
+    const parameters = {
+        ...defaultParameters,
+        ...partialParameters,
+    }
+
+    const handleEvent = makeAddHandleEvent(event, parameters);
+
+    await handleEvent();
+    expect(fetchRetryCalls.length).toBe(1);
+
+    const {url, options} = fetchRetryCalls[0];
+    const jsonBody = JSON.parse((await asyncGunzip(options.body)).toString());
+
+    expect(url).toBe(parameters.url);
+    expect(options.method).toBe('POST');
+    expect(options.headers['Api-Key']).toBe(parameters.licenseKey);
+    expect(options.headers['Content-Type']).toBe('application/json');
+    expect(options.headers['Content-Encoding']).toBe('gzip');
+    expect(options.headers).toMatchSnapshot();
+
+    expect(jsonBody.eventType).toBe('UnleashServiceEvent');
+    expect(jsonBody.unleashEventType).toBe(event.type);
+    expect(jsonBody.featureName).toBe(event.data.name);
+    expect(jsonBody.environment).toBe(event.environment);
+    expect(jsonBody.createdBy).toBe(event.createdBy);
+    expect(jsonBody.createdByUserId).toBe(event.createdByUserId);
+    expect(jsonBody.createdAt).toBe(event.createdAt.getTime());
+
+    expect(options.body).toMatchSnapshot();
+});
---
+++ src/lib/addons/new-relic.ts
@@ -0,0 +1,100 @@
+import Addon from './addon';
+
+import definition from './new-relic-definition';
+import Mustache from 'mustache';
+import type {IAddonConfig} from '../types/model';
+import {type FeatureEventFormatter, FeatureEventFormatterMd, LinkStyle,} from './feature-event-formatter-md';
+import {IEvent, IEventType} from '../types/events';
+import {gzip} from "node:zlib";
+import {promisify} from 'util';
+
+const asyncGzip = promisify(gzip);
+
+export interface INewRelicParameters {
+    url: string;
+    licenseKey: string;
+    customHeaders?: string;
+    bodyTemplate?: string;
+}
+
+interface INewRelicRequestBody {
+    eventType: 'Unleash Service Event';
+    unleashEventType: IEventType;
+    featureName: IEvent['featureName'];
+    environment: IEvent['environment'];
+    createdBy: IEvent['createdBy'];
+    createdByUserId: IEvent['createdByUserId'];
+    createdAt: IEvent['createdAt'];
+}
+
+export default class NewRelicAddon extends Addon {
+    private msgFormatter: FeatureEventFormatter;
+
+    constructor(config: IAddonConfig) {
+        super(definition, config);
+        this.msgFormatter = new FeatureEventFormatterMd(
+            config.unleashUrl,
+            LinkStyle.MD,
+        );
+    }
+
+    async handleEvent(
+        event: IEvent,
+        parameters: INewRelicParameters,
+    ): Promise<void> {
+        const {
+            url,
+            licenseKey,
+            customHeaders,
+            bodyTemplate,
+        } = parameters;
+        const context = {
+            event,
+        };
+
+        let text: string;
+        if (typeof bodyTemplate === 'string' && bodyTemplate.length > 1) {
+            text = Mustache.render(bodyTemplate, context);
+        } else {
+            text = `%%% \n ${this.msgFormatter.format(event).text} \n %%% `;
+        }
+
+        const body: INewRelicRequestBody = {
+            eventType: 'UnleashServiceEvent',
+            unleashEventType: event.type,
+            featureName: event.featureName,
+            environment: event.environment,
+            createdBy: event.createdBy,
+            createdByUserId: event.createdByUserId,
+            createdAt: event.createdAt.getTime(),
+            ...event.data,
+        };
+
+        let extraHeaders = {};
+        if (typeof customHeaders === 'string' && customHeaders.length > 1) {
+            try {
+                extraHeaders = JSON.parse(customHeaders);
+            } catch (e) {
+                this.logger.warn(
+                    `Could not parse the json in the customHeaders parameter. [${customHeaders}]`,
+                );
+            }
+        }
+
+        const requestOpts = {
+            method: 'POST',
+            headers: {
+                'Api-Key': licenseKey,
+                'Content-Type': 'application/json',
+                'Content-Encoding': 'gzip',
+                ...extraHeaders,
+            },
+            body: await asyncGzip(JSON.stringify(body)),
+        };
+
+        const res = await this.fetchRetry(url, requestOpts);
+        this.logger.info(
+            `Handled event ${event.type}. Status codes=${res.status}`,
+        );
+    }
+}

0a947e59510c38984cc1eb99e00fb81653663103
Thomas Heartman <thomas@getunleash.io>
2024-07-02 14:36:46 +0200

    feat: fix build issue by spreading in existing limits

--- src/lib/services/api-token-service.limit.test.ts
+++ src/lib/services/api-token-service.limit.test.ts
@@ -33,6 +33,7 @@ const createServiceWithLimit = (limit: number) => {
         {
             ...config,
             resourceLimits: {
+                ...config.resourceLimits,
                 apiTokens: limit,
             },
         },

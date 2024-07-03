711f4954c10e4437d34bc04d12934f494854887b
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2024-07-03 18:19:57 +0200

    feat: update description on project deletion

--- frontend/src/component/project/Project/ProjectSettings/Settings/DeleteProject.tsx
+++ frontend/src/component/project/Project/ProjectSettings/Settings/DeleteProject.tsx
@@ -36,41 +36,51 @@ export const DeleteProject = ({
     const automatedActionsEnabled = useUiFlag('automatedActions');
     const { actions } = useActions(projectId);
     const [showDelDialog, setShowDelDialog] = useState(false);
+    const actionsCount = actions.filter(({ enabled }) => enabled).length;
     const navigate = useNavigate();
     return (
         <StyledContainer>
-            <p>
-                Before you can delete a project, you must first archive all the
-                feature flags associated with it. Keep in mind that deleting a
-                project will permanently remove all the archived feature flags,
-                and they cannot be recovered once deleted.
-            </p>
             <ConditionallyRender
-                condition={isEnterprise() && automatedActionsEnabled}
+                condition={featureCount > 0 || actionsCount > 0}
                 show={
-                    <p>
-                        Additionally, all configured actions for this project
-                        will no longer be executed as they will be permanently
-                        deleted.
-                    </p>
+                    <>
+                        <p>
+                            Before you can delete a project, you must first
+                            archive all the feature flags associated with it.
+                        </p>
+                        <p>
+                            Currently there are{' '}
+                            <strong>{featureCount} feature flags active</strong>
+                        </p>
+                        <ConditionallyRender
+                            condition={
+                                isEnterprise() && automatedActionsEnabled
+                            }
+                            show={
+                                <p>
+                                    Currently there are{' '}
+                                    <strong>
+                                        {actionsCount} enabled actions
+                                    </strong>
+                                </p>
+                            }
+                        />
+                    </>
                 }
             />
             <p>
-                Currently there are{' '}
-                <strong>{featureCount} feature flags active</strong>
+                Keep in mind that deleting a project{' '}
+                <strong>will permanently remove</strong>
+                <ul>
+                    <li>all archived feature flags in this project</li>
+                    <li>API keys configured to access only this project</li>
+                    <ConditionallyRender
+                        condition={isEnterprise() && automatedActionsEnabled}
+                        show={<li>all actions configured for this project</li>}
+                    />
+                </ul>
+                and they <strong>cannot be recovered</strong> once deleted.
             </p>
-            <ConditionallyRender
-                condition={isEnterprise() && automatedActionsEnabled}
-                show={
-                    <p>
-                        Currently there are{' '}
-                        <strong>
-                            {actions.filter(({ enabled }) => enabled).length}{' '}
-                            enabled actions
-                        </strong>
-                    </p>
-                }
-            />
             <StyledButtonContainer>
                 <PermissionButton
                     permission={DELETE_PROJECT}

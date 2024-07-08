9a971fb789fbcc91c20c480fd38f50c06a5a6a95
kwasniew <kwasniewski.mateusz@gmail.com>
2024-07-08 12:10:18 +0200

    feat: constraints limit in a strategy UI

---
+++ frontend/src/component/feature/FeatureStrategy/FeatureStrategyConstraints/FeatureStrategyConstraintAccordionList/FeatureStrategyConstraintAccordionList.test.tsx
@@ -0,0 +1,61 @@
+import { screen } from '@testing-library/react';
+import { render } from 'utils/testRenderer';
+import { testServerRoute, testServerSetup } from 'utils/testServer';
+import { FeatureStrategyConstraintAccordionList } from './FeatureStrategyConstraintAccordionList';
+import type { IConstraint } from 'interfaces/strategy';
+
+const server = testServerSetup();
+
+const LIMIT = 5;
+
+const setupApi = () => {
+    testServerRoute(server, '/api/admin/ui-config', {
+        flags: {
+            resourceLimits: true,
+        },
+        resourceLimits: {
+            constraints: LIMIT,
+        },
+    });
+    testServerRoute(server, '/api/admin/context', [{ name: 'text' }]);
+};
+
+const constraints = (limit: number): IConstraint[] =>
+    Array.from(Array(limit).keys()).map(() => ({
+        contextName: 'test',
+        operator: 'IN',
+    }));
+
+test('show limit reached and disable adding new constraints', async () => {
+    setupApi();
+    render(
+        <FeatureStrategyConstraintAccordionList
+            constraints={constraints(LIMIT)}
+            showCreateButton={true}
+            setConstraints={() => {}}
+        />,
+    );
+
+    await screen.findByText(
+        'You have reached the limit for constraints in this strategy',
+    );
+    const button = await screen.findByText('Add constraint');
+    expect(button).toBeDisabled();
+});
+
+test('show nearing limit', async () => {
+    setupApi();
+    render(
+        <FeatureStrategyConstraintAccordionList
+            constraints={constraints(LIMIT - 1)}
+            showCreateButton={true}
+            setConstraints={() => {}}
+        />,
+    );
+
+    await screen.findByText(
+        'You are nearing the limit for constraints in this strategy',
+    );
+    const button = await screen.findByText('Add constraint');
+    expect(button).toBeEnabled();
+});

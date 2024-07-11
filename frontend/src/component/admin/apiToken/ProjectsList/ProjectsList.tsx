d440d3230acaedf677c900423709fcbb0263114b
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2024-07-11 14:06:22 +0200

    feat: show orphaned API tokens (#7569)
    Add a visual indication that a token was scoped to projects that have
    been deleted.

--- frontend/src/component/admin/apiToken/ProjectsList/ProjectsList.tsx
+++ frontend/src/component/admin/apiToken/ProjectsList/ProjectsList.tsx
@@ -1,11 +1,13 @@
+import { Fragment, type FC } from 'react';
 import { styled } from '@mui/material';
 import { Highlighter } from 'component/common/Highlighter/Highlighter';
 import { HtmlTooltip } from 'component/common/HtmlTooltip/HtmlTooltip';
 import { LinkCell } from 'component/common/Table/cells/LinkCell/LinkCell';
 import { TextCell } from 'component/common/Table/cells/TextCell/TextCell';
 import { useSearchHighlightContext } from 'component/common/Table/SearchHighlightContext/SearchHighlightContext';
-import { Fragment, type FC } from 'react';
+import ErrorIcon from '@mui/icons-material/Error';
 import { Link } from 'react-router-dom';
+import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
 
 const StyledLink = styled(Link)(({ theme }) => ({
     textDecoration: 'none',
@@ -15,12 +17,28 @@ const StyledLink = styled(Link)(({ theme }) => ({
     },
 }));
 
+const StyledErrorIcon = styled(ErrorIcon)(({ theme }) => ({
+    color: theme.palette.error.main,
+    marginBottom: theme.spacing(0.5),
+    marginLeft: theme.spacing(0.5),
+}));
+
+const StyledContainer = styled('div')({
+    display: 'flex',
+    alignItems: 'center',
+});
+
 interface IProjectsListProps {
     project?: string;
     projects?: string | string[];
+    secret?: string;
 }
 
-export const ProjectsList: FC<IProjectsListProps> = ({ projects, project }) => {
+export const ProjectsList: FC<IProjectsListProps> = ({
+    projects,
+    project,
+    secret,
+}) => {
     const { searchQuery } = useSearchHighlightContext();
 
     const projectsList =
@@ -67,16 +85,36 @@ export const ProjectsList: FC<IProjectsListProps> = ({ projects, project }) => {
         return <LinkCell to={`/projects/${item}`} title={item} />;
     }
 
+    const tokenFormat = secret?.includes(':') ? 'v2' : 'v1'; // see https://docs.getunleash.io/reference/api-tokens-and-client-keys#format
+    const isWildcardToken = secret?.startsWith('*:');
+    const isOrphanedToken = tokenFormat === 'v2' && !isWildcardToken;
+
     return (
         <TextCell>
             <HtmlTooltip
-                title='ALL current and future projects'
+                title={
+                    isOrphanedToken ? (
+                        <>
+                            This is an orphaned token. All of its original
+                            projects have been deleted and it now has access to
+                            all current and future projects. You should stop
+                            using this token and delete it. It will lose access
+                            to all projects at a later date.
+                        </>
+                    ) : (
+                        'ALL current and future projects.'
+                    )
+                }
                 placement='bottom'
                 arrow
             >
-                <span>
+                <StyledContainer>
                     <Highlighter search={searchQuery}>*</Highlighter>
-                </span>
+                    <ConditionallyRender
+                        condition={isOrphanedToken}
+                        show={<StyledErrorIcon aria-label='Orphaned token' />}
+                    />
+                </StyledContainer>
             </HtmlTooltip>
         </TextCell>
     );

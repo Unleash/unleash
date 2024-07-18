b6200076384e7276eac787e713cb5eb53f5ca647
Thomas Heartman <thomas@getunleash.io>
2024-07-18 11:12:58 +0200

    chore: make event bus emission optional

--- src/lib/error/exceeds-limit-error.ts
+++ src/lib/error/exceeds-limit-error.ts
@@ -3,13 +3,13 @@ import { GenericUnleashError } from './unleash-error';
 import { EXCEEDS_LIMIT } from '../metric-events';
 
 export class ExceedsLimitError extends GenericUnleashError {
-    constructor(resource: string, limit: number, eventBus: EventEmitter) {
+    constructor(resource: string, limit: number, eventBus?: EventEmitter) {
         super({
             name: 'ExceedsLimitError',
             message: `Failed to create ${resource}. You can't create more than the established limit of ${limit}.`,
             statusCode: 400,
         });
 
-        eventBus.emit(EXCEEDS_LIMIT, { resource, limit });
+        eventBus?.emit(EXCEEDS_LIMIT, { resource, limit });
     }
 }

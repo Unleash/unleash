b2787440fe18987f34d5347220d1919ab5a023e2
Nuno Góis <github@nunogois.com>
2024-07-22 15:00:57 +0100

    chore: integration events hook

---
+++ frontend/src/hooks/api/getters/useIntegrationEvents/useIntegrationEvents.ts
@@ -0,0 +1,74 @@
+import { useContext } from 'react';
+import useSWRInfinite, {
+    type SWRInfiniteConfiguration,
+    type SWRInfiniteKeyLoader,
+} from 'swr/infinite';
+import { formatApiPath } from 'utils/formatPath';
+import type { IntegrationEventsSchema } from '@server/openapi/spec';
+import { useUiFlag } from 'hooks/useUiFlag';
+import AccessContext from 'contexts/AccessContext';
+import handleErrorResponses from '../httpErrorResponseHandler';
+
+const fetcher = async (url: string) => {
+    const response = await fetch(url);
+    await handleErrorResponses('Integration events')(response);
+    return response.json();
+};
+
+export const useIntegrationEvents = (
+    integrationId?: number,
+    limit = 50,
+    options: SWRInfiniteConfiguration = {},
+) => {
+    const { isAdmin } = useContext(AccessContext);
+    const integrationEventsEnabled = useUiFlag('integrationEvents');
+
+    const getKey: SWRInfiniteKeyLoader = (
+        pageIndex: number,
+        previousPageData: IntegrationEventsSchema,
+    ) => {
+        // Does not meet conditions
+        if (!integrationId || !isAdmin || !integrationEventsEnabled)
+            return null;
+
+        // Reached the end
+        if (previousPageData && !previousPageData.integrationEvents.length)
+            return null;
+
+        return formatApiPath(
+            `api/admin/addons/${integrationId}/events?limit=${limit}&offset=${
+                pageIndex * limit
+            }`,
+        );
+    };
+
+    const { data, error, size, setSize, mutate } =
+        useSWRInfinite<IntegrationEventsSchema>(getKey, fetcher, {
+            ...options,
+            revalidateAll: true,
+        });
+
+    const integrationEvents = data
+        ? data.flatMap(({ integrationEvents }) => integrationEvents)
+        : [];
+
+    const isLoadingInitialData = !data && !error;
+    const isLoadingMore = size > 0 && !data?.[size - 1];
+    const loading = isLoadingInitialData || isLoadingMore;
+
+    const hasMore = data?.[size - 1]?.integrationEvents.length === limit;
+
+    const loadMore = () => {
+        if (loading || !hasMore) return;
+        setSize(size + 1);
+    };
+
+    return {
+        integrationEvents,
+        hasMore,
+        loadMore,
+        loading,
+        refetch: () => mutate(),
+        error,
+    };
+};

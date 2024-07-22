47ff73afb204f811dcb56bb805bf12c0cf58578c
Nuno Góis <github@nunogois.com>
2024-07-22 12:14:32 +0100

    chore: register integration events in Datadog integration (#7635)
    https://linear.app/unleash/issue/2-2461/register-integration-events-datadog
    
    Registers integration events in the **Datadog** integration.
    
    Similar to:
     - #7634 
     - #7631
     - #7626
     - #7621

--- src/lib/addons/datadog.test.ts
+++ src/lib/addons/datadog.test.ts
@@ -9,10 +9,15 @@ import type { Logger } from '../logger';
 import DatadogAddon from './datadog';
 
 import noLogger from '../../test/fixtures/no-logger';
-import type { IAddonConfig, IFlagResolver } from '../types';
+import {
+    serializeDates,
+    type IAddonConfig,
+    type IFlagResolver,
+} from '../types';
 import type { IntegrationEventsService } from '../services';
 
 let fetchRetryCalls: any[] = [];
+const registerEventMock = jest.fn();
 
 const INTEGRATION_ID = 1337;
 const ARGS: IAddonConfig = {
@@ -40,207 +45,266 @@ jest.mock(
                     retries,
                     backoff,
                 });
-                return Promise.resolve({ status: 200 });
+                return Promise.resolve({ ok: true, status: 200 });
             }
 
-            async registerEvent(_) {
-                return Promise.resolve();
+            async registerEvent(event) {
+                return registerEventMock(event);
             }
         },
 );
 
-test('Should call datadog webhook', async () => {
-    const addon = new DatadogAddon(ARGS);
-    const event: IEvent = {
-        id: 1,
-        createdAt: new Date(),
-        type: FEATURE_CREATED,
-        createdBy: 'some@user.com',
-        createdByUserId: -1337,
-        featureName: 'some-toggle',
-        data: {
-            name: 'some-toggle',
-            enabled: false,
-            strategies: [{ name: 'default' }],
-        },
-    };
+describe('Datadog integration', () => {
+    beforeEach(() => {
+        registerEventMock.mockClear();
+    });
 
-    const parameters = {
-        url: 'http://api.datadoghq.com/api/v1/events',
-        apiKey: 'fakeKey',
-    };
+    test('Should call datadog webhook', async () => {
+        const addon = new DatadogAddon(ARGS);
+        const event: IEvent = {
+            id: 1,
+            createdAt: new Date(),
+            type: FEATURE_CREATED,
+            createdBy: 'some@user.com',
+            createdByUserId: -1337,
+            featureName: 'some-toggle',
+            data: {
+                name: 'some-toggle',
+                enabled: false,
+                strategies: [{ name: 'default' }],
+            },
+        };
 
-    await addon.handleEvent(event, parameters, INTEGRATION_ID);
-    expect(fetchRetryCalls.length).toBe(1);
-    expect(fetchRetryCalls[0].url).toBe(parameters.url);
-    expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
-});
+        const parameters = {
+            url: 'http://api.datadoghq.com/api/v1/events',
+            apiKey: 'fakeKey',
+        };
 
-test('Should call datadog webhook  for archived toggle', async () => {
-    const addon = new DatadogAddon(ARGS);
-    const event: IEvent = {
-        id: 2,
-        createdAt: new Date(),
-        type: FEATURE_ARCHIVED,
-        createdBy: 'some@user.com',
-        createdByUserId: -1337,
-        featureName: 'some-toggle',
-        data: {
-            name: 'some-toggle',
-        },
-    };
+        await addon.handleEvent(event, parameters, INTEGRATION_ID);
+        expect(fetchRetryCalls.length).toBe(1);
+        expect(fetchRetryCalls[0].url).toBe(parameters.url);
+        expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
+    });
 
-    const parameters = {
-        url: 'http://api.datadoghq.com/api/v1/events',
-        apiKey: 'fakeKey',
-    };
+    test('Should call datadog webhook  for archived toggle', async () => {
+        const addon = new DatadogAddon(ARGS);
+        const event: IEvent = {
+            id: 2,
+            createdAt: new Date(),
+            type: FEATURE_ARCHIVED,
+            createdBy: 'some@user.com',
+            createdByUserId: -1337,
+            featureName: 'some-toggle',
+            data: {
+                name: 'some-toggle',
+            },
+        };
 
-    await addon.handleEvent(event, parameters, INTEGRATION_ID);
-    expect(fetchRetryCalls.length).toBe(1);
-    expect(fetchRetryCalls[0].url).toBe(parameters.url);
-    expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
-});
+        const parameters = {
+            url: 'http://api.datadoghq.com/api/v1/events',
+            apiKey: 'fakeKey',
+        };
 
-test('Should call datadog webhook  for archived toggle with project info', async () => {
-    const addon = new DatadogAddon(ARGS);
-    const event: IEvent = {
-        id: 2,
-        createdAt: new Date(),
-        type: FEATURE_ARCHIVED,
-        createdBy: 'some@user.com',
-        featureName: 'some-toggle',
-        createdByUserId: -1337,
-        project: 'some-project',
-        data: {
-            name: 'some-toggle',
-        },
-    };
+        await addon.handleEvent(event, parameters, INTEGRATION_ID);
+        expect(fetchRetryCalls.length).toBe(1);
+        expect(fetchRetryCalls[0].url).toBe(parameters.url);
+        expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
+    });
 
-    const parameters = {
-        url: 'http://api.datadoghq.com/api/v1/events',
-        apiKey: 'fakeKey',
-    };
+    test('Should call datadog webhook  for archived toggle with project info', async () => {
+        const addon = new DatadogAddon(ARGS);
+        const event: IEvent = {
+            id: 2,
+            createdAt: new Date(),
+            type: FEATURE_ARCHIVED,
+            createdBy: 'some@user.com',
+            featureName: 'some-toggle',
+            createdByUserId: -1337,
+            project: 'some-project',
+            data: {
+                name: 'some-toggle',
+            },
+        };
 
-    await addon.handleEvent(event, parameters, INTEGRATION_ID);
-    expect(fetchRetryCalls.length).toBe(1);
-    expect(fetchRetryCalls[0].url).toBe(parameters.url);
-    expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
-});
+        const parameters = {
+            url: 'http://api.datadoghq.com/api/v1/events',
+            apiKey: 'fakeKey',
+        };
 
-test('Should call datadog webhook for toggled environment', async () => {
-    const addon = new DatadogAddon(ARGS);
-    const event: IEvent = {
-        id: 2,
-        createdAt: new Date(),
-        type: FEATURE_ENVIRONMENT_DISABLED,
-        createdBy: 'some@user.com',
-        createdByUserId: -1337,
-        environment: 'development',
-        project: 'default',
-        featureName: 'some-toggle',
-        data: {
-            name: 'some-toggle',
-        },
-    };
+        await addon.handleEvent(event, parameters, INTEGRATION_ID);
+        expect(fetchRetryCalls.length).toBe(1);
+        expect(fetchRetryCalls[0].url).toBe(parameters.url);
+        expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
+    });
 
-    const parameters = {
-        url: 'http://hooks.slack.com',
-        apiKey: 'fakeKey',
-    };
+    test('Should call datadog webhook for toggled environment', async () => {
+        const addon = new DatadogAddon(ARGS);
+        const event: IEvent = {
+            id: 2,
+            createdAt: new Date(),
+            type: FEATURE_ENVIRONMENT_DISABLED,
+            createdBy: 'some@user.com',
+            createdByUserId: -1337,
+            environment: 'development',
+            project: 'default',
+            featureName: 'some-toggle',
+            data: {
+                name: 'some-toggle',
+            },
+        };
 
-    await addon.handleEvent(event, parameters, INTEGRATION_ID);
-    expect(fetchRetryCalls).toHaveLength(1);
-    expect(fetchRetryCalls[0].url).toBe(parameters.url);
-    expect(fetchRetryCalls[0].options.body).toMatch(/disabled/);
-    expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
-});
+        const parameters = {
+            url: 'http://hooks.slack.com',
+            apiKey: 'fakeKey',
+        };
 
-test('Should include customHeaders in headers when calling service', async () => {
-    const addon = new DatadogAddon(ARGS);
-    const event: IEvent = {
-        id: 2,
-        createdAt: new Date(),
-        type: FEATURE_ENVIRONMENT_DISABLED,
-        createdBy: 'some@user.com',
-        environment: 'development',
-        createdByUserId: -1337,
-        project: 'default',
-        featureName: 'some-toggle',
-        data: {
-            name: 'some-toggle',
-        },
-    };
+        await addon.handleEvent(event, parameters, INTEGRATION_ID);
+        expect(fetchRetryCalls).toHaveLength(1);
+        expect(fetchRetryCalls[0].url).toBe(parameters.url);
+        expect(fetchRetryCalls[0].options.body).toMatch(/disabled/);
+        expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
+    });
 
-    const parameters = {
-        url: 'http://hooks.slack.com',
-        apiKey: 'fakeKey',
-        customHeaders: `{ "MY_CUSTOM_HEADER": "MY_CUSTOM_VALUE" }`,
-    };
-    await addon.handleEvent(event, parameters, INTEGRATION_ID);
-    expect(fetchRetryCalls).toHaveLength(1);
-    expect(fetchRetryCalls[0].url).toBe(parameters.url);
-    expect(fetchRetryCalls[0].options.body).toMatch(/disabled/);
-    expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
-    expect(fetchRetryCalls[0].options.headers).toMatchSnapshot();
-});
+    test('Should include customHeaders in headers when calling service', async () => {
+        const addon = new DatadogAddon(ARGS);
+        const event: IEvent = {
+            id: 2,
+            createdAt: new Date(),
+            type: FEATURE_ENVIRONMENT_DISABLED,
+            createdBy: 'some@user.com',
+            environment: 'development',
+            createdByUserId: -1337,
+            project: 'default',
+            featureName: 'some-toggle',
+            data: {
+                name: 'some-toggle',
+            },
+        };
 
-test('Should not include source_type_name when included in the config', async () => {
-    const addon = new DatadogAddon(ARGS);
-    const event: IEvent = {
-        id: 2,
-        createdAt: new Date(),
-        type: FEATURE_ENVIRONMENT_DISABLED,
-        createdBy: 'some@user.com',
-        createdByUserId: -1337,
-        environment: 'development',
-        project: 'default',
-        featureName: 'some-toggle',
-        data: {
-            name: 'some-toggle',
-        },
-    };
+        const parameters = {
+            url: 'http://hooks.slack.com',
+            apiKey: 'fakeKey',
+            customHeaders: `{ "MY_CUSTOM_HEADER": "MY_CUSTOM_VALUE" }`,
+        };
+        await addon.handleEvent(event, parameters, INTEGRATION_ID);
+        expect(fetchRetryCalls).toHaveLength(1);
+        expect(fetchRetryCalls[0].url).toBe(parameters.url);
+        expect(fetchRetryCalls[0].options.body).toMatch(/disabled/);
+        expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
+        expect(fetchRetryCalls[0].options.headers).toMatchSnapshot();
+    });
 
-    const parameters = {
-        url: 'http://hooks.slack.com',
-        apiKey: 'fakeKey',
-        sourceTypeName: 'my-custom-source-type',
-    };
+    test('Should not include source_type_name when included in the config', async () => {
+        const addon = new DatadogAddon(ARGS);
+        const event: IEvent = {
+            id: 2,
+            createdAt: new Date(),
+            type: FEATURE_ENVIRONMENT_DISABLED,
+            createdBy: 'some@user.com',
+            createdByUserId: -1337,
+            environment: 'development',
+            project: 'default',
+            featureName: 'some-toggle',
+            data: {
+                name: 'some-toggle',
+            },
+        };
 
-    await addon.handleEvent(event, parameters, INTEGRATION_ID);
-    expect(fetchRetryCalls).toHaveLength(1);
-    expect(fetchRetryCalls[0].url).toBe(parameters.url);
-    expect(fetchRetryCalls[0].options.body).toMatch(
-        /"source_type_name":"my-custom-source-type"/,
-    );
-    expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
-    expect(fetchRetryCalls[0].options.headers).toMatchSnapshot();
-});
+        const parameters = {
+            url: 'http://hooks.slack.com',
+            apiKey: 'fakeKey',
+            sourceTypeName: 'my-custom-source-type',
+        };
 
-test('Should call datadog webhook with JSON when template set', async () => {
-    const addon = new DatadogAddon(ARGS);
-    const event: IEvent = {
-        id: 1,
-        createdAt: new Date(),
-        type: FEATURE_CREATED,
-        createdBy: 'some@user.com',
-        createdByUserId: -1337,
-        featureName: 'some-toggle',
-        data: {
-            name: 'some-toggle',
-            enabled: false,
-            strategies: [{ name: 'default' }],
-        },
-    };
+        await addon.handleEvent(event, parameters, INTEGRATION_ID);
+        expect(fetchRetryCalls).toHaveLength(1);
+        expect(fetchRetryCalls[0].url).toBe(parameters.url);
+        expect(fetchRetryCalls[0].options.body).toMatch(
+            /"source_type_name":"my-custom-source-type"/,
+        );
+        expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
+        expect(fetchRetryCalls[0].options.headers).toMatchSnapshot();
+    });
 
-    const parameters = {
-        url: 'http://api.datadoghq.com/api/v1/events',
-        apiKey: 'fakeKey',
-        bodyTemplate:
-            '{\n  "event": "{{event.type}}",\n  "createdBy": "{{event.createdBy}}"\n}',
-    };
+    test('Should call datadog webhook with JSON when template set', async () => {
+        const addon = new DatadogAddon(ARGS);
+        const event: IEvent = {
+            id: 1,
+            createdAt: new Date(),
+            type: FEATURE_CREATED,
+            createdBy: 'some@user.com',
+            createdByUserId: -1337,
+            featureName: 'some-toggle',
+            data: {
+                name: 'some-toggle',
+                enabled: false,
+                strategies: [{ name: 'default' }],
+            },
+        };
 
-    await addon.handleEvent(event, parameters, INTEGRATION_ID);
-    expect(fetchRetryCalls.length).toBe(1);
-    expect(fetchRetryCalls[0].url).toBe(parameters.url);
-    expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
+        const parameters = {
+            url: 'http://api.datadoghq.com/api/v1/events',
+            apiKey: 'fakeKey',
+            bodyTemplate:
+                '{\n  "event": "{{event.type}}",\n  "createdBy": "{{event.createdBy}}"\n}',
+        };
+
+        await addon.handleEvent(event, parameters, INTEGRATION_ID);
+        expect(fetchRetryCalls.length).toBe(1);
+        expect(fetchRetryCalls[0].url).toBe(parameters.url);
+        expect(fetchRetryCalls[0].options.body).toMatchSnapshot();
+    });
+
+    test('Should call registerEvent', async () => {
+        const addon = new DatadogAddon(ARGS);
+        const event: IEvent = {
+            id: 1,
+            createdAt: new Date(),
+            type: FEATURE_CREATED,
+            createdBy: 'some@user.com',
+            createdByUserId: -1337,
+            featureName: 'some-toggle',
+            data: {
+                name: 'some-toggle',
+                enabled: false,
+                strategies: [{ name: 'default' }],
+            },
+            tags: [
+                {
+                    type: 'test',
+                    value: '1',
+                },
+                {
+                    type: 'test',
+                    value: '2',
+                },
+            ],
+        };
+
+        const parameters = {
+            url: 'http://api.datadoghq.com/api/v1/events',
+            apiKey: 'fakeKey',
+            bodyTemplate:
+                '{\n  "event": "{{event.type}}",\n  "createdBy": "{{event.createdBy}}"\n}',
+        };
+
+        await addon.handleEvent(event, parameters, INTEGRATION_ID);
+
+        expect(registerEventMock).toHaveBeenCalledTimes(1);
+        expect(registerEventMock).toHaveBeenCalledWith({
+            integrationId: INTEGRATION_ID,
+            state: 'success',
+            stateDetails:
+                'Datadog Events API request was successful with status code: 200.',
+            event: serializeDates(event),
+            details: {
+                url: parameters.url,
+                body: {
+                    text: `{\n  "event": "${event.type}",\n  "createdBy": "${event.createdBy}"\n}`,
+                    title: 'Unleash notification update',
+                    tags: ['test:1', 'test:2'],
+                },
+            },
+        });
+    });
 });

9edf7206be0e3d6df7eda7ff6f6677ff4fadb629
Thomas Heartman <thomas@getunleash.io>
2024-07-23 09:34:39 +0200

    feat: add basic tests

---
+++ frontend/src/component/feature/FeatureView/Collaborators.test.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import { Collaborators } from './Collaborators';
+
+test('renders nothing if collaborators is undefined', () => {
+    const { container } = render(<Collaborators collaborators={undefined} />);
+
+    expect(container).toBeEmptyDOMElement();
+});
+
+test('renders nothing if users is empty', () => {
+    const { container } = render(
+        <Collaborators collaborators={{ users: [] }} />,
+    );
+
+    expect(container).toBeEmptyDOMElement();
+});

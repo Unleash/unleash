d89f908743151552bff54321d1f1f1f1486317b6
sjaanus <sellinjaanus@gmail.com>
2024-07-24 16:25:06 +0300

    feat: update openapi schema for tags

---
+++ src/lib/openapi/spec/create-feature-schema.test.ts
@@ -0,0 +1,31 @@
+import { validateSchema } from '../validate';
+import type { CreateFeatureSchema } from './create-feature-schema';
+
+test('createFeatureSchema', () => {
+    const data: CreateFeatureSchema = {
+        name: 'disable-comments',
+        type: 'release',
+        description:
+            'Controls disabling of the comments section in case of an incident',
+        impressionData: false,
+        tags: ['simple:test', 'simple:test2'],
+    };
+
+    expect(
+        validateSchema('#/components/schemas/createFeatureSchema', data),
+    ).toBeUndefined();
+});
+
+test('createFeatureSchema without tags', () => {
+    const data: CreateFeatureSchema = {
+        name: 'disable-comments',
+        type: 'release',
+        description:
+            'Controls disabling of the comments section in case of an incident',
+        impressionData: false,
+    };
+
+    expect(
+        validateSchema('#/components/schemas/createFeatureSchema', data),
+    ).toBeUndefined();
+});

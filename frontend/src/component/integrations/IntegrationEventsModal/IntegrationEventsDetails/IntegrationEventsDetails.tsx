e63503e832c7db8d31d83fafb7f42ec39a84d0bd
Nuno Góis <github@nunogois.com>
2024-07-24 08:14:16 +0100

    chore: add integration events modal (#7648)
    https://linear.app/unleash/issue/2-2441/create-integration-events-modal
    
    Adds the integration events modal to the UI, allowing us to visualize
    them. This is the core of the UI work for this feature.
    
    <img width="587" alt="image"
    src="https://github.com/user-attachments/assets/f64cbb8c-1c01-4638-a661-5943ad7a890c">
    
    ### Example: Success
    
    <img width="1277" alt="image"
    src="https://github.com/user-attachments/assets/578bc7dc-d37d-4c0a-b74a-4bd33e859b51">
    
    ### Example: Success with errors
    
    <img width="1255" alt="image"
    src="https://github.com/user-attachments/assets/f784104d-7f11-4146-829d-6b3a3808815b">
    
    ### Example: Failed
    
    <img width="1254" alt="image"
    src="https://github.com/user-attachments/assets/543f857d-3877-4c17-92eb-58e6f038b8ac">

---
+++ frontend/src/component/integrations/IntegrationEventsModal/IntegrationEventsDetails/IntegrationEventsDetails.tsx
@@ -0,0 +1,75 @@
+import { Alert, styled } from '@mui/material';
+import type { IntegrationEvent } from 'interfaces/integrationEvent';
+import { IntegrationEventsDetailsAccordion } from './IntegrationEventsDetailsAccordion';
+import CheckCircleOutline from '@mui/icons-material/CheckCircleOutline';
+import { Link } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+const LazyReactJSONEditor = lazy(
+    () => import('component/common/ReactJSONEditor/ReactJSONEditor'),
+);
+
+const StyledDetails = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(2),
+    padding: theme.spacing(2),
+}));
+
+const StyledAlert = styled(Alert)({
+    fontSize: 'inherit',
+    lineBreak: 'anywhere',
+});
+
+const StyledLink = styled(Link)(({ theme }) => ({
+    marginLeft: theme.spacing(1),
+}));
+
+export const IntegrationEventsDetails = ({
+    state,
+    stateDetails,
+    event,
+    details,
+}: IntegrationEvent) => {
+    const severity =
+        state === 'failed'
+            ? 'error'
+            : state === 'success'
+              ? 'success'
+              : 'warning';
+
+    const icon = state === 'success' ? <CheckCircleOutline /> : undefined;
+
+    return (
+        <StyledDetails>
+            <StyledAlert severity={severity} icon={icon}>
+                {stateDetails}
+            </StyledAlert>
+            <IntegrationEventsDetailsAccordion
+                header={
+                    <>
+                        Event:
+                        <StyledLink to='/history'>{event.type}</StyledLink>
+                    </>
+                }
+            >
+                <Suspense fallback={null}>
+                    <LazyReactJSONEditor
+                        content={{ json: event }}
+                        readOnly
+                        statusBar={false}
+                        editorStyle='sidePanel'
+                    />
+                </Suspense>
+            </IntegrationEventsDetailsAccordion>
+            <Suspense fallback={null}>
+                <LazyReactJSONEditor
+                    content={{ json: details }}
+                    readOnly
+                    statusBar={false}
+                    editorStyle='sidePanel'
+                />
+            </Suspense>
+        </StyledDetails>
+    );
+};

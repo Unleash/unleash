01ea743aa077fbe73a54d1abce90571e036b1ec1
Thomas Heartman <thomas@getunleash.io>
2024-07-26 09:31:45 +0200

    fix: replace existing project icon and add wrapping component

---
+++ frontend/src/component/common/ProjectIcon/ProjectIcon.tsx
@@ -0,0 +1,6 @@
+import { SvgIcon } from '@mui/material';
+import { ReactComponent as Svg } from 'assets/icons/projectIconSmall.svg';
+
+export const ProjectIcon = () => (
+    <SvgIcon component={Svg} viewBox={'0 0 14 10'} />
+);

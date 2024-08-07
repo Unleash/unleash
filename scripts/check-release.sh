64b9c00c1f24834ee463273c710ef651e9a40d85
sjaanus <sellinjaanus@gmail.com>
2024-08-07 12:58:47 +0300

    Fix

--- scripts/check-release.sh
+++ scripts/check-release.sh
@@ -2,17 +2,13 @@
 
 set -e
 
-echo "CI: $CI"
-echo "TEST_DATABASE_URL: $TEST_DATABASE_URL"
-echo "DATABASE_URL: $DATABASE_URL"
-
-
 yarn
 yarn run lint
 yarn run build:backend
 yarn run test:report
 
 cd frontend
+yarn
 yarn run ts:check
 yarn run lint:check
 yarn run fmt:check

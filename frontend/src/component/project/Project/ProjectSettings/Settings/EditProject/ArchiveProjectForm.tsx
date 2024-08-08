a01305040dfefe54f0749d326146f1c2391543b9
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2024-08-08 09:29:28 +0200

    feat: archive project form (#7797)

---
+++ frontend/src/component/project/Project/ProjectSettings/Settings/EditProject/ArchiveProjectForm.tsx
@@ -0,0 +1,43 @@
+import FormTemplate from 'component/common/FormTemplate/FormTemplate';
+import { useRequiredPathParam } from 'hooks/useRequiredPathParam';
+import useProjectApi from 'hooks/api/actions/useProjectApi/useProjectApi';
+import useUiConfig from 'hooks/api/getters/useUiConfig/useUiConfig';
+import { styled } from '@mui/material';
+import { ArchiveProject } from '../ArchiveProject';
+
+const StyledContainer = styled('div')(({ theme }) => ({
+    borderRadius: theme.spacing(2),
+    overflow: 'hidden',
+}));
+
+interface IDeleteProjectForm {
+    featureCount: number;
+}
+export const ArchiveProjectForm = ({ featureCount }: IDeleteProjectForm) => {
+    const id = useRequiredPathParam('projectId');
+    const { uiConfig } = useUiConfig();
+    const { loading } = useProjectApi();
+    const formatProjectArchiveApiCode = () => {
+        return `curl --location --request DELETE '${uiConfig.unleashUrl}/api/admin/projects/${id}/archive' \\
+--header 'Authorization: INSERT_API_KEY' '`;
+    };
+
+    return (
+        <StyledContainer>
+            <FormTemplate
+                loading={loading}
+                title='Archive project'
+                description=''
+                documentationLink='https://docs.getunleash.io/reference/projects'
+                documentationLinkLabel='Projects documentation'
+                formatApiCode={formatProjectArchiveApiCode}
+                compact
+                compactPadding
+                showDescription={false}
+                showLink={false}
+            >
+                <ArchiveProject projectId={id} featureCount={featureCount} />
+            </FormTemplate>
+        </StyledContainer>
+    );
+};

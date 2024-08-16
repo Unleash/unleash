1e42687c84b22c3cbeb48d54951c4a1afba6abf1
Thomas Heartman <thomas@getunleash.io>
2024-08-16 08:46:06 +0200

    feat: revert changes to original query

--- src/lib/features/project/project-store.ts
+++ src/lib/features/project/project-store.ts
@@ -336,7 +336,6 @@ class ProjectStore implements IProjectStore {
                 'projects.id',
             )
             .leftJoin('project_stats', 'project_stats.project', 'projects.id')
-            .leftJoin('events', 'events.feature_name', 'features.name')
             .orderBy('projects.name', 'asc');
 
         if (this.flagResolver.isEnabled('archiveProjects')) {
@@ -356,9 +355,7 @@ class ProjectStore implements IProjectStore {
                 'projects.id, projects.name, projects.description, projects.health, projects.updated_at, projects.created_at, ' +
                     'count(features.name) FILTER (WHERE features.archived_at is null) AS number_of_features, ' +
                     'count(features.name) FILTER (WHERE features.archived_at is null and features.stale IS TRUE) AS stale_feature_count, ' +
-                    'count(features.name) FILTER (WHERE features.archived_at is null and features.potentially_stale IS TRUE) AS potentially_stale_feature_count,' +
-                    'MAX(features.last_seen_at) AS last_usage,' +
-                    'MAX(events.created_at) AS last_updated',
+                    'count(features.name) FILTER (WHERE features.archived_at is null and features.potentially_stale IS TRUE) AS potentially_stale_feature_count,',
             ),
             'project_settings.default_stickiness',
             'project_settings.project_mode',

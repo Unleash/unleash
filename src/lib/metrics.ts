4e11e57f7f2cc3eeabb341c6845ee0040397b2cf
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2024-08-20 09:46:39 +0200

    feat: project actions count metric (#7929)

--- src/lib/metrics.ts
+++ src/lib/metrics.ts
@@ -19,6 +19,10 @@ import {
     CLIENT_METRICS,
     CLIENT_REGISTER,
     PROJECT_ENVIRONMENT_REMOVED,
+    PROJECT_CREATED,
+    PROJECT_ARCHIVED,
+    PROJECT_REVIVED,
+    PROJECT_DELETED,
 } from './types/events';
 import type { IUnleashConfig } from './types/option';
 import type { ISettingStore, IUnleashStores } from './types/stores';
@@ -313,6 +317,12 @@ export default class MetricsMonitor {
             name: 'feature_lifecycle_stage_entered',
             help: 'Count how many features entered a given stage',
             labelNames: ['stage'],
+        });
+
+        const projectActionsCounter = createCounter({
+            name: 'project_actions_count',
+            help: 'Count project actions',
+            labelNames: ['action'],
         });
 
         const projectEnvironmentsDisabled = createCounter({
@@ -850,6 +860,18 @@ export default class MetricsMonitor {
                 environment: 'n/a',
                 environmentType: 'n/a',
             });
+        });
+        eventStore.on(PROJECT_CREATED, () => {
+            projectActionsCounter.labels({ action: PROJECT_CREATED }).inc();
+        });
+        eventStore.on(PROJECT_ARCHIVED, () => {
+            projectActionsCounter.labels({ action: PROJECT_ARCHIVED }).inc();
+        });
+        eventStore.on(PROJECT_REVIVED, () => {
+            projectActionsCounter.labels({ action: PROJECT_REVIVED }).inc();
+        });
+        eventStore.on(PROJECT_DELETED, () => {
+            projectActionsCounter.labels({ action: PROJECT_DELETED }).inc();
         });
 
         const logger = config.getLogger('metrics.ts');

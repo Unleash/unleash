8ebebd790d5ff6a1cc8c8ab280145158a1440132
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2024-08-21 16:48:50 +0200

    fix: enable disabled strategies keeps settings (#7955)

--- src/lib/features/feature-toggle/feature-toggle-service.ts
+++ src/lib/features/feature-toggle/feature-toggle-service.ts
@@ -1783,7 +1783,7 @@ class FeatureToggleService {
                     strategies.map((strategy) =>
                         this.updateStrategy(
                             strategy.id,
-                            { disabled: false },
+                            { ...strategy, disabled: false },
                             {
                                 environment,
                                 projectId: project,

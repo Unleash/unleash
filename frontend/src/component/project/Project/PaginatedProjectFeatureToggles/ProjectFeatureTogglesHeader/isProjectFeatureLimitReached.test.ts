52ea1a60c069bba5614890e2a85312d926dfdc0a
sjaanus <sellinjaanus@gmail.com>
2024-08-22 11:11:24 +0300

    Fix

---
+++ frontend/src/component/project/Project/PaginatedProjectFeatureToggles/ProjectFeatureTogglesHeader/isProjectFeatureLimitReached.test.ts
@@ -0,0 +1,21 @@
+import { isProjectFeatureLimitReached } from './useFlagLimits';
+
+test('isFeatureLimitReached  should return false when featureLimit is null', async () => {
+    expect(isProjectFeatureLimitReached(null, 5)).toBe(false);
+});
+
+test('isFeatureLimitReached  should return false when featureLimit is undefined', async () => {
+    expect(isProjectFeatureLimitReached(undefined, 5)).toBe(false);
+});
+
+test('isFeatureLimitReached should return false when featureLimit is smaller current feature count', async () => {
+    expect(isProjectFeatureLimitReached(6, 5)).toBe(false);
+});
+
+test('isFeatureLimitReached should return true when featureLimit is smaller current feature count', async () => {
+    expect(isProjectFeatureLimitReached(4, 5)).toBe(true);
+});
+
+test('isFeatureLimitReached should return true when featureLimit is equal to current feature count', async () => {
+    expect(isProjectFeatureLimitReached(5, 5)).toBe(true);
+});

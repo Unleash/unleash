dacddb7fdf78004e83ff8cf934b8258963e9a02f
sjaanus <sellinjaanus@gmail.com>
2024-08-30 10:57:24 +0300

    Fix

--- src/migrations/20240830102144-onboarding-events.js
+++ src/migrations/20240830102144-onboarding-events.js
@@ -3,12 +3,19 @@
 exports.up = function (db, cb) {
     db.runSql(
         `
-            CREATE TABLE onboarding_events (
+            CREATE TABLE onboarding_events_instance (
+                event VARCHAR(255) NOT NULL,
+                time_to_event INTEGER NOT NULL,
+                PRIMARY KEY (event)
+            );
+
+            CREATE TABLE onboarding_events_project (
                event VARCHAR(255) NOT NULL,
-               diff INTEGER NOT NULL,
-               project VARCHAR(255),
+               time_to_event INTEGER NOT NULL,
+               project VARCHAR(255) REFERENCES projects(id) ON DELETE CASCADE,
                PRIMARY KEY (event, project)
             );
+
         `,
         cb,
     );
@@ -17,7 +24,8 @@ exports.up = function (db, cb) {
 exports.down = function (db, cb) {
     db.runSql(
         `
-            DROP TABLE IF EXISTS onboarding_events;
+            DROP TABLE IF EXISTS onboarding_events_instance;
+            DROP TABLE IF EXISTS onboarding_events_project;
         `,
         cb);
 };

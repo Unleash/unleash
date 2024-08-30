97cdc8c57365564fa183ab7b275dd443a5d473d2
sjaanus <sellinjaanus@gmail.com>
2024-08-30 10:27:01 +0300

    feat: collect onboarding events in separate table

---
+++ src/migrations/20240830102144-onboarding-events.js
@@ -0,0 +1,23 @@
+'use strict';
+
+exports.up = function (db, cb) {
+    db.runSql(
+        `
+            CREATE TABLE onboarding_events (
+               event VARCHAR(50) NOT NULL,
+               diff INTEGER NOT NULL,
+               project VARCHAR(255),
+               PRIMARY KEY (event, project)
+            );
+        `,
+        cb,
+    );
+};
+
+exports.down = function (db, cb) {
+    db.runSql(
+        `
+            DROP TABLE IF EXISTS onboarding_events;
+        `,
+        cb);
+};

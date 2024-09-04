8d0ca5edd354edf44c5bfac6740c27c21fbe62bd
sjaanus <sellinjaanus@gmail.com>
2024-09-04 11:27:08 +0300

    Add test

--- frontend/src/component/project/Project/PaginatedProjectFeatureToggles/ProjectOnboarding/WelcomeToProject.test.tsx
+++ frontend/src/component/project/Project/PaginatedProjectFeatureToggles/ProjectOnboarding/WelcomeToProject.test.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 import { testServerRoute, testServerSetup } from 'utils/testServer';
 import { WelcomeToProject } from './WelcomeToProject';
 import { screen } from '@testing-library/react';
-import { ProjectFeatureToggles } from '../ProjectFeatureToggles';
 
 const server = testServerSetup();
 
@@ -50,58 +49,4 @@ test('Project can connect SDK', async () => {
     await screen.findByText(
         'Your project is not yet connected to any SDK. In order to start using your feature flag connect an SDK to the project.',
     );
-});
-
-test('Project is onboarded', async () => {
-    const projectId = 'default';
-    testServerRoute(server, '/api/admin/projects/default/overview', {
-        onboardingStatus: {
-            status: 'onboarded',
-        },
-    });
-    testServerRoute(server, '/api/admin/ui-config', {
-        flags: {
-            onboardingUI: true,
-        },
-    });
-    render(
-        <Routes>
-            <Route
-                path={'/projects/:projectId'}
-                element={<ProjectFeatureToggles environments={[]} />}
-            />
-        </Routes>,
-        {
-            route: `/projects/${projectId}`,
-        },
-    );
-    expect(
-        screen.queryByText('Welcome to your project'),
-    ).not.toBeInTheDocument();
-});
-
-test('Project is not onboarded', async () => {
-    const projectId = 'default';
-    testServerRoute(server, '/api/admin/ui-config', {
-        flags: {
-            onboardingUI: true,
-        },
-    });
-    testServerRoute(server, '/api/admin/projects/default/overview', {
-        onboardingStatus: {
-            status: 'onboarding-started',
-        },
-    });
-    render(
-        <Routes>
-            <Route
-                path={'/projects/:projectId'}
-                element={<ProjectFeatureToggles environments={[]} />}
-            />
-        </Routes>,
-        {
-            route: `/projects/${projectId}`,
-        },
-    );
-    await screen.findByText('Welcome to your project');
 });

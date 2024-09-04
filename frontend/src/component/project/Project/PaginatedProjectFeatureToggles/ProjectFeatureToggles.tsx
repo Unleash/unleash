3ddc66ed2e65e79204a4067ed08348fad20e877b
sjaanus <sellinjaanus@gmail.com>
2024-09-04 13:11:15 +0300

    Fix

--- frontend/src/component/project/Project/PaginatedProjectFeatureToggles/ProjectFeatureToggles.tsx
+++ frontend/src/component/project/Project/PaginatedProjectFeatureToggles/ProjectFeatureToggles.tsx
@@ -504,7 +504,9 @@ export const ProjectFeatureToggles = ({
 
                     <ConnectSdkDialog
                         open={connectSdkOpen}
-                        onClose={() => {}}
+                        onClose={() => {
+                            setConnectSdkOpen(false);
+                        }}
                         project={projectId}
                         environments={environments}
                     />

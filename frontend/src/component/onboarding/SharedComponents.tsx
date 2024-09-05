09e55c10c2aeb29500235e6510b428216d17ac93
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2024-09-05 15:21:27 +0200

    feat: onboarding step badges (#8102)

--- frontend/src/component/onboarding/SharedComponents.tsx
+++ frontend/src/component/onboarding/SharedComponents.tsx
@@ -5,3 +5,10 @@ export const SectionHeader = styled('div')(({ theme }) => ({
     marginBottom: theme.spacing(1),
     fontSize: theme.typography.body1.fontSize,
 }));
+
+export const StepperBox = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: theme.spacing(2),
+}));

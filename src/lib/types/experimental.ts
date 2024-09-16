efb3f37b9c7468a55c26529a2d4359ea20d3c48f
Gitar <noreply@gitar.co>
2024-09-16 16:45:29 +0000

    [Gitar] Updating TypeScript files

--- src/lib/types/experimental.ts
+++ src/lib/types/experimental.ts
@@ -56,7 +56,6 @@ export type IFlagKey =
     | 'extendedMetrics'
     | 'removeUnsafeInlineStyleSrc'
     | 'originMiddleware'
-    | 'newEventSearch'
     | 'archiveProjects'
     | 'projectListImprovements'
     | 'useProjectReadModel'
@@ -278,10 +277,6 @@ const flags: IFlags = {
     ),
     originMiddleware: parseEnvVarBoolean(
         process.env.UNLEASH_EXPERIMENTAL_ORIGIN_MIDDLEWARE,
-        false,
-    ),
-    newEventSearch: parseEnvVarBoolean(
-        process.env.UNLEASH_EXPERIMENTAL_NEW_EVENT_SEARCH,
         false,
     ),
     archiveProjects: parseEnvVarBoolean(

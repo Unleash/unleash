ebbc0ab71339e4ecd308e6930ba0913eefdc6938
Thomas Heartman <thomas@getunleash.io>
2024-09-23 13:35:30 +0200

    feat: add placeholder for new component

---
+++ frontend/src/component/personalDashboard/SetupComplete.tsx
@@ -0,0 +1,46 @@
+import { styled, Typography } from '@mui/material';
+import type { FC } from 'react';
+import { Link } from 'react-router-dom';
+import Lightbulb from '@mui/icons-material/LightbulbOutlined';
+
+const TitleContainer = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    gap: theme.spacing(2),
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const ActionBox = styled('article')(({ theme }) => ({
+    padding: theme.spacing(4, 2),
+    display: 'flex',
+    gap: theme.spacing(3),
+    flexDirection: 'column',
+}));
+
+export const SetupComplete: FC<{ project: string }> = ({ project }) => {
+    return (
+        <ActionBox>
+            <TitleContainer>
+                <Lightbulb color='primary' />
+                <h3>Project Insight</h3>
+            </TitleContainer>
+            <Typography>
+                This project already has connected SDKs and existing feature
+                flags. You can set up another SDK locally, or if your codebase
+                has an existing setup, there is no need to take further action.
+            </Typography>
+
+            <Typography>
+                <Link to={`/projects/${project}?create=true`}>
+                    Create a new feature flag
+                </Link>{' '}
+                or go to the{' '}
+                <Link to={`/projects/${project}`} title={project}>
+                    go to the project
+                </Link>{' '}
+                to work with existing flags
+            </Typography>
+        </ActionBox>
+    );
+};

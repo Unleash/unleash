cac0809a524b6011b3ec1f0da563e02c172aec57
Thomas Heartman <thomas@getunleash.io>
2024-09-26 13:27:27 +0200

    fix: trim name and description before validation
    This fixes a bug where you can input just whitespace for
    name/description. It also means that you can no longer have both "my
    role" and "my role    " as separate roles.
    
    API fix will follow.

---
+++ frontend/src/component/admin/roles/RoleForm/useRoleForm.test.ts
@@ -0,0 +1,50 @@
+import { renderHook } from '@testing-library/react';
+import { useRoleForm } from './useRoleForm';
+import { act } from 'react-test-renderer';
+import { test } from 'vitest';
+
+describe('trim names and description', () => {
+    test('name is trimmed before being set', () => {
+        const { result } = renderHook(() => useRoleForm());
+
+        act(() => {
+            result.current.setName('  my role    ');
+        });
+
+        expect(result.current.name).toBe('my role');
+    });
+
+    test('description is trimmed before being set', () => {
+        const { result } = renderHook(() => useRoleForm());
+
+        act(() => {
+            result.current.setDescription('  my description    ');
+        });
+
+        expect(result.current.description).toBe('my description');
+    });
+
+    test('name that is just whitespace triggers an error', () => {
+        const { result } = renderHook(() => useRoleForm());
+
+        act(() => {
+            result.current.validateName('    ');
+        });
+
+        expect(result.current.errors).toMatchObject({
+            name: 'Name is required.',
+        });
+    });
+
+    test('description that is just whitespace triggers an error', () => {
+        const { result } = renderHook(() => useRoleForm());
+
+        act(() => {
+            result.current.validateDescription('    ');
+        });
+
+        expect(result.current.errors).toMatchObject({
+            description: 'Description is required.',
+        });
+    });
+});

cf476f9e7dd526fe6de403190b4407b67efdf97f
Thomas Heartman <thomas@getunleash.io>
2024-09-26 11:03:55 +0200

    chore: update orval for personal dashboards (#8268)
    Updates orval for personal dashboards

--- frontend/src/openapi/models/personalDashboardSchemaProjectsItem.ts
+++ frontend/src/openapi/models/personalDashboardSchemaProjectsItem.ts
@@ -3,8 +3,19 @@
  * Do not edit manually.
  * See `gen:api` script in package.json
  */
+import type { PersonalDashboardSchemaProjectsItemOwners } from './personalDashboardSchemaProjectsItemOwners';
+import type { PersonalDashboardSchemaProjectsItemRolesItem } from './personalDashboardSchemaProjectsItemRolesItem';
 
 export type PersonalDashboardSchemaProjectsItem = {
     /** The id of the project */
     id: string;
+    /** The name of the project */
+    name: string;
+    /** The users and/or groups that have the "owner" role in this project. If no such users or groups exist, the list will contain the "system" owner instead. */
+    owners?: PersonalDashboardSchemaProjectsItemOwners;
+    /**
+     * The list of roles that the user has in this project.
+     * @minItems 1
+     */
+    roles: PersonalDashboardSchemaProjectsItemRolesItem[];
 };

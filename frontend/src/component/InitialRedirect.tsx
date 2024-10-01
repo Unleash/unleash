323ba6a1422db2479a3cd70a3125ab4f2fda6e97
kwasniew <kwasniewski.mateusz@gmail.com>
2024-10-01 11:57:38 +0200

    feat: redirect to personal dashboard when no last project

--- frontend/src/component/InitialRedirect.tsx
+++ frontend/src/component/InitialRedirect.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import useProjects from 'hooks/api/getters/useProjects/useProjects';
 import { useLastViewedProject } from 'hooks/useLastViewedProject';
 import Loader from './common/Loader/Loader';
-import { getSessionStorageItem, setSessionStorageItem } from '../utils/storage';
+import { getSessionStorageItem, setSessionStorageItem } from 'utils/storage';
 import { useUiFlag } from 'hooks/useUiFlag';
 
 export const InitialRedirect = () => {

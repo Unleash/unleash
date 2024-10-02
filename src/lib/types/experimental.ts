9675aaad2bf70f4fda3d0a10db2e15f01b6b76d3
Gastón Fournier <gaston@getunleash.io>
2024-10-02 12:35:16 +0200

    Remove unused flag

--- src/lib/types/experimental.ts
+++ src/lib/types/experimental.ts
@@ -31,7 +31,6 @@ export type IFlagKey =
     | 'celebrateUnleash'
     | 'featureSearchFeedback'
     | 'featureSearchFeedbackPosting'
-    | 'edgeBulkMetrics'
     | 'extendedUsageMetrics'
     | 'adminTokenKillSwitch'
     | 'feedbackComments'
@@ -165,10 +164,6 @@ const flags: IFlags = {
     ),
     encryptEmails: parseEnvVarBoolean(
         process.env.UNLEASH_EXPERIMENTAL_ENCRYPT_EMAILS,
-        false,
-    ),
-    edgeBulkMetrics: parseEnvVarBoolean(
-        process.env.UNLEASH_EXPERIMENTAL_EDGE_BULK_METRICS,
         false,
     ),
     extendedUsageMetrics: parseEnvVarBoolean(

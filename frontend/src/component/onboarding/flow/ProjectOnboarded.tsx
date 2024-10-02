92f75fd26f1788e4baded090989abd52f38f2ced
sjaanus <sellinjaanus@gmail.com>
2024-10-02 13:04:52 +0300

    feat: add all outlined icons

--- frontend/src/component/onboarding/flow/ProjectOnboarded.tsx
+++ frontend/src/component/onboarding/flow/ProjectOnboarded.tsx
@@ -1,9 +1,12 @@
 import { IconButton, styled, Tooltip, Typography } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import Check from '@mui/icons-material/Check';
-import People from '@mui/icons-material/People';
-import MenuBook from '@mui/icons-material/MenuBook';
 import { Link } from 'react-router-dom';
+import {
+    ExtensionOutlined,
+    MenuBookOutlined,
+    PeopleOutlined,
+} from '@mui/icons-material';
 
 interface IProjectOnboardedProps {
     projectId: string;
@@ -66,11 +69,15 @@ const StyledCheck = styled(Check)(({ theme }) => ({
     height: '28px',
 }));
 
-const ColoredPeople = styled(People)(({ theme }) => ({
+const ColoredExtension = styled(ExtensionOutlined)(({ theme }) => ({
     color: theme.palette.primary.main,
 }));
 
-const ColoredMenuBook = styled(MenuBook)(({ theme }) => ({
+const ColoredPeople = styled(PeopleOutlined)(({ theme }) => ({
+    color: theme.palette.primary.main,
+}));
+
+const ColoredMenuBook = styled(MenuBookOutlined)(({ theme }) => ({
     color: theme.palette.primary.main,
 }));
 
@@ -96,6 +103,7 @@ export const ProjectOnboarded = ({
             <Actions>
                 <ActionBox>
                     <TitleContainer>
+                        <ColoredExtension />
                         Expose your feature flag to users
                     </TitleContainer>
                     <Typography>

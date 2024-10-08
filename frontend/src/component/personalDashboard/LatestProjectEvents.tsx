96013a6fac888c0c33b519bf4a13f3b7a70e8bbc
Thomas Heartman <thomas@getunleash.io>
2024-10-08 13:40:27 +0200

    fix: add events list header

--- frontend/src/component/personalDashboard/LatestProjectEvents.tsx
+++ frontend/src/component/personalDashboard/LatestProjectEvents.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react';
 import { Markdown } from '../common/Markdown/Markdown';
 import type { PersonalDashboardProjectDetailsSchema } from '../../openapi';
 import { UserAvatar } from '../common/UserAvatar/UserAvatar';
-import { styled } from '@mui/material';
+import { Typography, styled } from '@mui/material';
 
 const Events = styled('ul')(({ theme }) => ({
     padding: 0,
@@ -18,25 +18,51 @@ const Event = styled('li')(({ theme }) => ({
     marginBottom: theme.spacing(4),
 }));
 
+const TitleContainer = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    gap: theme.spacing(2),
+    alignItems: 'center',
+}));
+
+const ActionBox = styled('article')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    display: 'flex',
+    gap: theme.spacing(3),
+    flexDirection: 'column',
+}));
+
 export const LatestProjectEvents: FC<{
     latestEvents: PersonalDashboardProjectDetailsSchema['latestEvents'];
 }> = ({ latestEvents }) => {
     return (
-        <Events>
-            {latestEvents.map((event) => {
-                return (
-                    <Event key={event.id}>
-                        <UserAvatar
-                            src={event.createdByImageUrl}
-                            sx={{ mt: 1 }}
-                        />
-                        <Markdown>
-                            {event.summary ||
-                                'No preview available for this event'}
-                        </Markdown>
-                    </Event>
-                );
-            })}
-        </Events>
+        <ActionBox>
+            <TitleContainer>
+                <Typography
+                    sx={{
+                        fontWeight: 'bold',
+                    }}
+                    component='h4'
+                >
+                    Latest Events
+                </Typography>
+            </TitleContainer>
+            <Events>
+                {latestEvents.map((event) => {
+                    return (
+                        <Event key={event.id}>
+                            <UserAvatar
+                                src={event.createdByImageUrl}
+                                sx={{ mt: 1 }}
+                            />
+                            <Markdown>
+                                {event.summary ||
+                                    'No preview available for this event'}
+                            </Markdown>
+                        </Event>
+                    );
+                })}
+            </Events>
+        </ActionBox>
     );
 };

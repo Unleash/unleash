12d0bb5ced952dcde04ec8298ee2392dd835f0f6
Thomas Heartman <thomas@getunleash.io>
2024-10-11 09:23:52 +0200

    fix: adjust the height of the flag section
    This commit adjusts the height of the flag section in the personal
    dashboard, so that the chart doesn't cause scrolling on the widest version.

--- frontend/src/component/personalDashboard/Grid.tsx
+++ frontend/src/component/personalDashboard/Grid.tsx
@@ -49,7 +49,7 @@ export const ProjectGrid = styled(ContentGrid)(
 export const FlagGrid = styled(ContentGrid)(
     onWideContainer({
         gridTemplateColumns: '1fr 1fr 1fr',
-        gridTemplateRows: '450px',
+        gridTemplateRows: '480px',
         display: 'grid',
         gridTemplateAreas: `
                 "flags chart chart"

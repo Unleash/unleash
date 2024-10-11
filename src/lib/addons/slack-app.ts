8c8c1f20644cdde17ce5f87ff1b48bc5fbe03a49
David Leek <david@getunleash.io>
2024-10-11 10:16:27 +0200

    chore: add back returns in slack-app addon

--- src/lib/addons/slack-app.ts
+++ src/lib/addons/slack-app.ts
@@ -69,6 +69,7 @@ export default class SlackAppAddon extends Addon {
                     event,
                     noAccessTokenMessage,
                 );
+                return;
             }
 
             const taggedChannels = this.findTaggedChannels(event);
@@ -84,6 +85,7 @@ export default class SlackAppAddon extends Addon {
                     event,
                     noSlackChannelsMessage,
                 );
+                return;
             }
 
             this.logger.debug(

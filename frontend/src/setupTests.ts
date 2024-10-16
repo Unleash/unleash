498fd008e80b616993346dc4662788fbe907bfef
Thomas Heartman <thomas@getunleash.io>
2024-10-16 15:29:19 +0200

    chore: string interpolation

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -17,9 +17,9 @@ process.env.TZ = 'UTC';
 
 const messagesToIgnore = [
     'Warning: An update to %s inside a test was not wrapped in act',
-    '[MSW] Found a redundant usage of query parameters in the request handler URL for',
+    '[MSW] Found a redundant usage of query parameters in the request handler URL for %s',
     'An exception was caught and handled.',
-    'MUI: You have provided an out-of-range value',
+    'MUI: You have provided an out-of-range value %s',
     "Failed to create chart: can't acquire context from the given item",
 ];
 

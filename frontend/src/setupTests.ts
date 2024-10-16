6787bae18544f4e561f3660d899ba2a2dc80846a
Thomas Heartman <thomas@getunleash.io>
2024-10-16 15:24:09 +0200

    chore: silence remaining front end test warnings
    This silences front end test warnings that we don't care about. The
    reason we don't care is that:
    - we won't fix
    - it's test-specific, doesn't appear to happen in real life
    
    And it clogs the test logs.

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -22,9 +22,19 @@ beforeAll(() => {
         if (
             !(
                 typeof args[0] === 'string' &&
-                args[0].includes(
+                (args[0].includes(
                     'Warning: An update to %s inside a test was not wrapped in act',
-                )
+                ) ||
+                    args[0].includes(
+                        '[MSW] Found a redundant usage of query parameters in the request handler URL for',
+                    ) ||
+                    args[0].includes('An exception was caught and handled.') ||
+                    args[0].includes(
+                        'MUI: You have provided an out-of-range value',
+                    ) ||
+                    args[0].includes(
+                        "Failed to create chart: can't acquire context from the given item",
+                    ))
             )
         ) {
             consoleError(...args);

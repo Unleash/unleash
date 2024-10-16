ac97b0c93078fadb18a9e006e9b593c7be77311c
Thomas Heartman <thomas@getunleash.io>
2024-10-16 15:28:17 +0200

    chore: refactor

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -15,6 +15,14 @@ if (!window.ResizeObserver) {
 
 process.env.TZ = 'UTC';
 
+const messagesToIgnore = [
+    'Warning: An update to %s inside a test was not wrapped in act',
+    '[MSW] Found a redundant usage of query parameters in the request handler URL for',
+    'An exception was caught and handled.',
+    'MUI: You have provided an out-of-range value',
+    "Failed to create chart: can't acquire context from the given item",
+];
+
 // ignore known React warnings
 const consoleError = console.error;
 beforeAll(() => {
@@ -22,19 +30,7 @@ beforeAll(() => {
         if (
             !(
                 typeof args[0] === 'string' &&
-                (args[0].includes(
-                    'Warning: An update to %s inside a test was not wrapped in act',
-                ) ||
-                    args[0].includes(
-                        '[MSW] Found a redundant usage of query parameters in the request handler URL for',
-                    ) ||
-                    args[0].includes('An exception was caught and handled.') ||
-                    args[0].includes(
-                        'MUI: You have provided an out-of-range value',
-                    ) ||
-                    args[0].includes(
-                        "Failed to create chart: can't acquire context from the given item",
-                    ))
+                messagesToIgnore.some((message) => args[0].includes(message))
             )
         ) {
             consoleError(...args);

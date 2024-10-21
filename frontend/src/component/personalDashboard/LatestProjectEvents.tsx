69fcb572effb1f775d54698abb42a9da787c408c
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2024-10-21 15:10:20 +0200

    fix: break words first, break all second (#8495)

--- frontend/src/component/personalDashboard/LatestProjectEvents.tsx
+++ frontend/src/component/personalDashboard/LatestProjectEvents.tsx
@@ -44,9 +44,9 @@ const StyledUserAvatar = styled(UserAvatar)(({ theme }) => ({
     marginTop: theme.spacing(0.5),
 }));
 
-const StyledMarkdown = styled(Markdown)(({ theme }) => ({
-    wordBreak: 'break-all',
-}));
+const StyledMarkdown = styled(Markdown)({
+    overflowWrap: 'anywhere',
+});
 
 export const LatestProjectEvents: FC<{
     latestEvents: PersonalDashboardProjectDetailsSchema['latestEvents'];

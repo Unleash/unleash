32228af7a3741ee4fb7bab6f6be8c6805f49790b
Thomas Heartman <thomas@getunleash.io>
2024-10-28 13:33:28 +0100

    fix: invalid spreading of keys into table rows everywhere
    This commit fixes MUI prop spreading warnings in all the table rows I
    could find through a quick search in the code base.

--- frontend/src/component/environments/EnvironmentTable/EnvironmentTableSingle.tsx
+++ frontend/src/component/environments/EnvironmentTable/EnvironmentTableSingle.tsx
@@ -97,13 +97,19 @@ export const EnvironmentTableSingle = ({
             <TableBody {...getTableBodyProps()}>
                 {rows.map((row) => {
                     prepareRow(row);
+                    const { key, ...rowProps } = row.getRowProps();
                     return (
-                        <TableRow hover {...row.getRowProps()}>
-                            {row.cells.map((cell) => (
-                                <TableCell {...cell.getCellProps()}>
-                                    {cell.render('Cell')}
-                                </TableCell>
-                            ))}
+                        <TableRow hover key={key} {...rowProps}>
+                            {row.cells.map((cell) => {
+                                const { key, ...cellProps } =
+                                    cell.getCellProps();
+
+                                return (
+                                    <TableCell key={key} {...cellProps}>
+                                        {cell.render('Cell')}
+                                    </TableCell>
+                                );
+                            })}
                         </TableRow>
                     );
                 })}

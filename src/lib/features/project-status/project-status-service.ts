bf9d200a04b07100c5fafcf9b1bf1eb8e8a03621
Thomas Heartman <thomas@getunleash.io>
2024-11-06 10:00:08 +0100

    1-3076: you gotta do what you gotta do. impl

--- src/lib/features/project-status/project-status-service.ts
+++ src/lib/features/project-status/project-status-service.ts
@@ -1,31 +1,66 @@
 import type { ProjectStatusSchema } from '../../openapi';
-import type { IEventStore, IProjectStore, IUnleashStores } from '../../types';
+import type {
+    IApiTokenStore,
+    IEventStore,
+    IProjectStore,
+    ISegmentStore,
+    IUnleashStores,
+} from '../../types';
 
 export class ProjectStatusService {
     private eventStore: IEventStore;
     private projectStore: IProjectStore;
+    private apiTokenStore: IApiTokenStore;
+    private segmentStore: ISegmentStore;
 
     constructor({
         eventStore,
         projectStore,
-    }: Pick<IUnleashStores, 'eventStore' | 'projectStore'>) {
+        apiTokenStore,
+        segmentStore,
+    }: Pick<
+        IUnleashStores,
+        'eventStore' | 'projectStore' | 'apiTokenStore' | 'segmentStore'
+    >) {
         this.eventStore = eventStore;
         this.projectStore = projectStore;
+        this.apiTokenStore = apiTokenStore;
+        this.segmentStore = segmentStore;
     }
 
     async getProjectStatus(projectId: string): Promise<ProjectStatusSchema> {
+        const [
+            connectedEnvironments,
+            members,
+            apiTokens,
+            segments,
+            activityCountByDate,
+        ] = await Promise.all([
+            this.projectStore.getConnectedEnvironmentCountForProject(projectId),
+            this.projectStore.getMembersCountByProject(projectId),
+            this.apiTokenStore
+                .getAll()
+                .then(
+                    (tokens) =>
+                        tokens.filter(
+                            (token) =>
+                                token.project === projectId ||
+                                token.projects.includes(projectId),
+                        ).length,
+                ),
+
+            this.segmentStore.getProjectSegmentCount(projectId),
+            this.eventStore.getProjectEventActivity(projectId),
+        ]);
+
         return {
             resources: {
-                connectedEnvironments:
-                    await this.projectStore.getConnectedEnvironmentCountForProject(
-                        projectId,
-                    ),
-                members: 0,
-                apiTokens: 0,
-                segments: 0,
+                connectedEnvironments,
+                members,
+                apiTokens,
+                segments,
             },
-            activityCountByDate:
-                await this.eventStore.getProjectEventActivity(projectId),
+            activityCountByDate,
         };
     }
 }

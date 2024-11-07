cac9355f2219012839922f8ed01020d85b217334
Thomas Heartman <thomas@getunleash.io>
2024-11-07 09:27:06 +0100

    1-3059: flesh out some stuff

--- frontend/src/component/project/Project/ProjectStatus/ProjectLifecycleSummary.tsx
+++ frontend/src/component/project/Project/ProjectStatus/ProjectLifecycleSummary.tsx
@@ -1,10 +1,26 @@
-import { styled } from '@mui/material';
+import { Typography, styled } from '@mui/material';
+import { FeatureLifecycleStageIcon } from 'component/feature/FeatureView/FeatureOverview/FeatureLifecycle/FeatureLifecycleStageIcon';
 
 const LifecycleBox = styled('article')(({ theme }) => ({
     padding: theme.spacing(3),
     borderRadius: theme.shape.borderRadiusExtraLarge,
     border: `2px solid ${theme.palette.divider}`,
     aspectRatio: '1/1',
+    display: 'grid',
+
+    gridTemplateAreas: `
+        "count icon"
+        "description description"
+        "stats stats"
+    `,
+
+    gridTemplateColumns: '1fr auto',
+}));
+
+const GridCell = styled('span', {
+    shouldForwardProp: (prop) => prop !== 'gridArea',
+})<{ gridArea: string }>(({ gridArea }) => ({
+    gridArea,
 }));
 
 const Wrapper = styled('article')(({ theme }) => ({
@@ -16,7 +32,24 @@ const Wrapper = styled('article')(({ theme }) => ({
 export const ProjectLifecycleSummary = () => {
     return (
         <Wrapper>
-            <LifecycleBox />
+            <LifecycleBox>
+                <GridCell gridArea='count'>
+                    <Typography variant='h1' component='span'>
+                        15
+                    </Typography>
+                </GridCell>
+                <GridCell gridArea='icon'>
+                    <FeatureLifecycleStageIcon
+                        stage={{
+                            name: 'live',
+                            enteredStageAt: '',
+                            environments: [],
+                        }}
+                    />
+                </GridCell>
+                <GridCell gridArea='description'>flags in live</GridCell>
+                <GridCell gridArea='stats'>Avg. time in stage 10 days</GridCell>
+            </LifecycleBox>
             <LifecycleBox />
             <LifecycleBox />
             <LifecycleBox />

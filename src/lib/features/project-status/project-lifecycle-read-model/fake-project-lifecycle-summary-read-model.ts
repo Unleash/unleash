f6dc928af1d53b3439c5e045f5058aa42b014614
Thomas Heartman <thomas@getunleash.io>
2024-11-08 12:03:53 +0100

    1-3085: add fake read model

---
+++ src/lib/features/project-status/project-lifecycle-read-model/fake-project-lifecycle-summary-read-model.ts
@@ -0,0 +1,33 @@
+import type {
+    IProjectLifecycleSummaryReadModel,
+    ProjectLifecycleSummary,
+} from './project-lifecycle-read-model-type';
+
+export class FakeProjectLifecycleSummaryReadModel
+    implements IProjectLifecycleSummaryReadModel
+{
+    async getProjectLifecycleSummary(): Promise<ProjectLifecycleSummary> {
+        return {
+            initial: {
+                averageDays: 0,
+                currentFlags: 0,
+            },
+            preLive: {
+                averageDays: 0,
+                currentFlags: 0,
+            },
+            live: {
+                averageDays: 0,
+                currentFlags: 0,
+            },
+            completed: {
+                averageDays: 0,
+                currentFlags: 0,
+            },
+            archived: {
+                currentFlags: 0,
+                archivedFlagsLast30Days: 0,
+            },
+        };
+    }
+}

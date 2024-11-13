99342c4522790f24aa80db5ceae2efe88cc0e52b
Thomas Heartman <thomas@getunleash.io>
2024-11-13 12:09:24 +0100

    1-3106: only do generic messages

--- frontend/src/component/project/Project/ProjectStatus/LifecycleMessages.ts
+++ frontend/src/component/project/Project/ProjectStatus/LifecycleMessages.ts
@@ -1,6 +1,4 @@
-import type { ProjectStatusSchemaLifecycleSummary } from 'openapi';
-
-const genericMessages: Record<
+export const lifecycleMessages: Record<
     keyof ProjectStatusSchemaLifecycleSummary,
     string
 > = {
@@ -13,30 +11,4 @@ const genericMessages: Record<
         'Flags that are in cleanup are potentially stale flags. View the flags to evaluate whether you should archive them in Unleash and clean up your codebase to reduce technical debt',
     archived:
         'Flags that have been archived and are no longer in use, but kept for future reference.',
-};
-
-const mostTimeMessages: Partial<
-    Record<keyof ProjectStatusSchemaLifecycleSummary, string>
-> = {
-    initial:
-        'You spend most of your time the initial phase. This indicates you have created flags that is not detected in any environments which mean either integration issues or unused flags',
-    preLive:
-        'You spend most of your time in the pre-live phase. This indicates time is spent on development and testing, but the feature has not been rolled out in production. ',
-};
-
-export const getLifecycleMessages = (
-    spendsMostTimeIn: Array<keyof ProjectStatusSchemaLifecycleSummary>,
-) => {
-    const messages = Object.entries(genericMessages).map(([stage, message]) => {
-        const stageKey = stage as keyof ProjectStatusSchemaLifecycleSummary;
-        if (
-            spendsMostTimeIn.includes(stageKey) &&
-            stageKey in mostTimeMessages
-        ) {
-            return [stageKey, mostTimeMessages[stageKey]];
-        }
-        return [stage, message];
-    });
-
-    return Object.fromEntries(messages);
 };

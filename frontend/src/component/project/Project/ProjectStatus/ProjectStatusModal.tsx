7cb4b7af9f93d9e6b28535006241bbadba1d3236
Thomas Heartman <thomas@getunleash.io>
2024-11-14 13:48:28 +0100

    1-3088: move headers into modal

--- frontend/src/component/project/Project/ProjectStatus/ProjectStatusModal.tsx
+++ frontend/src/component/project/Project/ProjectStatus/ProjectStatusModal.tsx
@@ -6,6 +6,8 @@ import { ProjectActivity } from './ProjectActivity';
 import { ProjectHealth } from './ProjectHealth';
 import { ProjectLifecycleSummary } from './ProjectLifecycleSummary';
 import { StaleFlags } from './StaleFlags';
+import type { FC } from 'react';
+import { HelpIcon } from 'component/common/HelpIcon/HelpIcon';
 
 const ModalContentContainer = styled('section')(({ theme }) => ({
     minHeight: '100vh',
@@ -53,17 +55,65 @@ const HealthGrid = styled('div')(({ theme }) => ({
 const HeaderRow = styled('div')(({ theme }) => ({
     display: 'flex',
     alignItems: 'center',
-    gap: theme.spacing(1.5),
 }));
 
 const StyledProjectStatusSvg = styled(ProjectStatusSvg)(({ theme }) => ({
     fill: theme.palette.primary.main,
+    marginRight: theme.spacing(1.5),
 }));
 
 const ModalHeader = styled('h3')(({ theme }) => ({
     fontSize: theme.typography.h2.fontSize,
     margin: 0,
 }));
+
+const RowHeader = styled('h4')(({ theme }) => ({
+    margin: 0,
+    fontWeight: 'normal',
+}));
+
+const Row = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(1),
+}));
+
+const TooltipContent = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0.5),
+}));
+
+const TooltipText = styled('p')(({ theme }) => ({
+    fontSize: theme.typography.body1.fontSize,
+    '& + p': {
+        marginTop: theme.spacing(1),
+    },
+}));
+
+const LifecycleTooltip: FC = () => {
+    return (
+        <HelpIcon
+            htmlTooltip
+            htmlTooltipMaxWidth='550px'
+            tooltip={
+                <TooltipContent>
+                    <TooltipText>
+                        Based on usage metrics and interactions with Unleash,
+                        feature flags can go through five distinct lifecycle
+                        stages. These stages mirror the typical software
+                        development process and allow you to identify
+                        bottlenecks at any stage of the lifecycle.
+                    </TooltipText>
+
+                    <TooltipText>
+                        <a href='https://docs.getunleash.io/reference/feature-toggles#feature-flag-lifecycle'>
+                            Read more in our documentation
+                        </a>
+                    </TooltipText>
+                </TooltipContent>
+            }
+        />
+    );
+};
 
 export const ProjectStatusModal = ({ open, close }: Props) => {
     return (
@@ -73,17 +123,29 @@ export const ProjectStatusModal = ({ open, close }: Props) => {
                     <StyledProjectStatusSvg aria-hidden='true' />
                     <ModalHeader>Project status</ModalHeader>
                 </HeaderRow>
-                <HealthContainer>
-                    <HealthGrid>
-                        <ProjectHealth />
-                        <StaleFlags />
-                        <ProjectResources />
-                    </HealthGrid>
-                </HealthContainer>
+                <Row>
+                    <RowHeader>Health</RowHeader>
+                    <HealthContainer>
+                        <HealthGrid>
+                            <ProjectHealth />
+                            <StaleFlags />
+                            <ProjectResources />
+                        </HealthGrid>
+                    </HealthContainer>
+                </Row>
 
-                <ProjectActivity />
+                <Row>
+                    <RowHeader>Activity in project</RowHeader>
+                    <ProjectActivity />
+                </Row>
 
-                <ProjectLifecycleSummary />
+                <Row>
+                    <HeaderRow>
+                        <RowHeader>Flag lifecycle</RowHeader>
+                        <LifecycleTooltip />
+                    </HeaderRow>
+                    <ProjectLifecycleSummary />
+                </Row>
             </ModalContentContainer>
         </DynamicSidebarModal>
     );

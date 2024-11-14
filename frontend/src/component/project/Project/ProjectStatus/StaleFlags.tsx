85b8cf0f6c3e9f1876c6394a9deb0863080dc10c
Thomas Heartman <thomas@getunleash.io>
2024-11-14 08:41:04 +0100

    1-3107: hard code stale flags

--- frontend/src/component/project/Project/ProjectStatus/StaleFlags.tsx
+++ frontend/src/component/project/Project/ProjectStatus/StaleFlags.tsx
@@ -1,17 +1,49 @@
-import { Box, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { styled } from '@mui/material';
+import { PrettifyLargeNumber } from 'component/common/PrettifyLargeNumber/PrettifyLargeNumber';
+import { useRequiredPathParam } from 'hooks/useRequiredPathParam';
+import type { FC } from 'react';
+import { Link } from 'react-router-dom';
 
-const DescriptionText = styled(Typography)(({ theme }) => ({
-    color: theme.palette.text.secondary,
-    marginBottom: theme.spacing(2),
+const Wrapper = styled('article')(({ theme }) => ({
+    backgroundColor: theme.palette.envAccordion.expanded,
+    padding: theme.spacing(3),
+    borderRadius: theme.shape.borderRadiusExtraLarge,
+    minWidth: '300px',
+    gridArea: 'stale',
 }));
 
-export const StaleFlags = () => (
-    <Box gridArea='stale' sx={{ background: 'grey' }}>
-        <Typography variant='h3'>6 Stale flags</Typography>
-        <DescriptionText variant='body2'>
-            Remember to archive your stale feature flags to keep the project
-            health
-        </DescriptionText>
-    </Box>
-);
+const BigText = styled('span')(({ theme }) => ({
+    fontSize: `calc(2 * ${theme.typography.body1.fontSize})`,
+    color: theme.palette.primary.main,
+}));
+
+const BigNumber: FC<{ value?: number }> = ({ value }) => {
+    return (
+        <BigText>
+            <PrettifyLargeNumber
+                value={value ?? 0}
+                threshold={1000}
+                precision={1}
+            />
+        </BigText>
+    );
+};
+
+export const StaleFlags = () => {
+    const projectId = useRequiredPathParam('projectId');
+    return (
+        <Wrapper>
+            <Typography component='h4'>
+                <BigNumber value={6} />{' '}
+                <Link to={`/projects/${projectId}?state=IS%3Astale`}>
+                    stale flags
+                </Link>
+            </Typography>
+            <Typography variant='body2'>
+                Remember to archive your stale feature flags to keep the project
+                health
+            </Typography>
+        </Wrapper>
+    );
+};

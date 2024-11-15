517d6d7b5427e0854d237e2d1a58ae822eb50500
Thomas Heartman <thomas@getunleash.io>
2024-11-15 09:09:48 +0100

    1-3116: fix colors for health grid

--- frontend/src/component/project/Project/ProjectStatus/ProjectHealthGrid.styles.tsx
+++ frontend/src/component/project/Project/ProjectStatus/ProjectHealthGrid.styles.tsx
@@ -1,7 +1,7 @@
 import { styled } from '@mui/material';
 
 export const HealthGridTile = styled('article')(({ theme }) => ({
-    backgroundColor: theme.palette.envAccordion.expanded,
+    backgroundColor: theme.palette.neutral.light,
     padding: theme.spacing(3),
     borderRadius: theme.shape.borderRadiusExtraLarge,
 }));

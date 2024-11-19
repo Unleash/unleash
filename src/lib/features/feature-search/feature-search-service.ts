4a349c63f5d5040f982313b4300d917615b98e27
Thomas Heartman <thomas@getunleash.io>
2024-11-19 12:39:39 +0100

    1-3128: remove comments

--- src/lib/features/feature-search/feature-search-service.ts
+++ src/lib/features/feature-search/feature-search-service.ts
@@ -46,31 +46,6 @@ export class FeatureSearchService {
             const parsedState = parseSearchOperatorValue('stale', params.state);
             if (parsedState) {
                 queryParams.push(parsedState);
-                console.log(queryParams, parsedState);
-                // there's 4 possible flag states to handle:
-                // a: active and not potentiallyStale
-                // b: stale and not potentiallyStale
-                // c: active and potentiallyStale
-                // d: stale and potentiallyStale
-                //
-                // if stale does not include potentiallyStale,
-                // the potential combinations that include potentiallyStale are:
-                // IS ANY OF: active, stale, potentiallyStale = no filters => a, b, c, d
-                // IS ANY OF: active, potentially stale = filter out stale => a, c
-                // IS ANY OF: stale, potentially stale = I think this is a problem => b, c, d (active and not potentially stale or stale)
-                // IS NONE OF: active, stale, potentiallyStale = empty set => [nothing]
-                // IS NONE OF: active, potentially stale => only return stale => b, d
-                // IS NONE OF: stale, potentially stale => return active without potentially stale => a
-                //
-                //
-                // if potentiallyStale *does* include stale,
-                // the potential combinations that include potentiallyStale are:
-                // IS ANY OF: active, stale, potentiallyStale => a, b, c, d
-                // IS ANY OF: active, potentially stale => a, c, d
-                // IS ANY OF: stale, potentially stale => b, c, d (active and potentiallyStale or stale)
-                // IS NONE OF: active, stale, potentiallyStale => [nothing]
-                // IS NONE OF: active, potentially stale => d
-                // IS NONE OF: stale, potentially stale => a
             }
         }
 

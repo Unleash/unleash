b49526e4ac86d4cf07fb303e0fd5305165a0489d
Thomas Heartman <thomas@getunleash.io>
2024-11-19 12:00:09 +0100

    1-3131: format nicely

--- src/migrations/20241119103819-make-potentially-stale-non-nullable.js
+++ src/migrations/20241119103819-make-potentially-stale-non-nullable.js
@@ -3,19 +3,23 @@
 exports.up = function (db, cb) {
     db.runSql(
         `
-        UPDATE features SET potentially_stale = FALSE WHERE potentially_stale IS NULL;
-        ALTER TABLE features ALTER COLUMN potentially_stale SET NOT NULL;
+        UPDATE features
+        SET potentially_stale = FALSE
+        WHERE potentially_stale IS NULL;
+
+        ALTER TABLE features
+        ALTER COLUMN potentially_stale SET NOT NULL;
         `,
-        cb,
+        cb
     );
 };
 
 exports.down = function (db, cb) {
     db.runSql(
         `
-        ALTER TABLE features ALTER COLUMN potentially_stale DROP NOT NULL;
+        ALTER TABLE features
+        ALTER COLUMN potentially_stale DROP NOT NULL;
         `,
-        cb,
+        cb
     );
 };
-

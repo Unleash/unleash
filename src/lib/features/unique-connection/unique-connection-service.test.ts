486f009ca5dde1ebbc6136c92fe0dac723c424de
kwasniew <kwasniewski.mateusz@gmail.com>
2025-01-10 10:48:51 +0100

    feat: improve logic for syncing counts

--- src/lib/features/unique-connection/unique-connection-service.test.ts
+++ src/lib/features/unique-connection/unique-connection-service.test.ts
@@ -4,7 +4,7 @@ import getLogger from '../../../test/fixtures/no-logger';
 import type { IFlagResolver } from '../../types';
 import { SDK_CONNECTION_ID_RECEIVED } from '../../metric-events';
 import { addHours } from 'date-fns';
-import { EventEmitter } from 'events';
+import EventEmitter from 'events';
 
 const alwaysOnFlagResolver = {
     isEnabled() {
@@ -67,13 +67,13 @@ test('sync to existing current bucket from the same service', async () => {
     );
     uniqueConnectionService.listen();
 
-    eventBus.emit(SDK_CONNECTION_ID_RECEIVED, 'connection1');
-    eventBus.emit(SDK_CONNECTION_ID_RECEIVED, 'connection2');
+    uniqueConnectionService.count('connection1');
+    uniqueConnectionService.count('connection2');
 
     await uniqueConnectionService.sync();
 
-    eventBus.emit(SDK_CONNECTION_ID_RECEIVED, 'connection1');
-    eventBus.emit(SDK_CONNECTION_ID_RECEIVED, 'connection3');
+    uniqueConnectionService.count('connection1');
+    uniqueConnectionService.count('connection3');
 
     const stats = await uniqueConnectionService.getStats();
     expect(stats).toEqual({ previous: 0, current: 3 });
@@ -139,4 +139,31 @@ test('sync to existing previous bucket from another service', async () => {
     expect(stats1).toEqual({ previous: 3, current: 0 });
     const stats2 = await uniqueConnectionService2.getStats();
     expect(stats1).toEqual({ previous: 3, current: 0 });
+});
+
+test('populate previous and current', async () => {
+    const eventBus = new EventEmitter();
+    const config = { flagResolver: alwaysOnFlagResolver, getLogger, eventBus };
+    const uniqueConnectionStore = new FakeUniqueConnectionStore();
+    const uniqueConnectionService = new UniqueConnectionService(
+        { uniqueConnectionStore },
+        config,
+    );
+
+    uniqueConnectionService.count('connection1');
+    uniqueConnectionService.count('connection2');
+    await uniqueConnectionService.sync();
+    await uniqueConnectionService.sync();
+
+    uniqueConnectionService.count('connection3');
+    await uniqueConnectionService.sync(() => addHours(new Date(), 1));
+    await uniqueConnectionService.sync(() => addHours(new Date(), 1));
+
+    uniqueConnectionService.count('connection3');
+    uniqueConnectionService.count('connection4');
+    await uniqueConnectionService.sync(() => addHours(new Date(), 1));
+    await uniqueConnectionService.sync(() => addHours(new Date(), 1));
+
+    const stats = await uniqueConnectionService.getStats();
+    expect(stats).toEqual({ previous: 3, current: 2 });
 });

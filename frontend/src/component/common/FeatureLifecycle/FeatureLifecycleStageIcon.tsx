b150392cde8d8d1b5a90646f19ce7b2715cd8c2e
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-01-15 14:56:27 +0100

    update icon to accept additional props

--- frontend/src/component/common/FeatureLifecycle/FeatureLifecycleStageIcon.tsx
+++ frontend/src/component/common/FeatureLifecycle/FeatureLifecycleStageIcon.tsx
@@ -9,23 +9,39 @@ import { ReactComponent as Stage2 } from 'assets/icons/lifecycle/stage-2.svg';
 import { ReactComponent as Stage3 } from 'assets/icons/lifecycle/stage-3.svg';
 import { ReactComponent as Stage4 } from 'assets/icons/lifecycle/stage-4.svg';
 import { ReactComponent as Stage5 } from 'assets/icons/lifecycle/stage-5.svg';
-import type { LifecycleStage } from './LifecycleStage';
+import type { LifecycleStage } from '../../feature/FeatureView/FeatureOverview/FeatureLifecycle/LifecycleStage';
 import { useUiFlag } from 'hooks/useUiFlag';
 
 export const FeatureLifecycleStageIcon: FC<{
     stage: Pick<LifecycleStage, 'name'>;
-}> = ({ stage }) => {
+}> = ({ stage, ...props }) => {
     const newIcons = useUiFlag('lifecycleImprovements');
 
     if (stage.name === 'archived') {
-        return newIcons ? <Stage5 /> : <ArchivedStageIcon />;
+        return newIcons ? (
+            <Stage5 {...props} />
+        ) : (
+            <ArchivedStageIcon {...props} />
+        );
     } else if (stage.name === 'pre-live') {
-        return newIcons ? <Stage2 /> : <PreLiveStageIcon />;
+        return newIcons ? (
+            <Stage2 {...props} />
+        ) : (
+            <PreLiveStageIcon {...props} />
+        );
     } else if (stage.name === 'live') {
-        return newIcons ? <Stage3 /> : <LiveStageIcon />;
+        return newIcons ? <Stage3 {...props} /> : <LiveStageIcon {...props} />;
     } else if (stage.name === 'completed') {
-        return newIcons ? <Stage4 /> : <CompletedStageIcon />;
+        return newIcons ? (
+            <Stage4 {...props} />
+        ) : (
+            <CompletedStageIcon {...props} />
+        );
     } else {
-        return newIcons ? <Stage1 /> : <InitialStageIcon />;
+        return newIcons ? (
+            <Stage1 {...props} />
+        ) : (
+            <InitialStageIcon {...props} />
+        );
     }
 };

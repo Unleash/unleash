eb052f23718a822c2fd74c16b1fcf41a23495693
FredrikOseberg <fredrik.no@gmail.com>
2025-01-16 14:08:55 +0100

    fix: ui config

--- frontend/src/interfaces/uiConfig.ts
+++ frontend/src/interfaces/uiConfig.ts
@@ -91,11 +91,8 @@ export type UiFlags = {
     flagOverviewRedesign?: boolean;
     granularAdminPermissions?: boolean;
     sortProjectRoles?: boolean;
-<<<<<<< HEAD
     lifecycleImprovements?: boolean;
-=======
     frontendHeaderRedesign?: boolean;
->>>>>>> 03c02e86b (feat: transparent header)
 };
 
 export interface IVersionInfo {

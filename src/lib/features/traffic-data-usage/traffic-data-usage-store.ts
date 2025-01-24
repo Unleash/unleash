9d0f9647e9cfd14374541eb0ef207d3a222c209f
Thomas Heartman <thomas@getunleash.io>
2025-01-24 15:39:01 +0100

    chore(1-3293): fix some method impls

--- src/lib/features/traffic-data-usage/traffic-data-usage-store.ts
+++ src/lib/features/traffic-data-usage/traffic-data-usage-store.ts
@@ -96,7 +96,7 @@ export class TrafficDataUsageStore implements ITrafficDataUsageStore {
             });
     }
 
-    async getDailyTrafficDataUsageForPeriod(
+    async getDailyTrafficDataForPeriod(
         from: Date,
         to: Date,
     ): Promise<IStatTrafficUsage[]> {
@@ -107,7 +107,7 @@ export class TrafficDataUsageStore implements ITrafficDataUsageStore {
         return rows.map(mapRow);
     }
 
-    async getMonthlyTrafficDataUsageForPeriod(
+    async getMonthlyTrafficDataForPeriod(
         from: Date,
         to: Date,
     ): Promise<IStatMonthlyTrafficUsage[]> {
@@ -143,7 +143,7 @@ export class TrafficDataUsageStore implements ITrafficDataUsageStore {
         period: string,
     ): Promise<IStatTrafficUsage[]> {
         const month = new Date(period);
-        return this.getDailyTrafficDataUsageForPeriod(
+        return this.getDailyTrafficDataForPeriod(
             startOfMonth(month),
             endOfMonth(month),
         );
@@ -155,6 +155,6 @@ export class TrafficDataUsageStore implements ITrafficDataUsageStore {
     ): Promise<IStatMonthlyTrafficUsage[]> {
         const to = endOfMonth(new Date());
         const from = startOfMonth(subMonths(to, monthsBack));
-        return this.getMonthlyTrafficDataUsageForPeriod(from, to);
+        return this.getMonthlyTrafficDataForPeriod(from, to);
     }
 }

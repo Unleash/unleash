bbc882a271deaa1267f2b2028f350ca03e133942
FredrikOseberg <fredrik.no@gmail.com>
2025-01-27 10:50:33 +0100

    feat: configurable global font size

--- frontend/src/component/App.tsx
+++ frontend/src/component/App.tsx
@@ -23,6 +23,7 @@ import { LicenseBanner } from './banners/internalBanners/LicenseBanner';
 import { Demo } from './demo/Demo';
 import { LoginRedirect } from './common/LoginRedirect/LoginRedirect';
 import { SecurityBanner } from './banners/internalBanners/SecurityBanner';
+import { useUiFlag } from 'hooks/useUiFlag';
 
 const StyledContainer = styled('div')(() => ({
     '& ul': {
@@ -33,6 +34,9 @@ const StyledContainer = styled('div')(() => ({
 export const App = () => {
     const { authDetails } = useAuthDetails();
     const { refetch: refetchUiConfig } = useUiConfig();
+    const uiGlobalFontSizeVariant = useUiFlag('uiGlobalFontSize');
+
+    console.log("uiGlobalFontSizeVariant: ", uiGlobalFontSizeVariant);
 
     const { user } = useAuthUser();
     const hasFetchedAuth = Boolean(authDetails || user);
@@ -42,6 +46,35 @@ export const App = () => {
     const availableRoutes = isOss()
         ? routes.filter((route) => !route.enterprise)
         : routes;
+
+    useEffect(() => {
+        let style: HTMLStyleElement | null = null;
+        if (!uiGlobalFontSizeVariant) return;
+
+        try {
+            // Create a new style element
+            style = document.createElement('style');
+            style.type = 'text/css';
+            style.innerHTML = `
+                html {
+                    font-size: ${uiGlobalFontSizeVariant?.payload?.value}px;
+                    height: 100%;
+                    overflow: auto;
+                    overflow-y: scroll;
+                }
+            `;
+            // Append the style element to the document head
+            document.head.appendChild(style);
+        } catch (err) {
+            console.error('Error setting global font size', err);
+        }
+
+        // Cleanup function to remove the style element when the component unmounts
+        return () => {
+            if (!style) return;
+            document.head.removeChild(style);
+        };
+    }, [uiGlobalFontSizeVariant]);
 
     useEffect(() => {
         if (hasFetchedAuth && Boolean(user?.id)) {

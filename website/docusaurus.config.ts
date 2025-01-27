44f2cebb927263ddb1ca199a7e45167517d1a68d
Nuno Góis <github@nunogois.com>
2025-01-27 10:43:35 +0000

    chore: make header style consistent in smaller screens

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -83,10 +83,10 @@ export default async function createConfigAsync(): Promise<Config> {
                 indexName: 'getunleash',
             },
             navbar: {
-                style: 'dark',
                 logo: {
                     alt: 'Unleash logo',
-                    src: 'img/unleash_logo_white.svg',
+                    src: 'img/unleash_logo_dark.svg',
+                    srcDark: 'img/unleash_logo_white.svg',
                     href: 'https://www.getunleash.io',
                 },
                 items: [

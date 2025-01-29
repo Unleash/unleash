22ea159dfb9863be5d0edf6989c63f0d9fce1e5a
David Leek <david@getunleash.io>
2025-01-29 13:12:13 +0100

    feat: actually send the change request when submitted

--- frontend/src/component/feature/FeatureStrategy/FeatureStrategyMenu/FeatureStrategyMenu.tsx
+++ frontend/src/component/feature/FeatureStrategy/FeatureStrategyMenu/FeatureStrategyMenu.tsx
@@ -188,6 +188,7 @@ export const FeatureStrategyMenu = ({
                 />
             </Popover>
             <ReleasePlanAddChangeRequestDialog
+                projectId={projectId}
                 onClosing={() => setTemplateForChangeRequestDialog(undefined)}
                 featureId={featureId}
                 environmentId={environmentId}

52282a1302db58bd3737d1c3da2f82d780cc807c
Thomas Heartman <thomas@getunleash.io>
2025-01-29 11:57:48 +0100

    fix: revert to object

--- frontend/src/component/common/Chart/customHighlightPlugin.ts
+++ frontend/src/component/common/Chart/customHighlightPlugin.ts
@@ -1,37 +1,35 @@
 import type { Chart } from 'chart.js';
 
-export const customHighlightPlugin = (bottomOverflow = 34) => {
-    return {
-        id: 'customLine',
-        beforeDraw: (chart: Chart) => {
-            if (chart.tooltip?.opacity && chart.tooltip.x) {
-                const x = chart.tooltip.caretX;
-                const yAxis = chart.scales.y;
-                const ctx = chart.ctx;
-                ctx.save();
-                const gradient = ctx.createLinearGradient(
-                    x,
-                    yAxis.top,
-                    x,
-                    yAxis.bottom + 34,
-                );
-                gradient.addColorStop(0, 'rgba(129, 122, 254, 0)');
-                gradient.addColorStop(1, 'rgba(129, 122, 254, 0.12)');
-                ctx.fillStyle = gradient;
+export const customHighlightPlugin = (bottomOverflow = 34) => ({
+    id: 'customLine',
+    beforeDraw: (chart: Chart) => {
+        if (chart.tooltip?.opacity && chart.tooltip.x) {
+            const x = chart.tooltip.caretX;
+            const yAxis = chart.scales.y;
+            const ctx = chart.ctx;
+            ctx.save();
+            const gradient = ctx.createLinearGradient(
+                x,
+                yAxis.top,
+                x,
+                yAxis.bottom + 34,
+            );
+            gradient.addColorStop(0, 'rgba(129, 122, 254, 0)');
+            gradient.addColorStop(1, 'rgba(129, 122, 254, 0.12)');
+            ctx.fillStyle = gradient;
 
-                const barWidth =
-                    (chart.width / (chart.data.labels?.length ?? 1)) *
-                    (chart.options.datasets?.bar?.categoryPercentage ?? 1);
-                ctx.roundRect(
-                    x - barWidth / 2,
-                    yAxis.top,
-                    barWidth,
-                    yAxis.bottom - yAxis.top + bottomOverflow,
-                    5,
-                );
-                ctx.fill();
-                ctx.restore();
-            }
-        },
-    };
-};
+            const barWidth =
+                (chart.width / (chart.data.labels?.length ?? 1)) *
+                (chart.options.datasets?.bar?.categoryPercentage ?? 1);
+            ctx.roundRect(
+                x - barWidth / 2,
+                yAxis.top,
+                barWidth,
+                yAxis.bottom - yAxis.top + bottomOverflow,
+                5,
+            );
+            ctx.fill();
+            ctx.restore();
+        }
+    },
+});

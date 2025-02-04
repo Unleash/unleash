640db0c057932cc27600fe6a444a013530cce91e
Jaanus Sellin <sellinjaanus@gmail.com>
2025-02-04 14:37:39 +0200

    chore: pull delta controller out of OSS (#9206)
    We are moving delta controller to enterprise. This sets it up.

--- src/lib/routes/client-api/index.ts
+++ src/lib/routes/client-api/index.ts
@@ -3,16 +3,11 @@ import FeatureController from '../../features/client-feature-toggles/client-feat
 import MetricsController from '../../features/metrics/instance/metrics';
 import RegisterController from '../../features/metrics/instance/register';
 import type { IUnleashConfig, IUnleashServices } from '../../types';
-import ClientFeatureToggleDeltaController from '../../features/client-feature-toggles/delta/client-feature-toggle-delta-controller';
 
 export default class ClientApi extends Controller {
     constructor(config: IUnleashConfig, services: IUnleashServices) {
         super(config);
 
-        this.use(
-            '/delta',
-            new ClientFeatureToggleDeltaController(services, config).router,
-        );
         this.use('/features', new FeatureController(services, config).router);
         this.use('/metrics', new MetricsController(services, config).router);
         this.use('/register', new RegisterController(services, config).router);

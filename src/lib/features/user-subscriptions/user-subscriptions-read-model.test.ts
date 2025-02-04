e270fb9f6a7aef218677f38a935e1baee74e13e4
kwasniew <kwasniewski.mateusz@gmail.com>
2025-02-04 11:50:06 +0100

    feat: prductivity report only for seen users

--- src/lib/features/user-subscriptions/user-subscriptions-read-model.test.ts
+++ src/lib/features/user-subscriptions/user-subscriptions-read-model.test.ts
@@ -32,11 +32,12 @@ afterAll(async () => {
 });
 
 describe('getSubscribedUsers', () => {
-    test('returns users that did not unsubscribe', async () => {
+    test('returns users that did not unsubscribe and logged in', async () => {
         const user1 = await userStore.insert({
             email: 'user1@example.com',
             name: 'User One',
         });
+        await userStore.successfullyLogin(user1);
         const user2 = await userStore.insert({
             email: 'user2@example.com',
             name: 'User Two',
@@ -54,11 +55,10 @@ describe('getSubscribedUsers', () => {
         const subscribers =
             await userSubscriptionsReadModel.getSubscribedUsers(subscription);
 
-        expect(subscribers).toHaveLength(2);
+        expect(subscribers).toHaveLength(1);
         expect(subscribers).toEqual(
             expect.arrayContaining([
                 { email: 'user1@example.com', name: 'User One' },
-                { email: 'user3@example.com', name: 'User Three' },
             ]),
         );
     });
@@ -68,6 +68,7 @@ describe('getSubscribedUsers', () => {
             email: 'user7@example.com',
             name: 'User Seven',
         });
+        await userStore.successfullyLogin(user);
 
         let subscribers =
             await userSubscriptionsReadModel.getSubscribedUsers(subscription);

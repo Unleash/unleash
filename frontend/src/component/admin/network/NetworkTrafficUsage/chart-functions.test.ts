ff4e12de73a83c8b8563f6a05481d62eaa25550f
Thomas Heartman <thomas@getunleash.io>
2025-02-04 14:19:18 +0100

    chore: test chart conversion

---
+++ frontend/src/component/admin/network/NetworkTrafficUsage/chart-functions.test.ts
@@ -0,0 +1,124 @@
+import type { TrafficUsageDataSegmentedCombinedSchema } from 'openapi';
+import { toChartData } from './chart-functions';
+import { endpointsInfo } from './endpoint-info';
+
+describe('toChartData', () => {
+    const dataPoint = (period: string, count: number) => ({
+        period,
+        trafficTypes: [{ count, group: 'successful-requests' }],
+    });
+
+    const fromEndpointInfo = (endpoint: keyof typeof endpointsInfo) => {
+        const info = endpointsInfo[endpoint];
+        return {
+            backgroundColor: info.color,
+            hoverBackgroundColor: info.color,
+            label: info.label,
+        };
+    };
+
+    test('monthly data conversion', () => {
+        const input: TrafficUsageDataSegmentedCombinedSchema = {
+            grouping: 'monthly',
+            dateRange: {
+                from: '2025-01-01',
+                to: '2025-06-31',
+            },
+            apiData: [
+                {
+                    apiPath: '/api/admin',
+                    dataPoints: [
+                        dataPoint('2025-06', 5),
+                        dataPoint('2025-05', 4),
+                        dataPoint('2025-02', 6),
+                        dataPoint('2025-04', 2),
+                    ],
+                },
+                {
+                    apiPath: '/api/client',
+                    dataPoints: [
+                        dataPoint('2025-06', 10),
+                        dataPoint('2025-01', 7),
+                        dataPoint('2025-03', 11),
+                        dataPoint('2025-04', 13),
+                    ],
+                },
+            ],
+        };
+
+        const expectedOutput = {
+            datasets: [
+                {
+                    data: [0, 6, 0, 2, 4, 5],
+                    ...fromEndpointInfo('/api/admin'),
+                },
+                {
+                    data: [7, 0, 11, 13, 0, 10],
+                    ...fromEndpointInfo('/api/client'),
+                },
+            ],
+            labels: [
+                '2025-01',
+                '2025-02',
+                '2025-03',
+                '2025-04',
+                '2025-05',
+                'Current month',
+            ],
+        };
+
+        expect(toChartData(input)).toMatchObject(expectedOutput);
+    });
+
+    test('daily data conversion', () => {
+        const input: TrafficUsageDataSegmentedCombinedSchema = {
+            grouping: 'daily',
+            dateRange: {
+                from: '2025-01-01',
+                to: '2025-01-31',
+            },
+            apiData: [
+                {
+                    apiPath: '/api/admin',
+                    dataPoints: [
+                        dataPoint('2025-01-01', 5),
+                        dataPoint('2025-01-15', 4),
+                        dataPoint('2025-01-14', 6),
+                        dataPoint('2025-01-06', 2),
+                    ],
+                },
+                {
+                    apiPath: '/api/client',
+                    dataPoints: [
+                        dataPoint('2025-01-02', 2),
+                        dataPoint('2025-01-17', 6),
+                        dataPoint('2025-01-19', 4),
+                        dataPoint('2025-01-06', 8),
+                    ],
+                },
+            ],
+        };
+
+        const expectedOutput = {
+            datasets: [
+                {
+                    data: [
+                        5, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 6, 4, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    ],
+                    ...fromEndpointInfo('/api/admin'),
+                },
+                {
+                    data: [
+                        0, 2, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 4,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    ],
+                    ...fromEndpointInfo('/api/client'),
+                },
+            ],
+            labels: Array.from({ length: 31 }).map((_, index) => index + 1),
+        };
+
+        expect(toChartData(input)).toMatchObject(expectedOutput);
+    });
+});

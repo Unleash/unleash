54e4fd21908cef6de5a902d5ff38413511a0ea70
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-02-06 14:54:45 +0100

    chore: remove lifecycle v2 flag (#9224)

--- frontend/src/component/common/FeatureLifecycle/FeatureLifecycleStageIcon.tsx
+++ frontend/src/component/common/FeatureLifecycle/FeatureLifecycleStageIcon.tsx
@@ -1,47 +1,25 @@
 import type { FC } from 'react';
-import { ReactComponent as InitialStageIcon } from 'assets/icons/stage-initial.svg';
-import { ReactComponent as PreLiveStageIcon } from 'assets/icons/stage-pre-live.svg';
-import { ReactComponent as LiveStageIcon } from 'assets/icons/stage-live.svg';
-import { ReactComponent as CompletedStageIcon } from 'assets/icons/stage-completed.svg';
-import { ReactComponent as ArchivedStageIcon } from 'assets/icons/stage-archived.svg';
-import { ReactComponent as Stage1 } from 'assets/icons/lifecycle/stage-1.svg';
-import { ReactComponent as Stage2 } from 'assets/icons/lifecycle/stage-2.svg';
-import { ReactComponent as Stage3 } from 'assets/icons/lifecycle/stage-3.svg';
-import { ReactComponent as Stage4 } from 'assets/icons/lifecycle/stage-4.svg';
-import { ReactComponent as Stage5 } from 'assets/icons/lifecycle/stage-5.svg';
+import { ReactComponent as CreatedIcon } from 'assets/icons/lifecycle/stage-created.svg';
+import { ReactComponent as PreLiveIcon } from 'assets/icons/lifecycle/stage-prelive.svg';
+import { ReactComponent as LiveIcon } from 'assets/icons/lifecycle/stage-live.svg';
+import { ReactComponent as CompletedIcon } from 'assets/icons/lifecycle/stage-completed.svg';
+import { ReactComponent as ArchivedIcon } from 'assets/icons/lifecycle/stage-archived.svg';
 import type { LifecycleStage } from '../../feature/FeatureView/FeatureOverview/FeatureLifecycle/LifecycleStage';
-import { useUiFlag } from 'hooks/useUiFlag';
 
 export const FeatureLifecycleStageIcon: FC<{
     stage: Pick<LifecycleStage, 'name'>;
 }> = ({ stage, ...props }) => {
-    const newIcons = useUiFlag('lifecycleImprovements');
-
     if (stage.name === 'archived') {
-        return newIcons ? (
-            <Stage5 {...props} />
-        ) : (
-            <ArchivedStageIcon {...props} />
-        );
-    } else if (stage.name === 'pre-live') {
-        return newIcons ? (
-            <Stage2 {...props} />
-        ) : (
-            <PreLiveStageIcon {...props} />
-        );
-    } else if (stage.name === 'live') {
-        return newIcons ? <Stage3 {...props} /> : <LiveStageIcon {...props} />;
-    } else if (stage.name === 'completed') {
-        return newIcons ? (
-            <Stage4 {...props} />
-        ) : (
-            <CompletedStageIcon {...props} />
-        );
-    } else {
-        return newIcons ? (
-            <Stage1 {...props} />
-        ) : (
-            <InitialStageIcon {...props} />
-        );
+        return <ArchivedIcon {...props} />;
     }
+    if (stage.name === 'pre-live') {
+        return <PreLiveIcon {...props} />;
+    }
+    if (stage.name === 'live') {
+        return <LiveIcon {...props} />;
+    }
+    if (stage.name === 'completed') {
+        return <CompletedIcon {...props} />;
+    }
+    return <CreatedIcon {...props} />;
 };

e2571065fcf45883e8bbb26b54c367b90fb558ff
Christopher Kolstad <chriswk@getunleash.io>
2025-02-11 11:44:36 +0100

    updated to not have reported at as PK part

--- src/migrations/20250210130408-add-edge-observability-tables.js
+++ src/migrations/20250210130408-add-edge-observability-tables.js
@@ -1,7 +1,7 @@
-exports.up = function(db, cb) {
-  return db.runSql(`
+exports.up = (db, cb) => {
+  db.runSql(`
     CREATE TABLE stat_edge_observability (
-        instance_id TEXT NOT NULL,
+        instance_id TEXT NOT NULL PRIMARY KEY,
         reported_at TIMESTAMP WITH TIME ZONE NOT NULL,
         app_name TEXT,
         started TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -10,14 +10,13 @@ exports.up = function(db, cb) {
         cpu_usage NUMERIC DEFAULT 0.0,
         memory_usage INTEGER DEFAULT 0,
         connected_streaming_clients INTEGER NOT NULL DEFAULT 0,
-        connected_via TEXT,
-        PRIMARY KEY (instance_id, reported_at)
+        connected_via TEXT
     );
     CREATE INDEX edge_observability_connected_via_idx ON stat_edge_observability(connected_via) WHERE connected_via IS NOT NULL;
     CREATE INDEX edge_observability_app_name_idx ON stat_edge_observability(app_name) WHERE app_name IS NOT NULL;
   `, cb);
 };
 
-exports.down = function(db, cb) {
-  return db.runSql(`DROP TABLE IF EXISTS stat_edge_observability;`, cb);
+exports.down = (db, cb) => {
+  db.runSql(`DROP TABLE IF EXISTS stat_edge_observability;`, cb);
 };

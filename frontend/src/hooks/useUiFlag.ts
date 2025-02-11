8bec94da080f926a95f99b08efbf067df9278482
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-02-11 13:48:17 +0100

    fix(1-3363): frontend flag type should not return truthy for variant

--- frontend/src/hooks/useUiFlag.ts
+++ frontend/src/hooks/useUiFlag.ts
@@ -2,8 +2,11 @@ import useUiConfig from 'hooks/api/getters/useUiConfig/useUiConfig';
 
 type flags = ReturnType<typeof useUiConfig>['uiConfig']['flags'];
 
-export const useUiFlag = <K extends keyof flags>(flag: K) => {
+export const useUiFlag = <K extends keyof flags>(flag: K): boolean => {
     const { uiConfig } = useUiConfig();
+    if (typeof uiConfig?.flags?.[flag] !== 'boolean') {
+        console.error(`Flag ${flag} is undefined or a variant.`);
+    }
 
-    return uiConfig?.flags?.[flag] || false;
+    return Boolean(uiConfig?.flags?.[flag]) || false;
 };

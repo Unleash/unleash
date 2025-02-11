60d3df3282f25500a3ed87e9e353bdbf0aaf2291
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-02-11 14:03:42 +0100

    fix: useUiFlag type

--- frontend/src/hooks/useUiFlag.ts
+++ frontend/src/hooks/useUiFlag.ts
@@ -4,9 +4,12 @@ type flags = ReturnType<typeof useUiConfig>['uiConfig']['flags'];
 
 export const useUiFlag = <K extends keyof flags>(flag: K): boolean => {
     const { uiConfig } = useUiConfig();
-    if (typeof uiConfig?.flags?.[flag] !== 'boolean') {
-        console.error(`Flag ${flag} is undefined or a variant.`);
+    const value = uiConfig?.flags?.[flag];
+    if (typeof value === 'boolean') {
+        return value;
+    } else if (typeof value !== 'undefined') {
+        console.error(`Flag ${flag} does not return a boolean.`);
     }
 
-    return Boolean(uiConfig?.flags?.[flag]) || false;
+    return false;
 };

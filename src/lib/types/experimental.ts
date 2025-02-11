2afc1fb08ee1d30b1b8084969fa8046d9b0b28e5
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-02-11 14:11:42 +0100

    fix: ts check

--- src/lib/types/experimental.ts
+++ src/lib/types/experimental.ts
@@ -310,17 +310,10 @@ const flags: IFlags = {
         process.env.UNLEASH_EXPERIMENTAL_DATA_USAGE_MULTI_MONTH_VIEW,
         false,
     ),
-    uiGlobalFontSize: {
-        name: 'uiGlobalFontSize',
-        enabled: parseEnvVarBoolean(
-            process.env.EXPERIMENTAL_UI_GLOBAL_FONT_SIZE_NAME,
-            false,
-        ),
-        payload: {
-            type: PayloadType.JSON,
-            value: '14',
-        },
-    },
+    uiGlobalFontSize: parseEnvVarBoolean(
+        process.env.EXPERIMENTAL_UI_GLOBAL_FONT_SIZE_NAME,
+        false,
+    ),
 };
 
 export const defaultExperimentalOptions: IExperimentalOptions = {

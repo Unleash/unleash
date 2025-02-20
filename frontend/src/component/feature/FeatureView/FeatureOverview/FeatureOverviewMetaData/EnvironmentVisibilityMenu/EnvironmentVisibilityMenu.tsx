095a0e8273f7fc2eafd3ab850dfd97924613c8e7
Thomas Heartman <thomas@getunleash.io>
2025-02-20 15:02:35 +0100

    feat(1-3385): first pass at making env selector filterable

--- frontend/src/component/feature/FeatureView/FeatureOverview/FeatureOverviewMetaData/EnvironmentVisibilityMenu/EnvironmentVisibilityMenu.tsx
+++ frontend/src/component/feature/FeatureView/FeatureOverview/FeatureOverviewMetaData/EnvironmentVisibilityMenu/EnvironmentVisibilityMenu.tsx
@@ -1,8 +1,9 @@
-import { Button, Checkbox, Menu, MenuItem, styled } from '@mui/material';
-import { useState, type FC } from 'react';
+import { Button, Popover, styled } from '@mui/material';
+import { useMemo, useState, type FC } from 'react';
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import { DropdownList } from 'component/common/DialogFormTemplate/ConfigButtons/DropdownList';
 
 type EnvironmentVisibilityMenuProps = {
     environments: Array<{ name: string }>;
@@ -19,6 +20,13 @@ const StyledContainer = styled('div')(({ theme }) => ({
     paddingTop: theme.spacing(4),
 }));
 
+export const StyledPopover = styled(Popover)(({ theme }) => ({
+    '& .MuiPaper-root': {
+        borderRadius: `${theme.shape.borderRadiusMedium}px`,
+        paddingTop: theme.spacing(2),
+    },
+}));
+
 export const EnvironmentVisibilityMenu: FC<EnvironmentVisibilityMenuProps> = ({
     environments,
     hiddenEnvironments,
@@ -33,6 +41,25 @@ export const EnvironmentVisibilityMenu: FC<EnvironmentVisibilityMenuProps> = ({
         setAnchorEl(null);
     };
 
+    const allEnvironments = useMemo(
+        () => environments.map((environment) => environment.name),
+        [environments],
+    );
+
+    const selectedOptions = useMemo(
+        () =>
+            new Set(
+                allEnvironments.filter(
+                    (environment) => !hiddenEnvironments.includes(environment),
+                ),
+            ),
+        [environments, hiddenEnvironments],
+    );
+
+    const handleToggle = (value: string) => {
+        onChange(value);
+    };
+
     return (
         <StyledContainer>
             <Button
@@ -40,30 +67,43 @@ export const EnvironmentVisibilityMenu: FC<EnvironmentVisibilityMenuProps> = ({
                 endIcon={isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
                 variant='outlined'
                 id={buttonId}
-                aria-controls={isOpen ? menuId : undefined}
+                aria-controls={menuId}
                 aria-haspopup='true'
                 aria-expanded={isOpen ? 'true' : undefined}
                 data-loading
             >
                 Hide/show environments
             </Button>
-            <Menu
+
+            <StyledPopover
                 id={menuId}
+                open={Boolean(anchorEl)}
                 anchorEl={anchorEl}
-                open={isOpen}
                 onClose={handleClose}
-                MenuListProps={{ 'aria-labelledby': buttonId }}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
             >
-                {environments.map(({ name }) => (
-                    <MenuItem key={name} onClick={() => onChange(name)}>
-                        <Checkbox
-                            onChange={() => onChange(name)}
-                            checked={!hiddenEnvironments?.includes(name)}
-                        />
-                        {name}
-                    </MenuItem>
-                ))}
-            </Menu>
+                <DropdownList
+                    multiselect={{
+                        selectedOptions,
+                    }}
+                    onChange={handleToggle}
+                    options={allEnvironments.map((env) => ({
+                        label: env,
+                        value: env,
+                    }))}
+                    search={{
+                        label: '',
+                        placeholder: 'Select environment',
+                    }}
+                />
+            </StyledPopover>
         </StyledContainer>
     );
 };

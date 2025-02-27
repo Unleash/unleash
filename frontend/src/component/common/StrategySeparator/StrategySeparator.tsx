c6bc8651768cd03c03af34a215032b79e43ac19b
Thomas Heartman <thomas@getunleash.io>
2025-02-27 15:11:37 +0100

    chore(1-3427): only have styled or

--- frontend/src/component/common/StrategySeparator/StrategySeparator.tsx
+++ frontend/src/component/common/StrategySeparator/StrategySeparator.tsx
@@ -1,24 +1,18 @@
-import { styled, useTheme } from '@mui/material';
+import { styled } from '@mui/material';
 
 interface IStrategySeparatorProps {
     text: 'AND' | 'OR';
 }
 
-const StyledAnd = styled('div')(({ theme }) => ({
+const StyledOr = styled('div')(({ theme }) => ({
     padding: theme.spacing(0.75, 1),
-    color: theme.palette.text.primary,
     fontSize: theme.fontSizes.smallerBody,
-    backgroundColor: theme.palette.background.elevation2,
     position: 'absolute',
     zIndex: theme.zIndex.fab,
     top: 0,
-    left: theme.spacing(2),
     transform: 'translateY(-50%)',
     lineHeight: 1,
     borderRadius: theme.shape.borderRadiusLarge,
-}));
-
-const StyledOr = styled(StyledAnd)(({ theme }) => ({
     fontWeight: 'bold',
     backgroundColor: theme.palette.background.alternative,
     color: theme.palette.primary.contrastText,
@@ -26,12 +20,5 @@ const StyledOr = styled(StyledAnd)(({ theme }) => ({
 }));
 
 export const StrategySeparator = ({ text }: IStrategySeparatorProps) => {
-    const theme = useTheme();
-    return text === 'AND' ? (
-        <StyledAnd>{text}</StyledAnd>
-    ) : (
-        <>
-            <StyledOr>{text}</StyledOr>
-        </>
-    );
+    return <StyledOr>{text}</StyledOr>;
 };

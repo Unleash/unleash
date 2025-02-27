e0f0108c196dc4a0541d0b3786c94ab514252533
Jaanus Sellin <sellinjaanus@gmail.com>
2025-02-27 09:11:00 +0200

    feat: SQL performance optimization to count instances (#9369)

--- src/lib/db/client-instance-store.ts
+++ src/lib/db/client-instance-store.ts
@@ -253,17 +253,26 @@ export default class ClientInstanceStore implements IClientInstanceStore {
     }
 
     async getDistinctApplicationsCount(daysBefore?: number): Promise<number> {
-        let query = this.db.from(TABLE);
-        if (daysBefore) {
-            query = query.where(
-                'last_seen',
-                '>',
-                subDays(new Date(), daysBefore),
-            );
-        }
-        return query
-            .countDistinct('app_name')
-            .then((res) => Number(res[0].count));
+        const query = this.db
+            .from((qb) =>
+                qb
+                    .select('app_name')
+                    .from(TABLE)
+                    .modify((qb) => {
+                        if (daysBefore) {
+                            qb.where(
+                                'last_seen',
+                                '>',
+                                subDays(new Date(), daysBefore),
+                            );
+                        }
+                    })
+                    .groupBy('app_name')
+                    .as('subquery'),
+            )
+            .count('* as count');
+
+        return query.then((res) => Number(res[0].count));
     }
 
     async deleteForApplication(appName: string): Promise<void> {

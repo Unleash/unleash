aa9f693c276491fa22446eb8882b618449a0def1
Thomas Heartman <thomas@getunleash.io>
2025-03-05 08:47:36 +0100

    Begin flag removal

--- frontend/src/component/admin/billing/BillingDashboard/BillingPlan/useOverageCost.ts
+++ frontend/src/component/admin/billing/BillingDashboard/BillingPlan/useOverageCost.ts
@@ -1,24 +1,14 @@
 import { endOfMonth, format, startOfMonth } from 'date-fns';
-import {
-    useInstanceTrafficMetrics,
-    useTrafficSearch,
-} from 'hooks/api/getters/useInstanceTrafficMetrics/useInstanceTrafficMetrics';
+import { useTrafficSearch } from 'hooks/api/getters/useInstanceTrafficMetrics/useInstanceTrafficMetrics';
 import { useMemo } from 'react';
 import {
     calculateOverageCost,
     calculateTotalUsage,
 } from 'utils/traffic-calculations';
 import { BILLING_TRAFFIC_BUNDLE_PRICE } from './BillingPlan';
-import { useUiFlag } from 'hooks/useUiFlag';
-import { useTrafficDataEstimation } from 'hooks/useTrafficData';
 
-export const useOverageCost = (includedTraffic: number) => {
-    if (useUiFlag('dataUsageMultiMonthView')) {
-        return useNewOverageCostCalculation(includedTraffic);
-    } else {
-        return useOldOverageCostCalculation(includedTraffic);
-    }
-};
+export const useOverageCost = (includedTraffic: number) =>
+    useNewOverageCostCalculation(includedTraffic);
 
 const useNewOverageCostCalculation = (includedTraffic: number) => {
     if (!includedTraffic) {
@@ -43,37 +33,6 @@ const useNewOverageCostCalculation = (includedTraffic: number) => {
             BILLING_TRAFFIC_BUNDLE_PRICE,
         );
     }, [includedTraffic, JSON.stringify(result)]);
-
-    return overageCost;
-};
-
-const useOldOverageCostCalculation = (includedTraffic: number) => {
-    const {
-        currentPeriod,
-        toChartData,
-        toTrafficUsageSum,
-        endpointsInfo,
-        getDayLabels,
-    } = useTrafficDataEstimation();
-
-    const traffic = useInstanceTrafficMetrics(currentPeriod.key);
-
-    const overageCost = useMemo(() => {
-        if (!includedTraffic) {
-            return 0;
-        }
-        const trafficData = toChartData(
-            getDayLabels(currentPeriod.dayCount),
-            traffic,
-            endpointsInfo,
-        );
-        const totalTraffic = toTrafficUsageSum(trafficData);
-        return calculateOverageCost(
-            totalTraffic,
-            includedTraffic,
-            BILLING_TRAFFIC_BUNDLE_PRICE,
-        );
-    }, [includedTraffic, traffic, currentPeriod, endpointsInfo]);
 
     return overageCost;
 };

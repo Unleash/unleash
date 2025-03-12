73eb30ab2ef5ef35998c0feb8df6a1519a6ae076
sjaanus <sellinjaanus@gmail.com>
2025-03-12 16:29:44 +0200

    feat: frontend consumption table

--- src/migrations/20250312162155-connection-count-consumption-table.js
+++ src/migrations/20250312162155-connection-count-consumption-table.js
@@ -2,7 +2,7 @@
 exports.up = (db, callback) => {
     db.runSql(
         `
-        CREATE TABLE IF NOT EXISTS frontend_consumption_count(
+        CREATE TABLE IF NOT EXISTS request_count_consumption(
             day DATE NOT NULL,
             metered_group TEXT NOT NULL,
             requests BIGINT NOT NULL DEFAULT 0,
@@ -16,7 +16,7 @@ exports.up = (db, callback) => {
 exports.down = (db, callback) => {
     db.runSql(
         `
-        DROP TABLE IF EXISTS frontend_consumption_count;
+        DROP TABLE IF EXISTS request_count_consumption;
         `,
         callback,
     );

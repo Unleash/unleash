dadda7b64839f1c9431c87b83d25f9f78dc04fd4
Thomas Heartman <thomas@getunleash.io>
2025-03-13 15:44:10 +0100

    fix: Data usage graphs don't work in UTC-n time zones (#9530)
    Fixes a number of issues that would surface in UTC-n (where n > 1)
    timezones. I've not found a way to check this with tests (and it looks
    like [we weren't able to last time
    either](https://github.com/Unleash/unleash/pull/9110/files#r1919746328)),
    so all the testing's been done manually by adjusting my system time and
    zone. (From what I understand, you can't generate a Date with a specific
    TZ offset in JS: it's only utc or local time)
    
    Resolved:
    - [x] Selecting "Jan" in the dropdown results in the selection being
    "December" (off by one in the selector)
    
    - [x] Selecting a month view only gives you one data point (and it's
    probably empty). Wrong date parsing on the way out resulted in sending
    `{ from: "2025-02-28", to: "2025-02-28"}` instead of `{ from:
    "2025-03-01", to: "2025-03-31"}`
    
    - [x] The dates we create when making "daysRec" need to be adjusted.
    They showed the wrong month, so the dates were off.
    
    - [x] Make sure the labels are correct when hovering over. Again: we
    used the wrong month for generating these.
    
    - [x] The available months are wrong. Incorrect month parsing again.
    
    - [x] The request summary month is wrong. You guessed it: incorrect
    month parsing

--- frontend/src/component/admin/network/NetworkTrafficUsage/dates.ts
+++ frontend/src/component/admin/network/NetworkTrafficUsage/dates.ts
@@ -1,4 +1,4 @@
-import { format, getDaysInMonth } from 'date-fns';
+import { format, getDaysInMonth, parse } from 'date-fns';
 
 export const currentDate = new Date();
 
@@ -11,3 +11,13 @@ export const daysInCurrentMonth = getDaysInMonth(currentDate);
 
 export const formatMonth = (date: Date) => format(date, 'yyyy-MM');
 export const formatDay = (date: Date) => format(date, 'yyyy-MM-dd');
+
+export const parseMonthString = (month: string): Date => {
+    // parses a month into a Date starting on the first day of the month, regardless of the current time zone (e.g. works in Norway and Brazil)
+    return parse(month, 'yyyy-MM', new Date());
+};
+
+export const parseDateString = (month: string): Date => {
+    // parses a date string into a Date, regardless of the current time zone (e.g. works in Norway and Brazil)
+    return parse(month, 'yyyy-MM-dd', new Date());
+};

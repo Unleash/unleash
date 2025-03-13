4a49b314ebf6ac7d43c7b4a3ef4189916d9540a3
David Leek <david@getunleash.io>
2025-03-13 13:57:25 +0100

    chore: cleaning up frontend mobile/advanced routes

--- frontend/src/component/layout/MainLayout/NavigationSidebar/useRoutes.ts
+++ frontend/src/component/layout/MainLayout/NavigationSidebar/useRoutes.ts
@@ -1,18 +1,15 @@
 import useUiConfig from 'hooks/api/getters/useUiConfig/useUiConfig';
-import { getCondensedRoutes, getRoutes } from 'component/menu/routes';
+import { getNavRoutes } from 'component/menu/routes';
 import { useAdminRoutes } from 'component/admin/useAdminRoutes';
 import { filterByConfig, mapRouteLink } from 'component/common/util';
 
 export const useRoutes = () => {
     const { uiConfig } = useUiConfig();
-    const routes = getRoutes();
+    const routes = getNavRoutes();
     const adminRoutes = useAdminRoutes();
 
     const filteredMainRoutes = {
-        mainNavRoutes: getCondensedRoutes(routes.mainNavRoutes)
-            .filter(filterByConfig(uiConfig))
-            .map(mapRouteLink),
-        mobileRoutes: getCondensedRoutes(routes.mobileRoutes)
+        mainNavRoutes: routes
             .filter(filterByConfig(uiConfig))
             .map(mapRouteLink),
         adminRoutes,

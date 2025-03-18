86c9d08d74d98ef263ef98868bcbf2d94efe63e2
FredrikOseberg <fredrik.no@gmail.com>
2025-03-18 13:59:05 +0100

    fix: rm comment

--- src/migrations/20250318091635-add-tag-type-color.js
+++ src/migrations/20250318091635-add-tag-type-color.js
@@ -4,7 +4,6 @@ exports.up = function (db, cb) {
         ALTER TABLE tag_types
         ADD COLUMN IF NOT EXISTS color VARCHAR(10);
         
-        -- Backfill existing tag types with the default color #FFFFFF
         UPDATE tag_types
         SET color = '#FFFFFF'
         WHERE color IS NULL;

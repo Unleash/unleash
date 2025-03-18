3dfb23ade62c42dcc2e6d2ee8be9113dfab1c777
FredrikOseberg <fredrik.no@gmail.com>
2025-03-18 13:43:51 +0100

    fix: syntax

--- src/migrations/20250318091635-add-tag-type-color.js
+++ src/migrations/20250318091635-add-tag-type-color.js
@@ -1,19 +1,3 @@
-'use strict';
-
-var dbm;
-var type;
-var seed;
-
-/**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
-};
-
 exports.up = function (db, cb) {
     db.runSql(
         `
@@ -37,8 +21,4 @@ exports.down = function (db, cb) {
         `,
         cb,
     );
-};
-
-exports._meta = {
-  "version": 1
 };

c005cf3b1c1c19e370665c0d7e925cacf7f5126d
Thomas Heartman <thomas@getunleash.io>
2025-03-18 13:31:27 +0100

    Use segment item

--- frontend/src/component/playground/Playground/PlaygroundResultsTable/FeatureResultInfoPopoverCell/FeatureStrategyList/StrategyList/StrategyItem/StrategyExecution/SegmentExecution/SegmentExecution.tsx
+++ frontend/src/component/playground/Playground/PlaygroundResultsTable/FeatureResultInfoPopoverCell/FeatureStrategyList/StrategyList/StrategyItem/StrategyExecution/SegmentExecution/SegmentExecution.tsx
@@ -1,23 +1,14 @@
 import type { FC } from 'react';
 import type { PlaygroundSegmentSchema, PlaygroundRequestSchema } from 'openapi';
-import { ConstraintExecution } from '../ConstraintExecution/LegacyConstraintExecution';
-import CancelOutlined from '@mui/icons-material/CancelOutlined';
-import { styled, Typography } from '@mui/material';
-import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
+import { ConstraintExecution } from '../ConstraintExecution/ConstraintExecution';
 import { SegmentItem } from 'component/common/SegmentItem/SegmentItem';
+import { objectId } from 'utils/objectId';
+import { ConstraintsList } from 'component/common/ConstraintsList/ConstraintsList';
 
 type SegmentExecutionProps = {
     segment: PlaygroundSegmentSchema;
     input?: PlaygroundRequestSchema;
 };
-
-const SegmentResultTextWrapper = styled('div')(({ theme }) => ({
-    color: theme.palette.error.main,
-    display: 'inline-flex',
-    justifyContent: 'center',
-    marginLeft: 'auto',
-    gap: theme.spacing(1),
-}));
 
 export const SegmentExecution: FC<SegmentExecutionProps> = ({
     segment,
@@ -27,26 +18,15 @@ export const SegmentExecution: FC<SegmentExecutionProps> = ({
         <SegmentItem
             segment={segment}
             constraintList={
-                <ConstraintExecution
-                    constraints={segment.constraints}
-                    input={input}
-                />
-            }
-            headerContent={
-                <ConditionallyRender
-                    condition={!segment.result}
-                    show={
-                        <SegmentResultTextWrapper>
-                            <Typography variant={'subtitle2'} sx={{ pt: 0.25 }}>
-                                segment is false
-                            </Typography>
-                            <span>
-                                <CancelOutlined />
-                            </span>
-                        </SegmentResultTextWrapper>
-                    }
-                    elseShow={undefined}
-                />
+                <ConstraintsList>
+                    {segment.constraints.map((constraint) => (
+                        <ConstraintExecution
+                            key={objectId(constraint)}
+                            constraint={constraint}
+                            input={input}
+                        />
+                    ))}
+                </ConstraintsList>
             }
             isExpanded
         />

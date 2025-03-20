90eed05296aab0e5fe5088761fbfbdead2b40d36
Thomas Heartman <thomas@getunleash.io>
2025-03-20 13:19:16 +0100

    Chore(1-3520)/playground disabled badges (#9583)
    Gives a small update in how we deal with unevaluated and disabled
    strategies in the new playground design:
    
    - "Unevaluated" badges go from yellow warning to blue info and their
    text changed to "Not evaluated"
    - Don't show "Not evaluated" badges on strategies that are disabled.
    
    To avoid this change affecting the current playground setup, I
    duplicated the old resultschip into a legacy file and changed the
    existing impl. To avoid updating all other files that use that chip
    (it's all over the playground) and checking flags or creating duplicates
    there, I decided to do a quick check at the top of the legacy file and
    use the new file if the flag is on.
    
    In doing so, I've also simplified the actual chip file and have more or
    less cut the total line count in it in two 😄
    
    
    ![image](https://github.com/user-attachments/assets/44e38a8e-4faa-440b-82ed-9ee377160922)

---
+++ frontend/src/component/playground/Playground/PlaygroundResultsTable/PlaygroundResultChip/LegacyPlaygroundResultChip.tsx
@@ -0,0 +1,96 @@
+// deprecated; remove with 'flagOverviewRedesign' flag
+import type { VFC } from 'react';
+import { useTheme } from '@mui/material';
+import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
+import { ReactComponent as FeatureEnabledIcon } from 'assets/icons/isenabled-true.svg';
+import { ReactComponent as FeatureDisabledIcon } from 'assets/icons/isenabled-false.svg';
+import WarningOutlined from '@mui/icons-material/WarningOutlined';
+import { Badge } from 'component/common/Badge/Badge';
+import { useUiFlag } from 'hooks/useUiFlag';
+import { PlaygroundResultChip as NewPlaygroundResultChip } from './PlaygroundResultChip';
+
+interface IResultChipProps {
+    enabled: boolean | 'unevaluated' | 'unknown';
+    label: string;
+    // Result icon - defaults to true
+    showIcon?: boolean;
+    tabindex?: number;
+}
+
+export const PlaygroundResultChip: VFC<IResultChipProps> = ({
+    enabled,
+    label,
+    showIcon = true,
+    tabindex,
+}) => {
+    const theme = useTheme();
+    const flagOverviewRedesign = useUiFlag('flagOverviewRedesign');
+    if (flagOverviewRedesign) {
+        return (
+            <NewPlaygroundResultChip
+                enabled={enabled}
+                label={label}
+                showIcon={showIcon}
+            />
+        );
+    }
+    const icon = (
+        <ConditionallyRender
+            condition={enabled === 'unknown' || enabled === 'unevaluated'}
+            show={<WarningOutlined color={'warning'} fontSize='inherit' />}
+            elseShow={
+                <ConditionallyRender
+                    condition={typeof enabled === 'boolean' && Boolean(enabled)}
+                    show={
+                        <FeatureEnabledIcon
+                            aria-hidden
+                            color={theme.palette.success.main}
+                            strokeWidth='0.25'
+                        />
+                    }
+                    elseShow={
+                        <FeatureDisabledIcon
+                            aria-hidden
+                            color={theme.palette.error.main}
+                            strokeWidth='0.25'
+                        />
+                    }
+                />
+            }
+        />
+    );
+
+    return (
+        <ConditionallyRender
+            condition={enabled === 'unknown' || enabled === 'unevaluated'}
+            show={
+                <Badge icon={showIcon ? icon : undefined} color='warning'>
+                    {label}
+                </Badge>
+            }
+            elseShow={
+                <ConditionallyRender
+                    condition={typeof enabled === 'boolean' && Boolean(enabled)}
+                    show={
+                        <Badge
+                            tabIndex={tabindex}
+                            color='success'
+                            icon={showIcon ? icon : undefined}
+                        >
+                            {label}
+                        </Badge>
+                    }
+                    elseShow={
+                        <Badge
+                            color='error'
+                            icon={showIcon ? icon : undefined}
+                            tabIndex={tabindex}
+                        >
+                            {label}
+                        </Badge>
+                    }
+                />
+            }
+        />
+    );
+};

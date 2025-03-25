380d2c2c5d1958456f4c35eea4b8c628b82050f6
Gastón Fournier <gaston@getunleash.io>
2025-03-25 17:01:04 +0100

    feat: release template feedback module (#9614)
    ## About the changes
    
    Adds a release template feedback module to release templates: 
    
    ![image](https://github.com/user-attachments/assets/848d386b-0e20-43d0-b113-51e1e26c5a13)
    
    It uses the Card component by defining a new variant of it.
    
    ---------
    
    Co-authored-by: Nuno Góis <github@nunogois.com>

---
+++ frontend/src/component/releases/ReleaseManagement/ReleasesFeedback.tsx
@@ -0,0 +1,38 @@
+import { styled } from '@mui/material';
+import { Card } from 'component/common/Card/Card';
+import { Link } from 'react-router-dom';
+
+const StyledCardLink = styled(Link)(({ theme }) => ({
+    textDecoration: 'none',
+    fontWeight: theme.typography.fontWeightBold,
+    color: theme.palette.links,
+    '&:hover, &:focus': {
+        textDecoration: 'underline',
+    },
+}));
+
+const feedbackLink =
+    'https://docs.google.com/forms/d/1ElbScYxbAhFcjQWgRinifoymYHeuXzqIoQXfpUVYGR8/preview';
+
+export const ReleasesFeedback: React.FC<{
+    title: string;
+    children: React.ReactNode;
+}> = ({ title, children }: { title: string; children?: React.ReactNode }) => {
+    return (
+        <Card
+            cardVariant='secondary'
+            title={title}
+            footer={
+                <StyledCardLink
+                    to={feedbackLink}
+                    target='_blank'
+                    rel='noreferrer'
+                >
+                    Give feedback
+                </StyledCardLink>
+            }
+        >
+            {children}
+        </Card>
+    );
+};

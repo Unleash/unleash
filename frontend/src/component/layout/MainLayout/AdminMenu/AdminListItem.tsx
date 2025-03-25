82739e71497f27abe91a939e8d699b4840bbc7cf
David Leek <david@getunleash.io>
2025-03-25 09:13:44 +0100

    feat: first iteration of admin menu

---
+++ frontend/src/component/layout/MainLayout/AdminMenu/AdminListItem.tsx
@@ -0,0 +1,166 @@
+import {
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Typography,
+    styled,
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import type { FC, ReactNode } from 'react';
+import { Link } from 'react-router-dom';
+import type { Theme } from '@mui/material/styles/createTheme';
+
+const listItemButtonStyle = (theme: Theme) => ({
+    borderRadius: theme.spacing(0.5),
+    borderLeft: `${theme.spacing(0.5)} solid transparent`,
+    m: 0,
+    '&.Mui-selected': {
+        borderLeft: `${theme.spacing(0.5)} solid ${theme.palette.primary.main}`,
+    },
+    minHeight: '0px',
+    '.MuiAccordionSummary-content': { margin: 0 },
+    '&>.MuiAccordionSummary-content.MuiAccordionSummary-content': {
+        margin: '0',
+        alignItems: 'center',
+        padding: theme.spacing(0.5, 0),
+    },
+});
+
+const subListItemButtonStyle = (theme: Theme) => ({
+    paddingLeft: theme.spacing(4),
+    borderRadius: theme.spacing(0.5),
+    borderLeft: `${theme.spacing(0.5)} solid transparent`,
+    m: 0,
+    '&.Mui-selected': {
+        borderLeft: `${theme.spacing(0.5)} solid ${theme.palette.primary.main}`,
+    },
+});
+
+const CappedText = styled(Typography)({
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    width: '100%',
+});
+
+const StyledListItemIcon = styled(ListItemIcon)(({ theme }) => ({
+    minWidth: theme.spacing(4),
+    margin: theme.spacing(0.25, 0),
+}));
+
+const StyledListItemText = styled(ListItemText)(({ theme }) => ({
+    margin: 0,
+}));
+
+const StyledAccordion = styled(Accordion)(({ theme }) => ({
+    paddingTop: theme.spacing(0),
+}));
+
+const StyledAccordionSummary = styled(AccordionSummary)(({ theme }) => ({
+    '&:hover': {
+        backgroundColor: theme.palette.action.hover,
+    },
+}));
+
+interface IMenuGroupProps {
+    title: string;
+    children: ReactNode;
+    icon: ReactNode;
+    activeIcon: ReactNode;
+    isActiveMenu: boolean;
+}
+
+export const MenuGroup = ({
+    title,
+    children,
+    icon,
+    activeIcon,
+    isActiveMenu,
+}: IMenuGroupProps) => {
+    return (
+        <StyledAccordion
+            disableGutters={true}
+            sx={{
+                boxShadow: 'none',
+                '&:before': {
+                    display: 'none',
+                },
+            }}
+        >
+            <StyledAccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls='configure-content'
+                id='configure-header'
+                sx={listItemButtonStyle}
+            >
+                <StyledListItemIcon>
+                    {isActiveMenu ? activeIcon : icon}
+                </StyledListItemIcon>
+                <StyledListItemText>
+                    <CappedText
+                        sx={{ fontWeight: isActiveMenu ? 'bold' : 'normal' }}
+                    >
+                        {title}
+                    </CappedText>
+                </StyledListItemText>
+            </StyledAccordionSummary>
+            <AccordionDetails sx={{ p: 0 }}>{children}</AccordionDetails>
+        </StyledAccordion>
+    );
+};
+
+export const AdminListItem: FC<{
+    href: string;
+    text: string;
+    badge?: ReactNode;
+    selected?: boolean;
+    children?: React.ReactNode;
+}> = ({ href, text, badge, selected, children }) => {
+    return (
+        <ListItem disablePadding>
+            <ListItemButton
+                dense={true}
+                component={Link}
+                to={href}
+                sx={listItemButtonStyle}
+                selected={selected}
+            >
+                <StyledListItemIcon>{children}</StyledListItemIcon>
+                <StyledListItemText>
+                    <CappedText>{text}</CappedText>
+                </StyledListItemText>
+                {badge}
+            </ListItemButton>
+        </ListItem>
+    );
+};
+
+export const AdminSubListItem: FC<{
+    href: string;
+    text: string;
+    badge?: ReactNode;
+    selected?: boolean;
+    children?: React.ReactNode;
+}> = ({ href, text, badge, selected, children }) => {
+    return (
+        <ListItem disablePadding>
+            <ListItemButton
+                dense={true}
+                component={Link}
+                to={href}
+                sx={subListItemButtonStyle}
+                selected={selected}
+            >
+                <StyledListItemIcon>{children}</StyledListItemIcon>
+                <StyledListItemText>
+                    <CappedText>{text}</CappedText>
+                </StyledListItemText>
+                {badge}
+            </ListItemButton>
+        </ListItem>
+    );
+};
---
+++ frontend/src/component/layout/MainLayout/AdminMenu/AdminMenu.tsx
@@ -0,0 +1,268 @@
+import { Grid, styled, Paper, Typography, Button, List } from '@mui/material';
+import { useUiFlag } from 'hooks/useUiFlag';
+import type { ReactNode } from 'react';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import HomeIcon from '@mui/icons-material/Home';
+import LaptopIcon from '@mui/icons-material/Laptop';
+import StopRoundedIcon from '@mui/icons-material/StopRounded';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import PeopleOutlineRoundedIcon from '@mui/icons-material/PeopleOutlineRounded';
+import KeyRoundedIcon from '@mui/icons-material/KeyRounded';
+import CloudIcon from '@mui/icons-material/Cloud';
+import HubOutlinedIcon from '@mui/icons-material/HubOutlined';
+import BuildOutlinedIcon from '@mui/icons-material/BuildOutlined';
+import { AdminListItem, AdminSubListItem, MenuGroup } from './AdminListItem';
+import { useLocation } from 'react-router-dom';
+
+const StyledAdminMainGrid = styled(Grid)(({ theme }) => ({
+    minWidth: 0, // this is a fix for overflowing flex
+    maxWidth: '1812px',
+    margin: '0 auto',
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    [theme.breakpoints.up(2156)]: {
+        width: '100%',
+    },
+    [theme.breakpoints.down(2156)]: {
+        marginLeft: theme.spacing(7),
+        marginRight: theme.spacing(7),
+    },
+    [theme.breakpoints.down('lg')]: {
+        maxWidth: '1550px',
+        paddingLeft: theme.spacing(1),
+        paddingRight: theme.spacing(1),
+    },
+    [theme.breakpoints.down(1024)]: {
+        marginLeft: 0,
+        marginRight: 0,
+    },
+    [theme.breakpoints.down('sm')]: {
+        minWidth: '100%',
+    },
+    minHeight: '94vh',
+}));
+
+const StyledMenuPaper = styled(Paper)(({ theme }) => ({
+    width: '100%',
+    minWidth: 320,
+    padding: theme.spacing(3),
+    marginTop: theme.spacing(6.5),
+    borderRadius: `${theme.shape.borderRadiusLarge}px`,
+    boxShadow: 'none',
+}));
+
+const SettingsHeader = styled(Typography)(({ theme }) => ({
+    fontSize: theme.fontSizes.mainHeader,
+    fontWeight: theme.fontWeight.bold,
+}));
+
+const StyledButton = styled(Button)(({ theme }) => ({
+    paddingLeft: theme.spacing(0),
+    marginBottom: theme.spacing(3),
+}));
+
+const StyledStopRoundedIcon = styled(StopRoundedIcon)(({ theme }) => ({
+    color: theme.palette.primary.main,
+}));
+
+const StyledPeopleOutlineRoundedIcon = styled(PeopleOutlineRoundedIcon)(
+    ({ theme }) => ({
+        color: theme.palette.primary.main,
+    }),
+);
+
+const StyledKeyRoundedIcon = styled(KeyRoundedIcon)(({ theme }) => ({
+    color: theme.palette.primary.main,
+}));
+
+const StyledCloudIcon = styled(CloudIcon)(({ theme }) => ({
+    color: theme.palette.primary.main,
+}));
+
+const StyledHubOutlinedIcon = styled(HubOutlinedIcon)(({ theme }) => ({
+    color: theme.palette.primary.main,
+}));
+
+const StyledBuildOutlinedIcon = styled(BuildOutlinedIcon)(({ theme }) => ({
+    color: theme.palette.primary.main,
+}));
+
+const RotatedKeyRoundedIcon = styled(KeyRoundedIcon)(({ theme }) => ({
+    transform: 'rotate(-45deg)',
+}));
+
+interface IWrapIfAdminSubpageProps {
+    children: ReactNode;
+}
+
+export const WrapIfAdminSubpage = ({ children }: IWrapIfAdminSubpageProps) => {
+    const newAdminUIEnabled = useUiFlag('adminNavUI');
+    const showOnlyAdminMenu =
+        newAdminUIEnabled && location.pathname.indexOf('/admin') === 0;
+
+    if (showOnlyAdminMenu) {
+        return <AdminMenu>{children}</AdminMenu>;
+    }
+
+    return <>{children}</>;
+};
+
+const DashboardLink = () => {
+    return (
+        <>
+            <StyledButton
+                href='/personal'
+                rel='noreferrer'
+                startIcon={<ArrowBackIcon />}
+            >
+                Back to Unleash
+            </StyledButton>
+        </>
+    );
+};
+
+interface IAdminMenuProps {
+    children: ReactNode;
+}
+
+export const AdminMenu = ({ children }: IAdminMenuProps) => {
+    const isActiveItem = (item: string) => location.pathname === item;
+    const location = useLocation();
+
+    const userAdmItems = [
+        { href: '/admin/users', text: 'Users' },
+        { href: '/admin/groups', text: 'Groups' },
+        { href: '/admin/roles/project-roles', text: 'Project roles' },
+        { href: '/admin/roles', text: 'Root roles' },
+        { href: '/admin/logins', text: 'Login history' },
+    ];
+
+    const accessControlItems = [
+        { href: '/admin/api', text: 'API access' },
+        { href: '/admin/cors', text: 'CORS origins' },
+    ];
+
+    const ssoItems = [
+        { href: '/admin/auth', text: 'OpenID Connect' },
+        { href: '/admin/auth', text: 'SAML 2.0' },
+        { href: '/admin/auth', text: 'Password' },
+        { href: '/admin/auth', text: 'SCIM' },
+    ];
+
+    const networkItems = [
+        { href: '/admin/network', text: 'Overview' },
+        { href: '/admin/network/traffic', text: 'Traffic' },
+        { href: '/admin/network/connected-edges', text: 'Connected edges' },
+        {
+            href: '/admin/network/backend-connections',
+            text: 'Backend Connections',
+        },
+        {
+            href: '/admin/network/frontend-data-usage',
+            text: 'Frontend Traffic',
+        },
+    ];
+
+    const instanceConfItems = [
+        { href: '/admin/maintenance', text: 'Maintenance' },
+        { href: '/admin/banners', text: 'Banners' },
+        { href: '/admin/license', text: 'License' },
+        { href: '/admin/instance', text: 'Instance stats' },
+        { href: '/admin/instance-privacy', text: 'Instance privacy' },
+    ];
+
+    const items = [
+        { href: '/admin', text: 'Admin home', icon: <HomeIcon /> },
+        {
+            text: 'User administration',
+            icon: <PeopleOutlineRoundedIcon />,
+            activeIcon: <StyledPeopleOutlineRoundedIcon />,
+            items: userAdmItems,
+        },
+        {
+            href: '/admin/service-accounts',
+            text: 'Service accounts',
+            icon: <LaptopIcon />,
+        },
+        {
+            text: 'Access control',
+            icon: <KeyRoundedIcon />,
+            activeIcon: <StyledKeyRoundedIcon />,
+            items: accessControlItems,
+        },
+        {
+            text: 'Single sign-on',
+            icon: <CloudIcon />,
+            activeIcon: <StyledCloudIcon />,
+            items: ssoItems,
+        },
+        {
+            text: 'Network',
+            icon: <HubOutlinedIcon />,
+            activeIcon: <StyledHubOutlinedIcon />,
+            items: networkItems,
+        },
+        {
+            text: 'Instance configuration',
+            icon: <BuildOutlinedIcon />,
+            activeIcon: <StyledBuildOutlinedIcon />,
+            items: instanceConfItems,
+        },
+        { href: '/history', text: 'Event log', icon: <EventNoteIcon /> },
+    ];
+
+    return (
+        <>
+            <StyledAdminMainGrid container spacing={1}>
+                <Grid item>
+                    <StyledMenuPaper>
+                        <SettingsHeader>Admin settings</SettingsHeader>
+                        <DashboardLink />
+                        <List>
+                            {items.map((item) => {
+                                if (item.items) {
+                                    const isActiveMenu = item.items.find(
+                                        (itm) => isActiveItem(itm.href),
+                                    );
+                                    return (
+                                        <MenuGroup
+                                            title={item.text}
+                                            icon={item.icon}
+                                            activeIcon={item.activeIcon}
+                                            isActiveMenu={Boolean(isActiveMenu)}
+                                        >
+                                            {item.items.map((subItem) => (
+                                                <AdminSubListItem
+                                                    href={subItem.href}
+                                                    text={subItem.text}
+                                                    selected={isActiveItem(
+                                                        subItem.href,
+                                                    )}
+                                                >
+                                                    <StyledStopRoundedIcon />
+                                                </AdminSubListItem>
+                                            ))}
+                                        </MenuGroup>
+                                    );
+                                }
+
+                                return (
+                                    <AdminListItem
+                                        href={item.href}
+                                        text={item.text}
+                                        selected={isActiveItem(item.href)}
+                                    >
+                                        {item.icon}
+                                    </AdminListItem>
+                                );
+                            })}
+                        </List>
+                    </StyledMenuPaper>
+                </Grid>
+                <Grid item md={9}>
+                    {children}
+                </Grid>
+            </StyledAdminMainGrid>
+        </>
+    );
+};

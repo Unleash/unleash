c40f6eab6c372adfc2ea71d0131993ffddce544e
Thomas Heartman <thomas@getunleash.io>
2025-03-27 10:05:17 +0100

    Style component

--- frontend/src/component/feature/FeatureView/FeatureOverview/FeatureOverview.tsx
+++ frontend/src/component/feature/FeatureView/FeatureOverview/FeatureOverview.tsx
@@ -1,4 +1,5 @@
 import FeatureOverviewMetaData from './FeatureOverviewMetaData/FeatureOverviewMetaData';
+import Close from '@mui/icons-material/Close';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { SidebarModal } from 'component/common/SidebarModal/SidebarModal';
 import {
@@ -7,7 +8,7 @@ import {
 } from 'component/feature/FeatureStrategy/FeatureStrategyEdit/FeatureStrategyEdit';
 import { useRequiredPathParam } from 'hooks/useRequiredPathParam';
 import { usePageTitle } from 'hooks/usePageTitle';
-import { styled } from '@mui/material';
+import { Box, Button, IconButton, styled } from '@mui/material';
 import { FeatureStrategyCreate } from 'component/feature/FeatureStrategy/FeatureStrategyCreate/FeatureStrategyCreate';
 import { useEffect, useState } from 'react';
 import { useLastViewedFlags } from 'hooks/useLastViewedFlags';
@@ -15,7 +16,7 @@ import { useUiFlag } from 'hooks/useUiFlag';
 import { FeatureOverviewEnvironments } from './FeatureOverviewEnvironments/FeatureOverviewEnvironments';
 import { default as LegacyFleatureOverview } from './LegacyFeatureOverview';
 import { useEnvironmentVisibility } from './FeatureOverviewMetaData/EnvironmentVisibilityMenu/hooks/useEnvironmentVisibility';
-import Joyride from 'react-joyride';
+import Joyride, { type TooltipRenderProps } from 'react-joyride';
 import useSplashApi from 'hooks/api/actions/useSplashApi/useSplashApi';
 import { useAuthSplash } from 'hooks/api/getters/useAuth/useAuthSplash';
 
@@ -34,6 +35,73 @@ const StyledMainContent = styled('div')(({ theme }) => ({
     flexGrow: 1,
     gap: theme.spacing(2),
 }));
+
+const StyledTooltip = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(2),
+    maxWidth: '300px',
+    background: '#201e42',
+    borderRadius: theme.shape.borderRadiusMedium,
+    color: theme.palette.common.white,
+    transform: 'translateX(35%)',
+    padding: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+    fontSize: theme.typography.body2.fontSize,
+}));
+
+const OkButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.secondary.border,
+    alignSelf: 'start',
+    marginLeft: theme.spacing(-1),
+}));
+
+// const TopRow = styled('div')(({ theme }) => ({
+//     fontSize: theme.typography.body1.fontSize,
+//     display: 'flex',
+//     flexDirection: 'row',
+//     alignItems: 'top',
+// }));
+//
+const StyledCloseButton = styled(IconButton)(({ theme }) => ({
+    color: theme.palette.common.white,
+    background: 'none',
+    border: 'none',
+    position: 'absolute',
+    top: theme.spacing(1),
+    right: theme.spacing(1),
+    svg: {
+        width: theme.spacing(2),
+        height: theme.spacing(2),
+    },
+}));
+
+const StyledHeader = styled('p')(({ theme }) => ({
+    fontSize: theme.typography.body1.fontSize,
+    fontWeight: 'bold',
+}));
+
+const CustomTooltip = ({ closeProps }: TooltipRenderProps) => {
+    return (
+        <StyledTooltip component='article'>
+            <StyledCloseButton type='button' {...closeProps}>
+                <Close />
+            </StyledCloseButton>
+            <StyledHeader>Decide the order evaluation</StyledHeader>
+            <p>
+                Strategies are evaluated in the order presented here. Drag and
+                rearrange the strategies to get the order you prefer.
+            </p>
+            <OkButton
+                type='button'
+                data-action={closeProps['data-action']}
+                onClick={closeProps.onClick}
+            >
+                Ok, got it!
+            </OkButton>
+        </StyledTooltip>
+    );
+};
 
 export const FeatureOverview = () => {
     const navigate = useNavigate();
@@ -81,15 +149,26 @@ export const FeatureOverview = () => {
                             setSplashSeen(dragTooltipSplashId);
                         }
                     }}
+                    floaterProps={{
+                        styles: {
+                            arrow: {
+                                color: '#201e42',
+                                spread: 16,
+                                length: 10,
+                            },
+                        },
+                    }}
                     debug
                     run={showTooltip}
                     disableOverlay
                     disableScrolling
+                    tooltipComponent={CustomTooltip}
                     steps={[
                         {
                             disableBeacon: true,
+                            offset: 0,
                             target: '.strategy-drag-handle',
-                            content: 'Welcome to the Feature Overview!',
+                            content: <></>,
                         },
                     ]}
                 />

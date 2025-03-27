b2240508ee7ae088ea40d03222958db0ea03f61a
Simon Hornby <liquidwicked64@gmail.com>
2025-03-27 14:54:45 +0200

    chore: feedback link on feature strategy

---
+++ frontend/src/constants/links.ts
@@ -0,0 +1,4 @@
+// honestly there's no nice place for this in our folder structure and I really, really need this to change in one place
+// might be deletable once we're done with the initial feedback phase
+export const RELEASE_TEMPLATE_FEEDBACK =
+    'https://docs.google.com/forms/d/1ElbScYxbAhFcjQWgRinifoymYHeuXzqIoQXfpUVYGR8/preview';

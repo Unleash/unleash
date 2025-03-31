f600ceb4fb18e89f9f45301bd93d877437fae273
FredrikOseberg <fredrik.no@gmail.com>
2025-03-31 14:09:24 +0200

    refactor: migration

---
+++ src/migrations/20250331115805-alter-tag-type-color.js
@@ -0,0 +1,25 @@
+exports.up = function (db, cb) {
+    db.runSql(
+        `
+        ALTER TABLE tag_types
+        ALTER COLUMN color TYPE VARCHAR(50);
+        
+        UPDATE tag_types
+        SET color = 'common.white';
+        `,
+        cb,
+    );
+};
+
+exports.down = function (db, cb) {
+    db.runSql(
+        `
+        ALTER TABLE tag_types
+        ALTER COLUMN color TYPE VARCHAR(10);
+        
+        UPDATE tag_types
+        SET color = '#FFFFFF';
+        `,
+        cb,
+    );
+};

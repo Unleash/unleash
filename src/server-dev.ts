b37d8c21aef89d5dac93d4eb742f3f691f56ffc8
Thomas Heartman <thomas@getunleash.io>
2025-04-04 10:05:34 +0200

    Add flag for add/edit strategies

--- src/server-dev.ts
+++ src/server-dev.ts
@@ -59,6 +59,7 @@ process.nextTick(async () => {
                         adminNavUI: true,
                         tagTypeColor: true,
                         newStrategyDropdown: true,
+                        addEditStrategy: true,
                     },
                 },
                 authentication: {

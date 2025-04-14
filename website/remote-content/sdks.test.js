f068ff110a9e40f6ea8f2d08868b6f84542ee2a2
melindafekete <melinda.fekete@getunleash.io>
2025-04-14 16:33:19 +0200

    Remove extending the test as jest is not set up properly to run these

--- website/remote-content/sdks.test.js
+++ website/remote-content/sdks.test.js
@@ -14,13 +14,11 @@ test('Modifies filenames and content properly', () => {
     const edgeMainContent = docs.modifyContent(docs.urls[1], '');
     const firstSubpage = docs.modifyContent(docs.urls[2], '');
     const secondSubpage = docs.modifyContent(docs.urls[3], '');
-    const thirdSubpage = docs.modifyContent(docs.urls[4], '');
 
     expect(proxyContent.filename).toBe('unleash-proxy.md');
     expect(edgeMainContent.filename).toBe('unleash-edge.md');
     expect(firstSubpage.filename).toBe('unleash-edge/concepts.md');
     expect(secondSubpage.filename).toBe('unleash-edge/deploying.md');
-    expect(thirdSubpage.filename).toBe('unleash-edge/CLI.md');
 
     expect(edgeMainContent.content).toContain('title: Unleash Edge');
     expect(edgeMainContent.content).toContain('slug: /reference/unleash-edge');
@@ -34,29 +32,5 @@ test('Modifies filenames and content properly', () => {
     );
     expect(firstSubpage.content).toContain(
         'custom_edit_url: https://github.com/Unleash/unleash-edge/edit/main/docs/concepts.md',
-    );
-
-    expect(thirdSubpage.content).toContain('title: Benchmarking');
-    expect(firstSubpage.content).toContain('slug: /reference/unleash-edge/benchmarking');
-    expect(firstSubpage.content).toContain(
-        'custom_edit_url: https://github.com/Unleash/unleash-edge/edit/main/docs/benchmarking.md',
-    );
-
-    expect(thirdSubpage.content).toContain('title: CLI');
-    expect(firstSubpage.content).toContain('slug: /reference/unleash-edge/cli');
-    expect(firstSubpage.content).toContain(
-        'custom_edit_url: https://github.com/Unleash/unleash-edge/edit/main/docs/CLI.md',
-    );
-
-    expect(thirdSubpage.content).toContain('title: Development guide');
-    expect(firstSubpage.content).toContain('slug: /reference/unleash-edge/development-guide');
-    expect(firstSubpage.content).toContain(
-        'custom_edit_url: https://github.com/Unleash/unleash-edge/edit/main/docs/development-guide.md',
-    );
-
-    expect(thirdSubpage.content).toContain('title: Migration guide');
-    expect(firstSubpage.content).toContain('slug: /reference/unleash-edge/migration-guide');
-    expect(firstSubpage.content).toContain(
-        'custom_edit_url: https://github.com/Unleash/unleash-edge/edit/main/docs/migration-guide.md',
     );
 });

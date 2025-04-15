35b024ce667c48de2057f0a0fccbe6a49b85891d
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-04-15 17:18:34 +0200

    revert flags overview page title change

--- frontend/src/component/menu/routes.ts
+++ frontend/src/component/menu/routes.ts
@@ -144,6 +144,15 @@ export const routes: IRoute[] = [
         component: FeatureToggleListTable,
         type: 'protected',
         menu: { primary: true },
+        notFlag: 'flagsReleaseManagementUI',
+    },
+    {
+        path: '/search',
+        title: 'Flags overview',
+        component: FeatureToggleListTable,
+        type: 'protected',
+        menu: { primary: true },
+        flag: 'flagsReleaseManagementUI',
     },
 
     // Playground

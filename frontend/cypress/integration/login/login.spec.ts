ae07346b857e0dfd9ffa1e84c43f0f9bc4d47ae7
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-04-17 10:48:07 +0200

    test with navigation awaits

--- frontend/cypress/integration/login/login.spec.ts
+++ frontend/cypress/integration/login/login.spec.ts
@@ -23,58 +23,83 @@ describe('login', { testIsolation: true }, () => {
 
     it('is redirecting to /personal after first login', () => {
         cy.visit('/');
+        cy.wait(1_000);
         cy.url().should('eq', `${baseUrl}/personal`);
         cy.visit('/');
+        cy.wait(1_000);
         // "/" should again redirect to last "home" page
         cy.url().should('eq', `${baseUrl}/personal`);
     });
 
     it('is redirecting to last visited projects', () => {
         cy.visit('/projects');
+        cy.wait(1_000);
         cy.visit('/');
+        cy.wait(1_000);
         cy.url().should('eq', `${baseUrl}/projects`);
         cy.contains('a', projectName).click();
+        cy.wait(1_000);
         cy.get(`h1 span`).should('not.have.class', 'skeleton');
         cy.visit('/');
-        cy.url().should('eq', `${baseUrl}/projects/${projectName}`); // last visited project
+        cy.wait(1_000);
+        cy.url().should((url) =>
+            url.startsWith(`${baseUrl}/projects/${projectName}`),
+        ); // last visited project
     });
 
     it('is redirecting to other pages', () => {
         cy.visit('/search');
+        cy.wait(1_000);
         cy.visit('/');
+        cy.wait(1_000);
         cy.url().should('eq', `${baseUrl}/search`);
         cy.visit('/search');
+        cy.wait(1_000);
         cy.visit('/admin');
+        cy.wait(1_000);
         cy.visit('/applications');
+        cy.wait(1_000);
         cy.visit('/');
         cy.url().should('eq', `${baseUrl}/admin`);
     });
 
     it('is redirecting to other pages', () => {
         cy.visit('/search');
+        cy.wait(1_000);
         cy.visit('/playground');
+        cy.wait(1_000);
         cy.visit('/');
+        cy.wait(1_000);
         cy.url().should('eq', `${baseUrl}/playground`);
         cy.visit('/admin');
+        cy.wait(1_000);
         cy.visit('/applications'); // not one of main pages
+        cy.wait(1_000);
         cy.visit('/');
+        cy.wait(1_000);
         cy.url().should('eq', `${baseUrl}/admin`);
     });
 
     it('clears last visited page on manual logout', () => {
         cy.visit('/search');
+        cy.wait(1_000);
         cy.get('[data-testid=HEADER_USER_AVATAR]').click();
         cy.get('button').contains('Log out').click();
+        cy.wait(1_000);
         cy.url().should('eq', `${baseUrl}/login`);
         cy.visit('/');
+        cy.wait(1_000);
         cy.url().should('eq', `${baseUrl}/login`);
         cy.do_login();
+        cy.wait(1_000);
         cy.visit('/');
+        cy.wait(1_000);
         cy.url().should('eq', `${baseUrl}/personal`);
     });
 
     it('remembers last visited page on next login', () => {
         cy.visit('/insights');
+        cy.wait(1_000);
         cy.window().then((win) => {
             win.sessionStorage.clear(); // not localStorage
             win.location.reload();
@@ -82,6 +107,7 @@ describe('login', { testIsolation: true }, () => {
         cy.url().should('eq', `${baseUrl}/login`);
         cy.do_login();
         cy.visit('/');
+        cy.wait(1_000);
         cy.url().should('eq', `${baseUrl}/insights`);
     });
 });

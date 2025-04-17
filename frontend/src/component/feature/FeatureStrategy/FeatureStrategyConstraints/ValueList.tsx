ba021bb890232981e38c563e2ec0072ed474feb0
sjaanus <sellinjaanus@gmail.com>
2025-04-17 11:41:08 +0300

    feat: add values chip styling

--- frontend/src/component/feature/FeatureStrategy/FeatureStrategyConstraints/ValueList.tsx
+++ frontend/src/component/feature/FeatureStrategy/FeatureStrategyConstraints/ValueList.tsx
@@ -39,6 +39,16 @@ const ValueChipBase = styled(
     background: theme.palette.secondary.light,
     color: theme.palette.secondary.dark,
     border: `1px solid ${theme.palette.secondary.border}`,
+    padding: 0,
+    height: 'auto',
+    '& .MuiChip-label': {
+        paddingTop: theme.spacing(0.5),
+        paddingBottom: theme.spacing(0.5),
+        paddingLeft: theme.spacing(1.5),
+    },
+    '& .MuiChip-deleteIcon': {
+        marginRight: theme.spacing(1),
+    },
     ':hover, :focus-visible': {
         background: theme.palette.secondary.light,
     },
@@ -74,8 +84,8 @@ const AddValuesButton = styled('button')(({ theme }) => ({
     whiteSpace: 'nowrap',
     gap: theme.spacing(0.25),
     alignItems: 'center',
-    paddingInline: theme.spacing(1.5),
-    height: theme.spacing(3),
+    padding: theme.spacing(0.5, 1.5, 0.5, 1.5),
+    height: 'auto',
     transition: 'all 0.3s ease',
     outline: `1px solid #0000`,
     background: theme.palette.background.elevation1,

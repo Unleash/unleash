5127d69f9982e05859085d221841a8985768ab86
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-04-28 15:16:43 +0200

    feat: update search placement on flags overview screen

--- frontend/src/component/feature/FeatureToggleList/FeatureToggleFilters/LifecycleFilters.tsx
+++ frontend/src/component/feature/FeatureToggleList/FeatureToggleFilters/LifecycleFilters.tsx
@@ -1,5 +1,5 @@
 import { Box, Chip, styled } from '@mui/material';
-import type { FC } from 'react';
+import type { FC, ReactNode } from 'react';
 import type { FilterItemParamHolder } from '../../../filter/Filters/Filters';
 import type { LifecycleStage } from '../../FeatureView/FeatureOverview/FeatureLifecycle/LifecycleStage';
 import { useLifecycleCount } from 'hooks/api/getters/useLifecycleCount/useLifecycleCount';
@@ -30,13 +30,22 @@ interface ILifecycleFiltersProps {
     state: FilterItemParamHolder;
     onChange: (value: FilterItemParamHolder) => void;
     total?: number;
+    children?: ReactNode;
 }
 
 const Wrapper = styled(Box)(({ theme }) => ({
     display: 'flex',
+    justifyContent: 'space-between',
+    padding: theme.spacing(1.5, 3, 0, 3),
+    minHeight: theme.spacing(7),
+    gap: theme.spacing(2),
+}));
+
+const StyledContainer = styled(Box)(({ theme }) => ({
+    display: 'flex',
     alignItems: 'center',
+    flexWrap: 'wrap',
     gap: theme.spacing(1),
-    padding: theme.spacing(2, 3, 0, 3),
 }));
 
 const lifecycleOptions: {
@@ -74,43 +83,49 @@ export const LifecycleFilters: FC<ILifecycleFiltersProps> = ({
     state,
     onChange,
     total,
+    children,
 }) => {
     const { lifecycleCount } = useLifecycleCount();
     const current = state.lifecycle?.values ?? [];
 
     return (
         <Wrapper>
-            {lifecycleOptions.map(({ label, value }) => {
-                const isActive =
-                    value === null ? !state.lifecycle : current.includes(value);
-                const count = getStageCount(value, lifecycleCount);
-                const dynamicLabel =
-                    isActive && Number.isInteger(total)
-                        ? `${label} (${total === count ? total : `${total} of ${count}`})`
-                        : `${label}${count !== undefined ? ` (${count})` : ''}`;
-
-                const handleClick = () =>
-                    onChange(
+            <StyledContainer>
+                {lifecycleOptions.map(({ label, value }) => {
+                    const isActive =
                         value === null
-                            ? { lifecycle: null }
-                            : {
-                                  lifecycle: {
-                                      operator: 'IS',
-                                      values: [value],
-                                  },
-                              },
-                    );
+                            ? !state.lifecycle
+                            : current.includes(value);
+                    const count = getStageCount(value, lifecycleCount);
+                    const dynamicLabel =
+                        isActive && Number.isInteger(total)
+                            ? `${label} (${total === count ? total : `${total} of ${count}`})`
+                            : `${label}${count !== undefined ? ` (${count})` : ''}`;
 
-                return (
-                    <StyledChip
-                        key={label}
-                        label={dynamicLabel}
-                        variant='outlined'
-                        isActive={isActive}
-                        onClick={handleClick}
-                    />
-                );
-            })}
+                    const handleClick = () =>
+                        onChange(
+                            value === null
+                                ? { lifecycle: null }
+                                : {
+                                      lifecycle: {
+                                          operator: 'IS',
+                                          values: [value],
+                                      },
+                                  },
+                        );
+
+                    return (
+                        <StyledChip
+                            key={label}
+                            label={dynamicLabel}
+                            variant='outlined'
+                            isActive={isActive}
+                            onClick={handleClick}
+                        />
+                    );
+                })}
+            </StyledContainer>
+            {children}
         </Wrapper>
     );
 };

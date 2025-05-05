d22466ad1943e1ff5fa217b6fcfbca4b24d3b85f
Gastón Fournier <gaston@getunleash.io>
2025-05-05 11:18:03 +0200

    feat: enable isolatedModules

--- src/lib/features/playground/feature-evaluator/index.ts
+++ src/lib/features/playground/feature-evaluator/index.ts
@@ -2,12 +2,13 @@ import {
     FeatureEvaluator,
     type FeatureEvaluatorConfig,
 } from './feature-evaluator.js';
-import { Variant } from './variant.js';
-import { Context } from './context.js';
+import type { Variant } from './variant.js';
+import type { Context } from './context.js';
 import type { ClientFeaturesResponse } from './feature.js';
 import InMemStorageProvider from './repository/storage-provider-in-mem.js';
 
 // exports
 export { Strategy } from './strategy/index.js';
-export { Context, Variant, FeatureEvaluator, InMemStorageProvider };
+export { FeatureEvaluator, InMemStorageProvider };
+export type { Context, Variant };
 export type { ClientFeaturesResponse, FeatureEvaluatorConfig };
--- src/lib/server-impl.ts
+++ src/lib/server-impl.ts
@@ -40,7 +40,7 @@ import Controller from './routes/controller.js';
 import { createClientFeatureToggleDelta } from './features/client-feature-toggles/delta/createClientFeatureToggleDelta.js';
 import { CRUDStore } from './db/crud/crud-store.js';
 import type { CrudStoreConfig } from './db/crud/crud-store.js';
-import { Logger, LogLevel, type LogProvider } from './logger.js';
+import { type Logger, LogLevel, type LogProvider } from './logger.js';
 import { ImportTogglesStore } from './features/export-import-toggles/import-toggles-store.js';
 import { ALL_PROJECTS, CUSTOM_ROOT_ROLE_TYPE } from './util/constants.js';
 import {
@@ -149,7 +149,7 @@ import {
     createPrivateProjectChecker,
 } from './features/private-project/createPrivateProjectChecker.js';
 import type { ProductivityReportMetrics } from './features/productivity-report/productivity-report-view-model.js';
-import { IProjectStats } from './features/project/project-service.js';
+import type { IProjectStats } from './features/project/project-service.js';
 import type { IImportService } from './features/export-import-toggles/export-import-service.js';
 import type { IContextFieldDto } from './features/context/context-field-store-type.js';
 import { SegmentReadModel } from './features/segment/segment-read-model.js';
@@ -165,7 +165,7 @@ import FeatureTagStore from './db/feature-tag-store.js';
 import ExportImportController from './features/export-import-toggles/export-import-controller.js';
 import type { QueryOverride } from './features/client-feature-toggles/client-feature-toggle.controller.js';
 import { DELTA_EVENT_TYPES } from './features/client-feature-toggles/delta/client-feature-toggle-delta-types.js';
-import { AdvancedPlaygroundFeatureEvaluationResult } from './features/playground/playground-service.js';
+import type { AdvancedPlaygroundFeatureEvaluationResult } from './features/playground/playground-service.js';
 import { advancedPlaygroundViewModel } from './features/playground/playground-view-model.js';
 import {
     createAccessReadModel,
@@ -351,7 +351,6 @@ export {
     create,
     createDb,
     Controller,
-    Logger,
     LogLevel,
     withRollbackTransaction,
     withTransactional,
@@ -405,7 +404,6 @@ export {
     createFakePrivateProjectChecker,
     createProjectReadModel,
     createPrivateProjectChecker,
-    IProjectStats,
     createFakeInstanceStatsService,
     createInstanceStatsService,
     SegmentReadModel,
@@ -425,7 +423,6 @@ export {
     DELTA_EVENT_TYPES,
     createKnexTransactionStarter,
     createPlaygroundService,
-    AdvancedPlaygroundFeatureEvaluationResult,
     advancedPlaygroundViewModel,
     withFakeTransactional,
     createAccessReadModel,
@@ -477,12 +474,15 @@ export type {
     ReleasePlanMilestoneWriteModel,
     ReleasePlanMilestoneStrategyWriteModel,
     IChangeRequestAccessReadModel,
+    AdvancedPlaygroundFeatureEvaluationResult,
+    IProjectStats,
     IRoleWithProject,
     ISchemaValidationErrors,
     IImportService,
     IContextFieldDto,
     Gauge,
     Counter,
+    Logger,
     Subscriber,
     ProjectAccess,
     IPrivateProjectChecker,

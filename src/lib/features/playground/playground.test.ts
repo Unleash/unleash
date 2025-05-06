78db9a8f219d21e7b5c0a33b386ce5ff1ce9487a
Christopher Kolstad <chriswk@getunleash.io>
2025-05-06 14:35:58 +0200

    Update how we import fast-check in playground

--- src/lib/features/playground/playground.test.ts
+++ src/lib/features/playground/playground.test.ts
@@ -1,6 +1,6 @@
-import fc from 'fast-check';
+import * as fc from 'fast-check';
 
-import supertest from 'supertest';
+import * as supertest from 'supertest';
 import { createServices } from '../../services/index.js';
 import { createTestConfig } from '../../../test/config/test-config.js';
 

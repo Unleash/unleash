c85a905ee052481df19a6fedd88748c0270aadc1
sjaanus <sellinjaanus@gmail.com>
2025-05-06 11:02:04 +0300

    Change to chip

--- frontend/src/component/feature/FeatureStrategy/FeatureStrategySegment/RecentlyUsedSegments/RecentlyUsedSegmentChip.tsx
+++ frontend/src/component/feature/FeatureStrategy/FeatureStrategySegment/RecentlyUsedSegments/RecentlyUsedSegmentChip.tsx
@@ -1,46 +1,28 @@
 import type React from 'react';
 import type { ISegment } from 'interfaces/segment';
-import Add from '@mui/icons-material/Add';
-import { styled, type Theme, Tooltip } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { Chip, styled, Tooltip } from '@mui/material';
 
 interface IRecentlyUsedSegmentChipProps {
     segment: ISegment;
     setSegments: React.Dispatch<React.SetStateAction<ISegment[]>>;
 }
 
-const StyledChip = styled('button')(({ theme }) => ({
-    all: 'unset',
-    display: 'flex',
-    alignItems: 'center',
-    gap: theme.spacing(0.5),
-    paddingInlineStart: theme.spacing(2),
-    paddingInlineEnd: theme.spacing(1),
-    paddingBlockStart: theme.spacing(0.5),
-    paddingBlockEnd: theme.spacing(0.5),
-    borderRadius: '100rem',
-    color: theme.palette.text.primary,
-    border: `1px dashed ${theme.palette.divider}`,
+const StyledChip = styled(Chip)(({ theme }) => ({
     background: 'transparent',
-    cursor: 'pointer',
-    boxSizing: 'border-box',
-    '&:hover': {
-        backgroundColor: 'rgba(0, 0, 0, 0.05)',
+    height: 'auto',
+    '.MuiChip-label': {
+        fontSize: theme.typography.body2.fontSize,
+        padding: theme.spacing(0.5, 1),
     },
-    '&:focus': {
-        outline: `1px solid ${theme.palette.primary.main}`,
-        outlineOffset: '2px',
+    '&.MuiChip-outlined': {
+        border: `1px dashed ${theme.palette.divider}`,
+    },
+    '& .MuiChip-icon': {
+        color: theme.palette.neutral.main,
+        marginLeft: theme.spacing(1),
     },
 }));
-
-const StyledSegmentName = styled('span')(({ theme }) => ({
-    marginLeft: theme.spacing(0.5),
-    fontSize: theme.typography.body2.fontSize,
-}));
-
-const styledIcon = (theme: Theme) => ({
-    fontSize: theme.fontSizes.bodySize,
-    color: theme.palette.neutral.main,
-});
 
 export const RecentlyUsedSegmentChip = ({
     segment,
@@ -57,10 +39,13 @@ export const RecentlyUsedSegmentChip = ({
 
     return (
         <Tooltip title='Add segment' arrow>
-            <StyledChip type='button' onClick={onAdd}>
-                <Add titleAccess='Add' sx={styledIcon} />
-                <StyledSegmentName>{segment.name}</StyledSegmentName>
-            </StyledChip>
+            <StyledChip
+                label={segment.name}
+                icon={<AddIcon />}
+                onClick={onAdd}
+                variant='outlined'
+                clickable
+            />
         </Tooltip>
     );
 };

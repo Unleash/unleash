22e5bdeb3c4b4ce48056c51265ceb692bee0d06a
Thomas Heartman <thomas@getunleash.io>
2025-05-08 11:19:14 +0200

    revert redundant changes

--- frontend/src/utils/cleanConstraint.ts
+++ frontend/src/utils/cleanConstraint.ts
@@ -1,12 +1,13 @@
-import { isSingleValueOperator } from 'constants/operators';
+import { singleValueOperators } from 'constants/operators';
 import type { IConstraint } from 'interfaces/strategy';
+import { oneOf } from 'utils/oneOf';
 import produce from 'immer';
 
 export const cleanConstraint = (
     constraint: Readonly<IConstraint>,
 ): IConstraint => {
     return produce(constraint, (draft) => {
-        if (isSingleValueOperator(constraint.operator)) {
+        if (oneOf(singleValueOperators, constraint.operator)) {
             delete draft.values;
         } else {
             delete draft.value;

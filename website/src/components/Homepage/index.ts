c5493b3c310a5b0c4dd21243d70da403ae08a6fc
melindafekete <melinda.fekete@getunleash.io>
2025-05-12 18:19:40 +0200

    fix more broken links and redirects

--- website/src/components/Homepage/index.ts
+++ website/src/components/Homepage/index.ts
@@ -1,4 +1,2 @@
 export { default as Cards } from './HomepageCards';
 export { default as Content } from './HomepageContent';
-export { default as APICards } from './APICards';
-export { default as APIContent } from './APIContent';

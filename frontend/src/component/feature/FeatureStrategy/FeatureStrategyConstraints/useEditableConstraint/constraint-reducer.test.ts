4566a12b889658674e8a86d9051f4d40fc3695a9
Thomas Heartman <thomas@getunleash.io>
2025-05-12 14:58:29 +0200

    start stubbing out tests

---
+++ frontend/src/component/feature/FeatureStrategy/FeatureStrategyConstraints/useEditableConstraint/constraint-reducer.test.ts
@@ -0,0 +1,9 @@
+describe('changing context field', () => {
+    test('changing context field to the same field is a no-op', () => {});
+});
+describe('changing operator', () => {
+    test('changing operator to the same operator field is a no-op', () => {});
+});
+describe('adding values', () => {});
+describe('clearing values', () => {});
+describe('toggle options', () => {});

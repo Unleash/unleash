995a953ad82c68171c3ea4920384081eff6b288d
Gastón Fournier <gaston@getunleash.io>
2025-05-14 13:58:30 +0200

    Make test follow schema

--- src/lib/openapi/spec/ui-config-schema.test.ts
+++ src/lib/openapi/spec/ui-config-schema.test.ts
@@ -1,5 +1,4 @@
 import { validateSchema } from '../validate.js';
-import type { ResourceLimitsSchema } from './resource-limits-schema.js';
 import type { UiConfigSchema } from './ui-config-schema.js';
 
 test('uiConfigSchema', () => {
@@ -13,7 +12,21 @@ test('uiConfigSchema', () => {
         resourceLimits: {
             segmentValues: 0,
             strategySegments: 0,
-        } as ResourceLimitsSchema,
+            actionSetActions: 0,
+            actionSetsPerProject: 0,
+            actionSetFilters: 0,
+            actionSetFilterValues: 0,
+            signalEndpoints: 0,
+            signalTokensPerEndpoint: 0,
+            featureEnvironmentStrategies: 0,
+            constraintValues: 0,
+            environments: 1,
+            projects: 1,
+            apiTokens: 0,
+            segments: 0,
+            featureFlags: 1,
+            constraints: 0,
+        },
         versionInfo: {
             current: {},
             latest: {},
@@ -22,6 +35,7 @@ test('uiConfigSchema', () => {
         },
     };
 
+    console.log(validateSchema('#/components/schemas/uiConfigSchema', data));
     expect(
         validateSchema('#/components/schemas/uiConfigSchema', data),
     ).toBeUndefined();

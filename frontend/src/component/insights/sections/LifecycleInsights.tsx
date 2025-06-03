4691a4eff3b6e829001f6b98a5b0a3712a2f7d64
Thomas Heartman <thomas@getunleash.io>
2025-06-03 13:29:41 +0200

    add lifecycle trends query

--- frontend/src/component/insights/sections/LifecycleInsights.tsx
+++ frontend/src/component/insights/sections/LifecycleInsights.tsx
@@ -3,13 +3,25 @@ import type { FC } from 'react';
 import { FilterItemParam } from 'utils/serializeQueryParams';
 import { InsightsSection } from 'component/insights/sections/InsightsSection';
 import { InsightsFilters } from 'component/insights/InsightsFilters';
+import { allOption } from 'component/common/ProjectSelect/ProjectSelect';
+import { useInsights } from 'hooks/api/getters/useInsights/useInsights';
 
 export const LifecycleInsights: FC = () => {
     const statePrefix = 'lifecycle-';
     const stateConfig = {
         [`${statePrefix}project`]: FilterItemParam,
     };
-    const [state, setState] = usePersistentTableState('insights', stateConfig);
+    const [state, setState] = usePersistentTableState(
+        'insights-lifecycle',
+        stateConfig,
+    );
+
+    const projects = state[`${statePrefix}project`]?.values ?? [allOption.id];
+    const { insights, loading } = useInsights();
+
+    // @ts-expect-error (lifecycleMetrics): The schema hasn't been updated yet.
+    const { lifecycleTrends } = insights;
+    console.log('lifecycleTrends', lifecycleTrends);
 
     return (
         <InsightsSection
@@ -21,6 +33,8 @@ export const LifecycleInsights: FC = () => {
                     filterNamePrefix={statePrefix}
                 />
             }
-        />
+        >
+            <pre>{JSON.stringify(lifecycleTrends, null, 2)}</pre>
+        </InsightsSection>
     );
 };

c77839c7b0f6223c9e7130ca8240f2e0d3077df8
Thomas Heartman <thomas@getunleash.io>
2025-06-04 14:29:23 +0200

    shuffle some stuff around

--- frontend/src/component/insights/sections/LifecycleInsights.tsx
+++ frontend/src/component/insights/sections/LifecycleInsights.tsx
@@ -63,70 +63,13 @@ export const LifecycleInsights: FC = () => {
         'insights-lifecycle',
         stateConfig,
     );
-    const chartColors = useChartColors();
 
+    // todo: use data from the actual endpoint when we have something useful to return
     const projects = state[`${statePrefix}project`]?.values ?? [allOption.id];
     const { insights, loading } = useInsights();
 
     // @ts-expect-error (lifecycleMetrics): The schema hasn't been updated yet.
     const { lifecycleTrends } = insights;
-
-    const mockData: LifecycleInsights = {
-        develop: {
-            totalFlags: 35,
-            averageTimeInStageDays: 28,
-            categories: {
-                experimental: {
-                    flagsOlderThanWeek: 11,
-                    newFlagsThisWeek: 4,
-                },
-                release: {
-                    flagsOlderThanWeek: 12,
-                    newFlagsThisWeek: 1,
-                },
-                permanent: {
-                    flagsOlderThanWeek: 7,
-                    newFlagsThisWeek: 0,
-                },
-            },
-        },
-        production: {
-            totalFlags: 10,
-            averageTimeInStageDays: 14,
-            categories: {
-                experimental: {
-                    flagsOlderThanWeek: 2,
-                    newFlagsThisWeek: 3,
-                },
-                release: {
-                    flagsOlderThanWeek: 1,
-                    newFlagsThisWeek: 1,
-                },
-                permanent: {
-                    flagsOlderThanWeek: 3,
-                    newFlagsThisWeek: 0,
-                },
-            },
-        },
-        cleanup: {
-            totalFlags: 5,
-            averageTimeInStageDays: 16,
-            categories: {
-                experimental: {
-                    flagsOlderThanWeek: 0,
-                    newFlagsThisWeek: 3,
-                },
-                release: {
-                    flagsOlderThanWeek: 0,
-                    newFlagsThisWeek: 1,
-                },
-                permanent: {
-                    flagsOlderThanWeek: 1,
-                    newFlagsThisWeek: 0,
-                },
-            },
-        },
-    };
 
     return (
         <InsightsSection
@@ -141,98 +84,141 @@ export const LifecycleInsights: FC = () => {
         >
             <ChartRow>
                 {Object.entries(mockData).map(([stage, data]) => {
-                    const oldData = [
-                        data.categories.experimental.flagsOlderThanWeek,
-                        data.categories.release.flagsOlderThanWeek,
-                        data.categories.permanent.flagsOlderThanWeek,
-                    ];
                     return (
                         <ChartContainer key={stage}>
-                            <LifecycleChart
-                                data={{
-                                    labels: [
-                                        `Experimental`,
-                                        `Release`,
-                                        `Other flags`,
-                                    ],
-                                    datasets: [
-                                        {
-                                            label: 'Flags > 1 week old',
-                                            data: oldData,
-                                            stack: '1',
-                                            backgroundColor:
-                                                chartColors.olderThanWeek,
-                                            borderRadius: 4,
-                                            datalabels: {
-                                                labels: {
-                                                    value: {
-                                                        formatter: (
-                                                            value,
-                                                            context,
-                                                        ) => {
-                                                            // todo (lifecycleMetrics): use a nice formatter here, so that 1,000,000 flags are instead formatted as 1M
-                                                            if (
-                                                                context.chart
-                                                                    .legend
-                                                                    ?.legendItems?.[1]
-                                                                    .hidden
-                                                            ) {
-                                                                return value;
-                                                            }
-                                                            return '';
-                                                        },
-                                                    },
-                                                },
-                                            },
-                                        },
-                                        {
-                                            label: 'New flags this week',
-                                            data: [
-                                                data.categories.experimental
-                                                    .newFlagsThisWeek,
-                                                data.categories.release
-                                                    .newFlagsThisWeek,
-                                                data.categories.permanent
-                                                    .newFlagsThisWeek,
-                                            ],
-                                            stack: '1',
-                                            backgroundColor:
-                                                chartColors.newThisWeek,
-                                            borderRadius: 4,
-                                            datalabels: {
-                                                labels: {
-                                                    value: {
-                                                        formatter: (
-                                                            value,
-                                                            context,
-                                                        ) => {
-                                                            if (
-                                                                context.chart
-                                                                    .legend
-                                                                    ?.legendItems?.[0]
-                                                                    .hidden
-                                                            ) {
-                                                                return value;
-                                                            }
-                                                            return (
-                                                                value +
-                                                                oldData[
-                                                                    context
-                                                                        .dataIndex
-                                                                ]
-                                                            );
-                                                        },
-                                                    },
-                                                },
-                                            },
-                                        },
-                                    ],
-                                }}
-                            />
+                            <Chart data={data} />
                         </ChartContainer>
                     );
                 })}
             </ChartRow>
         </InsightsSection>
     );
+};
+
+const Chart: React.FC<{ data: LifecycleTrend }> = ({ data }) => {
+    const chartColors = useChartColors();
+    const oldData = [
+        data.categories.experimental.flagsOlderThanWeek,
+        data.categories.release.flagsOlderThanWeek,
+        data.categories.permanent.flagsOlderThanWeek,
+    ];
+    return (
+        <LifecycleChart
+            data={{
+                labels: [`Experimental`, `Release`, `Other flags`],
+                datasets: [
+                    {
+                        label: 'Flags > 1 week old',
+                        data: oldData,
+                        stack: '1',
+                        backgroundColor: chartColors.olderThanWeek,
+                        borderRadius: 4,
+                        datalabels: {
+                            labels: {
+                                value: {
+                                    formatter: (value, context) => {
+                                        // todo (lifecycleMetrics): use a nice
+                                        // formatter here, so that 1,000,000
+                                        // flags are instead formatted as 1M
+                                        if (
+                                            context.chart.legend
+                                                ?.legendItems?.[1].hidden
+                                        ) {
+                                            return value;
+                                        }
+                                        return '';
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    {
+                        label: 'New flags this week',
+                        data: [
+                            data.categories.experimental.newFlagsThisWeek,
+                            data.categories.release.newFlagsThisWeek,
+                            data.categories.permanent.newFlagsThisWeek,
+                        ],
+                        stack: '1',
+                        backgroundColor: chartColors.newThisWeek,
+                        borderRadius: 4,
+                        datalabels: {
+                            labels: {
+                                value: {
+                                    formatter: (value, context) => {
+                                        if (
+                                            context.chart.legend
+                                                ?.legendItems?.[0].hidden
+                                        ) {
+                                            return value;
+                                        }
+                                        return (
+                                            value + oldData[context.dataIndex]
+                                        );
+                                    },
+                                },
+                            },
+                        },
+                    },
+                ],
+            }}
+        />
+    );
+};
+
+const mockData: LifecycleInsights = {
+    develop: {
+        totalFlags: 35,
+        averageTimeInStageDays: 28,
+        categories: {
+            experimental: {
+                flagsOlderThanWeek: 11,
+                newFlagsThisWeek: 4,
+            },
+            release: {
+                flagsOlderThanWeek: 12,
+                newFlagsThisWeek: 1,
+            },
+            permanent: {
+                flagsOlderThanWeek: 7,
+                newFlagsThisWeek: 0,
+            },
+        },
+    },
+    production: {
+        totalFlags: 10,
+        averageTimeInStageDays: 14,
+        categories: {
+            experimental: {
+                flagsOlderThanWeek: 2,
+                newFlagsThisWeek: 3,
+            },
+            release: {
+                flagsOlderThanWeek: 1,
+                newFlagsThisWeek: 1,
+            },
+            permanent: {
+                flagsOlderThanWeek: 3,
+                newFlagsThisWeek: 0,
+            },
+        },
+    },
+    cleanup: {
+        totalFlags: 5,
+        averageTimeInStageDays: 16,
+        categories: {
+            experimental: {
+                flagsOlderThanWeek: 0,
+                newFlagsThisWeek: 3,
+            },
+            release: {
+                flagsOlderThanWeek: 0,
+                newFlagsThisWeek: 1,
+            },
+            permanent: {
+                flagsOlderThanWeek: 1,
+                newFlagsThisWeek: 0,
+            },
+        },
+    },
 };

cc75ff8abac032afd26a1efd5d5abcdef489d9f1
Thomas Heartman <thomas@getunleash.io>
2025-06-11 09:25:46 +0200

    Make extra constraintId explicit

--- frontend/src/component/common/NewConstraintAccordion/NewConstraintAccordionList/createEmptyConstraint.ts
+++ frontend/src/component/common/NewConstraintAccordion/NewConstraintAccordionList/createEmptyConstraint.ts
@@ -2,10 +2,17 @@ import { dateOperators } from 'constants/operators';
 import type { IConstraint } from 'interfaces/strategy';
 import { oneOf } from 'utils/oneOf';
 import { operatorsForContext } from 'utils/operatorsForContext';
+import { v4 as uuidv4 } from 'uuid';
 
 export const constraintId = Symbol('id');
 
-export const createEmptyConstraint = (contextName: string): IConstraint => {
+type ConstraintWithId = IConstraint & {
+    [constraintId]: string;
+};
+
+export const createEmptyEditableConstraint = (
+    contextName: string,
+): ConstraintWithId => {
     const operator = operatorsForContext(contextName)[0];
 
     const value = oneOf(dateOperators, operator)
@@ -19,5 +26,6 @@ export const createEmptyConstraint = (contextName: string): IConstraint => {
         values: [],
         caseInsensitive: false,
         inverted: false,
+        [constraintId]: uuidv4(),
     };
 };

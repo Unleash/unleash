e53cf12a92fe36dc1e408015929643a5a0e7e5ed
Thomas Heartman <thomas@getunleash.io>
2025-06-11 14:33:21 +0200

    Update hooks to take new data

--- frontend/src/hooks/api/getters/useLifecycleInsights/useLifecycleInsights.ts
+++ frontend/src/hooks/api/getters/useLifecycleInsights/useLifecycleInsights.ts
@@ -2,7 +2,69 @@ import useSWR, { mutate, type SWRConfiguration } from 'swr';
 import { useCallback } from 'react';
 import { formatApiPath } from 'utils/formatPath';
 import handleErrorResponses from '../httpErrorResponseHandler.js';
-import type { InstanceInsightsSchema } from 'openapi';
+import type { LifecycleTrendsSchema } from 'openapi';
+
+const emptyLifecycleTrends: LifecycleTrendsSchema = {
+    lifecycleTrends: {
+        cleanup: {
+            categories: {
+                experimental: {
+                    flagsOlderThanWeek: 0,
+                    newFlagsThisWeek: 0,
+                },
+                permanent: {
+                    flagsOlderThanWeek: 0,
+                    newFlagsThisWeek: 0,
+                },
+                release: {
+                    flagsOlderThanWeek: 0,
+                    newFlagsThisWeek: 0,
+                },
+            },
+            medianDaysHistorically: 0,
+            medianDaysInCurrentStage: 0,
+            totalFlags: 0,
+        },
+        develop: {
+            categories: {
+                experimental: {
+                    flagsOlderThanWeek: 0,
+                    newFlagsThisWeek: 0,
+                },
+                permanent: {
+                    flagsOlderThanWeek: 0,
+                    newFlagsThisWeek: 0,
+                },
+                release: {
+                    flagsOlderThanWeek: 0,
+                    newFlagsThisWeek: 0,
+                },
+            },
+            medianDaysHistorically: 0,
+            medianDaysInCurrentStage: 0,
+            totalFlags: 0,
+        },
+        production: {
+            categories: {
+                experimental: {
+                    flagsOlderThanWeek: 0,
+                    newFlagsThisWeek: 0,
+                },
+                permanent: {
+                    flagsOlderThanWeek: 0,
+                    newFlagsThisWeek: 0,
+                },
+                release: {
+                    flagsOlderThanWeek: 0,
+                    newFlagsThisWeek: 0,
+                },
+            },
+            medianDaysHistorically: 0,
+            medianDaysInCurrentStage: 0,
+            totalFlags: 0,
+        },
+    },
+};
 
 export const useLifecycleInsights = (
     projects?: string[],
@@ -12,7 +74,7 @@ export const useLifecycleInsights = (
         `api/admin/insights/lifecycle?projects=${projects?.join(',')}`,
     );
 
-    const { data, error } = useSWR<InstanceInsightsSchema>(
+    const { data, error } = useSWR<LifecycleTrendsSchema>(
         path,
         fetchLifecycleInsights,
         options,
@@ -23,16 +85,7 @@ export const useLifecycleInsights = (
     }, [path]);
 
     return {
-        insights:
-            data ||
-            /* @ts-expect-error FIXME (lifecycleMetrics): lifecycle trends */
-            ({
-                userTrends: [],
-                flagTrends: [],
-                projectFlagTrends: [],
-                metricsSummaryTrends: [],
-                environmentTypeTrends: [],
-            } as InstanceInsightsSchema),
+        insights: data || emptyLifecycleTrends,
         refetchInsights,
         loading: !error && !data,
         error,
@@ -41,7 +94,7 @@ export const useLifecycleInsights = (
 
 const fetchLifecycleInsights = (
     path: string,
-): Promise<InstanceInsightsSchema> => {
+): Promise<LifecycleTrendsSchema> => {
     return fetch(path)
         .then(handleErrorResponses('Lifecycle Insights data'))
         .then((res) => res.json());

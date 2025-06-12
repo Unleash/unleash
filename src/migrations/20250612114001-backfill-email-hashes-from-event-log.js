6f70aed38a8a7714c57623f10b8d4bfa4d9b6000
Christopher Kolstad <chriswk@getunleash.io>
2025-06-12 13:59:11 +0200

    task: added backfill from last month of events

--- src/migrations/20250612114001-backfill-email-hashes-from-event-log.js
+++ src/migrations/20250612114001-backfill-email-hashes-from-event-log.js
@@ -1,16 +1,16 @@
 exports.up = function(db, cb) {
   db.runSql(`
     WITH deleted_users AS (
-        SELECT (pre_data ->> 'id' AS user_id,
-                pre_data ->> 'email' AS email)
+        SELECT (pre_data ->> 'id')::int AS user_id,
+                pre_data ->> 'email' AS email
         FROM events
         WHERE type = 'user-deleted'
         AND created_at >= now() - interval '30 days'
-    );
+    )
     UPDATE users u
-    SET email_hash = encode(sha256(du.email), 'hex')
+    SET email_hash = encode(sha256(du.email::bytea), 'hex')
     FROM deleted_users du
-    WHERE u.id = du.id AND email_hash IS NULL
+    WHERE u.id = du.user_id AND u.email IS NULL;
   `, cb);
 };
 

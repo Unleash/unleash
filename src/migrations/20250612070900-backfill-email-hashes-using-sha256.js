93dea64039740e1fcaa2e02670573c2d15db236e
Christopher Kolstad <chriswk@getunleash.io>
2025-06-12 09:53:33 +0200

    fix: create index after field has been updated for existing users

--- src/migrations/20250612070900-backfill-email-hashes-using-sha256.js
+++ src/migrations/20250612070900-backfill-email-hashes-using-sha256.js
@@ -2,8 +2,8 @@ exports.up = function(db, cb) {
   db.runSql(`
     ALTER TABLE users DROP COLUMN email_hash;
     ALTER TABLE users ADD COLUMN email_hash TEXT;
-    CREATE INDEX users_email_hash_idx ON users(email_hash);
     UPDATE users SET email_hash = encode(sha256(email::bytea), 'hex') WHERE email IS NOT NULL;
+    CREATE INDEX users_email_hash_idx ON users(email_hash);
 `, cb);
 };
 

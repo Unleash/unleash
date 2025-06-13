b70f862f93eca83ece1c89f081403ee6015bffb6
Christopher Kolstad <chriswk@getunleash.io>
2025-06-13 09:41:40 +0200

    fix: md5 is deprecated and fails FEDRAMP. Replace with sha256 (#10125)
    #10121 points out that we're using md5 functions still. This PR updates
    our migrations to no longer use md5 at all (so if you haven't run the
    migrations, you won't get email hashes until you get to the included
    migration with this PR). If you've already run the migrations, we'll
    drop the existing `email_hash varchar(32)` column and replace it with a
    `email_hash TEXT` column.
    
    We're also replacing the md5 function with `encode(sha256(email),
    'hex')`. encode has been supported since PG10, sha256 came with PG11.
    
    Do we want an index on the email_hash? I wasn't sure, but if we want to
    do lookup we probably should have an index on it (though not a unique
    one)

--- src/migrations/20250211114205-scim-email-hash-backfill.js
+++ src/migrations/20250211114205-scim-email-hash-backfill.js
@@ -1,14 +1,8 @@
 exports.up = (db, cb) => {
-    db.runSql(`
-        UPDATE users
-        SET email_hash = md5(email::text)
-        WHERE scim_id IS NOT NULL;
-    `, cb);
-  
+    cb();
   };
-  
+
   exports.down = (db, cb) => {
     cb();
   };
-  
-  
+

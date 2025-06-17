0bad9101fc7c78b0081f7513399b875d35350bd0
Gastón Fournier <gaston@getunleash.io>
2025-06-17 15:37:57 +0200

    chore: use logger instead of console.error (#10150)

--- src/lib/features/scheduler/schedule-services.ts
+++ src/lib/features/scheduler/schedule-services.ts
@@ -152,13 +152,13 @@ export const scheduleServices = (
     );
 
     schedulerService.schedule(
-        () => clientMetricsServiceV2.bulkAdd().catch(console.error),
+        () => clientMetricsServiceV2.bulkAdd(),
         secondsToMilliseconds(5),
         'bulkAddMetrics',
     );
 
     schedulerService.schedule(
-        () => clientMetricsServiceV2.clearMetrics(48).catch(console.error),
+        () => clientMetricsServiceV2.clearMetrics(48),
         hoursToMilliseconds(12),
         'clearMetrics',
     );

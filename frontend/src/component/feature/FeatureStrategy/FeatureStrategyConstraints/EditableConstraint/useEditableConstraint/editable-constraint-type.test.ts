999a5ea9c147458611996e262d3169c093edcc38
Thomas Heartman <thomas@getunleash.io>
2025-06-18 12:53:07 +0200

    enforce rendering order

---
+++ frontend/src/component/feature/FeatureStrategy/FeatureStrategyConstraints/EditableConstraint/useEditableConstraint/editable-constraint-type.test.ts
@@ -0,0 +1,45 @@
+import { createEmptyConstraint } from 'utils/createEmptyConstraint';
+import { fromIConstraint, toIConstraint } from './editable-constraint-type.ts';
+import { constraintId } from 'constants/constraintId';
+
+test('mapping to and from retains the constraint id', () => {
+    const constraint = createEmptyConstraint('context');
+
+    expect(toIConstraint(fromIConstraint(constraint))[constraintId]).toEqual(
+        constraint[constraintId],
+    );
+});
+
+test('mapping to an editable constraint adds a constraint id if there is none', () => {
+    const constraint = createEmptyConstraint('context');
+    // @ts-expect-error: this violates the type constraint
+    delete constraint[constraintId];
+
+    const editableConstraint = fromIConstraint(constraint);
+
+    expect(editableConstraint[constraintId]).toBeDefined();
+
+    const iConstraint = toIConstraint(editableConstraint);
+    expect(iConstraint[constraintId]).toEqual(editableConstraint[constraintId]);
+});
+
+test('mapping from an empty constraint removes redundant value / values', () => {
+    const constraint = createEmptyConstraint('context');
+    expect('value' in constraint).toBe(true);
+
+    const transformed = toIConstraint(fromIConstraint(constraint));
+    expect('value' in transformed).toBe(false);
+});
+
+test('mapping to constraint returns properties in expected order', () => {
+    const constraint = createEmptyConstraint('context');
+    const transformed = toIConstraint(fromIConstraint(constraint));
+
+    expect(Object.keys(transformed)).toEqual([
+        'values',
+        'inverted',
+        'operator',
+        'contextName',
+        'caseInsensitive',
+    ]);
+});

f80e9a9e17bb0bd1ed6fa3011d6bd116159f84f7
David Leek <david@getunleash.io>
2025-06-24 11:22:15 +0200

    feat(change-requests): requesting reviews when submitting change requests

---
+++ frontend/src/hooks/api/getters/useAvailableChangeRequestReviewers/useAvailableChangeRequestReviewers.ts
@@ -0,0 +1,47 @@
+import useSWR from 'swr';
+import { useMemo } from 'react';
+import { formatApiPath } from 'utils/formatPath';
+import handleErrorResponses from '../httpErrorResponseHandler.js';
+
+export interface AvailableReviewerSchema {
+    id: string;
+    name?: string;
+    email: string;
+    username?: string;
+    imageUrl?: string;
+}
+
+export interface IAvailableReviewersResponse {
+    reviewers: AvailableReviewerSchema[];
+    refetchReviewers: () => void;
+    loading: boolean;
+    error?: Error;
+}
+
+export const useAvailableChangeRequestReviewers = (
+    project: string,
+    environment: string,
+): IAvailableReviewersResponse => {
+    const { data, error, mutate } = useSWR(
+        formatApiPath(
+            `api/admin/projects/${project}/change-requests/available-reviewers/${environment}`,
+        ),
+        fetcher,
+    );
+
+    return useMemo(
+        () => ({
+            reviewers: data?.reviewers || [],
+            loading: !error && !data,
+            refetchReviewers: () => mutate(),
+            error,
+        }),
+        [data, error, mutate],
+    );
+};
+
+const fetcher = (path: string) => {
+    return fetch(path)
+        .then(handleErrorResponses('Available Change Request Reviewers'))
+        .then((res) => res.json());
+};

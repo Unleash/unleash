5bafa852ab07ac5eb15ff2a3dde860bae523221e
Thomas Heartman <thomas@getunleash.io>
2025-07-02 13:45:57 +0200

    remove redundant code

--- frontend/src/component/changeRequest/ChangeRequest/Changes/Change/ChangeSegmentName.tsx
+++ frontend/src/component/changeRequest/ChangeRequest/Changes/Change/ChangeSegmentName.tsx
@@ -1,12 +1,3 @@
-import type { FC } from 'react';
 import { NameWithChangeInfo } from './NameWithChangeInfo/NameWithChangeInfo.tsx';
 
-type ChangeSegmentNameProps = {
-    name?: string;
-    previousName?: string;
-};
-
-export const ChangeSegmentName: FC<ChangeSegmentNameProps> = ({
-    name,
-    previousName,
-}) => <NameWithChangeInfo previousName={previousName} newName={name} />;
+export const ChangeSegmentName = NameWithChangeInfo;

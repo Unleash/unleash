607700b932cf250350e050f37bf28b27a0ad957d
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-07-29 20:43:34 +0200

    refactor: remove conditionally render

--- frontend/src/component/common/Table/SortableTableHeader/CellSortable/CellSortable.tsx
+++ frontend/src/component/common/Table/SortableTableHeader/CellSortable/CellSortable.tsx
@@ -9,7 +9,6 @@ import {
     useState,
 } from 'react';
 import { Tooltip } from '@mui/material';
-import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
 import { AnnouncerContext } from 'component/common/Announcer/AnnouncerContext/AnnouncerContext';
 import {
     StyledButton,
@@ -114,42 +113,40 @@ export const CellSortable: FC<ICellSortableProps> = ({
             isFlexGrow={isFlexGrow}
             isSortable={isSortable}
         >
-            <ConditionallyRender
-                condition={isSortable}
-                show={
-                    <Tooltip title={tooltipTitle} arrow>
-                        <StyledButton
-                            isSorted={isSorted}
-                            type='button'
-                            onClick={onSortClick}
+            {isSortable ? (
+                <Tooltip title={tooltipTitle} arrow>
+                    <StyledButton
+                        isSorted={isSorted}
+                        type='button'
+                        onClick={onSortClick}
+                    >
+                        <StyledHiddenMeasurementLayer
+                            style={alignStyle}
+                            aria-hidden
                         >
-                            <StyledHiddenMeasurementLayer
-                                style={alignStyle}
-                                aria-hidden
-                            >
-                                <StyledLabel tabIndex={-1} data-text={children}>
-                                    {children}
-                                </StyledLabel>
-                                <SortArrow
-                                    isSorted={isSorted}
-                                    isDesc={isDescending}
-                                />
-                            </StyledHiddenMeasurementLayer>
-                            <StyledVisibleAbsoluteLayer style={alignStyle}>
-                                <span ref={ref} tabIndex={-1}>
-                                    <span>{children}</span>
-                                </span>
-                                <SortArrow
-                                    isSorted={isSorted}
-                                    isDesc={isDescending}
-                                    className='sort-arrow'
-                                />
-                            </StyledVisibleAbsoluteLayer>
-                        </StyledButton>
-                    </Tooltip>
-                }
-                elseShow={<div style={alignStyle}>{children}</div>}
-            />
+                            <StyledLabel tabIndex={-1} data-text={children}>
+                                {children}
+                            </StyledLabel>
+                            <SortArrow
+                                isSorted={isSorted}
+                                isDesc={isDescending}
+                            />
+                        </StyledHiddenMeasurementLayer>
+                        <StyledVisibleAbsoluteLayer style={alignStyle}>
+                            <span ref={ref} tabIndex={-1}>
+                                <span>{children}</span>
+                            </span>
+                            <SortArrow
+                                isSorted={isSorted}
+                                isDesc={isDescending}
+                                className='sort-arrow'
+                            />
+                        </StyledVisibleAbsoluteLayer>
+                    </StyledButton>
+                </Tooltip>
+            ) : (
+                <div style={alignStyle}>{children}</div>
+            )}
         </StyledTableCell>
     );
 };

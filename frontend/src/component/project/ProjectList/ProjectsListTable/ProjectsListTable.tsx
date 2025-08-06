44650e4e2f9c25e31e4be665b67e3d6b7c2d7165
Nuno Góis <github@nunogois.com>
2025-08-06 11:36:21 +0100

    chore: project list table view (#10466)
    https://linear.app/unleash/issue/2-3740/implement-the-project-list-view
    
    Implements the list (table) view of the projects page.
    
    <img width="1300" height="956" alt="image"
    src="https://github.com/user-attachments/assets/603bc9a8-21a9-4888-8804-1be23e2d63ee"
    />
    
    <img width="1300" height="681" alt="image"
    src="https://github.com/user-attachments/assets/67f6e8e9-cedf-4a70-ba95-b9c73e8d29a8"
    />

---
+++ frontend/src/component/project/ProjectList/ProjectsListTable/ProjectsListTable.tsx
@@ -0,0 +1,136 @@
+import { VirtualizedTable } from 'component/common/Table';
+import { FavoriteIconCell } from 'component/common/Table/cells/FavoriteIconCell/FavoriteIconCell';
+import { HighlightCell } from 'component/common/Table/cells/HighlightCell/HighlightCell';
+import { TextCell } from 'component/common/Table/cells/TextCell/TextCell';
+import { TimeAgoCell } from 'component/common/Table/cells/TimeAgoCell/TimeAgoCell';
+import { ProjectOwners } from 'component/project/ProjectCard/ProjectCardFooter/ProjectOwners/ProjectOwners';
+import { ProjectLastSeen } from 'component/project/ProjectCard/ProjectLastSeen/ProjectLastSeen';
+import { useFavoriteProjectsApi } from 'hooks/api/actions/useFavoriteProjectsApi/useFavoriteProjectsApi';
+import useProjects from 'hooks/api/getters/useProjects/useProjects';
+import type { ProjectSchema, ProjectSchemaOwners } from 'openapi';
+import { useCallback, useMemo } from 'react';
+import { useFlexLayout, useTable } from 'react-table';
+import { formatDateYMDHMS } from 'utils/formatDate';
+import { ProjectsListTableProjectName } from './ProjectsListTableProjectName.tsx';
+
+type ProjectsListTableProps = {
+    projects: ProjectSchema[];
+};
+
+export const ProjectsListTable = ({ projects }: ProjectsListTableProps) => {
+    const { refetch } = useProjects();
+    const { favorite, unfavorite } = useFavoriteProjectsApi();
+
+    const onFavorite = useCallback(
+        async (project: ProjectSchema) => {
+            if (project?.favorite) {
+                await unfavorite(project.id);
+            } else {
+                await favorite(project.id);
+            }
+            refetch();
+        },
+        [refetch],
+    );
+
+    const columns = useMemo(
+        () => [
+            {
+                Header: '',
+                accessor: 'favorite',
+                Cell: ({ row }: { row: { original: ProjectSchema } }) => (
+                    <FavoriteIconCell
+                        value={row.original.favorite}
+                        onClick={() => onFavorite(row.original)}
+                    />
+                ),
+                width: 40,
+            },
+            {
+                Header: 'Project name',
+                accessor: 'name',
+                minWidth: 200,
+                searchable: true,
+                Cell: ProjectsListTableProjectName,
+            },
+            {
+                Header: 'Last updated',
+                id: 'lastUpdatedAt',
+                Cell: ({ row }: { row: { original: ProjectSchema } }) => (
+                    <TimeAgoCell
+                        value={
+                            row.original.lastUpdatedAt || row.original.createdAt
+                        }
+                        dateFormat={formatDateYMDHMS}
+                    />
+                ),
+            },
+            {
+                Header: 'Number of flags',
+                accessor: 'featureCount',
+                Cell: ({ value }: { value: number }) => (
+                    <TextCell>
+                        {value} flag{value === 1 ? '' : 's'}
+                    </TextCell>
+                ),
+            },
+            {
+                Header: 'Health',
+                accessor: 'health',
+                Cell: ({ value }: { value: number }) => (
+                    <TextCell>{value}%</TextCell>
+                ),
+                width: 100,
+            },
+            {
+                Header: 'Last seen',
+                accessor: 'lastReportedFlagUsage',
+                Cell: ({ value }: { value: Date }) => (
+                    <ProjectLastSeen date={value} />
+                ),
+            },
+            {
+                Header: 'Owner',
+                accessor: 'owners',
+                Cell: ({ value }: { value: ProjectSchemaOwners }) => (
+                    <ProjectOwners
+                        owners={value?.filter(
+                            (owner) => owner.ownerType !== 'system',
+                        )}
+                    />
+                ),
+            },
+            {
+                Header: 'Members',
+                accessor: 'memberCount',
+                Cell: ({ value }: { value: number }) => (
+                    <TextCell>{value} members</TextCell>
+                ),
+            },
+        ],
+        [onFavorite],
+    );
+
+    const { headerGroups, rows, prepareRow } = useTable(
+        {
+            columns: columns as any,
+            data: projects,
+            autoResetHiddenColumns: false,
+            autoResetSortBy: false,
+            disableSortRemove: true,
+            disableMultiSort: true,
+            defaultColumn: {
+                Cell: HighlightCell,
+            },
+        },
+        useFlexLayout,
+    );
+
+    return (
+        <VirtualizedTable
+            rows={rows}
+            headerGroups={headerGroups}
+            prepareRow={prepareRow}
+        />
+    );
+};
---
+++ frontend/src/component/project/ProjectList/ProjectsListTable/ProjectsListTableProjectName.tsx
@@ -0,0 +1,46 @@
+import { styled } from '@mui/material';
+import { Highlighter } from 'component/common/Highlighter/Highlighter';
+import { useSearchHighlightContext } from 'component/common/Table/SearchHighlightContext/SearchHighlightContext';
+import { Truncator } from 'component/common/Truncator/Truncator';
+import { ProjectModeBadge } from 'component/project/ProjectCard/ProjectModeBadge/ProjectModeBadge';
+import type { ProjectSchema } from 'openapi';
+import { Link } from 'react-router-dom';
+
+const StyledCellContainer = styled('div')(({ theme }) => ({
+    display: 'inline-flex',
+    alignItems: 'center',
+    gap: theme.spacing(1),
+    padding: theme.spacing(1, 2),
+}));
+
+const StyledFeatureLink = styled(Link)({
+    textDecoration: 'none',
+    '&:hover, &:focus': {
+        textDecoration: 'underline',
+    },
+});
+
+type ProjectsListTableProjectNameProps = {
+    row: {
+        original: ProjectSchema;
+    };
+};
+
+export const ProjectsListTableProjectName = ({
+    row,
+}: ProjectsListTableProjectNameProps) => {
+    const { searchQuery } = useSearchHighlightContext();
+
+    return (
+        <StyledCellContainer>
+            <ProjectModeBadge mode={row.original.mode} />
+            <StyledFeatureLink to={`/projects/${row.original.id}`}>
+                <Truncator title={row.original.name} lines={2} arrow>
+                    <Highlighter search={searchQuery}>
+                        {row.original.name}
+                    </Highlighter>
+                </Truncator>
+            </StyledFeatureLink>
+        </StyledCellContainer>
+    );
+};

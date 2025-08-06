d2545aab91e65b3e3f5c4402632b0026f2b250b6
Nuno Góis <github@nunogois.com>
2025-08-06 11:26:17 +0100

    chore: truncate project name in project list

--- frontend/src/component/project/ProjectList/ProjectsListTable/ProjectsListTableProjectName.tsx
+++ frontend/src/component/project/ProjectList/ProjectsListTable/ProjectsListTableProjectName.tsx
@@ -1,6 +1,7 @@
 import { styled } from '@mui/material';
 import { Highlighter } from 'component/common/Highlighter/Highlighter';
 import { useSearchHighlightContext } from 'component/common/Table/SearchHighlightContext/SearchHighlightContext';
+import { Truncator } from 'component/common/Truncator/Truncator';
 import { ProjectModeBadge } from 'component/project/ProjectCard/ProjectModeBadge/ProjectModeBadge';
 import type { ProjectSchema } from 'openapi';
 import { Link } from 'react-router-dom';
@@ -34,9 +35,11 @@ export const ProjectsListTableProjectName = ({
         <StyledCellContainer>
             <ProjectModeBadge mode={row.original.mode} />
             <StyledFeatureLink to={`/projects/${row.original.id}`}>
-                <Highlighter search={searchQuery}>
-                    {row.original.name}
-                </Highlighter>
+                <Truncator title={row.original.name} lines={2} arrow>
+                    <Highlighter search={searchQuery}>
+                        {row.original.name}
+                    </Highlighter>
+                </Truncator>
             </StyledFeatureLink>
         </StyledCellContainer>
     );

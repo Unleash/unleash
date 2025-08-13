7a9ed98bc19abb4f3f26089a3e361a4b23c3bd33
sjaanus <sellinjaanus@gmail.com>
2025-08-13 15:09:53 +0300

    feat: add origin column for consumption based tables

---
+++ src/migrations/202508131142433-consumption-origin.js
@@ -0,0 +1,37 @@
+exports.up = function (db, cb) {
+    db.runSql(
+        `
+            ALTER TABLE request_count_consumption
+                ADD COLUMN origin TEXT NOT NULL DEFAULT 'sdk';
+
+            ALTER TABLE request_count_consumption
+            DROP CONSTRAINT request_count_consumption_pkey,
+                ADD PRIMARY KEY(day, metered_group, origin);
+
+            ALTER TABLE connection_count_consumption
+                ADD COLUMN origin TEXT NOT NULL DEFAULT 'sdk';
+
+            ALTER TABLE connection_count_consumption
+            DROP CONSTRAINT connection_count_consumption_pkey,
+                ADD PRIMARY KEY(day, metered_group, origin);
+        `,
+        cb
+    );
+};
+
+exports.down = function (db, cb) {
+    db.runSql(
+        `
+            ALTER TABLE request_count_consumption
+                DROP CONSTRAINT request_count_consumption_pkey,
+                DROP COLUMN origin,
+                ADD PRIMARY KEY(day, metered_group);
+
+            ALTER TABLE connection_count_consumption
+                DROP CONSTRAINT connection_count_consumption_pkey,
+                DROP COLUMN origin,
+                ADD PRIMARY KEY(day, metered_group);
+        `,
+        cb
+    );
+};

dbe95ad324a70c97d7ca099fe0f3f9cf0f3c232e
FredrikOseberg <fredrik.no@gmail.com>
2025-08-22 13:58:31 +0200

    fix: rm custom component

--- website/src/theme/ApiItem/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ApiItemOriginal from '@theme-original/ApiItem';
-
-// Simple wrapper that doesn't break SSR
-export default function ApiItem(props: any) {
-    return <ApiItemOriginal {...props} />;
-}

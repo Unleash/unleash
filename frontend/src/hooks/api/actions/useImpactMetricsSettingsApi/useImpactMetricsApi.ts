5c78c5cbe2ea17630dc6b35b4e87279663d8d96d
kwasniew <kwasniewski.mateusz@gmail.com>
2025-08-29 11:04:14 +0200

    feat: make impact metrics config save call

---
+++ frontend/src/hooks/api/actions/useImpactMetricsSettingsApi/useImpactMetricsApi.ts
@@ -0,0 +1,32 @@
+import { useCallback } from 'react';
+import useAPI from '../useApi/useApi.js';
+import type { ImpactMetricsConfigSchema } from '../../../../openapi.js';
+
+export const useImpactMetricsApi = () => {
+    const { makeRequest, createRequest, errors, loading } = useAPI({
+        propagateErrors: true,
+    });
+
+    const updateImpactMetric = useCallback(
+        async (config: ImpactMetricsConfigSchema) => {
+            const path = `api/admin/impact-metrics/config`;
+            const req = createRequest(
+                path,
+                {
+                    method: 'POST',
+                    body: JSON.stringify(config),
+                },
+                'updateImpactMetric',
+            );
+
+            return makeRequest(req.caller, req.id);
+        },
+        [makeRequest, createRequest],
+    );
+
+    return {
+        updateImpactMetric,
+        errors,
+        loading,
+    };
+};

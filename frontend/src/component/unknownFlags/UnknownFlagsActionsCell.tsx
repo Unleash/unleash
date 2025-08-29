fe2ef2ac8f30b37dcba29c1d1cee27cf87a482cf
Nuno Góis <github@nunogois.com>
2025-08-29 10:43:54 +0100

    chore: improved project suggestion logic

--- frontend/src/component/unknownFlags/UnknownFlagsActionsCell.tsx
+++ frontend/src/component/unknownFlags/UnknownFlagsActionsCell.tsx
@@ -6,6 +6,8 @@ import type { UnknownFlag } from './hooks/useUnknownFlags.js';
 import { Link } from 'react-router-dom';
 import useProjects from 'hooks/api/getters/useProjects/useProjects.js';
 import { DEFAULT_PROJECT_ID } from 'hooks/api/getters/useDefaultProject/useDefaultProjectId.js';
+import AccessContext from 'contexts/AccessContext.js';
+import { useContext } from 'react';
 
 const StyledBox = styled(Box)(() => ({
     display: 'flex',
@@ -20,18 +22,27 @@ export const UnknownFlagsActionsCell = ({
     unknownFlag,
 }: IUnknownFlagsActionsCellProps) => {
     const { projects } = useProjects();
+    const { hasAccess } = useContext(AccessContext);
 
-    const project = projects.find(({ id }) => id === DEFAULT_PROJECT_ID)
-        ? DEFAULT_PROJECT_ID
-        : projects[0]?.id || DEFAULT_PROJECT_ID;
+    let project =
+        projects.find(({ id }) => id === DEFAULT_PROJECT_ID) || projects[0];
+    if (!hasAccess(CREATE_FEATURE, project?.id)) {
+        for (const proj of projects) {
+            if (hasAccess(CREATE_FEATURE, proj.id)) {
+                project = proj;
+                break;
+            }
+        }
+    }
 
     return (
         <StyledBox>
             <PermissionIconButton
                 component={Link}
                 data-loading
-                to={`/projects/${project}?create=true&name=${unknownFlag.name}`}
+                to={`/projects/${project?.id}?create=true&name=${unknownFlag.name}`}
                 permission={CREATE_FEATURE}
+                projectId={project?.id}
                 tooltipProps={{
                     title: 'Create feature flag',
                 }}

d50d393a68955cec47ec29a4f2c71fec8e8ea586
Nuno Góis <github@nunogois.com>
2025-09-01 18:01:14 +0100

    chore: make create feature flag button in unknown flags a text button

--- frontend/src/component/unknownFlags/UnknownFlagsActionsCell.tsx
+++ frontend/src/component/unknownFlags/UnknownFlagsActionsCell.tsx
@@ -1,13 +1,9 @@
-import Add from '@mui/icons-material/Add';
 import { Box, styled } from '@mui/material';
-import PermissionIconButton from 'component/common/PermissionIconButton/PermissionIconButton';
 import { CREATE_FEATURE } from 'component/providers/AccessProvider/permissions';
 import type { UnknownFlag } from './hooks/useUnknownFlags.js';
-import { Link } from 'react-router-dom';
-import useProjects from 'hooks/api/getters/useProjects/useProjects.js';
-import { DEFAULT_PROJECT_ID } from 'hooks/api/getters/useDefaultProject/useDefaultProjectId.js';
-import AccessContext from 'contexts/AccessContext.js';
-import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import PermissionButton from 'component/common/PermissionButton/PermissionButton.js';
+import type { ProjectSchema } from 'openapi/index.js';
 
 const StyledBox = styled(Box)(() => ({
     display: 'flex',
@@ -16,39 +12,32 @@ const StyledBox = styled(Box)(() => ({
 
 interface IUnknownFlagsActionsCellProps {
     unknownFlag: UnknownFlag;
+    suggestedProject?: ProjectSchema;
 }
 
 export const UnknownFlagsActionsCell = ({
     unknownFlag,
+    suggestedProject,
 }: IUnknownFlagsActionsCellProps) => {
-    const { projects } = useProjects();
-    const { hasAccess } = useContext(AccessContext);
+    const navigate = useNavigate();
 
-    let project =
-        projects.find(({ id }) => id === DEFAULT_PROJECT_ID) || projects[0];
-    if (!hasAccess(CREATE_FEATURE, project?.id)) {
-        for (const proj of projects) {
-            if (hasAccess(CREATE_FEATURE, proj.id)) {
-                project = proj;
-                break;
-            }
-        }
-    }
+    if (!suggestedProject) return null;
 
     return (
         <StyledBox>
-            <PermissionIconButton
-                component={Link}
-                data-loading
-                to={`/projects/${project?.id}?create=true&name=${unknownFlag.name}`}
+            <PermissionButton
+                variant='text'
+                size='small'
                 permission={CREATE_FEATURE}
-                projectId={project?.id}
-                tooltipProps={{
-                    title: 'Create feature flag',
-                }}
+                projectId={suggestedProject.id}
+                onClick={() =>
+                    navigate(
+                        `/projects/${suggestedProject.id}?create=true&name=${unknownFlag.name}`,
+                    )
+                }
             >
-                <Add />
-            </PermissionIconButton>
+                Create feature flag
+            </PermissionButton>
         </StyledBox>
     );
 };

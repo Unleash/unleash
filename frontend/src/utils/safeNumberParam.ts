386a42aafa5630769bdcd05eb771b8bfd0d43438
kwasniew <kwasniewski.mateusz@gmail.com>
2025-09-09 13:09:18 +0200

    fix: limit and offset with non numbers

--- frontend/src/utils/safeNumberParam.ts
+++ frontend/src/utils/safeNumberParam.ts
@@ -1,5 +1,8 @@
 import { encodeNumber, decodeNumber } from 'serialize-query-params';
 
+/**
+ * @see: https://github.com/pbeshai/use-query-params/issues/175#issuecomment-982791559
+ */
 export const SafeNumberParam = {
     encode: encodeNumber,
     decode: (input: any) => {

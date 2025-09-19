d58ed1dee29df326575e451ee6d692a636437b36
kwasniew <kwasniewski.mateusz@gmail.com>
2025-09-19 11:07:01 +0200

    refactor: remove unused code

--- frontend/src/hooks/api/actions/useApplicationsApi/useApplicationsApi.ts
+++ frontend/src/hooks/api/actions/useApplicationsApi/useApplicationsApi.ts
@@ -7,22 +7,6 @@ const useApplicationsApi = () => {
 
     const URI = 'api/admin/metrics/applications';
 
-    const storeApplicationMetaData = async (
-        appName: string,
-        key: string,
-        value: string,
-    ) => {
-        const data: { [key: string]: any } = {};
-        data[key] = value;
-        const path = `${URI}/${appName}`;
-        const req = createRequest(path, {
-            method: 'POST',
-            body: JSON.stringify(data),
-        });
-
-        return makeRequest(req.caller, req.id);
-    };
-
     const deleteApplication = async (appName: string) => {
         const path = `${URI}/${encodeURIComponent(appName)}`;
         const req = createRequest(path, { method: 'DELETE' });
@@ -31,7 +15,6 @@ const useApplicationsApi = () => {
     };
 
     return {
-        storeApplicationMetaData,
         deleteApplication,
         errors,
         loading,

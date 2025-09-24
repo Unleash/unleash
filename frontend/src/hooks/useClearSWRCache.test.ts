e3fc88b11f42e7463e9040afdf8097488e9d8d96
Thomas Heartman <thomas@getunleash.io>
2025-09-24 14:37:58 +0200

    fix: cache eviction bug and the eternal loading screen (#10689)
    Fixes an issue where the project feature list (and potentially other
    places in the app that use the `useClearSWRCache` hook) would end up in
    an infinite loading screen because the latest entry that we want to show
    was overwritten.
    
    The primary reason this happened is that we used `keysToDelete =
    array.slice(SWR_CACHE_SIZE - 1)`. Because the map keys are returned in
    insertion order, this would make us never delete the oldest keys, but
    always anything after the cache reached it's maximum size. The fix was
    to instead do `slice(0, -(SWR_CACHE_SIZE - 1))`, unless that is `0, 0`.
    If so, then just delete the entire filtered keys set.
    
    As a bonus: this PR also deduplicates cache entries that have the same
    query params but in different order for the feature search. This further
    reduces the cache space needed.

--- frontend/src/hooks/useClearSWRCache.test.ts
+++ frontend/src/hooks/useClearSWRCache.test.ts
@@ -14,7 +14,7 @@ describe('manageCacheEntries', () => {
         expect(cacheMock.has('prefix-3')).toBe(true);
     });
 
-    it('should keep the SWR_CACHE_SIZE entries and delete the rest', () => {
+    it('should keep the SWR_CACHE_SIZE entries and delete the rest, keeping the most recent entries', () => {
         const cacheMock = new Map();
         cacheMock.set('prefix-1', {});
         cacheMock.set('prefix-2', {});
@@ -23,8 +23,19 @@ describe('manageCacheEntries', () => {
 
         clearCacheEntries(cacheMock, 'prefix-4', 'prefix-', 2);
 
-        expect(cacheMock.has('prefix-4')).toBe(true);
-        expect([...cacheMock.keys()].length).toBe(2);
+        expect([...cacheMock.keys()]).toStrictEqual(['prefix-3', 'prefix-4']);
+    });
+
+    it('should not delete the current key, even if it would be cleared as part of the cache size limit', () => {
+        const cacheMock = new Map();
+        cacheMock.set('prefix-1', {});
+        cacheMock.set('prefix-2', {});
+        cacheMock.set('prefix-3', {});
+        cacheMock.set('prefix-4', {});
+
+        clearCacheEntries(cacheMock, 'prefix-2', 'prefix-', 2);
+
+        expect([...cacheMock.keys()]).toStrictEqual(['prefix-2', 'prefix-4']);
     });
 
     it('should handle case when SWR_CACHE_SIZE is larger than number of entries', () => {
@@ -49,5 +60,17 @@ describe('manageCacheEntries', () => {
         expect(cacheMock.has('prefix-1')).toBe(true);
         expect(cacheMock.has('other-2')).toBe(true);
         expect(cacheMock.has('prefix-3')).toBe(true);
+    });
+
+    it('treats a negative cache size as zero', () => {
+        const cacheMock = new Map();
+        cacheMock.set('prefix-1', {});
+        cacheMock.set('prefix-2', {});
+        cacheMock.set('prefix-3', {});
+        cacheMock.set('prefix-4', {});
+
+        clearCacheEntries(cacheMock, 'prefix-3', 'prefix-', -1);
+
+        expect([...cacheMock.keys()]).toStrictEqual(['prefix-3']);
     });
 });

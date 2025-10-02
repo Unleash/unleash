c03df86ee006f531024137a1cfc846eb282ab672
Gastón Fournier <gaston@getunleash.io>
2025-10-02 18:10:29 +0200

    Add tests and validations to always return next page

--- src/lib/features/users/user-updates-read-model.ts
+++ src/lib/features/users/user-updates-read-model.ts
@@ -50,20 +50,18 @@ export class UserUpdatesReadModel {
         return result ? result.last_updated_at : null;
     }
 
-    /**
-     * @deprecated use getUsersUpdatedAfterOrEqual instead. We could have users updated at the exact same time
-     * as the last fetched user, so we need to include that timestamp in the next fetch to avoid missing users, even at the cost of processing some users twice.
-     */
+    /** @deprecated */
     async getUsersUpdatedAfter(
         date: Date,
         limit: number = 100,
     ): Promise<UpdatedUser[]> {
-        return this.getUsersUpdatedAfterOrEqual(date, limit);
+        return this.getUsersUpdatedAfterOrEqual(date, limit, 0);
     }
 
     async getUsersUpdatedAfterOrEqual(
         date: Date,
         limit: number = 100,
+        afterId: number = 0,
     ): Promise<UpdatedUser[]> {
         const result = await this.db(USERS_TABLE)
             .where({
@@ -71,8 +69,15 @@ export class UserUpdatesReadModel {
                 is_system: false,
                 is_service: false,
             })
-            .where('updated_at', '>=', date)
+            .where((builder) => {
+                builder.where('updated_at', '>', date).orWhere((subBuilder) => {
+                    subBuilder
+                        .where('updated_at', '=', date)
+                        .where('id', '>', afterId);
+                });
+            })
             .orderBy('updated_at', 'asc')
+            .orderBy('id', 'asc')
             .select([
                 ...USER_COLUMNS_PUBLIC,
                 'created_at',

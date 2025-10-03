9963f05bdc622d56e803694d8089f7ec328ce5d9
Gastón Fournier <gaston@getunleash.io>
2025-10-03 17:41:14 +0200

    Fix tests

--- src/test/e2e/users/user-updates-read-model.e2e.test.ts
+++ src/test/e2e/users/user-updates-read-model.e2e.test.ts
@@ -15,10 +15,10 @@ beforeEach(async () => {
 
 test('should have no users', async () => {
     const readModel = stores.userUpdatesReadModel;
-    const lastUpdatedAt = await readModel.getLastUpdatedAt();
-    expect(lastUpdatedAt).toBeNull();
+    const lastUpdate = await readModel.getLastUpdatedAt();
+    expect(lastUpdate).toBeNull();
 
-    const users = await readModel.getUsersUpdatedAfter(new Date(0));
+    const users = await readModel.getUsersUpdatedAfterOrEqual(new Date(0));
     expect(users).toEqual([]);
 });
 
@@ -28,15 +28,15 @@ test('Adding a user should return that user', async () => {
     const beforeInsert = new Date(Date.now() - 1000);
     await userStore.upsert({ email: 'test@example.com' });
 
-    const lastUpdatedAt = await readModel.getLastUpdatedAt();
-    expect(lastUpdatedAt).toBeDefined();
-    expect(lastUpdatedAt).toBeInstanceOf(Date);
+    const lastUpdate = await readModel.getLastUpdatedAt();
+    expect(lastUpdate).toBeDefined();
+    expect(lastUpdate!.lastUpdatedAt).toBeInstanceOf(Date);
     // check that it's recent
-    expect(lastUpdatedAt!.getTime()).toBeGreaterThanOrEqual(
+    expect(lastUpdate!.lastUpdatedAt!.getTime()).toBeGreaterThanOrEqual(
         beforeInsert.getTime(),
     );
 
-    const users = await readModel.getUsersUpdatedAfter(beforeInsert);
+    const users = await readModel.getUsersUpdatedAfterOrEqual(beforeInsert);
     expect(users).toHaveLength(1);
     expect(users[0].email).toBe('test@example.com');
     expect(users[0].createdAt).toBeInstanceOf(Date);
@@ -52,23 +52,25 @@ test('Modifying a user should return that user', async () => {
     });
 
     const afterInsert = new Date();
-    const lastUpdatedAt = await readModel.getLastUpdatedAt();
-    expect(lastUpdatedAt).toBeDefined();
+    const lastUpdate = await readModel.getLastUpdatedAt();
+    expect(lastUpdate).toBeDefined();
+    const lastUpdatedAt = lastUpdate!.lastUpdatedAt;
     expect(lastUpdatedAt).toBeInstanceOf(Date);
 
-    const users = await readModel.getUsersUpdatedAfter(afterInsert);
+    const users = await readModel.getUsersUpdatedAfterOrEqual(afterInsert);
     expect(users).toHaveLength(0);
 
     await userStore.update(inserted.id, { name: 'New Name' });
 
-    const lastUpdatedAt2 = await readModel.getLastUpdatedAt();
-    expect(lastUpdatedAt2).toBeDefined();
+    const lastUpdate2 = await readModel.getLastUpdatedAt();
+    expect(lastUpdate2).toBeDefined();
+    const lastUpdatedAt2 = lastUpdate2!.lastUpdatedAt;
     expect(lastUpdatedAt2).toBeInstanceOf(Date);
     expect(lastUpdatedAt2!.getTime()).toBeGreaterThanOrEqual(
         lastUpdatedAt!.getTime(),
     );
 
-    const users2 = await readModel.getUsersUpdatedAfter(afterInsert);
+    const users2 = await readModel.getUsersUpdatedAfterOrEqual(afterInsert);
     expect(users2).toHaveLength(1);
     expect(users2[0].email).toBe('test@example.com');
     expect(users2[0].name).toBe('New Name');
@@ -85,23 +87,25 @@ test('Deleting a user should return that user', async () => {
     });
 
     const afterInsert = new Date();
-    const lastUpdatedAt = await readModel.getLastUpdatedAt();
-    expect(lastUpdatedAt).toBeDefined();
+    const lastUpdate = await readModel.getLastUpdatedAt();
+    expect(lastUpdate).toBeDefined();
+    const lastUpdatedAt = lastUpdate!.lastUpdatedAt;
     expect(lastUpdatedAt).toBeInstanceOf(Date);
 
-    const users = await readModel.getUsersUpdatedAfter(afterInsert);
+    const users = await readModel.getUsersUpdatedAfterOrEqual(afterInsert);
     expect(users).toHaveLength(0);
 
     await userStore.delete(inserted.id);
 
-    const lastUpdatedAt2 = await readModel.getLastUpdatedAt();
-    expect(lastUpdatedAt2).toBeDefined();
+    const lastUpdate2 = await readModel.getLastUpdatedAt();
+    expect(lastUpdate2).toBeDefined();
+    const lastUpdatedAt2 = lastUpdate2!.lastUpdatedAt;
     expect(lastUpdatedAt2).toBeInstanceOf(Date);
     expect(lastUpdatedAt2!.getTime()).toBeGreaterThanOrEqual(
         lastUpdatedAt!.getTime(),
     );
 
-    const users2 = await readModel.getUsersUpdatedAfter(afterInsert);
+    const users2 = await readModel.getUsersUpdatedAfterOrEqual(afterInsert);
     expect(users2).toHaveLength(1);
     expect(users2[0].email).toBeNull(); // currently we nullify the email but this might change in the future
     expect(users2[0].createdAt).toBeInstanceOf(Date);

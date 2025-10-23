271c6dce7fec4a1ef0be99da581be33168fd91a0
Jaanus <sellinjaanus@gmail.com>
2025-10-23 10:18:53 +0300

    feat: add plausibleMetrics feature flag

--- src/lib/types/experimental.ts
+++ src/lib/types/experimental.ts
@@ -63,7 +63,8 @@ export type IFlagKey =
     | 'trafficBillingDisplay'
     | 'milestoneProgression'
     | 'envAddStrategySuggestion'
-    | 'featureReleasePlans';
+    | 'featureReleasePlans'
+    | 'plausibleMetrics';
 
 export type IFlags = Partial<{ [key in IFlagKey]: boolean | Variant }>;
 
@@ -290,6 +291,10 @@ const flags: IFlags = {
     ),
     featureReleasePlans: parseEnvVarBoolean(
         process.env.UNLEASH_EXPERIMENTAL_FEATURE_RELEASE_PLANS,
+        false,
+    ),
+    plausibleMetrics: parseEnvVarBoolean(
+        process.env.UNLEASH_EXPERIMENTAL_PLAUSIBLE_METRICS,
         false,
     ),
 };

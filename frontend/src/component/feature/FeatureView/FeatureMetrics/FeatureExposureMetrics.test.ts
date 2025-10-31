ea75e0e51ae0fcb2dc189c08cd6001fe003b7d26
Mohammed Laniyan <mohammed.laniyan@getunleash.io>
2025-10-31 10:07:21 +0000

    test(metrics): add unit tests for default environment prioritization

--- frontend/src/component/feature/FeatureView/FeatureMetrics/FeatureExposureMetrics.test.ts
+++ frontend/src/component/feature/FeatureView/FeatureMetrics/FeatureExposureMetrics.test.ts
@@ -1,39 +1,18 @@
-import { vi } from 'vitest';
 import { renderHook } from '@testing-library/react';
+import { useDefaultEnvironments } from './FeatureExposureMetrics.tsx';
+import { vi } from 'vitest';
 import * as useFeatureHook from 'hooks/api/getters/useFeature/useFeature';
-import {
-    useFeatureMetricsEnvironments,
-    useDefaultEnvironments,
-} from './FeatureExposureMetrics.tsx';
+import type { IFeatureEnvironment } from 'interfaces/featureToggle.ts';
 
-describe('useFeatureMetricsEnvironments', () => {
+describe('useDefaultEnvironments', () => {
     beforeEach(() => {
         vi.clearAllMocks();
     });
 
-    it('should return all environments in their original order', () => {
+    const mockUseFeature = (environments: IFeatureEnvironment[]) => {
         vi.spyOn(useFeatureHook, 'useFeature').mockReturnValue({
             feature: {
-                environments: [
-                    {
-                        name: 'dev',
-                        type: 'development',
-                        enabled: false,
-                        strategies: [],
-                    },
-                    {
-                        name: 'prod',
-                        type: 'production',
-                        enabled: false,
-                        strategies: [],
-                    },
-                    {
-                        name: 'staging',
-                        type: 'staging',
-                        enabled: false,
-                        strategies: [],
-                    },
-                ],
+                environments,
                 stale: false,
                 archived: false,
                 createdAt: '',
@@ -49,61 +28,29 @@ describe('useFeatureMetricsEnvironments', () => {
             refetchFeature: vi.fn(),
             loading: false,
         });
+    };
 
-        const { result } = renderHook(() =>
-            useFeatureMetricsEnvironments('project-x', 'feature-y'),
-        );
-
-        const envArray = Array.from(result.current);
-        expect(envArray).toEqual(['dev', 'prod', 'staging']);
-    });
-});
-
-describe('useDefaultEnvironments', () => {
-    beforeEach(() => {
-        vi.clearAllMocks();
-    });
-
-    it('should return the name of the first production environment if it exists', () => {
-        vi.spyOn(useFeatureHook, 'useFeature').mockReturnValue({
-            feature: {
-                environments: [
-                    {
-                        name: 'dev',
-                        type: 'development',
-                        enabled: false,
-                        strategies: [],
-                    },
-                    {
-                        name: 'prod',
-                        type: 'production',
-                        enabled: false,
-                        strategies: [],
-                    },
-                    {
-                        name: 'staging',
-                        type: 'staging',
-                        enabled: false,
-                        strategies: [],
-                    },
-                ],
-                stale: false,
-                archived: false,
-                createdAt: '',
-                name: '',
-                favorite: false,
-                project: '',
-                type: 'experiment',
-                variants: [],
-                impressionData: false,
-                dependencies: [],
-                children: [],
+    test('should return production environment name when production exists', () => {
+        mockUseFeature([
+            {
+                name: 'dev',
+                type: 'development',
+                enabled: false,
+                strategies: [],
             },
-            refetchFeature: (): void => {
-                throw new Error('Function not implemented.');
+            {
+                name: 'prod',
+                type: 'production',
+                enabled: false,
+                strategies: [],
             },
-            loading: false,
-        });
+            {
+                name: 'staging',
+                type: 'staging',
+                enabled: false,
+                strategies: [],
+            },
+        ]);
 
         const { result } = renderHook(() =>
             useDefaultEnvironments('project-x', 'feature-y'),
@@ -112,74 +59,61 @@ describe('useDefaultEnvironments', () => {
         expect(result.current).toBe('prod');
     });
 
-    it('should return the first environment name if no production environment exists', () => {
-        vi.spyOn(useFeatureHook, 'useFeature').mockReturnValue({
-            feature: {
-                environments: [
-                    {
-                        name: 'alpha',
-                        type: 'staging',
-                        enabled: false,
-                        strategies: [],
-                    },
-                    {
-                        name: 'beta',
-                        type: 'testing',
-                        enabled: false,
-                        strategies: [],
-                    },
-                ],
-                stale: false,
-                archived: false,
-                createdAt: '',
-                name: '',
-                favorite: false,
-                project: '',
-                type: 'experiment',
-                variants: [],
-                impressionData: false,
-                dependencies: [],
-                children: [],
+    test('should return first environment name when multiple production environment exists', () => {
+        mockUseFeature([
+            {
+                name: 'alpha',
+                type: 'testing',
+                enabled: false,
+                strategies: [],
             },
-            refetchFeature: (): void => {
-                throw new Error('Function not implemented.');
+            {
+                name: 'prod 1',
+                type: 'production',
+                enabled: false,
+                strategies: [],
             },
-            loading: false,
-        });
+            {
+                name: 'beta',
+                type: 'staging',
+                enabled: false,
+                strategies: [],
+            },
+            {
+                name: 'prod 2',
+                type: 'production',
+                enabled: false,
+                strategies: [],
+            },
+            {
+                name: 'prod 3',
+                type: 'production',
+                enabled: false,
+                strategies: [],
+            },
+        ]);
 
         const { result } = renderHook(() =>
             useDefaultEnvironments('project-x', 'feature-y'),
         );
 
-        expect(result.current).toBe('alpha');
+        expect(result.current).toBe('prod 1');
     });
 
-    it('should return an empty string if no environments exist', () => {
-        vi.spyOn(useFeatureHook, 'useFeature').mockReturnValue({
-            feature: {
-                environments: [],
-                stale: false,
-                archived: false,
-                createdAt: '',
-                name: '',
-                favorite: false,
-                project: '',
-                type: 'experiment',
-                variants: [],
-                impressionData: false,
-                dependencies: [],
-                children: [],
+    test('should handle single production environment gracefully', () => {
+        mockUseFeature([
+            {
+                name: 'prod',
+                type: 'production',
+                enabled: false,
+                strategies: [],
             },
-            refetchFeature: (): void => {
-                throw new Error('Function not implemented.');
-            },
-            loading: false,
-        });
+        ]);
 
         const { result } = renderHook(() =>
             useDefaultEnvironments('project-x', 'feature-y'),
         );
 
-        expect(result.current).toBe('');
+        expect(result.current).toBe('prod');
     });
 });

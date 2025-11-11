51444f5508b4b5e92251ed914dbb198ba854e6c1
Thomas Heartman <thomas@getunleash.io>
2025-11-11 13:30:12 +0100

    extract and test maintenance mode banner message setting

--- frontend/src/component/maintenance/MaintenanceBanner.tsx
+++ frontend/src/component/maintenance/MaintenanceBanner.tsx
@@ -1,22 +1,5 @@
 import { Banner } from 'component/banners/Banner/Banner';
-import useUiConfig from 'hooks/api/getters/useUiConfig/useUiConfig';
-import { PayloadType } from 'utils/variants';
-
-const defaultMessage =
-    '**Maintenance mode!** During maintenance mode, any changes made will not be saved and you may receive an error. We apologize for any inconvenience this may cause.';
-
-const useMaintenanceBannerMessage = (): string => {
-    const { uiConfig } = useUiConfig();
-    const flag = uiConfig.flags.maintenanceMode;
-    if (typeof flag === 'boolean') {
-        return defaultMessage;
-    }
-
-    if (flag?.payload?.type === PayloadType.STRING) {
-        return flag.payload.value;
-    }
-    return defaultMessage;
-};
+import { useMaintenanceBannerMessage } from './useMaintenanceBannerMessage.ts';
 
 export const MaintenanceBanner = () => {
     const message = useMaintenanceBannerMessage();

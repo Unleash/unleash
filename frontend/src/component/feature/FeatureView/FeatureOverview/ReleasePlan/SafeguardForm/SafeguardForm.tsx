4890b16b499857abac25fa1834dccc16f83f9718
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2025-11-19 17:13:03 +0100

    feat: improve safeguard threshold field (#11004)

--- frontend/src/component/feature/FeatureView/FeatureOverview/ReleasePlan/SafeguardForm/SafeguardForm.tsx
+++ frontend/src/component/feature/FeatureView/FeatureOverview/ReleasePlan/SafeguardForm/SafeguardForm.tsx
@@ -244,7 +244,6 @@ export const SafeguardForm = ({
                 <StyledLabel>filtered by</StyledLabel>
                 <FormControl variant='outlined' size='small'>
                     <StyledSelect
-                        sx={{ minWidth: 200 }}
                         value={appName}
                         onChange={(e) =>
                             handleApplicationChange(String(e.target.value))
@@ -287,12 +286,15 @@ export const SafeguardForm = ({
 
                 <FormControl variant='outlined' size='small'>
                     <TextField
-                        sx={{ minWidth: 120 }}
                         type='number'
+                        inputProps={{
+                            step: 0.1,
+                        }}
                         value={threshold}
-                        onChange={(e) =>
-                            handleThresholdChange(Number(e.target.value))
-                        }
+                        onChange={(e) => {
+                            const value = e.target.value;
+                            handleThresholdChange(Number(value));
+                        }}
                         placeholder='Value'
                         variant='outlined'
                         size='small'

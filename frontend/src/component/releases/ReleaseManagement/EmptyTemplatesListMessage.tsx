34318cbee8cb700399a8a48ef59cd2f24c3a01e3
Thomas Heartman <thomas@getunleash.io>
2025-11-25 14:57:57 +0100

    chore: improve release template empty state

--- frontend/src/component/releases/ReleaseManagement/EmptyTemplatesListMessage.tsx
+++ frontend/src/component/releases/ReleaseManagement/EmptyTemplatesListMessage.tsx
@@ -1,38 +1,68 @@
-import { styled } from '@mui/material';
+import {
+    Button,
+    type ButtonProps,
+    styled,
+    Typography,
+    type TypographyProps,
+} from '@mui/material';
 import { ReactComponent as ReleaseTemplateIcon } from 'assets/img/releaseTemplates.svg';
+import { Link } from 'react-router-dom';
 
-const NoReleaseTemplatesMessage = styled('div')(({ theme }) => ({
+const Container = styled('article')(({ theme }) => ({
+    paddingBlock: theme.spacing(5),
+    display: 'flex',
+    gap: theme.spacing(2),
+    flexFlow: 'column nowrap',
+    alignItems: 'center',
+    maxWidth: theme.spacing(40),
+    margin: 'auto',
+}));
+
+const CenteredText = styled(Typography)<TypographyProps>({
     textAlign: 'center',
-    padding: theme.spacing(1, 0, 0, 0),
+});
+
+const Buttons = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexFlow: 'row wrap',
+    columnGap: theme.spacing(3),
+    rowGap: theme.spacing(2),
+    justifyContent: 'center',
 }));
 
-const TemplatesEasierMessage = styled('div')(({ theme }) => ({
-    textAlign: 'center',
-    padding: theme.spacing(1, 0, 9, 0),
-    color: theme.palette.text.secondary,
-}));
+const LinkButton = styled((props: ButtonProps<typeof Link>) => (
+    <Button component={Link} {...props} />
+))({
+    whiteSpace: 'nowrap',
+});
 
-const StyledCenter = styled('div')(({ theme }) => ({
-    textAlign: 'center',
-}));
-
-const StyledDiv = styled('div')(({ theme }) => ({
-    paddingTop: theme.spacing(5),
-}));
 export const EmptyTemplatesListMessage = () => {
     return (
-        <StyledDiv>
-            <StyledCenter>
-                <ReleaseTemplateIcon />
-            </StyledCenter>
-            <NoReleaseTemplatesMessage>
-                You have no release templates set up
-            </NoReleaseTemplatesMessage>
-            <TemplatesEasierMessage>
-                Make the set up of strategies easier for your
-                <br />
-                teams by creating templates
-            </TemplatesEasierMessage>
-        </StyledDiv>
+        <Container>
+            <ReleaseTemplateIcon />
+            <CenteredText component='h2' variant='h3'>
+                Get started with release templates
+            </CenteredText>
+            <CenteredText component='p' sx={{ color: 'text.secondary' }}>
+                Control your releases with milestones that can be reused by the
+                entire team.
+            </CenteredText>
+            <Buttons>
+                <LinkButton
+                    to='https://docs.getunleash.io/reference/release-templates'
+                    variant='text'
+                    rel='noopener noreferrer'
+                    target='_blank'
+                >
+                    Read docs
+                </LinkButton>
+                <LinkButton
+                    to='/release-templates/create-template'
+                    variant='contained'
+                >
+                    New template
+                </LinkButton>
+            </Buttons>
+        </Container>
     );
 };

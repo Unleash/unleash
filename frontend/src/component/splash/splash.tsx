3fd14d54882a198a67a99c7cd9d7da9465bbf516
FredrikOseberg <fredrik.no@gmail.com>
2025-12-17 10:29:38 +0100

    fix: rm unused splashid

--- frontend/src/component/splash/splash.tsx
+++ frontend/src/component/splash/splash.tsx
@@ -1,8 +1,4 @@
 // All known splash IDs.
-export const splashIds = [
-    'operators',
-    'release-management',
-    'release-management-v3',
-] as const;
+export const splashIds = ['operators', 'release-management-v3'] as const;
 
 export type SplashId = (typeof splashIds)[number];

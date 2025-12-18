f772254d0f29533ed41ec1ae4f4b44c097d1cf48
Nuno Góis <github@nunogois.com>
2025-12-18 15:46:42 +0000

    chore: create Reader project role

---
+++ src/migrations/20251218153722-predefined-project-role-reader.js
@@ -0,0 +1,25 @@
+
+exports.up = (db, callback) => {
+    db.runSql(
+        `
+        ALTER TABLE roles DROP CONSTRAINT unique_name;
+        CREATE UNIQUE INDEX roles_type_name_unique ON roles (name, type);
+
+        INSERT INTO roles (name, description, type)
+        VALUES ('Reader', 'Users with the project reader role have read-only access to the project and cannot make changes.', 'project')
+        ON CONFLICT (name, type) DO NOTHING;
+        `,
+        callback,
+    );
+};
+
+exports.down = (db, callback) => {
+    db.runSql(
+        `
+        DELETE FROM roles WHERE type = 'project' AND name = 'Reader';
+        DROP INDEX IF EXISTS roles_type_name_unique;
+        ALTER TABLE roles ADD CONSTRAINT unique_name UNIQUE (name);
+        `,
+        callback,
+    );
+};

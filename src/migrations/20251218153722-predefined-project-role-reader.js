7af4a026a6903c4e24cb80183873b6086fd2311c
Nuno Góis <github@nunogois.com>
2025-12-19 08:33:19 +0000

    Update src/migrations/20251218153722-predefined-project-role-reader.js
    Co-authored-by: Gastón Fournier <gaston@getunleash.io>

--- src/migrations/20251218153722-predefined-project-role-reader.js
+++ src/migrations/20251218153722-predefined-project-role-reader.js
@@ -3,7 +3,7 @@ exports.up = (db, callback) => {
     db.runSql(
         `
         ALTER TABLE roles DROP CONSTRAINT unique_name;
-        CREATE UNIQUE INDEX roles_type_name_unique ON roles (name, type);
+        ALTER TABLE roles ADD CONSTRAINT roles_type_name_unique UNIQUE (type, name);
 
         INSERT INTO roles (name, description, type)
         VALUES ('Reader', 'Users with the project reader role have read-only access to the project and cannot make changes.', 'project')

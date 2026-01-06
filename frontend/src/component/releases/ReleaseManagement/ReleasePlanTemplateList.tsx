6e61aa4a0617d29965b661cc385c39cfcc3727da
FredrikOseberg <fredrik.no@gmail.com>
2026-01-06 15:21:18 +0100

    fix: asset path

--- frontend/src/component/releases/ReleaseManagement/ReleasePlanTemplateList.tsx
+++ frontend/src/component/releases/ReleaseManagement/ReleasePlanTemplateList.tsx
@@ -1,7 +1,6 @@
 import { Grid, styled } from '@mui/material';
 import { ReleasePlanTemplateCard } from './ReleasePlanTemplateCard/ReleasePlanTemplateCard.tsx';
 import type { IReleasePlanTemplate } from 'interfaces/releasePlans';
-import { ReleasesFeedback } from './ReleasesFeedback.tsx';
 
 const StyledGridItem = styled(Grid)({
     minHeight: '180px',
@@ -21,15 +20,6 @@ export const ReleasePlanTemplateList: React.FC<ITemplateList> = ({
                     <ReleasePlanTemplateCard template={template} />
                 </StyledGridItem>
             ))}
-            {templates.length > 0 && (
-                <StyledGridItem key='feedback' item xs={6} md={4}>
-                    <ReleasesFeedback title='Release Templates'>
-                        We would love to get your feedback on the concept around
-                        release templates so we can bring it into our work going
-                        forward
-                    </ReleasesFeedback>
-                </StyledGridItem>
-            )}
         </>
     );
 };

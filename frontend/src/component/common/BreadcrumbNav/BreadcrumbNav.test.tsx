f8a8970d079343e0fb7996c4b5d0d8625f4160fb
Kamala Rocci <kamala.rocci@getunleash.io>
2026-01-07 18:08:25 +0100

    lint double to single quotes fix

--- frontend/src/component/common/BreadcrumbNav/BreadcrumbNav.test.tsx
+++ frontend/src/component/common/BreadcrumbNav/BreadcrumbNav.test.tsx
@@ -1,15 +1,15 @@
-import { render } from "utils/testRenderer";
-import { Route, Routes } from "react-router-dom";
-import BreadcrumbNav from "./BreadcrumbNav.tsx";
-import { screen } from "@testing-library/react";
-import { vi } from "vitest";
+import { render } from 'utils/testRenderer';
+import { Route, Routes } from 'react-router-dom';
+import BreadcrumbNav from './BreadcrumbNav.tsx';
+import { screen } from '@testing-library/react';
+import { vi } from 'vitest';
 
-vi.mock("hooks/api/getters/useProjectOverview/useProjectOverview", () => {
+vi.mock('hooks/api/getters/useProjectOverview/useProjectOverview', () => {
     return {
         default: vi.fn(() => ({
             project: {
-                id: "/my-project",
-                name: "My Test Project",
+                id: '/my-project',
+                name: 'My Test Project',
             },
             loading: false,
             error: null,
@@ -18,33 +18,33 @@ vi.mock("hooks/api/getters/useProjectOverview/useProjectOverview", () => {
     };
 });
 
-test("decode URI encoded path in breadcrumbs nav", async () => {
+test('decode URI encoded path in breadcrumbs nav', async () => {
     render(
         <Routes>
             <Route
-                path={"/applications/:name/:instance"}
+                path={'/applications/:name/:instance'}
                 element={<BreadcrumbNav />}
             />
         </Routes>,
         {
-            route: "/applications/my%20app/my%20instance",
+            route: '/applications/my%20app/my%20instance',
         }
     );
 
-    await screen.findByText("applications");
-    await screen.findByText("my app");
-    await screen.findByText("my instance");
+    await screen.findByText('applications');
+    await screen.findByText('my app');
+    await screen.findByText('my instance');
 });
 
-test("use project name when in a project path", async () => {
+test('use project name when in a project path', async () => {
     render(
         <Routes>
-            <Route path={"/projects/:projectId"} element={<BreadcrumbNav />} />
+            <Route path={'/projects/:projectId'} element={<BreadcrumbNav />} />
         </Routes>,
         {
-            route: "/projects/my-project",
+            route: '/projects/my-project',
         }
     );
 
-    await screen.findByText("My Test Project");
+    await screen.findByText('My Test Project');
 });

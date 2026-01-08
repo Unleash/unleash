a753d3bb06b5bdccbd9bc8d7705c90f6e1166295
Boris <72118807+Curis-lab@users.noreply.github.com>
2026-01-08 07:51:49 +0630

    refactor(LinkCell): add prop to control tooltip rendering

--- frontend/src/component/common/Table/cells/LinkCell/LinkCell.tsx
+++ frontend/src/component/common/Table/cells/LinkCell/LinkCell.tsx
@@ -3,6 +3,7 @@ import { Link as RouterLink } from "react-router-dom";
 import { ConditionallyRender } from "component/common/ConditionallyRender/ConditionallyRender";
 import { Highlighter } from "component/common/Highlighter/Highlighter";
 import { useSearchHighlightContext } from "component/common/Table/SearchHighlightContext/SearchHighlightContext";
+import { HtmlTooltip } from "component/common/HtmlTooltip/HtmlTooltip";
 import { Truncator } from "component/common/Truncator/Truncator";
 import {
     StyledWrapper,
@@ -13,6 +14,7 @@ import {
 
 interface ILinkCellProps {
     title?: string;
+    disableTooltip?: boolean;
     to?: string;
     onClick?: () => void;
     subtitle?: string;
@@ -21,6 +23,7 @@ interface ILinkCellProps {
 
 export const LinkCell: React.FC<ILinkCellProps> = ({
     title,
+    disableTooltip,
     to,
     onClick,
     subtitle,
@@ -28,13 +31,27 @@ export const LinkCell: React.FC<ILinkCellProps> = ({
 }) => {
     const { searchQuery } = useSearchHighlightContext();
 
+    const subtitleContent = (
+        <StyledDescription data-loading>
+            <Highlighter search={searchQuery}>{subtitle}</Highlighter>
+        </StyledDescription>
+    );
+
     const renderSubtitle = (
         <ConditionallyRender
             condition={Boolean(subtitle && subtitle.length > 40)}
             show={
-                <StyledDescription data-loading>
-                    <Highlighter search={searchQuery}>{subtitle}</Highlighter>
-                </StyledDescription>
+                !disableTooltip ? (
+                    <HtmlTooltip
+                        title={subtitle}
+                        placement="bottom-start"
+                        arrow
+                    >
+                        {subtitleContent}
+                    </HtmlTooltip>
+                ) : (
+                    subtitleContent
+                )
             }
             elseShow={
                 <StyledDescription data-loading>

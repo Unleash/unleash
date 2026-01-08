7efb332e116f75e8e25603845482cbac6c62596c
Kamala Rocci <kamala.rocci@getunleash.io>
2026-01-08 13:09:57 +0100

    check that the path is on right index in isProjectPath

--- frontend/src/component/common/BreadcrumbNav/BreadcrumbNav.tsx
+++ frontend/src/component/common/BreadcrumbNav/BreadcrumbNav.tsx
@@ -89,7 +89,7 @@ const BreadcrumbNav = () => {
                                 {paths.map((path, index) => {
                                     const isProjectPath =
                                         path === projectId &&
-                                        paths[index - 1] === 'projects' &&
+                                        index === 1 &&
                                         project.name !== '';
                                     const pathName = isProjectPath
                                         ? project.name

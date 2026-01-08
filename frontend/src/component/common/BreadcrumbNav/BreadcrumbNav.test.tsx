e6df2a5b86548d4a3d8e19a2bfdb90cde2a68563
Kamala <kamala.rocci@getunleash.io>
2026-01-08 14:30:11 +0100

    chore: use project name in breadcrumbs (#11201)
    This PR changes the way breadcrumbs look in project pages. Instead of
    the project ID, the project name is now displayed.
    The change should make it less confusing for users as the project ID can
    sometimes differ a lot from the project name, plus the latter can also
    be updated.
    
    
    https://linear.app/unleash/issue/CJUX-352/make-breadcrumbs-show-correct-page-title-consistently
    
    Before: 
    <img width="500" height="224" alt="Screenshot 2026-01-07 at 18 05 01"
    src="https://github.com/user-attachments/assets/1665b6d6-b715-4350-a53c-cdb527739963"
    />
    
    After:
    <img width="500" height="224" alt="Screenshot 2026-01-07 at 18 04 35"
    src="https://github.com/user-attachments/assets/637a920f-12e3-4570-be02-e2e8ea4d0eca"
    />

--- frontend/src/component/common/BreadcrumbNav/BreadcrumbNav.test.tsx
+++ frontend/src/component/common/BreadcrumbNav/BreadcrumbNav.test.tsx
@@ -2,6 +2,9 @@ import { render } from 'utils/testRenderer';
 import { Route, Routes } from 'react-router-dom';
 import BreadcrumbNav from './BreadcrumbNav.tsx';
 import { screen } from '@testing-library/react';
+import { testServerRoute, testServerSetup } from 'utils/testServer';
+
+const server = testServerSetup();
 
 test('decode URI encoded path in breadcrumbs nav', async () => {
     render(
@@ -19,4 +22,23 @@ test('decode URI encoded path in breadcrumbs nav', async () => {
     await screen.findByText('applications');
     await screen.findByText('my app');
     await screen.findByText('my instance');
+});
+
+test('use project name when in a project path', async () => {
+    testServerRoute(server, '/api/admin/projects/my-project/overview', {
+        name: 'My Test Project',
+        onboardingStatus: { status: 'onboarded' },
+        version: '1.0.0',
+    });
+
+    render(
+        <Routes>
+            <Route path={'/projects/:projectId'} element={<BreadcrumbNav />} />
+        </Routes>,
+        {
+            route: '/projects/my-project',
+        },
+    );
+
+    await screen.findByText('My Test Project');
 });

fbd7503073a164f5afbac58b91913ad02adf09ea
Thomas Heartman <thomas@getunleash.io>
2026-01-13 14:22:31 +0100

    feat: make generalselect component also accept option groups
    Updates the general select component to accept option groups.

--- frontend/src/component/common/GeneralSelect/GeneralSelect.tsx
+++ frontend/src/component/common/GeneralSelect/GeneralSelect.tsx
@@ -6,6 +6,7 @@ import {
     type SelectProps,
     type SelectChangeEvent,
     styled,
+    ListSubheader,
 } from '@mui/material';
 import { SELECT_ITEM_ID } from 'utils/testIds';
 import KeyboardArrowDownOutlined from '@mui/icons-material/KeyboardArrowDownOutlined';
@@ -21,12 +22,24 @@ export interface ISelectOption {
     sx?: SxProps<Theme>;
 }
 
+export type SelectOptionGroup = {
+    groupHeader: string;
+    options: ISelectOption[];
+};
+
+const isSelectOptionGroup = (
+    options: ISelectOption[] | SelectOptionGroup[],
+): options is SelectOptionGroup[] => {
+    const firstElement = options[0];
+    return firstElement && 'groupHeader' in firstElement;
+};
+
 export interface IGeneralSelectProps<T extends string = string>
     extends Omit<SelectProps, 'onChange'> {
     name?: string;
     value?: T;
     label?: string;
-    options: ISelectOption[];
+    options: ISelectOption[] | SelectOptionGroup[];
     onChange: (key: T) => void;
     disabled?: boolean;
     fullWidth?: boolean;
@@ -39,6 +52,19 @@ export interface IGeneralSelectProps<T extends string = string>
 const StyledFormControl = styled(FormControl)({
     maxWidth: '100%',
 });
+
+const toMenuItem = (option: ISelectOption) => (
+    <MenuItem
+        sx={option.sx}
+        key={option.key}
+        value={option.key}
+        title={option.title || ''}
+        data-testid={`${SELECT_ITEM_ID}-${option.label}`}
+        disabled={option.disabled}
+    >
+        {option.label}
+    </MenuItem>
+);
 
 function GeneralSelect<T extends string = string>({
     variant = 'outlined',
@@ -90,18 +116,15 @@ function GeneralSelect<T extends string = string>({
                 labelId={labelId}
                 {...rest}
             >
-                {options.map((option) => (
-                    <MenuItem
-                        sx={option.sx}
-                        key={option.key}
-                        value={option.key}
-                        title={option.title || ''}
-                        data-testid={`${SELECT_ITEM_ID}-${option.label}`}
-                        disabled={option.disabled}
-                    >
-                        {option.label}
-                    </MenuItem>
-                ))}
+                {isSelectOptionGroup(options)
+                    ? options?.flatMap((group, index) => {
+                          return [
+                              <ListSubheader key={group.groupHeader}>
+                                  {group.groupHeader}
+                              </ListSubheader>,
+                          ].concat(group.options?.map(toMenuItem));
+                      })
+                    : options?.map(toMenuItem)}
             </Select>
         </StyledFormControl>
     );

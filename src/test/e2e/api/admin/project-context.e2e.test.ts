d572ee36d33b9b9b1f416dabe84bb610ec236bf5
Kamala Rocci <kamala.rocci@getunleash.io>
2026-01-15 10:09:28 +0100

    changes after review

--- src/test/e2e/api/admin/project-context.e2e.test.ts
+++ src/test/e2e/api/admin/project-context.e2e.test.ts
@@ -7,11 +7,8 @@ import getLogger from '../../../fixtures/no-logger.js';
 import {
     RoleName,
     TEST_AUDIT_USER,
-    CREATE_CONTEXT_FIELD,
-    UPDATE_CONTEXT_FIELD,
     UPDATE_PROJECT_CONTEXT,
 } from '../../../../lib/types/index.js';
-import type { ICustomRole, IRole } from '../../../../lib/server-impl.js';
 
 let db: ITestDb;
 let app: IUnleashTest;
@@ -35,20 +32,6 @@ beforeAll(async () => {
 test('should reject creating a project context field without permission', async () => {
     const viewerRole = await db.stores.roleStore.getRoleByName(RoleName.VIEWER);
 
-    await app.services.accessService.createRole(
-        {
-            name: 'no-project-context-perm',
-            description: 'Role for testing project permissions',
-            permissions: [
-                {
-                    name: UPDATE_CONTEXT_FIELD,
-                },
-            ],
-            createdByUserId: TEST_AUDIT_USER.id,
-        },
-        TEST_AUDIT_USER,
-    );
-
     const email = 'user@test.com';
     await app.services.userService.createUser(
         { email, rootRole: viewerRole.id },
@@ -64,111 +47,26 @@ test('should reject creating a project context field without permission', async 
         .expect(403);
 });
 
-test('should create project context field with UPDATE_PROJECT_CONTEXT permission', async () => {
-    const viewerRole = await db.stores.roleStore.getRoleByName(RoleName.VIEWER);
-
-    const updateProjectContextPerm =
-        await app.services.accessService.createRole(
-            {
-                name: 'update-project-context-perm',
-                description: 'Role for testing project permissions',
-                permissions: [
-                    {
-                        name: UPDATE_PROJECT_CONTEXT,
-                    },
-                ],
-                createdByUserId: TEST_AUDIT_USER.id,
-            },
-            TEST_AUDIT_USER,
+describe('with UPDATE_PROJECT_CONTEXT permission', () => {
+    it('allows to perform CRUD operations', async () => {
+        const viewerRole = await db.stores.roleStore.getRoleByName(
+            RoleName.VIEWER,
         );
 
-    const email = 'user@test.com';
-    const user = await app.services.userService.createUser(
-        { email, rootRole: viewerRole.id },
-        TEST_AUDIT_USER,
-    );
-
-    await app.services.accessService.addUserToRole(
-        user.id,
-        updateProjectContextPerm.id,
-        'default',
-    );
-
-    await app.request.post('/auth/demo/login').send({ email });
-
-    await app.request
-        .post('/api/admin/projects/default/context')
-        .send({ name: 'newContext' })
-        .set('Content-Type', 'application/json')
-        .expect(201);
-});
-
-test('should update project context field with UPDATE_PROJECT_CONTEXT permission', async () => {
-    const viewerRole = await db.stores.roleStore.getRoleByName(RoleName.VIEWER);
-
-    const updateProjectContextPerm =
-        await app.services.accessService.createRole(
-            {
-                name: 'update-project-context-perm',
-                description: 'Role for testing project permissions',
-                permissions: [
-                    {
-                        name: UPDATE_PROJECT_CONTEXT,
-                    },
-                ],
-                createdByUserId: TEST_AUDIT_USER.id,
-            },
-            TEST_AUDIT_USER,
-        );
-
-    const email = 'user@test.com';
-    const user = await app.services.userService.createUser(
-        { email, rootRole: viewerRole.id },
-        TEST_AUDIT_USER,
-    );
-
-    await app.services.accessService.addUserToRole(
-        user.id,
-        updateProjectContextPerm.id,
-        'default',
-    );
-
-    await app.request.post('/auth/demo/login').send({ email });
-
-    const contextName = 'newContext';
-    await app.request
-        .post('/api/admin/projects/default/context')
-        .send({ name: contextName })
-        .set('Content-Type', 'application/json');
-
-    await app.request
-        .put(`/api/admin/projects/default/context/${contextName}`)
-        .send({ name: 'updated context' })
-        .set('Content-Type', 'application/json')
-        .expect(200);
-});
-
-describe('when a project context field is created with CREATE_CONTEXT_FIELD permission ', () => {
-    let viewerRole: IRole;
-    let createContextPerm: ICustomRole;
-    const name = 'newContext';
-
-    beforeEach(async () => {
-        viewerRole = await db.stores.roleStore.getRoleByName(RoleName.VIEWER);
-
-        createContextPerm = await app.services.accessService.createRole(
-            {
-                name: 'create-context-perm',
-                description: 'Role for testing project permissions',
-                permissions: [
-                    {
-                        name: CREATE_CONTEXT_FIELD,
-                    },
-                ],
-                createdByUserId: TEST_AUDIT_USER.id,
-            },
-            TEST_AUDIT_USER,
-        );
+        const updateProjectContextPerm =
+            await app.services.accessService.createRole(
+                {
+                    name: 'update-project-context-perm',
+                    description: 'Role for testing project permissions',
+                    permissions: [
+                        {
+                            name: UPDATE_PROJECT_CONTEXT,
+                        },
+                    ],
+                    createdByUserId: TEST_AUDIT_USER.id,
+                },
+                TEST_AUDIT_USER,
+            );
 
         const email = 'user@test.com';
         const user = await app.services.userService.createUser(
@@ -178,51 +76,93 @@ describe('when a project context field is created with CREATE_CONTEXT_FIELD perm
 
         await app.services.accessService.addUserToRole(
             user.id,
-            createContextPerm.id,
+            updateProjectContextPerm.id,
             'default',
         );
 
         await app.request.post('/auth/demo/login').send({ email });
 
-        await app.request
+        const res = await app.request
             .post('/api/admin/projects/default/context')
-            .send({ name })
-            .set('Content-Type', 'application/json');
-    });
+            .send({ name: 'newContext' })
+            .set('Content-Type', 'application/json')
+            .expect(201);
 
-    it('should allow to update it with UPDATE_CONTEXT_FIELD', async () => {
-        const email = 'user2@test.com';
-        const user = await app.services.userService.createUser(
-            { email, rootRole: viewerRole.id },
+        const context = res.body;
+
+        await app.request
+            .get(`/api/admin/projects/default/context/${context.name}`)
+            .set('Content-Type', 'application/json')
+            .expect(200);
+
+        await app.request
+            .put(`/api/admin/projects/default/context/${context.name}`)
+            .send({ name: 'updated context' })
+            .set('Content-Type', 'application/json')
+            .expect(200);
+
+        await app.request
+            .delete(`/api/admin/projects/default/context/${context.name}`)
+            .set('Content-Type', 'application/json')
+            .expect(200);
+    });
+});
+
+describe('with Editor role', () => {
+    it('allows to perform CRUD operations on a new project', async () => {
+        const editorRole = await db.stores.roleStore.getRoleByName(
+            RoleName.EDITOR,
+        );
+
+        const dummyAdmin = await app.services.userService.createUser(
+            {
+                name: 'Some Name',
+                email: 'test@getunleash.io',
+                rootRole: RoleName.ADMIN,
+            },
             TEST_AUDIT_USER,
         );
 
-        const createContextFieldRole =
-            await app.services.accessService.createRole(
-                {
-                    name: 'update-context-perm',
-                    description: 'Role for testing project permissions',
-                    permissions: [
-                        {
-                            name: UPDATE_CONTEXT_FIELD,
-                        },
-                    ],
-                    createdByUserId: TEST_AUDIT_USER.id,
-                },
-                TEST_AUDIT_USER,
-            );
+        const newProject = await app.services.projectService.createProject(
+            {
+                id: 'new-project',
+                name: 'New Project',
+            },
+            dummyAdmin,
+            TEST_AUDIT_USER,
+        );
 
-        await app.services.accessService.addUserToRole(
-            user.id,
-            createContextFieldRole.id,
-            'default',
+        const email = 'user@test.com';
+        await app.services.userService.createUser(
+            { email, rootRole: editorRole.id },
+            TEST_AUDIT_USER,
         );
 
         await app.request.post('/auth/demo/login').send({ email });
 
+        const res = await app.request
+            .post(`/api/admin/projects/${newProject.id}/context`)
+            .send({ name: 'newContext' })
+            .set('Content-Type', 'application/json')
+            .expect(201);
+
+        const context = res.body;
+
         await app.request
-            .put(`/api/admin/projects/default/context/${name}`)
+            .get(`/api/admin/projects/${newProject.id}/context/${context.name}`)
+            .set('Content-Type', 'application/json')
+            .expect(200);
+
+        await app.request
+            .put(`/api/admin/projects/${newProject.id}/context/${context.name}`)
             .send({ name: 'updated context' })
+            .set('Content-Type', 'application/json')
+            .expect(200);
+
+        await app.request
+            .delete(
+                `/api/admin/projects/${newProject.id}/context/${context.name}`,
+            )
             .set('Content-Type', 'application/json')
             .expect(200);
     });

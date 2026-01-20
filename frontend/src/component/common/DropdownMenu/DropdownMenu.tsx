631de63260bb1fc6f072c3c4973846690404f9a9
Kamala <kamala.rocci@getunleash.io>
2026-01-20 17:36:57 +0100

    chore: update lifecycle filters appearance  (#11221)
    https://linear.app/unleash/issue/CJUX-359/adjustments-to-quick-filters
    
    This changes the way lifecycle filters are displayed (cc @Healsi).
    It includes changes mainly to:
    
    - `DropdownMenu` and `DropdownButton` components: to display the
    selected option in the `DropdownButton` and update the styling;
    
    - `LifecycleFilters` component: filters now look similar to
    `ChangeRequestFilters`, as they are grouped together. On small
    viewports, they are collapsed into a dropdown.
    
    - `ProjectFeatureToggles` (Feature flags section in
    `/projects/:projectId` overview page) and `FeatureToggleListTable`
    (Flags overview page) components: to update breakpoints as the lifecycle
    filters now take up more space.
    
    ### Project overview 
    
    
    https://github.com/user-attachments/assets/c6307eb1-28f7-4e1a-a875-f48d5de38796
    
    
    ### Flags overview 
    
    
    https://github.com/user-attachments/assets/b9663a15-3f4e-4ba1-b3e1-467a1fd90c29
    
    
    <img width="362" height="367" alt="Screenshot 2026-01-20 at 13 31 57"
    src="https://github.com/user-attachments/assets/183c0f81-9094-40b1-bcab-7e449e70a267"
    />

--- frontend/src/component/common/DropdownMenu/DropdownMenu.tsx
+++ frontend/src/component/common/DropdownMenu/DropdownMenu.tsx
@@ -1,34 +1,42 @@
 import {
     type CSSProperties,
+    type FC,
     type MouseEventHandler,
     type ReactNode,
     useState,
-    type VFC,
 } from 'react';
-import { Menu } from '@mui/material';
+import { Menu, type SxProps, type Theme } from '@mui/material';
 import ArrowDropDown from '@mui/icons-material/ArrowDropDown';
 import { DropdownButton } from './DropdownButton/DropdownButton.tsx';
 
 export interface IDropdownMenuProps {
-    renderOptions: () => ReactNode;
+    renderOptions: () => ReactNode | ReactNode[];
     id: string;
     title?: string;
     callback?: MouseEventHandler;
     icon?: ReactNode;
     label: string;
     startIcon?: ReactNode;
-    style?: CSSProperties;
+    selected?: ReactNode;
+    layout?: DropdownMenuLayout;
 }
 
-const DropdownMenu: VFC<IDropdownMenuProps> = ({
+type DropdownMenuLayout = {
+    width?: number | string;
+    button?: React.CSSProperties;
+    menu?: SxProps<Theme>;
+};
+
+export const DropdownMenu: FC<IDropdownMenuProps> = ({
     renderOptions,
     id,
     title,
     callback,
     icon = <ArrowDropDown titleAccess='Toggle' />,
     label,
-    style,
     startIcon,
+    selected,
+    layout,
     ...rest
 }) => {
     const [anchor, setAnchor] = useState<Element | null>(null);
@@ -49,14 +57,17 @@ const DropdownMenu: VFC<IDropdownMenuProps> = ({
         <>
             <DropdownButton
                 id={id}
-                label={label}
+                label={selected || label}
                 title={title}
                 startIcon={startIcon}
                 onClick={handleOpen}
-                style={style}
                 aria-controls={id}
                 aria-expanded={Boolean(anchor)}
                 icon={icon}
+                style={{
+                    width: layout?.width,
+                    ...layout?.button,
+                }}
                 {...rest}
             />
             <Menu
@@ -64,11 +75,15 @@ const DropdownMenu: VFC<IDropdownMenuProps> = ({
                 onClick={handleClose}
                 anchorEl={anchor}
                 open={Boolean(anchor)}
+                sx={{
+                    ...layout?.menu,
+                    '& .MuiPaper-root': {
+                        width: layout?.width,
+                    },
+                }}
             >
                 {renderOptions()}
             </Menu>
         </>
     );
 };
-
-export default DropdownMenu;

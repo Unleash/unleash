18e7db752531a4db4a88f0c64852d9f9a5cb21b1
Boris <72118807+Curis-lab@users.noreply.github.com>
2026-01-21 09:15:04 +0630

    chore: limit formatting changes and resolve CI failures

--- frontend/src/component/common/Table/cells/LinkCell/LinkCell.tsx
+++ frontend/src/component/common/Table/cells/LinkCell/LinkCell.tsx
@@ -1,16 +1,16 @@
-import type React from "react";
-import { Link as RouterLink } from "react-router-dom";
-import { ConditionallyRender } from "component/common/ConditionallyRender/ConditionallyRender";
-import { Highlighter } from "component/common/Highlighter/Highlighter";
-import { useSearchHighlightContext } from "component/common/Table/SearchHighlightContext/SearchHighlightContext";
-import { HtmlTooltip } from "component/common/HtmlTooltip/HtmlTooltip";
-import { Truncator } from "component/common/Truncator/Truncator";
+import type React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
+import { Highlighter } from 'component/common/Highlighter/Highlighter';
+import { useSearchHighlightContext } from 'component/common/Table/SearchHighlightContext/SearchHighlightContext';
+import { HtmlTooltip } from 'component/common/HtmlTooltip/HtmlTooltip';
+import { Truncator } from 'component/common/Truncator/Truncator';
 import {
     StyledWrapper,
     StyledLink,
     StyledContainer,
     StyledDescription,
-} from "./LinkCell.styles";
+} from './LinkCell.styles';
 
 interface ILinkCellProps {
     title?: string;
@@ -82,7 +82,7 @@ export const LinkCell: React.FC<ILinkCellProps> = ({
 
     if (to) {
         return (
-            <StyledLink component={RouterLink} to={to} underline="hover">
+            <StyledLink component={RouterLink} to={to} underline='hover'>
                 {content}
             </StyledLink>
         );
@@ -90,7 +90,7 @@ export const LinkCell: React.FC<ILinkCellProps> = ({
 
     if (onClick) {
         return (
-            <StyledLink onClick={onClick} underline="hover">
+            <StyledLink onClick={onClick} underline='hover'>
                 {content}
             </StyledLink>
         );

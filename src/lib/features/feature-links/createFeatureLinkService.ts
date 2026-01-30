4caa07b8efcae8a37a5dae935923e93cd3a7ebad
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2026-01-30 11:08:47 +0100

    fix: client error on feature link missing feature (#11275)

--- src/lib/features/feature-links/createFeatureLinkService.ts
+++ src/lib/features/feature-links/createFeatureLinkService.ts
@@ -7,25 +7,36 @@ import {
 } from '../events/createEventsService.js';
 import type { Db } from '../../db/db.js';
 import { FeatureLinkStore } from './feature-link-store.js';
+import { FeaturesReadModel } from '../feature-toggle/features-read-model.js';
+import { FakeFeaturesReadModel } from '../feature-toggle/fakes/fake-features-read-model.js';
+import type { IFeaturesReadModel } from '../feature-toggle/types/features-read-model-type.js';
 
 export const createFeatureLinkService =
     (config: IUnleashConfig) => (db: Db) => {
         const eventService = createEventsService(db, config);
         const featureLinkStore = new FeatureLinkStore(db, config);
+        const featuresReadModel = new FeaturesReadModel(db);
 
         return new FeatureLinkService(
-            { featureLinkStore },
+            { featureLinkStore, featuresReadModel },
             config,
             eventService,
         );
     };
 
-export const createFakeFeatureLinkService = (config: IUnleashConfig) => {
+export const createFakeFeatureLinkService = (
+    config: IUnleashConfig,
+    overrides?: {
+        featuresReadModel?: IFeaturesReadModel;
+    },
+) => {
     const eventService = createFakeEventsService(config);
     const featureLinkStore = new FakeFeatureLinkStore();
+    const featuresReadModel =
+        overrides?.featuresReadModel ?? new FakeFeaturesReadModel();
 
     const featureLinkService = new FeatureLinkService(
-        { featureLinkStore },
+        { featureLinkStore, featuresReadModel },
         config,
         eventService,
     );

2c6f226685b34fea7578d564575b4fff309babb7
Nuno Góis <github@nunogois.com>
2026-02-13 09:54:58 +0000

    chore: update migration

--- src/migrations/20260213095258-users-company-role-and-email-consent.js
+++ src/migrations/20260213095258-users-company-role-and-email-consent.js
@@ -1,7 +1,6 @@
 exports.up = function (db, cb) {
     db.runSql(
         `ALTER TABLE users
-            ADD COLUMN IF NOT EXISTS should_set_password BOOLEAN,
             ADD COLUMN IF NOT EXISTS company_role TEXT,
             ADD COLUMN IF NOT EXISTS product_updates_email_consent BOOLEAN;`,
         cb,
@@ -11,7 +10,6 @@ exports.up = function (db, cb) {
 exports.down = function (db, cb) {
     db.runSql(
         `ALTER TABLE users
-            DROP COLUMN IF EXISTS should_set_password,
             DROP COLUMN IF EXISTS company_role,
             DROP COLUMN IF EXISTS product_updates_email_consent;`,
         cb,

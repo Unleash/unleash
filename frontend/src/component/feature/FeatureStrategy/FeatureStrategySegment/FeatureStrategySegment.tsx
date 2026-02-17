b4b417e7cae876714636d43dbc45d0b773a282eb
Thomas Heartman <thomas@getunleash.io>
2026-02-17 11:46:36 +0100

    chore: remove milestone strategy segment, merge with feature strategy segment (#11338)
    Replaces the milestone strategy segment component with the feature
    strategy segment component and deletes the milestone strategy segment
    component.
    
    To achieve this, I have:
    - Removed the hardcoded project segment filter from the feature strategy
    segment component. This has now become a component prop
    (`selectableSegmentFilter`) instead.
    - Populated this prop with the previous hardcoded filter function in the
    two existing call sites
    - Populated it with a filter that filters out any project-specific
    segments from the milestone form.
    
    ## Discussion points
    
    ### ~Potential~ Bonus bugfix
    
    Previously, we wouldn't filter out project segments from the dropdown
    for milestone strategies. This means that you can select project
    segments in a template, but then apply the template to a project that
    doesn't have those segments. This seems unintentional, so I've added a
    filter for it in this iteration.
    
    Before merging, we should double check that that's a good(tm) decision.
    Update: David agrees that this was an oversight.
    
    ### Recently used segments
    
    The main difference (aside from project filtering) between the two
    components is that feature strategy segments includes "recently used
    segments". I can't really see a reason why we'd want to exclude those
    from milestone strategies, so I think including them makes sense.
    However, they don't do any filtering for project/non-project segments,
    so I've added a task to add that in.
    
    ---
    
    Closes 1-4465.

--- frontend/src/component/feature/FeatureStrategy/FeatureStrategySegment/FeatureStrategySegment.tsx
+++ frontend/src/component/feature/FeatureStrategy/FeatureStrategySegment/FeatureStrategySegment.tsx
@@ -1,5 +1,4 @@
 import type React from 'react';
-import { useSegments } from 'hooks/api/getters/useSegments/useSegments';
 import type { ISegment } from 'interfaces/segment';
 import {
     AutocompleteBox,
@@ -10,13 +9,12 @@ import { SegmentDocsStrategyWarning } from 'component/segments/SegmentDocs';
 import { useSegmentLimits } from 'hooks/api/getters/useSegmentLimits/useSegmentLimits';
 import { Box, styled, Typography } from '@mui/material';
 import { HelpIcon } from 'component/common/HelpIcon/HelpIcon';
-
 import { RecentlyUsedSegments } from './RecentlyUsedSegments/RecentlyUsedSegments.tsx';
 
 interface IFeatureStrategySegmentProps {
     segments: ISegment[];
     setSegments: React.Dispatch<React.SetStateAction<ISegment[]>>;
-    projectId: string;
+    availableSegments: ISegment[];
 }
 
 const StyledHelpIconBox = styled(Box)(({ theme }) => ({
@@ -29,9 +27,8 @@ const StyledHelpIconBox = styled(Box)(({ theme }) => ({
 export const FeatureStrategySegment = ({
     segments: selectedSegments,
     setSegments: setSelectedSegments,
-    projectId,
+    availableSegments,
 }: IFeatureStrategySegmentProps) => {
-    const { segments: allSegments } = useSegments();
     const { strategySegmentsLimit } = useSegmentLimits();
 
     const atStrategySegmentsLimit: boolean = Boolean(
@@ -39,15 +36,7 @@ export const FeatureStrategySegment = ({
             selectedSegments.length >= strategySegmentsLimit,
     );
 
-    if (!allSegments || allSegments.length === 0) {
-        return null;
-    }
-
-    const allSelectableSegments = allSegments.filter(
-        ({ project }) => !project || project === projectId,
-    );
-
-    const unusedSegments = allSelectableSegments.filter((segment) => {
+    const unusedSegments = availableSegments.filter((segment) => {
         return !selectedSegments.find((selected) => selected.id === segment.id);
     });
 
@@ -57,7 +46,7 @@ export const FeatureStrategySegment = ({
     }));
 
     const onChange = ([option]: IAutocompleteBoxOption[]) => {
-        const selectedSegment = allSegments.find((segment) => {
+        const selectedSegment = availableSegments.find((segment) => {
             return String(segment.id) === option.value;
         });
         if (selectedSegment) {

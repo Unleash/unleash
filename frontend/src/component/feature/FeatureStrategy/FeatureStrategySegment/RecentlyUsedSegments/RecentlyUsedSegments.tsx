ce1e77fddafa2b86e27d4a0902a4f0c87a747d8e
Thomas Heartman <thomas@getunleash.io>
2026-02-17 12:58:18 +0100

    fix: only show assignable segments in recently used segments (#11340)
    Previously, the recently used segments were not filtered in any way,
    meaning that they might show you a project segment belonging to a
    different project than the one you're working in.
    
    This fix uses the new `useAssignableSegments` hook to give you only
    root-level segments in root contexts and root and project-level segments
    (for the current project) in project contexts.
    
    closes 1-4468

--- frontend/src/component/feature/FeatureStrategy/FeatureStrategySegment/RecentlyUsedSegments/RecentlyUsedSegments.tsx
+++ frontend/src/component/feature/FeatureStrategy/FeatureStrategySegment/RecentlyUsedSegments/RecentlyUsedSegments.tsx
@@ -1,12 +1,10 @@
 import { styled, Typography } from '@mui/material';
-import { useRecentlyUsedSegments } from './useRecentlyUsedSegments.ts';
 import type { ISegment } from 'interfaces/segment';
 import { RecentlyUsedSegmentChip } from './RecentlyUsedSegmentChip.tsx';
-import { useSegments } from 'hooks/api/getters/useSegments/useSegments';
 
 type RecentlyUsedSegmentsProps = {
-    setSegments?: React.Dispatch<React.SetStateAction<ISegment[]>>;
-    segments?: ISegment[];
+    setSegments: React.Dispatch<React.SetStateAction<ISegment[]>>;
+    segments: ISegment[];
 };
 
 const StyledContainer = styled('div')(({ theme }) => ({
@@ -27,31 +25,13 @@ const StyledSegmentsContainer = styled('div')(({ theme }) => ({
 
 export const RecentlyUsedSegments = ({
     setSegments,
-    segments = [],
+    segments,
 }: RecentlyUsedSegmentsProps) => {
-    const { items: recentlyUsedSegmentIds } = useRecentlyUsedSegments();
-    const { segments: allSegments } = useSegments();
-    if (recentlyUsedSegmentIds.length === 0 || !setSegments || !allSegments) {
-        return null;
-    }
-
-    const segmentObjects = recentlyUsedSegmentIds
-        .map((id) => allSegments.find((segment) => segment.id === id))
-        .filter((segment) => segment !== undefined) as ISegment[];
-
-    const nonSelectedRecentSegments = segmentObjects.filter(
-        (segment) => !segments.some((selected) => selected.id === segment.id),
-    );
-
-    if (nonSelectedRecentSegments.length === 0) {
-        return null;
-    }
-
     return (
         <StyledContainer>
             <StyledHeader>Recently used segments</StyledHeader>
             <StyledSegmentsContainer>
-                {nonSelectedRecentSegments.map((segment) => (
+                {segments.map((segment) => (
                     <RecentlyUsedSegmentChip
                         key={segment.id}
                         segment={segment}

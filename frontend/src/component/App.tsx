Unleash/unleash

commit 587681836b8cd1f5961da11bdd3be5bdf2f31b38

chore: fix signup-related dialog zIndex (#11396)
https://linear.app/unleash/issue/SA-126/dont-show-release-management-dialog-on-top-of-key-concepts

Ensures signup-related dialogs zIndex make sense: welcome dialog above
other dialogs, signup dialog above the welcome one.

frontend/src/component/personalDashboard/WelcomeDialog.tsx
@@ -8,6 +8,7 @@ import { ScreenReaderOnly } from '../common/ScreenReaderOnly/ScreenReaderOnly.ts
 import { formatAssetPath } from 'utils/formatPath';
 
 const StyledDialog = styled(Dialog)(({ theme }) => ({
+    zIndex: theme.zIndex.modal + 1,
     '& .MuiDialog-paper': {
         borderRadius: theme.shape.borderRadiusLarge,
         width: '65vw',

frontend/src/component/signup/SignupDialog/SignupDialog.tsx
@@ -28,6 +28,7 @@ const StyledUnleashLogo = styled(UnleashLogo)({
 });
 
 const StyledDialog = styled(Dialog)(({ theme }) => ({
+    zIndex: theme.zIndex.modal + 2,
     '.MuiBackdrop-root': {
         backdropFilter: 'blur(8px)',
     },

commit b7ac5d67aeae08473df97d05a756fe718d3a322e

chore: fix dialog order and other zindex problems (#11424)
https://linear.app/unleash/issue/SA-144/fix-dialog-order-and-other-zindex-problems

Fixes initial dialog order and other zIndex problems introduced in
https://github.com/Unleash/unleash/pull/11396

frontend/src/component/App.tsx
@@ -25,6 +25,7 @@ import { LoginRedirect } from './common/LoginRedirect/LoginRedirect.tsx';
 import { SecurityBanner } from './banners/internalBanners/SecurityBanner.tsx';
 import { MonthsOldVersionBanner } from './banners/internalBanners/MonthsOldVersionBanner.tsx';
 import { SignupDialog } from './signup/SignupDialog/SignupDialog.tsx';
+import { WelcomeDialog } from './personalDashboard/WelcomeDialog.tsx';
 
 const StyledContainer = styled('div')(() => ({
     '& ul': {
@@ -115,6 +116,8 @@ export const App = () => {
                                     <FeedbackNPS openUrl='http://feedback.unleash.run' />
 
                                     <SplashOverlay />
+
+                                    <WelcomeDialog />
 
                                     <SignupDialog />
                                 </StyledContainer>

frontend/src/component/personalDashboard/PersonalDashboard.tsx
@@ -7,15 +7,11 @@ import {
     styled,
     Typography,
 } from '@mui/material';
-import { WelcomeDialog } from './WelcomeDialog.tsx';
-import { useLocalStorageState } from 'hooks/useLocalStorageState';
 import { usePersonalDashboard } from 'hooks/api/getters/usePersonalDashboard/usePersonalDashboard';
 import { usePersonalDashboardProjectDetails } from 'hooks/api/getters/usePersonalDashboard/usePersonalDashboardProjectDetails';
 import { MyProjects } from './MyProjects.tsx';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import { usePlausibleTracker } from 'hooks/usePlausibleTracker';
-import useSplashApi from 'hooks/api/actions/useSplashApi/useSplashApi';
-import { useAuthSplash } from 'hooks/api/getters/useAuth/useAuthSplash';
 import { useDashboardState } from './useDashboardState.ts';
 import { MyFlags } from './MyFlags.tsx';
 import { usePageTitle } from 'hooks/usePageTitle';
@@ -28,6 +24,7 @@ import { EventTimeline } from 'component/events/EventTimeline/EventTimeline';
 import { AccordionContent } from './SharedComponents.tsx';
 import { Link } from 'react-router-dom';
 import { ReleaseTemplatesBanner } from 'component/common/ReleaseTemplatesBanner/ReleaseTemplatesBanner';
+import { useWelcomeDialogContext } from './WelcomeDialogContext.tsx';
 
 const WelcomeSection = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -274,20 +271,12 @@ const ProjectPanel = () => {
 export const PersonalDashboard = () => {
     const { user } = useAuthUser();
     const { trackEvent } = usePlausibleTracker();
-    const { setSplashSeen } = useSplashApi();
-    const { splash } = useAuthSplash();
+    const { setWelcomeDialog } = useWelcomeDialogContext();
     const { isOss, isEnterprise } = useUiConfig();
     const gtmReleaseManagementEnabled = useUiFlag('gtmReleaseManagement');
     const name = user?.name || '';
 
     usePageTitle(name ? `Dashboard: ${name}` : 'Dashboard');
-
-    const [welcomeDialog, setWelcomeDialog] = useLocalStorageState<
-        'open' | 'closed'
-    >(
-        'welcome-dialog:v1',
-        splash?.personalDashboardKeyConcepts ? 'closed' : 'open',
-    );
 
     useEffect(() => {
         trackEvent('personal-dashboard', {
@@ -333,14 +322,6 @@ export const PersonalDashboard = () => {
             <ProjectPanel />
 
             <FlagPanel />
-
-            <WelcomeDialog
-                open={welcomeDialog === 'open'}
-                onClose={() => {
-                    setSplashSeen('personalDashboardKeyConcepts');
-                    setWelcomeDialog('closed');
-                }}
-            />
         </MainContent>
     );
 };

frontend/src/component/personalDashboard/WelcomeDialog.tsx
@@ -1,14 +1,13 @@
 import { Box, Button, Dialog, styled, Typography } from '@mui/material';
-import type { FC } from 'react';
 import { ReactComponent as UnleashLogo } from 'assets/img/logoDarkWithText.svg';
 import { ReactComponent as UnleashLogoWhite } from 'assets/img/logoWithWhiteText.svg';
 import { ThemeMode } from '../common/ThemeMode/ThemeMode.tsx';
 import onboardingConcepts from 'assets/img/onboardingConcepts.png';
 import { ScreenReaderOnly } from '../common/ScreenReaderOnly/ScreenReaderOnly.tsx';
 import { formatAssetPath } from 'utils/formatPath';
+import { useWelcomeDialogContext } from './WelcomeDialogContext.tsx';
 
 const StyledDialog = styled(Dialog)(({ theme }) => ({
-    zIndex: theme.zIndex.modal + 1,
     '& .MuiDialog-paper': {
         borderRadius: theme.shape.borderRadiusLarge,
         width: '65vw',
@@ -44,14 +43,11 @@ const StyledImg = styled('img')({
     width: '100%',
 });
 
-interface IWelcomeDialogProps {
-    open: boolean;
-    onClose: () => void;
-}
+export const WelcomeDialog = () => {
+    const { welcomeDialog, onClose } = useWelcomeDialogContext();
 
-export const WelcomeDialog: FC<IWelcomeDialogProps> = ({ open, onClose }) => {
     return (
-        <StyledDialog open={open} onClose={onClose}>
+        <StyledDialog open={welcomeDialog === 'open'} onClose={onClose}>
             <ContentWrapper>
                 <WelcomeLine>
                     <Typography variant='h2'>Welcome to</Typography>

frontend/src/component/personalDashboard/WelcomeDialogContext.tsx
@@ -1,0 +1,18 @@
+import { createContext, useContext } from 'react';
+import type { IWelcomeDialogContext } from './WelcomeDialogProvider.tsx';
+
+export const WelcomeDialogContext = createContext<
+    IWelcomeDialogContext | undefined
+>(undefined);
+
+export const useWelcomeDialogContext = (): IWelcomeDialogContext => {
+    const context = useContext(WelcomeDialogContext);
+
+    if (!context) {
+        throw new Error(
+            'useWelcomeDialogContext must be used within a WelcomeDialogProvider',
+        );
+    }
+
+    return context;
+};

frontend/src/component/personalDashboard/WelcomeDialogProvider.tsx
@@ -1,0 +1,46 @@
+import type { Dispatch, SetStateAction, ReactNode } from 'react';
+import { WelcomeDialogContext } from './WelcomeDialogContext.tsx';
+import { useAuthSplash } from 'hooks/api/getters/useAuth/useAuthSplash.ts';
+import useSplashApi from 'hooks/api/actions/useSplashApi/useSplashApi.ts';
+import { useLocalStorageState } from 'hooks/useLocalStorageState.ts';
+
+type DialogState = 'open' | 'closed';
+
+export interface IWelcomeDialogContext {
+    welcomeDialog: DialogState;
+    setWelcomeDialog: Dispatch<SetStateAction<DialogState>>;
+    onClose: () => void;
+}
+
+interface IWelcomeDialogProviderProps {
+    children: ReactNode;
+}
+
+export const WelcomeDialogProvider = ({
+    children,
+}: IWelcomeDialogProviderProps) => {
+    const { splash } = useAuthSplash();
+    const { setSplashSeen } = useSplashApi();
+
+    const [welcomeDialog, setWelcomeDialog] = useLocalStorageState<DialogState>(
+        'welcome-dialog:v1',
+        splash?.personalDashboardKeyConcepts ? 'closed' : 'open',
+    );
+
+    const onClose = () => {
+        setSplashSeen('personalDashboardKeyConcepts');
+        setWelcomeDialog('closed');
+    };
+
+    const contextValue: IWelcomeDialogContext = {
+        welcomeDialog,
+        setWelcomeDialog,
+        onClose,
+    };
+
+    return (
+        <WelcomeDialogContext.Provider value={contextValue}>
+            {children}
+        </WelcomeDialogContext.Provider>
+    );
+};

frontend/src/component/signup/SignupDialog/SignupDialog.tsx
@@ -28,7 +28,6 @@ const StyledUnleashLogo = styled(UnleashLogo)({
 });
 
 const StyledDialog = styled(Dialog)(({ theme }) => ({
-    zIndex: theme.zIndex.modal + 2,
     '.MuiBackdrop-root': {
         backdropFilter: 'blur(8px)',
     },

frontend/src/index.tsx
@@ -23,6 +23,7 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { useRecordUIErrorApi } from 'hooks/api/actions/useRecordUIErrorApi/useRecordUiErrorApi';
 import { HighlightProvider } from 'component/common/Highlight/HighlightProvider';
 import { UnleashFlagProvider } from 'component/providers/UnleashFlagProvider/UnleashFlagProvider';
+import { WelcomeDialogProvider } from 'component/personalDashboard/WelcomeDialogProvider.tsx';
 
 window.global ||= window;
 
@@ -60,10 +61,12 @@ const ApplicationRoot = () => {
                                                 <FeedbackCESProvider>
                                                     <StickyProvider>
                                                         <HighlightProvider>
-                                                            <InstanceStatus>
-                                                                <ScrollTop />
-                                                                <App />
-                                                            </InstanceStatus>
+                                                            <WelcomeDialogProvider>
+                                                                <InstanceStatus>
+                                                                    <ScrollTop />
+                                                                    <App />
+                                                                </InstanceStatus>
+                                                            </WelcomeDialogProvider>
                                                         </HighlightProvider>
                                                     </StickyProvider>
                                                 </FeedbackCESProvider>

frontend/src/component/personalDashboard/PersonalDashboard.test.tsx
@@ -1,7 +1,8 @@
 import { PersonalDashboard } from './PersonalDashboard.tsx';
 import { render } from 'utils/testRenderer';
-import { fireEvent, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { testServerRoute, testServerSetup } from 'utils/testServer';
+import { WelcomeDialogProvider } from './WelcomeDialogProvider.tsx';
 
 const server = testServerSetup();
 
@@ -130,13 +131,11 @@ HTMLElement.prototype.scrollIntoView = () => {};
 
 test('Render personal dashboard for a long running project', async () => {
     setupLongRunningProject();
-    render(<PersonalDashboard />);
-
-    const welcomeDialogClose = await screen.findByText(
-        "Got it, let's get started!",
+    render(
+        <WelcomeDialogProvider>
+            <PersonalDashboard />
+        </WelcomeDialogProvider>,
     );
-
-    fireEvent.click(welcomeDialogClose);
 
     await screen.findByText('Welcome Unleash User');
     await screen.findByText('projectName');
@@ -155,7 +154,11 @@ test('Render personal dashboard for a long running project', async () => {
 
 test('Render personal dashboard for a new project', async () => {
     setupNewProject();
-    render(<PersonalDashboard />);
+    render(
+        <WelcomeDialogProvider>
+            <PersonalDashboard />
+        </WelcomeDialogProvider>,
+    );
 
     await screen.findByText('Welcome Unleash User');
     await screen.findByText('projectName');
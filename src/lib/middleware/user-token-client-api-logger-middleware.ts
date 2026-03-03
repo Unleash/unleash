0cb3d4d7a75f1bc627e552560e09cafbc5841175
David Leek <david@getunleash.io>
2026-03-03 15:32:52 +0100

    chore: fix multiline log entry

--- src/lib/middleware/user-token-client-api-logger-middleware.ts
+++ src/lib/middleware/user-token-client-api-logger-middleware.ts
@@ -17,8 +17,9 @@ export const userTokenClientApiLogger = ({
             apiToken?.startsWith('user:') &&
             flagResolver.isEnabled('userTokenWithClientApiLogging')
         ) {
-            logger.info(`In the next version update, calling API endpoints under /api/client/ using Personal Access Tokens and Service Accounts will no longer be supported.
-            Please update your integrations to use the new supported authentication method before upgrading to avoid service disruption.`);
+            logger.info(
+                'In the next version update, calling API endpoints under /api/client/ using Personal Access Tokens and Service Accounts will no longer be supported. Please update your integrations to use the new supported authentication method before upgrading to avoid service disruption.',
+            );
         }
         next();
     };

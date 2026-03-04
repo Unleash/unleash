34faa49f78675d2ed3ea16dcd71f7bf12a1d10b9
Nuno Góis <github@nunogois.com>
2026-03-04 15:19:13 +0000

    test: fix find

--- frontend/cypress/support/UI.ts
+++ frontend/cypress/support/UI.ts
@@ -41,9 +41,13 @@ export const do_login = (
     // Wait for the login redirect to complete.
     cy.get("[data-testid='HEADER_USER_AVATAR']");
 
-    cy.get('body')
-        .find("[data-testid='CLOSE_SPLASH']")
-        .then(($el) => $el.length && cy.wrap($el).click({ multiple: true }));
+    cy.get('body').then(($body) => {
+        const $el = $body.find("[data-testid='CLOSE_SPLASH']");
+
+        if ($el.length) {
+            cy.wrap($el).click({ multiple: true });
+        }
+    });
 
     return cy;
 };

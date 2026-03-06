41556105183e97d0fa46667101118dfe28c4f1b0
Kris (K) Kula <kris.kula@getunleash.io>
2026-03-06 13:40:13 +0100

    Bigger screenshots and try just clicking

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -6,6 +6,8 @@ export default defineConfig({
     projectId: 'tc2qff',
     defaultCommandTimeout: 12000,
     screenshotOnRunFailure: true,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     video: false,
     e2e: {
         specPattern: '**/*.spec.ts',

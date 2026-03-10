11ece04cfa2f745856950bc2bf9e0f65afc33dbf
Kris (K) Kula <kris.kula@getunleash.io>
2026-03-10 19:54:38 +0100

    simpler test

--- frontend/cypress/oss/feature/feature.spec.ts
+++ frontend/cypress/oss/feature/feature.spec.ts
@@ -19,7 +19,6 @@ describe('feature', () => {
 
     it('can create a feature flag', () => {
         cy.createFeature_UI(featureToggleName, true, 'default', true);
-        cy.contains(featureToggleName).should('be.visible').click();
-        cy.url().should('include', featureToggleName);
+        cy.contains(featureToggleName).should('be.visible');
     });
 });

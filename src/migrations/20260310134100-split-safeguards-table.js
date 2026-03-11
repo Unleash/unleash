71e36a417f051d51974d5665a369abf2f48ec3c9
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2026-03-11 08:13:52 +0100

    chore: remove insert for future migration

--- src/migrations/20260310134100-split-safeguards-table.js
+++ src/migrations/20260310134100-split-safeguards-table.js
@@ -20,26 +20,6 @@ exports.up = function (db, cb) {
             feature_name TEXT NOT NULL,
             FOREIGN KEY (environment, feature_name) REFERENCES feature_environments(environment, feature_name) ON DELETE CASCADE
         );
-
-        INSERT INTO release_plan_safeguards (id, impact_metric_id, trigger_condition, release_plan_id, environment, feature_name)
-        SELECT
-            s.id, s.impact_metric_id, s.trigger_condition,
-            s.action->>'id',
-            rpd.environment,
-            rpd.feature_name
-        FROM safeguards s
-            JOIN release_plan_definitions rpd ON rpd.id = (s.action->>'id')
-        WHERE s.action->>'type' = 'pauseReleasePlanProgressions';
-
-        INSERT INTO feature_environment_safeguards (id, impact_metric_id, trigger_condition, environment, feature_name, project)
-        SELECT
-            s.id, s.impact_metric_id, s.trigger_condition,
-            s.action->>'environment',
-            s.action->>'featureName',
-            s.action->>'project'
-        FROM safeguards s
-        WHERE s.action->>'type' = 'disableFeatureEnvironment';
-
         `,
         cb,
     );

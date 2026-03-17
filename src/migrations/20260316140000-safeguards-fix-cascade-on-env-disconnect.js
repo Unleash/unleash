5f8b3e94f50f27a146f3709f82e290c3964a8771
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2026-03-17 09:55:19 +0100

    feat: keep safeguards on env disconnect

--- src/migrations/20260316140000-safeguards-fix-cascade-on-env-disconnect.js
+++ src/migrations/20260316140000-safeguards-fix-cascade-on-env-disconnect.js
@@ -5,8 +5,14 @@ exports.up = function (db, cb) {
             DROP CONSTRAINT IF EXISTS feature_environment_safeguards_environment_feature_name_fkey;
 
         ALTER TABLE feature_environment_safeguards
+            DROP CONSTRAINT IF EXISTS feature_environment_safeguards_feature_name_fkey;
+
+        ALTER TABLE feature_environment_safeguards
             ADD CONSTRAINT feature_environment_safeguards_feature_name_fkey
             FOREIGN KEY (feature_name) REFERENCES features(name) ON DELETE CASCADE;
+
+        ALTER TABLE feature_environment_safeguards
+            DROP CONSTRAINT IF EXISTS feature_environment_safeguards_environment_fkey;
 
         ALTER TABLE feature_environment_safeguards
             ADD CONSTRAINT feature_environment_safeguards_environment_fkey
@@ -16,8 +22,14 @@ exports.up = function (db, cb) {
             DROP CONSTRAINT IF EXISTS release_plan_safeguards_environment_feature_name_fkey;
 
         ALTER TABLE release_plan_safeguards
+            DROP CONSTRAINT IF EXISTS release_plan_safeguards_feature_name_fkey;
+
+        ALTER TABLE release_plan_safeguards
             ADD CONSTRAINT release_plan_safeguards_feature_name_fkey
             FOREIGN KEY (feature_name) REFERENCES features(name) ON DELETE CASCADE;
+
+        ALTER TABLE release_plan_safeguards
+            DROP CONSTRAINT IF EXISTS release_plan_safeguards_environment_fkey;
 
         ALTER TABLE release_plan_safeguards
             ADD CONSTRAINT release_plan_safeguards_environment_fkey

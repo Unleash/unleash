419d5d5f88b928e623e4966c635160b3ac923de3
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2026-03-17 11:32:33 +0100

    feat: keep safeguards on env disconnect (#11626)

---
+++ src/migrations/20260316140000-safeguards-fix-cascade-on-env-disconnect.js
@@ -0,0 +1,59 @@
+exports.up = function (db, cb) {
+    db.runSql(
+        `
+        ALTER TABLE feature_environment_safeguards
+            DROP CONSTRAINT IF EXISTS feature_environment_safeguards_environment_feature_name_fkey;
+
+        ALTER TABLE feature_environment_safeguards
+            ADD CONSTRAINT feature_environment_safeguards_feature_name_fkey
+            FOREIGN KEY (feature_name) REFERENCES features(name) ON DELETE CASCADE;
+
+        ALTER TABLE feature_environment_safeguards
+            ADD CONSTRAINT feature_environment_safeguards_environment_fkey
+            FOREIGN KEY (environment) REFERENCES environments(name) ON DELETE CASCADE;
+
+        ALTER TABLE release_plan_safeguards
+            DROP CONSTRAINT IF EXISTS release_plan_safeguards_environment_feature_name_fkey;
+
+        ALTER TABLE release_plan_safeguards
+            ADD CONSTRAINT release_plan_safeguards_feature_name_fkey
+            FOREIGN KEY (feature_name) REFERENCES features(name) ON DELETE CASCADE;
+
+        ALTER TABLE release_plan_safeguards
+            ADD CONSTRAINT release_plan_safeguards_environment_fkey
+            FOREIGN KEY (environment) REFERENCES environments(name) ON DELETE CASCADE;
+        `,
+        cb,
+    );
+};
+
+exports.down = function (db, cb) {
+    db.runSql(
+        `
+        ALTER TABLE feature_environment_safeguards
+            DROP CONSTRAINT feature_environment_safeguards_environment_fkey;
+
+        ALTER TABLE feature_environment_safeguards
+            DROP CONSTRAINT feature_environment_safeguards_feature_name_fkey;
+
+        ALTER TABLE feature_environment_safeguards
+            ADD CONSTRAINT feature_environment_safeguards_environment_feature_name_fkey
+            FOREIGN KEY (environment, feature_name)
+            REFERENCES feature_environments(environment, feature_name)
+            ON DELETE CASCADE;
+
+        ALTER TABLE release_plan_safeguards
+            DROP CONSTRAINT release_plan_safeguards_environment_fkey;
+
+        ALTER TABLE release_plan_safeguards
+            DROP CONSTRAINT release_plan_safeguards_feature_name_fkey;
+
+        ALTER TABLE release_plan_safeguards
+            ADD CONSTRAINT release_plan_safeguards_environment_feature_name_fkey
+            FOREIGN KEY (environment, feature_name)
+            REFERENCES feature_environments(environment, feature_name)
+            ON DELETE CASCADE;
+        `,
+        cb,
+    );
+};

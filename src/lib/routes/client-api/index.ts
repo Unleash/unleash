3e05a95c7b2abc3e2c77b60dd48caab92a87d748
Gastón Fournier <gaston@getunleash.io>
2026-03-17 16:48:38 +0100

    Revert nonsense

--- src/lib/routes/client-api/index.ts
+++ src/lib/routes/client-api/index.ts
@@ -1,6 +1,5 @@
 import Controller from '../controller.js';
 import FeatureController from '../../features/client-feature-toggles/client-feature-toggle.controller.js';
-import ClientFeatureToggleDeltaController from '../../features/client-feature-toggles/delta/client-feature-toggle-delta-controller.js';
 import MetricsController from '../../features/metrics/instance/metrics.js';
 import RegisterController from '../../features/metrics/instance/register.js';
 import type { IUnleashConfig } from '../../types/index.js';
@@ -10,10 +9,6 @@ export default class ClientApi extends Controller {
     constructor(config: IUnleashConfig, services: IUnleashServices) {
         super(config);
 
-        this.use(
-            '/delta',
-            new ClientFeatureToggleDeltaController(services, config).router,
-        );
         this.use('/features', new FeatureController(services, config).router);
         this.use('/metrics', new MetricsController(services, config).router);
         this.use('/register', new RegisterController(services, config).router);

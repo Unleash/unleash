1bb8b0a2f1d6f0fbd3de0de2cc78f59a60073a29
Jaanus Sellin <sellinjaanus@gmail.com>
2026-03-20 09:12:00 +0200

    feat: add new forgotten password page with updated styling (#11636)

--- frontend/src/component/user/ForgottenPassword/DeprecatedForgottenPassword.tsx
+++ frontend/src/component/user/ForgottenPassword/DeprecatedForgottenPassword.tsx
@@ -1,33 +1,27 @@
-import {
-    Button,
-    Divider,
-    styled,
-    TextField,
-    Typography,
-    AlertTitle,
-    Alert,
-} from '@mui/material';
+import { Button, styled, TextField, Typography } from '@mui/material';
+import { AlertTitle, Alert } from '@mui/material';
 import { type SyntheticEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 import useLoading from 'hooks/useLoading';
 import { FORGOTTEN_PASSWORD_FIELD } from 'utils/testIds';
 import { formatApiPath } from 'utils/formatPath';
 import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
-import { useFlag } from '@unleash/proxy-client-react';
-import DeprecatedForgottenPassword from './DeprecatedForgottenPassword';
-import { AuthPageLayout } from '../common/AuthPageLayout';
+import DividerText from 'component/common/DividerText/DividerText';
+import StandaloneLayout from '../common/StandaloneLayout.tsx';
+import {
+    contentSpacingY,
+    flexColumn,
+    textCenter,
+    title,
+} from 'themes/themeStyles';
 
-const StyledTitle = styled(Typography)(({ theme }) => ({
-    fontSize: theme.typography.h2.fontSize,
-    fontWeight: theme.typography.fontWeightBold,
-    lineHeight: '28px',
-    marginBottom: theme.spacing(3),
-}));
-
-const StyledForm = styled('form')(({ theme }) => ({
-    display: 'flex',
-    flexDirection: 'column',
-    gap: theme.spacing(3),
+const StyledDiv = styled('div')(({ theme }) => ({
+    ...contentSpacingY,
+    ...flexColumn,
+    width: '350px',
+    [theme.breakpoints.down('sm')]: {
+        width: '100%',
+    },
 }));
 
 const StyledStrong = styled('strong')(({ theme }) => ({
@@ -35,15 +29,34 @@ const StyledStrong = styled('strong')(({ theme }) => ({
     margin: theme.spacing(1, 0),
 }));
 
+const StyledButton = styled(Button)(({ theme }) => ({
+    width: '150px',
+    margin: theme.spacing(2, 'auto'),
+}));
+
+const StyledTitle = styled('h2')(({ theme }) => ({
+    ...title(theme),
+    ...textCenter,
+}));
+
+const StyledForm = styled('form')(({ theme }) => ({
+    ...contentSpacingY(theme),
+    ...flexColumn,
+}));
+
+const StyledTypography = styled(Typography)(({ theme }) => ({
+    ...textCenter,
+}));
+
 type State = 'initial' | 'loading' | 'attempted' | 'too_many_attempts';
 
-const NewForgottenPassword = () => {
+const DeprecatedForgottenPassword = () => {
     const [email, setEmail] = useState('');
     const [state, setState] = useState<State>('initial');
     const [attemptedEmail, setAttemptedEmail] = useState('');
     const ref = useLoading(state === 'loading');
 
-    const onSubmit = async (e: SyntheticEvent) => {
+    const onClick = async (e: SyntheticEvent) => {
         e.preventDefault();
         setState('loading');
         setAttemptedEmail(email);
@@ -64,13 +77,13 @@ const NewForgottenPassword = () => {
     };
 
     return (
-        <AuthPageLayout>
-            <div ref={ref}>
-                <StyledTitle>Forgot your password?</StyledTitle>
+        <StandaloneLayout>
+            <StyledDiv ref={ref}>
+                <StyledTitle data-loading>Forgotten password</StyledTitle>
                 <ConditionallyRender
                     condition={state === 'attempted'}
                     show={
-                        <Alert severity='success' sx={{ mb: 3 }}>
+                        <Alert severity='success' data-loading>
                             <AlertTitle>Attempted to send email</AlertTitle>
                             We've attempted to send a reset password email to:
                             <StyledStrong>{attemptedEmail}</StyledStrong>
@@ -84,7 +97,7 @@ const NewForgottenPassword = () => {
                 <ConditionallyRender
                     condition={state === 'too_many_attempts'}
                     show={
-                        <Alert severity='warning' sx={{ mb: 3 }}>
+                        <Alert severity='warning' data-loading>
                             <AlertTitle>
                                 Too many password reset attempts
                             </AlertTitle>
@@ -92,53 +105,55 @@ const NewForgottenPassword = () => {
                         </Alert>
                     }
                 />
-                <StyledForm onSubmit={onSubmit}>
-                    <Typography variant='body2'>
-                        Enter your email address. If it exists in the system,
-                        we'll send you a reset password link.
-                    </Typography>
+                <StyledForm onSubmit={onClick}>
+                    <StyledTypography variant='body1' data-loading>
+                        Please provide your email address. If it exists in the
+                        system we'll send a new reset link.
+                    </StyledTypography>
                     <TextField
-                        label='Email'
                         variant='outlined'
                         size='small'
+                        placeholder='email'
                         type='email'
+                        data-loading
                         data-testid={FORGOTTEN_PASSWORD_FIELD}
                         value={email}
                         onChange={(e) => {
                             setEmail(e.target.value);
                         }}
-                        autoFocus
                     />
-                    <Button
+                    <StyledButton
                         variant='contained'
                         type='submit'
+                        data-loading
                         color='primary'
                         disabled={state === 'loading'}
-                        fullWidth
                     >
-                        Reset password
-                    </Button>
-                    <Divider>OR</Divider>
+                        <ConditionallyRender
+                            condition={state === 'initial'}
+                            show={<span>Submit</span>}
+                            elseShow={<span>Try again</span>}
+                        />
+                    </StyledButton>
+                    <DividerText text='Or log in' />
                     <Button
+                        type='submit'
+                        data-loading
                         variant='outlined'
+                        disabled={state === 'loading'}
                         component={Link}
                         to='/login'
-                        fullWidth
+                        sx={(theme) => ({
+                            width: '150px',
+                            margin: theme.spacing(2, 'auto'),
+                        })}
                     >
-                        Go back to sign in
+                        Log in
                     </Button>
                 </StyledForm>
-            </div>
-        </AuthPageLayout>
+            </StyledDiv>
+        </StandaloneLayout>
     );
 };
 
-const ForgottenPassword = () => {
-    const newLogin = useFlag('newLogin');
-    if (newLogin) {
-        return <NewForgottenPassword />;
-    }
-    return <DeprecatedForgottenPassword />;
-};
-
-export default ForgottenPassword;
+export default DeprecatedForgottenPassword;
--- frontend/src/component/user/Login/Login.tsx
+++ frontend/src/component/user/Login/Login.tsx
@@ -10,63 +10,7 @@ import { getSessionStorageItem, setSessionStorageItem } from 'utils/storage';
 import { DEMO_TYPE } from 'constants/authTypes';
 import { useFlag } from '@unleash/proxy-client-react';
 import DeprecatedLogin from './DeprecatedLogin';
-import { ReactComponent as UnleashLogo } from 'assets/img/logoDarkWithText.svg';
-import loginBackground from 'assets/img/loginBackground.png';
-
-const StyledPage = styled('div')(({ theme }) => ({
-    minHeight: '100vh',
-    backgroundColor: theme.palette.background.application,
-}));
-
-const StyledBackground = styled('img')({
-    position: 'absolute',
-    right: 0,
-    width: '70%',
-    maxHeight: '100%',
-    objectFit: 'contain',
-    objectPosition: 'bottom right',
-});
-
-const StyledHeader = styled('header')(({ theme }) => ({
-    padding: theme.spacing(3),
-}));
-
-const StyledLogo = styled(UnleashLogo)({
-    width: 150,
-});
-
-const StyledMain = styled('main')(({ theme }) => ({
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: theme.spacing(2),
-}));
-
-const StyledCardWrapper = styled('div')({
-    position: 'relative',
-    width: 500,
-    maxWidth: '100%',
-});
-
-const StyledSquare = styled('div')<{
-    size: number;
-    color: string;
-}>(({ size, color }) => ({
-    position: 'absolute',
-    width: size,
-    height: size,
-    backgroundColor: color,
-}));
-
-const StyledCard = styled('div')(({ theme }) => ({
-    position: 'relative',
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(5, 4),
-    width: '100%',
-    [theme.breakpoints.down('sm')]: {
-        padding: theme.spacing(3, 2),
-    },
-}));
+import { AuthPageLayout } from '../common/AuthPageLayout';
 
 const StyledTitle = styled(Typography)(({ theme }) => ({
     fontSize: theme.typography.h2.fontSize,
@@ -90,65 +34,31 @@ const NewLogin = () => {
     }
 
     return (
-        <StyledPage>
-            <StyledBackground src={loginBackground} alt='' />
-            <StyledHeader>
-                <StyledLogo aria-label='Unleash logo' />
-            </StyledHeader>
-            <StyledMain>
-                <StyledCardWrapper>
-                    <StyledSquare
-                        size={8}
-                        color='#B3DAED'
-                        sx={{ top: -20, left: -20 }}
-                    />
-                    <StyledSquare
-                        size={24}
-                        color='#98E3AF'
-                        sx={{ top: -12, left: -12 }}
-                    />
-                    <StyledSquare
-                        size={24}
-                        color='#6c65e5'
-                        sx={{ bottom: -12, right: -12 }}
-                    />
-                    <StyledSquare
-                        size={8}
-                        color='#ffffff'
-                        sx={{ bottom: -20, right: -20 }}
-                    />
-                    <StyledCard>
-                        <ConditionallyRender
-                            condition={resetPassword}
-                            show={
-                                <Alert severity='success' sx={{ mb: 3 }}>
-                                    <AlertTitle>Success</AlertTitle>
-                                    You successfully reset your password.
-                                </Alert>
-                            }
-                        />
-                        <ConditionallyRender
-                            condition={invited}
-                            show={
-                                <Alert severity='success' sx={{ mb: 3 }}>
-                                    <AlertTitle>Success</AlertTitle>
-                                    Your account has been created.
-                                </Alert>
-                            }
-                        />
-                        <ConditionallyRender
-                            condition={authDetails?.type !== DEMO_TYPE}
-                            show={
-                                <StyledTitle>
-                                    Sign in to your instance
-                                </StyledTitle>
-                            }
-                        />
-                        <Authentication redirect={redirect} invited={invited} />
-                    </StyledCard>
-                </StyledCardWrapper>
-            </StyledMain>
-        </StyledPage>
+        <AuthPageLayout>
+            <ConditionallyRender
+                condition={resetPassword}
+                show={
+                    <Alert severity='success' sx={{ mb: 3 }}>
+                        <AlertTitle>Success</AlertTitle>
+                        You successfully reset your password.
+                    </Alert>
+                }
+            />
+            <ConditionallyRender
+                condition={invited}
+                show={
+                    <Alert severity='success' sx={{ mb: 3 }}>
+                        <AlertTitle>Success</AlertTitle>
+                        Your account has been created.
+                    </Alert>
+                }
+            />
+            <ConditionallyRender
+                condition={authDetails?.type !== DEMO_TYPE}
+                show={<StyledTitle>Sign in to your instance</StyledTitle>}
+            />
+            <Authentication redirect={redirect} invited={invited} />
+        </AuthPageLayout>
     );
 };
 

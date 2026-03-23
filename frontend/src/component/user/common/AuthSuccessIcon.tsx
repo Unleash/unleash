8d72c5b86a5433ac8111e8d9fac118bd9341e42d
Jaanus <sellinjaanus@gmail.com>
2026-03-23 16:48:10 +0200

    Fix

--- frontend/src/component/user/common/AuthSuccessIcon.tsx
+++ frontend/src/component/user/common/AuthSuccessIcon.tsx
@@ -1,12 +1,5 @@
-import { styled } from '@mui/material';
 import passwordSuccess from 'assets/img/passwordSuccess.png';
 
-export const AuthSuccessIcon = styled('img')({
-    width: 56,
-    height: 56,
-});
-
-AuthSuccessIcon.defaultProps = {
-    src: passwordSuccess,
-    alt: '',
-};
+export const AuthSuccessIcon = () => (
+    <img src={passwordSuccess} alt='' width={56} height={56} />
+);

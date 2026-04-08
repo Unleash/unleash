ceac756b5ce153e2db5f05b299d76024e4e946e2
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2026-04-08 13:33:33 +0200

    chore: orval gen with safegaurd split fix

--- frontend/src/interfaces/releasePlans.ts
+++ frontend/src/interfaces/releasePlans.ts
@@ -1,5 +1,8 @@
 import type { IFeatureStrategy } from './strategy.ts';
-import type { ReleasePlanSafeguardSchema, FeatureEnvironmentSafeguardSchema } from '../openapi/index.js';
+import type {
+    ReleasePlanSafeguardSchema,
+    FeatureEnvironmentSafeguardSchema,
+} from '../openapi/index.js';
 
 export interface IReleasePlanTemplate {
     id: string;
@@ -19,7 +22,9 @@ export interface IReleasePlanTemplate {
     archivedAt?: string;
 }
 
-export type ISafeguard = ReleasePlanSafeguardSchema | FeatureEnvironmentSafeguardSchema;
+export type ISafeguard =
+    | ReleasePlanSafeguardSchema
+    | FeatureEnvironmentSafeguardSchema;
 export interface IReleasePlan {
     id: string;
     name: string;

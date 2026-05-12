f61874d310bff6e9aa1f40feaf3850ed43528b04
Kris (K) Kula <kris.kula@getunleash.io>
2026-05-12 11:30:33 +0200

    fix lint

--- frontend/src/component/onboarding/dialog/NewConnectSdkDialog/GenerateApiKeyStep/TokenExplanation.test.tsx
+++ frontend/src/component/onboarding/dialog/NewConnectSdkDialog/GenerateApiKeyStep/TokenExplanation.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, test, vi } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { render } from 'utils/testRenderer';
 import { screen } from '@testing-library/react';
 import { TokenExplanation } from './TokenExplanation';

c6ea3424d7bbd2eea6f7ecac541270c6c0e3fe24
Kamala Rocci <kamala.rocci@getunleash.io>
2026-05-12 11:16:57 +0200

    fix broken props propagation + missing permission in test

--- frontend/src/component/common/AutocompleteBox/AutocompleteBox.tsx
+++ frontend/src/component/common/AutocompleteBox/AutocompleteBox.tsx
@@ -49,8 +49,6 @@ export const AutocompleteBox = ({
     const theme = useTheme();
 
     const renderCustomInput = (params: AutocompleteRenderInputParams) => {
-        const { input: inputSlotProps } = params.slotProps;
-
         let startAdornment: undefined | JSX.Element;
         if (icon !== null) {
             startAdornment = (
@@ -95,8 +93,9 @@ export const AutocompleteBox = ({
                 }}
                 placeholder={label}
                 slotProps={{
+                    ...params.slotProps,
                     input: {
-                        ...inputSlotProps,
+                        ...params.slotProps.input,
                         startAdornment,
                     },
                 }}

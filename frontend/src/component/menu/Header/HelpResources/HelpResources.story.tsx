f460650259568c23baa4aed9566fcd18236d5410
Krzysztof (Kris) Kula <kris.kula@getunleash.io>
2026-06-16 10:29:33 +0200

    chore: add new help icon with menu (#12308)
    ## About the changes
    
    Not used anywhere yet. Only the base component. 
    
    ### Important files
    
    ## Discussion points
    
    ## OSS PR checklist
    
    - [x] I have read and agree to the [Unleash Contributor License
    Agreement](https://github.com/Unleash/unleash/blob/main/CLA.md).
    - [x] I have added tests or explained why tests are not needed.
    - [x] I have updated documentation where relevant.
    
    
    There are still some discussions going on about colors/icons but overall
    its should be ok-ish.
    
    <img width="617" height="563" alt="Screenshot 2026-06-15 at 14 09 52"
    src="https://github.com/user-attachments/assets/74c245a4-4280-4c05-91f7-4f5ba5457f94"
    />
    <img width="540" height="605" alt="Screenshot 2026-06-15 at 14 09 39"
    src="https://github.com/user-attachments/assets/21d854dd-6380-4158-b970-d626c859470d"
    />

---
+++ frontend/src/component/menu/Header/HelpResources/HelpResources.story.tsx
@@ -0,0 +1,8 @@
+import type { Story, StoryMeta } from 'component/stories/types';
+import { HelpResources } from './HelpResources';
+
+export const meta: StoryMeta = {
+    title: 'Menu/Header/HelpResources',
+};
+
+export const Default: Story = () => <HelpResources />;

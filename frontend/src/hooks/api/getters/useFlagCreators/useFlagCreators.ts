4bbe8c51707cae3142aa9a3703530d1c1c3e9151
Krzysztof (Kris) Kula <kris.kula@getunleash.io>
2026-07-28 11:57:39 +0200

    chore: use correct type for flag creators hook
    using type from orval

--- frontend/src/hooks/api/getters/useFlagCreators/useFlagCreators.ts
+++ frontend/src/hooks/api/getters/useFlagCreators/useFlagCreators.ts
@@ -1,18 +1,13 @@
 import { fetcher } from '../useApiGetter/useApiGetter.js';
 import { useConditionalSWR } from '../useConditionalSWR/useConditionalSWR.js';
 import { formatApiPath } from 'utils/formatPath';
-import type { ProjectFlagCreatorsSchemaItem } from 'openapi';
+import type { FlagCreatorsSchema } from 'openapi';
 import { useUiFlag } from 'hooks/useUiFlag';
-
-type FlagCreatorsResponse = {
-    total: number;
-    flagCreators: ProjectFlagCreatorsSchemaItem[];
-};
 
 export const useFlagCreators = () => {
     const enabled = useUiFlag('flagListCreatedByFilter');
     const url = formatApiPath('api/admin/flag-creators?limit=10000');
-    const { data } = useConditionalSWR<FlagCreatorsResponse>(
+    const { data } = useConditionalSWR<FlagCreatorsSchema>(
         enabled,
         { total: 0, flagCreators: [] },
         url,

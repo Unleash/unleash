298430e917dcdb489390cd707d3a9a8a24b65e58
Jaanus <sellinjaanus@gmail.com>
2026-09-08 12:34:31 +0300

    test: drop redundant no-SSO-configured case from new-user tests

--- frontend/src/component/user/NewUser/NewUser.test.tsx
+++ frontend/src/component/user/NewUser/NewUser.test.tsx
@@ -17,20 +17,14 @@ const ssoOptions = [
     },
 ];
 
-const setupAuthDetails = ({
-    defaultHidden = false,
-    options = ssoOptions,
-}: {
-    defaultHidden?: boolean;
-    options?: typeof ssoOptions;
-} = {}) => {
+const setupAuthDetails = (defaultHidden = false) => {
     testServerRoute(server, '/api/admin/ui-config', {});
     testServerRoute(server, '/api/admin/user', {
         type: 'password',
         path: '/auth/simple/login',
         message: 'You must sign in in order to use Unleash',
         defaultHidden,
-        options,
+        options: ssoOptions,
     });
 };
 
@@ -60,25 +54,13 @@ test('should not render SSO options when signing up with an invite link', async 
 });
 
 test('should render SSO options for an invite link when password auth is hidden', async () => {
-    setupAuthDetails({ defaultHidden: true });
+    setupAuthDetails(true);
     setupValidInvite();
 
     render(<NewUser />, { route: '/new-user?invite=valid-secret' });
 
     await screen.findByTestId(`${SSO_LOGIN_BUTTON}-oidc`);
     expect(screen.queryByLabelText(/Full name/)).not.toBeInTheDocument();
-});
-
-test('should not render SSO options for an invite link when no SSO is configured', async () => {
-    setupAuthDetails({ options: [] });
-    setupValidInvite();
-
-    render(<NewUser />, { route: '/new-user?invite=valid-secret' });
-
-    await screen.findByLabelText(/Full name/);
-    expect(
-        screen.queryByTestId(`${SSO_LOGIN_BUTTON}-oidc`),
-    ).not.toBeInTheDocument();
 });
 
 test('should render SSO options when resetting a password', async () => {
